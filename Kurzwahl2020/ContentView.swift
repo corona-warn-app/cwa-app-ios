a79ed69d2aeaffa4ece3cd4828d38a250774a647
tcfos <andreas@tcfos.org>
2019-10-25 22:59:43 +0200

    Initial Commit

---
+++ Kurzwahl2020/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kurzwahl2020
+//
+//  Created by Vogel, Andreas on 25.10.19.
+//  Copyright © 2019 Vogel, Andreas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kurzwahl2020Tests/Kurzwahl2020Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Kurzwahl2020Tests.swift
+//  Kurzwahl2020Tests
+//
+//  Created by Vogel, Andreas on 25.10.19.
+//  Copyright © 2019 Vogel, Andreas. All rights reserved.
+//
+
+import XCTest
+@testable import Kurzwahl2020
+
+class Kurzwahl2020Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
