2397d7ec9065d561e64b7253592f1950cdd92bb7
tcfos <andreas@tcfos.org>
2019-10-31 08:47:20 +0100

    Grid

--- Kurzwahl2020/ContentView.swift
+++ Kurzwahl2020/ContentView.swift
@@ -9,10 +9,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    let hsize: CGFloat = 180
+    let vsize: CGFloat = 80
+    let border: CGFloat = 1
+    let fontsize: CGFloat = 26
+    
     var body: some View {
-        Text("Hello World")
+        VStack(spacing: 0) {
+            HStack(spacing: 10) {
+                Text("Andreas Vogel")
+                    .font(.system(size: 26))
+                    .frame(width: hsize, height: vsize, alignment: .center)
+                    .border(Color.gray, width: border)
+                Text("John Appleseed")
+                    .font(.system(size: 26))
+                    .frame(width: hsize, height: vsize, alignment: .center)
+                    .border(Color.gray, width: border)
+            } .padding(.bottom, 10)
+            HStack(spacing: 10) {
+                Text("Andreas Vogel")
+                    .font(.system(size: 26))
+                    .frame(width: hsize, height: vsize, alignment: .center)
+                    .border(Color.gray, width: border)
+                Text("John Appleseed")
+                    .font(.system(size: 26))
+                    .frame(width: hsize, height: vsize, alignment: .center)
+                    .border(Color.gray, width: border)
+            } .padding(.bottom, 10)
+            
+        } //.scaledToFill() .offset(x:0,y:0)
+        
     }
 }
+
+// https://iosexample.com/the-missing-swiftui-collection-view/
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
