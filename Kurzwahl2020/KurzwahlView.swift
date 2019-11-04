68e54e74aec5f312f192c4a9da94659e75c1e110
tcfos <andreas@tcfos.org>
2019-11-04 11:48:10 +0100

    Test: use Stack and HStack for drawing the text fields

--- Kurzwahl2020/KurzwahlView.swift
+++ Kurzwahl2020/KurzwahlView.swift
@@ -17,7 +17,7 @@ struct KurzwahlView: View {
     @State private var selection = 0
     
     var body: some View {
-    
+        
         TabView(selection: $selection) {
             GeometryReader { geometry in
                 //Spacer()
@@ -56,6 +56,25 @@ struct KurzwahlView: View {
                 }
             }
             .tag(1)
+            
+//            GeometryReader { geometry in
+//                QGrid(Storage.people,
+//                      columns: 2, vSpacing: 2, hSpacing: 2,
+//                      vPadding: 0, hPadding: 0 )
+//                { GridCell(person: $0,
+//                           height: geometry.size.height / 6 - 2 ,
+//                           width: geometry.size.width / 2 - 2 ,
+//                           pageIndex: 6 )
+//                }
+//            }
+//            .tabItem {
+//                VStack {
+//                    Image("first")
+//                    Text("3rd")
+//                }
+//            }
+//            .tag(2)
+            
             //NavigationLink("Hallo", destination: KurzwahlView2())
             //.edgesIgnoringSafeArea(.bottom)
         }
@@ -64,14 +83,11 @@ struct KurzwahlView: View {
 
 
 
-
-
-
 var AssetColorList: [String] = [
-    "OrangeFF9500","Darkblue00398E", "RedFF3A2D",  "RedAC193D", "Green008A00",
-    "OrangeD24726", "Green00A600", "Blue2E8DEF", "Darkgrey6E6E6E", "lightGreyAEAEAE",
-    "DarkViolet5856D6", "grey8E8E8E",
-    
+    "OrangeFF9500","Darkblue00398E", "RedFF3A2D",  "RedAC193D",
+    "Green008A00", "OrangeD24726", "Green00A600", "Blue2E8DEF",
+    "Darkgrey6E6E6E", "lightGreyAEAEAE", "DarkViolet5856D6", "grey8E8E8E",
+    //    2nd screen – lipstick pink
     "E69D95", "E07260", "C83773", "B8A89F",
     "665A5C", "C64247", "B4938C", "EAA598",
     "9B7983", "B897BB", "885D8D", "742E34"
@@ -84,6 +100,7 @@ extension Color {
         return Color.init(name, bundle: nil)
     }
 }
+
 
 struct KurzwahlView_Previews: PreviewProvider {
     static var previews: some View {
