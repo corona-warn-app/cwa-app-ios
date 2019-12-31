b947ec3b55b68ce8fbb89b6d0a8ff0dc8cda71c9
Andreas Vogel <outer.space.mailings@gmx.de>
2019-12-31 15:12:15 +0100

    added links to the web about iOS app groups, examples, ...

--- Kurzwahl2020/Storage.swift
+++ Kurzwahl2020/Storage.swift
@@ -5,7 +5,16 @@
 //  Created by Vogel, Andreas on 30.10.19.
 //  Copyright © 2019 Vogel, Andreas. All rights reserved.
 //
+// iOS Persistence: https://www.iosapptemplates.com/blog/ios-development/data-persistence-ios-swift
 //
+// APPGROUP https://www.avanderlee.com/swift/core-data-app-extension-data-sharing/
+// https://developer.apple.com/library/archive/documentation/General/Conceptual/ExtensibilityPG/ExtensionScenarios.html
+// https://github.com/maximbilan/iOS-Shared-CoreData-Storage-for-App-Groups (Swift 4.2)
+// https://dmtopolog.com/ios-app-extensions-data-sharing/
+// https://medium.com/swift2go/app-groups-as-an-alternative-to-deep-linking-between-your-apps-a8c214c6582a
+// https://blog.zeroseven.de/app-entwicklung/erfolgreiche-veroeffentlichung-einer-ios-app-mit-app-extensions/
+// https://agostini.tech/2017/08/13/sharing-data-between-applications-and-extensions-using-app-groups/
+
 import SwiftUI
 
 
