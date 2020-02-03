3dc2040beb12028c745d3fe33bc4e58b16a867dc
Andreas Vogel <outer.space.mailings@gmx.de>
2020-02-03 20:21:58 +0100

    new file for displaying all numbers of one contact.

---
+++ Kurzwahl2020/ContactDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  ContactDetailView.swift
+//  Kurzwahl2020
+//
+//  Created by Andreas Vogel on 03.02.20.
+//  Copyright © 2020 Vogel, Andreas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContactDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContactDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContactDetailView()
+    }
+}
