0faaff3bb186d6e9eea642d8cc1158a2299417e9
tcfos <andreas@tcfos.org>
2020-02-22 21:05:43 +0100

    ask the user for accessing contacts at app start

--- Kurzwahl2020/AppDelegate.swift
+++ Kurzwahl2020/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Contacts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        CNContactStore().requestAccess(for: .contacts) { (access, error) in
+          print("Access: \(access)")
+        }
         return true
     }
 
