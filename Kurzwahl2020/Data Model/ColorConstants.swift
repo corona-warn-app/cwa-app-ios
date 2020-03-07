5bdac11fef0c7fc1092fd84e5f4a6a7e074b2c26
Andreas Vogel <outer.space.mailings@gmx.de>
2020-03-07 09:26:38 +0100

    doc as ASCI art; Refactoring – color constants as separate file

---
+++ Kurzwahl2020/Data Model/ColorConstants.swift
@@ -0,0 +1,53 @@
+//
+//  ColorConstants.swift
+//  Kurzwahl2020
+//
+//  Created by Andreas Vogel on 07.03.20.
+//  Copyright © 2020 Vogel, Andreas. All rights reserved.
+//
+
+import Foundation
+
+
+// the color names refer to the items in Asset.xcassets
+let ColorPaletteSummer: [String] = [
+    "OrangeFF9500", "Darkblue00398E", "RedFF3A2D",  "RedAC193D",
+    "Green008A00", "OrangeD24726", "Green00A600", "Blue2E8DEF",
+    "Darkgrey6E6E6E", "lightGreyAEAEAE", "DarkViolet5856D6", "grey8E8E8E"]
+
+let ColorPaletteDarkPink: [String] = [
+    "E69D95", "E07260", "C83773", "B8A89F",
+    "665A5C", "C64247", "B4938C", "EAA598",
+    "9B7983", "B897BB", "885D8D", "742E34"]
+
+let ColorPaletteForest: [String] = [
+    "5b6c48","afc1a7",
+    "415740","6b8760",
+    "213522","593649",
+    "7e5ef2","b2e2be",
+    "5d8c60","bfe28b",
+    "c189d9","2b220f"]
+
+let ColorPaletteBlue: [String] = [
+    "Blue003C76","Blue003C76",
+    "Blue00417F","Blue00417F",
+    "Blue00488C","Blue00488C",
+    "Blue004F99","Blue004F99",
+    "Blue0055A6","Blue0055A6",
+    "Blue005CB2","Blue005CB2"]
+
+let ColorPaletteGreen: [String] = [
+    "Green004D00","Green004D00",
+    "Green005900","Green005900",
+    "Green006600","Green006600",
+    "Green007300","Green007300",
+    "Green007F00","Green007F00",
+    "Green008D00","Green008D00"]
+
+let ColorPaletteRed: [String] = [
+    "Red4D0000","Red4D0000",
+    "Red590000","Red590000",
+    "Red660000","Red660000",
+    "Red730000","Red730000",
+    "Red7F0000","Red7F0000",
+    "Red8C0000","Red8C0000"]
