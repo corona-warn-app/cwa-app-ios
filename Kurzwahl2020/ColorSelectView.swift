48287792c348705aec3c6a01e6e7be8f2f7b8462
tcfos <andreas@tcfos.org>
2020-03-09 21:30:11 +0100

    Thumbnail of screen with number of screen

--- Kurzwahl2020/ColorSelectView.swift
+++ Kurzwahl2020/ColorSelectView.swift
@@ -18,7 +18,7 @@ struct ColorSelectView: View {
     @EnvironmentObject var navigation: NavigationStack
     @EnvironmentObject var paletteViewState: PaletteSelectViewState
     @EnvironmentObject var colorManager: ColorManagement
-//    let viewState: PaletteSelectViewState = PaletteSelectViewState()
+    //    let viewState: PaletteSelectViewState = PaletteSelectViewState()
     
     var body: some View {
         VStack{
@@ -33,26 +33,39 @@ struct ColorSelectView: View {
                     .customFont(name: globalDataModel.font, style: .body)
                     .padding(.horizontal)
                 HStack{
-                    Button(action: {
-                        self.navigation.advance(NavigationItem(
-                            view: AnyView(SelectColorPalette(screenIndex: 0)))) }) {
-                                Image(colorManager.getThumbnailName(withIndex: 0)).resizable()
-                                    .frame(width: 100, height: 190)//.padding()
-                    }.buttonStyle(PlainButtonStyle())
-                    
-                    Button(action: {
-                        self.navigation.advance(NavigationItem(
-                            view: AnyView(SelectColorPalette(screenIndex: 1)))) }) {
-                                Image(colorManager.getThumbnailName(withIndex: 1)).resizable()
-                                    .frame(width: 100, height: 190)//.padding()
-                    }.buttonStyle(PlainButtonStyle())
-                    
-                    Button(action: {
-                        self.navigation.advance(NavigationItem(
-                            view: AnyView(SelectColorPalette(screenIndex: 2)))) }) {
-                                Image(colorManager.getThumbnailName(withIndex: 2)).resizable()
-                                    .frame(width: 100, height: 190)//.padding()
-                    }.buttonStyle(PlainButtonStyle())
+                    Spacer()
+                    VStack{
+                        Button(action: {
+                            self.navigation.advance(NavigationItem(
+                                view: AnyView(SelectColorPalette(screenIndex: 0)))) }) {
+                                    Image(colorManager.getThumbnailName(withIndex: 0)).resizable()
+                                        .frame(width: 100, height: 190)//.padding()
+                        }.buttonStyle(PlainButtonStyle())
+                        
+                        Image(systemName: "1.square")
+                        
+                    }
+                    Spacer()
+                    VStack{
+                        Button(action: {
+                            self.navigation.advance(NavigationItem(
+                                view: AnyView(SelectColorPalette(screenIndex: 1)))) }) {
+                                    Image(colorManager.getThumbnailName(withIndex: 1)).resizable()
+                                        .frame(width: 100, height: 190)//.padding()
+                        }.buttonStyle(PlainButtonStyle())
+                        Image(systemName: "2.square")
+                    }
+                    Spacer()
+                    VStack{
+                        Button(action: {
+                            self.navigation.advance(NavigationItem(
+                                view: AnyView(SelectColorPalette(screenIndex: 2)))) }) {
+                                    Image(colorManager.getThumbnailName(withIndex: 2)).resizable()
+                                        .frame(width: 100, height: 190)//.padding()
+                        }.buttonStyle(PlainButtonStyle())
+                        Image(systemName: "3.square")
+                    }
+                    Spacer()
                 }
                 Spacer()
             }
