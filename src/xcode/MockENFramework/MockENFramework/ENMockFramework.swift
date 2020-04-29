e4aea9fa9468ba1a5cb7ac6581522c345ce09f97
Aleksandr Tikhonov <aleksandr.tikhonov@sap.com>
2020-04-29 12:41:05 +0200

    fetched last mock framework changes

--- src/xcode/MockENFramework/MockENFramework/ENMockFramework.swift
+++ src/xcode/MockENFramework/MockENFramework/ENMockFramework.swift
@@ -1,10 +1,9 @@
 /// Modified from TracePrivately project
 
-
 import Foundation
 import UIKit
 
-enum ENErrorCode {
+public enum ENErrorCode {
     case success
     case unknown
     case badParameter
@@ -36,28 +35,9 @@ enum ENErrorCode {
         case .insufficientMemory: return "Insufficient Memory"
         }
     }
-    
-    
-    var localizedDescription: String {
-           switch self {
-           case .success: return "Operation succeeded"
-           case .unknown: return "Unknown"
-           case .badParameter: return "Missing or incorrect parameter"
-           case .notEntitled: return "Calling process doesn't have the correct entitlement"
-           case .notAuthorized: return "User denied this process access to Exposure Notification functionality"
-           case .unsupported: return "Operation is not supported"
-           case .invalidated: return "Invalidate was called before the operation completed normally"
-           case .bluetoothOff: return "Bluetooth was turned off the by user"
-           case .insufficientStorage: return "Insufficient storage space to enable exposure notification"
-           case .notEnabled: return "Exposure Notification has not been enabled"
-           case .apiMisuse: return "API was used incorrectly"
-           case .internalError: return "Internal error indicating a bug in this framework."
-           case .insufficientMemory: return "Not enough memory to perform an operation"
-           }
-       }
 }
 
-struct ENError: LocalizedError {
+public struct ENError: LocalizedError {
     let errorCode: ENErrorCode
     
     var localizedDescription: String {
@@ -65,25 +45,22 @@ struct ENError: LocalizedError {
     }
 }
 
-///An enumeration that specifies the app's preference for authorization with Exposure Notification
-enum ENAuthorizationMode {
+public enum ENAuthorizationMode {
     case defaultMode
     case nonUi
     case ui
 }
 
-///An enumeration that indicates the status of authorization for the app
-enum ENAuthorizationStatus {
+public enum ENAuthorizationStatus {
     case unknown
     case restricted
     case notAuthorized
     case authorized
 }
 
-typealias ENErrorHandler = ((Error?) -> Void)
+public typealias ENErrorHandler = ((Error?) -> Void)
 
-/// A protocol for objects that support asynchronous operations and cancellation.
-protocol ENActivatable {
+public protocol ENActivatable {
     var dispatchQueue: DispatchQueue? { get set }
     var invalidationHandler: (() -> Void)? { get set }
     
@@ -91,16 +68,14 @@ protocol ENActivatable {
     func invalidate()
 }
 
-/// A protocol for objects that require authorization from the user before they can be used
-protocol ENAuthorizable {
+public protocol ENAuthorizable {
     var authorizationStatus: ENAuthorizationStatus { get }
     var authorizationMode: ENAuthorizationMode { get set }
 }
 
-typealias ENMultiState = Bool
+public typealias ENMultiState = Bool
 
-/// Defines nonmodifiable settings for Exposure Notification
-open class ENSettings: NSObject {
+public class ENSettings {
     let enableState: ENMultiState
     
     init(enableState: ENMultiState) {
@@ -108,13 +83,11 @@ open class ENSettings: NSObject {
     }
 }
 
-///Defines modifiable settings for Exposure Notification.
-open class ENMutableSettings: ENSettings {
+public class ENMutableSettings: ENSettings {
 
 }
 
-///Requests the current settings for Exposure Notification
-open class ENSettingsGetRequest: ENBaseRequest {
+public class ENSettingsGetRequest: ENBaseRequest {
     private var _settings: ENSettings? = nil
     
     var settings: ENSettings? {
@@ -131,7 +104,7 @@ open class ENSettingsGetRequest: ENBaseRequest {
 
     }
     
-    override func activate(queue: DispatchQueue, completion: @escaping (Error?) -> Void) {
+    override fileprivate func activate(queue: DispatchQueue, completion: @escaping (Error?) -> Void) {
         queue.async {
             self.settings = ENSettings(enableState: ENInternalState.shared.tracingEnabled)
             completion(nil)
@@ -139,9 +112,7 @@ open class ENSettingsGetRequest: ENBaseRequest {
     }
 }
 
-
-///Changes settings for Exposure Notification after authorization by the user.
-open class ENSettingsChangeRequest: ENAuthorizableBaseRequest {
+public class ENSettingsChangeRequest: ENAuthorizableBaseRequest {
     let settings: ENSettings
     
     override var permissionDialogMessage: String? {
@@ -156,7 +127,7 @@ open class ENSettingsChangeRequest: ENAuthorizableBaseRequest {
         return self.settings.enableState == true
     }
     
-    override func activateWithPermission(queue: DispatchQueue, completion: @escaping (Error?) -> Void) {
+    override fileprivate func activateWithPermission(queue: DispatchQueue, completion: @escaping (Error?) -> Void) {
         queue.async {
             ENInternalState.shared.tracingEnabled = self.settings.enableState
             completion(nil)
@@ -164,12 +135,11 @@ open class ENSettingsChangeRequest: ENAuthorizableBaseRequest {
     }
 }
 
-typealias ENIntervalNumber = Int
+public typealias ENIntervalNumber = UInt32
 
-///The key used to generate Rolling Proximity Identifiers
-open class ENTemporaryExposureKey: NSObject {
-    var keyData: Data
-    var rollingStartNumber: ENIntervalNumber
+public struct ENTemporaryExposureKey {
+    let keyData: Data
+    let rollingStartNumber: ENIntervalNumber
 }
 
 extension ENTemporaryExposureKey {
@@ -208,37 +178,31 @@ extension ENTemporaryExposureKey {
     }
 }
 
-///Provides a summary of exposures.
-open class ENExposureDetectionSummary: NSObject {
-    var daysSinceLastExposure: Int
-    var matchedKeyCount: UInt64
+public struct ENExposureDetectionSummary {
+    let daysSinceLastExposure: Int
+    let matchedKeyCount: UInt64
 }
 
-typealias ENExposureDetectionFinishCompletion = ((ENExposureDetectionSummary?, Swift.Error?) -> Void)
+public typealias ENExposureDetectionFinishCompletion = ((ENExposureDetectionSummary?, Swift.Error?) -> Void)
 
-typealias ENExposureDetectionGetExposureInfoCompletion = (([ENExposureInfo]?, Bool, Swift.Error?) -> Void)
+public typealias ENExposureDetectionGetExposureInfoCompletion = (([ENExposureInfo]?, Bool, Swift.Error?) -> Void)
 
-///Performs exposure detection based on previously collected proximity data and keys.
-open class ENExposureDetectionSession: ENBaseRequest {
+public class ENExposureDetectionSession: ENBaseRequest {
     var attenuationThreshold: UInt8 = 0
     var durationThreshold: TimeInterval = 0
-    var maxKeyCount: Int = 0
+    var maxKeyCount: Int = 10
     
     private var _infectedKeys: [ENTemporaryExposureKey] = []
 
-    private static let maximumFakeMatches = 1
+    private static let maximumFakeMatches = 5
     
     private var remoteInfectedKeys: [ENTemporaryExposureKey] {
         // Filters out keys for local device for the purposes of better testing
         
-        let localDeviceId = ENInternalState.shared.localDeviceId
+        let localDeviceId = ENInternalState.shared.localDeviceId.data
         
         return self._infectedKeys.filter { key in
-            guard let str = key.stringValue else {
-                return false
-            }
-            
-            return !str.hasPrefix(localDeviceId)
+            return key.keyData != localDeviceId
         }
     }
 
@@ -273,6 +237,8 @@ open class ENExposureDetectionSession: ENBaseRequest {
 
     }
     
+    private var cursor: Int = 0
+    
     func getExposureInfoWithMaxCount(maxCount: UInt32, completion: @escaping ENExposureDetectionGetExposureInfoCompletion) {
         
         let queue: DispatchQueue = self.dispatchQueue ?? .main
@@ -281,56 +247,59 @@ open class ENExposureDetectionSession: ENBaseRequest {
                 
         queue.asyncAfter(deadline: .now() + delay) {
             guard !self.isInvalidated else {
+                self.cursor = 0
                 completion(nil, true, ENError(errorCode: .invalidated))
                 return
             }
 
             // For now this is assuming that every key is infected. Obviously this isn't accurate, just useful for testing.
-            let keys: [ENTemporaryExposureKey] = enQueue.sync { self.remoteInfectedKeys }
-                    
-            let calendar = Calendar(identifier: .gregorian)
+            let allKeys: [ENTemporaryExposureKey] = enQueue.sync { self.remoteInfectedKeys }
+            
+            guard allKeys.count > 0 else {
+                completion([], true, nil)
+                return
+            }
+            
+            let allMatchedKeys: [ENTemporaryExposureKey] = Array(allKeys[0 ..< min(Self.maximumFakeMatches, allKeys.count)])
+            
+            let fromIndex = self.cursor
+            let toIndex   = min(allMatchedKeys.count, self.cursor + Int(maxCount))
+            
+            guard fromIndex < toIndex else {
+                self.cursor = 0
+                completion([], true, nil)
+                return
+            }
+            
+            let keys = Array(allMatchedKeys[fromIndex ..< toIndex])
             
             let contacts: [ENExposureInfo] = keys.compactMap { key in
-                        
-                guard var dc = key.ymd else {
-                    return nil
-                }
-                
-                dc.hour = 12
-                dc.minute = 12
-                dc.second = 0
-                
-                guard let date = calendar.date(from: dc) else {
-                    return nil
-                }
-                
+
+                let date = Date(timeIntervalSince1970: TimeInterval(key.rollingStartNumber * 600))
                 let duration: TimeInterval = 15 * 60
+
                 return ENExposureInfo(attenuationValue: 0, date: date, duration: duration)
             }
-                    
-            let numItems = min(Self.maximumFakeMatches, contacts.count)
             
-            if numItems == 0 {
-                completion([], true, nil)
-            }
-            else {
-                completion(Array(contacts[0 ..< numItems ]), true, nil)
-            }
+            let inDone = toIndex >= allMatchedKeys.count
+            self.cursor = inDone ? 0 : toIndex
+            
+            completion(contacts, inDone, nil)
         }
     }
 }
 
-open class ENExposureInfo: NSObject {
-    var attenuationValue: UInt8
-    var date: Date
-    var duration: TimeInterval
+public struct ENExposureInfo {
+    let attenuationValue: UInt8
+    let date: Date
+    let duration: TimeInterval
 }
 
-open class ENSelfExposureInfo: NSObject{
-    var keys: [ENTemporaryExposureKey]
+public struct ENSelfExposureInfo {
+    let keys: [ENTemporaryExposureKey]
 }
 
-class ENSelfExposureInfoRequest: ENAuthorizableBaseRequest {
+public class ENSelfExposureInfoRequest: ENAuthorizableBaseRequest {
     private var _selfExposureInfo: ENSelfExposureInfo?
     
     var selfExposureInfo: ENSelfExposureInfo? {
@@ -350,15 +319,13 @@ class ENSelfExposureInfoRequest: ENAuthorizableBaseRequest {
         return "Allow this app to retrieve your anonymous tracing keys?"
     }
     
-    override func activateWithPermission(queue: DispatchQueue, completion: @escaping (Error?) -> Void) {
+    override fileprivate func activateWithPermission(queue: DispatchQueue, completion: @escaping (Error?) -> Void) {
         
         let delay: TimeInterval = 0.5
         
         queue.asyncAfter(deadline: .now() + delay) {
             
-            let keys: [ENTemporaryExposureKey] = ENInternalState.shared.dailyKeys.map { ENTemporaryExposureKey(keyData: $0, rollingStartNumber: 0) }
-            
-            let info = ENSelfExposureInfo(keys: keys)
+            let info = ENSelfExposureInfo(keys: ENInternalState.shared.dailyKeys)
             self.selfExposureInfo = info
             
             completion(nil)
@@ -366,23 +333,27 @@ class ENSelfExposureInfoRequest: ENAuthorizableBaseRequest {
     }
 }
 
-open class ENSelfExposureResetRequest: ENAuthorizableBaseRequest {
+public class ENSelfExposureResetRequest: ENAuthorizableBaseRequest {
     
     override var permissionDialogMessage: String? {
         return "Allow this app to reset your anonymous tracing keys?"
     }
 
-    override func activateWithPermission(queue: DispatchQueue, completion: @escaping (Error?) -> Void) {
-        queue.async {
+    override fileprivate func activateWithPermission(queue: DispatchQueue, completion: @escaping (Error?) -> Void) {
+        
+        print("Resetting keys ...")
+        queue.asyncAfter(deadline: .now() + 0.5) {
+            print("Finished resetting keys")
+            
             // Nothing to do since we're generating fake stable keys for the purpose of testing
             completion(nil)
         }
     }
 }
 
-class ENAuthorizableBaseRequest: ENBaseRequest, ENAuthorizable {
-    var authorizationStatus: ENAuthorizationStatus = .unknown
-    var authorizationMode: ENAuthorizationMode = .defaultMode
+public class ENAuthorizableBaseRequest: ENBaseRequest, ENAuthorizable {
+    public var authorizationStatus: ENAuthorizationStatus = .unknown
+    public var authorizationMode: ENAuthorizationMode = .defaultMode
     
     fileprivate var permissionDialogTitle: String? {
         return nil
@@ -395,7 +366,7 @@ class ENAuthorizableBaseRequest: ENBaseRequest, ENAuthorizable {
         return true
     }
     
-    final override func activate(queue: DispatchQueue, completion: @escaping (Error?) -> Void) {
+    final override fileprivate func activate(queue: DispatchQueue, completion: @escaping (Error?) -> Void) {
         
         if self.shouldPrompt {
             DispatchQueue.main.async {
@@ -432,7 +403,7 @@ class ENAuthorizableBaseRequest: ENBaseRequest, ENAuthorizable {
         }
     }
     
-    func activateWithPermission(queue: DispatchQueue, completion: @escaping (Error?) -> Void) {
+    fileprivate func activateWithPermission(queue: DispatchQueue, completion: @escaping (Error?) -> Void) {
         queue.async {
             print("Should be overridden")
             completion(nil)
@@ -442,13 +413,13 @@ class ENAuthorizableBaseRequest: ENBaseRequest, ENAuthorizable {
 
 fileprivate let enQueue = DispatchQueue(label: "TracePrivately", qos: .default, attributes: [])
 
-class ENBaseRequest: ENActivatable, NSObject {
+public class ENBaseRequest: ENActivatable {
     /// This property holds the the dispatch queue used to invoke handlers on. If this property isn’t set, the framework uses the main queue.
-    var dispatchQueue: DispatchQueue?
+    public var dispatchQueue: DispatchQueue?
     
     private var _invalidationHandler: (() -> Void)?
     
-    var invalidationHandler: (() -> Void)? {
+    public var invalidationHandler: (() -> Void)? {
         get {
             return enQueue.sync {
                 return self._invalidationHandler
@@ -489,7 +460,7 @@ class ENBaseRequest: ENActivatable, NSObject {
         }
     }
 
-    final func activateWithCompletion(_ completion: @escaping (Swift.Error?) -> Void) {
+    final public func activateWithCompletion(_ completion: @escaping (Swift.Error?) -> Void) {
         let queue: DispatchQueue = self.dispatchQueue ?? .main
         
         self.isRunning = true
@@ -512,7 +483,7 @@ class ENBaseRequest: ENActivatable, NSObject {
         }
     }
     
-    func invalidate() {
+    public func invalidate() {
         self.isInvalidated = true
         
         let queue: DispatchQueue = self.dispatchQueue ?? .main
@@ -547,18 +518,16 @@ private class ENInternalState {
         
     }
     
-    // This is only for testing as it would otherwise be considered identifiable. This class is purely
+    // This is only for testing as it would otherwise be considered identifiable. This public class is purely
     // a mock implementation of Apple's framework, so allowances like this are made in order to help
     // develop and test.
-    fileprivate lazy var localDeviceId: String = {
-        return UIDevice.current.identifierForVendor!.uuidString
+    fileprivate lazy var localDeviceId: UUID = {
+        return UIDevice.current.identifierForVendor!
     }()
     
     // These keys are stable for this device as they use a device specific ID with an index appended
-    var dailyKeys: [Data] {
+    var dailyKeys: [ENTemporaryExposureKey] {
         return enQueue.sync {
-            
-            var keys: [String] = []
             
             let deviceId = self.localDeviceId
             
@@ -573,23 +542,34 @@ private class ENInternalState {
                 return []
             }
             
+            var keys: [ENTemporaryExposureKey] = []
+            
+            let keyData = deviceId.data
+            
             for idx in 0 ..< 14 {
                 guard let date = calendar.date(byAdding: .day, value: -idx, to: todayMidday, wrappingComponents: false) else {
                     continue
                 }
+                
+//                let dc = calendar.dateComponents([ .day, .month, .year ], from: date)
+//
+//                let dateStr = String(format: "%04d%02d%02d", dc.year!, dc.month!, dc.day!)
+//
+//                let str = deviceId + "_" + dateStr
+//
+//                guard let keyData = str.data(using: .utf8) else {
+//                    continue
+//                }
+                
+                let intervalNumber = ENIntervalNumber(date.timeIntervalSince1970 / 600)
+                let rollingStartNumber = intervalNumber / 144 * 144
 
-                let dc = calendar.dateComponents([ .day, .month, .year ], from: date)
-                
-                let dateStr = String(format: "%04d%02d%02d", dc.year!, dc.month!, dc.day!)
-                
-                let str = deviceId + "_" + dateStr
-                keys.append(str)
-                
+                keys.append(ENTemporaryExposureKey(keyData: keyData, rollingStartNumber: rollingStartNumber))
             }
             
             print("Generated keys: \(keys)")
-            
-            return keys.compactMap { $0.data(using: .utf8) }
+
+            return keys
         }
     }
 
