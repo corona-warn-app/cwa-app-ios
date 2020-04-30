12318efd5c032bd62ebee1ed7a18e40b6454e331
Hao Hu <hao.hu@sap.com>
2020-04-30 13:28:34 +0200

    Add view controller for ExposureNotifcation setting.

---
+++ src/xcode/ENA/ENA/Source/Scenes/ENSetting/ExposureNotificationSettingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ExposureNotificationSettingViewController.swift
+//  ENA
+//
+//  Created by Hu, Hao on 30.04.20.
+//  Copyright © 2020 SAP SE. All rights reserved.
+//
+
+import UIKit
+
+class ExposureNotificationSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
