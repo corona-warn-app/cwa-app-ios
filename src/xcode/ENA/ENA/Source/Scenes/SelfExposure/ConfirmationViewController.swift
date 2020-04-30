6654dc9b1a6f00f1826a46061cd1983ce316f212
Adnan Zildzic <adnan.zildzic@sap.com>
2020-04-30 18:28:43 +0200

    Added Tan Entry and Confirmation views.

---
+++ src/xcode/ENA/ENA/Source/Scenes/SelfExposure/ConfirmationViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ConfirmationViewController.swift
+//  ENA
+//
+//  Created by Zildzic, Adnan on 30.04.20.
+//  Copyright © 2020 SAP SE. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmationViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ src/xcode/ENA/ENA/Source/Scenes/SelfExposure/TanEntryViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TanEntryViewController.swift
+//  ENA
+//
+//  Created by Zildzic, Adnan on 30.04.20.
+//  Copyright © 2020 SAP SE. All rights reserved.
+//
+
+import UIKit
+
+class TanEntryViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var tanTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func submitClicked(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
