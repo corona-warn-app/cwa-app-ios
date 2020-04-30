d25399d2a04d5bd4c9e2d096f75583f4ad1db26c
Conrad Steinmetz <conrad.steinmetz@sap.com>
2020-04-30 15:19:46 +0200

    few changes

--- src/xcode/ENA/ENA/Source/Scenes/Settings/SettingsViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Settings/SettingsViewController.swift
@@ -6,14 +6,33 @@
 //  Copyright © 2020 SAP SE. All rights reserved.
 //
 
+import ExposureNotification
 import UIKit
 
 class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var trackingStatusLabel: UILabel!
+    @IBOutlet weak var notificationsSwitch: UISwitch!
+    @IBOutlet weak var dataInWifiOnlySwitch: UISwitch!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+
+    private func setTrackingStatus(for status: ENStatus) {
+        switch status {
+        case .active:
+            DispatchQueue.main.async {
+                self.trackingStatusLabel.text = "Aktiv"
+            }
+        default:
+            DispatchQueue.main.async {
+                self.trackingStatusLabel.text = "Inaktiv"
+            }
+        }
     }
 
 }
