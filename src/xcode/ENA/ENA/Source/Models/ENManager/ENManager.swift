79e372fa5fd526949c715701af6eb2137299e00c
Conrad Steinmetz <conrad.steinmetz@sap.com>
2020-05-01 18:30:46 +0200

    ENManager wrapper according to singleton pattern

---
+++ src/xcode/ENA/ENA/Source/Models/ENManager/ENManager.swift
@@ -0,0 +1,16 @@
+//
+//  ENManager.swift
+//  ENA
+//
+//  Created by Steinmetz, Conrad on 01.05.20.
+//  Copyright © 2020 SAP SE. All rights reserved.
+//
+
+import ExposureNotification
+import Foundation
+
+final class ENManager {
+    static let sharedManager = ENManager()
+
+    private init() {}
+}
