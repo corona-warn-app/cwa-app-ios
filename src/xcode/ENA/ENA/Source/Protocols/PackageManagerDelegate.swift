786b0bf5d347701aba80d5c278fd4d268bf9104a
Marc Bormeth <marc.bormeth@sap.com>
2020-05-01 13:40:21 +0200

    Add PackageManagerDelegate

---
+++ src/xcode/ENA/ENA/Source/Protocols/PackageManagerDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  PackageManagerDelegate.swift
+//  ENA
+//
+//  Created by Bormeth, Marc on 01.05.20.
+//  Copyright © 2020 SAP SE. All rights reserved.
+//
+
+import ExposureNotification
+
+protocol PackageManagerDelegate: class {
+    func didDownloadPackages(_ sender: PackageManager, result: [ENTemporaryExposureKey])
+}
