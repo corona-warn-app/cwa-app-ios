c32e7ea2f212e584688ee114c855a8758d799744
Conrad Steinmetz <conrad.steinmetz@sap.com>
2020-05-02 18:46:15 +0200

    add colors light and dark variant

---
+++ src/xcode/ENA/ENA/Source/Extensions/UIColor.swift
@@ -0,0 +1,85 @@
+//
+//  UIColor.swift
+//  ENA
+//
+//  Created by Steinmetz, Conrad on 02.05.20.
+//  Copyright © 2020 SAP SE. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+
+    public class func preferredColor(for style: ColorStyle, variant: UIUserInterfaceStyle = .light) -> UIColor {
+
+        switch variant {
+        case .light:
+            return preferredColorLightVariant(for: style)
+        case .dark:
+            return preferredColorDarkVariant(for: style)
+        default:
+            return preferredColorLightVariant(for: style)
+        }
+    }
+
+    private class func preferredColorLightVariant(for style: ColorStyle) -> UIColor {
+        switch style {
+        case .darkText:
+            return .darkText
+        case .lightText:
+            return .lightGray
+        case .tintColor:
+            return .systemBlue
+
+        case .separator:
+            return .systemGray2
+        case .border:
+            return .systemGray6
+        case .shadow:
+            return .systemGray3
+
+        case .backgroundBase:
+            return systemBackground
+        case .backgroundContrast:
+            return systemGroupedBackground
+
+        case .positive:
+            return .systemGreen
+        case .negative:
+            return .systemRed
+        case .critical:
+            return .systemOrange
+        }
+    }
+
+    private class func preferredColorDarkVariant(for style: ColorStyle) -> UIColor {
+        switch style {
+        case .darkText:
+            return .white
+        case .lightText:
+            return .lightText
+        case .tintColor:
+            return .systemBlue
+
+        case .separator:
+            return .systemGray2
+        case .border:
+            return .systemGray6
+        case .shadow:
+            return .systemGray3
+
+        case .backgroundBase:
+            return systemBackground
+        case .backgroundContrast:
+            return systemGroupedBackground
+
+        case .positive:
+            return .systemGreen
+        case .negative:
+            return .systemRed
+        case .critical:
+            return .systemOrange
+        }
+    }
+}
