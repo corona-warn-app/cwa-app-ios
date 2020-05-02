24ea77ba0e2373643ca0c477007409ebca0bc9a8
Aleksandr Tikhonov <aleksandr.tikhonov@sap.com>
2020-05-03 01:06:07 +0200

    removed BLE, refactoring onboarding

--- src/xcode/ENA/ENA/Source/Scenes/Onboarding/OnboardingInfoViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Onboarding/OnboardingInfoViewController.swift
@@ -7,6 +7,11 @@
 //
 
 import UIKit
+import ExposureNotification
+
+protocol OnboardingInfoViewControllerDelegate: AnyObject {
+    func didFinished(onboardingInfoViewController: OnboardingInfoViewController)
+}
 
 class OnboardingInfoViewController: UIViewController {
     
@@ -14,7 +19,9 @@ class OnboardingInfoViewController: UIViewController {
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var textView: UITextView!
     
-    var onboardingInfo: OnboardingInfo? {
+    weak var delegate: OnboardingInfoViewControllerDelegate?
+    
+    var onboardingInfo: OnboardingInfo! {
         didSet {
             updateUI()
         }
@@ -23,22 +30,58 @@ class OnboardingInfoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
+        
+        // should be revised in the future
+        viewRespectsSystemMinimumLayoutMargins = false
+        view.layoutMargins = .zero
+    }
+    
+    func run(index: Int) {
+        let closure: () -> Void = {
+            self.delegate?.didFinished(onboardingInfoViewController: self)
+        }
+        if index == 2 {
+            askExposureNotificationsPermissions(completion: closure)
+        } else {
+            closure()
+        }
     }
     
     private func updateUI() {
         guard isViewLoaded else { return }
-        titleLabel.text = onboardingInfo?.title
-        if let imageName = onboardingInfo?.imageName {
-            imageView.image = UIImage(named: imageName)
-        } else {
-            imageView.image = nil
-        }
-        textView.text = onboardingInfo?.text
+        guard let onboardingInfo = onboardingInfo else { return }
+        // here is big onboarding text, should it be translated?
+        // TODO: localize
+        titleLabel.text = onboardingInfo.title
+        imageView.image = UIImage(named: onboardingInfo.imageName)
+        textView.text = onboardingInfo.text
     }
-}
-
-extension OnboardingInfoViewController: OnboardingNextPageAvailable {
-    func isNextPageAvailable() -> Bool {
-        true
+    
+    
+    // MARK: Exposure notifications
+    
+    private func askExposureNotificationsPermissions(completion: (() -> Void)?) {
+        // still in the development
+        ExposureManager.shared.manager.setExposureNotificationEnabled(true) { error in
+            if let error = error as? ENError, error.code == .notAuthorized {
+                print("Encourage the user to consider enabling Exposure Notifications.")
+                completion?()
+            } else if let error = error {
+                self.showError(error, from: self, completion: completion)
+            } else {
+                completion?()
+            }
+        }
+    }
+    
+    func openSettings() {
+        guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+    
+    func showError(_ error: Error, from viewController: UIViewController, completion: (() -> Void)?) {
+        let alert = UIAlertController(title: "Error", message: String(describing: error), preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+        viewController.present(alert, animated: true, completion: completion)
     }
 }
