9c33a1d4fd0c140d9fc03d911d24c4bc0b3f38e6
Hao Hu <hao.hu@sap.com>
2020-05-03 13:48:32 +0200

    Add logic to check if the ENS enabled.

--- src/xcode/ENA/ENA/Source/Scenes/ENSetting/ExposureNotificationSettingViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ENSetting/ExposureNotificationSettingViewController.swift
@@ -10,22 +10,130 @@ import UIKit
 import ExposureNotification
 
 class ExposureNotificationSettingViewController: UIViewController {
-
+    @IBOutlet weak var contactTracingSwitch: UISwitch!
+    
+    //TODO: This should be checked later.
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        //TODO: Subscribe to the even when the app from backendground to foreground should also check the statsu.
+        
+        NotificationCenter
+            .default
+            .addObserver(forName: UIApplication.willEnterForegroundNotification, object: nil, queue: .main) { notifcation in
+                print("[viewDidLoad]: willEnterForegroundNotification, checking notifcation status.")
+                self.checkNotifcationEnablement()
+        }
+                
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        checkNotifcationEnablement()
     }
-    */
+    
+    
+    deinit {
+        print("[ExposureNotificationSettingViewController deinit] got called.")
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+}
 
+extension ExposureNotificationSettingViewController {
+    
+    @IBAction func contactTracingValueChanged(_ sender: Any) {
+                
+        let enManager = ENManager()
+        enManager.activate { error in
+            
+            if let error = error as NSError? {
+                self.alertError(message: error.localizedDescription, title: "Error")
+                return
+            }
+            
+            assert(enManager.exposureNotificationStatus != .unknown)
+            let isEnable = self.contactTracingSwitch.isOn
+            enManager.setExposureNotificationEnabled(isEnable) { error in
+                
+                if let error = error as? ENError {
+                    if error.code == .notAuthorized {
+                        //TODO:Tell the user to enable it on the setting, It can help users to jump to the settings page.
+                        print("[contactTracingValueChanged]: Tell the user to enable it on the setting")
+                    }
+                    
+                    print("[contactTracingValueChanged] Error occurs, while setExposureNotificationEnabled. Error code is \(error.code.rawValue) ")
+                }
+                
+                
+                //Check status again.
+                self.checkNotifcationStatus(for: enManager)
+                enManager.invalidate()
+            }
+        }
+        
+        enManager.invalidationHandler = {
+            
+            //Oberserver the behaviour of ENManager.
+            print("[contactTracingValueChanged]: EnManaber invalid")
+        }
+        
+    }
+}
+
+extension ExposureNotificationSettingViewController {
+    
+    private func checkNotifcationEnablement(){
+        
+        let enManager = ENManager()
+        
+        enManager.activate { (error) in
+                    
+            if let error = error as NSError? {
+                print("[contactTracingValueChanged]: \(error.localizedDescription)")
+                return
+            }
+            
+            assert(enManager.exposureNotificationStatus != .unknown)
+            let exposureEnabled = enManager.exposureNotificationEnabled
+            self.contactTracingSwitch.setOn(exposureEnabled, animated: true)
+            enManager.invalidate()
+            
+        }
+        
+        enManager.invalidationHandler = {
+            //Oberserver the behaviour of ENManager.
+            print("[checkNotifcationEnablement]: EnManaber invalidationHandler got called.")
+        }
+    }
+    
+    
+    private func checkNotifcationStatus(for enManager:ENManager) {
+        assert(enManager.exposureNotificationStatus != .unknown)
+        let status = enManager.exposureNotificationStatus
+        if status == .active || status == .disabled{
+            let isEnable = enManager.exposureNotificationEnabled
+            contactTracingSwitch.setOn(isEnable, animated: true)
+            } else {
+            switch status {
+                case .bluetoothOff:
+                    alertError(message: "Bluetooth is off", title: "Error")
+                    break
+//                case .disabled:
+//                    //Q: What is the different between this disable and the exposureNotificationEnabled?
+//                    alertError(message: "Exposure Notification is disabled", title: "Error")
+//                    break
+                case .restricted:
+                    alertError(message: "Exposure Notification is not active due to system restrictions, such as parental controls", title: "Error")
+                    break
+                case .unknown:
+                    alertError(message: "Status of Exposure Notification is unknown. This is the status before ENManager has activated successfully", title: "Error")
+                    break
+                default:
+                    break
+            }
+        }
+    }
 }
