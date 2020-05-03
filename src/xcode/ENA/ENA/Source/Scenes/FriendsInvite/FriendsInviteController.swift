4dbff90efda9d8d7e4e5dff562908672d8c6a231
Conrad Steinmetz <conrad.steinmetz@sap.com>
2020-05-03 18:54:00 +0200

    initial commit

---
+++ src/xcode/ENA/ENA/Source/Scenes/FriendsInvite/FriendsInviteController.swift
@@ -0,0 +1,15 @@
+//
+//  FriendsInviteController.swift
+//  ENA
+//
+//  Created by Steinmetz, Conrad on 03.05.20.
+//  Copyright © 2020 SAP SE. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FriendsInviteController: UIViewController {
+
+    
+}
