15a17451ac066385875be0cabc5325181c8232f7
Hao Hu <hao.hu@sap.com>
2020-05-04 15:40:15 +0200

    add final keyword and documentation.

--- src/xcode/ENA/ENA/Source/Views/ENASwitch.swift
+++ src/xcode/ENA/ENA/Source/Views/ENASwitch.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ENASwitch: UISwitch {
+/// A Switch UI control which has the same behavior of UISwitch, but with different tint color.
+final class ENASwitch: UISwitch {
     override init(frame: CGRect) {
         super.init(frame: frame)
         customizeSwitch()
