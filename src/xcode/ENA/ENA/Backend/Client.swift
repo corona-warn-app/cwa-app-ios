d2c003948ea259e1dab97f3656eef2b8ebac9e67
Christian Kienle <christian.kienle@sap.com>
2020-05-06 11:51:01 +0200

    remove _Client

--- src/xcode/ENA/ENA/Backend/Client.swift
+++ src/xcode/ENA/ENA/Backend/Client.swift
@@ -8,15 +8,27 @@
 import Foundation
 import ExposureNotification
 
+/// Describes how to interfact with the backend.
 protocol Client {
-    typealias ExposureConfigurationCompletionHandler = (Result<ENExposureConfiguration, Error>) -> Void
+    // MARK: Types
     typealias SubmitKeysCompletionHandler = (Error?) -> Void
     typealias FetchKeysCompletionHandler = (Result<[ENTemporaryExposureKey], Error>) -> Void
 
-    /// `completion` will be called on the main queue.
+    // MARK: Interacting with a Client
+
+    // MARK: Getting the Configuration
+    typealias ExposureConfigurationCompletionHandler = (Result<ENExposureConfiguration, Error>) -> Void
+
+    /// Gets the remove exposure configuration. See `ENExposureConfiguration` for more details
+    /// Parameters:
+    /// - completion: Will be called with the remove configuration or an error if something went wrong. The completion handler will always be called on the main thread.
     func exposureConfiguration(completion: @escaping ExposureConfigurationCompletionHandler)
 
-    /// `completion` will be called on the main queue.
+
+    /// Submits exposure keys to the backend. This makes the local information available to the world so that the risk of others can be calculated on their local devices.
+    /// Parameters:
+    /// - keys: An array of `ENTemporaryExposureKey`s  to submit to the backend.
+    /// - tan: A transaction number 
     func submit(keys: [ENTemporaryExposureKey], tan: String, completion: @escaping SubmitKeysCompletionHandler)
 
     /// `completion` will be called on the main queue.
