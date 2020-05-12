644cf8ea9a133f20ab4afcf3b18553133b561ad4
Adnan Zildzic <adnan.zildzic@sap.com>
2020-05-12 15:32:37 +0200

    Created new unit tests files. Fixed existing tests.

---
+++ src/xcode/ENA/ENATests/Client/HTTPClientTests.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPClientTests.swift
+//  ENATests
+//
+//  Created by Zildzic, Adnan on 12.05.20.
+//  Copyright © 2020 SAP SE. All rights reserved.
+//
+
+import XCTest
+
+class HTTPClientTests: XCTestCase {
+
+}
