731a0e3363e436014013b3ce02950e81ccdc7348
Dunne, Liam <liam.dunne@sap.com>
2020-05-14 15:56:03 +0100

    Added ENAUITests.swift back into project

--- src/xcode/ENA/ENAUITests/ENAUITests.swift
+++ src/xcode/ENA/ENAUITests/ENAUITests.swift
@@ -27,7 +27,7 @@ class ENAUITests: XCTestCase {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-
+		
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
