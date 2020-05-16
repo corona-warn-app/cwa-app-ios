d297d48ddf72018b9c649c062b69acbf003aa61f
Aleksandr Tikhonov <aleksandr.tikhonov@sap.com>
2020-05-16 15:36:57 +0200

    removed unused properties

--- src/xcode/ENA/ENA/Source/Models/Home/HomeRiskCellConfigurator/HomeRiskCellPropertyHolder.swift
+++ src/xcode/ENA/ENA/Source/Models/Home/HomeRiskCellConfigurator/HomeRiskCellPropertyHolder.swift
@@ -12,23 +12,17 @@ final class HomeRiskCellPropertyHolder {
     
     let title: String
     let titleColor: UIColor
-    let body: String
-    let date: String?
     let color: UIColor
     let chevronTintColor: UIColor
     let chevronImage: UIImage?
-    let iconImage: UIImage?
     let itemCellConfigurators: [HomeRiskItemViewConfigurator]
     
-    init(title: String, titleColor: UIColor, body: String, date: String?, color: UIColor, chevronTintColor: UIColor, chevronImage: UIImage?, iconImage: UIImage?, itemCellConfigurators: [HomeRiskItemViewConfigurator]) {
+    init(title: String, titleColor: UIColor, color: UIColor, chevronTintColor: UIColor, chevronImage: UIImage?, itemCellConfigurators: [HomeRiskItemViewConfigurator]) {
         self.title = title
         self.titleColor = titleColor
-        self.body = body
-        self.date = date
         self.color = color
         self.chevronTintColor = chevronTintColor
         self.chevronImage = chevronImage
-        self.iconImage = iconImage
         self.itemCellConfigurators = itemCellConfigurators
     }
     
@@ -40,12 +34,9 @@ final class HomeRiskCellPropertyHolder {
                 title: AppStrings.Home.riskCardUnknownTitle,
                 // swiftlint:disable:next discouraged_object_literal
                 titleColor: #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1),
-                body: AppStrings.RiskView.unknownRiskDetail,
-                date: dateString,
                 color: UIColor.preferredColor(for: .unknownRisk),
                 chevronTintColor: .systemBlue,
                 chevronImage: UIImage(systemName: "chevron.right"),
-                iconImage: UIImage(named: "onboarding_ipad"),
                 itemCellConfigurators: []
             )
         case .low:
@@ -53,13 +44,10 @@ final class HomeRiskCellPropertyHolder {
                 title: AppStrings.Home.riskCardLowTitle,
                 // swiftlint:disable:next discouraged_object_literal
                 titleColor: #colorLiteral(red: 0.9090440273, green: 1, blue: 0.8056635857, alpha: 1),
-                body: AppStrings.RiskView.lowRiskDetail,
-                date: dateString,
                 color: UIColor.preferredColor(for: .positive),
                 // swiftlint:disable:next discouraged_object_literal
                 chevronTintColor: #colorLiteral(red: 0.9090440273, green: 1, blue: 0.8056635857, alpha: 1),
                 chevronImage: UIImage(systemName: "chevron.right"),
-                iconImage: UIImage(named: "onboarding_ipad"),
                 itemCellConfigurators: []
             )
         case .high:
@@ -67,13 +55,10 @@ final class HomeRiskCellPropertyHolder {
                 title: AppStrings.Home.riskCardHighTitle,
                 // swiftlint:disable:next discouraged_object_literal
                 titleColor: #colorLiteral(red: 0.06978602707, green: 0.1870820522, blue: 0.3886224329, alpha: 1),
-                body: AppStrings.RiskView.highRiskDetail,
-                date: dateString,
                 color: UIColor.preferredColor(for: .negative),
                 // swiftlint:disable:next discouraged_object_literal
                 chevronTintColor: #colorLiteral(red: 1, green: 0.8961167932, blue: 0.8636761308, alpha: 1),
                 chevronImage: UIImage(systemName: "chevron.right"),
-                iconImage: UIImage(named: "onboarding_ipad"),
                 itemCellConfigurators: []
             )
         case .moderate:
@@ -85,12 +70,9 @@ final class HomeRiskCellPropertyHolder {
             return HomeRiskCellPropertyHolder(
                 title: AppStrings.Home.riskCardModerateTitle,
                 titleColor: titleColor,
-                body: AppStrings.RiskView.moderateRiskDetail,
-                date: dateString,
                 color: color,
                 chevronTintColor: .white,
                 chevronImage: UIImage(systemName: "chevron.right"),
-                iconImage: UIImage(named: "onboarding_ipad"),
                 itemCellConfigurators: [item1, item2, item3]
             )
         }
