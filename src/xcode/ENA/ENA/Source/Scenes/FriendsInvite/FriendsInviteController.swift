b8c686b045709ca59a72e249e9aa9fbe2d60c73e
Adnan Zildzic <adnan.zildzic@sap.com>
2020-05-16 15:23:20 +0200

    Improved sharing via Invite Friends (title, icon, url).

--- src/xcode/ENA/ENA/Source/Scenes/FriendsInvite/FriendsInviteController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/FriendsInvite/FriendsInviteController.swift
@@ -10,10 +10,14 @@ import Foundation
 import LinkPresentation
 import UIKit
 
-final class FriendsInviteController: UIViewController {
+final class FriendsInviteController: UIViewController, UIActivityItemSource {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var inviteButton: ENAButton!
+
+    let shareTitle = AppStrings.InviteFriends.shareTitle
+    // swiftlint:disable:next force_unwrapping
+    let shareUrl = URL(string: AppStrings.InviteFriends.shareUrl)!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,10 +46,46 @@ final class FriendsInviteController: UIViewController {
     }
 
     @IBAction func inviteAction(_ sender: UIButton) {
-        if let url = URL(string: "https://apps.apple.com/de/app/") {
-            let inviteViewController = UIActivityViewController(activityItems: [url], applicationActivities: [])
-            inviteViewController.popoverPresentationController?.sourceView = view
-            present(inviteViewController, animated: true)
+        let inviteViewController = UIActivityViewController(activityItems: [self], applicationActivities: [])
+        inviteViewController.popoverPresentationController?.sourceView = view
+        present(inviteViewController, animated: true)
+    }
+
+    // MARK: UIActivityItemSource
+    func activityViewControllerPlaceholderItem(_ activityViewController: UIActivityViewController) -> Any {
+        return shareTitle
+    }
+
+    func activityViewController(_ activityViewController: UIActivityViewController, itemForActivityType activityType: UIActivity.ActivityType?) -> Any? {
+        return shareUrl
+    }
+
+    func activityViewControllerLinkMetadata(_: UIActivityViewController) -> LPLinkMetadata? {
+        let metadata = LPLinkMetadata()
+
+        metadata.title = shareTitle
+        metadata.url = shareUrl
+        metadata.originalURL = shareUrl
+
+        if let appIcon = appIcon() {
+            metadata.iconProvider = NSItemProvider(object: appIcon)
         }
+
+        return metadata
+    }
+
+    func activityViewController(_ activityViewController: UIActivityViewController, subjectForActivityType activityType: UIActivity.ActivityType?) -> String {
+        return shareTitle
+    }
+
+    // MARK: Private functions
+    private func appIcon() -> UIImage? {
+        if let icons = Bundle.main.infoDictionary?["CFBundleIcons"] as? [String: Any],
+            let icon = icons["CFBundlePrimaryIcon"] as? [String: Any],
+            let iconFiles = icon["CFBundleIconFiles"] as? [String],
+            let lastIcon = iconFiles.last {
+            return UIImage(named: lastIcon)
+        }
+        return nil
     }
 }
