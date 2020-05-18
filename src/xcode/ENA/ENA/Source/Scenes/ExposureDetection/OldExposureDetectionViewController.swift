1ed59972d75f38e9a2ff29578c0d489eea6df74a
Marc-Peter Eisinger <marc-peter.eisinger@sap.com>
2020-05-18 15:58:33 +0200

    Cleaned up project file after merge conflicts.

--- src/xcode/ENA/ENA/Source/Scenes/ExposureDetection/OldExposureDetectionViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  OldExposureDetectionViewController.swift
-//  ENA
-//
-//  Created by Marc-Peter Eisinger on 18.05.20.
-//  Copyright © 2020 SAP SE. All rights reserved.
-//
-
-import Foundation
