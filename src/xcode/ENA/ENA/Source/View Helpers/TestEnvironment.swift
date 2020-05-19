36957f7306d0282f73c4d2b76f88a8914e4ccbda
Dunne, Liam <liam.dunne@sap.com>
2020-05-19 15:46:58 +0100

    Added TestEnvironment struct for checking XCUITest running state

---
+++ src/xcode/ENA/ENA/Source/View Helpers/TestEnvironment.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+struct TestEnvironment {
+	static let shared = TestEnvironment()
+	let isUITesting = (ProcessInfo.processInfo.environment["XCUI"] == "YES")
+	let isOnboarded = ProcessInfo.processInfo.environment["isOnboarded"] == "YES"
+}
