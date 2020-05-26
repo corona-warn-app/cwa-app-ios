67f0fb97be9bd11d605ed4e082cc962629ff6732
Andreas Vogel <outer.space.mailings@gmx.de>
2020-05-26 14:14:57 +0200

    Einstellungen – Hilfe: Beschreibung erweitert

--- Kurzwahl2020/AskForAccessToContactsView.swift
+++ Kurzwahl2020/AskForAccessToContactsView.swift
@@ -20,7 +20,16 @@ struct AskForAccessToContactsView: View {
                                     self.navigation.unwind()
             })
             VStack{
-                Text("In case you want to access your contacts to pick phone numbers then please go to Settings to grant access.").multilineTextAlignment(.leading).customFont(name: globalDataModel.font, style: .body).padding(.horizontal)
+                Text("How to Enter a Number").customFont(name: globalDataModel.font, style: .body)
+                Text("Double tap to enter a phone number and name. Alternatively tap and hold a field. Then choose 'Edit' from the menue.").multilineTextAlignment(.leading).customFont(name: globalDataModel.font, style: .body).padding(.horizontal)
+                Text("")
+                Text("Start a Phone Call").customFont(name: globalDataModel.font, style: .body)
+                
+                Text("Tap to start a phone call. Alternatively tap and hold, then choose 'Call number' from the  menue.").multilineTextAlignment(.leading).customFont(name: globalDataModel.font, style: .body).padding(.horizontal)
+                Text("")
+                Text("iPhone Contacts").customFont(name: globalDataModel.font, style: .body)
+                Text("In case you want to pick phone numbers from your contacts then please go to Settings – Privacy – Contacts and grant access to the contacts.").multilineTextAlignment(.leading).customFont(name: globalDataModel.font, style: .body).padding(.horizontal)
+                
                 Spacer()
             }
         }
