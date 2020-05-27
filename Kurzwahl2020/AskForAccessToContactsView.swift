0b4aecd92b4762629fcf1e18fb40cfde04350894
Andreas Vogel <outer.space.mailings@gmx.de>
2020-05-27 16:57:34 +0200

    removed custom font in AskForAccessToContactsView

--- Kurzwahl2020/AskForAccessToContactsView.swift
+++ Kurzwahl2020/AskForAccessToContactsView.swift
@@ -20,15 +20,18 @@ struct AskForAccessToContactsView: View {
                                     self.navigation.unwind()
             })
             VStack{
-                Text("How to Enter a Number").customFont(name: globalDataModel.font, style: .body)
-                Text("Double tap to enter a phone number and name. Alternatively tap and hold a field. Then choose 'Edit' from the menue.").multilineTextAlignment(.leading).customFont(name: globalDataModel.font, style: .body).padding(.horizontal)
+                Text("How to Enter a Number")
+                    .fontWeight(.bold)//.customFont(name: globalDataModel.font, style: .body)
+                Text("Double tap to enter a phone number and name. Alternatively tap and hold a field. Then choose 'Edit' from the menue.").multilineTextAlignment(.leading).padding()//.customFont(name: globalDataModel.font, style: .body).padding(.horizontal)
                 Text("")
-                Text("Start a Phone Call").customFont(name: globalDataModel.font, style: .body)
+                Text("Start a Phone Call")
+                    .fontWeight(.bold)//.customFont(name: globalDataModel.font, style: .body)
                 
-                Text("Tap to start a phone call. Alternatively tap and hold, then choose 'Call number' from the  menue.").multilineTextAlignment(.leading).customFont(name: globalDataModel.font, style: .body).padding(.horizontal)
+                Text("Tap to start a phone call. Alternatively tap and hold, then choose 'Call number' from the  menue.").multilineTextAlignment(.leading).padding()//.customFont(name: globalDataModel.font, style: .body).padding(.horizontal)
                 Text("")
-                Text("iPhone Contacts").customFont(name: globalDataModel.font, style: .body)
-                Text("In case you want to pick phone numbers from your contacts then please go to Settings – Privacy – Contacts and grant access to the contacts.").multilineTextAlignment(.leading).customFont(name: globalDataModel.font, style: .body).padding(.horizontal)
+                Text("Access Contacts")
+                    .fontWeight(.bold)//.customFont(name: globalDataModel.font, style: .body)
+                Text("In case you want to pick phone numbers from your contacts then please go to Settings – Privacy – Contacts and grant access to the contacts.").multilineTextAlignment(.leading).padding()//.customFont(name: globalDataModel.font, style: .body).padding(.horizontal)
                 
                 Spacer()
             }
