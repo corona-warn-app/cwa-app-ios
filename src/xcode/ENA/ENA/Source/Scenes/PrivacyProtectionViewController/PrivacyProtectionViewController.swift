5569fb7f1df99ca3c808448d14ba34c7e75f96d1
Max Wittfeld <maximilian.wittfeld@tu-dortmund.de>
2020-06-04 01:01:11 +0200

    Updated PrivacyProtectionScreen look and feel
    -Updated the look and feel of the 'Privacy protection screen'
    -Added localization for the privacy protection message (Also AppString implementation)
    -Added an asset for the standalone logo of the CWA app. (Can be replaced later on, just for the Protection screen as of right now.)

--- src/xcode/ENA/ENA/Source/Scenes/PrivacyProtectionViewController/PrivacyProtectionViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/PrivacyProtectionViewController/PrivacyProtectionViewController.swift
@@ -18,12 +18,54 @@
 import UIKit
 
 class PrivacyProtectionViewController: UIViewController {
+	let gradient = CAGradientLayer()
+	let blurEffectView = UIVisualEffectView()
+	let label = UILabel()
+	let logoImageView = UIImageView()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		view.backgroundColor = UIColor.preferredColor(for: .backgroundPrimary)
+		view.backgroundColor = .clear
 		view.alpha = 0.0
+		
+		gradient.frame = self.view.bounds
+		let appRedColor = UIColor(named: ColorStyle.brandRed.rawValue)?.withAlphaComponent(0.5).cgColor
+		let appBlueColor = UIColor(named: ColorStyle.brandBlue.rawValue)?.withAlphaComponent(0.5).cgColor
+		gradient.colors = [appRedColor ?? UIColor.clear.cgColor, appBlueColor ?? UIColor.clear.cgColor]
+		view.layer.insertSublayer(gradient, at: 0)
+		
+		let blurEffect = UIBlurEffect(style: .systemMaterial)
+		blurEffectView.effect = blurEffect
+		blurEffectView.frame = self.view.bounds
+		
+        label.font = UIFont.preferredFont(forTextStyle: .title1)
+        label.adjustsFontForContentSizeCategory = true
+        label.adjustsFontSizeToFitWidth = true
+        label.textAlignment = .center
+        label.textColor = .secondaryLabel
+        label.numberOfLines = 0
+		label.text = AppStrings.Common.privacyProtectionMessage
+		let labelHeight = view.bounds.height * 0.66
+		(label.frame, _) = blurEffectView.contentView.bounds.divided(atDistance: labelHeight, from: .minYEdge)
+		
+		let logo = UIImage(named: "cwa_logo.pdf")
+		logoImageView.image = logo
+		logoImageView.center = blurEffectView.center
+		logoImageView.contentMode = .scaleAspectFit
+		logoImageView.frame.size.width = 70
+		logoImageView.frame.size.height = 70
+		logoImageView.center = blurEffectView.center
+		
+		let vibrancyEffect = UIVibrancyEffect(blurEffect: blurEffect, style: .fill)
+		let vibrancyView = UIVisualEffectView(effect: vibrancyEffect)
+		vibrancyView.translatesAutoresizingMaskIntoConstraints = false
+		vibrancyView.frame = self.view.bounds
+		vibrancyView.contentView.addSubview(label)
+		vibrancyView.contentView.addSubview(logoImageView)
+		blurEffectView.contentView.addSubview(vibrancyView)
+		view.addSubview(blurEffectView)
 	}
-
+	
 	func show() {
 		UIView.animate(withDuration: 0.2, animations: {
 			self.view.alpha = 1.0
--- src/xcode/ENA/ENA/Source/View Helpers/AppStrings.swift
+++ src/xcode/ENA/ENA/Source/View Helpers/AppStrings.swift
@@ -34,6 +34,8 @@ enum AppStrings {
 		static let alertDescriptionBluetoothOff = NSLocalizedString("Alert_BluetoothOff_Description", comment: "")
 		static let alertActionLater = NSLocalizedString("Alert_CancelAction_Later", comment: "")
 		static let alertActionOpenSettings = NSLocalizedString("Alert_DefaultAction_OpenSettings", comment: "")
+		
+		static let privacyProtectionMessage = NSLocalizedString("PrivacyProtectionScreen_Message", comment: "")
 	}
 
 	enum ExposureSubmission {
