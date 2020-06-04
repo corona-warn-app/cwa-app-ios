522d7a4488e1f47b682279736f3e8e562aa9fd4d
Marc Bormeth <marc.bormeth@sap.com>
2020-06-04 10:42:06 +0200

    Fix review findings

--- src/xcode/ENA/ENA/Source/Scenes/AppInformation/AppInformationViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/AppInformation/AppInformationViewController.swift
@@ -1,7 +1,7 @@
 //
 // Corona-Warn-App
 //
-// SAP SE and all other contributors /
+// SAP SE and all other contributors
 // copyright owners license this file to you under the Apache
 // License, Version 2.0 (the "License"); you may not use this
 // file except in compliance with the License.
@@ -15,11 +15,10 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-//
 
 import UIKit
 
-class AppInformationViewController: UITableViewController {
+class AppInformationViewController: DynamicTableViewController {
 
 	// MARK: - Properties
 	private let model: [AppInformationDetailModel] = [
@@ -31,50 +30,57 @@ class AppInformationViewController: UITableViewController {
 		.legal
 	]
 
+	override func loadView() {
+		let tableView = UITableView(frame: .zero, style: .grouped)
+		tableView.backgroundColor = .preferredColor(for: .separator)
+
+		self.view = tableView
+	}
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
 		navigationItem.largeTitleDisplayMode = .always
 		title = "Home_AppInformationCard_Title".localized
 
-        setupTableView()
+		dynamicTableViewModel = .init([
+			.section(
+				header: .space(height: 30),
+				footer: .view(footerView()),
+				separators: false,
+				cells: self.model.map({ .body(text: $0.title) })
+			)
+		])
+
+		tableView.dataSource = self
+		tableView.delegate = self
     }
 
-	private func setupTableView() {
-		tableView.contentInset = .init(top: 30, left: 0, bottom: 0, right: 0)
-		tableView.backgroundColor = .preferredColor(for: .backgroundSecondary)
+	func footerView() -> UIView {
+		let bundleVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] ?? ""
+		let bundleBuild = Bundle.main.infoDictionary?["CFBundleVersion"] ?? ""
 
-		tableView.register(UITableViewCell.self, forCellReuseIdentifier: "appInformationCell")
+		let footerView = UIView()
 
-		tableView.sectionFooterHeight = UITableView.automaticDimension
-		tableView.estimatedSectionFooterHeight = 20
-		tableView.tableFooterView = UIView()
+		let versionLabel = ENALabel()
+		versionLabel.translatesAutoresizingMaskIntoConstraints = false
+		versionLabel.text = "\(AppStrings.Home.appInformationVersion) \(bundleVersion) (\(bundleBuild))"
+		versionLabel.textColor = UIColor.preferredColor(for: .textPrimary2)
+		versionLabel.style = .footnote
+
+		footerView.addSubview(versionLabel)
+
+		versionLabel.centerXAnchor.constraint(equalTo: footerView.centerXAnchor).isActive = true
+		versionLabel.topAnchor.constraint(equalTo: footerView.topAnchor, constant: 10).isActive = true
+		versionLabel.bottomAnchor.constraint(equalTo: footerView.bottomAnchor, constant: 10).isActive = true
+
+		return footerView
 	}
-
 }
 
 extension AppInformationViewController {
-
-	override func numberOfSections(in _: UITableView) -> Int {
-		1
-	}
-
-	override func tableView(_: UITableView, titleForHeaderInSection section: Int) -> String? {
-		""
-	}
-
-	override func tableView(_: UITableView, numberOfRowsInSection section: Int) -> Int {
-		model.count
-	}
-
-	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		let cell = tableView.dequeueReusableCell(withIdentifier: "appInformationCell", for: indexPath)
-		cell.textLabel?.text = model[indexPath.row].title
-
-		return cell
-	}
-
 	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		tableView.deselectRow(at: indexPath, animated: true)
 
 		if indexPath.row == 1 {
 			WebPageHelper.showWebPage(from: self)
@@ -89,24 +95,5 @@ extension AppInformationViewController {
 				animated: true
 			)
 		}
-	}
-
-	override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-		let bundleVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] ?? ""
-		let bundleBuild = Bundle.main.infoDictionary?["CFBundleVersion"] ?? ""
-
-		let footerView = UIView()
-
-		let versionLabel = UILabel()
-		versionLabel.translatesAutoresizingMaskIntoConstraints = false
-		versionLabel.text = "\(AppStrings.Home.appInformationVersion) \(bundleVersion) (\(bundleBuild))"
-		versionLabel.font = UIFont.preferredFont(forTextStyle: .body).scaledFont(size: 13, weight: .semibold)
-		versionLabel.textColor = UIColor.preferredColor(for: .textPrimary2)
-		footerView.addSubview(versionLabel)
-		versionLabel.centerXAnchor.constraint(equalTo: footerView.centerXAnchor).isActive = true
-		versionLabel.topAnchor.constraint(equalTo: footerView.topAnchor, constant: 10).isActive = true
-		versionLabel.bottomAnchor.constraint(equalTo: footerView.bottomAnchor, constant: 10).isActive = true
-
-		return footerView
 	}
 }
