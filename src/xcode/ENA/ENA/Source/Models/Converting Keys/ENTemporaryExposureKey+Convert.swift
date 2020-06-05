a3fe7974fd44b1b222c4750261e38bce55237e48
Christian Kienle <christian.kienle@sap.com>
2020-06-05 17:42:05 +0200

    also copy rollingPeriod

--- src/xcode/ENA/ENA/Source/Models/Converting Keys/ENTemporaryExposureKey+Convert.swift
+++ src/xcode/ENA/ENA/Source/Models/Converting Keys/ENTemporaryExposureKey+Convert.swift
@@ -21,7 +21,7 @@ extension ENTemporaryExposureKey {
 	var sapKey: SAP_TemporaryExposureKey {
 		SAP_TemporaryExposureKey.with {
 			$0.keyData = self.keyData
-			$0.rollingPeriod = 144 // Temporarily set to magic number
+			$0.rollingPeriod = Int32(self.rollingPeriod)
 			$0.rollingStartIntervalNumber = Int32(self.rollingStartNumber)
 			$0.transmissionRiskLevel = Int32(self.transmissionRiskLevel)
 		}
