c2e4614cda303a2d704cda07a4b51a40205b5def
Wild, Sebastian <sebastian.wild@sap.com>
2020-06-05 21:46:51 -0700

    Start working on hash verification

---
+++ src/xcode/ENA/ENA/Resources/Keys/CWAKeys.swift
@@ -0,0 +1,67 @@
+//
+// Corona-Warn-App
+//
+// SAP SE and all other contributors
+// copyright owners license this file to you under the Apache
+// License, Version 2.0 (the "License"); you may not use this
+// file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+import Foundation
+import CommonCrypto
+
+enum CWAKeys {
+	// How to get current environment?
+	enum Environment {
+		case production
+		case development
+
+		var publicKeyString: String {
+			self == .production ? prodPublic : devPublic
+		}
+	}
+
+	enum KeyError: Error {
+		/// It was not possible to create the base64 encoded data from the public key string
+		case encodingError
+		case createError
+	}
+
+	static func getPubSecKey(for environment: Environment) throws -> SecKey {
+		guard let data = Data(base64Encoded: environment.publicKeyString) else {
+			throw KeyError.encodingError
+		}
+
+		let attributes: [String: Any] = [
+			kSecAttrKeyType as String: kSecAttrKeyTypeEC,
+			kSecAttrKeyClass as String: kSecAttrKeyClassPublic
+		]
+
+		var error: Unmanaged<CFError>?
+		let key = SecKeyCreateWithData(data as NSData, attributes as CFDictionary, &error)
+
+		if let error = error { throw error.takeRetainedValue() as Error }
+
+		guard let secKey = key else { throw KeyError.createError }
+
+		return secKey
+	}
+
+	/*
+	Keys here were generated with script from raw text PEM files like so:
+	./getRawKey.sh ./keys/de.rki.coronawarnapp-prod-public.pem
+	*/
+
+	private static let devPublic = "BNwWE8a9h7iWEBvnexM7uikvBhGxZIhL8iRYNI/gvN/YE40+o+x6NBF12f2tO2X7ynAtnJnNUrp7K6lPSaL9SeU="
+	private static let prodPublic = "BHOwxLLXFCEXMpN+TmAyfef4U4N1FYbGg1yk9MyuzYRz16Ms4qRJzqdxsjVw1aWj4t3AH0JsXrhTGQCF9vugOV4="
+}
--- src/xcode/ENA/ENA/Source/Client/HTTP Client/SAPDownloadedPackage.swift
+++ src/xcode/ENA/ENA/Source/Client/HTTP Client/SAPDownloadedPackage.swift
@@ -32,6 +32,7 @@ struct SAPDownloadedPackage {
 		}
 		do {
 			self = try archive.extractKeyPackage()
+			try verifyHash()
 		} catch {
 			return nil
 		}
@@ -43,11 +44,42 @@ struct SAPDownloadedPackage {
 	let signature: Data
 }
 
+extension SAPDownloadedPackage {
+	/// Verify that the .bin file actually originated from our server and was not tampered with.
+	///
+	/// This works as follows:
+	/// - We store the public key of our server (depends on landscape, we have two public keys right now)
+	/// - Neither the .bin or .sig data is encrypted (besides in transit), but the .sig file stores an encrypted SHA256 hash of the .bin file
+	/// - The server has signed this hash with their private key.
+	///
+	/// The actual checking is performed as follows:
+	/// 1. Deserialze the `signature` `Data` into the corresponding model `SAP_TEKSignature`
+	/// 2. Decrypt the therein contained signature with out public key
+	/// 3. We now have the SHA256 hash of the .bin file
+	/// 4. Hash the .bin file, and compare the two.
+	/// 5. If they match, we can be sure that they have not been tampered with and originated from our server.
+	func verifyHash() throws {
+		let parsedSignatureFile = try SAP_TEKSignature(serializedData: signature)
+		let encryptedSignature = parsedSignatureFile.signature
+		let key = try CWAKeys.getPubSecKey(for: .development)
+
+		guard let decryptedHash = encryptedSignature.decrypted(with: key) else {
+			logError(message: "Package signature decryption failed!")
+			throw Archive.KeyPackageError.signatureCheckFailed
+		}
+
+		if decryptedHash != bin.sha256() {
+			throw Archive.KeyPackageError.signatureCheckFailed
+		}
+	}
+}
+
 private extension Archive {
 	typealias KeyPackage = (bin: Data, sig: Data)
 	enum KeyPackageError: Error {
 		case binNotFound
 		case sigNotFound
+		case signatureCheckFailed
 	}
 
 	func extractData(from entry: Entry) throws -> Data {
