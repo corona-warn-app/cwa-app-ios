e705472cff14aaa635cf8359f6916dfce88907f7
Dunne, Liam <liam.dunne@sap.com>
2020-06-08 10:34:10 +0100

    Renamed AppInformtaion test class

--- src/xcode/ENA/ENAUITests/AppInformation/ENAUITestsAppInformation.swift
+++ src/xcode/ENA/ENAUITests/AppInformation/ENAUITestsAppInformation.swift
@@ -19,7 +19,7 @@
 
 import XCTest
 
-class ENAUITests_AppInformation: XCTestCase {
+class ENAUITestsAppInformation: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
