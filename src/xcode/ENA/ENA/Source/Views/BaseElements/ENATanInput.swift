668978dea0a893c9a8f6449c086a3a8a43974455
Thomas Augsten <thomas.augsten@sap.com>
2020-06-08 12:16:51 +0200

    fix default font size

--- src/xcode/ENA/ENA/Source/Views/BaseElements/ENATanInput.swift
+++ src/xcode/ENA/ENA/Source/Views/BaseElements/ENATanInput.swift
@@ -27,7 +27,7 @@ class ENATanInput: UIControl, UIKeyInput {
 	@IBInspectable var labelTextColor: UIColor = .enaColor(for: .textPrimary1)
 	@IBInspectable var boxColor: UIColor = .enaColor(for: .separator)
 
-	@IBInspectable var fontSize: CGFloat = 30
+	@IBInspectable var fontSize: CGFloat = 28
 	@IBInspectable var groups: String = "3,3,4"
 	@IBInspectable var whiteList: String = "2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,J,K,M,N,P,Q,R,S,T,U,V,W,X,Y,Z"
 
