57e115f0d4078e7af93cc51367a2d4c79d3db466
Christian Kienle <christian.kienle@sap.com>
2020-06-09 23:59:27 +0200

    removes version checker

--- src/xcode/ENA/ENA/Source/Workers/AppUpdateCheckHelper.swift
+++ src/xcode/ENA/ENA/Source/Workers/AppUpdateCheckHelper.swift
@@ -44,6 +44,7 @@ class AppUpdateCheckHelper {
 	}
 
 	func checkAppVersionDialog(for vc: UIViewController?) {
+		/**
 		client.appConfiguration { result in
 			guard let versionInfo: SAP_ApplicationVersionConfiguration = result?.appVersion else {
 				return
@@ -57,6 +58,7 @@ class AppUpdateCheckHelper {
 				return
 			}
 			vc?.present(alert, animated: true, completion: nil)
+			*/
 		}
 	}
 
