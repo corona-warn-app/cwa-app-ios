dafa9173eac6ff7f5bcf090f5e112e7d7c465de7
Christian Kienle <christian.kienle@sap.com>
2020-06-09 19:10:44 +0200

    fix more

--- src/xcode/ENA/ENA/Source/Services/__tests__/Mocks/MockTestStore.swift
+++ src/xcode/ENA/ENA/Source/Services/__tests__/Mocks/MockTestStore.swift
@@ -47,6 +47,5 @@ class MockTestStore: Store {
 	var allowRiskChangesNotification: Bool = true
 	var allowTestsStatusNotification: Bool = true
 	var hourlyFetchingEnabled: Bool = true
-	var previousRisk: Risk?
 	var lastCheckedVersion: String?
 }
