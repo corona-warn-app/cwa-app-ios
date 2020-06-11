5d5a2ceda26f4bc4942949f24fa027be4f9e4215
Wild, Sebastian <sebastian.wild@sap.com>
2020-06-10 22:28:46 -0700

    Re-enable AppUpdateCheckHelper in SceneDelegate

--- src/xcode/ENA/ENA/Source/SceneDelegate.swift
+++ src/xcode/ENA/ENA/Source/SceneDelegate.swift
@@ -37,7 +37,7 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate, RequiresAppDepend
 	}
 
 	private var developerMenu: DMDeveloperMenu?
-	private var appUpdateChecker: AppUpdateCheckHelper?
+	private lazy var appUpdateChecker = AppUpdateCheckHelper(client: self.client, store: self.store)
 
 	private func enableDeveloperMenuIfAllowed(in controller: UIViewController) {
 		developerMenu = DMDeveloperMenu(
@@ -148,7 +148,7 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate, RequiresAppDepend
 		UIImageView.appearance().accessibilityIgnoresInvertColors = true
 		window?.rootViewController = navigationController
 		window?.makeKeyAndVisible()
-		appUpdateChecker?.checkAppVersionDialog(for: window?.rootViewController)
+		appUpdateChecker.checkAppVersionDialog(for: window?.rootViewController)
 	}
 
 	private func setupNavigationBarAppearance() {
