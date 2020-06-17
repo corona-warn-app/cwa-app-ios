3913d7af522d998385921c9e80c9883561efa336
Wild, Sebastian <sebastian.wild@sap.com>
2020-06-16 18:09:04 -0700

    Slightly improved testability of String+Today
    Also forced DateFormatter Calendar to Gregorian

--- src/xcode/ENA/ENA/Source/Extensions/__tests__/String+TodayTests.swift
+++ src/xcode/ENA/ENA/Source/Extensions/__tests__/String+TodayTests.swift
@@ -22,8 +22,33 @@ import Foundation
 import XCTest
 
 final class StringTodayTests: XCTestCase {
-	func testTodayFormat() {
+	func testTodayFormat_Now() {
 		let formatted = String.formattedToday()
 		XCTAssertNotNil(formatted.range(of: #"\d{4}-\d{2}-\d{2}"#, options: .regularExpression), "Expected format to be in yyyy-MM-dd")
+	}
+
+	func testTodayFormat_SetDate() {
+		let date = DateComponents(
+			calendar: Calendar.current,
+			timeZone: TimeZone(abbreviation: "UTC"),
+			year: 2020,
+			month: 6,
+			day: 16,
+			hour: 23,
+			minute: 59,
+			second: 59
+		).date ?? .distantPast
+		let formatter = DateFormatter.packagesDateFormatter
+
+		XCTAssertEqual(formatter.string(from: date), "2020-06-16")
+	}
+
+	func testFormatterProperties() {
+		let formatter = DateFormatter.packagesDateFormatter
+
+		XCTAssertEqual(formatter.dateFormat, "yyyy-MM-dd")
+		XCTAssertEqual(formatter.timeZone, TimeZone(abbreviation: "UTC"))
+		XCTAssertEqual(formatter.locale, Locale(identifier: "en_US_POSIX"))
+		XCTAssertEqual(formatter.calendar, Calendar(identifier: .gregorian))
 	}
 }
