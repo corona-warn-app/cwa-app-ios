df488c840219eb01483fa6d64756102515f1681c
Marc Bormeth <marc.bormeth@sap.com>
2020-06-24 19:09:31 +0200

    Update comment

--- scripts/switch_to_dev_env.sh
+++ scripts/switch_to_dev_env.sh
@@ -9,7 +9,7 @@ distr_endp='https://svc90.main.px.t-online.de'
 subm_endp='https://submission.coronawarn.app'
 verif_endp='https://verification.coronawarn.app'
 
-# Distribution endpoint
+# Replace endpoints
 sed "s,$distr_endp,$SECRET_DIST_URL,g ; s,$subm_endp,$SECRET_SUBM_URL,g ; s,$verif_endp,$SECRET_VERIF_URL,g" project.pbxproj > tmp.pbxproj
 
 # Replace original pbxproj
