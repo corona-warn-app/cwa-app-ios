2ca88b7a7e28d6dd36c5d059eda95fa0f77220f2
Aleksandr Tikhonov <aleksandr.tikhonov@sap.com>
2020-07-13 15:19:42 +0200

    fixing tests

--- src/xcode/ENA/ENA/Source/Models/Home/HomeRiskCellConfigurator/HomeRiskCellConfigurators/__tests__/HomeHighRiskCellConfiguratorTests.swift
+++ src/xcode/ENA/ENA/Source/Models/Home/HomeRiskCellConfigurator/HomeRiskCellConfigurators/__tests__/HomeHighRiskCellConfiguratorTests.swift
@@ -34,8 +34,8 @@ class HomeHighRiskCellConfiguratorTests: XCTestCase {
 
 	func test_unknownRiskLevelCell_shouldHaveEqualHash() {
 		let date = Date()
-		let configurator1 = HomeHighRiskCellConfigurator(numberRiskContacts: 0, daysSinceLastExposure: 0, lastUpdateDate: date, manualExposureDetectionState: .possible, detectionMode: .default, validityDuration: 0)
-		let configurator2 = HomeHighRiskCellConfigurator(numberRiskContacts: 0, daysSinceLastExposure: 0, lastUpdateDate: date, manualExposureDetectionState: .possible, detectionMode: .default, validityDuration: 0)
+		let configurator1 = HomeHighRiskCellConfigurator(isLoading: false, numberRiskContacts: 0, daysSinceLastExposure: 0, lastUpdateDate: date, manualExposureDetectionState: .possible, detectionMode: .default, validityDuration: 0)
+		let configurator2 = HomeHighRiskCellConfigurator(isLoading: false, numberRiskContacts: 0, daysSinceLastExposure: 0, lastUpdateDate: date, manualExposureDetectionState: .possible, detectionMode: .default, validityDuration: 0)
 
 		let hash1 = configurator1.hashValue
 		let hash2 = configurator2.hashValue
@@ -47,8 +47,8 @@ class HomeHighRiskCellConfiguratorTests: XCTestCase {
 
 		let date = Date()
 
-		let configurator1 = HomeHighRiskCellConfigurator(numberRiskContacts: 0, daysSinceLastExposure: 0, lastUpdateDate: date, manualExposureDetectionState: .possible, detectionMode: .default, validityDuration: 0)
-		let configurator2 = HomeHighRiskCellConfigurator(numberRiskContacts: 4, daysSinceLastExposure: 0, lastUpdateDate: date, manualExposureDetectionState: .possible, detectionMode: .default, validityDuration: 0)
+		let configurator1 = HomeHighRiskCellConfigurator(isLoading: false, numberRiskContacts: 0, daysSinceLastExposure: 0, lastUpdateDate: date, manualExposureDetectionState: .possible, detectionMode: .default, validityDuration: 0)
+		let configurator2 = HomeHighRiskCellConfigurator(isLoading: false, numberRiskContacts: 4, daysSinceLastExposure: 0, lastUpdateDate: date, manualExposureDetectionState: .possible, detectionMode: .default, validityDuration: 0)
 
 		let hash1 = configurator1.hashValue
 		let hash2 = configurator2.hashValue
@@ -60,8 +60,8 @@ class HomeHighRiskCellConfiguratorTests: XCTestCase {
 
 		let date = Date()
 
-		let configurator1 = HomeHighRiskCellConfigurator(numberRiskContacts: 0, daysSinceLastExposure: 0, lastUpdateDate: date, manualExposureDetectionState: .waiting, detectionMode: .default, validityDuration: 0)
-		let configurator2 = HomeHighRiskCellConfigurator(numberRiskContacts: 0, daysSinceLastExposure: 0, lastUpdateDate: date, manualExposureDetectionState: .possible, detectionMode: .default, validityDuration: 0)
+		let configurator1 = HomeHighRiskCellConfigurator(isLoading: false, numberRiskContacts: 0, daysSinceLastExposure: 0, lastUpdateDate: date, manualExposureDetectionState: .waiting, detectionMode: .default, validityDuration: 0)
+		let configurator2 = HomeHighRiskCellConfigurator(isLoading: false, numberRiskContacts: 0, daysSinceLastExposure: 0, lastUpdateDate: date, manualExposureDetectionState: .possible, detectionMode: .default, validityDuration: 0)
 
 		let hash1 = configurator1.hashValue
 		let hash2 = configurator2.hashValue
@@ -71,8 +71,8 @@ class HomeHighRiskCellConfiguratorTests: XCTestCase {
 
 	func test_unknownRiskLevelCell_shouldBeEqual() {
 		let date = Date()
-		let configurator1 = HomeHighRiskCellConfigurator(numberRiskContacts: 0, daysSinceLastExposure: 0, lastUpdateDate: date, manualExposureDetectionState: .waiting, detectionMode: .default, validityDuration: 0)
-		let configurator2 = HomeHighRiskCellConfigurator(numberRiskContacts: 0, daysSinceLastExposure: 0, lastUpdateDate: date, manualExposureDetectionState: .waiting, detectionMode: .default, validityDuration: 0)
+		let configurator1 = HomeHighRiskCellConfigurator(isLoading: false, numberRiskContacts: 0, daysSinceLastExposure: 0, lastUpdateDate: date, manualExposureDetectionState: .waiting, detectionMode: .default, validityDuration: 0)
+		let configurator2 = HomeHighRiskCellConfigurator(isLoading: false, numberRiskContacts: 0, daysSinceLastExposure: 0, lastUpdateDate: date, manualExposureDetectionState: .waiting, detectionMode: .default, validityDuration: 0)
 
 		let isEqual = configurator1 == configurator2
 		XCTAssertTrue(isEqual)
@@ -80,8 +80,8 @@ class HomeHighRiskCellConfiguratorTests: XCTestCase {
 
 	func test_unknownRiskLevelCell_shouldntBeEqual1() {
 		let date = Date()
-		let configurator1 = HomeHighRiskCellConfigurator(numberRiskContacts: 0, daysSinceLastExposure: 0, lastUpdateDate: date, manualExposureDetectionState: .possible, detectionMode: .default, validityDuration: 0)
-		let configurator2 = HomeHighRiskCellConfigurator(numberRiskContacts: 0, daysSinceLastExposure: 0, lastUpdateDate: date, manualExposureDetectionState: .possible, detectionMode: .default, validityDuration: 99)
+		let configurator1 = HomeHighRiskCellConfigurator(isLoading: false, numberRiskContacts: 0, daysSinceLastExposure: 0, lastUpdateDate: date, manualExposureDetectionState: .possible, detectionMode: .default, validityDuration: 0)
+		let configurator2 = HomeHighRiskCellConfigurator(isLoading: false, numberRiskContacts: 0, daysSinceLastExposure: 0, lastUpdateDate: date, manualExposureDetectionState: .possible, detectionMode: .default, validityDuration: 99)
 
 		let isEqual = configurator1 == configurator2
 
@@ -90,8 +90,8 @@ class HomeHighRiskCellConfiguratorTests: XCTestCase {
 
 	func test_unknownRiskLevelCell_shouldntBeEqual2() {
 		let date = Date()
-		let configurator1 = HomeHighRiskCellConfigurator(numberRiskContacts: 0, daysSinceLastExposure: 0, lastUpdateDate: date, manualExposureDetectionState: .possible, detectionMode: .default, validityDuration: 0)
-		let configurator2 = HomeHighRiskCellConfigurator(numberRiskContacts: 0, daysSinceLastExposure: 0, lastUpdateDate: date.addingTimeInterval(22), manualExposureDetectionState: .possible, detectionMode: .default, validityDuration: 0)
+		let configurator1 = HomeHighRiskCellConfigurator(isLoading: false, numberRiskContacts: 0, daysSinceLastExposure: 0, lastUpdateDate: date, manualExposureDetectionState: .possible, detectionMode: .default, validityDuration: 0)
+		let configurator2 = HomeHighRiskCellConfigurator(isLoading: false, numberRiskContacts: 0, daysSinceLastExposure: 0, lastUpdateDate: date.addingTimeInterval(22), manualExposureDetectionState: .possible, detectionMode: .default, validityDuration: 0)
 
 		let isEqual = configurator1 == configurator2
 
