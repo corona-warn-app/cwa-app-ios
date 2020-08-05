feeefbecae11f9d85a3e3e525d01006dce286cd1
Johannes Rohwer <johannes.rohwer@sap.com>
2020-08-05 15:22:39 +0200

    chore: test request header size

--- src/xcode/ENA/ENA/Source/Client/HTTP Client/__tests__/HTTPClient+PlausibeDeniabilityTests.swift
+++ src/xcode/ENA/ENA/Source/Client/HTTP Client/__tests__/HTTPClient+PlausibeDeniabilityTests.swift
@@ -90,6 +90,49 @@ class HTTPClientPlausibleDeniabilityTests: XCTestCase {
 		waitForExpectations(timeout: .short)
 	}
 
+	/// This test makes sure that all headers + urls have the same length.
+	func test_headerPadding() {
+
+		// Setup.
+
+		let expectation = self.expectation(description: "all callbacks called")
+		expectation.expectedFulfillmentCount = 12
+
+		var previousSize: Int?
+
+		let session = MockUrlSession(data: nil, nextResponse: nil, error: nil) { request in
+			expectation.fulfill()
+			guard
+				// Hack: We cannot directly access the HTTP headers here,
+				// we therefore compare their JSON encoded lenght.
+				let data = try? JSONEncoder().encode(request.allHTTPHeaderFields),
+				let url = request.url?.absoluteString
+			else {
+				XCTFail("Could not execute test")
+				return
+			}
+
+			let size = url.count + data.count
+			if previousSize == nil { previousSize = size }
+			XCTAssertEqual(size, previousSize)
+			previousSize = size
+		}
+
+		let stack = MockNetworkStack(mockSession: session)
+		let client = HTTPClient.makeWith(mock: stack)
+
+		// Test.
+
+		client.getTANForExposureSubmit(forDevice: "dummyRegToken") { _ in expectation.fulfill() }
+		client.getTANForExposureSubmit(forDevice: "dummyRegToken", isFake: true) { _ in expectation.fulfill() }
+		client.getRegistrationToken(forKey: "123456789", withType: "TELETAN") { _ in expectation.fulfill() }
+		client.getRegistrationToken(forKey: "123456789", withType: "TELETAN", isFake: true) { _ in expectation.fulfill() }
+		client.getTestResult(forDevice: "dummyDevice") { _ in expectation.fulfill() }
+		client.getTestResult(forDevice: "dummyDevice", isFake: true) { _ in expectation.fulfill() }
+
+		waitForExpectations(timeout: .short)
+	}
+
 	func test_submit_requestPaddingIdenticalForRealAndFake() {
 		let noKeys = [ENTemporaryExposureKey]()
 		compareSubmitRequestPadding(for: noKeys)
