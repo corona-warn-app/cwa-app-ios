cc413a69f802453253204bddac425b9370486857
Carsten Knoblich <carsten@staticline.de>
2020-08-17 12:59:42 +0200

    fixed typo
    Co-authored-by: Nick Guendling <nickolas@guendling.org>

--- src/xcode/ENA/ENA/Source/AppDelegate+PlausibleDeniability.swift
+++ src/xcode/ENA/ENA/Source/AppDelegate+PlausibleDeniability.swift
@@ -73,7 +73,7 @@ extension AppDelegate {
 	/// - Returns: Bool to indicate wether the fake request has been sent
 	@discardableResult
 	func executeFakeRequestOnAppLaunch(probability p: Double) -> Bool {
-		assert(p <= 1, "p should be lighter than or equal 1.0")
+		assert(p <= 1, "p should be lower than or equal 1.0")
 		assert(p >= 0, "p should be greater than or equal 0.0")
 		if Double.random(in: 0...1) <= p {
 			sendFakeRequest()
