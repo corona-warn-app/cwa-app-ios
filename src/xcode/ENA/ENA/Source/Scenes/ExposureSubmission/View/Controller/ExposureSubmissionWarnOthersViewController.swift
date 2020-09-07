060a3f85fa0b7406fbebc2f54be0ebca1e413257
Johannes Rohwer <johannes.rohwer@sap.com>
2020-09-07 11:58:41 +0200

    chore: add RequiresDismissConfirmation for relevant VCs

--- src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/View/Controller/ExposureSubmissionWarnOthersViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/View/Controller/ExposureSubmissionWarnOthersViewController.swift
@@ -195,3 +195,8 @@ extension ExposureSubmissionWarnOthersViewController {
 		case roundedCell
 	}
 }
+
+// MARK: - RequiresDismissConfirmation.
+
+/// - NOTE: Marker protocol.
+extension ExposureSubmissionWarnOthersViewController: RequiresDismissConfirmation { }
