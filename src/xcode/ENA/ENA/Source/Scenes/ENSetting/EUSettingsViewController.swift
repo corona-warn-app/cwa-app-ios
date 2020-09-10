b62c7d14b9694ca25fa87f960bc8a95c57752a55
Sandra Grujovic <sandra.grujovic@sap.com>
2020-09-10 16:13:31 +0200

    chore: adjust to country struct

--- src/xcode/ENA/ENA/Source/Scenes/ENSetting/EUSettingsViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ENSetting/EUSettingsViewController.swift
@@ -200,7 +200,7 @@ private class EUSettingsViewModel {
 				.stepCell(
 					title: AppStrings.ExposureNotificationSetting.euGermanRiskTitle,
 					description: AppStrings.ExposureNotificationSetting.euGermanRiskDescription,
-					icon: UIImage(named: "Icons_Grey_1"),
+					icon: Country(countryCode: "de")?.flag,
 					hairline: .none
 				),
 				.custom(withIdentifier: EUSettingsViewController.CustomCellReuseIdentifiers.roundedCell,
@@ -311,61 +311,5 @@ private class SwitchCell: UITableViewCell {
 		onSwitchSubject?
 			.sink { self.uiSwitch.setOn($0, animated: true) }
 			.store(in: &subscriptions)
-	}
-}
-
-// MARK: - Delete this...
-
-/// A simple data countainer representing a country or political region.
-struct Country: Equatable, Hashable {
-
-	typealias ID = String
-
-	/// The country identifier. Equals the initializing country code.
-	let id: ID
-
-	/// The localized name of the country using the current locale.
-	let localizedName: String
-
-	/// The flag of the current country, if present.
-	let flag: UIImage?
-
-	/// Initialize a country with a given. If no valid `countryCode` is given the initalizer returns `nil`.
-	///
-	/// - Parameter countryCode: An [ISO 3166 (Alpha-2)](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements) country two-digit code. Examples: "DE", "FR"
-	init?(countryCode: ID) {
-		// Check if this is a valid country
-		guard let name = Locale.current.regionName(forCountryCode: countryCode) else { return nil }
-
-		id = countryCode
-		localizedName = name
-		flag = UIImage(named: "flag.\(countryCode.lowercased())") ?? UIImage.checkmark
-	}
-
-	static func defaultCountry() -> Country {
-		// swiftlint:disable:next force_unwrapping
-		return Country(countryCode: "DE")!
-	}
-}
-
-extension Locale {
-	func regionName(forCountryCode code: String) -> String? {
-		var identifier: String
-		// quick solution for the EU scenario
-		switch code.lowercased() {
-		case "el":
-			identifier = "gr"
-		case "no":
-			identifier = "nb_NO"
-		default:
-			identifier = code
-		}
-		let target = Locale(identifier: identifier)
-
-		// catch cases where multiple languages per region might appear, e.g. Norway
-		guard let regionCode = target.identifier.components(separatedBy: "_").last else {
-			return nil
-		}
-		return localizedString(forRegionCode: regionCode)
 	}
 }
