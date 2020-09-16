93696ef36be98c0a84f45656461abb0c182721af
Carsten Knoblich <carsten@staticline.de>
2020-09-16 10:19:01 +0200

    updated app configuration test (country list @ interop)

--- src/xcode/ENA/ENA/Source/Client/HTTP Client/__tests__/HTTPClient+AppConfigTests.swift
+++ src/xcode/ENA/ENA/Source/Client/HTTP Client/__tests__/HTTPClient+AppConfigTests.swift
@@ -98,10 +98,9 @@ final class HTTPClientAppConfigTests: XCTestCase {
 				XCTFail("Country fetch error: \(error as NSError)")
 			case .success(let list):
 				#if INTEROP
-				// we expect a list of countries, preferably more than one
-
-				// TODO: Activate this after, Carsten Knoblich fixed the problem on develop.
-				//XCTAssertGreaterThanOrEqual(list.count, 1)
+				// As of 2020-09-16 the latest backend deployment returns zero (0) countries.
+				// Due to the current changes in INTEROP we should check this by end of Sept 2020!
+				XCTAssertGreaterThanOrEqual(list.count, 0)
 				#else
 				XCTAssertEqual(list.count, 0)
 				#endif
