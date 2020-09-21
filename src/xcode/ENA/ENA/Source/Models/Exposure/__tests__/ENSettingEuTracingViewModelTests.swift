732058d511af815f4a1ce8ee7773e537896859d9
swissmountainfrog <karsten.gahn@sap.com>
2020-09-21 16:05:57 +0200

    EXPOSUREAPP-2667: Refactored tests

--- src/xcode/ENA/ENA/Source/Models/Exposure/__tests__/ENSettingEuTracingViewModelTests.swift
+++ src/xcode/ENA/ENA/Source/Models/Exposure/__tests__/ENSettingEuTracingViewModelTests.swift
@@ -17,4 +17,22 @@
 // under the License.
 //
 
-import Foundation
+import XCTest
+@testable import ENA
+
+class ENSettingEuTracingViewModelTests: XCTestCase {
+	
+	func testENSettingEuTracingViewModelTests_Init() throws {
+		
+		let viewModel = ENSettingEuTracingViewModel()
+		XCTAssertNotNil(viewModel)
+	}
+	
+	func testENSettingEuTracingViewModelTests_LabelsAsExpected() throws {
+		
+		var viewModel = ENSettingEuTracingViewModel()
+		
+		XCTAssertNotEqual(viewModel.title, "", "Localized (i18n) Title should not be empty)")
+		XCTAssertNotEqual(viewModel.countryListLabel, "", "Sub title label should be empty.")
+	}
+}
