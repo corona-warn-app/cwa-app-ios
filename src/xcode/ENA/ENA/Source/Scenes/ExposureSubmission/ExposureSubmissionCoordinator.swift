0b12558df649c0f6666799c41a02ff7708dd4d01
Marcus Scherer <marcus@skunkbrothers.de>
2020-09-22 13:09:58 +0200

    Fix for new Coordinator

--- src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/ExposureSubmissionCoordinator.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/ExposureSubmissionCoordinator.swift
@@ -379,8 +379,7 @@ extension ExposureSubmissionCoordinator {
 				switch error {
 				// If User doesn`t allow TEK`s to be shared with the app, we stay on the screen (https://jira.itc.sap.com/browse/EXPOSUREAPP-2293)
 				case .notAuthorized:
-					self.navigationFooterItem?.isPrimaryButtonLoading = false
-					self.navigationFooterItem?.isPrimaryButtonEnabled = true
+					onError()
 					return
 				
 				// We continue the regular flow even if there are no keys collected.
