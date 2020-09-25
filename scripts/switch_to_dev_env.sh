49876de3e21103fcae31222a6fee2185956e5726
Artur Friesen <mail@arturfriesen.de>
2020-09-25 16:56:11 +0200

    Change environment injection script to support server environments json.

--- scripts/switch_to_dev_env.sh
+++ scripts/switch_to_dev_env.sh
@@ -2,17 +2,26 @@
 set -euo pipefail
 
 script_dir=${0:a:h}
-cd $script_dir/../src/xcode/ENA/ENA/Source/Client/HTTP\ Client
+cd $script_dir/../src/xcode/ENA/ENA/Resources/ServerEnvironment
 
-# Strings to be replaced
-distr_endp='Hosts.distributionURLString'
-subm_endp='Hosts.submissionURLString'
-verif_endp='Hosts.verificationURLString'
+serverEnvironments="{
+	\"ServerEnvironments\":[
+		{
+			\"name\": \"Production\",
+			\"distributionURL\": \"${SECRET_DIST_URL}\",
+			\"submissionURL\": \"${SECRET_SUBM_URL}\",
+			\"verificationURL\": \"${SECRET_VERIF_URL}\"
+		},
+		{
+			\"name\": \"ECME\",
+			\"distributionURL\": \"${ECME_SECRET_DIST_URL}\",
+			\"submissionURL\": \"${ECME_SECRET_SUBM_URL}\",
+			\"verificationURL\": \"${ECME_SECRET_VERIF_URL}\"
+		}
+	]
+}
+"
 
-# Replace endpoints
-sed "s,$distr_endp,\"${SECRET_DIST_URL}\",g ; s,$subm_endp,\"${SECRET_SUBM_URL}\",g ; s,$verif_endp,\"${SECRET_VERIF_URL}\",g" HTTPClient+Configuration.swift > tmp.swift
-
-# Replace original file
-mv tmp.swift HTTPClient+Configuration.swift
+echo $serverEnvironments > ServerEnvironments.json
 
 cd -
