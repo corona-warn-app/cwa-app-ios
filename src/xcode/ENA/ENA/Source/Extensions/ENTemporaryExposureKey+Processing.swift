2cf7668f654b3d0162d20311d8fa83148c808fae
Nick Guendling <hello@nickguendling.com>
2020-09-25 14:58:49 +0200

    Add documentation

--- src/xcode/ENA/ENA/Source/Extensions/ENTemporaryExposureKey+Processing.swift
+++ src/xcode/ENA/ENA/Source/Extensions/ENTemporaryExposureKey+Processing.swift
@@ -23,34 +23,38 @@ import ExposureNotification
 extension Array where Element: ENTemporaryExposureKey {
 
 	/// Prepare an array of `ENTemporaryExposureKey` for exposure submission.
-	///
-	/// Performs the following steps:
-	/// 1. Groups the keys by their `rollingStartNumber`
-	/// 2. Applies the `transmissionRiskVector` to the grouped keys
 	func processedForSubmission(with symptomsOnset: SymptomsOnset, today: Date = Date()) -> [ENTemporaryExposureKey] {
+		/// 1. Group exposure keys by the day their rolling period started in the UTC timezone
 		var groupedExposureKeys: [Int: Self] = Dictionary(grouping: self, by: {
-			let rollingStartNumber = $0.rollingStartNumber
-			let startDate = Date(timeIntervalSince1970: Double(rollingStartNumber) * 600)
+			/// Use the rolling start number to get the date the rolling period started.
+			/// The rollingStartNumber is the unix timestamp divided by 600, giving the amount of of 10-minute-intervals that passed since 01.01.1970 00:00 UTC.
+			let startDate = Date(timeIntervalSince1970: Double($0.rollingStartNumber) * 600)
 
+			/// Make sure to use a calendar in UTC timezone with 24 hour days and leap seconds etc. in sync with the gregorian calendar
 			var calendar = Calendar(identifier: .gregorian)
 			// swiftlint:disable:next force_unwrapping
 			calendar.timeZone = TimeZone(secondsFromGMT: 0)!
 
+			/// Get the amount of days between start date and today to group the keys by that amount
 			guard let daysUntilToday = calendar.dateComponents([.day], from: startDate, to: today).day else { fatalError("Getting days since rolling start day failed") }
 
 			return daysUntilToday
 		})
 
+		/// 2. Assign the corresponding transmission risk levels and filter out keys that have no corresponding transmission risk level
 		for (daysUntilToday, exposureKeys) in groupedExposureKeys {
 			if daysUntilToday >= 0 && daysUntilToday <= 14 {
 				for exposureKey in exposureKeys {
+					/// Assign corresponding transmission risk level
 					exposureKey.transmissionRiskLevel = symptomsOnset.transmissionRiskVector[daysUntilToday]
 				}
 			} else {
+				/// Remove keys that have no corresponding transmission risk level
 				groupedExposureKeys[daysUntilToday] = nil
 			}
 		}
 
+		// Flatten dictionary [[0] -> [Key0], [1] -> [Key1, Key2]] to unsorted array [Key1, Key2, Key0]
 		return groupedExposureKeys.values.flatMap { $0 }
 	}
 
