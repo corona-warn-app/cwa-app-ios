617ccbcb5009a795ffd2b9d19b562931cbf683ff
Nick Guendling <hello@nickguendling.com>
2020-10-05 14:29:21 +0200

    Remove extension implementation from view controller

--- src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/View/Controller/QRScanner/ExposureSubmissionQRScannerViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/View/Controller/QRScanner/ExposureSubmissionQRScannerViewController.swift
@@ -309,20 +309,5 @@ private extension Array {
 	}
 }
 
-private extension UIImage {
-	static func with(color: UIColor) -> UIImage? {
-		let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
 
-		UIGraphicsBeginImageContext(rect.size)
-
-		if let context = UIGraphicsGetCurrentContext() {
-			context.setFillColor(color.cgColor)
-			context.fill(rect)
-		}
-
-		let image = UIGraphicsGetImageFromCurrentImageContext()
-		UIGraphicsEndImageContext()
-
-		return image
-	}
 }
