43a44d4339f8017d5db78808050935fec8e8f983
Nick Guendling <hello@nickguendling.com>
2020-10-08 13:56:28 +0200

    Add documentation

--- src/xcode/ENA/ENA/Source/Extensions/String+Insertion.swift
+++ src/xcode/ENA/ENA/Source/Extensions/String+Insertion.swift
@@ -19,6 +19,7 @@ import UIKit
 
 extension String {
 
+	/// Returns an attributed string that inserts the given string in an emphasized style
 	func inserting(emphasizedString: String) -> NSAttributedString {
 		return NSMutableAttributedString.generateAttributedString(normalText: self, attributedText: [
 			NSAttributedString(string: emphasizedString, attributes: [
