d54a6606116e15120f584ab8c85c69c68a4f66d9
tcfos <andreas.vogel@sap.com>
2020-10-13 17:41:52 +0200

    struct instead of tupel

--- src/xcode/ENA/ENA/Source/Scenes/AppInformation/AppInformationViewController+DynamicTableViewModel.swift
+++ src/xcode/ENA/ENA/Source/Scenes/AppInformation/AppInformationViewController+DynamicTableViewModel.swift
@@ -19,40 +19,47 @@
 import Foundation
 import UIKit
 
+struct category {
+	var text: String
+	var accessibilityIdentifier: String?
+	var action: DynamicAction
+}
+
 extension AppInformationViewController {
 	static let imprintViewModel = AppInformationImprintViewModel(preferredLocalization: Bundle.main.preferredLocalizations.first ?? "de")
-	static let model: [Category: (text: String, accessibilityIdentifier: String?, action: DynamicAction)] = [
-		.about: (
+	static let model: [Category: (category)] = [
+		.about: category(
 			text: AppStrings.AppInformation.aboutNavigation,
 			accessibilityIdentifier: AccessibilityIdentifiers.AppInformation.aboutNavigation,
 			action: .push(model: AppInformationModel.aboutModel, withTitle:  AppStrings.AppInformation.aboutNavigation)
-		),
-		.faq: (
+			),
+		
+		.faq: category(
 			text: AppStrings.AppInformation.faqNavigation,
 			accessibilityIdentifier: AccessibilityIdentifiers.AppInformation.faqNavigation,
 			action: .safari
 		),
-		.terms: (
+		.terms: category(
 			text: AppStrings.AppInformation.termsTitle,
 			accessibilityIdentifier: AccessibilityIdentifiers.AppInformation.termsNavigation,
 			action: .push(model: AppInformationModel.termsModel, withTitle:  AppStrings.AppInformation.termsNavigation)
 		),
-		.privacy: (
+		.privacy: category(
 			text: AppStrings.AppInformation.privacyNavigation,
 			accessibilityIdentifier: AccessibilityIdentifiers.AppInformation.privacyNavigation,
 			action: .push(model: AppInformationModel.privacyModel, withTitle:  AppStrings.AppInformation.privacyNavigation)
 		),
-		.legal: (
+		.legal: category(
 			text: AppStrings.AppInformation.legalNavigation,
 			accessibilityIdentifier: AccessibilityIdentifiers.AppInformation.legalNavigation,
 			action: .push(model: legalModel, separators: true, withTitle:  AppStrings.AppInformation.legalNavigation)
 		),
-		.contact: (
+		.contact: category(
 			text: AppStrings.AppInformation.contactNavigation,
 			accessibilityIdentifier: AccessibilityIdentifiers.AppInformation.contactNavigation,
 			action: .push(model: AppInformationModel.contactModel, withTitle:  AppStrings.AppInformation.contactNavigation)
 		),
-		.imprint: (
+		.imprint: category(
 			text: AppStrings.AppInformation.imprintNavigation,
 			accessibilityIdentifier: AccessibilityIdentifiers.AppInformation.imprintNavigation,
 			action: .push(model: imprintViewModel.dynamicTable, withTitle:  AppStrings.AppInformation.imprintNavigation)
