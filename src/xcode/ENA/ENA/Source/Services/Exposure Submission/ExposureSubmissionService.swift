0b83918f504dbc5ac2beae4854a8cd96b172a953
Kai Teuber <kai.teuber@app-developer.club>
2020-10-15 18:07:56 +0200

    removed a mark

--- src/xcode/ENA/ENA/Source/Services/Exposure Submission/ExposureSubmissionService.swift
+++ src/xcode/ENA/ENA/Source/Services/Exposure Submission/ExposureSubmissionService.swift
@@ -92,8 +92,6 @@ class ENAExposureSubmissionService: ExposureSubmissionService {
 		}
 	}
 
-	// Public API for accessing the service methods needed for Exposure Submission.
-
 	/// This method gets the test result based on the registrationToken that was previously
 	/// received, either from the TAN or QR Code flow. After successful completion,
 	/// the timestamp of the last received test is updated.
