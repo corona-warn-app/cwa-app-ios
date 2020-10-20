cbd2fe3956295299985583aa4d11cadd71eb174b
tcfos <andreas.vogel@sap.com>
2020-10-20 15:30:43 +0200

    New file for enum OnboardingPageType

--- src/xcode/ENA/ENA/Source/Scenes/Onboarding/OnboardingInfoViewController+Extension.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Onboarding/OnboardingInfoViewController+Extension.swift
@@ -19,24 +19,6 @@
 
 import UIKit
 
-
-enum OnboardingPageType: Int, CaseIterable {
-	case togetherAgainstCoronaPage = 0
-	case privacyPage = 1
-	case enableLoggingOfContactsPage = 2
-	case howDoesDataExchangeWorkPage = 3
-	case alwaysStayInformedPage = 4
-
-	func next() -> OnboardingPageType? {
-		OnboardingPageType(rawValue: rawValue + 1)
-	}
-
-	func isLast() -> Bool {
-		(self == OnboardingPageType.allCases.last)
-	}
-}
-
-
 extension OnboardingInfoViewController {
 
 	func addPanel(
