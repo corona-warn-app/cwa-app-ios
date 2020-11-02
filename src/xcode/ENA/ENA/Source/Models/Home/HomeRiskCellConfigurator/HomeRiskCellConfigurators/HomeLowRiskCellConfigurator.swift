11b098fe37d7da047edd0e8f0a32137a8c6058f2
Marcus Scherer <marcus@skunkbrothers.de>
2020-11-02 15:29:35 +0100

    Remove Label “Aktualisierung alle 24 Stunden" from riskcards

--- src/xcode/ENA/ENA/Source/Models/Home/HomeRiskCellConfigurator/HomeRiskCellConfigurators/HomeLowRiskCellConfigurator.swift
+++ src/xcode/ENA/ENA/Source/Models/Home/HomeRiskCellConfigurator/HomeRiskCellConfigurators/HomeLowRiskCellConfigurator.swift
@@ -45,7 +45,6 @@ final class HomeLowRiskCellConfigurator: HomeRiskLevelCellConfigurator {
 			state: state,
 			isButtonEnabled: manualExposureDetectionState == .possible,
 			isButtonHidden: isButtonHidden,
-			detectionIntervalLabelHidden: detectionMode != .automatic,
 			lastUpdateDate: lastUpdateDate,
 			detectionInterval: detectionInterval
 		)
@@ -69,10 +68,6 @@ final class HomeLowRiskCellConfigurator: HomeRiskLevelCellConfigurator {
 		cell.configureBody(text: "", bodyColor: titleColor, isHidden: true)
 		cell.configureRiskViews(cellConfigurators: itemCellConfigurators)
 		cell.configureBackgroundColor(color: color)
-		cell.configureDetectionIntervalLabel(
-			text: String(format: AppStrings.Home.riskCardIntervalUpdateTitle, "\(detectionInterval)"),
-			isHidden: detectionIntervalLabelHidden
-		)
 
 		configureButton(for: cell)
 		setupAccessibility(cell)
@@ -156,7 +151,6 @@ final class HomeLowRiskCellConfigurator: HomeRiskLevelCellConfigurator {
 		lhs.riskProviderState == rhs.riskProviderState &&
 		lhs.isButtonEnabled == rhs.isButtonEnabled &&
 		lhs.isButtonHidden == rhs.isButtonHidden &&
-		lhs.detectionIntervalLabelHidden == rhs.detectionIntervalLabelHidden &&
 		lhs.lastUpdateDate == rhs.lastUpdateDate &&
 		lhs.numberRiskContacts == rhs.numberRiskContacts &&
 		lhs.numberDays == rhs.numberDays &&
