98bd9d3b3f035f47589f52787b025dde51975bcc
Gahn, Karsten <karsten.gahn@sap.com>
2020-11-10 11:09:26 +0100

    Recatored unit test. Replace store with MockStore

--- src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/__tests__/ExposureSubmissionWarnOthersViewControllerTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/__tests__/ExposureSubmissionWarnOthersViewControllerTests.swift
@@ -23,10 +23,10 @@ import XCTest
 
 class ExposureSubmissionWarnOthersViewControllerTests: XCTestCase {
 	
-	private var store: SecureStore!
+	private var store: Store!
 	
 	override func setUpWithError() throws {
-		store = try SecureStore(at: URL(staticString: ":memory:"), key: "123456", serverEnvironment: ServerEnvironment())
+		store = MockTestStore()
 	}
 
 	private func createVC() -> ExposureSubmissionWarnOthersViewController {
