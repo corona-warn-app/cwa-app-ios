f34fc620c9574f4cd9608615361e0182fa1a3719
Artur Friesen <mail@arturfriesen.de>
2020-11-13 09:00:02 +0100

    Add marks.

--- src/xcode/ENA/ENA/Source/Models/Exposure/AppConfigMetadata.swift
+++ src/xcode/ENA/ENA/Source/Models/Exposure/AppConfigMetadata.swift
@@ -20,16 +20,9 @@
 import Foundation
 
 struct AppConfigMetadata: Codable, Equatable {
-	enum CodingKeys: String, CodingKey {
-		case lastAppConfigETag
-		case lastAppConfigFetch
-		case appConfig
-	}
-
-	var lastAppConfigETag: String
-	var lastAppConfigFetch: Date
-	var appConfig: SAP_Internal_ApplicationConfiguration
-
+	
+	// MARK: - Init
+	
 	init(
 		lastAppConfigETag: String,
 		lastAppConfigFetch: Date,
@@ -39,28 +32,42 @@ struct AppConfigMetadata: Codable, Equatable {
 		self.lastAppConfigFetch = lastAppConfigFetch
 		self.appConfig = appConfig
 	}
-
-	mutating func refeshLastAppConfigFetchDate() {
-		lastAppConfigFetch = Date()
+	
+	// MARK: - Protocol Codable
+	
+	enum CodingKeys: String, CodingKey {
+		case lastAppConfigETag
+		case lastAppConfigFetch
+		case appConfig
 	}
-
+	
 	init(from decoder: Decoder) throws {
 		let container = try decoder.container(keyedBy: CodingKeys.self)
-
+		
 		lastAppConfigETag = try container.decode(String.self, forKey: .lastAppConfigETag)
 		lastAppConfigFetch = try container.decode(Date.self, forKey: .lastAppConfigFetch)
-
+		
 		let appConfigData = try container.decode(Data.self, forKey: .appConfig)
 		appConfig = try SAP_Internal_ApplicationConfiguration(serializedData: appConfigData)
 	}
-
+	
 	func encode(to encoder: Encoder) throws {
 		var container = encoder.container(keyedBy: CodingKeys.self)
-
+		
 		try container.encode(lastAppConfigETag, forKey: .lastAppConfigETag)
 		try container.encode(lastAppConfigFetch, forKey: .lastAppConfigFetch)
-
+		
 		let appConfigData = try appConfig.serializedData()
 		try container.encode(appConfigData, forKey: .appConfig)
+	}
+	
+	// MARK: - Internal
+	
+	var lastAppConfigETag: String
+	var lastAppConfigFetch: Date
+	var appConfig: SAP_Internal_ApplicationConfiguration
+	
+	mutating func refeshLastAppConfigFetchDate() {
+		lastAppConfigFetch = Date()
 	}
 }
