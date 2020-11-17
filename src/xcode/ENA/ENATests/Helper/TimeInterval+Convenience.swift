3567edc5ecc856a668a58af8afe348a8062a21d9
Kai Teuber <kai.teuber@app-developer.club>
2020-11-17 18:07:31 +0100

    updated comments

--- src/xcode/ENA/ENATests/Helper/TimeInterval+Convenience.swift
+++ src/xcode/ENA/ENATests/Helper/TimeInterval+Convenience.swift
@@ -14,11 +14,11 @@ extension TimeInterval {
 		self = Double(days * 24 * 60 * 60)
 	}
 
-	/// One second
+	/// two seconds
 	static let short = 2.0
-	/// Three seconds
+	/// five seconds
 	static let medium = 5.0
-	/// Fife seconds
+	/// ten seconds
 	static let long = 10.0
 
 }
