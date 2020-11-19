9c1f91c67b3450f06c6a202266f64e665812b19d
Andreas Vogel <andreas.vogel@sap.com>
2020-11-19 13:33:02 +0100

    fixed test of ExposureSubmissionNavigationController

--- src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/__tests__/ExposureSubmissionNavigationControllerTest.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/__tests__/ExposureSubmissionNavigationControllerTest.swift
@@ -17,82 +17,83 @@
 // under the License.
 //
 
-//import XCTest
-//@testable import ENA
-//
-//final class ExposureSubmissionNavigationControllerTest: XCTestCase {
-//
-//	private func createVC() -> ExposureSubmissionNavigationController {
-//		// rootVC needs to be a ENANavigationControllerWithFooterChild to support buttons.
-//		let rootVC = AppStoryboard.exposureSubmission.initiate(viewControllerType: ExposureSubmissionIntroViewController.self) { coder -> UIViewController? in
-//			return ExposureSubmissionIntroViewController(coder: coder, coordinator: MockExposureSubmissionCoordinator())
-//		}
-//
-//		return AppStoryboard.exposureSubmission.initiateInitial { coder in
-//			ExposureSubmissionNavigationController(
-//				coder: coder,
-//				coordinator: MockExposureSubmissionCoordinator(),
-//				rootViewController: rootVC
-//			)
-//		}
-//	}
-//
-//	func testSetupSecondaryButton() {
-//		let vc = createVC()
-//		_ = vc.view
-//
-//		let rootVC = vc.topViewController
-//		let navItem = rootVC?.navigationItem as? ENANavigationFooterItem
-//
-//		let title = "Second Button Test"
-//
-//		navItem?.secondaryButtonTitle = title
-//		navItem?.isSecondaryButtonHidden = false
-//
-//		XCTAssertFalse(vc.footerView.secondaryButton.isHidden)
-//		XCTAssertEqual(vc.footerView.secondaryButton.currentTitle, title)
-//		XCTAssertEqual(vc.footerView.secondaryButton.state, UIButton.State.normal)
-//	}
-//
-//	func testHideSecondaryButton() {
-//		let vc = createVC()
-//		_ = vc.view
-//
-//		let rootVC = vc.topViewController
-//		let navItem = rootVC?.navigationItem as? ENANavigationFooterItem
-//
-//		navItem?.isSecondaryButtonHidden = false
-//		XCTAssertFalse(vc.footerView.secondaryButton.isHidden)
-//
-//		navItem?.isSecondaryButtonHidden = true
-//		XCTAssertLessThan(vc.footerView.secondaryButton.alpha, 0.01)
-//	}
-//
-//	func testSecondaryButtonAction() {
-//		let vc = createVC()
-//		_ = vc.view
-//
-//		let child = MockExposureSubmissionNavigationControllerChild()
-//		let expectation = self.expectation(description: "Button action executed.")
-//		child.didTapSecondButtonCallback = { expectation.fulfill() }
-//
-//		vc.pushViewController(child, animated: false)
-//		vc.footerView.secondaryButton.sendActions(for: .touchUpInside)
-//
-//		waitForExpectations(timeout: .short)
-//	}
-//
-//	func testButtonAction() {
-//		let vc = createVC()
-//		_ = vc.view
-//
-//		let child = MockExposureSubmissionNavigationControllerChild()
-//		let expectation = self.expectation(description: "Button action executed.")
-//		child.didTapButtonCallback = { expectation.fulfill() }
-//
-//		vc.pushViewController(child, animated: false)
-//		vc.footerView.primaryButton.sendActions(for: .touchUpInside)
-//
-//		waitForExpectations(timeout: .short)
-//	}
-//}
+import XCTest
+@testable import ENA
+
+final class ExposureSubmissionNavigationControllerTest: XCTestCase {
+
+	private func createVC() -> ExposureSubmissionNavigationController {
+		// rootVC needs to be a ENANavigationControllerWithFooterChild to support buttons.
+		let rootVC = AppStoryboard.exposureSubmission.initiate(viewControllerType: ExposureSubmissionHotlineViewController.self) { coder -> UIViewController? in
+			return ExposureSubmissionHotlineViewController(coder: coder, coordinator: MockExposureSubmissionCoordinator())
+		}
+		
+		return AppStoryboard.exposureSubmission.initiateInitial { coder in
+			ExposureSubmissionNavigationController(
+				coder: coder,
+				coordinator: MockExposureSubmissionCoordinator(),
+				rootViewController: rootVC
+			)
+		}
+	}
+
+	func testSetupSecondaryButton() {
+		let vc = createVC()
+		_ = vc.view
+
+		let rootVC = vc.topViewController
+		let navItem = rootVC?.navigationItem as? ENANavigationFooterItem
+
+		let title = "Second Button Test"
+
+		navItem?.secondaryButtonTitle = title
+		navItem?.isSecondaryButtonHidden = false
+
+		XCTAssertFalse(vc.footerView.secondaryButton.isHidden)
+		XCTAssertEqual(vc.footerView.secondaryButton.currentTitle, title)
+		XCTAssertEqual(vc.footerView.secondaryButton.state, UIButton.State.normal)
+	}
+
+	func testHideSecondaryButton() {
+		let vc = createVC()
+		_ = vc.view
+
+		let rootVC = vc.topViewController
+		let navItem = rootVC?.navigationItem as? ENANavigationFooterItem
+
+		navItem?.isSecondaryButtonHidden = false
+		XCTAssertFalse(vc.footerView.secondaryButton.isHidden)
+
+		navItem?.isSecondaryButtonHidden = true
+		XCTAssertLessThan(vc.footerView.secondaryButton.alpha, 0.01)
+	}
+
+	func testSecondaryButtonAction() {
+		let vc = createVC()
+		_ = vc.view
+
+		let child = MockExposureSubmissionNavigationControllerChild()
+		let expectation = self.expectation(description: "Button action executed.")
+		child.didTapSecondButtonCallback = { expectation.fulfill() }
+
+		vc.pushViewController(child, animated: false)	
+		vc.footerView.secondaryButton.sendActions(for: .touchUpInside)
+
+		waitForExpectations(timeout: .short)
+	}
+
+	func testButtonAction() {
+		let vc = createVC()
+		_ = vc.view
+
+		let child = MockExposureSubmissionNavigationControllerChild()
+		let expectation = self.expectation(description: "Button action executed.")
+		child.didTapButtonCallback = { expectation.fulfill() }
+
+		vc.pushViewController(child, animated: false)
+		vc.footerView.primaryButton.sendActions(for: .touchUpInside)
+
+		waitForExpectations(timeout: .short)
+	}
+	
+}
