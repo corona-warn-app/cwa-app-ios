4aba46c8b7509541d8bcbda9206c448c4807312d
Kai Teuber <kai.teuber@app-developer.club>
2020-11-19 12:37:38 +0100

    added empty viewcontroller and viewmodel

---
+++ src/xcode/ENA/ENA/Source/Scenes/TestresultAvailable/TestresultAvailableViewModel.swift
@@ -0,0 +1,27 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+
+struct TestresultAvailableViewModel {
+
+	// MARK: - Init
+
+	init(_ store: Store) {
+		self.store = store
+	}
+
+	// MARK: - Overrides
+
+	// MARK: - Protocol <#Name#>
+
+	// MARK: - Public
+
+	// MARK: - Internal
+
+	// MARK: - Private
+
+	private let store: Store
+
+}
