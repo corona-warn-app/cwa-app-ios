c21780f902b02b1cc7c213c05649c293482e7c36
Kai Teuber <kai.teuber@app-developer.club>
2020-11-20 11:49:36 +0100

    test to check if cell count match expectations

--- src/xcode/ENA/ENA/Source/Scenes/TestresultAvailable/__tests__/TestresultAvailableViewModelTest.swift
+++ src/xcode/ENA/ENA/Source/Scenes/TestresultAvailable/__tests__/TestresultAvailableViewModelTest.swift
@@ -30,4 +30,30 @@ class TestresultAvailableViewModelTest: XCTestCase {
 		waitForExpectations(timeout: .medium)
 	}
 
+	func testGIVEN_ViewModel_WHEN_getDynamicTableViewModel_THEN_SectionsAndCellMatchExpectation() {
+		// GIVEN
+		let mockStore = MockTestStore()
+		let expectationNotFulFill = expectation(description: "consent cell conde excecute")
+		expectationNotFulFill.isInverted = true
+
+		let viewModel = TestresultAvailableViewModel(
+			mockStore,
+			didTapConsentCell: {
+				expectationNotFulFill.fulfill()
+			},
+			didTapPrimaryFooterButton: {
+				expectationNotFulFill.fulfill()
+			})
+
+		// WHEN
+		let dynamicTableViewModel = viewModel.dynamicTableViewModel
+
+		// THEN
+		XCTAssertEqual(3, dynamicTableViewModel.numberOfSection)
+		XCTAssertEqual(0, dynamicTableViewModel.numberOfRows(section: 0))
+		XCTAssertEqual(1, dynamicTableViewModel.numberOfRows(section: 1))
+		XCTAssertEqual(2, dynamicTableViewModel.numberOfRows(section: 2))
+		waitForExpectations(timeout: .short)
+	}
+
 }
