cadf32e65f0e9f947e450d0fa89e42bf01324098
Kai Teuber <kai.teuber@app-developer.club>
2020-11-20 11:49:10 +0100

    added a helper method to dynamicTableViewModel

--- src/xcode/ENA/ENA/Source/Scenes/DynamicTableViewController/DynamicTableViewModel.swift
+++ src/xcode/ENA/ENA/Source/Scenes/DynamicTableViewController/DynamicTableViewModel.swift
@@ -13,28 +13,38 @@ struct DynamicTableViewModel {
 		creator(&model)
 		return model
 	}
-
+	
 	private var content: [DynamicSection]
-
+	
 	init(_ content: [DynamicSection]) {
 		self.content = content
 	}
-
+	
 	func section(_ section: Int) -> DynamicSection {
 		content[section]
 	}
-
+	
 	func section(at indexPath: IndexPath) -> DynamicSection {
 		section(indexPath.section)
 	}
-
+	
 	func cell(at indexPath: IndexPath) -> DynamicCell {
 		content[indexPath.section].cells[indexPath.row]
 	}
-
+	
 	var numberOfSection: Int { content.count }
-	func numberOfRows(inSection section: Int, for _: DynamicTableViewController) -> Int { self.section(section).cells.count }
-
+	
+	func numberOfRows(section index: Int) -> Int? {
+		guard content.indices.contains(index) else {
+			return nil
+		}
+		return section(index).cells.count
+	}
+	
+	func numberOfRows(inSection section: Int, for _: DynamicTableViewController) -> Int {
+		self.section(section).cells.count
+	}
+	
 	mutating func add(_ section: DynamicSection) {
 		content.append(section)
 	}
