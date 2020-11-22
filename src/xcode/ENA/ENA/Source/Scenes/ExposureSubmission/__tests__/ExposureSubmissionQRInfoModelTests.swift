585f1313be86a7ea52beab390a8307cfcb0f0e5c
Carsten Knoblich <carsten@staticline.de>
2020-11-22 15:51:06 +0100

    quick fix to unit test

--- src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/__tests__/ExposureSubmissionQRInfoModelTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/__tests__/ExposureSubmissionQRInfoModelTests.swift
@@ -12,8 +12,13 @@ class ExposureSubmissionQRInfoModelTests: XCTestCase {
 
 		let dynamicTableViewModel = viewModel.dynamicTableViewModel
 
-		XCTAssertEqual(dynamicTableViewModel.numberOfSection, 1)
-		XCTAssertEqual(dynamicTableViewModel.section(0).cells.count, 4)
+		XCTAssertEqual(dynamicTableViewModel.numberOfSection, 6)
+		XCTAssertEqual(dynamicTableViewModel.section(0).cells.count, 1)
+		XCTAssertEqual(dynamicTableViewModel.section(1).cells.count, 4)
+		XCTAssertEqual(dynamicTableViewModel.section(2).cells.count, 2)
+		XCTAssertEqual(dynamicTableViewModel.section(3).cells.count, 1)
+		XCTAssertEqual(dynamicTableViewModel.section(4).cells.count, 1)
+		XCTAssertEqual(dynamicTableViewModel.section(5).cells.count, 1)
 	}
 
 }
