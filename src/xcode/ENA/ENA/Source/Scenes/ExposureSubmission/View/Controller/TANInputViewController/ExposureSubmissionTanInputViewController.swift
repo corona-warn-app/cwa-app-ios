0d5a683d5a70adaf8af625de48cc54e2a9230edd
Kai Teuber <Kai.Teuber@app-developer.club>
2020-11-28 16:41:01 +0100

    ExposureSubmissionTanInputViewController is deprecated by now

--- src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/View/Controller/TANInputViewController/ExposureSubmissionTanInputViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/View/Controller/TANInputViewController/ExposureSubmissionTanInputViewController.swift
@@ -5,6 +5,7 @@
 import Foundation
 import UIKit
 
+@available(*, deprecated)
 class ExposureSubmissionTanInputViewController: UIViewController, ENANavigationControllerWithFooterChild, ENATanInputDelegate {
 
 	// MARK: - Init
