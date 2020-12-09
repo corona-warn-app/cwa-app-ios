d357cd3a0630c59da5b723c3b2499de678712cb4
Andreas Vogel <andreas.vogel@sap.com>
2020-12-09 16:48:17 +0100

    prepare for the new label

--- src/xcode/ENA/ENA/Source/Views/ENSetting/DescriptionTableViewCell.swift
+++ src/xcode/ENA/ENA/Source/Views/ENSetting/DescriptionTableViewCell.swift
@@ -20,21 +20,25 @@ class DescriptionTableViewCell: UITableViewCell, ConfigurableENSettingCell {
 		label1.text = AppStrings.ExposureNotificationSetting.descriptionText1
 		label2.text = AppStrings.ExposureNotificationSetting.descriptionText2
 		label3.text = AppStrings.ExposureNotificationSetting.descriptionText3
+//		label4.text = AppStrings.ExposureNotificationSetting.descriptionText4
 
 		titleLabel.isAccessibilityElement = true
 		label1.isAccessibilityElement = true
 		label2.isAccessibilityElement = true
 		label3.isAccessibilityElement = true
+//		label4.isAccessibilityElement = true
 
 		titleLabel.accessibilityIdentifier = (riskDetectionState == .disabled) ?
 			AccessibilityIdentifiers.ExposureNotificationSetting.descriptionTitleInactive : AccessibilityIdentifiers.ExposureNotificationSetting.descriptionTitle
 		label1.accessibilityIdentifier = AccessibilityIdentifiers.ExposureNotificationSetting.descriptionText1
 		label2.accessibilityIdentifier = AccessibilityIdentifiers.ExposureNotificationSetting.descriptionText2
 		label3.accessibilityIdentifier = AccessibilityIdentifiers.ExposureNotificationSetting.descriptionText3
+//		label4.accessibilityIdentifier = AccessibilityIdentifiers.ExposureNotificationSetting.descriptionText4
 
 		titleLabel.accessibilityTraits = .header
 		label1.accessibilityTraits = .staticText
 		label2.accessibilityTraits = .staticText
 		label3.accessibilityTraits = .staticText
+//		label4.accessibilityTraits = .staticText
 	}
 }
