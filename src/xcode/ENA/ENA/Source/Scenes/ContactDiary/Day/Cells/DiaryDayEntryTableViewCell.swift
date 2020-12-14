998df0bc90a5646b9d04cd151373a4845218c77b
Nick Guendling <hello@nickguendling.com>
2020-12-14 10:14:56 +0100

    Move cell configuration logic to cell models

--- src/xcode/ENA/ENA/Source/Scenes/ContactDiary/Day/Cells/DiaryDayEntryTableViewCell.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ContactDiary/Day/Cells/DiaryDayEntryTableViewCell.swift
@@ -9,20 +9,11 @@ class DiaryDayEntryTableViewCell: UITableViewCell {
 	// MARK: - Internal
 
 	func configure(entry: DiaryEntry) {
-		switch entry {
-		case .contactPerson(let contactPerson):
-			checkboxImageView.image = contactPerson.isSelected ? UIImage(named: "Diary_Checkmark_Selected") : UIImage(named: "Diary_Checkmark_Unselected")
-			label.text = contactPerson.name
-		case .location(let location):
-			checkboxImageView.image = location.isSelected ? UIImage(named: "Diary_Checkmark_Selected") : UIImage(named: "Diary_Checkmark_Unselected")
-			label.text = location.name
-		}
+		let cellModel = DiaryDayEntryCellModel(entry: entry)
 
-		if entry.isSelected {
-			accessibilityTraits = [.button, .selected]
-		} else {
-			accessibilityTraits = [.button]
-		}
+		checkboxImageView.image = cellModel.image
+		label.text = cellModel.text
+		accessibilityTraits = cellModel.accessibilityTraits
 	}
 
 	// MARK: - Private
