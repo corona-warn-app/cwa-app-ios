a1719bfa8407624f88fd86873ae892b810834856
Marcus Scherer <marcus@skunkbrothers.de>
2020-12-15 13:48:05 +0100

    Use more external frameworks

---
+++ src/xcode/ENA/ENA/Source/Utils/IBPCollectionViewCompositionalLayoutInteroperability.swift
@@ -0,0 +1,26 @@
+import IBPCollectionViewCompositionalLayout
+
+typealias NSCollectionLayoutAnchor = IBPNSCollectionLayoutAnchor
+typealias NSCollectionLayoutBoundarySupplementaryItem = IBPNSCollectionLayoutBoundarySupplementaryItem
+typealias NSCollectionLayoutDecorationItem = IBPNSCollectionLayoutDecorationItem
+typealias NSCollectionLayoutDimension = IBPNSCollectionLayoutDimension
+typealias NSCollectionLayoutEdgeSpacing = IBPNSCollectionLayoutEdgeSpacing
+typealias NSCollectionLayoutEnvironment = IBPNSCollectionLayoutEnvironment
+typealias NSCollectionLayoutGroup = IBPNSCollectionLayoutGroup
+typealias NSCollectionLayoutGroupCustomItem = IBPNSCollectionLayoutGroupCustomItem
+typealias NSCollectionLayoutItem = IBPNSCollectionLayoutItem
+typealias NSCollectionLayoutSection = IBPNSCollectionLayoutSection
+typealias NSCollectionLayoutSize = IBPNSCollectionLayoutSize
+typealias NSCollectionLayoutSpacing = IBPNSCollectionLayoutSpacing
+typealias NSCollectionLayoutSupplementaryItem = IBPNSCollectionLayoutSupplementaryItem
+typealias NSCollectionLayoutVisibleItem = IBPNSCollectionLayoutVisibleItem
+typealias NSDirectionalEdgeInsets = IBPNSDirectionalEdgeInsets
+typealias UICollectionLayoutSectionOrthogonalScrollingBehavior = IBPUICollectionLayoutSectionOrthogonalScrollingBehavior
+typealias UICollectionViewCompositionalLayout = IBPUICollectionViewCompositionalLayout
+typealias UICollectionViewCompositionalLayoutConfiguration = IBPUICollectionViewCompositionalLayoutConfiguration
+
+extension IBPNSDirectionalEdgeInsets {
+    public static var zero: IBPNSDirectionalEdgeInsets {
+        return IBPNSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+    }
+}
