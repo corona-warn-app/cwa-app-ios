b2c5a3b3f90d50118685851e5241d6bd306280f1
Nick Guendling <hello@nickguendling.com>
2020-12-18 11:35:14 +0100

    Restructure app delegate

--- src/xcode/ENA/ENA/Source/AppDelegate.swift
+++ src/xcode/ENA/ENA/Source/AppDelegate.swift
@@ -20,13 +20,130 @@ protocol CoronaWarnAppDelegate: AnyObject {
 	var contactDiaryStore: ContactDiaryStoreV1 { get }
 }
 
-extension AppDelegate: CoronaWarnAppDelegate {
-	// required - otherwise app will crash because cast will fail.
-}
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, CoronaWarnAppDelegate {
 
-extension AppDelegate {
+	// MARK: - Init
 
-	func showError(_ riskProviderError: RiskProviderError) {
+	override init() {
+		self.serverEnvironment = ServerEnvironment()
+
+		self.store = SecureStore(subDirectory: "database", serverEnvironment: serverEnvironment)
+
+		let configuration = HTTPClient.Configuration.makeDefaultConfiguration(store: store)
+		self.client = HTTPClient(configuration: configuration)
+		self.wifiClient = WifiOnlyHTTPClient(configuration: configuration)
+
+		downloadedPackagesStore.keyValueStore = self.store
+	}
+
+	// MARK: - Protocol UIApplicationDelegate
+
+	func application(
+		_: UIApplication,
+		didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil
+	) -> Bool {
+		UIDevice.current.isBatteryMonitoringEnabled = true
+
+		taskScheduler.delegate = self
+
+		// Setup DeadmanNotification after AppLaunch
+		UNUserNotificationCenter.current().scheduleDeadmanNotificationIfNeeded()
+
+		consumer.didFailCalculateRisk = { [weak self] error in
+			self?.showError(error)
+		}
+		riskProvider.observeRisk(consumer)
+
+		return true
+	}
+
+	// MARK: - Protocol CoronaWarnAppDelegate
+
+	let client: HTTPClient
+	let wifiClient: WifiOnlyHTTPClient
+	let downloadedPackagesStore: DownloadedPackagesStore = DownloadedPackagesSQLLiteStore(fileName: "packages")
+	let store: Store
+	let taskScheduler: ENATaskScheduler = ENATaskScheduler.shared
+	let serverEnvironment: ServerEnvironment
+	let contactDiaryStore = ContactDiaryStoreV1(fileName: "ContactDiary")
+
+	lazy var appConfigurationProvider: AppConfigurationProviding = {
+		#if DEBUG
+		if isUITesting {
+			// provide a static app configuration for ui tests to prevent validation errors
+			return CachedAppConfigurationMock()
+		}
+		#endif
+		// use a custom http client that uses/recognized caching mechanisms
+		let appFetchingClient = CachingHTTPClient(clientConfiguration: client.configuration)
+
+		let provider = CachedAppConfiguration(client: appFetchingClient, store: store)
+		// used to remove invalidated key packages
+		provider.packageStore = downloadedPackagesStore
+		return provider
+	}()
+
+	lazy var riskProvider: RiskProvider = {
+		let keyPackageDownload = KeyPackageDownload(
+			downloadedPackagesStore: downloadedPackagesStore,
+			client: client,
+			wifiClient: wifiClient,
+			store: store
+		)
+
+		#if !RELEASE
+		return RiskProvider(
+			configuration: .default,
+			store: store,
+			appConfigurationProvider: appConfigurationProvider,
+			exposureManagerState: exposureManager.exposureManagerState,
+			riskCalculation: DebugRiskCalculation(riskCalculation: RiskCalculation(), store: store),
+			keyPackageDownload: keyPackageDownload,
+			exposureDetectionExecutor: exposureDetectionExecutor
+		)
+		#else
+		return RiskProvider(
+			configuration: .default,
+			store: store,
+			appConfigurationProvider: appConfigurationProvider,
+			exposureManagerState: exposureManager.exposureManagerState,
+			keyPackageDownload: keyPackageDownload,
+			exposureDetectionExecutor: exposureDetectionExecutor
+		)
+		#endif
+	}()
+
+	#if targetEnvironment(simulator) || COMMUNITY
+	// Enable third party contributors that do not have the required
+	// entitlements to also use the app
+	let exposureManager: ExposureManager = {
+		let keys = [ENTemporaryExposureKey()]
+		return MockExposureManager(exposureNotificationError: nil, diagnosisKeysResult: (keys, nil))
+	}()
+	#else
+	let exposureManager: ExposureManager = ENAExposureManager()
+	#endif
+
+	// MARK: - Internal
+
+	let backgroundTaskConsumer = RiskConsumer()
+
+	// MARK: - Private
+
+	private var exposureDetection: ExposureDetection?
+	private let consumer = RiskConsumer()
+
+	private lazy var exposureDetectionExecutor: ExposureDetectionExecutor = {
+		ExposureDetectionExecutor(
+			client: self.client,
+			downloadedPackagesStore: self.downloadedPackagesStore,
+			store: self.store,
+			exposureDetector: self.exposureManager
+		)
+	}()
+
+	private func showError(_ riskProviderError: RiskProviderError) {
 		guard
 			let scene = UIApplication.shared.connectedScenes.first,
 			let delegate = scene.delegate as? SceneDelegate,
@@ -113,134 +230,5 @@ extension AppDelegate {
 			)
 		}
 	}
-}
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-	let store: Store
-	let contactDiaryStore = ContactDiaryStoreV1(fileName: "ContactDiary")
-	let serverEnvironment: ServerEnvironment
-
-	let taskScheduler: ENATaskScheduler = ENATaskScheduler.shared
-	let backgroundTaskConsumer = RiskConsumer()
-
-	lazy var appConfigurationProvider: AppConfigurationProviding = {
-		#if DEBUG
-		if isUITesting {
-			// provide a static app configuration for ui tests to prevent validation errors
-			return CachedAppConfigurationMock()
-		}
-		#endif
-		// use a custom http client that uses/recognized caching mechanisms
-		let appFetchingClient = CachingHTTPClient(clientConfiguration: client.configuration)
-		
-		let provider = CachedAppConfiguration(client: appFetchingClient, store: store)
-		// used to remove invalidated key packages
-		provider.packageStore = downloadedPackagesStore
-		return provider
-	}()
-
-	lazy var riskProvider: RiskProvider = {
-		let keyPackageDownload = KeyPackageDownload(
-			downloadedPackagesStore: downloadedPackagesStore,
-			client: client,
-			wifiClient: wifiClient,
-			store: store
-		)
-
-		#if !RELEASE
-		return RiskProvider(
-			configuration: .default,
-			store: store,
-			appConfigurationProvider: appConfigurationProvider,
-			exposureManagerState: exposureManager.exposureManagerState,
-			riskCalculation: DebugRiskCalculation(riskCalculation: RiskCalculation(), store: store),
-			keyPackageDownload: keyPackageDownload,
-			exposureDetectionExecutor: exposureDetectionExecutor
-		)
-		#else
-		return RiskProvider(
-			configuration: .default,
-			store: store,
-			appConfigurationProvider: appConfigurationProvider,
-			exposureManagerState: exposureManager.exposureManagerState,
-			keyPackageDownload: keyPackageDownload,
-			exposureDetectionExecutor: exposureDetectionExecutor
-		)
-		#endif
-
-	}()
-
-	#if targetEnvironment(simulator) || COMMUNITY
-	// Enable third party contributors that do not have the required
-	// entitlements to also use the app
-	let exposureManager: ExposureManager = {
-		let keys = [ENTemporaryExposureKey()]
-		return MockExposureManager(exposureNotificationError: nil, diagnosisKeysResult: (keys, nil))
-	}()
-	#else
-	let exposureManager: ExposureManager = ENAExposureManager()
-	#endif
-
-	private var exposureDetection: ExposureDetection?
-	private let consumer = RiskConsumer()
-
-	let downloadedPackagesStore: DownloadedPackagesStore = DownloadedPackagesSQLLiteStore(fileName: "packages")
-
-	let client: HTTPClient
-	let wifiClient: WifiOnlyHTTPClient
-
-	private lazy var exposureDetectionExecutor: ExposureDetectionExecutor = {
-		ExposureDetectionExecutor(
-			client: self.client,
-			downloadedPackagesStore: self.downloadedPackagesStore,
-			store: self.store,
-			exposureDetector: self.exposureManager
-		)
-	}()
-
-	override init() {
-		self.serverEnvironment = ServerEnvironment()
-
-		self.store = SecureStore(subDirectory: "database", serverEnvironment: serverEnvironment)
-
-		let configuration = HTTPClient.Configuration.makeDefaultConfiguration(store: store)
-		self.client = HTTPClient(configuration: configuration)
-		self.wifiClient = WifiOnlyHTTPClient(configuration: configuration)
-
-		downloadedPackagesStore.keyValueStore = self.store
-	}
-
-	func application(
-		_: UIApplication,
-		didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil
-	) -> Bool {
-
-		UIDevice.current.isBatteryMonitoringEnabled = true
-
-		taskScheduler.delegate = self
-
-		// Setup DeadmanNotification after AppLaunch
-		UNUserNotificationCenter.current().scheduleDeadmanNotificationIfNeeded()
-
-		consumer.didFailCalculateRisk = { [weak self] error in
-			self?.showError(error)
-		}
-		riskProvider.observeRisk(consumer)
-
-		return true
-	}
-
-	// MARK: UISceneSession Lifecycle
-
-	func application(
-		_: UIApplication,
-		configurationForConnecting connectingSceneSession: UISceneSession,
-		options _: UIScene.ConnectionOptions
-	) -> UISceneConfiguration {
-		UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-	}
-
-	func application(_: UIApplication, didDiscardSceneSessions _: Set<UISceneSession>) {}
 }
