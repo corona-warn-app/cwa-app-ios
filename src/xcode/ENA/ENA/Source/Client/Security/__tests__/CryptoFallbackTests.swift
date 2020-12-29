107ca51c32bab460678106416b2c54126efc0f54
Carsten Knoblich <carsten@staticline.de>
2020-12-29 12:11:07 +0100

    added dedicated tests for crypto fallbacks

---
+++ src/xcode/ENA/ENA/Source/Client/Security/__tests__/CryptoFallbackTests.swift
@@ -0,0 +1,42 @@
+////
+// 🦠 Corona-Warn-App
+//
+
+import XCTest
+import CryptoKit
+@testable import ENA
+
+
+@available(iOS 13.0, *)
+class CryptoFallbackTests: XCTestCase {
+
+    func testCustomSha256() throws {
+		let data = try XCTUnwrap("Alice".data(using: .utf8))
+
+		let customDigest = data.sha256_fallback()
+		let referenceDigest = Data(SHA256.hash(data: data))
+
+		XCTAssertEqual(customDigest, referenceDigest)
+    }
+
+    func testPerformanceCryptoKitSHA256() throws {
+        self.measure {
+			for i in 0..<500_000 {
+				// swiftlint:disable:next force_unwrapping
+				let data = "Alice\(i)".data(using: .utf8)!
+				_ = Data(SHA256.hash(data: data))
+			}
+        }
+    }
+
+	func testPerformanceFallbackSHA256() throws {
+		self.measure {
+			for i in 0..<500_000 {
+				// swiftlint:disable:next force_unwrapping
+				let data = "Alice\(i)".data(using: .utf8)!
+				_ = data.sha256_fallback()
+			}
+		}
+	}
+
+}
