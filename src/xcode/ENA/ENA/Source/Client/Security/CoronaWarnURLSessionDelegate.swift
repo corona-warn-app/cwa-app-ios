2592151ab8a14707cdd8eb1cf4e9de1f5211e799
Carsten Knoblich <carsten@staticline.de>
2021-01-03 21:36:36 +0100

    updated certificate hash for pinning

--- src/xcode/ENA/ENA/Source/Client/Security/CoronaWarnURLSessionDelegate.swift
+++ src/xcode/ENA/ENA/Source/Client/Security/CoronaWarnURLSessionDelegate.swift
@@ -5,11 +5,15 @@
 import Foundation
 
 final class CoronaWarnURLSessionDelegate: NSObject {
-	private let localPublicKey: String
+	private let publicKeyHash: String
 
-	// MARK: Creating a Delegate
-	init(localPublicKey: String) {
-		self.localPublicKey = localPublicKey
+	// MARK: - Creating a Delegate
+
+
+	/// Initializes a CWA Session delegate
+	/// - Parameter publicKeyHash: the SHA256 of the certificate to pin
+	init(publicKeyHash: String) {
+		self.publicKeyHash = publicKeyHash
 	}
 }
 
@@ -33,8 +37,9 @@ extension CoronaWarnURLSessionDelegate: URLSessionDelegate {
 			#if DEBUG
 			// debug/review: print the chain
 			for i in 0..<SecTrustGetCertificateCount(trust) {
-				let cert = SecTrustGetCertificateAtIndex(trust, i)
-				Log.debug("[\(challenge.protectionSpace.host)] @ \(i): \(cert.debugDescription)", log: .api)
+				if let cert = SecTrustGetCertificateAtIndex(trust, i) {
+					Log.debug("[\(challenge.protectionSpace.host)] @ \(i): \(cert)", log: .crypto)
+				}
 			}
 			#endif
 
@@ -47,7 +52,7 @@ extension CoronaWarnURLSessionDelegate: URLSessionDelegate {
 
 				// Matching fingerprint?
 				let keyHash = serverPublicKeyData.sha256String()
-				if localPublicKey == keyHash {
+				if publicKeyHash == keyHash {
 					// Success! This is our server
 					completionHandler(.useCredential, URLCredential(trust: trust))
 					return
@@ -56,23 +61,5 @@ extension CoronaWarnURLSessionDelegate: URLSessionDelegate {
 		}
 
 		completionHandler(.cancelAuthenticationChallenge, /* credential */ nil)
-	}
-}
-
-// [0] https://developer.apple.com/documentation/security/certificate_key_and_trust_services/trust/evaluating_a_trust_and_parsing_the_result
-
-extension CoronaWarnURLSessionDelegate {
-	var rsa2048Asn1HeaderBytes: [UInt8] { [
-		0x30, 0x82, 0x01, 0x22, 0x30, 0x0d, 0x06, 0x09,
-		0x2a, 0x86, 0x48, 0x86, 0xf7, 0x0d, 0x01, 0x01,
-		0x01, 0x05, 0x00, 0x03, 0x82, 0x01, 0x0f, 0x00
-	] }
-
-	private func sha256ForRSA2048(data: Data) -> String {
-		var keyWithHeader = Data(rsa2048Asn1HeaderBytes)
-		keyWithHeader.append(data)
-
-		let hash = keyWithHeader.sha256()
-		return Data(hash).base64EncodedString()
 	}
 }
