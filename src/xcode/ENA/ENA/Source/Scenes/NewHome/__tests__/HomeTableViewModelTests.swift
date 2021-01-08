0ea135fe387df8a0d2bfa8a3aebdd6e4bb95d537
Marcus Scherer <marcus@skunkbrothers.de>
2021-01-08 17:21:06 +0100

    HomeTableViewModelTests

--- src/xcode/ENA/ENA/Source/Scenes/NewHome/__tests__/HomeTableViewModelTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/NewHome/__tests__/HomeTableViewModelTests.swift
@@ -3,6 +3,7 @@
 //
 
 import XCTest
+@testable import ENA
 
 class HomeTableViewModelTests: XCTestCase {
 
@@ -14,16 +15,83 @@ class HomeTableViewModelTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSectionsRowsAndHeights() throws {
+		let sut = HomeTableViewModel(
+			state: .init(
+				store: MockTestStore(),
+				riskProvider: MockRiskProvider(),
+				exposureManagerState: .init(authorized: true, enabled: true, status: .active),
+				enState: .enabled,
+				exposureSubmissionService: MockExposureSubmissionService()
+			)
+		)
+		// Number of Sections
+		XCTAssertEqual(sut.numberOfSections, 5, "Number of sections does not match.")
+		
+		// Number of Rows per Section
+		XCTAssertEqual(sut.numberOfRows(in: 0), 1, "Number of rows in section 0 does not match.")
+		XCTAssertEqual(sut.numberOfRows(in: 1), 2, "Number of rows in section 1 does not match.")
+		XCTAssertEqual(sut.numberOfRows(in: 2), 1, "Number of rows in section 2 does not match.")
+		XCTAssertEqual(sut.numberOfRows(in: 3), 2, "Number of rows in section 3 does not match.")
+		XCTAssertEqual(sut.numberOfRows(in: 4), 2, "Number of rows in section 4 does not match.")
+		
+		// Check riskAndTestRows
+		XCTAssertEqual(sut.riskAndTestRows, [.risk, .testResult], "Risk and Test Rows does not match.")
+		
+		// Height for Header
+		XCTAssertEqual(sut.heightForHeader(in: 0), 0, "Height for Header in Section 0 does not match.")
+		XCTAssertEqual(sut.heightForHeader(in: 1), 0, "Height for Header in Section 1 does not match.")
+		XCTAssertEqual(sut.heightForHeader(in: 2), 0, "Height for Header in Section 2 does not match.")
+		XCTAssertEqual(sut.heightForHeader(in: 3), 16, "Height for Header in Section 3 does not match.")
+		XCTAssertEqual(sut.heightForHeader(in: 4), 16, "Height for Header in Section 4 does not match.")
+		
+		// Height for Footer
+		XCTAssertEqual(sut.heightForFooter(in: 0), 0, "Height for Footer in Section 0 does not match.")
+		XCTAssertEqual(sut.heightForFooter(in: 1), 0, "Height for Footer in Section 1 does not match.")
+		XCTAssertEqual(sut.heightForFooter(in: 2), 0, "Height for Footer in Section 2 does not match.")
+		XCTAssertEqual(sut.heightForFooter(in: 3), 16, "Height for Footer in Section 3 does not match.")
+		XCTAssertEqual(sut.heightForFooter(in: 4), 32, "Height for Footer in Section 4 does not match.")
+		
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+	func testRiskAndTestRowsIfKeysSubmitted() {
+		let mockStore = MockTestStore()
+		mockStore.lastSuccessfulSubmitDiagnosisKeyTimestamp = Int64(Date().timeIntervalSince1970)
+		
+		let sut = HomeTableViewModel(
+			state: .init(
+				store: mockStore,
+				riskProvider: MockRiskProvider(),
+				exposureManagerState: .init(authorized: true, enabled: true, status: .active),
+				enState: .enabled,
+				exposureSubmissionService: MockExposureSubmissionService()
+			)
+		)
+		
+		XCTAssertEqual(sut.numberOfRows(in: 1), 1, "Number of rows in section 1 does not match.")
+		
+		XCTAssertEqual(sut.riskAndTestRows, [.thankYou], "Risk and Test Rows does not match.")
+	}
+	
+	func testRiskAndTestRowsIfPositiveTestResultWasShown() {
+		let mockStore = MockTestStore()
+		mockStore.registrationToken = "FAKETOKEN!"
+		mockStore.positiveTestResultWasShown = true
+		
+		let sut = HomeTableViewModel(
+			state: .init(
+				store: mockStore,
+				riskProvider: MockRiskProvider(),
+				exposureManagerState: .init(authorized: true, enabled: true, status: .active),
+				enState: .enabled,
+				exposureSubmissionService: MockExposureSubmissionService()
+			)
+		)
+		sut.state.testResult = .positive
+		
+		XCTAssertEqual(sut.numberOfRows(in: 1), 1, "Number of rows in section 1 does not match.")
+		XCTAssertEqual(sut.riskAndTestRows, [.shownPositiveTestResult], "Risk and Test Rows does not match.")
+
+	}
 
 }
