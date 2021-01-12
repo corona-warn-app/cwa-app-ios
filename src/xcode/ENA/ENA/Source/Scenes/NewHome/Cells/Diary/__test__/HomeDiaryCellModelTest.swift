d6f5deb6669420e76c8980e5f5c46935fb4dfb20
Kai Teuber <Kai.Teuber@app-developer.club>
2021-01-12 14:43:41 +0100

    added HomeDiaryCellModelTest

---
+++ src/xcode/ENA/ENA/Source/Scenes/NewHome/Cells/Diary/__test__/HomeDiaryCellModelTest.swift
@@ -0,0 +1,21 @@
+////
+// 🦠 Corona-Warn-App
+//
+
+import XCTest
+@testable import ENA
+
+class HomeDiaryCellModelTest: XCTestCase {
+
+	func testGIVEN_HomeDiaryCellModel_THEN_InitilizedAsExpected() {
+		// GIVEN
+		let homeDiaryCellModel = HomeDiaryCellModel()
+
+		// THEN
+		XCTAssertEqual(homeDiaryCellModel.title, AppStrings.Home.diaryCardTitle)
+		XCTAssertEqual(homeDiaryCellModel.description, AppStrings.Home.diaryCardBody)
+		XCTAssertEqual(homeDiaryCellModel.buttonTitle, AppStrings.Home.diaryCardButton)
+		XCTAssertEqual(homeDiaryCellModel.image, UIImage(named: "Illu_Diary"))
+		XCTAssertEqual(homeDiaryCellModel.accessibilityIdentifier, AccessibilityIdentifiers.Home.diaryCardButton)
+	}
+}
