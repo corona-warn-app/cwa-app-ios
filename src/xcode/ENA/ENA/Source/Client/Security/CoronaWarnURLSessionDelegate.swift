0006855133867301052a66a7708c339564a4e2ca
Carsten Knoblich <carsten@staticline.de>
2021-01-17 22:32:51 +0100

    added iOS 12.5 fallback code to certificate pinning

--- src/xcode/ENA/ENA/Source/Client/Security/CoronaWarnURLSessionDelegate.swift
+++ src/xcode/ENA/ENA/Source/Client/Security/CoronaWarnURLSessionDelegate.swift
@@ -35,38 +35,59 @@ extension CoronaWarnURLSessionDelegate: URLSessionDelegate {
 		// that might return something different than `errSecSuccess`.
 		//
 		// [1]: https://developer.apple.com/documentation/security/certificate_key_and_trust_services/trust/evaluating_a_trust_and_parsing_the_result
-		SecTrustEvaluateAsyncWithError(trust, .main) { [weak self] trust, isValid, error in
-			guard isValid else {
-				Log.error("Evaluation failed", log: .api, error: error)
-				completionHandler(.cancelAuthenticationChallenge, /* credential */ nil)
-				return
-			}
-			#if DEBUG
-			// debug/review: print the chain
-			for i in 0..<SecTrustGetCertificateCount(trust) {
-				if let cert = SecTrustGetCertificateAtIndex(trust, i) {
-					Log.debug("[\(challenge.protectionSpace.host)] @ \(i): \(cert)", log: .crypto)
-				}
-			}
-			#endif
-
-			// we expect a chain of at least 2 certificates
-			// index '1' is the required intermediate
-			if
-				let serverCertificate = SecTrustGetCertificateAtIndex(trust, 1),
-				let serverPublicKey = SecCertificateCopyKey(serverCertificate),
-				let serverPublicKeyData = SecKeyCopyExternalRepresentation(serverPublicKey, nil ) as Data? {
-
-				// Matching fingerprint?
-				let keyHash = serverPublicKeyData.sha256String()
-				if self?.publicKeyHash == keyHash {
-					// Success! This is our server
-					completionHandler(.useCredential, URLCredential(trust: trust))
+		if #available(iOS 13.0, *) {
+			SecTrustEvaluateAsyncWithError(trust, .main) { [weak self] trust, isValid, error in
+				guard isValid else {
+					Log.error("Evaluation failed", log: .api, error: error)
+					completionHandler(.cancelAuthenticationChallenge, /* credential */ nil)
 					return
 				}
+				self?.evaluate(challenge: challenge, trust: trust, completionHandler: completionHandler)
 			}
+		} else {
+			var secresult = SecTrustResultType.invalid
+			let status = SecTrustEvaluate(trust, &secresult)
 
-			completionHandler(.cancelAuthenticationChallenge, /* credential */ nil)
+			if status == errSecSuccess {
+				self.evaluate(challenge: challenge, trust: trust, completionHandler: completionHandler)
+			} else {
+				completionHandler(.cancelAuthenticationChallenge, /* credential */ nil)
+			}
 		}
+	}
+
+
+	/// Common evaluation, covering iOS versions 12.5 or 13.x
+	/// - Parameters:
+	///   - challenge: A challenge from a server requiring authentication from the client.
+	///   - trust: Shortcur for `challenge.protectionSpace.serverTrust`
+	///   - completionHandler: the completion handler to accept or reject the request
+	private func evaluate(challenge: URLAuthenticationChallenge, trust: SecTrust, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+		#if DEBUG
+		// debug/review: print the chain
+		for i in 0..<SecTrustGetCertificateCount(trust) {
+			if let cert = SecTrustGetCertificateAtIndex(trust, i) {
+				Log.debug("[\(challenge.protectionSpace.host)] @ \(i): \(cert)", log: .crypto)
+			}
+		}
+		#endif
+
+		// we expect a chain of at least 2 certificates
+		// index '1' is the required intermediate
+		if
+			let serverCertificate = SecTrustGetCertificateAtIndex(trust, 1),
+			let serverPublicKey = SecCertificateCopyKey(serverCertificate),
+			let serverPublicKeyData = SecKeyCopyExternalRepresentation(serverPublicKey, nil ) as Data? {
+
+			// Matching fingerprint?
+			let keyHash = serverPublicKeyData.sha256String()
+			if publicKeyHash == keyHash {
+				// Success! This is our server
+				completionHandler(.useCredential, URLCredential(trust: trust))
+				return
+			}
+		}
+
+		completionHandler(.cancelAuthenticationChallenge, /* credential */ nil)
 	}
 }
