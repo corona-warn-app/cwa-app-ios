a7dedbc7b7fa3f8feace9e15d935417bdc17d1b5
Marcus Scherer <marcus@skunkbrothers.de>
2021-01-19 15:23:18 +0100

    Use imageLiteralResourceName to avoid !

--- src/xcode/ENA/ENA/Source/Scenes/UpdateOS/UpdateOSViewModel.swift
+++ src/xcode/ENA/ENA/Source/Scenes/UpdateOS/UpdateOSViewModel.swift
@@ -5,10 +5,8 @@
 import UIKit
 
 struct UpdateOSViewModel {
-	// swiftlint:disable:next force_unwrapping
-	let image = UIImage(named: "Illu_UpdateOS")!
-	// swiftlint:disable:next force_unwrapping
-	let logoImage = UIImage(named: "Corona-Warn-App")!
+	let image = UIImage(imageLiteralResourceName: "Illu_UpdateOS")
+	let logoImage = UIImage(imageLiteralResourceName: "Corona-Warn-App")
 	let title = AppStrings.UpdateOS.title
 	let text = AppStrings.UpdateOS.text
 }
