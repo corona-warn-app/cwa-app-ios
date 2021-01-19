646e0defc564701d8eb4c2f110e9cd4c0e9b809c
Bastian Kohlbauer <bastian.kohlbauer@googelmail.com>
2021-01-19 16:09:25 +0100

    code style improvements

--- src/xcode/ENA/ENA/Source/Scenes/RiskLegend/Cell/RiskLegendDotBodyCell.swift
+++ src/xcode/ENA/ENA/Source/Scenes/RiskLegend/Cell/RiskLegendDotBodyCell.swift
@@ -17,25 +17,24 @@ class RiskLegendDotBodyCell: UITableViewCell {
 	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
 		super.init(style: style, reuseIdentifier: reuseIdentifier)
 		// dotView
-		dotView = UIView()
 		dotView.backgroundColor = .enaColor(for: .riskHigh)
 		dotView.layer.cornerRadius = 8
 		dotView.layer.masksToBounds = true
 		dotView.translatesAutoresizingMaskIntoConstraints = false
 		contentView.addSubview(dotView)
+		// label
+		label.style = .body
+		label.translatesAutoresizingMaskIntoConstraints = false
+		contentView.addSubview(label)
+		// set constraints
 		NSLayoutConstraint.activate([
+			// dotView
 			dotView.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor, constant: 8),
 			dotView.trailingAnchor.constraint(lessThanOrEqualTo: contentView.layoutMarginsGuide.trailingAnchor, constant: -8),
 			dotView.centerYAnchor.constraint(equalTo: contentView.layoutMarginsGuide.centerYAnchor),
 			dotView.heightAnchor.constraint(equalToConstant: 16),
-			dotView.widthAnchor.constraint(equalToConstant: 16)
-		])
-		// label
-		label = ENALabel()
-		label.style = .body
-		label.translatesAutoresizingMaskIntoConstraints = false
-		contentView.addSubview(label)
-		NSLayoutConstraint.activate([
+			dotView.widthAnchor.constraint(equalToConstant: 16),
+			// label
 			label.leadingAnchor.constraint(equalTo: dotView.trailingAnchor, constant: 24),
 			label.trailingAnchor.constraint(lessThanOrEqualTo: contentView.layoutMarginsGuide.trailingAnchor, constant: -8),
 			label.centerYAnchor.constraint(equalTo: contentView.layoutMarginsGuide.centerYAnchor),
@@ -46,6 +45,6 @@ class RiskLegendDotBodyCell: UITableViewCell {
 	
 	// MARK: - Internal
 	
-	var dotView: UIView!
-	var label: ENALabel!
+	let dotView = UIView()
+	let label = ENALabel()
 }
