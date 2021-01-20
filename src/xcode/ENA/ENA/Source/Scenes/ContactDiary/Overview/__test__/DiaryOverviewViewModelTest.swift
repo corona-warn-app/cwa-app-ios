7bb54355fb42f9d117d0179211ff8251eb22c5ac
Pascal Brause <pascal@brause.haus>
2021-01-20 16:00:56 +0100

    add tests for DiaryOverviewDayCellModel

--- src/xcode/ENA/ENA/Source/Scenes/ContactDiary/Overview/__test__/DiaryOverviewViewModelTest.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ContactDiary/Overview/__test__/DiaryOverviewViewModelTest.swift
@@ -52,6 +52,87 @@ class DiaryOverviewViewModelTest: XCTestCase {
 		XCTAssertEqual(viewModel.numberOfRows(in: 1), 14)
 	}
 
+	func testGIVEN_DiaryOverviewViewModel_WHEN_noneHistoryExposureIsInStore_THEN_NoneHistoryExposureIsReturned() {
+		// GIVEN
+		let viewModel = DiaryOverviewViewModel(
+			diaryStore: makeMockStore(),
+			store: MockTestStore()
+		)
+
+		// WHEN
+		let diaryOverviewDayCellModel = viewModel.cellModel(for: IndexPath(row: 4, section: 0))
+
+		// THEN
+		XCTAssertEqual(diaryOverviewDayCellModel.historyExposure, .none)
+	}
+
+	func testGIVEN_DiaryOverviewViewModel_WHEN_lowHistoryExposureIsInStore_THEN_LowHistoryExposureIsReturned() throws {
+
+		// GIVEN
+		let dateFormatter = ISO8601DateFormatter.contactDiaryFormatter
+
+		let todayString = dateFormatter.string(from: Date())
+		let today = try XCTUnwrap(dateFormatter.date(from: todayString))
+
+		let todayMinus5Days = try XCTUnwrap(Calendar.current.date(byAdding: .day, value: -5, to: today))
+		let store = MockTestStore()
+		store.riskCalculationResult = RiskCalculationResult(
+			riskLevel: .low,
+			minimumDistinctEncountersWithLowRisk: 1,
+			minimumDistinctEncountersWithHighRisk: 1,
+			mostRecentDateWithLowRisk: nil,
+			mostRecentDateWithHighRisk: nil,
+			numberOfDaysWithLowRisk: 1,
+			numberOfDaysWithHighRisk: 1,
+			calculationDate: today,
+			riskLevelPerDate: [todayMinus5Days: .low]
+		)
+		let viewModel = DiaryOverviewViewModel(
+			diaryStore: makeMockStore(),
+			store: store
+		)
+
+		// WHEN
+		let diaryOverviewDayCellModel = viewModel.cellModel(for: IndexPath(row: 5, section: 0))
+
+		// THEN
+		XCTAssertEqual(diaryOverviewDayCellModel.historyExposure, .encounter(.low))
+	}
+
+	func testGIVEN_DiaryOverviewViewModel_WHEN_highHistoryExposureIsInStore_THEN_HighHistoryExposureIsReturned() throws {
+
+		// GIVEN
+		let dateFormatter = ISO8601DateFormatter.contactDiaryFormatter
+
+		let todayString = dateFormatter.string(from: Date())
+		let today = try XCTUnwrap(dateFormatter.date(from: todayString))
+
+		let todayMinus5Days = try XCTUnwrap(Calendar.current.date(byAdding: .day, value: -7, to: today))
+		let store = MockTestStore()
+		store.riskCalculationResult = RiskCalculationResult(
+			riskLevel: .low,
+			minimumDistinctEncountersWithLowRisk: 1,
+			minimumDistinctEncountersWithHighRisk: 1,
+			mostRecentDateWithLowRisk: nil,
+			mostRecentDateWithHighRisk: nil,
+			numberOfDaysWithLowRisk: 1,
+			numberOfDaysWithHighRisk: 1,
+			calculationDate: today,
+			riskLevelPerDate: [todayMinus5Days: .high]
+		)
+		let viewModel = DiaryOverviewViewModel(
+			diaryStore: makeMockStore(),
+			store: store
+		)
+
+
+		// WHEN
+		let diaryOverviewDayCellModel = viewModel.cellModel(for: IndexPath(row: 7, section: 0))
+
+		// THEN
+		XCTAssertEqual(diaryOverviewDayCellModel.historyExposure, .encounter(.high))
+	}
+
 	// MARK: - Private Helpers
 
 	func makeMockStore() -> MockDiaryStore {
