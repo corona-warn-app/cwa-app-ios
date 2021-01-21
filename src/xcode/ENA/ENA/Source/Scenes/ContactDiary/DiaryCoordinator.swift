413e9b5b08a05033b2d344ad2ec34da4f4373d0f
Bastian Kohlbauer <bastian.kohlbauer@googelmail.com>
2021-01-21 11:02:19 +0100

    add export subject by utilizing UIActivityItemSource

--- src/xcode/ENA/ENA/Source/Scenes/ContactDiary/DiaryCoordinator.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ContactDiary/DiaryCoordinator.swift
@@ -169,9 +169,9 @@ class DiaryCoordinator {
 		} else {
 			exportString = ""
 		}
-
+		let exportItem = DiaryExportItem(subject: "", body: exportString)
 		let viewController = UIActivityViewController(
-			activityItems: [exportString],
+			activityItems: [exportItem],
 			applicationActivities: nil
 		)
 		parentNavigationController?.present(viewController, animated: true, completion: nil)
