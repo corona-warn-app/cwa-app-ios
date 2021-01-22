972652d9c10b1d35c1543b000924ce84f0336fd2
Gahn, Karsten <karsten.gahn@sap.com>
2021-01-22 17:33:43 +0100

    Updated UnitTests

--- src/xcode/ENA/ENA/Source/Scenes/Onboarding/DeltaOnboarding/NewVersionFeatures/DeltaOnboardingNewVersionFeatures.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Onboarding/DeltaOnboarding/NewVersionFeatures/DeltaOnboardingNewVersionFeatures.swift
@@ -7,19 +7,7 @@ import UIKit
 class DeltaOnboardingNewVersionFeatures: DeltaOnboarding {
 
 	let version = "1.12"
-	// [KGA] Code cleanup
-//	let version = Bundle.main.appVersion
 	let store: Store
-	
-//	/// Overwrites DeltaOnboarding protocol default isFinished to use 'store.newVersionFeaturesShowVersion' for boolean check
-//	var isFinished: Bool {
-//		return store.newVersionFeaturesShownForVersion.numericGreaterOrEqual(then: version)
-//	}
-//
-//	/// Overwrites DeltaOnboarding protocol default isFinished to use 'store.newVersionFeaturesShownForVersion' for boolean check
-//	func finish() {
-//		store.newVersionFeaturesShownForVersion = version
-//	}
 
 	init(store: Store) {
 		self.store = store
