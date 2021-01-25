7c0deef7492f5639cee5c653bcc6be42bde0999a
Gahn, Karsten <karsten.gahn@sap.com>
2021-01-25 09:28:36 +0100

    Added UI Tests

--- src/xcode/ENA/ENAUITests/DeltaOnboarding/ENAUITestsDeltaOnboarding.swift
+++ src/xcode/ENA/ENAUITests/DeltaOnboarding/ENAUITestsDeltaOnboarding.swift
@@ -75,8 +75,47 @@ class ENAUITests_06_DeltaOnboarding: XCTestCase {
 		
 		snapshot("\(screenshotLabel)_\(String(format: "%04d", (screenshotCounter.inc() )))")
 		app.swipeUp()
+	}
+	
+	func testDeltaOnboardingNewVersionFeaturesView() throws {
+		app.launchArguments.append(contentsOf: ["-isOnboarded", "YES"])
+		app.launchArguments.append(contentsOf: ["-onboardingVersion", "1.11"])
 		
+		app.launch()
 		
+		// The "Information zur Funktionsweise der Risiko-Ermittlung"
+		// appears on fresh installs (e.g. every CI-run) but not on already started apps.
+		// We dismiss it if present.
+		let alert = app.alerts.firstMatch
+		if alert.exists {
+			alert.buttons.firstMatch.tap()
+		}
+
+		let tablesQuery = XCUIApplication().tables
+		XCTAssertTrue(tablesQuery.images["AppStrings.DeltaOnboarding.newVersionFeaturesAccImageLabel"].waitForExistence(timeout: 5.0))
+	}
+	
+	func test_screenshotDeltaOnboardingNewVersionFeaturesView() throws {
+		app.launchArguments.append(contentsOf: ["-isOnboarded", "YES"])
+		app.launchArguments.append(contentsOf: ["-onboardingVersion", "1.11"])
+		
+		app.launch()
+		
+		var screenshotCounter = 0
+		let screenshotLabel = "deltaOnboarding_newVersionFeatures"
+		
+		// The "Information zur Funktionsweise der Risiko-Ermittlung"
+		// appears on fresh installs (e.g. every CI-run) but not on already started apps.
+		// We dismiss it if present.
+		let alert = app.alerts.firstMatch
+		if alert.exists {
+			alert.buttons.firstMatch.tap()
+		}
+
+		let tablesQuery = XCUIApplication().tables
+		XCTAssertTrue(tablesQuery.images["AppStrings.DeltaOnboarding.newVersionFeaturesAccImageLabel"].waitForExistence(timeout: 5.0))
+		
+		snapshot("\(screenshotLabel)_\(String(format: "%04d", (screenshotCounter.inc() )))")
 	}
 
 }
