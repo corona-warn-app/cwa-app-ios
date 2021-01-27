6e4b4ea8d8e53e8a3e92b99a2287f733042f4603
Carsten Knoblich <carsten@staticline.de>
2021-01-27 16:19:25 +0100

    updated test to new server environment handling

--- src/xcode/ENA/ENA/Source/Services/Risk/Provider/Helper/__tests__/StatisticsProviderTests.swift
+++ src/xcode/ENA/ENA/Source/Services/Risk/Provider/Helper/__tests__/StatisticsProviderTests.swift
@@ -38,10 +38,10 @@ class StatisticsProviderTests: XCTestCase {
 		fetchedFromClientExpectation.expectedFulfillmentCount = 1
 
 		let store = MockTestStore()
-		XCTAssertNil(store.appConfigMetadata)
+		XCTAssertNil(store.statistics)
 
-		let config = HTTPClient.Configuration.makeDefaultConfiguration(store: store)
-		let client = CachingHTTPClient(clientConfiguration: config)
+		store.selectedServerEnvironment = ServerEnvironment().defaultEnvironment()
+		let client = CachingHTTPClient(serverEnvironmentProvider: store)
 		client.fetchStatistics(etag: "foo") { result in
 			switch result {
 			case .success(let response): // StatisticsFetchingResponse
@@ -50,6 +50,7 @@ class StatisticsProviderTests: XCTestCase {
 
 				// check that we dont fetch mock data
 				XCTAssertNotEqual(CachingHTTPClientMock.staticStatistics, response.stats)
+				XCTAssertNotNil(store.statistics)
 			case .failure(let error):
 				XCTFail(error.localizedDescription)
 			}
