59ec85aa65b72343f7b340f0b264a09999b89324
Gahn, Karsten <karsten.gahn@sap.com>
2021-01-28 09:58:42 +0100

    New Version Features 1.13 - added AppStrings

--- src/xcode/ENA/ENA/Source/View Helpers/AppStrings.swift
+++ src/xcode/ENA/ENA/Source/View Helpers/AppStrings.swift
@@ -831,6 +831,33 @@ enum AppStrings {
 		
 		static let generalDescription = NSLocalizedString("DeltaOnboarding_NewVersionFeatures_Description", comment: "")
 		
+		/* Version 1.13 */
+		
+		static let feature113RkiSurveyTitle = NSLocalizedString("NewVersionFeature_113_rki_survey_title", comment: "")
+		
+		static let feature113RkiSurveyDescription = NSLocalizedString("NewVersionFeature_113_rki_survey_description", comment: "")
+		
+		static let feature113DataDonationTitle = NSLocalizedString("NewVersionFeature_113_data_donation_title", comment: "")
+		
+		static let feature113DataDonationDescription = NSLocalizedString("NewVersionFeature_113_data_donation_description", comment: "")
+		
+		static let feature113RiskCardTitle = NSLocalizedString("NewVersionFeature_113_risk_cards_title", comment: "")
+		
+		static let feature113RiskCardDescription = NSLocalizedString("NewVersionFeature_113_risk_cards_description", comment: "")
+		
+		static let feature113RiskDeterminationRandomIdTitle = NSLocalizedString("NewVersionFeature_113_risk_determination_random_id_title", comment: "")
+		
+		static let feature113RiskDeterminationRandomIdDescription = NSLocalizedString("NewVersionFeature_113_risk_determination_random_id_description", comment: "")
+		
+		static let feature113NewTabBarTitle = NSLocalizedString("NewVersionFeature_113_new_tabbar_title", comment: "")
+		
+		static let feature113NewTabBarDescription = NSLocalizedString("NewVersionFeature_113_new_tabbar_description", comment: "")
+		
+		static let feature113AddInfoAboutTestProcedureTitle = NSLocalizedString("NewVersionFeature_113_additonal_info_about_test_procedure_title", comment: "")
+		
+		static let feature113AddInfoAboutTestProcedureDescription = NSLocalizedString("NewVersionFeature_113_additonal_info_about_test_procedure_description", comment: "")
+		
+		/* Version 1.12 */
 		static let feature112StatisticsTitle = NSLocalizedString("NewVersionFeature_112_statistics_title", comment: "")
 		
 		static let feature112StatisticsDescription = NSLocalizedString("NewVersionFeature_112_statistics_description", comment: "")
