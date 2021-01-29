72b21eeb9250e56be1588d7a43c844eb16a6cddf
Pascal Brause <pascal@brause.haus>
2021-01-29 16:32:35 +0100

    init ppac

--- src/xcode/ENA/ENA/Source/Services/OTP/OTPService.swift
+++ src/xcode/ENA/ENA/Source/Services/OTP/OTPService.swift
@@ -4,6 +4,10 @@
 
 import Foundation
 
+enum OTPError: Error {
+	case ppacFailed
+}
+
 protocol OTPServiceProviding {
 	func getValidOTP(_ completion: @escaping (Result<String, Error>) -> Void)
 }
@@ -13,8 +17,13 @@ final class OTPService: OTPServiceProviding {
 
 	// MARK: - Init
 
-	init(store: Store) {
+	init(store: Store) throws {
 		self.store = store
+		do {
+			self.ppac = try PrivacyPreservingAccessControlService(store: store)
+		} catch {
+			throw OTPError.ppacFailed
+		}
 	}
 
 	// MARK: - Overrides
@@ -44,5 +53,6 @@ final class OTPService: OTPServiceProviding {
 	// MARK: - Private
 
 	private let store: Store
+	private let ppac: PrivacyPreservingAccessControl
 
 }
