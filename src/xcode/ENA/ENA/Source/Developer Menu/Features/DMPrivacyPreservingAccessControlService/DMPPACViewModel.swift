e1a6fd61415688b6e34145e54a68675a0981e41e
Kai Teuber <Kai.Teuber@app-developer.club>
2021-02-01 16:17:37 +0100

    added view model to generate cell view model cases

--- src/xcode/ENA/ENA/Source/Developer Menu/Features/DMPrivacyPreservingAccessControlService/DMPPACViewModel.swift
+++ src/xcode/ENA/ENA/Source/Developer Menu/Features/DMPrivacyPreservingAccessControlService/DMPPACViewModel.swift
@@ -5,21 +5,60 @@
 import Foundation
 
 final class DMPPCViewModel {
-
+	
 	// MARK: - Init
-
-	init(_ store: Store, deviceCheck: DeviceCheckable) {
-		do {
-			self.ppacService = try? PPACService(store: store, deviceCheck: deviceCheck)
-		}
+	
+	init(
+		_ store: Store,
+		deviceCheck: DeviceCheckable) {
+		self.ppacService = try? PPACService(store: store, deviceCheck: deviceCheck)
 	}
-
+	
 	// MARK: - Public
-
+	
 	// MARK: - Internal
-
+	
+	var numberOfSections: Int {
+		TableViewSections.allCases.count
+	}
+	
+	func numberOfRows(in section: Int) -> Int {
+		guard TableViewSections.allCases.indices.contains(section) else {
+			return 0
+		}
+		// at the moment we assume one cell per section only
+		return 1
+	}
+	
+	func cellViewModel(by indexPath: IndexPath) -> DMKeyValueCellViewModel {
+		guard let section = TableViewSections(rawValue: indexPath.section) else {
+			fatalError("Unknown cell requested - stop")
+		}
+		
+		switch section {
+		case .deviceTimeCheckState:
+			return DMKeyValueCellViewModel(key: "DeviceTime status", value: "unknown")
+		case .apiTokenWithCreatinDate:
+			return DMKeyValueCellViewModel(key: "API Token", value: "12345")
+		case .generateDeviceToken:
+			return DMKeyValueCellViewModel(key: "renew", value: "tap to generate a new API Token")
+		case .generateAPIToken:
+			return DMKeyValueCellViewModel(key: "API Token", value: "A12345 \n tap to generate a new one")
+		}
+		
+	}
+	
+	
 	// MARK: - Private
-
+	
+	private enum TableViewSections: Int, CaseIterable {
+		case deviceTimeCheckState
+		case apiTokenWithCreatinDate
+		case generateDeviceToken
+		case generateAPIToken
+		// todo: force API Token authorization -> OTP
+	}
+	
 	private let ppacService: PrivacyPreservingAccessControl?
-
+	
 }
