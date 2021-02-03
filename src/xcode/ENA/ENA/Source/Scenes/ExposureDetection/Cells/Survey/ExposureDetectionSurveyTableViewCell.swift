0bff052f498d1d9916d9a890d3219d7db1d1e208
Artur Friesen <mail@arturfriesen.de>
2021-02-03 14:21:29 +0100

    Add private.

--- src/xcode/ENA/ENA/Source/Scenes/ExposureDetection/Cells/Survey/ExposureDetectionSurveyTableViewCell.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ExposureDetection/Cells/Survey/ExposureDetectionSurveyTableViewCell.swift
@@ -56,7 +56,7 @@ class ExposureDetectionSurveyTableViewCell: UITableViewCell {
 	@IBOutlet private var descriptionLabel: ENALabel!
 	@IBOutlet private var illustrationView: UIImageView!
 	@IBOutlet private var button: ENAButton!
-	@IBOutlet weak var cardView: HomeCardView!
+	@IBOutlet private weak var cardView: HomeCardView!
 	
 	private var onPrimaryAction: (() -> Void)?
 
