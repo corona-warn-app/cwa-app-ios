5a7c77ec1365b00a4269326fe8091dc317fe9af1
Pascal Brause <pascal@brause.haus>
2021-02-03 14:42:20 +0100

    success simple test for OTPServiceTest

--- src/xcode/ENA/ENA/Source/Services/OTP/__tests__/OTPServiceTests.swift
+++ src/xcode/ENA/ENA/Source/Services/OTP/__tests__/OTPServiceTests.swift
@@ -7,15 +7,32 @@ import XCTest
 
 class OTPServiceTests: XCTestCase {
 
-	func testGIVEN_OTPService_WHEN_retrieveOTP_THEN_OTPIsGenerated() {
+	func testGIVEN_OTPService_WHEN_NoOtpIsStored_THEN_SuccessAndOtpIsGeneratedAndReturned() {
 		// GIVEN
-		let otpService = OTPServiceMock()
+		let store = MockTestStore()
+		let client = ClientMock()
+		let otpService = OTPService(store: store, client: client)
+		let ppacToken = PPACToken(apiToken: "apiTokenFake", deviceToken: "deviceTokenFake")
+
+		let expectation = self.expectation(description: "completion handler is called without an error")
+		var expectedOtp: String?
 
 		// WHEN
-
+		otpService.getValidOTP(ppacToken: ppacToken, completion: { result in
+			switch result {
+			case .success(let otp):
+				expectedOtp = otp
+				expectation.fulfill()
+			case .failure:
+				XCTFail("Test should not fail")
+			}
+		})
 
 		// THEN
+		waitForExpectations(timeout: .short)
 
-
+		XCTAssertNotNil(expectedOtp)
+		XCTAssertNotNil(store.otpToken)
+		XCTAssertEqual(expectedOtp, store.otpToken?.token)
 	}
 }
