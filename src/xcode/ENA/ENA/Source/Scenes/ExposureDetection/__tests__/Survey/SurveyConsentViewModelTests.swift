d73317c7b1e35d6c64813c4d2250e5142deb1f2b
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-02-04 18:03:39 +0100

    add test url

--- src/xcode/ENA/ENA/Source/Scenes/ExposureDetection/__tests__/Survey/SurveyConsentViewModelTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ExposureDetection/__tests__/Survey/SurveyConsentViewModelTests.swift
@@ -8,7 +8,8 @@ import XCTest
 class SurveyConsentViewModelTests: XCTestCase {
 
 	func testDynamicTableViewModel() {
-		let viewModel = SurveyConsentViewModel()
+		let placeHolderString = "https://www.test.de"
+		let viewModel = SurveyConsentViewModel(urlString: placeHolderString)
 
 		let dynamicTableViewModel = viewModel.dynamicTableViewModel
 
