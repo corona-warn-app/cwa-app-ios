febd32b915117c9e1bd0c8d3568e1de5fd405fc3
Gahn, Karsten <karsten.gahn@sap.com>
2021-02-05 09:54:59 +0100

    Code clean-up

--- src/xcode/ENA/ENA/Source/Scenes/Onboarding/DeltaOnboarding/DeltaOnboardingProtocols.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Onboarding/DeltaOnboarding/DeltaOnboardingProtocols.swift
@@ -21,21 +21,23 @@ protocol DeltaOnboarding {
 extension DeltaOnboarding {
 	var isFinished: Bool {
 		Log.debug("Check, if \(id) is already finished...")
-		// [KGA]
 		let presented = store.finishedDeltaOnboardings[version] != nil && ((store.finishedDeltaOnboardings[version]?.contains(id)) == true)
-		Log.debug("DeltaOnboarding \(id) already presented: \(presented).")
+		
+		Log.debug("DeltaOnboarding \(id) already presented? \(presented).")
 		if !store.onboardingVersion.numericGreater(then: version) && !presented {
-			Log.debug("isFinished will return: false")
+			Log.debug("isFinished -> false")
 			return false
 		}
-		Log.debug("isFinished will return: true")
+		Log.debug("isFinished -> true")
 		return true
 		
 	}
 	
-	// [KGA] Documentation
+	/// The `id` is a identifier generated out of the version of the concrete delta onboarding implementation and the corresponding class name
+	/// This ensures that we
+	/// a) can have multiple delta onboardings for the same version
+	/// b) can reuse existing prodotocl implementation (e. g. new features) by just changing the version.
 	var id: String {
-		Log.debug("DeltaOnboarding ID: \(version + String(describing: self))")
 		return version + String(describing: self)
 	}
 	
@@ -45,18 +47,15 @@ extension DeltaOnboarding {
 			store.onboardingVersion = version
 		}
 				
-		// [KGA]
 		if var finishedDeltaOnboardingVersion = store.finishedDeltaOnboardings[version] {
 			if !finishedDeltaOnboardingVersion.contains(id) {
 				finishedDeltaOnboardingVersion.append(id)
 				store.finishedDeltaOnboardings[version] = finishedDeltaOnboardingVersion
-				Log.debug("Finished DeltaOnboarding for Version \(version): \(id)")
+				Log.debug("Another DeltaOnboarding finished for Version \(version): \(id)")
 			}
 		} else {
 			store.finishedDeltaOnboardings[version] = [id]
 			Log.debug("First finished DeltaOnboarding for Version \(version): \(id)")
 		}
-
 	}
-
 }
