a3cf1dddff71a66766d209267917993114eb359c
Kai Teuber <Kai.Teuber@app-developer.club>
2021-02-08 16:46:21 +0100

    some layout improvments

--- src/xcode/ENA/ENA/Source/Scenes/Datadonation/DataDonationViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Datadonation/DataDonationViewController.swift
@@ -44,32 +44,41 @@ class DataDonationViewController: UIViewController {
 	private let didTapSelectAge: () -> Void
 
 	private func setupDummyView() {
+		title = "DataDonation Dummy"
+		view.backgroundColor = .enaColor(for: .background)
 
 		let firstButton = UIButton(type: .custom)
 		firstButton.translatesAutoresizingMaskIntoConstraints = false
 		firstButton.setTitle("Land", for: .normal)
+		firstButton.setTitleColor(.black, for: .normal)
+		firstButton.backgroundColor = .enaColor(for: .buttonPrimary)
 		firstButton.addTarget(self, action: #selector(didTapSelectCountryButton), for: .touchUpInside)
 
-		let secuondButton = UIButton(type: .custom)
-		secuondButton.translatesAutoresizingMaskIntoConstraints = false
-		secuondButton.setTitle("Kreis", for: .normal)
-		secuondButton.addTarget(self, action: #selector(didTapSelectRegionButton), for: .touchUpInside)
+		let secondButton = UIButton(type: .custom)
+		secondButton.translatesAutoresizingMaskIntoConstraints = false
+		secondButton.setTitle("Kreis", for: .normal)
+		secondButton.setTitleColor(.black, for: .normal)
+		secondButton.backgroundColor = .enaColor(for: .buttonPrimary)
+		secondButton.addTarget(self, action: #selector(didTapSelectRegionButton), for: .touchUpInside)
 
 		let thirdButton = UIButton(type: .custom)
 		thirdButton.translatesAutoresizingMaskIntoConstraints = false
 		thirdButton.setTitle("Alter", for: .normal)
+		thirdButton.setTitleColor(.black, for: .normal)
+		thirdButton.backgroundColor = .enaColor(for: .buttonPrimary)
 		thirdButton.addTarget(self, action: #selector(didTapAgeButton), for: .touchUpInside)
 
-		let stackView = UIStackView(arrangedSubviews: [firstButton, secuondButton, thirdButton])
+		let stackView = UIStackView(arrangedSubviews: [firstButton, secondButton, thirdButton])
 		stackView.translatesAutoresizingMaskIntoConstraints = false
 		stackView.alignment = .fill
 		stackView.distribution = .fillEqually
+		stackView.spacing = 8.0
 		stackView.axis = .vertical
 		view.addSubview(stackView)
 
 		NSLayoutConstraint.activate([
-			view.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
-			view.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
+			view.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: -25.0),
+			view.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: 25.0),
 			view.centerYAnchor.constraint(equalTo: stackView.centerYAnchor),
 			firstButton.heightAnchor.constraint(equalToConstant: 40.0)
 		])
