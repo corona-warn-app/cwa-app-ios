f661ec3e03a811d85aab11839916a1d6d062f300
Kai Teuber <Kai.Teuber@app-developer.club>
2021-02-08 16:31:25 +0100

    added DataDonationViewController (dummy)

---
+++ src/xcode/ENA/ENA/Source/Scenes/Datadonation/DataDonationViewController.swift
@@ -0,0 +1,93 @@
+////
+// 🦠 Corona-Warn-App
+//
+
+import UIKit
+
+class DataDonationViewController: UIViewController {
+
+	// MARK: - Init
+	init(
+		didTapSelectCountry: @escaping () -> Void,
+		didTapSelectRegion: @escaping () -> Void,
+		didTapSelectAge: @escaping () -> Void
+	) {
+		self.didTapSelectAge = didTapSelectAge
+		self.didTapSelectRegion = didTapSelectRegion
+		self.didTapSelectCountry = didTapSelectCountry
+		super.init(nibName: nil, bundle: nil)
+	}
+
+	@available(*, unavailable)
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	// MARK: - Overrides
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		setupDummyView()
+	}
+
+	// MARK: - Protocol <#Name#>
+
+	// MARK: - Public
+
+	// MARK: - Internal
+
+	// MARK: - Private
+
+	private let didTapSelectCountry: () -> Void
+	private let didTapSelectRegion: () -> Void
+	private let didTapSelectAge: () -> Void
+
+	private func setupDummyView() {
+
+		let firstButton = UIButton(type: .custom)
+		firstButton.translatesAutoresizingMaskIntoConstraints = false
+		firstButton.setTitle("Land", for: .normal)
+		firstButton.addTarget(self, action: #selector(didTapSelectCountryButton), for: .touchUpInside)
+
+		let secuondButton = UIButton(type: .custom)
+		secuondButton.translatesAutoresizingMaskIntoConstraints = false
+		secuondButton.setTitle("Kreis", for: .normal)
+		secuondButton.addTarget(self, action: #selector(didTapSelectRegionButton), for: .touchUpInside)
+
+		let thirdButton = UIButton(type: .custom)
+		thirdButton.translatesAutoresizingMaskIntoConstraints = false
+		thirdButton.setTitle("Alter", for: .normal)
+		thirdButton.addTarget(self, action: #selector(didTapAgeButton), for: .touchUpInside)
+
+		let stackView = UIStackView(arrangedSubviews: [firstButton, secuondButton, thirdButton])
+		stackView.translatesAutoresizingMaskIntoConstraints = false
+		stackView.alignment = .fill
+		stackView.distribution = .fillEqually
+		stackView.axis = .vertical
+		view.addSubview(stackView)
+
+		NSLayoutConstraint.activate([
+			view.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+			view.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
+			view.centerYAnchor.constraint(equalTo: stackView.centerYAnchor),
+			firstButton.heightAnchor.constraint(equalToConstant: 40.0)
+		])
+	}
+
+	@objc
+	private func didTapSelectCountryButton() {
+		Log.debug("Did hit select country Button")
+	}
+
+	@objc
+	private func didTapSelectRegionButton() {
+		Log.debug("Did hit select region Button")
+	}
+
+	@objc
+	private func didTapAgeButton() {
+		Log.debug("Did hit select age Button")
+	}
+
+}
