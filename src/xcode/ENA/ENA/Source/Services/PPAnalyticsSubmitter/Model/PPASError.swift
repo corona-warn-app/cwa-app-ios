253a1f41e8ccb426f4bf263b531f9e06ca2bceec
Pascal Brause <pascal@brause.haus>
2021-02-08 11:57:32 +0100

    made PPASError equatable

--- src/xcode/ENA/ENA/Source/Services/PPAnalyticsSubmitter/Model/PPASError.swift
+++ src/xcode/ENA/ENA/Source/Services/PPAnalyticsSubmitter/Model/PPASError.swift
@@ -5,10 +5,32 @@
 import Foundation
 
 enum PPASError: Error {
+
 	case generalError
 	case urlCreationError
 	case responseError(Int)
 	case jsonError
 	case serverError(PPAServerErrorCode)
 	case serverFailure(Error)
+}
+
+extension PPASError: Equatable {
+	static func == (lhs: PPASError, rhs: PPASError) -> Bool {
+		switch(lhs, rhs) {
+		case (.generalError, .generalError):
+			return true
+		case (.urlCreationError, .urlCreationError):
+			return true
+		case let (.responseError(lhsCode), .responseError(rhsCode)):
+			return lhsCode == rhsCode ? true : false
+		case (.jsonError, .jsonError):
+			return true
+		case let (.serverError(lhsError), .serverError(rhsError)):
+			return lhsError == rhsError ? true : false
+		case (.serverFailure, .serverFailure):
+			return true
+		default:
+			return false
+		}
+	}
 }
