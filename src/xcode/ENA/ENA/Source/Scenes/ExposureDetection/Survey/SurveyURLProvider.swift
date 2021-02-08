34104491b17922120c3c49c9ff159614291d42b8
Artur Friesen <mail@arturfriesen.de>
2021-02-08 14:08:40 +0100

    Add logs.

--- src/xcode/ENA/ENA/Source/Scenes/ExposureDetection/Survey/SurveyURLProvider.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ExposureDetection/Survey/SurveyURLProvider.swift
@@ -26,6 +26,7 @@ final class SurveyURLProvider: SurveyURLProvidable {
 	// MARK: - Internal
 
 	func getURL(_ completion: @escaping (Result<URL, SurveyConsentError>) -> Void) {
+		Log.info("Request Survey URL.", log: .survey)
 		getPPACToken(completion: completion)
 	}
 
@@ -37,34 +38,46 @@ final class SurveyURLProvider: SurveyURLProvidable {
 	private var subscriptions = [AnyCancellable]()
 
 	private func getPPACToken(completion: @escaping (Result<URL, SurveyConsentError>) -> Void) {
+		Log.info("Request PPAC token.", log: .survey)
+
 		ppacService.getPPACToken { [weak self] result in
 			switch result {
 			case .success(let ppacToken):
+				Log.info("Successfully created PPAC token.", log: .survey)
 				self?.getOTP(for: ppacToken, completion: completion)
 			case .failure(let ppacError):
+				Log.error("Failed to create PPAC token with error: \(ppacError)", log: .survey)
 				completion(.failure(SurveyConsentError(ppacError: ppacError)))
 			}
 		}
 	}
 
 	private func getOTP(for ppacToken: PPACToken, completion: @escaping (Result<URL, SurveyConsentError>) -> Void) {
+		Log.info("Request OTP token.", log: .survey)
+
 		otpService.getOTP(ppacToken: ppacToken) { [weak self] result in
 			switch result {
 			case .success(let otp):
+				Log.info("Successfully created survey OTP.", log: .survey)
 				self?.createSurveyURL(with: otp, completion: completion)
 			case .failure(let otpError):
+				Log.error("Failed to create survey OTP with error: \(otpError)", log: .survey)
 				completion(.failure(SurveyConsentError(otpError: otpError)))
 			}
 		}
 	}
 
 	private func createSurveyURL(with otp: String, completion: @escaping (Result<URL, SurveyConsentError>) -> Void) {
+		Log.info("Request surveyOnHighRiskURL and otpQueryParameterName from app config.", log: .survey)
+
 		configurationProvider.appConfiguration().sink { configuration in
 			let baseURLString = configuration.eventDrivenUserSurveyParameters.common.surveyOnHighRiskURL
 			let queryParameterName = configuration.eventDrivenUserSurveyParameters.common.otpQueryParameterName
 			if let surveyURL = URL(string: baseURLString + "?\(queryParameterName)=\(otp)") {
+				Log.info("Successfully created survey URL.", log: .survey)
 				completion(.success(surveyURL))
 			} else {
+				Log.error("Failed to create URL based on app config.", log: .survey)
 				completion(.failure(.tryAgainLater))
 			}
 		}.store(in: &subscriptions)
