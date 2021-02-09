1cd4f43e071641dd47a2df3a9a04e2a978028657
Andreas Vogel <andreas.vogel@sap.com>
2021-02-09 15:10:30 +0100

    ViewModel & ViewController

--- src/xcode/ENA/ENA/Source/Scenes/Datadonation/DataDonationDetailsViewModel.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Datadonation/DataDonationDetailsViewModel.swift
@@ -15,5 +15,26 @@ final class DataDonationDetailsViewModel {
 	
 	// MARK: - Internal
 	
+	var dynamicTableViewModel: DynamicTableViewModel {
+		DynamicTableViewModel.with {
+			
+			$0.add(
+				.section(
+					header: .image(
+						UIImage(named: "Illu_DataDonation"),
+						accessibilityLabel: "AppStrings.DataDonation.Info.accImageDescription",
+						accessibilityIdentifier: "AccessibilityIdentifiers.DataDonation.accImageDescription",
+						height: 250
+					),
+					cells: [
+						// [KGA] To be updated ;-)
+//						.subheadline(text: AppStrings.NewVersionFeatures.generalDescription, color: UIColor.enaColor(for: .textPrimary2), accessibilityIdentifier: AccessibilityIdentifiers.DeltaOnboarding.newVersionFeaturesGeneralDescription)
+						.subheadline(text: AppStrings.DataDonation.Info.description, accessibilityIdentifier: "")
+					]
+				)
+			)
+		}
+	}
+	
 	// MARK: - Private
 }
