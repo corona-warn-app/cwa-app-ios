22e944a0e7133ecf73ce504d2f188ead3d650102
Nick Guendling <hello@nickguendling.com>
2021-02-11 15:03:19 +0100

    Adjust data structure

--- src/xcode/ENA/ENA/Source/Scenes/ContactDiary/Model/ContactPersonEncounter.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ContactDiary/Model/ContactPersonEncounter.swift
@@ -4,12 +4,51 @@
 
 import Foundation
 
-struct ContactPersonEncounter {
+struct ContactPersonEncounter: Equatable {
+
+	// MARK: - Init
+
+	enum Duration: Int {
+		case lessThan15Minutes, moreThan15Minutes
+	}
+
+	enum MaskSituation: Int {
+		case withMask, withoutMask
+	}
+
+	enum LocationType: Int {
+		case outside, inside
+	}
+
+	init(
+		id: Int,
+		date: String,
+		contactPersonId: Int,
+		duration: Duration? = nil,
+		maskSituation: MaskSituation? = nil,
+		locationType: LocationType? = nil,
+		circumstances: String = ""
+	) {
+		self.id = id
+		self.date = date
+		self.contactPersonId = contactPersonId
+		self.duration = duration
+		self.maskSituation = maskSituation
+		self.locationType = locationType
+		self.circumstances = circumstances
+	}
+
 
 	// MARK: - Internal
 
 	let id: Int
 	let date: String
 	let contactPersonId: Int
+
+	let duration: Duration?
+	let maskSituation: MaskSituation?
+	let locationType: LocationType?
+
+	let circumstances: String
 	
 }
--- src/xcode/ENA/ENA/Source/Scenes/ContactDiary/Model/LocationVisit.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ContactDiary/Model/LocationVisit.swift
@@ -4,12 +4,32 @@
 
 import Foundation
 
-struct LocationVisit {
+struct LocationVisit: Equatable {
+
+	// MARK: - Init
+
+	internal init(
+		id: Int,
+		date: String,
+		locationId: Int,
+		duration: Int = 0,
+		circumstances: String = ""
+	) {
+		self.id = id
+		self.date = date
+		self.locationId = locationId
+		self.duration = duration
+		self.circumstances = circumstances
+	}
+
 
 	// MARK: - Internal
 
 	let id: Int
 	let date: String
 	let locationId: Int
+
+	let duration: Int
+	let circumstances: String
 
 }
