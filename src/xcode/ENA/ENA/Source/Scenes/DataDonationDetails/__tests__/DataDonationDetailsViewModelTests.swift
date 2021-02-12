bf54ce737267fff5977144b98eebd2e0247625ac
Andreas Vogel <andreas.vogel@sap.com>
2021-02-12 15:10:21 +0100

    refactoring unit test

--- src/xcode/ENA/ENA/Source/Scenes/DataDonationDetails/__tests__/DataDonationDetailsViewModelTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/DataDonationDetails/__tests__/DataDonationDetailsViewModelTests.swift
@@ -25,10 +25,15 @@ class DataDonationDetailsViewModelTests: XCTestCase {
 		let numberOfCells = tableViewModel.section(0).cells.count
 		// THEN
 		XCTAssertEqual(numberOfCells, 36)
-		
+	}
+
+	func testReuseIdentifierCell2() throws {
+		// GIVEN
+		let tableViewModel = model.dynamicTableViewModel
+		// WHEN
 		let cell2 = tableViewModel.section(0).cells[2]
+		// THEN
 		XCTAssertEqual(cell2.cellReuseIdentifier.rawValue, "roundedCell")
-		
 	}
 
 	func testReuseIdentifierCell34() throws {
@@ -38,7 +43,6 @@ class DataDonationDetailsViewModelTests: XCTestCase {
 		let cell34 = tableViewModel.section(0).cells[34]
 		// THEN
 		XCTAssertEqual(cell34.cellReuseIdentifier.rawValue, "bulletPointCell")
-		
 	}
 
 	func testReuseIdentifierCell35() throws {
@@ -48,7 +52,6 @@ class DataDonationDetailsViewModelTests: XCTestCase {
 		let cell35 = tableViewModel.section(0).cells[35]
 		// THEN
 		XCTAssertEqual(cell35.cellReuseIdentifier.rawValue, "labelCell")
-		
 	}
 
 }
