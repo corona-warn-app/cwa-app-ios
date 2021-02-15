c64e3d8e9e07d58f131063d195af152a5c9e5f78
Marcus Scherer <marcus@skunkbrothers.de>
2021-02-15 19:16:38 +0100

    Add missing commas, its late…

--- scripts/switch_to_dev_env.sh
+++ scripts/switch_to_dev_env.sh
@@ -10,21 +10,21 @@ serverEnvironments="{
 			\"name\": \"Default\",
 			\"distributionURL\": \"${WRUXD_SECRET_DIST_URL}\",
 			\"submissionURL\": \"${WRUXD_SECRET_SUBM_URL}\",
-			\"verificationURL\": \"${WRUXD_SECRET_VERIF_URL}\"
+			\"verificationURL\": \"${WRUXD_SECRET_VERIF_URL}\",
 			\"dataDonationURL\": \"${WRUXD_SECRET_DATAD_URL}\"
 		},
 		{
 			\"name\": \"WRU-XA\",
 			\"distributionURL\": \"${WRUXA_SECRET_DIST_URL}\",
 			\"submissionURL\": \"${WRUXA_SECRET_SUBM_URL}\",
-			\"verificationURL\": \"${WRUXA_SECRET_VERIF_URL}\"
+			\"verificationURL\": \"${WRUXA_SECRET_VERIF_URL}\",
 			\"dataDonationURL\": \"${WRUXA_SECRET_DATAD_URL}\"
 		},
 		{
 			\"name\": \"WRU\",
 			\"distributionURL\": \"${SECRET_DIST_URL}\",
 			\"submissionURL\": \"${SECRET_SUBM_URL}\",
-			\"verificationURL\": \"${SECRET_VERIF_URL}\"
+			\"verificationURL\": \"${SECRET_VERIF_URL}\",
 			\"dataDonationURL\": \"${SECRET_DATAD_URL}\"
 		}
 	]
