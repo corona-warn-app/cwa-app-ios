17ae251ebfd780b50e61c56c996c6ebf2e63448b
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-02-17 17:39:11 +0100

    Revert "refactoring"
    This reverts commit ffa848a3fae6ab227185f8885e476b921264a695.

--- src/xcode/ENA/ENA/Source/Services/Exposure Submission/ExposureSubmission+TestResult+Extenstion.swift
+++ src/xcode/ENA/ENA/Source/Services/Exposure Submission/ExposureSubmission+TestResult+Extenstion.swift
@@ -4,4 +4,16 @@
 
 import Foundation
 extension TestResult {
+	var protobuf: SAP_Internal_Ppdd_PPATestResult? {
+		switch self {
+		case .pending:
+			return .testResultPending
+		case .negative:
+			return .testResultNegative
+		case .positive:
+			return .testResultPositive
+		case .expired, .invalid:
+			return nil
+		}
+	}
 }
