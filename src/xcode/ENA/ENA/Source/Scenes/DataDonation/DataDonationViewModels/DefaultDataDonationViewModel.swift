8cd7083556e50535f5dc040b51c93bbf08a55a1c
Gahn, Karsten <karsten.gahn@sap.com>
2021-02-17 09:40:04 +0100

    Removed key visual from data donation view

--- src/xcode/ENA/ENA/Source/Scenes/DataDonation/DataDonationViewModels/DefaultDataDonationViewModel.swift
+++ src/xcode/ENA/ENA/Source/Scenes/DataDonation/DataDonationViewModels/DefaultDataDonationViewModel.swift
@@ -16,12 +16,6 @@ final class DefaultDataDonationViewModel: BaseDataDonationViewModel {
 		var dynamicTableViewModel = DynamicTableViewModel.with {
 			$0.add(
 				.section(
-					header: .image(
-						UIImage(named: "Illu_DataDonation"),
-						accessibilityLabel: AppStrings.DataDonation.Info.accImageDescription,
-						accessibilityIdentifier: AccessibilityIdentifiers.DataDonation.accImageDescription,
-						height: 250
-					),
 					cells: [
 						.title1(
 							text: AppStrings.DataDonation.Info.title,
