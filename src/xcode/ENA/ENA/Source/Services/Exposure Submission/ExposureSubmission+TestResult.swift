ffa848a3fae6ab227185f8885e476b921264a695
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-02-17 17:22:40 +0100

    refactoring

--- src/xcode/ENA/ENA/Source/Services/Exposure Submission/ExposureSubmission+TestResult.swift
+++ src/xcode/ENA/ENA/Source/Services/Exposure Submission/ExposureSubmission+TestResult.swift
@@ -10,4 +10,19 @@ enum TestResult: Int, CaseIterable, Codable {
 	case positive = 2
 	case invalid = 3
 	case expired = 4
+	
+	#if !canImport(XCTest) // only for the 'real' app, not the tests
+	var protobuf: SAP_Internal_Ppdd_PPATestResult? {
+		switch self {
+		case .pending:
+			return .testResultPending
+		case .negative:
+			return .testResultNegative
+		case .positive:
+			return .testResultPositive
+		case .expired, .invalid:
+			return nil
+		}
+	}
+	#endif
 }
