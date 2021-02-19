aa59df20fe0a95410735bc942f928e8ddb7f005c
Kai Teuber <Kai.Teuber@app-developer.club>
2021-02-19 13:46:41 +0100

    text label layout fixed

--- src/xcode/ENA/ENA/Source/Scenes/DynamicTableViewController/Views/DynamicTypeTableViewCell.swift
+++ src/xcode/ENA/ENA/Source/Scenes/DynamicTableViewController/Views/DynamicTypeTableViewCell.swift
@@ -6,9 +6,14 @@ import Foundation
 import UIKit
 
 class DynamicTypeTableViewCell: UITableViewCell, DynamicTableViewTextCell {
+
+	// MARK: - Init
+
 	required init?(coder: NSCoder) {
 		super.init(coder: coder)
 	}
+
+	// MARK: - Overrides
 
 	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
 		super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -20,31 +25,58 @@ class DynamicTypeTableViewCell: UITableViewCell, DynamicTableViewTextCell {
 		setup()
 	}
 
-	private func setup() {
-		selectionStyle = .none
-
-		backgroundColor = .enaColor(for: .background)
-
-		if let label = textLabel {
-			label.translatesAutoresizingMaskIntoConstraints = false
-			label.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor).isActive = true
-			label.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor).isActive = true
-			label.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor).isActive = true
-			label.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor).isActive = true
-		}
-
-		resetMargins()
-		configureDynamicType()
-		configure(text: "", color: .enaColor(for: .textPrimary1))
-	}
-
 	override func prepareForReuse() {
 		super.prepareForReuse()
 
 		accessoryType = .none
 		accessoryView = nil
 		selectionStyle = .none
-		
+
+		resetMargins()
+		configureDynamicType()
+		configure(text: "", color: .enaColor(for: .textPrimary1))
+	}
+
+	// MARK: - Internal
+
+	func configureDynamicType(size: CGFloat = 17, weight: UIFont.Weight = .regular, style: UIFont.TextStyle = .body) {
+		contentTextLabel.font = UIFont.preferredFont(forTextStyle: style).scaledFont(size: size, weight: weight)
+		contentTextLabel.adjustsFontForContentSizeCategory = true
+		contentTextLabel.numberOfLines = 0
+	}
+
+	func configure(text: String, color: UIColor? = nil) {
+		contentTextLabel.text = text
+		contentTextLabel.textColor = color ?? .enaColor(for: .textPrimary1)
+	}
+
+	func configureAccessibility(label: String? = nil, identifier: String? = nil, traits: UIAccessibilityTraits = .staticText) {
+		contentTextLabel.accessibilityLabel = label
+		accessibilityIdentifier = identifier
+		accessibilityTraits = traits
+	}
+
+	// MARK: - Private
+
+	/// better use a self create label instead if textLabel from the default UITableViewCell -> that leads to some strange layout issues
+	private var contentTextLabel = UILabel()
+
+	private func setup() {
+		selectionStyle = .none
+
+		backgroundColor = .enaColor(for: .background)
+
+		contentTextLabel.translatesAutoresizingMaskIntoConstraints = false
+
+		contentView.addSubview(contentTextLabel)
+
+		NSLayoutConstraint.activate([
+			contentView.layoutMarginsGuide.topAnchor.constraint(equalTo: contentTextLabel.topAnchor),
+			contentView.layoutMarginsGuide.bottomAnchor.constraint(equalTo: contentTextLabel.bottomAnchor),
+			contentView.layoutMarginsGuide.leadingAnchor.constraint(equalTo: contentTextLabel.leadingAnchor),
+			contentView.layoutMarginsGuide.trailingAnchor.constraint(equalTo: contentTextLabel.trailingAnchor)
+		])
+
 		resetMargins()
 		configureDynamicType()
 		configure(text: "", color: .enaColor(for: .textPrimary1))
@@ -53,22 +85,5 @@ class DynamicTypeTableViewCell: UITableViewCell, DynamicTableViewTextCell {
 	private func resetMargins() {
 		contentView.layoutMargins = UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 16)
 		contentView.insetsLayoutMarginsFromSafeArea = false
-	}
-
-	func configureDynamicType(size: CGFloat = 17, weight: UIFont.Weight = .regular, style: UIFont.TextStyle = .body) {
-		textLabel?.font = UIFont.preferredFont(forTextStyle: style).scaledFont(size: size, weight: weight)
-		textLabel?.adjustsFontForContentSizeCategory = true
-		textLabel?.numberOfLines = 0
-	}
-
-	func configure(text: String, color: UIColor? = nil) {
-		textLabel?.text = text
-		textLabel?.textColor = color ?? .enaColor(for: .textPrimary1)
-	}
-
-	func configureAccessibility(label: String? = nil, identifier: String? = nil, traits: UIAccessibilityTraits = .staticText) {
-		textLabel?.accessibilityLabel = label
-		accessibilityIdentifier = identifier
-		accessibilityTraits = traits
 	}
 }
