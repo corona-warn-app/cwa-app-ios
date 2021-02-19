bec60cc6384c9d012b05c77270f003e1b0ff12b1
Nick Guendling <hello@nickguendling.com>
2021-02-19 14:42:41 +0100

    Add attribute configuration ui elements

--- src/xcode/ENA/ENA/Source/Scenes/ContactDiary/Day/Cells/DiaryDayEntryTableViewCell.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ContactDiary/Day/Cells/DiaryDayEntryTableViewCell.swift
@@ -35,6 +35,83 @@ class DiaryDayEntryTableViewCell: UITableViewCell {
 	@IBOutlet private weak var parametersContainerStackView: UIStackView!
 	@IBOutlet private weak var parametersStackView: UIStackView!
 
+	lazy var durationSegmentedControl: DiarySegmentedControl = {
+		let segmentedControl = DiarySegmentedControl()
+		segmentedControl.insertSegment(withTitle: AppStrings.ContactDiary.Day.Encounter.lessThan15Minutes, at: 0, animated: false)
+		segmentedControl.insertSegment(withTitle: AppStrings.ContactDiary.Day.Encounter.lessThan15Minutes, at: 1, animated: false)
+
+		return segmentedControl
+	}()
+
+	lazy var maskSituationSegmentedControl: DiarySegmentedControl = {
+		let segmentedControl = DiarySegmentedControl()
+		segmentedControl.insertSegment(withTitle: AppStrings.ContactDiary.Day.Encounter.withMask, at: 0, animated: false)
+		segmentedControl.insertSegment(withTitle: AppStrings.ContactDiary.Day.Encounter.withoutMask, at: 1, animated: false)
+
+		return segmentedControl
+	}()
+
+	lazy var settingSegmentedControl: DiarySegmentedControl = {
+		let segmentedControl = DiarySegmentedControl()
+		segmentedControl.insertSegment(withTitle: AppStrings.ContactDiary.Day.Encounter.outside, at: 0, animated: false)
+		segmentedControl.insertSegment(withTitle: AppStrings.ContactDiary.Day.Encounter.inside, at: 1, animated: false)
+
+		return segmentedControl
+	}()
+
+	lazy var notesTextField: DiaryEntryTextField = {
+		let textField = DiaryEntryTextField(frame: .zero)
+		textField.backgroundColor = .enaColor(for: .darkBackground)
+		textField.clearButtonMode = .whileEditing
+		textField.textColor = .enaColor(for: .textPrimary1)
+		textField.returnKeyType = .done
+
+		textField.heightAnchor.constraint(greaterThanOrEqualToConstant: 40.0).isActive = true
+
+		return textField
+	}()
+
+	lazy var notesInfoButton: UIButton = {
+		let button = UIButton(type: .infoLight)
+		button.tintColor = .enaColor(for: .tint)
+
+		return button
+	}()
+
+	lazy var notesStackView: UIStackView = {
+		let stackView = UIStackView()
+		stackView.axis = .horizontal
+		stackView.spacing = 8
+
+		stackView.addArrangedSubview(notesTextField)
+		stackView.addArrangedSubview(notesInfoButton)
+
+		return stackView
+	}()
+
+	lazy var visitDurationStackView: UIStackView = {
+		let stackView = UIStackView()
+		stackView.axis = .horizontal
+		stackView.spacing = 8
+
+		let label = ENALabel()
+		label.text = AppStrings.ContactDiary.Day.Visit.duration
+
+		let durationPicker = UIDatePicker()
+		durationPicker.datePickerMode = .time
+		durationPicker.minuteInterval = 15
+		if #available(iOS 14.0, *) {
+			durationPicker.preferredDatePickerStyle = .inline
+		} else {
+			// Fallback on earlier versions
+		}
+
+		stackView.addArrangedSubview(label)
+		stackView.addArrangedSubview(durationPicker)
+
+		return stackView
+	}()
+
 	@objc
 	private func headerTapped() {
 		cellModel.toggleSelection()
@@ -45,14 +122,18 @@ class DiaryDayEntryTableViewCell: UITableViewCell {
 
 		switch entryType {
 		case .contactPerson:
-			let segmentedControl = DiarySegmentedControl()
-			segmentedControl.insertSegment(withTitle: "Test", at: 0, animated: false)
-			segmentedControl.insertSegment(withTitle: "2345", at: 1, animated: false)
+			parametersStackView.addArrangedSubview(durationSegmentedControl)
+			parametersStackView.addArrangedSubview(maskSituationSegmentedControl)
+			parametersStackView.addArrangedSubview(settingSegmentedControl)
 
-			parametersStackView.addArrangedSubview(segmentedControl)
+			notesTextField.placeholder = AppStrings.ContactDiary.Day.Encounter.notesPlaceholder
 		case .location:
-			break
+			parametersStackView.addArrangedSubview(visitDurationStackView)
+
+			notesTextField.placeholder = AppStrings.ContactDiary.Day.Visit.notesPlaceholder
 		}
+
+		parametersStackView.addArrangedSubview(notesStackView)
 	}
 
 }
