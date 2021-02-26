3cf8c0d244a7518108ff0464a7300fe6306325eb
Kai Teuber <Kai.Teuber@app-developer.club>
2021-02-26 17:58:42 +0100

    adde some dummy qr code open button

--- src/xcode/ENA/ENA/Source/Scenes/Events/EventsCoordinator.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Events/EventsCoordinator.swift
@@ -23,9 +23,23 @@ final class EventsCoordinator {
 
 	lazy var viewController: UINavigationController = {
 		let eventsListViewController = UITableViewController()
+
+		let qrCodeButton: UIBarButtonItem
+		if #available(iOS 13.0, *) {
+			qrCodeButton = UIBarButtonItem(image: UIImage(systemName: "qrcode.viewfinder"), style: .done, target: self, action: #selector(showQrCodeScanner))
+		} else {
+			qrCodeButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(showQrCodeScanner))
+		}
+		eventsListViewController.navigationItem.rightBarButtonItem = qrCodeButton
+
 		return UINavigationController(rootViewController: eventsListViewController)
 	}()
 
 	// MARK: - Private
+
+	@objc
+	private func showQrCodeScanner() {
+		Log.debug("Show QR Code scanner")
+	}
 
 }
