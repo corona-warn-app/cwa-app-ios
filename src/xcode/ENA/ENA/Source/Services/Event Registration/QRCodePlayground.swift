fe77c45e90a3dde1dc8cdae39963b9de03391050
Marcus Scherer <marcus@skunkbrothers.de>
2021-03-02 18:35:21 +0100

    Clean up QRCode generation

--- src/xcode/ENA/ENA/Source/Services/Event Registration/QRCodePlayground.swift
+++ src/xcode/ENA/ENA/Source/Services/Event Registration/QRCodePlayground.swift
@@ -7,36 +7,34 @@ import Base32
 
 enum QRCodePlayground {
 	
-	static func generateQRCode(with string: String) -> UIImage? {
-//		guard let cgImage = EFQRCode.generate(content: string, contentEncoding: .isoLatin1, size: .init(width: 400, height: 400), inputCorrectionLevel: .h) else {
-//			return nil
-//		}
-//		return UIImage(cgImage: cgImage)
-//		let smollData = String(string, radix: 8)
-//		let data = string.data(using: .isoLatin1)
-//		let encoded = String(1000, radix: 16)
-//		print(encoded)
-		let targetSize = CGSize(width: 400, height: 400)
-		
+	static func generateQRCode(with string: String, size: CGSize = CGSize(width: 400, height: 400)) -> UIImage? {
+
+		/// Create data from string which will be feed into the CoreImage Filter
 		guard let data = string.data(using: .shiftJIS) else {
 			return nil
 		}
-		let filter = CIFilter(name: "CIQRCodeGenerator")
-		filter?.setValue(data, forKey: "inputMessage")
-		filter?.setValue("H", forKey: "inputCorrectionLevel")
+		
+		/// Create CoreImage Filter to create QR-Code
+		guard let filter = CIFilter(name: "CIQRCodeGenerator") else {
+			return nil
+		}
+		filter.setValue(data, forKey: "inputMessage") /// Feed data into Filter
+		filter.setValue("H", forKey: "inputCorrectionLevel") /// Set ErrorCorrectionLevel
 
-		guard let image = filter?.outputImage else {
+		guard let image = filter.outputImage else {
 			return nil
 		}
 		
+		/// Depending on the length of the string the QRCode may vary in size. But we want an Image with a fixed size. This requires us to scale the QRCode to our desired image size.
 		
+		/// Calculate scaling factors
+		let scaleX = size.width / image.extent.size.width
+		let scaleY = size.height / image.extent.size.height
 		
-		let scaleX = targetSize.width / image.extent.size.width
-		let scaleY = targetSize.height / image.extent.size.height
-		
+		/// Scale image
 		let transformedImage = image.transformed(by: CGAffineTransform(scaleX: scaleX, y: scaleY))
 		
-		
+		/// Return scaled image
 		return UIImage(ciImage: transformedImage)
 	
 	}
