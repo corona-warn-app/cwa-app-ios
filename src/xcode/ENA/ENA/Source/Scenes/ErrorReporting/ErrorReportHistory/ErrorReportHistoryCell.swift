a244febc85ca223369d348526095d1b87379efb1
Naveed Khalid <nkhalid.here@gmail.com>
2021-03-03 23:30:58 +0100

    changes in history cell

--- src/xcode/ENA/ENA/Source/Scenes/ErrorReporting/ErrorReportHistory/ErrorReportHistoryCell.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ErrorReporting/ErrorReportHistory/ErrorReportHistoryCell.swift
@@ -6,7 +6,24 @@ import Foundation
 import UIKit
 
 class ErrorReportHistoryCell: UITableViewCell {
+		
+	@IBOutlet private var dateTimeLabel: ENALabel!
+	@IBOutlet private var idLabel: ENALabel!
 	
-	@IBOutlet var dateTimeLabel: UILabel?
-	@IBOutlet var idLabel: UILabel?
+	func configure(
+		dateTimeLabel: NSMutableAttributedString,
+		idLabel: NSMutableAttributedString
+		
+	) {
+		self.dateTimeLabel.attributedText = dateTimeLabel
+		self.idLabel.attributedText = idLabel
+	}
+	
+	override func prepareForInterfaceBuilder() {
+		super.prepareForInterfaceBuilder()
+	}
+
+	override func awakeFromNib() {
+		super.awakeFromNib()
+	}
 }
