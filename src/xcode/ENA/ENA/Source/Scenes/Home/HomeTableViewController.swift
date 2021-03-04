2f37a7e8552b634fbd6507846d518ba017a9f9bf
Artur Friesen <mail@arturfriesen.de>
2021-03-04 13:41:02 +0100

    Dispatch model change on main thread.

--- src/xcode/ENA/ENA/Source/Scenes/Home/HomeTableViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Home/HomeTableViewController.swift
@@ -691,8 +691,10 @@ class HomeTableViewController: UITableViewController, NavigationBarOpacityDelega
 	
 	@objc
 	private func refreshUI() {
-		viewModel.state.updateTestResult()
-		viewModel.state.updateStatistics()
+		DispatchQueue.main.async { [weak self] in
+			self?.viewModel.state.updateTestResult()
+			self?.viewModel.state.updateStatistics()
+		}
 	}
 
 	// swiftlint:disable:next file_length
