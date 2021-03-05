1e88b06f8b1d39141ee62b9d6315696589225dda
Artur Friesen <mail@arturfriesen.de>
2021-03-05 15:50:50 +0100

    Add store dummy.

--- src/xcode/ENA/ENA/Source/Scenes/Events/Store/EventStore.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Events/Store/EventStore.swift
@@ -2,4 +2,58 @@
 // 🦠 Corona-Warn-App
 //
 
-import Foundation
+import OpenCombine
+
+class EventStore: EventStoring, EventProviding {
+
+	// MARK: - Protocol EventStoring
+
+	func createEvent(
+		id: String,
+		description: String,
+		address: String,
+		start: Date,
+		end: Date,
+		defaultCheckInLengthInMinutes: Int,
+		signature: String
+	) -> EventStoring.VoidResult {
+
+		return .success(())
+	}
+
+	func deleteEvent(id: String) -> EventStoring.VoidResult {
+
+		return .success(())
+	}
+
+	// swiftlint:disable function_parameter_count
+	func createCheckin(
+		eventId: String,
+		eventType: Int,
+		eventDescription: String,
+		eventAddress: String,
+		eventStart: Date,
+		eventEnd: Date,
+		eventSignature: String,
+		checkinStart: Date,
+		checkinEnd: Date) -> EventStoring.IdResult {
+
+		return .success((checkingPublisher.value.count - 1))
+	}
+
+	func deleteCheckin(id: Int) -> EventStoring.VoidResult {
+
+		return .success(())
+	}
+
+	func updateCheckin(id: Int, end: Date) -> EventStoring.VoidResult {
+
+		return .success(())
+	}
+
+	// MARK: - Protocol EventProviding
+
+	var eventsPublisher = CurrentValueSubject<[Event], Never>([])
+
+	var checkingPublisher = CurrentValueSubject<[Checkin], Never>([])
+}
