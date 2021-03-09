040844d417d022546acf4a0baa4bad4d3f45fd1c
Marcus Scherer <marcus@skunkbrothers.de>
2021-03-09 17:41:16 +0100

    “Test” PDF

---
+++ src/xcode/ENA/ENA/Source/Extensions/__tests__/PDFDocument+EmbedImageTests.swift
@@ -0,0 +1,27 @@
+////
+// 🦠 Corona-Warn-App
+//
+
+import XCTest
+import PDFKit
+@testable import ENA
+
+class PDFDocument_EmbedImageTests: XCTestCase {
+	
+	// swiftlint:disable force_unwrapping
+	/// Not proud of this test, if you have a nicer idea, please go ahead.
+    func testEmbedingImage() throws {
+
+		let testBundle = Bundle(for: type(of: self))
+		
+		let documentURL = testBundle.url(forResource: "qr-code-print-template", withExtension: "pdf")!
+			
+		let pdfDocument = PDFDocument(url: documentURL)!
+				
+		let image = UIImage(contentsOfFile: testBundle.path(forResource: "qr-code-to-embed", ofType: "png")!)
+		
+		pdfDocument.embed(image: image!, at: CGPoint(x: 100, y: 100))
+    }
+	// swiftlint:enable force_unwrapping
+
+}
