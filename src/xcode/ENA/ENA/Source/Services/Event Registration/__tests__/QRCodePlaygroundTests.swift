d201f7e9e78f95eec9fd5ba0908785a0b5402c42
Marcus Scherer <marcus@skunkbrothers.de>
2021-03-10 10:42:17 +0100

    Improve QRCode Unittests

--- src/xcode/ENA/ENA/Source/Services/Event Registration/__tests__/QRCodePlaygroundTests.swift
+++ src/xcode/ENA/ENA/Source/Services/Event Registration/__tests__/QRCodePlaygroundTests.swift
@@ -6,20 +6,50 @@ import XCTest
 @testable import ENA
 
 class QRCodePlaygroundTests: XCTestCase {
-
-    func testQRCodeGeneration() throws {
-		let qrCodeImage = try XCTUnwrap(QRCodePlayground.generateQRCode(with: "HTTPS://CORONAWARN.APP/E1/BIPEY33SMVWSA2LQON2W2IDEN5WG64RAONUXIIDBNVSXILBAMNXRBCM4UQARRKM6UQASAHRKCC7CTDWGQ4JCO7RVZSWVIMQK4UPA.GBCAEIA7TEORBTUA25QHBOCWT26BCA5PORBS2E4FFWMJ3UU3P6SXOL7SHUBCA7UEZBDDQ2R6VRJH7WBJKVF7GZYJA6YMRN27IPEP7NKGGJSWX3XQ"))
+	
+	func testQRCodeGeneration() throws {
+		let stringToEncode = "HTTPS://CORONAWARN.APP/E1/BIPEY33SMVWSA2LQON2W2IDEN5WG64RAONUXIIDBNVSXILBAMNXRBCM4UQARRKM6UQASAHRKCC7CTDWGQ4JCO7RVZSWVIMQK4UPA.GBCAEIA7TEORBTUA25QHBOCWT26BCA5PORBS2E4FFWMJ3UU3P6SXOL7SHUBCA7UEZBDDQ2R6VRJH7WBJKVF7GZYJA6YMRN27IPEP7NKGGJSWX3XQ"
+		
+		let qrCodeImage = try XCTUnwrap(QRCodePlayground.generateQRCode(with: stringToEncode))
+		
+		let decodedString = extractFirstMessageFrom(qrCode: qrCodeImage)
 		
 		XCTAssertEqual(qrCodeImage.size.height, qrCodeImage.size.width)
 		XCTAssertEqual(qrCodeImage.size.height, 400)
-    }
+		XCTAssertEqual(stringToEncode, decodedString)
+	}
 	
 	
 	func testQRCodeGenerationWithCustomSize() throws {
-		let qrCodeImage = try XCTUnwrap(QRCodePlayground.generateQRCode(with: "HTTPS://CORONAWARN.APP/E1/BIPEY33SMVWSA2LQON2W2IDEN5WG64RAONUXIIDBNVSXILBAMNXRBCM4UQARRKM6UQASAHRKCC7CTDWGQ4JCO7RVZSWVIMQK4UPA.GBCAEIA7TEORBTUA25QHBOCWT26BCA5PORBS2E4FFWMJ3UU3P6SXOL7SHUBCA7UEZBDDQ2R6VRJH7WBJKVF7GZYJA6YMRN27IPEP7NKGGJSWX3XQ", size: CGSize(width: 1000, height: 1000)))
 		
+		let stringToEncode = "HTTPS://CORONAWARN.APP/E1/BIPEY33SMVWSA2LQON2W2IDEN5WG64RAONUXIIDBNVSXILBAMNXRBCM4UQARRKM6UQASAHRKCC7CTDWGQ4JCO7RVZSWVIMQK4UPA.GBCAEIA7TEORBTUA25QHBOCWT26BCA5PORBS2E4FFWMJ3UU3P6SXOL7SHUBCA7UEZBDDQ2R6VRJH7WBJKVF7GZYJA6YMRN27IPEP7NKGGJSWX3XQ"
+
+		let qrCodeImage = try XCTUnwrap(QRCodePlayground.generateQRCode(with: stringToEncode, size: CGSize(width: 1000, height: 1000)))
+		
+		let decodedString = extractFirstMessageFrom(qrCode: qrCodeImage)
+
 		XCTAssertEqual(qrCodeImage.size.height, qrCodeImage.size.width)
 		XCTAssertEqual(qrCodeImage.size.height, 1000)
+		XCTAssertEqual(stringToEncode, decodedString)
 	}
-
+	
+	
+	func extractFirstMessageFrom(qrCode: UIImage) -> String? {
+		if let ciImage = qrCode.ciImage {
+			var options: [String: Any]
+			let context = CIContext()
+			options = [CIDetectorAccuracy: CIDetectorAccuracyHigh]
+			let qrDetector = CIDetector(ofType: CIDetectorTypeQRCode, context: context, options: options)
+			if ciImage.properties.keys.contains((kCGImagePropertyOrientation as String)) {
+				options = [CIDetectorImageOrientation: ciImage.properties[(kCGImagePropertyOrientation as String)] ?? 1]
+			} else {
+				options = [CIDetectorImageOrientation: 1]
+			}
+			let features = qrDetector?.features(in: ciImage, options: options)
+			let firstFeature = features?.first as? CIQRCodeFeature
+			return firstFeature?.messageString
+		}
+		return nil
+	}
+	
 }
