c745ee8991c85878729d58b41b66219574830a8b
Pascal Brause <pascal@brause.haus>
2021-03-11 11:39:47 +0100

    change for review suggestion

--- src/xcode/ENA/ENA/Source/Workers/Logging/Logging.swift
+++ src/xcode/ENA/ENA/Source/Workers/Logging/Logging.swift
@@ -29,10 +29,10 @@ extension OSLog {
 	static let survey = OSLog(subsystem: subsystem, category: "survey")
 	/// PP Analytics
 	static let ppa = OSLog(subsystem: subsystem, category: "ppa")
-	/// Event registration - checkin / attendees
-	static let evregCheckin = OSLog(subsystem: subsystem, category: "evregCheckin")
-	/// Event registration - organizer / client
-	static let evregOrganizer = OSLog(subsystem: subsystem, category: "evregOrganizer")
+	/// Event / Location Checkin
+	static let checkin = OSLog(subsystem: subsystem, category: "checkin")
+	/// Event / Location Organizer
+	static let traceLocation = OSLog(subsystem: subsystem, category: "traceLocation")
 
 }
 
