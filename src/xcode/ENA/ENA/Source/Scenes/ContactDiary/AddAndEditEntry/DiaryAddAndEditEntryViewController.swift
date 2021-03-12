acacbfd18373d6709851a61d641c038907bdde1b
Bastian Kohlbauer <bastian.kohlbauer@googelmail.com>
2021-03-12 09:15:44 +0100

    switch to using uitableview to fix resetting of ui and keyboard issues

--- src/xcode/ENA/ENA/Source/Scenes/ContactDiary/AddAndEditEntry/DiaryAddAndEditEntryViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ContactDiary/AddAndEditEntry/DiaryAddAndEditEntryViewController.swift
@@ -5,20 +5,18 @@
 import UIKit
 import OpenCombine
 
-class DiaryAddAndEditEntryViewController: UIViewController, UITextFieldDelegate, ENANavigationControllerWithFooterChild, DismissHandling {
+class DiaryAddAndEditEntryViewController: UITableViewController, UITextFieldDelegate, ENANavigationControllerWithFooterChild, DismissHandling {
 
 	// MARK: - Init
 
 	init(
-		textFieldsManager: TextFieldsManager = TextFieldsManager(),
 		viewModel: DiaryAddAndEditEntryViewModel,
 		dismiss: @escaping () -> Void
 	) {
 		self.viewModel = viewModel
-		self.textFieldsManager = textFieldsManager
 		self.dismiss = dismiss
 
-		super.init(nibName: nil, bundle: nil)
+		super.init(style: .plain)
 	}
 
 	@available(*, unavailable)
@@ -44,39 +42,69 @@ class DiaryAddAndEditEntryViewController: UIViewController, UITextFieldDelegate,
 
 		navigationController?.navigationBar.prefersLargeTitles = true
 		navigationController?.navigationBar.sizeToFit()
-		
-		NotificationCenter.default.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
-		NotificationCenter.default.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
-	}
-
-	override func viewDidAppear(_ animated: Bool) {
-		super.viewDidAppear(animated)
-
-		DispatchQueue.main.async { [weak self] in
-			self?.textFieldsManager.nextFirstResponder()
-		}
-	}
-
-	override func viewWillDisappear(_ animated: Bool) {
-		super.viewWillDisappear(animated)
-		textFieldsManager.resignFirstResponder()
-		
-		NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
-		NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
 	}
 
 	override var navigationItem: UINavigationItem {
 		navigationFooterItem
 	}
+	
+	override func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return Row.allCases.count
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		guard let cell = tableView.dequeueReusableCell(withIdentifier: TextFieldCell.identifier, for: indexPath) as? TextFieldCell else {
+			fatalError("No registered cell found")
+		}
+		guard let row = Row(rawValue: indexPath.row) else {
+			fatalError("Something went horribly wrong")
+		}
+		cell.textField.tag = row.rawValue
+		cell.textField.addTarget(self, action: #selector(textValueChanged(sender:)), for: .editingChanged)
+		cell.textField.delegate = self
+		switch row {
+		case .name:
+			cell.textField.accessibilityIdentifier = AccessibilityIdentifiers.ContactDiaryInformation.EditEntries.nameTextField
+			cell.textField.placeholder = viewModel.namePlaceholder
+			cell.textField.autocapitalizationType = .sentences
+			cell.textField.keyboardType = .default
+			cell.textField.returnKeyType = .continue
+			cell.textField.text = viewModel.entryModel.name
+		case .phoneNumber:
+			cell.textField.accessibilityIdentifier = AccessibilityIdentifiers.ContactDiaryInformation.EditEntries.phoneNumberTextField
+			cell.textField.placeholder = viewModel.phoneNumberPlaceholder
+			cell.textField.autocapitalizationType = .none
+			cell.textField.keyboardType = .phonePad
+			cell.textField.returnKeyType = .continue
+			cell.textField.text = viewModel.entryModel.phoneNumber
+		case .email:
+			cell.textField.accessibilityIdentifier = AccessibilityIdentifiers.ContactDiaryInformation.EditEntries.eMailTextField
+			cell.textField.placeholder = viewModel.emailAddressPlaceholder
+			cell.textField.autocapitalizationType = .none
+			cell.textField.keyboardType = .emailAddress
+			cell.textField.returnKeyType = .done
+			cell.textField.text = viewModel.entryModel.emailAddress
+		}
+		return cell
+	}
 
 	// MARK: - Protocol UITextFieldDelegate
 
 	func textFieldShouldClear(_ textField: UITextField) -> Bool {
-		guard let keyPath = textFieldsManager.keyPath(for: textField) else {
+		switch Row(rawValue: textField.tag) {
+		case .name:
+			viewModel.reset(keyPath: \DiaryAddAndEditEntryModel.name)
+		case .phoneNumber:
+			viewModel.reset(keyPath: \DiaryAddAndEditEntryModel.phoneNumber)
+		case .email:
+			viewModel.reset(keyPath: \DiaryAddAndEditEntryModel.emailAddress)
+		default:
 			Log.debug("Textfield to clear not found", log: .default)
-			return false
 		}
-		viewModel.reset(keyPath: keyPath)
 		return true
 	}
 
@@ -90,10 +118,10 @@ class DiaryAddAndEditEntryViewController: UIViewController, UITextFieldDelegate,
 			if !viewModel.entryModel.isEmpty {
 				viewModel.save()
 			}
-			textFieldsManager.resignFirstResponder()
+			textField.resignFirstResponder()
 			dismiss()
 		case .next, .continue:
-			textFieldsManager.nextFirstResponder()
+			self.textField(for: textField.tag + 1)?.becomeFirstResponder()
 		default:
 			Log.debug("unsupport return key type")
 		}
@@ -104,22 +132,24 @@ class DiaryAddAndEditEntryViewController: UIViewController, UITextFieldDelegate,
 
 	func navigationController(_ navigationController: ENANavigationControllerWithFooter, didTapPrimaryButton button: UIButton) {
 		viewModel.save()
-		textFieldsManager.resignFirstResponder()
 		dismiss()
 	}
 
 	// MARK: - DismissHandling
 
 	func wasAttemptedToBeDismissed() {
-		textFieldsManager.resignFirstResponder()
 		dismiss()
 	}
 
 	// MARK: - Private
 
-	private var scrollView: UIScrollView!
+	enum Row: Int, CaseIterable {
+		case name
+		case phoneNumber
+		case email
+	}
+	
 	private let viewModel: DiaryAddAndEditEntryViewModel
-	private let textFieldsManager: TextFieldsManager
 	private let dismiss: () -> Void
 	private var bindings: [AnyCancellable] = []
 
@@ -149,127 +179,75 @@ class DiaryAddAndEditEntryViewController: UIViewController, UITextFieldDelegate,
 	private func setupView() {
 		title = viewModel.title
 
-		scrollView = UIScrollView(frame: view.frame)
-		scrollView.translatesAutoresizingMaskIntoConstraints = false
-		view.addSubview(scrollView)
-
-		NSLayoutConstraint.activate([
-			view.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-			view.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-			view.topAnchor.constraint(equalTo: scrollView.topAnchor),
-			view.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor)
-		])
-
-		let nameTextField = DiaryEntryTextField(frame: .zero)
-		nameTextField.translatesAutoresizingMaskIntoConstraints = false
-		nameTextField.accessibilityIdentifier = AccessibilityIdentifiers.ContactDiaryInformation.EditEntries.nameTextField
-		nameTextField.isUserInteractionEnabled = true
-		nameTextField.clearButtonMode = .whileEditing
-		nameTextField.placeholder = viewModel.namePlaceholder
-		nameTextField.textColor = .enaColor(for: .textPrimary1)
-		nameTextField.autocorrectionType = .no
-		nameTextField.autocapitalizationType = .sentences
-		nameTextField.spellCheckingType = .no
-		nameTextField.smartQuotesType = .no
-		nameTextField.keyboardAppearance = .default
-		nameTextField.keyboardType = .default
-		nameTextField.returnKeyType = .continue
-		nameTextField.addTarget(self, action: #selector(textValueChanged(sender:)), for: .editingChanged)
-		nameTextField.delegate = self
-		nameTextField.text = viewModel.entryModel.name
-
-		let phoneNumberTextField = DiaryEntryTextField(frame: .zero)
-		phoneNumberTextField.translatesAutoresizingMaskIntoConstraints = false
-		phoneNumberTextField.accessibilityIdentifier = AccessibilityIdentifiers.ContactDiaryInformation.EditEntries.phoneNumberTextField
-		phoneNumberTextField.isUserInteractionEnabled = true
-		phoneNumberTextField.clearButtonMode = .whileEditing
-		phoneNumberTextField.placeholder = viewModel.phoneNumberPlaceholder
-		phoneNumberTextField.textColor = .enaColor(for: .textPrimary1)
-		phoneNumberTextField.autocorrectionType = .no
-		phoneNumberTextField.autocapitalizationType = .none
-		phoneNumberTextField.spellCheckingType = .no
-		phoneNumberTextField.smartQuotesType = .no
-		phoneNumberTextField.keyboardAppearance = .default
-		phoneNumberTextField.keyboardType = .phonePad
-		phoneNumberTextField.returnKeyType = .continue
-		phoneNumberTextField.addTarget(self, action: #selector(textValueChanged(sender:)), for: .editingChanged)
-		phoneNumberTextField.delegate = self
-		phoneNumberTextField.text = viewModel.entryModel.phoneNumber
-
-		let emailTextField = DiaryEntryTextField(frame: .zero)
-		emailTextField.translatesAutoresizingMaskIntoConstraints = false
-		emailTextField.accessibilityIdentifier = AccessibilityIdentifiers.ContactDiaryInformation.EditEntries.eMailTextField
-		emailTextField.isUserInteractionEnabled = true
-		emailTextField.clearButtonMode = .whileEditing
-		emailTextField.placeholder = viewModel.emailAddressPlaceholder
-		emailTextField.textColor = .enaColor(for: .textPrimary1)
-		emailTextField.autocorrectionType = .no
-		emailTextField.autocapitalizationType = .none
-		emailTextField.spellCheckingType = .no
-		emailTextField.smartQuotesType = .no
-		emailTextField.keyboardAppearance = .default
-		emailTextField.keyboardType = .emailAddress
-		emailTextField.returnKeyType = .done
-		emailTextField.addTarget(self, action: #selector(textValueChanged(sender:)), for: .editingChanged)
-		emailTextField.delegate = self
-		emailTextField.text = viewModel.entryModel.emailAddress
-
-		nameTextField.translatesAutoresizingMaskIntoConstraints = false
-		nameTextField.isUserInteractionEnabled = true
-
-		let stackView = UIStackView(arrangedSubviews: [nameTextField, phoneNumberTextField, emailTextField])
-		stackView.translatesAutoresizingMaskIntoConstraints = false
-		stackView.alignment = .fill
-		stackView.axis = .vertical
-		stackView.spacing = 8.0
-		scrollView.addSubview(stackView)
-
-		NSLayoutConstraint.activate([
-			stackView.safeAreaLayoutGuide.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16.0),
-			stackView.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16.0),
-			stackView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 39.0),
-			nameTextField.heightAnchor.constraint(greaterThanOrEqualToConstant: 40.0),
-			phoneNumberTextField.heightAnchor.constraint(greaterThanOrEqualToConstant: 40.0),
-			emailTextField.heightAnchor.constraint(greaterThanOrEqualToConstant: 40.0)
-		])
+		tableView.separatorStyle = .none
+		tableView.register(
+			TextFieldCell.self,
+			forCellReuseIdentifier: TextFieldCell.identifier)
 
 		footerView?.isHidden = false
-
-		// register textfields with the associated key path to manage keyboard & input
-		textFieldsManager.appendTextField(textfieldWithKayPath: (nameTextField, \DiaryAddAndEditEntryModel.name))
-		textFieldsManager.appendTextField(textfieldWithKayPath: (phoneNumberTextField, \DiaryAddAndEditEntryModel.phoneNumber))
-		textFieldsManager.appendTextField(textfieldWithKayPath: (emailTextField, \DiaryAddAndEditEntryModel.emailAddress))
 	}
 
 	@objc
 	private func textValueChanged(sender: UITextField) {
-		guard let entryModelKeyPath = textFieldsManager.keyPath(for: sender) else {
+		switch Row(rawValue: sender.tag) {
+		case .name:
+			viewModel.update(sender.text, keyPath: \DiaryAddAndEditEntryModel.name)
+		case .phoneNumber:
+			viewModel.update(sender.text, keyPath: \DiaryAddAndEditEntryModel.phoneNumber)
+		case .email:
+			viewModel.update(sender.text, keyPath: \DiaryAddAndEditEntryModel.emailAddress)
+		default:
 			Log.debug("Failed to find matching textfield", log: .default)
-			return
 		}
-		viewModel.update(sender.text, keyPath: entryModelKeyPath)
 	}
 	
-	@objc
-	private func adjustForKeyboard(notification: Notification) {
-		guard let keyboardFrameEndUserInfo = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
-			  let keyboardAnimationDurationUserInfo = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? NSNumber,
-			  let keyboardAnimationCurveUserInfo = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? NSNumber else { return }
-
-		let keyboardScreenEndFrame = keyboardFrameEndUserInfo.cgRectValue
-		let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
-		
-		let animationDuration = keyboardAnimationDurationUserInfo.doubleValue
-		let animationCurve = keyboardAnimationCurveUserInfo.uintValue
-		 
-		UIView.animate(withDuration: animationDuration, delay: 0, options: UIView.AnimationOptions(rawValue: animationCurve), animations: { [weak self] in
-			guard let self = self else { return }
-			if notification.name == UIResponder.keyboardWillHideNotification {
-				self.scrollView.contentInset = .zero
-			} else {
-				self.scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - self.view.safeAreaInsets.bottom + (self.footerView?.bounds.height ?? 0), right: 0)
-			}
-		}, completion: nil)
+	private func textField(for row: Int) -> UITextField? {
+		guard let cell = tableView.visibleCells.first(where: { ($0 as? TextFieldCell)?.textField.tag == row }) as? TextFieldCell else {
+			return nil
+		}
+		return cell.textField
 	}
+}
 
+private final class TextFieldCell: UITableViewCell {
+	
+	// MARK: - Init
+	
+	@available(*, unavailable)
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	// MARK: - Overrides
+	
+	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+		// self
+		selectionStyle = .none
+		// textField
+		textField = DiaryEntryTextField(frame: .zero)
+		textField.autocorrectionType = .no
+		textField.isUserInteractionEnabled = true
+		textField.clearButtonMode = .whileEditing
+		textField.spellCheckingType = .no
+		textField.smartQuotesType = .no
+		textField.keyboardAppearance = .default
+		textField.textColor = .enaColor(for: .textPrimary1)
+		textField.translatesAutoresizingMaskIntoConstraints = false
+		contentView.addSubview(textField)
+		// activate constraints
+		NSLayoutConstraint.activate([
+			textField.safeAreaLayoutGuide.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16.0),
+			textField.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16.0),
+			textField.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 4),
+			textField.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -4),
+			textField.heightAnchor.constraint(greaterThanOrEqualToConstant: 40.0)
+		])
+	}
+	
+	// MARK: - Internal
+	
+	static let identifier = "TextFieldCell"
+	
+	var textField: DiaryEntryTextField!
 }
