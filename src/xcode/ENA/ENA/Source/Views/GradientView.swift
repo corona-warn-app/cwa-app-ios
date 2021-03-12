a47f055d6d2cc56b1e6acf7570ba4d046e84bc8f
Nick Guendling <hello@nickguendling.com>
2021-03-12 08:53:27 +0100

    Make gradient view available from interface builder

--- src/xcode/ENA/ENA/Source/Views/GradientView.swift
+++ src/xcode/ENA/ENA/Source/Views/GradientView.swift
@@ -10,12 +10,14 @@ class GradientView: UIView {
 
 	override init(frame: CGRect) {
 		super.init(frame: frame)
+
 		setupLayer()
 	}
 
-	@available(*, unavailable)
 	required init?(coder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
+		super.init(coder: coder)
+
+		setupLayer()
 	}
 
 	// MARK: - Overrides
