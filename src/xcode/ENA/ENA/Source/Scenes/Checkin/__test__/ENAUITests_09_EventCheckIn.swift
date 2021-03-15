88a18b4024b175ec600d43609ae5b0e52981a597
Andreas Vogel <andreas.vogel@sap.com>
2021-03-15 20:09:05 +0100

    UITest for info screen

--- src/xcode/ENA/ENA/Source/Scenes/Checkin/__test__/ENAUITests_09_EventCheckIn.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Checkin/__test__/ENAUITests_09_EventCheckIn.swift
@@ -22,9 +22,16 @@ class ENAUITests_09_EventCheckIn: XCTestCase {
 		app.launchArguments.append(contentsOf: ["-checkinInfoScreenShown", "NO"])
 		app.launch()
 		
-		XCTAssertTrue(app.buttons[AccessibilityIdentifiers.Tabbar.checkin].waitForExistence(timeout: .medium))
+		XCTAssertTrue(app.buttons[AccessibilityIdentifiers.Tabbar.checkin].waitForExistence(timeout: .short))
+		
+		// Navigate to CheckIn
 		app.buttons[AccessibilityIdentifiers.Tabbar.checkin].tap()
 
+		XCTAssertTrue(app.cells[AccessibilityIdentifiers.CheckinInformation.acknowledgementTitle].exists)
+		XCTAssertTrue(app.cells[AccessibilityIdentifiers.CheckinInformation.dataPrivacyTitle].exists)
+		XCTAssertTrue(app.buttons[AccessibilityIdentifiers.CheckinInformation.primaryButton].exists)
+		// Navigate to Data Privacy
+		XCTAssertTrue(app.staticTexts["AppStrings.AppInformation.privacyTitle"].waitForExistence(timeout: .short))
     }
 
 }
