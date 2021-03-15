c9d328baefa93b72ce13f4a09868645c09ac66c5
Andreas Vogel <andreas.vogel@sap.com>
2021-03-15 18:42:09 +0100

    added kvStore property: show info screen only once

--- src/xcode/ENA/ENA/Source/Workers/Store/SecureStore.swift
+++ src/xcode/ENA/ENA/Source/Workers/Store/SecureStore.swift
@@ -283,6 +283,11 @@ final class SecureStore: Store {
 		set { kvStore["journalWithExposureHistoryInfoScreenShown"] = newValue }
 	}
 
+	var checkinInfoScreenShown: Bool {
+		get { kvStore["checkinInfoScreenShown"] as Bool? ?? false }
+		set { kvStore["checkinInfoScreenShown"] = newValue }
+	}
+	
 	var traceLocationsInfoScreenShown: Bool {
 		get { kvStore["traceLocationsInfoScreenShown"] as Bool? ?? false }
 		set { kvStore["traceLocationsInfoScreenShown"] = newValue }
