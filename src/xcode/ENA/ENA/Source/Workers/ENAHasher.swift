5e2864892998186b35035e480cc976e2ea96bae6
Nick Guendling <hello@nickguendling.com>
2021-03-16 16:25:21 +0100

    Rename hasher to ENAHasher

--- src/xcode/ENA/ENA/Source/Workers/ENAHasher.swift
+++ src/xcode/ENA/ENA/Source/Workers/ENAHasher.swift
@@ -4,7 +4,7 @@
 
 import Foundation
 
-enum Hasher {
+enum ENAHasher {
 	/// Hashes the given input string using SHA-256.
 	static func sha256(_ input: String) -> String {
 		return Data(input.utf8).sha256String()
