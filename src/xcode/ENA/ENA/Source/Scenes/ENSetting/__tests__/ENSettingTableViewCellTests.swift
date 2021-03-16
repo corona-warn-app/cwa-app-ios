13a549d977abcb24ebbc55979df4f3f268860dd0
Bastian Kohlbauer <bastian.kohlbauer@googelmail.com>
2021-03-16 18:04:46 +0100

    extend tests slightly

--- src/xcode/ENA/ENA/Source/Scenes/ENSetting/__tests__/ENSettingTableViewCellTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ENSetting/__tests__/ENSettingTableViewCellTests.swift
@@ -9,31 +9,37 @@ class ENSettingTableViewCellTests: XCTestCase {
 	
 	func testDaysSinceInstallTableViewCell() {
 		let cell = DaysSinceInstallTableViewCell(style: .default, reuseIdentifier: "")
+		cell.configure(daysSinceInstall: 3)
 		XCTAssertNotNil(cell)
 	}
 	
 	func testImageTableViewCell() {
 		let cell = ImageTableViewCell(style: .default, reuseIdentifier: "")
+		cell.configure(for: .bluetoothOff)
 		XCTAssertNotNil(cell)
 	}
 	
 	func testDescriptionTableViewCell() {
 		let cell = DescriptionTableViewCell(style: .default, reuseIdentifier: "")
+		cell.configure(for: .bluetoothOff)
 		XCTAssertNotNil(cell)
 	}
 	
 	func testActionTableViewCell() {
 		let cell = ActionTableViewCell(style: .default, reuseIdentifier: "")
+		cell.configure(for: .bluetoothOff)
 		XCTAssertNotNil(cell)
 	}
 	
 	func testEuTracingTableViewCell() {
 		let cell = EuTracingTableViewCell(style: .default, reuseIdentifier: "")
+		cell.configure(using: ENSettingEuTracingViewModel())
 		XCTAssertNotNil(cell)
 	}
 	
 	func testActionDetailTableViewCell() {
 		let cell = ActionDetailTableViewCell(style: .default, reuseIdentifier: "")
+		cell.configure(for: .bluetoothOff)
 		XCTAssertNotNil(cell)
 	}
 }
