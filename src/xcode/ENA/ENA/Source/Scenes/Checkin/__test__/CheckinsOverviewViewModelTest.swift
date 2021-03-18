5570bf8d0e08e9a6cb8fd943f82a2a17b9496e4c
Nick Guendling <hello@nickguendling.com>
2021-03-18 15:31:05 +0100

    Test remaining checkins overview model functions

--- src/xcode/ENA/ENA/Source/Scenes/Checkin/__test__/CheckinsOverviewViewModelTest.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Checkin/__test__/CheckinsOverviewViewModelTest.swift
@@ -122,4 +122,109 @@ class CheckinsOverviewViewModelTest: XCTestCase {
 		XCTAssertTrue(viewModel.canEditRow(at: IndexPath(row: 0, section: CheckinsOverviewViewModel.Section.entries.rawValue)))
 	}
 
+	func testDidTapEntryCell() throws {
+		let eventStore = MockEventStore()
+		eventStore.createCheckin(Checkin.mock(checkinStartDate: Date(timeIntervalSinceNow: -10)))
+		eventStore.createCheckin(Checkin.mock(checkinStartDate: Date()))
+		eventStore.createCheckin(Checkin.mock(checkinStartDate: Date(timeIntervalSinceNow: 10)))
+
+		let cellTapExpectation = expectation(description: "onEntryCellTap called")
+
+		let viewModel = CheckinsOverviewViewModel(
+			store: eventStore,
+			onEntryCellTap: {
+				XCTAssertEqual($0.traceLocationGUID, "137")
+				cellTapExpectation.fulfill()
+			}
+		)
+
+		viewModel.didTapEntryCell(at: IndexPath(row: 1, section: 2))
+
+		waitForExpectations(timeout: .medium)
+	}
+
+	func testDidTapEntryCellButton() throws {
+		let eventStore = MockEventStore()
+		eventStore.createCheckin(Checkin.mock(checkinStartDate: Date(timeIntervalSinceNow: -10)))
+		eventStore.createCheckin(
+			Checkin.mock(traceLocationGUID: "137", checkinStartDate: Date(), checkinEndDate: nil)
+		)
+		eventStore.createCheckin(Checkin.mock(checkinStartDate: Date(timeIntervalSinceNow: 10)))
+
+		let viewModel = CheckinsOverviewViewModel(
+			store: eventStore,
+			onEntryCellTap: { _ in }
+		)
+
+		viewModel.didTapEntryCellButton(at: IndexPath(row: 1, section: 2))
+
+		let tappedCheckin = eventStore.checkinsPublisher.value.first {
+			$0.traceLocationGUID == "137"
+		}
+		XCTAssertNotNil(tappedCheckin?.checkinEndDate)
+	}
+
+	func testRemoveEntry() throws {
+		let eventStore = MockEventStore()
+		eventStore.createCheckin(
+			Checkin.mock(traceLocationGUID: "17", checkinStartDate: Date(timeIntervalSinceNow: -10))
+		)
+		eventStore.createCheckin(
+			Checkin.mock(traceLocationGUID: "137", checkinStartDate: Date())
+		)
+		eventStore.createCheckin(
+			Checkin.mock(traceLocationGUID: "964", checkinStartDate: Date(timeIntervalSinceNow: 10))
+		)
+
+		let viewModel = CheckinsOverviewViewModel(
+			store: eventStore,
+			onEntryCellTap: { _ in }
+		)
+
+		viewModel.removeEntry(at: IndexPath(row: 1, section: 2))
+
+		let remainingGUIDs = eventStore.checkinsPublisher.value
+			.sorted { $0.checkinStartDate < $1.checkinStartDate }
+			.map { $0.traceLocationGUID }
+
+		XCTAssertEqual(remainingGUIDs, ["17", "964"])
+	}
+
+	func testRemoveAll() throws {
+		let eventStore = MockEventStore()
+		eventStore.createCheckin(Checkin.mock())
+		eventStore.createCheckin(Checkin.mock())
+		eventStore.createCheckin(Checkin.mock())
+
+		let viewModel = CheckinsOverviewViewModel(
+			store: eventStore,
+			onEntryCellTap: { _ in }
+		)
+
+		viewModel.removeAll()
+
+		XCTAssertTrue(eventStore.checkinsPublisher.value.isEmpty)
+	}
+
+	func testUpdateForCameraPermission() throws {
+		let reloadExpectation = expectation(description: "shouldReload published")
+		reloadExpectation.expectedFulfillmentCount = 2 // initial call + update for camera permission
+
+		let viewModel = CheckinsOverviewViewModel(
+			store: MockEventStore(),
+			onEntryCellTap: { _ in }
+		)
+
+		let cancellable = viewModel.$shouldReload
+			.sink { _ in
+				reloadExpectation.fulfill()
+			}
+
+		viewModel.updateForCameraPermission()
+
+		waitForExpectations(timeout: .medium)
+
+		cancellable.cancel()
+	}
+
 }
