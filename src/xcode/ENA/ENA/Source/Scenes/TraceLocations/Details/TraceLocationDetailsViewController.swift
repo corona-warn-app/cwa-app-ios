bebeb81aea046a064db37a5f6feb32b845a32c71
Naveed Khalid <nkhalid.here@gmail.com>
2021-03-19 17:30:24 +0100

    detail controller and model

--- src/xcode/ENA/ENA/Source/Scenes/TraceLocations/Details/TraceLocationDetailsViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/TraceLocations/Details/TraceLocationDetailsViewController.swift
@@ -3,6 +3,8 @@
 //
 
 import UIKit
+import OpenCombine
+import PDFKit
 
 class TraceLocationDetailsViewController: UIViewController, ENANavigationControllerWithFooterChild {
 
@@ -10,7 +12,7 @@ class TraceLocationDetailsViewController: UIViewController, ENANavigationControl
 
 	init(
 		viewModel: TraceLocationDetailsViewModel,
-		onPrintVersionButtonTap: @escaping (TraceLocation) -> Void,
+		onPrintVersionButtonTap: @escaping (PDFView) -> Void,
 		onDuplicateButtonTap: @escaping (TraceLocation) -> Void,
 		onDismiss: @escaping () -> Void
 	) {
@@ -40,7 +42,7 @@ class TraceLocationDetailsViewController: UIViewController, ENANavigationControl
 				self?.onDismiss()
 			}
 		)
-
+		
 		footerView?.primaryButton?.accessibilityIdentifier = AccessibilityIdentifiers.ExposureSubmission.primaryButton
 	}
 
@@ -51,20 +53,69 @@ class TraceLocationDetailsViewController: UIViewController, ENANavigationControl
 	// MARK: - Protocol ENANavigationControllerWithFooterChild
 
 	func navigationController(_ navigationController: ENANavigationControllerWithFooter, didTapPrimaryButton button: UIButton) {
-		onPrintVersionButtonTap(viewModel.traceLocation)
+		navigationFooterItem.isPrimaryButtonEnabled = false
+		navigationFooterItem.isPrimaryButtonLoading = true
+		
+		generateAndPassQRCodePoster()
 	}
 
 	func navigationController(_ navigationController: ENANavigationControllerWithFooter, didTapSecondaryButton button: UIButton) {
 		onDuplicateButtonTap(viewModel.traceLocation)
+	}
+	
+	private func generateAndPassQRCodePoster() {
+		viewModel.fetchQRCodePosterTemplateData { [weak self] templateData in
+			switch templateData {
+			case let .success(templateData):
+				DispatchQueue.main.async { [weak self] in
+					self?.navigationFooterItem.isPrimaryButtonEnabled = true
+					self?.navigationFooterItem.isPrimaryButtonLoading = false
+					
+					guard let pdfView = self?.createPdfView(templateData: templateData) else { return }
+					self?.onPrintVersionButtonTap(pdfView)
+				}
+			case let .failure(error):
+				self?.navigationFooterItem.isPrimaryButtonEnabled = true
+				self?.navigationFooterItem.isPrimaryButtonLoading = false
+				
+				Log.error("Could not retrieve QR code poster template from protobuf.", log: .qrCode, error: error)
+				return
+			}
+		}
+	}
+
+	private func createPdfView(templateData: SAP_Internal_Pt_QRCodePosterTemplateIOS) -> PDFView {
+		let pdfView = PDFView()
+		let pdfDocument = PDFDocument(data: templateData.template)
+
+		let placeHolderString = "HTTPS://CORONAWARN.APP/E1/BIPEY33SMVWSA2LQON2W2IDEN5WG64RAONUXIIDBNVSXILBAMNXRBCM4UQARRKM6UQASAHRKCC7CTDWGQ4JCO7RVZSWVIMQK4UPA.GBCAEIA7TEORBTUA25QHBOCWT26BCA5PORBS2E4FFWMJ3UU3P6SXOL7SHUBCA7UEZBDDQ2R6VRJH7WBJKVF7GZYJA6YMRN27IPEP7NKGGJSWX3XQ"
+		guard let qrCodeImage = viewModel.traceLocation.generateQRCode(with: placeHolderString, size: CGSize(width: 400, height: 400)) else { return pdfView }
+
+		let textDetails = templateData.description_p
+		try? pdfDocument?.embed(
+			image: qrCodeImage,
+			at: CGPoint(x: CGFloat(templateData.offsetX), y: CGFloat(templateData.offsetY)),
+			text: viewModel.traceLocation.address,
+			of: CGFloat(textDetails.fontSize),
+			hex: textDetails.fontColor,
+			with: CGRect(x: CGFloat(textDetails.offsetX), y: CGFloat(textDetails.offsetY), width: CGFloat(textDetails.width), height: CGFloat(textDetails.height))
+		)
+
+		pdfView.document = pdfDocument
+		pdfView.scaleFactor = pdfView.scaleFactorForSizeToFit
+		pdfView.autoScales = true
+		return pdfView
 	}
 
 	// MARK: - Private
 
 	private let viewModel: TraceLocationDetailsViewModel
 
-	private let onPrintVersionButtonTap: (TraceLocation) -> Void
+	private let onPrintVersionButtonTap: (PDFView) -> Void
 	private let onDuplicateButtonTap: (TraceLocation) -> Void
 	private let onDismiss: () -> Void
+
+	private var subscriptions = [AnyCancellable]()
 
 	private lazy var navigationFooterItem: ENANavigationFooterItem = {
 		let item = ENANavigationFooterItem()
--- src/xcode/ENA/ENA/Source/Scenes/TraceLocations/Details/TraceLocationDetailsViewModel.swift
+++ src/xcode/ENA/ENA/Source/Scenes/TraceLocations/Details/TraceLocationDetailsViewModel.swift
@@ -3,21 +3,53 @@
 //
 
 import Foundation
+import PDFKit
+import OpenCombine
 
 class TraceLocationDetailsViewModel {
 
 	// MARK: - Init
 
 	init(
-		traceLocation: TraceLocation
+		traceLocation: TraceLocation,
+		store: Store,
+		qrCodePosterTemplateProvider: QRCodePosterTemplateProviding
 	) {
 		self.traceLocation = traceLocation
+		self.store = store
+		self.qrCodePosterTemplateProvider = qrCodePosterTemplateProvider
 	}
 
 	// MARK: - Internal
 
 	let traceLocation: TraceLocation
+	typealias QRCodePosterTemplateCompletionHandler = (Result<SAP_Internal_Pt_QRCodePosterTemplateIOS, URLSession.Response.Failure>) -> Void
+
+	func fetchQRCodePosterTemplateData(completion: @escaping QRCodePosterTemplateCompletionHandler) {
+		qrCodePosterTemplateProvider.latestQRCodePosterTemplate()
+			.sink(
+				receiveCompletion: { result in
+					switch result {
+					case .finished:
+						break
+					case .failure(let error):
+						if case CachingHTTPClient.CacheError.dataVerificationError = error {
+							Log.error("Signature verification error.", log: .qrCode, error: error)
+						}
+						Log.error("Could not fetch QR code poster template protobuf.", log: .qrCode, error: error)
+					}
+				}, receiveValue: { [weak self] in
+					self?.qrCodePosterTemplate = $0
+					completion(.success($0))
+				}
+			)
+			.store(in: &subscriptions)
+	}
 
 	// MARK: - Private
 
+	private let store: Store
+	private let qrCodePosterTemplateProvider: QRCodePosterTemplateProviding
+	private var subscriptions = Set<AnyCancellable>()
+	@OpenCombine.Published private(set) var qrCodePosterTemplate: SAP_Internal_Pt_QRCodePosterTemplateIOS = SAP_Internal_Pt_QRCodePosterTemplateIOS()
 }
