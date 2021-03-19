34b57362781b8972c4d358780a60f137cacc2d48
Pascal Brause <pascal@brause.haus>
2021-03-19 16:11:21 +0100

    create dummy matching component

---
+++ src/xcode/ENA/ENA/Source/Services/Event Registration/TraceWarningDownload/TraceWarningMatching.swift
@@ -0,0 +1,56 @@
+////
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+
+protocol TraceWarningMatching {
+	
+	func matchAndStore(package: SAPDownloadedPackage)
+	
+}
+
+final class TraceWarningMatcher: TraceWarningMatching {
+
+	// MARK: - Init
+	
+	init(
+		eventStore: EventStoringProviding
+	) {
+		self.eventStore = eventStore
+	}
+	
+	// MARK: - Overrides
+	
+	// MARK: - Protocol TraceWarningMatching
+	
+	func matchAndStore(package: SAPDownloadedPackage) {
+		/*
+		
+		Filter for Matches: for each downloaded TraceWarningPackage that is not empty (i.e. not a zip file, cwa-empty-pkg header set), the corresponding items of Protocol Buffer message TraceTimeIntervalWarning are filtered for those that have an overlap time of > 0 with any of check-ins from the Database Table for CheckIns as per Calculate Overlap of CheckIn and TraceTimeIntervalWarning. A match references a check-in, a TraceWarningPackage, and a TraceTimeIntervalWarning.
+
+		Store Matches: all matches are stored as a corresponding record in the Database Table for TraceTimeIntervalMatches as follows:
+
+		Check In ID column set to the ID of the check-in (from Database Table for CheckIns) that caused the match
+
+		TraceWarningPackage ID column set to the identifier of the TraceWarningPackage that produced the match
+
+		TraceLocation GUID column set to the corresponding column of the check-in (from Database Table for CheckIns)
+
+		Transmission Risk Level column set to the transmissionRiskLevel attribute from the TraceTimeIntervalWarning
+
+		StartIntervalNumber column set to the startIntervalNumber attribute from TraceTimeIntervalWarning
+
+		EndIntervalNumber column set to the sum of value of the startIntervalNumber attribute plus the value of the period attribute from TraceTimeIntervalWarning
+		
+		*/
+	}
+	
+	// MARK: - Public
+	
+	// MARK: - Internal
+	
+	// MARK: - Private
+	
+	private let eventStore: EventStoringProviding
+}
