b2b525251efe2113cc4e743b37bb89798dc6a200
Naveed Khalid <nkhalid.here@gmail.com>
2021-03-22 13:21:24 +0100

    adding better error handling

--- src/xcode/ENA/ENA/Source/Scenes/TraceLocations/Details/TraceLocationDetailsViewModel.swift
+++ src/xcode/ENA/ENA/Source/Scenes/TraceLocations/Details/TraceLocationDetailsViewModel.swift
@@ -23,7 +23,7 @@ class TraceLocationDetailsViewModel {
 	// MARK: - Internal
 
 	let traceLocation: TraceLocation
-	typealias QRCodePosterTemplateCompletionHandler = (Result<SAP_Internal_Pt_QRCodePosterTemplateIOS, URLSession.Response.Failure>) -> Void
+	typealias QRCodePosterTemplateCompletionHandler = (Result<SAP_Internal_Pt_QRCodePosterTemplateIOS, Error>) -> Void
 
 	func fetchQRCodePosterTemplateData(completion: @escaping QRCodePosterTemplateCompletionHandler) {
 		qrCodePosterTemplateProvider.latestQRCodePosterTemplate()
@@ -35,8 +35,10 @@ class TraceLocationDetailsViewModel {
 					case .failure(let error):
 						if case CachingHTTPClient.CacheError.dataVerificationError = error {
 							Log.error("Signature verification error.", log: .qrCode, error: error)
+							completion(.failure(error))
 						}
 						Log.error("Could not fetch QR code poster template protobuf.", log: .qrCode, error: error)
+						completion(.failure(error))
 					}
 				}, receiveValue: { [weak self] in
 					self?.qrCodePosterTemplate = $0
