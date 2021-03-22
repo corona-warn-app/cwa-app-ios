047ed8cf61c743b7e3bd41a9e85bb9e7e416c901
Naveed Khalid <nkhalid.here@gmail.com>
2021-03-22 17:24:10 +0100

    print and share functionality

--- src/xcode/ENA/ENA/Source/Scenes/TraceLocations/PrintVersion/TraceLocationPrintVersionViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/TraceLocations/PrintVersion/TraceLocationPrintVersionViewController.swift
@@ -27,7 +27,14 @@ class TraceLocationPrintVersionViewController: UIViewController {
 
 		self.view = viewModel.pdfView
 
-		navigationItem.rightBarButtonItems = [UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(didTapExportButton)), UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(didTapExportButton))]
+		let printButton = UIBarButtonItem(image: UIImage(named: "Icons_Print"), style: .plain, target: self, action: #selector(didTapPrintButton))
+		let shareButton = UIBarButtonItem(image: UIImage(named: "Icons_Share"), style: .plain, target: self, action: #selector(didTapShareButton))
+		
+		if UIPrintInteractionController.isPrintingAvailable {
+			navigationItem.rightBarButtonItems = [shareButton, printButton]
+		} else {
+			navigationItem.rightBarButtonItem = shareButton
+		}
 	}
 
 	// MARK: - Private
@@ -35,10 +42,18 @@ class TraceLocationPrintVersionViewController: UIViewController {
 	private let viewModel: TraceLocationPrintVersionViewModel
 
 	@objc
-	private func didTapExportButton() {
+	private func didTapShareButton() {
 		guard let data = viewModel.pdfView.document?.dataRepresentation() else { return }
 		let activityViewController = UIActivityViewController(activityItems: [data], applicationActivities: nil)
 		present(activityViewController, animated: true, completion: nil)
 	}
 
+	@objc
+	private func didTapPrintButton() {
+		guard let data = viewModel.pdfView.document?.dataRepresentation() else { return }
+		
+		let printController = UIPrintInteractionController.shared
+		printController.printingItem = data
+		printController.present(animated: true, completionHandler: nil)
+	}
 }
