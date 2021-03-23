6703130c9f5afdb68feefaa1f9a82d9db309972c
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-03-23 19:47:06 +0100

    Add Unit tests for mapping to protobufs

--- src/xcode/ENA/ENA/Source/Services/PPAnalyticsSubmitter/__tests__/PPAnalyticsSubmitterTests.swift
+++ src/xcode/ENA/ENA/Source/Services/PPAnalyticsSubmitter/__tests__/PPAnalyticsSubmitterTests.swift
@@ -381,22 +381,64 @@ class PPAnalyticsSubmitterTests: XCTestCase {
 		// THEN
 		XCTAssertEqual(expectation, SAP_Internal_Ppdd_PPARiskLevel.riskLevelHigh)
 	}
-	
-	func testGatherTestResultMetadata() {
-		// setup Submitter
+		
+	// MARK: - ProtoBuf Mapping
+
+	func testGatherUserMetadata() {
 		let store = MockTestStore()
+		let analyticsSubmitter = createMockSubmitter(with: store)
 		store.isPrivacyPreservingAnalyticsConsentGiven = true
-		let client = ClientMock()
-		var config = SAP_Internal_V2_ApplicationConfigurationIOS()
-		let appConfigurationProvider = CachedAppConfigurationMock(with: config)
-		let analyticsSubmitter = PPAnalyticsSubmitter(
-			store: store,
-			client: client,
-			appConfig: appConfigurationProvider
-		)
 		
 		// Setup Collector
-		Analytics.setupMock(store: store,submitter: analyticsSubmitter)
+		Analytics.setupMock(store: store, submitter: analyticsSubmitter)
+
+		// collect userMetadata
+		let state: FederalStateName = .badenWürttemberg
+		let ageGroup: AgeGroup = .ageBelow29
+		let administrativeUnit = 4
+		
+		store.userData = UserMetadata(
+			federalState: state,
+			administrativeUnit: administrativeUnit,
+			ageGroup: ageGroup)
+		
+		let protobuf = analyticsSubmitter.gatherUserMetadata()
+		XCTAssertNotNil(store.userMetadata, "userMetadata should be allocated")
+
+		XCTAssertEqual(protobuf.federalState, state.protobuf, "Wrong Registration date")
+		XCTAssertEqual(protobuf.ageGroup, ageGroup.protobuf, "Wrong Registration date")
+		XCTAssertEqual(protobuf.administrativeUnit, Int32(administrativeUnit), "Wrong Registration date")
+	}
+	
+	func testGatherClientMetadata() {
+		
+		let eTag = "123"
+		let store = MockTestStore()
+		store.appConfigMetadata = AppConfigMetadata(
+			lastAppConfigETag: eTag,
+			lastAppConfigFetch: Date(),
+			appConfig: SAP_Internal_V2_ApplicationConfigurationIOS()
+		)
+		store.isPrivacyPreservingAnalyticsConsentGiven = true
+		
+		let analyticsSubmitter = createMockSubmitter(with: store)
+		let clientMetadata = ClientMetadata(etag: eTag)
+		let protobuf = analyticsSubmitter.gatherClientMetadata()
+		
+		XCTAssertNotNil(store.clientMetadata, "clientMetadata should be allocated")
+		XCTAssertEqual(protobuf.appConfigEtag, eTag, "eTag not equal clientMetaData eTag")
+		XCTAssertEqual(protobuf.cwaVersion, clientMetadata.cwaVersion?.protobuf, "cwaVersion not equal clientMetaData cwaVersion")
+		XCTAssertEqual(protobuf.iosVersion, clientMetadata.iosVersion.protobuf, "iosVersion not equal clientMetaData iosVersion")
+	}
+	
+	func testGatherTestResultMetadata() {
+		let store = MockTestStore()
+		store.isPrivacyPreservingAnalyticsConsentGiven = true
+		let analyticsSubmitter = createMockSubmitter(with: store)
+
+		// Setup Collector
+		
+		Analytics.setupMock(store: store, submitter: analyticsSubmitter)
 		
 		// collect testResultMetadata
 		
@@ -404,7 +446,6 @@ class PPAnalyticsSubmitterTests: XCTestCase {
 		let registrationDate = Calendar.current.date(byAdding: .day, value: -10, to: today) ?? Date()
 		let mostRecentDayWithRisk = Calendar.current.date(byAdding: .day, value: -5, to: today)
 		let dateOfRiskChangeToHigh = Calendar.current.date(byAdding: .day, value: -12, to: today)
-		
 		let registrationToken = "123"
 		let testResult: TestResult = .negative
 		let numberOfDaysWithHightRisk = 25
@@ -427,48 +468,194 @@ class PPAnalyticsSubmitterTests: XCTestCase {
 		store.riskCalculationResult = riskCalculationResult
 		store.dateOfConversionToHighRisk = dateOfRiskChangeToHigh
 		
-		
-		// Test Saving Value To Store Correctly
+		let metadata = store.testResultMetadata
 		Analytics.collect(.testResultMetadata(.registerNewTestMetadata(registrationDate, registrationToken)))
-		
-		XCTAssertEqual(store.testResultMetadata?.testRegistrationDate, registrationDate, "Wrong Registration date")
-		XCTAssertEqual(store.testResultMetadata?.riskLevelAtTestRegistration, riskLevel, "Wrong Risk Level")
-		XCTAssertEqual(store.testResultMetadata?.daysSinceMostRecentDateAtRiskLevelAtTestRegistration, numberOfDaysWithHightRisk, "Wrong number of days with this risk level")
-		XCTAssertEqual(store.testResultMetadata?.hoursSinceHighRiskWarningAtTestRegistration, differenceInHoursBetweenChangeToHighRiskAndRegistrationDate, "Wrong difference hoursSinceHighRiskWarningAtTestRegistration")
+		XCTAssertEqual(metadata?.testRegistrationDate, registrationDate, "Wrong Registration date")
+		XCTAssertEqual(metadata?.riskLevelAtTestRegistration, riskLevel, "Wrong Risk Level")
+		XCTAssertEqual(metadata?.daysSinceMostRecentDateAtRiskLevelAtTestRegistration, numberOfDaysWithHightRisk, "Wrong number of days with this risk level")
+		XCTAssertEqual(metadata?.hoursSinceHighRiskWarningAtTestRegistration, differenceInHoursBetweenChangeToHighRiskAndRegistrationDate, "Wrong difference hoursSinceHighRiskWarningAtTestRegistration")
 
 		Analytics.collect(.testResultMetadata(.updateTestResult(testResult, registrationToken)))
-		
-		XCTAssertEqual(store.testResultMetadata?.testResult, testResult, "Wrong TestResult")
-		XCTAssertEqual(store.testResultMetadata?.hoursSinceTestRegistration, differenceInHoursBetweenRegistrationDateAndTestResult, "Wrong difference hoursSinceTestRegistration")
+		XCTAssertEqual(metadata?.testResult, testResult, "Wrong TestResult")
+		XCTAssertEqual(metadata?.hoursSinceTestRegistration, differenceInHoursBetweenRegistrationDateAndTestResult, "Wrong difference hoursSinceTestRegistration")
 
-		// Test mapping to protobuf
-
-		let protobuf = analyticsSubmitter.gatherTestResultMetadata()
+		// Mapping to protobuf
+		let protobuf = analyticsSubmitter.gatherTestResultMetadata().first
 		XCTAssertEqual(
 			store.testResultMetadata?.testResult?.protobuf,
-			protobuf.first?.testResult,
+			protobuf?.testResult,
 			"Wrong testResult protobuf mapping"
 		)
 		XCTAssertEqual(
 			store.testResultMetadata?.hoursSinceTestRegistration,
-			Int(protobuf.first?.hoursSinceTestRegistration ?? -1),
+			Int(protobuf?.hoursSinceTestRegistration ?? -1),
 			"Wrong hoursSinceTestRegistration protobuf mapping"
 		)
 		XCTAssertEqual(
 			store.testResultMetadata?.riskLevelAtTestRegistration?.protobuf,
-			protobuf.first?.riskLevelAtTestRegistration,
+			protobuf?.riskLevelAtTestRegistration,
 			"Wrong riskLevelAtTestRegistration protobuf mapping"
 		)
 		XCTAssertEqual(
 			store.testResultMetadata?.daysSinceMostRecentDateAtRiskLevelAtTestRegistration,
-			Int(protobuf.first?.daysSinceMostRecentDateAtRiskLevelAtTestRegistration ?? -1),
+			Int(protobuf?.daysSinceMostRecentDateAtRiskLevelAtTestRegistration ?? -1),
 			"Wrong daysSinceMostRecentDateAtRiskLevelAtTestRegistration protobuf mapping"
 		)
 		XCTAssertEqual(
 			store.testResultMetadata?.hoursSinceHighRiskWarningAtTestRegistration,
-			Int(protobuf.first?.hoursSinceHighRiskWarningAtTestRegistration ?? -1),
+			Int(protobuf?.hoursSinceHighRiskWarningAtTestRegistration ?? -1),
 			"Wrong hoursSinceHighRiskWarningAtTestRegistration protobuf mapping"
 		)
+	}
+	
+	func testGatherRiskExposureMetadata() {
+		let store = MockTestStore()
+		store.isPrivacyPreservingAnalyticsConsentGiven = true
+		let analyticsSubmitter = createMockSubmitter(with: store)
+
+		// Setup Collector
+		Analytics.setupMock(store: store, submitter: analyticsSubmitter)
+		
+		// Collect RiskExposureMetadata
+		let numberOfDaysWithHightRisk = 25
+		let riskLevel: RiskLevel = .high
+		let mostRecentDayWithRisk = Calendar.current.date(byAdding: .day, value: -5, to: Date())
+
+		let riskCalculationResult = RiskCalculationResult(
+			riskLevel: riskLevel,
+			minimumDistinctEncountersWithLowRisk: 6,
+			minimumDistinctEncountersWithHighRisk: 2,
+			mostRecentDateWithLowRisk: nil,
+			mostRecentDateWithHighRisk: mostRecentDayWithRisk,
+			numberOfDaysWithLowRisk: 0,
+			numberOfDaysWithHighRisk: numberOfDaysWithHightRisk,
+			calculationDate: Date(),
+			riskLevelPerDate: [:],
+			minimumDistinctEncountersWithHighRiskPerDate: [:]
+		)
+
+		let metadata = store.currentRiskExposureMetadata
+		Analytics.collect(.riskExposureMetadata(.updateRiskExposureMetadata(riskCalculationResult)))
+		XCTAssertNotNil(store.currentRiskExposureMetadata, "riskMetadata should be allocated")
+		XCTAssertEqual(metadata?.riskLevel, riskLevel, "Wrong riskLevel")
+		XCTAssertEqual(metadata?.riskLevelChangedComparedToPreviousSubmission, false, "should be false as this is the first submission")
+		XCTAssertEqual(metadata?.dateChangedComparedToPreviousSubmission, false, "should be false as this is the first submission")
+		
+		// Mapping to protobuf
+		let protobuf = analyticsSubmitter.gatherExposureRiskMetadata()
+		XCTAssertFalse(protobuf.isEmpty, "There should be at least one item in the array")
+		XCTAssertEqual(protobuf.first?.riskLevel, riskLevel.protobuf, "Wrong riskLevel mapped")
+		XCTAssertEqual(protobuf.first?.riskLevelChangedComparedToPreviousSubmission, metadata?.riskLevelChangedComparedToPreviousSubmission, "Wrong riskLevelChangedComparedToPreviousSubmission")
+		XCTAssertEqual(protobuf.first?.mostRecentDateAtRiskLevel, formatToUnixTimestamp(for: metadata?.mostRecentDateAtRiskLevel), "Wrong mostRecentDateAtRiskLevel")
+		XCTAssertEqual(protobuf.first?.dateChangedComparedToPreviousSubmission, metadata?.dateChangedComparedToPreviousSubmission, "Wrong dateChangedComparedToPreviousSubmission")
+	}
+	
+	func testGatherExposureWindowsMetadata() {
+		let store = MockTestStore()
+		store.isPrivacyPreservingAnalyticsConsentGiven = true
+		let analyticsSubmitter = createMockSubmitter(with: store)
+
+		// Setup Collector
+		Analytics.setupMock(store: store, submitter: analyticsSubmitter)
+		
+		// collect exposureWindowsMetadata
+		Analytics.collect(.exposureWindowsMetadata(.collectExposureWindows(mappedExposureWindows)))
+		let mappedSubmissionExposureWindows: [SubmissionExposureWindow] = mappedExposureWindows.map {
+			SubmissionExposureWindow(
+				exposureWindow: $0.exposureWindow,
+				transmissionRiskLevel: $0.transmissionRiskLevel,
+				normalizedTime: $0.normalizedTime,
+				hash: generateSHA256($0.exposureWindow),
+				date: $0.date
+			)
+		}
+		
+		let metadata = store.exposureWindowsMetadata
+		XCTAssertEqual(metadata?.newExposureWindowsQueue, mappedSubmissionExposureWindows, "Wrong newExposureWindowsQueue")
+		XCTAssertEqual(metadata?.reportedExposureWindowsQueue, mappedSubmissionExposureWindows, "Wrong reportedExposureWindowsQueue")
+
+		// Mapping to protobuf
+		let protobuf = analyticsSubmitter.gatherNewExposureWindows()
+		
+		for index in protobuf.indices {
+			XCTAssertEqual(protobuf[index].normalizedTime, mappedSubmissionExposureWindows[index].normalizedTime, "Wrong normalizedTime")
+			XCTAssertEqual(protobuf[index].transmissionRiskLevel, Int32(mappedSubmissionExposureWindows[index].transmissionRiskLevel), "Wrong transmissionRiskLevel")
+			XCTAssertEqual(protobuf[index].exposureWindow.calibrationConfidence, Int32(mappedSubmissionExposureWindows[index].exposureWindow.calibrationConfidence.rawValue), "Wrong calibrationConfidence")
+			XCTAssertEqual(protobuf[index].exposureWindow.infectiousness, mappedSubmissionExposureWindows[index].exposureWindow.infectiousness.protobuf, "Wrong infectiousness")
+			XCTAssertEqual(protobuf[index].exposureWindow.reportType, mappedSubmissionExposureWindows[index].exposureWindow.reportType.protobuf, "Wrong reportType")
+			XCTAssertEqual(protobuf[index].exposureWindow.date, Int64(mappedSubmissionExposureWindows[index].exposureWindow.date.timeIntervalSince1970), "Wrong date")
+			XCTAssertEqual(protobuf[index].exposureWindow.scanInstances.count, mappedSubmissionExposureWindows[index].exposureWindow.scanInstances.count, "Wrong scanInstances.count")
+
+			for (scanInstancesIndex, scanInstance)  in protobuf[index].exposureWindow.scanInstances.enumerated() {
+				XCTAssertEqual(scanInstance.minAttenuation, Int32(mappedSubmissionExposureWindows[index].exposureWindow.scanInstances[scanInstancesIndex].minAttenuation), "Wrong minAttenuation")
+				XCTAssertEqual(scanInstance.secondsSinceLastScan, Int32(mappedSubmissionExposureWindows[index].exposureWindow.scanInstances[scanInstancesIndex].secondsSinceLastScan), "Wrong secondsSinceLastScan")
+				XCTAssertEqual(scanInstance.typicalAttenuation, Int32(mappedSubmissionExposureWindows[index].exposureWindow.scanInstances[scanInstancesIndex].typicalAttenuation), "Wrong typicalAttenuation")
+			}
+		}
+	}
+	
+	func testGatherKeySubmissionMetadata() {
+		let store = MockTestStore()
+		store.isPrivacyPreservingAnalyticsConsentGiven = true
+		let analyticsSubmitter = createMockSubmitter(with: store)
+
+		// Setup Collector
+		Analytics.setupMock(store: store, submitter: analyticsSubmitter)
+		
+		// collect keySubmissionMetadata
+		let keySubmissionMetadata = KeySubmissionMetadata(
+			submitted: false,
+			submittedInBackground: false,
+			submittedAfterCancel: false,
+			submittedAfterSymptomFlow: false,
+			lastSubmissionFlowScreen: .submissionFlowScreenUnknown,
+			advancedConsentGiven: true,
+			hoursSinceTestResult: 0,
+			hoursSinceTestRegistration: 0,
+			daysSinceMostRecentDateAtRiskLevelAtTestRegistration: -1,
+			hoursSinceHighRiskWarningAtTestRegistration: -1
+		)
+		
+		let lastScreen: LastSubmissionFlowScreen = .submissionFlowScreenOther
+		Analytics.collect(.keySubmissionMetadata(.create(keySubmissionMetadata)))
+		Analytics.collect(.keySubmissionMetadata(.submitted(true)))
+		Analytics.collect(.keySubmissionMetadata(.submittedInBackground(true)))
+		Analytics.collect(.keySubmissionMetadata(.submittedAfterCancel(true)))
+		Analytics.collect(.keySubmissionMetadata(.submittedAfterSymptomFlow(true)))
+		Analytics.collect(.keySubmissionMetadata(.submittedWithTeletan(false)))
+		Analytics.collect(.keySubmissionMetadata(.lastSubmissionFlowScreen(lastScreen)))
+		Analytics.collect(.keySubmissionMetadata(.advancedConsentGiven(true)))
+		Analytics.collect(.keySubmissionMetadata(.hoursSinceTestResult(5)))
+		Analytics.collect(.keySubmissionMetadata(.keySubmissionHoursSinceTestRegistration(9)))
+		Analytics.collect(.keySubmissionMetadata(.daysSinceMostRecentDateAtRiskLevelAtTestRegistration(74)))
+		Analytics.collect(.keySubmissionMetadata(.hoursSinceHighRiskWarningAtTestRegistration(53)))
+
+		let metadata = store.keySubmissionMetadata
+		XCTAssertNotNil(metadata, "keySubmissionMetadata should be allocated")
+		XCTAssertEqual(metadata?.submitted, true, "Wrong keySubmissionMetadata")
+		XCTAssertEqual(metadata?.submittedInBackground, true, "Wrong keySubmissionMetadata")
+		XCTAssertEqual(metadata?.submittedAfterCancel, true, "Wrong keySubmissionMetadata")
+		XCTAssertEqual(metadata?.submittedAfterSymptomFlow, true, "Wrong keySubmissionMetadata")
+		XCTAssertEqual(metadata?.lastSubmissionFlowScreen, lastScreen, "Wrong keySubmissionMetadata")
+		XCTAssertEqual(metadata?.advancedConsentGiven, true, "Wrong keySubmissionMetadata")
+		XCTAssertEqual(metadata?.hoursSinceTestResult, 5, "Wrong keySubmissionMetadata")
+		XCTAssertEqual(metadata?.hoursSinceTestRegistration, 9, "Wrong keySubmissionMetadata")
+		XCTAssertEqual(metadata?.daysSinceMostRecentDateAtRiskLevelAtTestRegistration, 74, "Wrong keySubmissionMetadata")
+		XCTAssertEqual(metadata?.hoursSinceHighRiskWarningAtTestRegistration, 53, "Wrong keySubmissionMetadata")
+		
+		// Mapping to protobuf
+		let protobuf = analyticsSubmitter.gatherKeySubmissionMetadata().first
+		XCTAssertEqual(protobuf?.submitted, metadata?.submitted, "Wrong submitted")
+		XCTAssertEqual(protobuf?.submittedInBackground, metadata?.submittedInBackground, "Wrong submittedInBackground")
+		XCTAssertEqual(protobuf?.submittedAfterCancel, metadata?.submittedAfterCancel, "Wrong submittedAfterCancel")
+		XCTAssertEqual(protobuf?.submittedAfterSymptomFlow, metadata?.submittedAfterSymptomFlow, "Wrong submittedAfterSymptomFlow")
+		XCTAssertEqual(protobuf?.advancedConsentGiven, metadata?.advancedConsentGiven, "Wrong advancedConsentGiven")
+		XCTAssertEqual(protobuf?.lastSubmissionFlowScreen, metadata?.lastSubmissionFlowScreen?.protobuf, "Wrong lastSubmissionFlowScreen")
+		XCTAssertEqual(protobuf?.hoursSinceTestResult, metadata?.hoursSinceTestResult, "Wrong hoursSinceTestResult")
+		XCTAssertEqual(protobuf?.hoursSinceTestRegistration, metadata?.hoursSinceTestRegistration, "Wrong hoursSinceTestRegistration")
+		XCTAssertEqual(protobuf?.daysSinceMostRecentDateAtRiskLevelAtTestRegistration, metadata?.daysSinceMostRecentDateAtRiskLevelAtTestRegistration, "Wrong daysSinceMostRecentDateAtRiskLevelAtTestRegistration")
+		XCTAssertEqual(protobuf?.hoursSinceHighRiskWarningAtTestRegistration, metadata?.hoursSinceHighRiskWarningAtTestRegistration, "Wrong hoursSinceHighRiskWarningAtTestRegistration")
+		XCTAssertNotEqual(protobuf?.submittedWithTeleTan, store.submittedWithQR, "Wrong submittedWithTeleTan")
 	}
 	
 	// MARK: - Helpers
@@ -502,4 +689,40 @@ class PPAnalyticsSubmitterTests: XCTestCase {
 		}
 		return Int64(date.timeIntervalSince1970)
 	}
+	
+	private var mappedExposureWindows: [RiskCalculationExposureWindow] = [
+		RiskCalculationExposureWindow(
+			exposureWindow: ExposureWindow(
+				calibrationConfidence: .high,
+				date: Date(),
+				reportType: .confirmedClinicalDiagnosis,
+				infectiousness: .high,
+				scanInstances: []
+			),
+			configuration: RiskCalculationConfiguration(
+				from: SAP_Internal_V2_ApplicationConfigurationIOS().riskCalculationParameters)
+		),
+		RiskCalculationExposureWindow(
+			exposureWindow: ExposureWindow(
+				calibrationConfidence: .low,
+				date: Date(),
+				reportType: .confirmedClinicalDiagnosis,
+				infectiousness: .high,
+				scanInstances: []
+			),
+			configuration: RiskCalculationConfiguration(
+				from: SAP_Internal_V2_ApplicationConfigurationIOS().riskCalculationParameters)
+		),
+		RiskCalculationExposureWindow(
+			exposureWindow: ExposureWindow(
+				calibrationConfidence: .medium,
+				date: Date(),
+				reportType: .confirmedClinicalDiagnosis,
+				infectiousness: .high,
+				scanInstances: []
+			),
+			configuration: RiskCalculationConfiguration(
+				from: SAP_Internal_V2_ApplicationConfigurationIOS().riskCalculationParameters)
+		)
+	]
 }
