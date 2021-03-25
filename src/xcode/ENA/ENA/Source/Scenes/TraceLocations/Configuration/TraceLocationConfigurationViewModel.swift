bba790ad0782631e9d638c537dcbb61319fc6596
Nick Guendling <hello@nickguendling.com>
2021-03-25 18:55:14 +0100

    Prepare trace location data for network request

--- src/xcode/ENA/ENA/Source/Scenes/TraceLocations/Configuration/TraceLocationConfigurationViewModel.swift
+++ src/xcode/ENA/ENA/Source/Scenes/TraceLocations/Configuration/TraceLocationConfigurationViewModel.swift
@@ -165,6 +165,14 @@ class TraceLocationConfigurationViewModel {
 	}
 
 	func save(completion: @escaping (Bool) -> Void) {
+		var traceLocation = SAP_Internal_Pt_TraceLocation()
+		traceLocation.version = 1
+		traceLocation.description_p = description.trimmingCharacters(in: .whitespacesAndNewlines)
+		traceLocation.address = address.trimmingCharacters(in: .whitespacesAndNewlines)
+		traceLocation.startTimestamp = startDate?.unsignedTimeIntervalSince1970 ?? 0
+		traceLocation.endTimestamp = endDate?.unsignedTimeIntervalSince1970 ?? 0
+		traceLocation.defaultCheckInLengthInMinutes = defaultCheckInLengthInMinutes.map { UInt32($0) } ?? 0
+
 		DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
 			completion(true)
 		}
@@ -228,6 +236,14 @@ class TraceLocationConfigurationViewModel {
 		$permanentDefaultLengthPickerIsHidden
 			.map { $0 ? UIColor.enaColor(for: .textPrimary1) : UIColor.enaColor(for: .textTint) }
 			.assign(to: &$permanentDefaultLengthValueTextColor)
+	}
+
+}
+
+extension Date {
+
+	var unsignedTimeIntervalSince1970: UInt64 {
+		UInt64(timeIntervalSince1970)
 	}
 
 }
