294161658a5d27297b8a55f95d29bf1d2ea6d589
Bastian Kohlbauer <bastian.kohlbauer@googelmail.com>
2021-03-29 13:38:53 +0200

    replace ENANavigationControllerWithFooterChild with FooterViewHandling

--- src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/ExposureSubmissionCoordinator.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/ExposureSubmissionCoordinator.swift
@@ -199,6 +199,7 @@ class ExposureSubmissionCoordinator: NSObject, ExposureSubmissionCoordinating, R
 	// MARK: Initial Screens
 
 	private func createTestResultAvailableViewController(testResult: TestResult) -> UIViewController {
+		
 		let viewModel = TestResultAvailableViewModel(
 			exposureSubmissionService: model.exposureSubmissionService,
 			onSubmissionConsentCellTap: { [weak self] isLoading in
@@ -243,7 +244,19 @@ class ExposureSubmissionCoordinator: NSObject, ExposureSubmissionCoordinating, R
 				self?.showTestResultAvailableCloseAlert()
 			}
 		)
-		return TestResultAvailableViewController(viewModel)
+		
+		let footerViewModel = FooterViewModel(
+			primaryButtonName: AppStrings.ExposureSubmissionTestResultAvailable.primaryButtonTitle,
+			isSecondaryButtonEnabled: false,
+			isSecondaryButtonHidden: true
+		)
+		
+		let topBottomContainerViewController = TopBottomContainerViewController(
+			topController: TestResultAvailableViewController(viewModel),
+			bottomController: FooterViewController(footerViewModel)
+		)
+		
+		return topBottomContainerViewController
 	}
 
 	private func createTestResultViewController(with testResult: TestResult) -> TopBottomContainerViewController<ExposureSubmissionTestResultViewController, FooterViewController> {
