7d9de6b1ac2326c827394b48414aeac302bffced
Kai Teuber <Kai.Teuber@app-developer.club>
2021-03-29 16:39:08 +0200

    added unittest for toggle cell logic

--- src/xcode/ENA/ENA/Source/Scenes/Checkin/EditCheckInTime/__tests__/EditCheckinDetailViewModelTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Checkin/EditCheckInTime/__tests__/EditCheckinDetailViewModelTests.swift
@@ -33,5 +33,38 @@ class EditCheckinDetailViewModelTests: XCTestCase {
 		XCTAssertFalse(editCheckinDetailViewModel.isEndDatePickerVisible)
 		XCTAssertLessThanOrEqual(editCheckinDetailViewModel.startDate, startDate)
 		XCTAssertLessThanOrEqual(editCheckinDetailViewModel.endDate, endDate)
+		XCTAssertEqual(editCheckinDetailViewModel.numberOfRows(.header), 1)
+		XCTAssertEqual(editCheckinDetailViewModel.numberOfRows(.description), 1)
+		XCTAssertEqual(editCheckinDetailViewModel.numberOfRows(.topCorner), 1)
+		XCTAssertEqual(editCheckinDetailViewModel.numberOfRows(.checkInStart), 1)
+		XCTAssertEqual(editCheckinDetailViewModel.numberOfRows(.startPicker), 0)
+		XCTAssertEqual(editCheckinDetailViewModel.numberOfRows(.checkInEnd), 1)
+		XCTAssertEqual(editCheckinDetailViewModel.numberOfRows(.endPicker), 0)
+		XCTAssertEqual(editCheckinDetailViewModel.numberOfRows(.bottomCorner), 1)
+		XCTAssertEqual(editCheckinDetailViewModel.numberOfRows(.notice), 1)
+		XCTAssertEqual(editCheckinDetailViewModel.numberOfRows(EditCheckinDetailViewModel.TableViewSections(rawValue: 5000)), 0)
+
 	}
+
+	func testGIVEN_EditCheckinDetailViewModel_WHEN_TogglePicker_THEN_IsVisible() {
+		// GIVEN
+		let checkIn = Checkin.mock()
+		let eventStore = MockEventStore()
+
+		let editCheckinDetailViewModel = EditCheckinDetailViewModel(
+			checkIn,
+			eventStore: eventStore
+		)
+
+		// WHEN
+		editCheckinDetailViewModel.toggleStartPicker()
+		editCheckinDetailViewModel.toggleEndPicker()
+
+		// THEN
+		XCTAssertTrue(editCheckinDetailViewModel.isStartDatePickerVisible)
+		XCTAssertTrue(editCheckinDetailViewModel.isEndDatePickerVisible)
+		XCTAssertEqual(editCheckinDetailViewModel.numberOfRows(.startPicker), 1)
+		XCTAssertEqual(editCheckinDetailViewModel.numberOfRows(.endPicker), 1)
+	}
+
 }
