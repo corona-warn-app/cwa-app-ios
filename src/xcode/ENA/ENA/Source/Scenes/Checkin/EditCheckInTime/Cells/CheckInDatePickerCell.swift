2eee16a506edb35e7ff4b0d4a491b9523dde1310
Kai Teuber <Kai.Teuber@app-developer.club>
2021-03-30 11:14:59 +0200

    more cleanup

--- src/xcode/ENA/ENA/Source/Scenes/Checkin/EditCheckInTime/Cells/CheckInDatePickerCell.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Checkin/EditCheckInTime/Cells/CheckInDatePickerCell.swift
@@ -17,7 +17,6 @@ class CheckInDatePickerCell: UITableViewCell, ReuseIdentifierProviding {
 	required init?(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
-	// MARK: - Public
 
 	// MARK: - Internal
 
@@ -44,7 +43,7 @@ class CheckInDatePickerCell: UITableViewCell, ReuseIdentifierProviding {
 
 	private func setupView() {
 		selectionStyle = .none
-		backgroundColor = .green // .enaColor(for: .cellBackground)
+		backgroundColor = .enaColor(for: .cellBackground)
 		contentView.backgroundColor = .enaColor(for: .cellBackground)
 
 		let tileView = UIView()
