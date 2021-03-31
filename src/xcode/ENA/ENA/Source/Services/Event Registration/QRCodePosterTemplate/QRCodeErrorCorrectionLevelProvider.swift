3ab3c5c0c530614cb4be893f297a9ba66f01f6ef
Naveed Khalid <nkhalid.here@gmail.com>
2021-03-31 18:47:55 +0200

    adding provider

---
+++ src/xcode/ENA/ENA/Source/Services/Event Registration/QRCodePosterTemplate/QRCodeErrorCorrectionLevelProvider.swift
@@ -0,0 +1,41 @@
+////
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+import OpenCombine
+
+ protocol QRCodeErrorCorrectionLevelProviding {
+	func errorCorrectionLevel(
+		appConfigurationProvider: AppConfigurationProviding,
+		onCompletion: @escaping((String) -> Void)
+	)
+ }
+
+class QRCodeErrorCorrectionLevelProvider {
+	private var subscriptions: Set<AnyCancellable> = []
+
+	func errorCorrectionLevel(
+		appConfigurationProvider: AppConfigurationProviding,
+		onSuccess: @escaping((String) -> Void)
+	) {
+		appConfigurationProvider.appConfiguration().sink { appConfig in
+			let qrCodeErrorCorrectionLevel = appConfig.presenceTracingParameters.qrCodeErrorCorrectionLevel
+			var mappedErrorCorrectionLevel: String = "H"
+			
+			switch qrCodeErrorCorrectionLevel {
+			case .medium:
+				mappedErrorCorrectionLevel = "M"
+			case .low:
+				mappedErrorCorrectionLevel = "L"
+			case .quantile:
+				mappedErrorCorrectionLevel = "Q"
+			case .high:
+				mappedErrorCorrectionLevel = "H"
+			default:
+				mappedErrorCorrectionLevel = "H"
+			}
+			onSuccess(mappedErrorCorrectionLevel)
+		}.store(in: &subscriptions)
+	}
+}
