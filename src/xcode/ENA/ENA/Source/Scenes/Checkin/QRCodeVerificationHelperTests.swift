00302bdfe3b984fc1b5a8f520e0f1504fe73c6f0
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-04-05 23:25:31 +0200

    Add unit tests for the QRCode verification helper

---
+++ src/xcode/ENA/ENA/Source/Scenes/Checkin/QRCodeVerificationHelperTests.swift
@@ -0,0 +1,98 @@
+////
+// 🦠 Corona-Warn-App
+//
+
+import XCTest
+@testable import ENA
+
+// IMPORTANT: THESE TESTS ARE BASED ON THE CURRENT EXPECTED REGEX, WE NEED TO UPDATE THEM IF THE REGEX IS UPDATED
+class QRCodeVerificationHelperTests: XCTestCase {
+
+    func testValidURL() {
+		let appConfig = CachedAppConfigurationMock()
+		let validHostName = "https://presence-tracing.app"
+		let validVersion = "CWA1"
+		let redirectPath = "/lorem-ipsum#/"
+		let validPayload = "/CAESRggBEi1CdXJsaW5ndG9uIENvYXQgRmFjdG9yeSBXYXJlaG91c2UgQ29ycG9yYXRpb24aDzE5OTAgVGlrb2QgUGlrZSgAMAAacQgBElswWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAARzsMSy1xQhFzKTfk5gMn3n+FODdRWGxoNcpPTMrs2Ec9ejLOKkSc6ncbI1cNWlo+LdwB9CbF64UxkAhfb7oDleGhAgd3QGMXeySb/sMUUkwZMgIgcIARAHGNsK"
+		let validURL = validHostName + redirectPath + validVersion + validPayload
+
+		let onSuccessExpectation = expectation(description: "onSuccess called")
+
+		let qrCodeVerificationHelper = QRCodeVerificationHelper()
+		qrCodeVerificationHelper.verifyQrCode(
+			qrCodeString: validURL,
+			appConfigurationProvider: appConfig,
+			onSuccess: { _ in
+				onSuccessExpectation.fulfill()
+			},
+			onError: { _ in }
+		)
+		waitForExpectations(timeout: .short)
+	}
+	
+	func testInvalidURL_noVersion() {
+		let appConfig = CachedAppConfigurationMock()
+		let validHostName = "https://presence-tracing.app"
+		let invalidVersion = "wrong"
+		let redirectPath = "/lorem-ipsum#/"
+		let validPayload = "/CAESRggBEi1CdXJsaW5ndG9uIENvYXQgRmFjdG9yeSBXYXJlaG91c2UgQ29ycG9yYXRpb24aDzE5OTAgVGlrb2QgUGlrZSgAMAAacQgBElswWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAARzsMSy1xQhFzKTfk5gMn3n+FODdRWGxoNcpPTMrs2Ec9ejLOKkSc6ncbI1cNWlo+LdwB9CbF64UxkAhfb7oDleGhAgd3QGMXeySb/sMUUkwZMgIgcIARAHGNsK"
+		let validURL = validHostName + redirectPath + invalidVersion + validPayload
+		let onErrorExpectation = expectation(description: "onError called")
+
+		let qrCodeVerificationHelper = QRCodeVerificationHelper()
+		qrCodeVerificationHelper.verifyQrCode(
+			qrCodeString: validURL,
+			appConfigurationProvider: appConfig,
+			onSuccess: { _ in },
+			onError: { error in
+				XCTAssertEqual(error, .codeNotFound, "Invalid url code should be: .codeNotFound")
+				onErrorExpectation.fulfill()
+			}
+		)
+		waitForExpectations(timeout: .short)
+	}
+	
+	func testInvalidURL_WrongHost() {
+		let appConfig = CachedAppConfigurationMock()
+		let invalidHostName = "https://wrong.app"
+		let validVersion = "CWA1"
+		let redirectPath = "/lorem-ipsum#/"
+		let validPayload = "/CAESRggBEi1CdXJsaW5ndG9uIENvYXQgRmFjdG9yeSBXYXJlaG91c2UgQ29ycG9yYXRpb24aDzE5OTAgVGlrb2QgUGlrZSgAMAAacQgBElswWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAARzsMSy1xQhFzKTfk5gMn3n+FODdRWGxoNcpPTMrs2Ec9ejLOKkSc6ncbI1cNWlo+LdwB9CbF64UxkAhfb7oDleGhAgd3QGMXeySb/sMUUkwZMgIgcIARAHGNsK"
+		let validURL = invalidHostName + redirectPath + validVersion + validPayload
+		let onErrorExpectation = expectation(description: "onError called")
+
+		let qrCodeVerificationHelper = QRCodeVerificationHelper()
+		qrCodeVerificationHelper.verifyQrCode(
+			qrCodeString: validURL,
+			appConfigurationProvider: appConfig,
+			onSuccess: { _ in },
+			onError: { error in
+				XCTAssertEqual(error, .codeNotFound, "Invalid url code should be: .codeNotFound")
+				onErrorExpectation.fulfill()
+			}
+		)
+		waitForExpectations(timeout: .short)
+	}
+	
+	func testInvalidURL_WrongPayload() {
+		let appConfig = CachedAppConfigurationMock()
+		let validHostName = "https://presence-tracing.app"
+		let validVersion = "CWA1"
+		let redirectPath = "/lorem-ipsum#/"
+		let invalidPayload = "/wrong"
+		let validURL = validHostName + redirectPath + validVersion + invalidPayload
+		let onErrorExpectation = expectation(description: "onError called")
+
+		let qrCodeVerificationHelper = QRCodeVerificationHelper()
+		qrCodeVerificationHelper.verifyQrCode(
+			qrCodeString: validURL,
+			appConfigurationProvider: appConfig,
+			onSuccess: { _ in },
+			onError: { error in
+				XCTAssertEqual(error, .invalidVendorData, "Invalid url code should be: .invalidVendorData")
+				onErrorExpectation.fulfill()
+			}
+		)
+		waitForExpectations(timeout: .short)
+	}
+}
