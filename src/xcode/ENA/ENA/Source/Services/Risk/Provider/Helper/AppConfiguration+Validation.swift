98e0cb5b101131f7b92253b019f8f10d110fde48
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-04-06 11:24:18 +0200

    Add the correct new default app config

--- src/xcode/ENA/ENA/Source/Services/Risk/Provider/Helper/AppConfiguration+Validation.swift
+++ src/xcode/ENA/ENA/Source/Services/Risk/Provider/Helper/AppConfiguration+Validation.swift
@@ -9,25 +9,25 @@ import ZIPFoundation
 extension Archive {
 
 	func extractAppConfiguration() throws -> SAP_Internal_V2_ApplicationConfigurationIOS {
-		guard let binEntry = self["export.bin"] else {
-			throw FingerprintError.entryNotFound(entryID: "export.bin")
+		guard let binEntry = self["default_app_config_ios.bin"] else {
+			throw FingerprintError.entryNotFound(entryID: "default_app_config_ios.bin")
 		}
-//		guard let hashEntry = self["export.sha256"] else {
-//			throw FingerprintError.entryNotFound(entryID: "export.sig")
-//		}
+		guard let hashEntry = self["default_app_config_ios.sha256"] else {
+			throw FingerprintError.entryNotFound(entryID: "default_app_config_ios.sha256")
+		}
 
 		do {
-			//let hash = try extractData(from: hashEntry)
+			let hash = try extractData(from: hashEntry)
 			let bin = try extractData(from: binEntry)
 
-			//let hashString = String(data: hash, encoding: .utf8)
+			let hashString = String(data: hash, encoding: .utf8)
 			let config = try SAP_Internal_V2_ApplicationConfigurationIOS(serializedData: bin)
 
 			// we currently compare the raw bin instead of the deserialized object
-//			guard /*config.fingerprint*/ bin.sha256String() == hashString else {
-//				Log.error("Fingerprint mismatch", log: .localData)
-//				throw FingerprintError.binaryNotValidated
-//			}
+			guard /*config.fingerprint*/ bin.sha256String() == hashString else {
+				Log.error("Fingerprint mismatch", log: .localData)
+				throw FingerprintError.binaryNotValidated
+			}
 
 			return config
 		} catch {
