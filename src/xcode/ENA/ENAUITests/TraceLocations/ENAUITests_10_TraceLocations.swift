23bfba5cae5fc309022e6abfaa8f84ce77ebe620
Andreas Vogel <andreas.vogel@sap.com>
2021-04-06 14:36:25 +0200

    WIP – UITest for creating a QR code

--- src/xcode/ENA/ENAUITests/TraceLocations/ENAUITests_10_TraceLocations.swift
+++ src/xcode/ENA/ENAUITests/TraceLocations/ENAUITests_10_TraceLocations.swift
@@ -71,6 +71,47 @@ class ENAUITests_10_TraceLocations: XCTestCase {
 		XCTAssertFalse(app.buttons[AccessibilityIdentifiers.General.primaryFooterButton].exists)
 	}
 	
+	func testCreateTraceLocation() throws {
+		// GIVEN
+		app.launchArguments.append(contentsOf: ["-TraceLocationsInfoScreenShown", "YES"])
+		
+		// WHEN
+		app.launch()
+		if let button = UITestHelper.scrollTo(identifier: AccessibilityIdentifiers.Home.traceLocationsCardButton, element: app, app: app) {
+			button.tap()
+		} else {
+			XCTFail("Can't find element \(AccessibilityIdentifiers.Home.traceLocationsCardButton)")
+		}
+		
+		XCTAssertTrue(app.buttons[AccessibilityLabels.localized(AppStrings.TraceLocations.Overview.addButtonTitle)].waitForExistence(timeout: .short))
+		
+		// add trace location
+		app.buttons[AccessibilityLabels.localized(AppStrings.TraceLocations.Overview.addButtonTitle)].tap()
+
+		XCTAssertTrue(app.staticTexts[AccessibilityLabels.localized(AppStrings.TraceLocations.permanent.subtitle.workplace)].waitForExistence(timeout: .short))
+		app.staticTexts[AccessibilityLabels.localized(AppStrings.TraceLocations.permanent.subtitle.workplace)].tap()
+		
+		XCTAssertTrue(app.textFields[AccessibilityIdentifiers.TraceLocation.Configuration.descriptionPlaceholder].exists)
+		XCTAssertTrue(app.textFields[AccessibilityIdentifiers.TraceLocation.Configuration.addressPlaceholder].exists)
+		
+		XCTAssertFalse(app.staticTexts[AccessibilityIdentifiers.TraceLocation.Configuration.temporaryDefaultLengthTitleLabel].exists)
+		XCTAssertFalse(app.staticTexts[AccessibilityIdentifiers.TraceLocation.Configuration.temporaryDefaultLengthFootnoteLabel].exists)
+		XCTAssertTrue(app.staticTexts[AccessibilityIdentifiers.TraceLocation.Configuration.permanentDefaultLengthTitleLabel].exists)
+		XCTAssertTrue(app.staticTexts[AccessibilityIdentifiers.TraceLocation.Configuration.permanentDefaultLengthFootnoteLabel].exists)
+		
+		let descriptionInputField = app.textFields[AccessibilityIdentifiers.TraceLocation.Configuration.descriptionPlaceholder]
+		let locationInputField = app.textFields[AccessibilityIdentifiers.TraceLocation.Configuration.addressPlaceholder]
+		
+		descriptionInputField.tap()
+		descriptionInputField.typeText("Kantine")
+		locationInputField.tap()
+		locationInputField.typeText("Berlin")
+		
+		app.buttons["AppStrings.ExposureSubmission.primaryButton"].tap()
+		acc()
+	
+	}
+	
 	/*
 	func test_screenshot_traceLocation_print_flow() throws {
 	app.launch()
@@ -105,4 +146,26 @@ class ENAUITests_10_TraceLocations: XCTestCase {
 	snapshot("tracelocation_pdf_view")
 	}
 	*/
+
+	func acc() {
+		let b1 =         AccessibilityLabels.labelsOfElement(app.buttons)
+		let b2 = AccessibilityLabels.accIdentifiersOfElement(app.buttons)
+		let b3 =      AccessibilityLabels.accLabelsOfElement(app.buttons)
+
+		let s1 =         AccessibilityLabels.labelsOfElement(app.staticTexts)
+		let s2 = AccessibilityLabels.accIdentifiersOfElement(app.staticTexts)
+		let s3 =      AccessibilityLabels.accLabelsOfElement(app.staticTexts)
+
+		let c1 =         AccessibilityLabels.labelsOfElement(app.cells)
+		let c2 = AccessibilityLabels.accIdentifiersOfElement(app.cells)
+		let c3 =      AccessibilityLabels.accLabelsOfElement(app.cells)
+
+		let t1 =         AccessibilityLabels.labelsOfElement(app.textFields)
+		let t2 = AccessibilityLabels.accIdentifiersOfElement(app.textFields)
+		let t3 =      AccessibilityLabels.accLabelsOfElement(app.textFields)
+
+		let i1 = AccessibilityLabels.labelsOfElement(app.images)
+		let i2 = AccessibilityLabels.accIdentifiersOfElement(app.images)
+		let i3 = AccessibilityLabels.accLabelsOfElement(app.images)
+	}
 }
