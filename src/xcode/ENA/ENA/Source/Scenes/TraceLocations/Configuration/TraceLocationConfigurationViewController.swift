276654222232bcf4a58d6eb7db84770c9ea17f67
Kai Teuber <Kai.Teuber@app-developer.club>
2021-04-07 10:47:36 +0200

    typo fixed

--- src/xcode/ENA/ENA/Source/Scenes/TraceLocations/Configuration/TraceLocationConfigurationViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/TraceLocations/Configuration/TraceLocationConfigurationViewController.swift
@@ -170,15 +170,15 @@ class TraceLocationConfigurationViewController: UIViewController, FooterViewHand
 			endDatePicker.preferredDatePickerStyle = .inline
 		}
 
-		startDatePicker.addTarget(self, action: #selector(selectDatPicker(sender:)), for: .editingDidBegin)
-		endDatePicker.addTarget(self, action: #selector(selectDatPicker(sender:)), for: .editingDidBegin)
+		startDatePicker.addTarget(self, action: #selector(selectDatePicker(sender:)), for: .editingDidBegin)
+		endDatePicker.addTarget(self, action: #selector(selectDatePicker(sender:)), for: .editingDidBegin)
 
 		startDatePicker.addTarget(self, action: #selector(deselectDatePicker(sender:)), for: .editingDidEnd)
 		endDatePicker.addTarget(self, action: #selector(deselectDatePicker(sender:)), for: .editingDidEnd)
 	}
 
 	@objc
-	private func selectDatPicker(sender: UIDatePicker) {
+	private func selectDatePicker(sender: UIDatePicker) {
 		currentSelectedDatePicker = sender
 	}
 
