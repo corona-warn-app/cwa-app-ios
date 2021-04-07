b030b1957c161a6f090c71ae520dc584cd4cffb4
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-04-07 09:39:27 +0200

    update URLs to match new descriptors

--- src/xcode/ENA/ENA/Source/Scenes/Checkin/QRCodeVerificationHelperTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Checkin/QRCodeVerificationHelperTests.swift
@@ -10,11 +10,10 @@ class QRCodeVerificationHelperTests: XCTestCase {
 
     func testValidURL() {
 		let appConfig = CachedAppConfigurationMock()
-		let validHostName = "https://presence-tracing.app"
-		let validVersion = "CWA1"
-		let redirectPath = "/lorem-ipsum#/"
-		let validPayload = "/CAESRggBEi1CdXJsaW5ndG9uIENvYXQgRmFjdG9yeSBXYXJlaG91c2UgQ29ycG9yYXRpb24aDzE5OTAgVGlrb2QgUGlrZSgAMAAacQgBElswWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAARzsMSy1xQhFzKTfk5gMn3n+FODdRWGxoNcpPTMrs2Ec9ejLOKkSc6ncbI1cNWlo+LdwB9CbF64UxkAhfb7oDleGhAgd3QGMXeySb/sMUUkwZMgIgcIARAHGNsK"
-		let validURL = validHostName + redirectPath + validVersion + validPayload
+		let validHostName = "https://e.coronawarn.app"
+		let validVersion = "?v=1"
+		let validPayload = "#CAESRggBEi1CdXJsaW5ndG9uIENvYXQgRmFjdG9yeSBXYXJlaG91c2UgQ29ycG9yYXRpb24aDzE5OTAgVGlrb2QgUGlrZSgAMAAacQgBElswWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAARzsMSy1xQhFzKTfk5gMn3n+FODdRWGxoNcpPTMrs2Ec9ejLOKkSc6ncbI1cNWlo+LdwB9CbF64UxkAhfb7oDleGhAgd3QGMXeySb/sMUUkwZMgIgcIARAHGNsK"
+		let validURL = validHostName + validVersion + validPayload
 
 		let onSuccessExpectation = expectation(description: "onSuccess called")
 
@@ -32,11 +31,10 @@ class QRCodeVerificationHelperTests: XCTestCase {
 	
 	func testInvalidURL_noVersion() {
 		let appConfig = CachedAppConfigurationMock()
-		let validHostName = "https://presence-tracing.app"
-		let invalidVersion = "wrong"
-		let redirectPath = "/lorem-ipsum#/"
-		let validPayload = "/CAESRggBEi1CdXJsaW5ndG9uIENvYXQgRmFjdG9yeSBXYXJlaG91c2UgQ29ycG9yYXRpb24aDzE5OTAgVGlrb2QgUGlrZSgAMAAacQgBElswWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAARzsMSy1xQhFzKTfk5gMn3n+FODdRWGxoNcpPTMrs2Ec9ejLOKkSc6ncbI1cNWlo+LdwB9CbF64UxkAhfb7oDleGhAgd3QGMXeySb/sMUUkwZMgIgcIARAHGNsK"
-		let validURL = validHostName + redirectPath + invalidVersion + validPayload
+		let validHostName = "https://e.coronawarn.app"
+		let validPayload = "#CAESJQgBEgpBZ3dheSBJbmMuGhExNTk0IERlZmZlIEF2ZW51ZSgAMAAadggBEmA4xNrp5hKJoO_yVbXfF1gS8Yc5nURhOIVLG3nUcSg8IPsI2e8JSIhg-FrHUymQ3RR80KUKb1lZjLQkfTUINUP16r6-jFDURwUlCQQi6NXCgI0rQw0a4MrVrKMbF4NzhQMaEPXDJZ2XSeO0SY43-KCQlQciBggBEAQYHA"
+		let validURL = validHostName + validPayload
+
 		let onErrorExpectation = expectation(description: "onError called")
 
 		let qrCodeVerificationHelper = QRCodeVerificationHelper()
@@ -54,11 +52,11 @@ class QRCodeVerificationHelperTests: XCTestCase {
 	
 	func testInvalidURL_WrongHost() {
 		let appConfig = CachedAppConfigurationMock()
-		let invalidHostName = "https://wrong.app"
-		let validVersion = "CWA1"
-		let redirectPath = "/lorem-ipsum#/"
-		let validPayload = "/CAESRggBEi1CdXJsaW5ndG9uIENvYXQgRmFjdG9yeSBXYXJlaG91c2UgQ29ycG9yYXRpb24aDzE5OTAgVGlrb2QgUGlrZSgAMAAacQgBElswWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAARzsMSy1xQhFzKTfk5gMn3n+FODdRWGxoNcpPTMrs2Ec9ejLOKkSc6ncbI1cNWlo+LdwB9CbF64UxkAhfb7oDleGhAgd3QGMXeySb/sMUUkwZMgIgcIARAHGNsK"
-		let validURL = invalidHostName + redirectPath + validVersion + validPayload
+		let validHostName = "https://e.wrong.app"
+		let validVersion = "?v=1"
+		let validPayload = "#CAESJQgBEgpBZ3dheSBJbmMuGhExNTk0IERlZmZlIEF2ZW51ZSgAMAAadggBEmA4xNrp5hKJoO_yVbXfF1gS8Yc5nURhOIVLG3nUcSg8IPsI2e8JSIhg-FrHUymQ3RR80KUKb1lZjLQkfTUINUP16r6-jFDURwUlCQQi6NXCgI0rQw0a4MrVrKMbF4NzhQMaEPXDJZ2XSeO0SY43-KCQlQciBggBEAQYHA"
+		let validURL = validHostName + validVersion + validPayload
+
 		let onErrorExpectation = expectation(description: "onError called")
 
 		let qrCodeVerificationHelper = QRCodeVerificationHelper()
@@ -76,11 +74,10 @@ class QRCodeVerificationHelperTests: XCTestCase {
 	
 	func testInvalidURL_WrongPayload() {
 		let appConfig = CachedAppConfigurationMock()
-		let validHostName = "https://presence-tracing.app"
-		let validVersion = "CWA1"
-		let redirectPath = "/lorem-ipsum#/"
-		let invalidPayload = "/wrong"
-		let validURL = validHostName + redirectPath + validVersion + invalidPayload
+		let validHostName = "https://e.coronawarn.app"
+		let validVersion = "?v=1"
+		let validPayload = "#Wrong_payload"
+		let validURL = validHostName + validVersion + validPayload
 		let onErrorExpectation = expectation(description: "onError called")
 
 		let qrCodeVerificationHelper = QRCodeVerificationHelper()
