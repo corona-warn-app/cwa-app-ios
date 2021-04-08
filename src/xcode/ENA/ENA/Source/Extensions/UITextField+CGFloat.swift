1fea5660f64d0dac696788a9209ef1634c84b90c
Naveed Khalid <nkhalid.here@gmail.com>
2021-04-08 13:14:20 +0200

    changes to extension

--- src/xcode/ENA/ENA/Source/Extensions/UITextField+CGFloat.swift
+++ src/xcode/ENA/ENA/Source/Extensions/UITextField+CGFloat.swift
@@ -3,3 +3,11 @@
 //
 
 import Foundation
+import UIKit
+
+extension UITextField {
+	var cgFloatValue: CGFloat {
+		guard let doubleValue = NumberFormatter().number(from: self.text ?? "") else { return 0.0 }
+		return CGFloat(truncating: doubleValue)
+	}
+}
