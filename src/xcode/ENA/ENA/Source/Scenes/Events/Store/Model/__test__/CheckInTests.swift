02e1e3c0d8e3e3547a0ecc93761db1cdaf2d1ee5
Nick Guendling <hello@nickguendling.com>
2021-04-09 16:52:27 +0200

    Implement additional test

--- src/xcode/ENA/ENA/Source/Scenes/Events/Store/Model/__test__/CheckInTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Events/Store/Model/__test__/CheckInTests.swift
@@ -34,6 +34,81 @@ class CheckInTests: XCTestCase {
 		}
 	}
 
+	func testPrepareCheckinForSubmission() throws {
+		let traceLocationId = "asdf".data(using: .utf8) ?? Data()
+
+		// create a mock checkin and check preconditions
+		for i in stride(from: 1, to: 600, by: 10) {
+			let checkin = Checkin.mock(
+				traceLocationId: traceLocationId,
+				checkinStartDate: Date(timeIntervalSinceNow: -(Double(i * 10))),
+				checkinEndDate: Date()
+			)
+			XCTAssertGreaterThan(checkin.checkinEndDate, checkin.checkinStartDate)
+
+			let transformedCheckin = checkin.prepareForSubmission()
+			XCTAssertGreaterThanOrEqual(transformedCheckin.endIntervalNumber, transformedCheckin.startIntervalNumber)
+
+			// check calculated interval numbers and compare to start/end dates
+			XCTAssertEqual(
+				TimeInterval(transformedCheckin.startIntervalNumber * 600),
+				checkin.checkinStartDate.timeIntervalSince1970,
+				accuracy: 600
+			)
+
+			XCTAssertEqual(
+				TimeInterval(transformedCheckin.endIntervalNumber * 600),
+				checkin.checkinEndDate.timeIntervalSince1970,
+				accuracy: 600
+			)
+
+			XCTAssertEqual(transformedCheckin.locationID, traceLocationId)
+		}
+	}
+
+	func testCheckinTransformationWithTraceLocationStartAndEndDate() throws {
+		// create a mock checkin and check preconditions
+		let startDate = Date(timeIntervalSinceNow: -200)
+		let endDate = Date(timeIntervalSinceNow: 200)
+
+		let checkin = Checkin.mock(
+			traceLocationId: "traceLocationId".data(using: .utf8) ?? Data(),
+			traceLocationVersion: 17,
+			traceLocationDescription: "Description",
+			traceLocationAddress: "Address",
+			traceLocationStartDate: startDate,
+			traceLocationEndDate: endDate
+		)
+
+		let protobufTraceLocation = checkin.traceLocation
+
+		XCTAssertEqual(protobufTraceLocation.version, 17)
+		XCTAssertEqual(protobufTraceLocation.description_p, "Description")
+		XCTAssertEqual(protobufTraceLocation.address, "Address")
+		XCTAssertEqual(protobufTraceLocation.startTimestamp, UInt64(startDate.timeIntervalSince1970))
+		XCTAssertEqual(protobufTraceLocation.endTimestamp, UInt64(endDate.timeIntervalSince1970))
+	}
+
+	func testCheckinTransformationWithoutTraceLocationStartAndEndDate() throws {
+		// create a mock checkin and check preconditions
+		let checkin = Checkin.mock(
+			traceLocationId: "traceLocationId".data(using: .utf8) ?? Data(),
+			traceLocationVersion: 17,
+			traceLocationDescription: "Description",
+			traceLocationAddress: "Address",
+			traceLocationStartDate: nil,
+			traceLocationEndDate: nil
+		)
+
+		let protobufTraceLocation = checkin.traceLocation
+
+		XCTAssertEqual(protobufTraceLocation.version, 17)
+		XCTAssertEqual(protobufTraceLocation.description_p, "Description")
+		XCTAssertEqual(protobufTraceLocation.address, "Address")
+		XCTAssertEqual(protobufTraceLocation.startTimestamp, 0)
+		XCTAssertEqual(protobufTraceLocation.endTimestamp, 0)
+	}
+
 	// MARK: - Private
 
 	private lazy var testCasesWithConfiguration: CheckinTestCasesWithConfiguration = {
