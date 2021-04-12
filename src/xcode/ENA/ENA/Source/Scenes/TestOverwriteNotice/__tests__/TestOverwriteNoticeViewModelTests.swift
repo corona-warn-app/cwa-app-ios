027075b8039f2ad89f0d2831adb5b788023410fd
Kai Teuber <Kai.Teuber@app-developer.club>
2021-04-12 17:14:29 +0200

    added a Unittest

---
+++ src/xcode/ENA/ENA/Source/Scenes/TestOverwriteNotice/__tests__/TestOverwriteNoticeViewModelTests.swift
@@ -0,0 +1,31 @@
+////
+// 🦠 Corona-Warn-App
+//
+
+import XCTest
+@testable import ENA
+
+class TestOverwriteNoticeViewModelTests: XCTestCase {
+
+	func testGIVEN_ViewModelWithPcr_THEN_IsInitializedCorrect() {
+		// GIVEN
+		let viewModel = TestOverwriteNoticeViewModel(.pcr)
+
+		// THEN
+		XCTAssertEqual(viewModel.title, AppStrings.ExposureSubmission.OverwriteNotice.title)
+		XCTAssertEqual(viewModel.dynamicTableViewModel.numberOfSection, 1)
+		XCTAssertEqual(viewModel.dynamicTableViewModel.numberOfRows(section: 0), 2)
+	}
+
+	func testGIVEN_ViewModelWithAntigen_THEN_IsInitializedCorrect() {
+		// GIVEN
+		let viewModel = TestOverwriteNoticeViewModel(.antigen)
+
+		// THEN
+		XCTAssertEqual(viewModel.title, AppStrings.ExposureSubmission.OverwriteNotice.title)
+		XCTAssertEqual(viewModel.dynamicTableViewModel.numberOfSection, 1)
+		XCTAssertEqual(viewModel.dynamicTableViewModel.numberOfRows(section: 0), 2)
+	}
+
+
+}
