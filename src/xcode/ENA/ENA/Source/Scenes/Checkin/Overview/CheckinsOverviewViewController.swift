d2c1dd5773036fe46d29a80f9adb7e6c5015209a
Nick Guendling <hello@nickguendling.com>
2021-04-12 14:10:47 +0200

    Improve naming

--- src/xcode/ENA/ENA/Source/Scenes/Checkin/Overview/CheckinsOverviewViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Checkin/Overview/CheckinsOverviewViewController.swift
@@ -48,7 +48,7 @@ class CheckinsOverviewViewController: UITableViewController, FooterViewHandling 
 		viewModel.$triggerReload
 			.receive(on: DispatchQueue.main.ocombine)
 			.sink { [weak self] _ in
-				guard let self = self, self.shouldReload else { return }
+				guard let self = self, self.isAllowedToReload else { return }
 
 				self.tableView.reloadData()
 				self.updateEmptyState()
@@ -117,12 +117,12 @@ class CheckinsOverviewViewController: UITableViewController, FooterViewHandling 
 			confirmAction: { [weak self] in
 				guard let self = self else { return }
 
-				self.shouldReload = false
+				self.isAllowedToReload = false
 				self.viewModel.removeEntry(at: indexPath)
 				tableView.performBatchUpdates({
 					tableView.deleteRows(at: [indexPath], with: .automatic)
 				}, completion: { _ in
-					self.shouldReload = true
+					self.isAllowedToReload = true
 					self.viewModel.triggerReload = true
 
 					if self.viewModel.isEmpty {
@@ -164,7 +164,7 @@ class CheckinsOverviewViewController: UITableViewController, FooterViewHandling 
 
 	private var subscriptions = [AnyCancellable]()
 
-	private var shouldReload = true
+	private var isAllowedToReload = true
 	private var addEntryCellModel = AddCheckinCellModel()
 	private var missingPermissionsCellModel = MissingPermissionsCellModel()
 
@@ -322,7 +322,7 @@ class CheckinsOverviewViewController: UITableViewController, FooterViewHandling 
 
 				let numberOfRows = self.viewModel.numberOfRows(in: CheckinsOverviewViewModel.Section.entries.rawValue)
 
-				self.shouldReload = false
+				self.isAllowedToReload = false
 				self.viewModel.removeAll()
 				self.tableView.performBatchUpdates({
 					self.tableView.deleteRows(
@@ -332,7 +332,7 @@ class CheckinsOverviewViewController: UITableViewController, FooterViewHandling 
 						with: .automatic
 					)
 				}, completion: { _ in
-					self.shouldReload = true
+					self.isAllowedToReload = true
 
 					self.setEditing(false, animated: true)
 					self.updateEmptyState()
--- src/xcode/ENA/ENA/Source/Scenes/Checkin/Overview/__test__/CheckinsOverviewViewModelTest.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Checkin/Overview/__test__/CheckinsOverviewViewModelTest.swift
@@ -344,7 +344,7 @@ class CheckinsOverviewViewModelTest: XCTestCase {
 	}
 
 	func testUpdateForCameraPermission() throws {
-		let reloadExpectation = expectation(description: "shouldReload published")
+		let reloadExpectation = expectation(description: "triggerReload published")
 		reloadExpectation.expectedFulfillmentCount = 2 // initial call + update for camera permission
 
 		let eventStore = MockEventStore()
@@ -417,7 +417,7 @@ class CheckinsOverviewViewModelTest: XCTestCase {
 			onEntryCellTap: { _ in }
 		)
 
-		let reloadExpectation = expectation(description: "shouldReload published")
+		let reloadExpectation = expectation(description: "triggerReload published")
 		reloadExpectation.expectedFulfillmentCount = 2 // initial call + update for added checkin
 		let cancellable = viewModel.$triggerReload
 			.sink { _ in
@@ -450,7 +450,7 @@ class CheckinsOverviewViewModelTest: XCTestCase {
 			onEntryCellTap: { _ in }
 		)
 
-		let reloadExpectation = expectation(description: "shouldReload published")
+		let reloadExpectation = expectation(description: "triggerReload published")
 		reloadExpectation.expectedFulfillmentCount = 2 // initial call + update for removed checkin
 		let cancellable = viewModel.$triggerReload
 			.sink { _ in
@@ -492,7 +492,7 @@ class CheckinsOverviewViewModelTest: XCTestCase {
 			onUpdateExpectation.fulfill()
 		}
 
-		let reloadExpectation = expectation(description: "shouldReload published only once")
+		let reloadExpectation = expectation(description: "triggerReload published only once")
 		reloadExpectation.expectedFulfillmentCount = 1
 		let cancellable = viewModel.$triggerReload
 			.sink { _ in
