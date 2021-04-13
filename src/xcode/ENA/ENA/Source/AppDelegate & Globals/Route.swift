50bd9bef5290053c0d524ef530aab743c24edd7b
Kai Teuber <Kai.Teuber@app-developer.club>
2021-04-13 18:15:04 +0200

    moved extract of qr code payload to Route enum for reuse

--- src/xcode/ENA/ENA/Source/AppDelegate & Globals/Route.swift
+++ src/xcode/ENA/ENA/Source/AppDelegate & Globals/Route.swift
@@ -24,7 +24,33 @@ enum Route {
 
 		switch host {
 		case "s.coronawarn.app":
-			self = .rapidAntigen(url.absoluteString)
+			guard let payloadUrl = components?.fragment,
+				  let candidate = components?.query,
+				  candidate.count == 3 else {
+				return nil
+			}
+
+			// extract payload
+			guard let testInformation = AntigenTestInformation(payload: payloadUrl),
+				  testInformation.guid.range(
+					of: #"^[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}$"#,
+					options: .regularExpression
+				  ) != nil,
+				  testInformation.guid.count == 36,
+				  testInformation.timestamp >= 0
+			else {
+				return nil
+			}
+
+			// Check in case the dateOfBirth is available, that it is in the correct format
+			if let dateOfBirth = testInformation.dateOfBirth {
+				let dateFormatter = DateFormatter()
+				dateFormatter.dateFormat = "yyyy-MM-dd"
+				guard dateFormatter.date(from: dateOfBirth) != nil else {
+					return nil
+				}
+			}
+			self = .rapidAntigen(.antigen(testInformation))
 
 		case "e.coronawarn.app":
 			self = .checkin(url.absoluteString)
@@ -38,6 +64,6 @@ enum Route {
 	// MARK: - Internal
 
 	case checkin(String)
-	case rapidAntigen(String)
+	case rapidAntigen(CoronaTestQRCodeInformation)
 
 }
