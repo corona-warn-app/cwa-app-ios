f21f888ea7a838e9039be15ad7d7ecf84e5bbb9c
Nick Guendling <hello@nickguendling.com>
2021-04-15 08:42:01 +0200

    Improve subscription code formatting

--- src/xcode/ENA/ENA/Source/Scenes/Home/Cells/TestResult/HomeTestResultTableViewCell.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Home/Cells/TestResult/HomeTestResultTableViewCell.swift
@@ -37,7 +37,9 @@ final class HomeTestResultTableViewCell: UITableViewCell {
 	// MARK: - Internal
 
 	func configure(with cellModel: HomeTestResultCellModel, onPrimaryAction: @escaping () -> Void) {
-		cellModel.$title.assign(to: \.text, on: titleLabel).store(in: &subscriptions)
+		cellModel.$title
+			.assign(to: \.text, on: titleLabel)
+			.store(in: &subscriptions)
 
 		cellModel.$subtitle
 			.sink { [weak self] in
@@ -46,7 +48,9 @@ final class HomeTestResultTableViewCell: UITableViewCell {
 			}
 			.store(in: &subscriptions)
 
-		cellModel.$description.assign(to: \.text, on: descriptionLabel).store(in: &subscriptions)
+		cellModel.$description
+			.assign(to: \.text, on: descriptionLabel)
+			.store(in: &subscriptions)
 
 		cellModel.$footnote
 			.sink { [weak self] in
@@ -55,7 +59,9 @@ final class HomeTestResultTableViewCell: UITableViewCell {
 			}
 			.store(in: &subscriptions)
 
-		cellModel.$image.assign(to: \.image, on: illustrationView).store(in: &subscriptions)
+		cellModel.$image
+			.assign(to: \.image, on: illustrationView)
+			.store(in: &subscriptions)
 
 		cellModel.$buttonTitle
 			.sink { [weak self] buttonTitle in
@@ -67,13 +73,24 @@ final class HomeTestResultTableViewCell: UITableViewCell {
 			.assign(to: \.isHidden, on: negativeDiagnosisStackView)
 			.store(in: &subscriptions)
 
-		cellModel.$isActivityIndicatorHidden.assign(to: \.isHidden, on: activityIndicator).store(in: &subscriptions)
-		cellModel.$isActivityIndicatorHidden.map({ !$0 }).assign(to: \.isHidden, on: illustrationView).store(in: &subscriptions)
+		cellModel.$isActivityIndicatorHidden
+			.assign(to: \.isHidden, on: activityIndicator)
+			.store(in: &subscriptions)
+		cellModel.$isActivityIndicatorHidden
+			.map({ !$0 })
+			.assign(to: \.isHidden, on: illustrationView)
+			.store(in: &subscriptions)
 
-		cellModel.$isUserInteractionEnabled.assign(to: \.isUserInteractionEnabled, on: self).store(in: &subscriptions)
-		cellModel.$isUserInteractionEnabled.assign(to: \.isEnabled, on: button).store(in: &subscriptions)
+		cellModel.$isUserInteractionEnabled
+			.assign(to: \.isUserInteractionEnabled, on: self)
+			.store(in: &subscriptions)
+		cellModel.$isUserInteractionEnabled
+			.assign(to: \.isEnabled, on: button)
+			.store(in: &subscriptions)
 
-		cellModel.$accessibilityIdentifier.assign(to: \.accessibilityIdentifier, on: button).store(in: &subscriptions)
+		cellModel.$accessibilityIdentifier
+			.assign(to: \.accessibilityIdentifier, on: button)
+			.store(in: &subscriptions)
 
 		self.onPrimaryAction = onPrimaryAction
 
