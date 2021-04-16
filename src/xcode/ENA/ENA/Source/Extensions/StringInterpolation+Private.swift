1855f6f356d86f8585d839191d55c8bf224bee0c
Pascal Brause <pascal@brause.haus>
2021-04-16 13:39:37 +0200

    add comment

--- src/xcode/ENA/ENA/Source/Extensions/StringInterpolation+Private.swift
+++ src/xcode/ENA/ENA/Source/Extensions/StringInterpolation+Private.swift
@@ -4,6 +4,7 @@
 
 import Foundation
 	
+/// If we prefix the content of the string interpolation with 'private', the content will be replaced by '🙈🙉🙊' to ensure no private infomation is logged.
 extension String.StringInterpolation {
 	mutating func appendInterpolation<T>(private value: T) {
 		
