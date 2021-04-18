f66c65ccf791914da6c944b9280dc4276da35d0d
Carsten Knoblich <carsten@staticline.de>
2021-04-18 23:40:08 +0200

    refactoring environment switch for CI

--- scripts/switch_to_dev_env.sh
+++ scripts/switch_to_dev_env.sh
@@ -1,43 +1,13 @@
 #!/usr/bin/env zsh
+
 set -euo pipefail
 
-script_dir=${0:a:h}
-cd $script_dir/../src/xcode/ENA/ENA/Resources/ServerEnvironment
+SCRIPT_PATH=${0:A:h}
+ENV_PATH="${SCRIPT_PATH}/../src/xcode/ENA/ENA/Resources/Environment/Environments.test.json"
 
-serverEnvironments="{
-	\"ServerEnvironments\":[
-		{
-			\"name\": \"Default\",
-			\"distributionURL\": \"${WRUXD_SECRET_DIST_URL}\",
-			\"submissionURL\": \"${WRUXD_SECRET_SUBM_URL}\",
-			\"verificationURL\": \"${WRUXD_SECRET_VERIF_URL}\",
-			\"dataDonationURL\": \"${SECRET_DATAD_URL}\"
-		},
-		{
-			\"name\": \"WRU-XA\",
-			\"distributionURL\": \"${WRUXA_SECRET_DIST_URL}\",
-			\"submissionURL\": \"${WRUXA_SECRET_SUBM_URL}\",
-			\"verificationURL\": \"${WRUXA_SECRET_VERIF_URL}\",
-			\"dataDonationURL\": \"${SECRET_DATAD_URL}\"
-		},
-		{
-			\"name\": \"WRU\",
-			\"distributionURL\": \"${SECRET_DIST_URL}\",
-			\"submissionURL\": \"${SECRET_SUBM_URL}\",
-			\"verificationURL\": \"${SECRET_VERIF_URL}\",
-			\"dataDonationURL\": \"${SECRET_DATAD_URL}\"
-		},
-		{
-			\"name\": \"INT\",
-			\"distributionURL\": \"${INT_SECRET_DIST_URL}\",
-			\"submissionURL\": \"${INT_SECRET_SUBM_URL}\",
-			\"verificationURL\": \"${INT_SECRET_VERIF_URL}\",
-			\"dataDonationURL\": \"${SECRET_DATAD_URL}\"
-		}
-	]
-}
-"
-
-echo $serverEnvironments > ServerEnvironments.json
-
-cd -
+# Fetch CI configuration
+curl \
+  --header "Authorization: token ${GITHUB_ACCESS_TOKEN}" \
+  --header "Accept: application/vnd.github.v3.raw" \
+  --remote-name \
+  --location "${ENVIRONMENTS_DOWNLOAD_URL}${ENV_PROP_DOWNLOAD_FILENAME}" > ${ENV_PATH}
