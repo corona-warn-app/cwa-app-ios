644b4515eae2d622da1caa2789c93ec9e13b7723
Carsten Knoblich <carsten@staticline.de>
2021-04-20 12:19:03 +0200

    simplified switch env script

--- scripts/switch_to_dev_env.sh
+++ scripts/switch_to_dev_env.sh
@@ -3,11 +3,11 @@
 set -euo pipefail
 
 SCRIPT_PATH=${0:A:h}
-ENV_PATH="${SCRIPT_PATH}/../src/xcode/ENA/ENA/Resources/Environment/Environments.test.json"
+ENV_PATH="${SCRIPT_PATH}/../src/xcode/ENA/ENA/Resources/Environment/Environments.json"
 
-# Fetch CI configuration
+# fetch CI configuration and overwrite local environments file
 curl \
   --header "Authorization: token ${GITHUB_ACCESS_TOKEN}" \
   --header "Accept: application/vnd.github.v3.raw" \
   --remote-name \
-  --location "${ENVIRONMENTS_DOWNLOAD_URL}${ENV_PROP_DOWNLOAD_FILENAME}" > ${ENV_PATH}
+  --location "${ENVIRONMENTS_FILE_URL}" > ${ENV_PATH}
