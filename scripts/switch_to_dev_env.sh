5d73ffc3ef3692b26db02c14f52a10f3c4f8904b
Katharina Seiz <katharina.seiz@sap.com>
2021-04-21 16:39:10 +0200

    only allow WRU as test environment

--- scripts/switch_to_dev_env.sh
+++ scripts/switch_to_dev_env.sh
@@ -8,30 +8,9 @@ serverEnvironments="{
 	\"ServerEnvironments\":[
 		{
 			\"name\": \"Default\",
-			\"distributionURL\": \"${WRUXD_SECRET_DIST_URL}\",
-			\"submissionURL\": \"${WRUXD_SECRET_SUBM_URL}\",
-			\"verificationURL\": \"${WRUXD_SECRET_VERIF_URL}\",
-			\"dataDonationURL\": \"${SECRET_DATAD_URL}\"
-		},
-		{
-			\"name\": \"WRU-XA\",
-			\"distributionURL\": \"${WRUXA_SECRET_DIST_URL}\",
-			\"submissionURL\": \"${WRUXA_SECRET_SUBM_URL}\",
-			\"verificationURL\": \"${WRUXA_SECRET_VERIF_URL}\",
-			\"dataDonationURL\": \"${SECRET_DATAD_URL}\"
-		},
-		{
-			\"name\": \"WRU\",
 			\"distributionURL\": \"${SECRET_DIST_URL}\",
 			\"submissionURL\": \"${SECRET_SUBM_URL}\",
 			\"verificationURL\": \"${SECRET_VERIF_URL}\",
-			\"dataDonationURL\": \"${SECRET_DATAD_URL}\"
-		},
-		{
-			\"name\": \"INT\",
-			\"distributionURL\": \"${INT_SECRET_DIST_URL}\",
-			\"submissionURL\": \"${INT_SECRET_SUBM_URL}\",
-			\"verificationURL\": \"${INT_SECRET_VERIF_URL}\",
 			\"dataDonationURL\": \"${SECRET_DATAD_URL}\"
 		}
 	]
