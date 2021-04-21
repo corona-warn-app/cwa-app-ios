c2478ed7c64452ea09be1bdf444505dae1304245
Kai Teuber <Kai.Teuber@app-developer.club>
2021-04-21 17:23:10 +0200

    added information about justDate formatter

--- src/xcode/ENA/ENA/Source/Extensions/ISO8601DateFormatter+extentions.swift
+++ src/xcode/ENA/ENA/Source/Extensions/ISO8601DateFormatter+extentions.swift
@@ -6,6 +6,9 @@ import Foundation
 
 extension ISO8601DateFormatter {
 
+	// can get used to handle ISO8610 formatted date strings without time informations
+	// aka "1981-11-01"
+	
 	static let justDate: ISO8601DateFormatter = {
 		let isoFormatter = ISO8601DateFormatter()
 		isoFormatter.formatOptions = [.withFullDate]
