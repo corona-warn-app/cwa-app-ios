ac4d64c88444dfed80cd2ea7e1ebc2557cfafaa6
Kai Teuber <Kai.Teuber@app-developer.club>
2021-04-23 15:37:15 +0200

    transparent navigation bar

--- src/xcode/ENA/ENA/Source/Scenes/AntigenTestProfile/AntigenTestProfileViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/AntigenTestProfile/AntigenTestProfileViewController.swift
@@ -5,9 +5,9 @@
 import UIKit
 
 class AntigenTestProfileViewController: UIViewController, FooterViewHandling, DismissHandling {
-
+	
 	// MARK: - Init
-
+	
 	init(
 		store: AntigenTestProfileStoring,
 		didTapContinue: @escaping (@escaping (Bool) -> Void) -> Void,
@@ -18,36 +18,29 @@ class AntigenTestProfileViewController: UIViewController, FooterViewHandling, Di
 		self.didTapContinue = didTapContinue
 		self.didTapDeleteProfile = didTapDeleteProfile
 		self.dismiss = dismiss
-
+		
 		super.init(nibName: nil, bundle: nil)
 	}
-
+	
 	@available(*, unavailable)
 	required init?(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
-
+	
 	// MARK: - Overrides
-
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
-		let logoImage = UIImage(imageLiteralResourceName: "Corona-Warn-App").withRenderingMode(.alwaysTemplate)
-		let logoImageView = UIImageView(image: logoImage)
-		logoImageView.tintColor = .enaColor(for: .textContrast)
-
-		parent?.navigationController?.navigationBar.tintColor = .white
-		parent?.navigationItem.titleView = logoImageView
-		parent?.navigationItem.rightBarButtonItem = dismissHandlingCloseBarButton
-
-		navigationController?.viewControllers = [navigationController?.viewControllers.first, navigationController?.viewControllers.last].compactMap { $0 }
+		
+		setupNavigationBar()
+		setupBackground()
 	}
-
+	
 	// MARK: - Protocol FooterViewHandling
-
+	
 	func didTapFooterViewButton(_ type: FooterViewModel.ButtonType) {
 		switch type {
-
+		
 		case .primary:
 			didTapContinue({ _ in Log.debug("is loading closure here") })
 		case .secondary:
@@ -55,27 +48,62 @@ class AntigenTestProfileViewController: UIViewController, FooterViewHandling, Di
 			didTapDeleteProfile()
 		}
 	}
-
+	
 	// MARK: - Protocol DismissHandling
-
+	
 	func wasAttemptedToBeDismissed() {
 		dismiss()
 	}
-
+	
 	// MARK: - Public
-
+	
 	// MARK: - Internal
-
+	
 	// MARK: - Private
 	
 	private let viewModel: AntigenTestProfileViewModel
 	private let didTapContinue: (@escaping (Bool) -> Void) -> Void
 	private let didTapDeleteProfile: () -> Void
 	private let dismiss: () -> Void
-
+	
 	@objc
 	private func backToRootViewController() {
 		didTapDeleteProfile()
 	}
-
+	
+	private func setupNavigationBar() {
+		let logoImage = UIImage(imageLiteralResourceName: "Corona-Warn-App").withRenderingMode(.alwaysTemplate)
+		let logoImageView = UIImageView(image: logoImage)
+		logoImageView.tintColor = .enaColor(for: .textContrast)
+		
+		parent?.navigationController?.navigationBar.tintColor = .white
+		parent?.navigationItem.titleView = logoImageView
+		parent?.navigationItem.rightBarButtonItem = dismissHandlingCloseBarButton
+		
+		// remove previous view controllers from the stack, back button will
+		navigationController?.viewControllers = [navigationController?.viewControllers.first, navigationController?.viewControllers.last].compactMap { $0 }
+		
+		// create a transparent navigation bar
+		let emptyImage = UIImage()
+		navigationController?.navigationBar.setBackgroundImage(emptyImage, for: .default)
+		navigationController?.navigationBar.shadowImage = emptyImage
+		navigationController?.navigationBar.isTranslucent = true
+		navigationController?.view.backgroundColor = .clear
+	}
+	
+	private func setupBackground() {
+		let gradientBackgroundView = GradientBackgroundView()
+		gradientBackgroundView.translatesAutoresizingMaskIntoConstraints = false
+		view.addSubview(gradientBackgroundView)
+		
+		NSLayoutConstraint.activate(
+			[
+				gradientBackgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+				gradientBackgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+				gradientBackgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+				gradientBackgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+			]
+		)
+	}
+	
 }
