261f2c404673aba6e832eedd04e3bad072459690
Artur Friesen <mail@arturfriesen.de>
2021-04-27 11:29:15 +0200

    Add create profile tile.

--- src/xcode/ENA/ENA/Source/Views/ExposureSubmission/ExposureSubmissionImageCardCell.swift
+++ src/xcode/ENA/ENA/Source/Views/ExposureSubmission/ExposureSubmissionImageCardCell.swift
@@ -8,6 +8,11 @@ import UIKit
 @IBDesignable
 class ExposureSubmissionImageCardCell: UITableViewCell {
 
+	enum ImageLayout {
+		case right
+		case center
+	}
+	
 	// MARK: - Overrides
 
 	override func setHighlighted(_ highlighted: Bool, animated: Bool) {
@@ -42,6 +47,7 @@ class ExposureSubmissionImageCardCell: UITableViewCell {
 		description: String,
 		attributedDescription: NSAttributedString? = nil,
 		image: UIImage?,
+		imageLayout: ImageLayout = .right,
 		backgroundView bgView: UIView? = nil,
 		textColor: UIColor? = nil,
 		accessibilityIdentifier: String?
@@ -64,6 +70,17 @@ class ExposureSubmissionImageCardCell: UITableViewCell {
 			descriptionLabel.attributedText = attributedText
 		}
 
+		switch imageLayout {
+		case .right:
+			imageAndDescriptionStackView.addArrangedSubview(descriptionLabelContainer)
+			imageAndDescriptionStackView.addArrangedSubview(illustrationContainer)
+			imageAndDescriptionStackView.axis = .horizontal
+		case .center:
+			imageAndDescriptionStackView.addArrangedSubview(illustrationContainer)
+			imageAndDescriptionStackView.addArrangedSubview(descriptionLabelContainer)
+			imageAndDescriptionStackView.axis = .vertical
+		}
+
 		cardView.accessibilityLabel = "\(title)\n\n\(description) \(attributedDescription?.string ?? "")"
 		cardView.accessibilityIdentifier = accessibilityIdentifier
 	}
@@ -75,6 +92,9 @@ class ExposureSubmissionImageCardCell: UITableViewCell {
 	@IBOutlet private var descriptionLabel: ENALabel!
 	@IBOutlet private var illustrationView: UIImageView!
 	@IBOutlet private var cardBackgroundContainer: UIView!
+	@IBOutlet private var imageAndDescriptionStackView: UIStackView!
+	@IBOutlet private var descriptionLabelContainer: UIView!
+	@IBOutlet private var illustrationContainer: UIView!
 
 	private var highlightView: UIView!
 
