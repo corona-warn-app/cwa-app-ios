390bc336b475d225baab25b8030425613db3fdef
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-04-28 17:00:07 +0200

    update code to handle properties inside enums

--- src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/ExposureSubmissionCoordinator.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/ExposureSubmissionCoordinator.swift
@@ -48,7 +48,8 @@ class ExposureSubmissionCoordinator: NSObject, RequiresAppDependencies {
 					let qrInfoScreen = self.makeQRInfoScreen(supportedCountries: supportedCountries, testInformation: testInformation)
 					self.start(with: qrInfoScreen)
 				case let .failure(qrCodeError):
-					if qrCodeError == .invalidTestCode {
+					switch qrCodeError {
+					case .invalidTestCode:
 						self.showRATInvalidQQCode()
 					}
 				}
