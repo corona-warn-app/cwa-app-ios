01da05d11c4285239684d239742c810a057fc81d
Nick Guendling <hello@nickguendling.com>
2021-04-29 18:44:29 +0200

    Update store when observable object changes

--- src/xcode/ENA/ENA/Source/Scenes/HealthCertificate/HealthCertificateService.swift
+++ src/xcode/ENA/ENA/Source/Scenes/HealthCertificate/HealthCertificateService.swift
@@ -14,12 +14,18 @@ class HealthCertificateService {
 	) {
 		self.store = store
 
-		setup()
+		updatePublishersFromStore()
 	}
 
 	// MARK: - Internal
 
-	@OpenCombine.Published /*private(set)*/ var healthCertifiedPersons: [HealthCertifiedPerson] = []
+	@OpenCombine.Published var healthCertifiedPersons: [HealthCertifiedPerson] = [] {
+		didSet {
+			store.healthCertifiedPersons = healthCertifiedPersons
+
+			updateSubscriptions()
+		}
+	}
 
 	func updatePublishersFromStore() {
 		Log.info("[HealthCertificateService] Updating publishers from store", log: .api)
@@ -30,17 +36,21 @@ class HealthCertificateService {
 	// MARK: - Private
 
 	private var store: HealthCertificateStoring
-
 	private var subscriptions = Set<AnyCancellable>()
 
-	private func setup() {
-		updatePublishersFromStore()
+	private func updateSubscriptions() {
+		subscriptions = []
 
-		$healthCertifiedPersons
-			.sink { [weak self] healthCertifiedPersons in
-				self?.store.healthCertifiedPersons = healthCertifiedPersons
-			}
-			.store(in: &subscriptions)
+		healthCertifiedPersons.forEach { healthCertifiedPerson in
+			healthCertifiedPerson.objectWillChange
+				.receive(on: DispatchQueue.main.ocombine)
+				.sink { [weak self] in
+					guard let self = self else { return }
+					// Trigger publisher to inform subscribers and update store
+					self.healthCertifiedPersons = self.healthCertifiedPersons
+				}
+				.store(in: &subscriptions)
+		}
 	}
 
 }
