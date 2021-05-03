54e2c2835515eac4f43a54e34abab0a3a5566c14
Pascal Brause <pascal@brause.haus>
2021-05-03 17:12:03 +0200

    add waitforexistence

--- src/xcode/ENA/ENAUITests/ExposureSubmission/ExposureSubmissionUITests.swift
+++ src/xcode/ENA/ENAUITests/ExposureSubmission/ExposureSubmissionUITests.swift
@@ -353,6 +353,7 @@ class ENAUITests_04_ExposureSubmissionUITests: XCTestCase {
 		type(app, text: "qwdzxcsrhe")
 
 		// Click continue button.
+		XCTAssertTrue(app.buttons["AppStrings.ExposureSubmission.primaryButton"].waitForExistence(timeout: .medium))
 		XCTAssertTrue(app.buttons["AppStrings.ExposureSubmission.primaryButton"].isEnabled)
 		app.buttons["AppStrings.ExposureSubmission.primaryButton"].tap()
 
