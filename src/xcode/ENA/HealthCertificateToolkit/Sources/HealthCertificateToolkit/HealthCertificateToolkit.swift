74fcfbad3613a8dfab6705c466cf48d9c305b254
Artur Friesen <mail@arturfriesen.de>
2021-05-04 17:41:14 +0200

    Adjust to new tech spec. Remove handling of JSON.

--- src/xcode/ENA/HealthCertificateToolkit/Sources/HealthCertificateToolkit/HealthCertificateToolkit.swift
+++ src/xcode/ENA/HealthCertificateToolkit/Sources/HealthCertificateToolkit/HealthCertificateToolkit.swift
@@ -7,8 +7,8 @@ import Foundation
 import SwiftCBOR
 import base45_swift
 
-public typealias DecodeHealthCertificateResult = Result<CertificateRepresentations, HealthCertificateDecodingError>
-public typealias FetchProofCertificateResult = Result<CertificateRepresentations, ProofCertificateFetchingError>
+public typealias DecodeHealthCertificateResult = Result<HealthCertificateRepresentations, HealthCertificateDecodingError>
+public typealias FetchProofCertificateResult = Result<ProofCertificateRepresentations, ProofCertificateFetchingError>
 
 public enum ProofCertificateFetchingError: Error {
     case something
@@ -21,13 +21,17 @@ public enum HealthCertificateDecodingError: Error {
     case HC_COSE_TAG_INVALID
     case HC_COSE_MESSAGE_INVALID
     case HC_CBOR_DECODING_FAILED
+    case HC_CWT_NO_ISS
+    case HC_CWT_NO_EXP
+    case HC_CWT_NO_HCERT
+    case HC_CWT_NO_DGC
 }
 
 public protocol HealthCertificateToolkitProtocol {
 
     func decodeHealthCertificate(base45: String) -> DecodeHealthCertificateResult
 
-    func fetchProofCertificate(for healthCertificates: [CertificateRepresentations], completion: (FetchProofCertificateResult) -> Void)
+    func fetchProofCertificate(for healthCertificates: [HealthCertificateRepresentations], completion: (FetchProofCertificateResult) -> Void)
 }
 
 public struct HealthCertificateToolkit: HealthCertificateToolkitProtocol {
@@ -52,26 +56,50 @@ public struct HealthCertificateToolkit: HealthCertificateToolkitProtocol {
             }
         }
 
-        guard let jsonData = try? extractJSON(cosePayload) else {
-            return .failure(.HC_CBOR_DECODING_FAILED)
+        let cborWebTokenResult = decodeCBORWebToken(cosePayload)
+        guard case let .success(cborWebToken) = cborWebTokenResult else {
+            if case let .failure(error) = cosePayloadResult {
+                return .failure(error)
+            } else {
+                fatalError("Has to be an error at this point.")
+            }
         }
 
-        let certificateRepresentation = CertificateRepresentations(
+        let headerResult = extractHeader(cborWebToken)
+        guard case let .success(header) = headerResult else {
+            if case let .failure(error) = headerResult {
+                return .failure(error)
+            } else {
+                fatalError("Has to be an error at this point.")
+            }
+        }
+
+        let certificateResult = extractHealthCertificate(cborWebToken)
+        guard case let .success(certificate) = certificateResult else {
+            if case let .failure(error) = certificateResult {
+                return .failure(error)
+            } else {
+                fatalError("Has to be an error at this point.")
+            }
+        }
+
+        let certificateRepresentation = HealthCertificateRepresentations(
             base45: base45,
             cbor: cborData,
-            json: jsonData
+            header: header,
+            certificate: certificate
         )
 
         return .success(certificateRepresentation)
     }
 
-    public func fetchProofCertificate(for healthCertificates: [CertificateRepresentations], completion: (FetchProofCertificateResult) -> Void) {
+    public func fetchProofCertificate(for healthCertificates: [HealthCertificateRepresentations], completion: (FetchProofCertificateResult) -> Void) {
 
     }
 
     // MARK: - Internal
 
-    func decodeCOSEPayload(_ data: Data) -> Result<CBOR, HealthCertificateDecodingError> {
+    func decodeCOSEPayload(_ data: Data) -> Result<Data, HealthCertificateDecodingError> {
         let decoder = CBORDecoder(input: [UInt8](data))
 
         guard
@@ -85,20 +113,70 @@ public struct HealthCertificateToolkit: HealthCertificateToolkitProtocol {
         guard
             case let CBOR.array(message) = messageElement,
             message.count == 4,
-            case let CBOR.byteString(payloadBytes) = message[2],
-            let payload = try? CBOR.decode(payloadBytes) else {
+            case let CBOR.byteString(payload) = message[2] else {
 
             return .failure(.HC_COSE_MESSAGE_INVALID)
+        }
+
+        return .success(Data(payload))
+    }
+
+    func decodeCBORWebToken(_ data: Data) -> Result<CBOR, HealthCertificateDecodingError>  {
+        let decoder = CBORDecoder(input: [UInt8](data))
+
+        guard let payload = try? decoder.decodeItem() else {
+            return .failure(.HC_CBOR_DECODING_FAILED)
         }
 
         return .success(payload)
     }
 
-    func extractJSON(_ cbor: CBOR) throws -> Data {
-        guard case let .map(map) = cbor else {
-            throw HealthCertificateDecodingError.HC_CBOR_DECODING_FAILED
+    func extractHeader(_ cbor: CBOR) -> Result<HealthCertificateHeader, HealthCertificateDecodingError> {
+
+        guard let issuerElement = cbor[1],
+              case let .utf8String(issuer) = issuerElement else {
+            return .failure(.HC_CWT_NO_ISS)
         }
-        return try JSONSerialization.data(withJSONObject: map.anyMap)
+
+        guard let expirationTimeElement = cbor[6],
+              case let .unsignedInt(expirationTime) = expirationTimeElement else {
+            return .failure(.HC_CWT_NO_EXP)
+        }
+
+        var issuedAt: UInt64?
+        if let issuedAtElement = cbor[4],
+           case let .unsignedInt(_issuedAt) = issuedAtElement {
+            issuedAt = _issuedAt
+        }
+
+        return .success(HealthCertificateHeader(
+            issuer: issuer,
+            issuedAt: issuedAt,
+            expirationTime: expirationTime
+        ))
+    }
+
+    func extractHealthCertificate(_ cbor: CBOR) -> Result<HealthCertificate, HealthCertificateDecodingError> {
+
+        guard let healthCertificateElement = cbor[-260],
+              case let .map(healthCertificateMap) = healthCertificateElement else {
+            return .failure(.HC_CWT_NO_HCERT)
+        }
+
+        guard  let healthCertificateCBOR = healthCertificateMap[1] else {
+            return .failure(.HC_CWT_NO_DGC)
+        }
+
+        let _cborData = healthCertificateCBOR.encode()
+        let cborData = Data(_cborData)
+
+        let codableDecoder = CodableCBORDecoder()
+
+        guard let healthCertificate = try? codableDecoder.decode(HealthCertificate.self, from: cborData) else {
+            return .failure(.HC_CBOR_DECODING_FAILED)
+        }
+
+        return .success(healthCertificate)
     }
 }
 
@@ -122,7 +200,7 @@ public struct HealthCertificateToolkitStub: HealthCertificateToolkitProtocol {
         decodeResult
     }
 
-    public func fetchProofCertificate(for healthCertificates: [CertificateRepresentations], completion: (FetchProofCertificateResult) -> Void) {
+    public func fetchProofCertificate(for healthCertificates: [HealthCertificateRepresentations], completion: (FetchProofCertificateResult) -> Void) {
         completion(fetchProofCertificateResult)
     }
 
--- src/xcode/ENA/HealthCertificateToolkit/Sources/HealthCertificateToolkit/Model/CertificateRepresentations.swift
+++ src/xcode/ENA/HealthCertificateToolkit/Sources/HealthCertificateToolkit/Model/CertificateRepresentations.swift
@@ -4,9 +4,105 @@
 
 import Foundation
 
-public struct CertificateRepresentations: Codable {
+public struct ProofCertificateRepresentations: Codable {
 
     let base45: String
     let cbor: Data
-    let json: Data
+    let header: HealthCertificateHeader
+}
+
+public struct HealthCertificateRepresentations: Codable {
+
+    let base45: String
+    let cbor: Data
+    let header: HealthCertificateHeader
+    let certificate: HealthCertificate
+}
+
+public struct HealthCertificateHeader: Codable, Equatable {
+
+    // MARK: - Protocol Codable
+
+    enum CodingKeys: String, CodingKey {
+        case issuer = "iss"
+        case issuedAt = "iat"
+        case expirationTime = "exp"
+    }
+
+    // MARK: - Internal
+
+    let issuer: String
+    let issuedAt: UInt64?
+    let expirationTime: UInt64
+}
+
+public struct HealthCertificate: Codable, Equatable {
+
+    // MARK: - Protocol Codable
+
+    enum CodingKeys: String, CodingKey {
+        case version = "ver"
+        case name = "nam"
+        case dateOfBirth = "dob"
+        case vaccinationCertificates = "v"
+    }
+
+    // MARK: - Internal
+
+    let version: String
+    let name: HealthCertificateName
+    let dateOfBirth: String
+    let vaccinationCertificates: [VaccinationCertificate]
+}
+
+public struct VaccinationCertificate: Codable, Equatable {
+
+    // MARK: - Protocol Codable
+
+    enum CodingKeys: String, CodingKey {
+        case diseaseOrAgentTargeted = "tg"
+        case vaccineOrProphylaxis = "vp"
+        case vaccineMedicinalProduct = "mp"
+        case marketingAuthorizationHolder = "ma"
+        case doseNumber = "dn"
+        case totalSeriesOfDoses = "sd"
+        case dateOfVaccination = "dt"
+        case countryOfVaccination = "co"
+        case certificateIssuer = "is"
+        case uniqueCertificateIdentifier = "ci"
+    }
+
+    // MARK: - Internal
+
+    let diseaseOrAgentTargeted: String
+    let vaccineOrProphylaxis: String
+    let vaccineMedicinalProduct: String
+    let marketingAuthorizationHolder: String
+
+    let doseNumber: Int
+    let totalSeriesOfDoses: Int
+
+    let dateOfVaccination: String
+    let countryOfVaccination: String
+    let certificateIssuer: String
+    let uniqueCertificateIdentifier: String
+}
+
+public struct HealthCertificateName: Codable, Equatable {
+
+    // MARK: - Protocol Codable
+
+    enum CodingKeys: String, CodingKey {
+        case familyName = "fn"
+        case givenName = "gn"
+        case standardizedFamilyName = "fnt"
+        case standardizedGivenName = "gnt"
+    }
+
+    // MARK: - Internal
+
+    let familyName: String
+    let givenName: String
+    let standardizedFamilyName: String
+    let standardizedGivenName: String
 }
