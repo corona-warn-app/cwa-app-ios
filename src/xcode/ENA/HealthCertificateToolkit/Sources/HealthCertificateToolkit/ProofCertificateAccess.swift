317ff850643e6ade6f749ca3206fdf873356739d
Artur Friesen <mail@arturfriesen.de>
2021-05-06 15:29:06 +0200

    Add first quick shot implementation to check the server API.

--- src/xcode/ENA/HealthCertificateToolkit/Sources/HealthCertificateToolkit/ProofCertificateAccess.swift
+++ src/xcode/ENA/HealthCertificateToolkit/Sources/HealthCertificateToolkit/ProofCertificateAccess.swift
@@ -18,8 +18,37 @@ public struct ProofCertificateAccess {
         return certificateAccess.extractDigitalGreenCertificate(from: cborData)
     }
 
-    public func fetchProofCertificate(for healthCertificates: [Base45]) -> Result<CBORData, ProofCertificateFetchingError> {
-        return.success(CBORData())
+    public func fetchProofCertificate(for healthCertificates: [Base45], completion: @escaping (Result<CBORData, ProofCertificateFetchingError>) -> Void) {
+
+        let url = URL(string: "https://api.certify.demo.ubirch.com/api/certify/v2/reissue/cbor")
+        guard let requestUrl = url else {
+            fatalError()
+        }
+
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "POST"
+
+        let healthCertificateAccess = HealthCertificateAccess()
+
+        guard case let .success(healthCertificateCBORData) = healthCertificateAccess.extractCBOR(from: healthCertificates[0]) else {
+            fatalError()
+        }
+
+        request.httpBody = healthCertificateCBORData
+
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                if let error = error {
+                    print("Error \(error)")
+                    return
+                }
+
+                if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                    print("Response:\n \(dataString)")
+                }
+
+            completion(.success(CBORData()))
+        }
+        task.resume()
     }
 
     // MARK: - Internal
