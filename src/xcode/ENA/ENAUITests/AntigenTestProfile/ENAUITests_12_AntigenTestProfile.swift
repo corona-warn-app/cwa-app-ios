a9a8ec7a3419e71ef398b5573027d17c087a48fa
Bastian Kohlbauer <bastian@bastian.codes>
2021-05-06 10:56:40 +0200

    wait for existance on everything!

--- src/xcode/ENA/ENAUITests/AntigenTestProfile/ENAUITests_12_AntigenTestProfile.swift
+++ src/xcode/ENA/ENAUITests/AntigenTestProfile/ENAUITests_12_AntigenTestProfile.swift
@@ -32,6 +32,7 @@ class ENAUITests_12_AntigenTestProfile: XCTestCase {
 		/// Home Screen
 
 		let registerTestButton = try XCTUnwrap(app.buttons[AccessibilityIdentifiers.Home.submitCardButton])
+		XCTAssertTrue(registerTestButton.waitForExistence(timeout: .short))
 		registerTestButton.tap()
 		
 		/// Register Test Screen
@@ -43,22 +44,22 @@ class ENAUITests_12_AntigenTestProfile: XCTestCase {
 		/// Antigen Test Information Screen
 		
 		// header image exists
-		XCTAssertTrue(app.images[AccessibilityIdentifiers.ExposureSubmission.AntigenTest.Information.imageDescription].exists)
+		XCTAssertTrue(app.images[AccessibilityIdentifiers.ExposureSubmission.AntigenTest.Information.imageDescription].waitForExistence(timeout: .short))
 		// title exists
-		XCTAssertTrue(app.staticTexts[AccessibilityIdentifiers.ExposureSubmission.AntigenTest.Information.descriptionTitle].exists)
+		XCTAssertTrue(app.staticTexts[AccessibilityIdentifiers.ExposureSubmission.AntigenTest.Information.descriptionTitle].waitForExistence(timeout: .short))
 		// subtitle exists
-		XCTAssertTrue(app.staticTexts[AccessibilityIdentifiers.ExposureSubmission.AntigenTest.Information.descriptionSubHeadline].exists)
+		XCTAssertTrue(app.staticTexts[AccessibilityIdentifiers.ExposureSubmission.AntigenTest.Information.descriptionSubHeadline].waitForExistence(timeout: .short))
 		// legal text exists
-		XCTAssertTrue(app.cells[AccessibilityIdentifiers.ExposureSubmission.AntigenTest.Information.acknowledgementTitle].exists)
+		XCTAssertTrue(app.cells[AccessibilityIdentifiers.ExposureSubmission.AntigenTest.Information.acknowledgementTitle].waitForExistence(timeout: .short))
 		
 		let dataPrivacyButton = try XCTUnwrap(app.cells[AccessibilityIdentifiers.ExposureSubmission.AntigenTest.Information.dataPrivacyTitle])
+		XCTAssertTrue(dataPrivacyButton.waitForExistence(timeout: .short))
 		dataPrivacyButton.tap()
 		
 		/// Legal Text Screen
-		
-		XCTAssertTrue(app.cells[AccessibilityIdentifiers.ExposureSubmission.AntigenTest.Information.dataPrivacyTitle].exists)
-		
+
 		let backButton = try XCTUnwrap(app.navigationBars.buttons.element(boundBy: 0))
+		XCTAssertTrue(backButton.waitForExistence(timeout: .short))
 		backButton.tap()
 		
 		/// -> Antigen Test Information Screen
@@ -70,37 +71,46 @@ class ENAUITests_12_AntigenTestProfile: XCTestCase {
 		/// Create Antigen Test Profile Screen
 
 		let saveProfileButton = try XCTUnwrap(app.buttons[AccessibilityIdentifiers.ExposureSubmission.AntigenTest.Create.saveButton])
+		XCTAssertTrue(saveProfileButton.waitForExistence(timeout: .short))
 		XCTAssertFalse(saveProfileButton.isEnabled)
 		
 		let firstNameTextField = try XCTUnwrap(app.cells.textFields[AccessibilityIdentifiers.AntigenProfile.Create.firstNameTextField])
+		XCTAssertTrue(firstNameTextField.waitForExistence(timeout: .short))
 		firstNameTextField.tap()
 		firstNameTextField.typeText("Bastian")
 		
 		let lastNameTextField = try XCTUnwrap(app.cells.textFields[AccessibilityIdentifiers.AntigenProfile.Create.lastNameTextField])
+		XCTAssertTrue(lastNameTextField.waitForExistence(timeout: .short))
 		lastNameTextField.tap()
 		lastNameTextField.typeText("Kohlbauer")
 		
 		let birthDateTextField = try XCTUnwrap(app.cells.textFields[AccessibilityIdentifiers.AntigenProfile.Create.birthDateTextField])
+		XCTAssertTrue(birthDateTextField.waitForExistence(timeout: .short))
 		birthDateTextField.tap()
 		birthDateTextField.typeText("15-12-1986")
 		
 		let streetTextField = try XCTUnwrap(app.cells.textFields[AccessibilityIdentifiers.AntigenProfile.Create.streetTextField])
+		XCTAssertTrue(streetTextField.waitForExistence(timeout: .short))
 		streetTextField.tap()
 		streetTextField.typeText("Herr Bastian Kohlbauer Str. 1")
 		
 		let postalCodeTextField = try XCTUnwrap(app.cells.textFields[AccessibilityIdentifiers.AntigenProfile.Create.postalCodeTextField])
+		XCTAssertTrue(postalCodeTextField.waitForExistence(timeout: .short))
 		postalCodeTextField.tap()
 		postalCodeTextField.typeText("80639")
 		
 		let cityTextField = try XCTUnwrap(app.cells.textFields[AccessibilityIdentifiers.AntigenProfile.Create.cityTextField])
+		XCTAssertTrue(cityTextField.waitForExistence(timeout: .short))
 		cityTextField.tap()
 		cityTextField.typeText("München")
 		
 		let phoneNumberTextField = try XCTUnwrap(app.cells.textFields[AccessibilityIdentifiers.AntigenProfile.Create.phoneNumberTextField])
+		XCTAssertTrue(phoneNumberTextField.waitForExistence(timeout: .short))
 		phoneNumberTextField.tap()
 		phoneNumberTextField.typeText("089123456")
 		
 		let emailAddressTextField = try XCTUnwrap(app.cells.textFields[AccessibilityIdentifiers.AntigenProfile.Create.emailAddressTextField])
+		XCTAssertTrue(emailAddressTextField.waitForExistence(timeout: .short))
 		emailAddressTextField.tap()
 		emailAddressTextField.typeText("bastian@bastian.codes")
 		
@@ -110,11 +120,12 @@ class ENAUITests_12_AntigenTestProfile: XCTestCase {
 		/// Antigen Test Profile Screen
 
 		// continues button exists
-		XCTAssertTrue(app.buttons[AccessibilityIdentifiers.ExposureSubmission.AntigenTest.Profile.continueButton].exists)
+		XCTAssertTrue(app.buttons[AccessibilityIdentifiers.ExposureSubmission.AntigenTest.Profile.continueButton].waitForExistence(timeout: .short))
 		// delete profile button exists
-		XCTAssertTrue(app.buttons[AccessibilityIdentifiers.ExposureSubmission.AntigenTest.Profile.deleteButton].exists)
+		XCTAssertTrue(app.buttons[AccessibilityIdentifiers.ExposureSubmission.AntigenTest.Profile.deleteButton].waitForExistence(timeout: .short))
 		
 		let closeButton = try XCTUnwrap(app.navigationBars.buttons.element(boundBy: 1))
+		XCTAssertTrue(closeButton.waitForExistence(timeout: .short))
 		closeButton.tap()
 		
 		/// -> Home Screen
@@ -125,6 +136,7 @@ class ENAUITests_12_AntigenTestProfile: XCTestCase {
 		
 		// test profile button exists
 		let testProfileButton = try XCTUnwrap(app.buttons[AccessibilityIdentifiers.ExposureSubmission.AntigenTest.Profile.profileTile_Description])
+		XCTAssertTrue(testProfileButton.waitForExistence(timeout: .short))
 		testProfileButton.tap()
 		
 		/// Antigen Test Profile Screen
@@ -134,9 +146,14 @@ class ENAUITests_12_AntigenTestProfile: XCTestCase {
 		XCTAssertTrue(deleteTestProfileButton.waitForExistence(timeout: .short))
 		deleteTestProfileButton.tap()
 		
+		// confirm deletion on popup
+		let popupDeleteButton = try XCTUnwrap(app.alerts.firstMatch.buttons.element(boundBy: 1))
+		XCTAssertTrue(deleteTestProfileButton.waitForExistence(timeout: .short))
+		popupDeleteButton.tap()
+		
 		/// -> Register Test Screen
 		
 		// create test profile button exists
-		_ = try XCTUnwrap(app.buttons[AccessibilityIdentifiers.ExposureSubmission.AntigenTest.Profile.createProfileTile_Description])
+		XCTAssertTrue(app.buttons[AccessibilityIdentifiers.ExposureSubmission.AntigenTest.Profile.createProfileTile_Description].waitForExistence(timeout: .short))
 	}
 }
