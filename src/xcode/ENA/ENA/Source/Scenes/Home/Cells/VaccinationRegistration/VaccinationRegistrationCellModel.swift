8a0f2998561bf91a1faeef06c8a02859d143c61f
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-05-11 15:49:19 +0200

    Update src/xcode/ENA/ENA/Source/Scenes/Home/Cells/VaccinationRegistration/VaccinationRegistrationCellModel.swift
    Co-authored-by: Kai Teuber <Kai.Teuber@app-developer.club>

--- src/xcode/ENA/ENA/Source/Scenes/Home/Cells/VaccinationRegistration/VaccinationRegistrationCellModel.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Home/Cells/VaccinationRegistration/VaccinationRegistrationCellModel.swift
@@ -8,10 +8,10 @@ class VaccinationRegistrationCellModel {
 
 	// MARK: - Internal
 
-	var title = AppStrings.HealthCertificate.Info.RegisterCard.title
-	var description = AppStrings.HealthCertificate.Info.RegisterCard.description
-	var buttonTitle = AppStrings.HealthCertificate.Info.RegisterCard.buttonTitle
-	var image = UIImage(named: "Vacc_Register")
-	var tintColor: UIColor = .enaColor(for: .textPrimary1)
-	var accessibilityIdentifier = AccessibilityIdentifiers.Home.registerVaccinationButton
+	let title = AppStrings.HealthCertificate.Info.RegisterCard.title
+	let description = AppStrings.HealthCertificate.Info.RegisterCard.description
+	let buttonTitle = AppStrings.HealthCertificate.Info.RegisterCard.buttonTitle
+	let image = UIImage(named: "Vacc_Register")
+	let tintColor: UIColor = .enaColor(for: .textPrimary1)
+	let accessibilityIdentifier = AccessibilityIdentifiers.Home.registerVaccinationButton
 }
