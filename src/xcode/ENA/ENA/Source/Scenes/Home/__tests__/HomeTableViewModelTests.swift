dfadfe461c22283f49b4c8595166b09d0d526f21
Nick Guendling <hello@nickguendling.com>
2021-05-14 13:59:52 +0200

    Move tests to appropriate file

--- src/xcode/ENA/ENA/Source/Scenes/Home/__tests__/HomeTableViewModelTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Home/__tests__/HomeTableViewModelTests.swift
@@ -210,4 +210,34 @@ class HomeTableViewModelTests: XCTestCase {
 		}
 	}
 
+	func test_WhenHealthCertifiedPersonIsAdded() {
+		let healthCertificateService = MockHealthCertificateService()
+		let store = MockTestStore()
+
+		let viewModel = HomeTableViewModel(
+			state: .init(
+				store: store,
+				riskProvider: MockRiskProvider(),
+				exposureManagerState: .init(authorized: true, enabled: true, status: .active),
+				enState: .enabled,
+				statisticsProvider: StatisticsProvider(
+					client: CachingHTTPClientMock(),
+					store: store
+				)
+			),
+			store: store,
+			coronaTestService: CoronaTestService(
+				client: ClientMock(),
+				store: store,
+				appConfiguration: CachedAppConfigurationMock()
+			),
+			healthCertificateService: healthCertificateService, onTestResultCellTap: { _ in }
+		)
+		XCTAssertEqual(viewModel.numberOfRows(in: 2), 0) // HealthCertificates
+
+		_ = healthCertificateService.registerHealthCertificate(base45: HealthCertificate.mockBase45)
+
+		XCTAssertEqual(viewModel.numberOfRows(in: 2), 1) // HealthCertificates
+	}
+
 }
