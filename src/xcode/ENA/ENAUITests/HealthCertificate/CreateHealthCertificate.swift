a626279601bc03159a95b7a42801e4bf9d0442fb
Kai Teuber <Kai.Teuber@app-developer.club>
2021-05-18 16:19:41 +0200

    separated disclaimer, first certificate and last certificate UITests

--- src/xcode/ENA/ENAUITests/HealthCertificate/CreateHealthCertificate.swift
+++ src/xcode/ENA/ENAUITests/HealthCertificate/CreateHealthCertificate.swift
@@ -25,9 +25,7 @@ class CreateHealthCertificate: XCTestCase {
 
 	// MARK: - Tests
 
-	func test_FIRST_CreateAntigenTestProfile_THEN_DeleteProfile() throws {
-
-		app.launchArguments.append(contentsOf: ["-firstHealthCertificate", YES])
+	func test_shownConsentScreemAndDisclaimer() throws {
 		app.launch()
 
 		/// Home Screen
@@ -44,6 +42,15 @@ class CreateHealthCertificate: XCTestCase {
 		let backButton = try XCTUnwrap(app.navigationBars.buttons.element(boundBy: 0))
 		XCTAssertTrue(backButton.waitForExistence(timeout: .short))
 		backButton.tap()
+	}
+
+	func test_CreateAntigenTestProfileWithFirstCertificate_THEN_DeleteProfile() throws {
+		app.launch()
+
+		/// Home Screen
+		let registerCertificateTitle = try XCTUnwrap(app.buttons[AccessibilityIdentifiers.Home.registerHealthCertificateButton])
+		XCTAssertTrue(registerCertificateTitle.waitForExistence(timeout: .short))
+		registerCertificateTitle.tap()
 
 		// HealthCertificate consent screen
 		let primaryButton = try XCTUnwrap(app.buttons[AccessibilityIdentifiers.General.primaryFooterButton])
@@ -62,4 +69,34 @@ class CreateHealthCertificate: XCTestCase {
 		let headlineCell = try XCTUnwrap(app.cells[AccessibilityIdentifiers.HealthCertificate.Certificate.headline])
 		XCTAssertTrue(headlineCell.waitForExistence(timeout: .short))
 	}
+
+	func test_CreateAntigenTestProfileWithLastCertificate_THEN_DeleteProfile() throws {
+
+		app.launchArguments.append(contentsOf: ["-firstHealthCertificate", YES])
+//		"firstAndSecondHealthCertificate"
+		app.launch()
+
+		/// Home Screen
+		let registerCertificateTitle = try XCTUnwrap(app.buttons[AccessibilityIdentifiers.Home.registerHealthCertificateButton])
+		XCTAssertTrue(registerCertificateTitle.waitForExistence(timeout: .short))
+		registerCertificateTitle.tap()
+
+		// HealthCertificate consent screen
+		let primaryButton = try XCTUnwrap(app.buttons[AccessibilityIdentifiers.General.primaryFooterButton])
+		XCTAssertTrue(primaryButton.waitForExistence(timeout: .short))
+		primaryButton.tap()
+
+		// QRCode Scanner - close via flash will submit a healthCertificate
+		let flashBarButton = try XCTUnwrap(app.buttons[AccessibilityIdentifiers.ExposureSubmissionQRScanner.flash])
+		flashBarButton.waitAndTap(.short)
+
+		// Certified Person screen
+		let continuePrimaryButton = try XCTUnwrap(app.cells[AccessibilityIdentifiers.HealthCertificate.Person.certificateCell])
+		continuePrimaryButton.waitAndTap(.short)
+
+		// Certificate Screen
+		let headlineCell = try XCTUnwrap(app.cells[AccessibilityIdentifiers.HealthCertificate.Certificate.headline])
+		XCTAssertTrue(headlineCell.waitForExistence(timeout: .short))
+	}
+
 }
