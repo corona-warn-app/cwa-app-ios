1443b2a369e72e8e44e84434949fc078ee69aa8a
Kai Teuber <Kai.Teuber@app-developer.club>
2021-05-20 16:57:14 +0200

    added new database schema and some migration

---
+++ src/xcode/ENA/ENA/Source/Scenes/ContactDiary/Store/StoreV5/ContactDiaryMigration4To5.swift
@@ -0,0 +1,50 @@
+////
+// 🦠 Corona-Warn-App
+//
+
+import FMDB
+
+final class ContactDiaryMigration4To5: Migration {
+
+	private let databaseQueue: FMDatabaseQueue
+	private var error: Error?
+
+	init(databaseQueue: FMDatabaseQueue) {
+		self.databaseQueue = databaseQueue
+	}
+
+	// MARK: - Protocol Migration
+
+	let version = 5
+
+	func execute() throws {
+		var error: Error?
+
+		databaseQueue.inDatabase { database in
+
+			let sql = """
+				BEGIN TRANSACTION;
+
+				CREATE TABLE IF NOT EXISTS CoronaTest (
+					id INTEGER PRIMARY KEY NOT NULL,
+					date TEXT NOT NULL,
+					TestType INTEGER NOT NULL,
+					TestResult INTEGER NOT NULL
+				);
+
+				COMMIT;
+			"""
+
+			guard database.executeStatements(sql) else {
+				error = MigrationError.general(description: "(\(database.lastErrorCode())) \(database.lastErrorMessage())")
+				return
+			}
+
+			database.userVersion = UInt32(version)
+		}
+		
+		if let error = error {
+			throw error
+		}
+	}
+}
---
+++ src/xcode/ENA/ENA/Source/Scenes/ContactDiary/Store/StoreV5/ContactDiaryStoreSchemaV5.swift
@@ -0,0 +1,83 @@
+////
+// 🦠 Corona-Warn-App
+//
+
+import FMDB
+import CWASQLite
+
+class ContactDiaryStoreSchemaV5: StoreSchemaProtocol {
+
+	// MARK: - Init
+
+	init(databaseQueue: FMDatabaseQueue) {
+		self.databaseQueue = databaseQueue
+	}
+
+	// MARK: - Public
+
+	@discardableResult
+	func create() -> SecureSQLStore.VoidResult {
+		var result: SecureSQLStore.VoidResult = .success(())
+
+		databaseQueue.inDatabase { database in
+			let sql = """
+				CREATE TABLE IF NOT EXISTS ContactPerson (
+					id INTEGER PRIMARY KEY,
+					name TEXT NOT NULL CHECK (LENGTH(name) <= \(maxTextLength)),
+					phoneNumber TEXT CHECK (LENGTH(phoneNumber) <= \(maxTextLength)),
+					emailAddress TEXT CHECK (LENGTH(emailAddress) <= \(maxTextLength))
+				);
+				CREATE TABLE IF NOT EXISTS Location (
+					id INTEGER PRIMARY KEY,
+					name TEXT NOT NULL CHECK (LENGTH(name) <= \(maxTextLength)),
+					phoneNumber TEXT CHECK (LENGTH(phoneNumber) <= \(maxTextLength)),
+					emailAddress TEXT CHECK (LENGTH(emailAddress) <= \(maxTextLength)),
+					traceLocationId BLOB
+				);
+				CREATE TABLE IF NOT EXISTS ContactPersonEncounter (
+					id INTEGER PRIMARY KEY,
+					date TEXT NOT NULL,
+					duration INTEGER,
+					maskSituation INTEGER,
+					setting INTEGER,
+					circumstances TEXT CHECK (LENGTH(circumstances) <= \(maxTextLength)),
+					contactPersonId INTEGER NOT NULL,
+					FOREIGN KEY(contactPersonId) REFERENCES ContactPerson(id) ON DELETE CASCADE
+				);
+				CREATE TABLE IF NOT EXISTS LocationVisit (
+					id INTEGER PRIMARY KEY,
+					date TEXT NOT NULL,
+					durationInMinutes INTEGER,
+					circumstances TEXT CHECK (LENGTH(circumstances) <= \(maxTextLength)),
+					locationId INTEGER NOT NULL,
+					checkinId INTEGER,
+					FOREIGN KEY(locationId) REFERENCES Location(id) ON DELETE CASCADE
+				);
+				CREATE TABLE IF NOT EXISTS CoronaTest (
+					id INTEGER PRIMARY KEY NOT NULL,
+					date TEXT NOT NULL,
+					TestType INTEGER NOT NULL,
+					TestResult INTEGER NOT NULL
+				);
+			"""
+
+			guard database.executeStatements(sql) else {
+				Log.error("[SQLite] (\(database.lastErrorCode())) \(database.lastErrorMessage())", log: .localData)
+				let error = SecureSQLStoreError.database(SQLiteErrorCode(rawValue: database.lastErrorCode()) ?? SQLiteErrorCode.unknown)
+				result = .failure(error)
+				return
+			}
+
+			database.userVersion = 5
+			result = .success(())
+		}
+
+		return result
+	}
+
+	// MARK: - Private
+
+	private let databaseQueue: FMDatabaseQueue
+	private let maxTextLength = 250
+
+}
