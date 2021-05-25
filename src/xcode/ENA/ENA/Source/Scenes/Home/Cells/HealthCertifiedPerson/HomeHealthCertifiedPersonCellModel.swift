364fdb6175c69320cf695f54cc03a7cc8607373b
Nick Guendling <hello@nickguendling.com>
2021-05-25 17:44:26 +0200

    Configure cell synchronously as it is reloaded on changes anyway

--- src/xcode/ENA/ENA/Source/Scenes/Home/Cells/HealthCertifiedPerson/HomeHealthCertifiedPersonCellModel.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Home/Cells/HealthCertifiedPerson/HomeHealthCertifiedPersonCellModel.swift
@@ -12,39 +12,29 @@ class HomeHealthCertifiedPersonCellModel {
 	init(
 		healthCertifiedPerson: HealthCertifiedPerson
 	) {
-		self.healthCertifiedPerson = healthCertifiedPerson
+		backgroundGradientType = healthCertifiedPerson.vaccinationState == .completelyProtected ? .lightBlue : .solidGrey
+		iconImage = healthCertifiedPerson.vaccinationState == .partiallyVaccinated ? UIImage(named: "Vacc_Incomplete") : UIImage(named: "Vaccination_full")
+		name = healthCertifiedPerson.fullName
 
-		healthCertifiedPerson.$vaccinationState
-			.sink { [weak self] in
-				self?.backgroundGradientType = $0 == .completelyProtected ? .lightBlue : .solidGrey
-				self?.iconImage = $0 == .partiallyVaccinated ? UIImage(named: "Vacc_Incomplete") : UIImage(named: "Vaccination_full")
-				self?.name = healthCertifiedPerson.fullName
+		switch healthCertifiedPerson.vaccinationState {
+		case .partiallyVaccinated:
+			vaccinationStateDescription = AppStrings.HealthCertificate.Home.Person.partiallyVaccinated
+		case .fullyVaccinated(daysUntilCompleteProtection: let daysUntilCompleteProtection):
+			vaccinationStateDescription = String(
+				format: AppStrings.HealthCertificate.Home.Person.daysUntilCompleteProtection,
+				daysUntilCompleteProtection
+			)
+		case .completelyProtected:
+			vaccinationStateDescription = nil
+		}
 
-				switch $0 {
-				case .partiallyVaccinated:
-					self?.vaccinationState = AppStrings.HealthCertificate.Home.Person.partiallyVaccinated
-				case .fullyVaccinated(daysUntilCompleteProtection: let daysUntilCompleteProtection):
-					self?.vaccinationState = String(
-						format: AppStrings.HealthCertificate.Home.Person.daysUntilCompleteProtection,
-						daysUntilCompleteProtection
-					)
-				case .completelyProtected:
-					self?.vaccinationState = nil
-				}
-			}
-			.store(in: &subscriptions)
 	}
 	
 	// MARK: - Internal
 
-	@OpenCombine.Published var vaccinationState: String?
-	@OpenCombine.Published var backgroundGradientType: GradientView.GradientType = .solidGrey
-	@OpenCombine.Published var iconImage: UIImage?
-	@OpenCombine.Published var name: String?
-
-	// MARK: - Private
-
-	private let healthCertifiedPerson: HealthCertifiedPerson
-	private var subscriptions = Set<AnyCancellable>()
+	var vaccinationStateDescription: String?
+	var backgroundGradientType: GradientView.GradientType = .solidGrey
+	var iconImage: UIImage?
+	var name: String?
 
 }
