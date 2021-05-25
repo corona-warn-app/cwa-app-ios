781281b20d94da55f82660fcc8dd4852ecd59b28
Bastian Kohlbauer <bastian@bastian.codes>
2021-05-25 12:44:52 +0200

    add description label

--- src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/View/Controller/TANInputViewController/Cells/TANInputCell.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/View/Controller/TANInputViewController/Cells/TANInputCell.swift
@@ -11,15 +11,29 @@ class TANInputCell: UITableViewCell, ReuseIdentifierProviding {
 	init(viewModel: TanInputViewModel) {
 		super.init(style: .default, reuseIdentifier: TANInputCell.cellIdentifier)
 
+		let descriptionLabel = ENALabel()
+		descriptionLabel.style = .headline
+		descriptionLabel.text = AppStrings.ExposureSubmissionTanEntry.description
+		descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+		descriptionLabel.textColor = .enaColor(for: .textPrimary1)
+		descriptionLabel.numberOfLines = 0
+		contentView.addSubview(descriptionLabel)
+		
 		tanInputView = TanInputView(frame: .zero, viewModel: viewModel)
 		tanInputView.isUserInteractionEnabled = true
 		tanInputView.translatesAutoresizingMaskIntoConstraints = false
 		contentView.addSubview(tanInputView)
 		
 		NSLayoutConstraint.activate([
+			
+			descriptionLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
+			descriptionLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15),
+			descriptionLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
+			descriptionLabel.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor),
+			
 			tanInputView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
 			tanInputView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15),
-			tanInputView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
+			tanInputView.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 18),
 			tanInputView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -9)
 		])
 	}
