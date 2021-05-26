0168dd645d27d1395634ff27d01ce2a948b3784f
Carsten Knoblich <carsten@staticline.de>
2021-05-26 12:14:59 +0200

    quick and dirty hack to fail faster (wip)

---
+++ scripts/handle_test_failure.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+while [ ! -f "${1}" ]
+do
+  sleep 1
+done
+
+echo "Fast Fail! ☠️"
+
+# End current step and move on in job
+# via: https://support.circleci.com/hc/en-us/articles/360015562253-Conditionally-end-a-running-job-gracefully
+circleci-agent step halt
