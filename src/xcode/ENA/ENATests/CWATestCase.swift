f832595a99e1966287ca965a17eeab5caa7c91f3
Carsten Knoblich <carsten@staticline.de>
2021-05-26 21:03:21 +0200

    next try: skip tests in test case if one failed

--- src/xcode/ENA/ENATests/CWATestCase.swift
+++ src/xcode/ENA/ENATests/CWATestCase.swift
@@ -4,21 +4,13 @@
 
 import XCTest
 
-/// Indicator for CI-triggered tests. If `false` you are running it most likely locally.
-var isCI: Bool {
-	return ProcessInfo.processInfo.environment["CI"] == "YES"
-}
-
 class CWATestCase: XCTestCase {
 
-	override func tearDownWithError() throws {
-		try super.tearDownWithError()
+	override func setUpWithError() throws {
+		try super.setUpWithError()
 
 		// swiftlint:disable:next force_unwrapping
-		if /*isCI,*/ testRun!.totalFailureCount > 0 {
-			let pid = ProcessInfo.processInfo.processIdentifier
-			try "\(pid)".write(toFile: "/Users/distiller/project/.fail", atomically: true, encoding: .utf8)
-		}
+		try XCTSkipIf(testRun!.totalFailureCount > 0, "Fast fail ☠️")
 	}
 
 }
