088a1faef9f6ebee26707bf264a25e7c65483862
Carsten Knoblich <carsten@staticline.de>
2021-05-26 11:57:58 +0200

    create common superclass for test cases to mark failing test runs

---
+++ src/xcode/ENA/ENATests/CWATestCase.swift
@@ -0,0 +1,23 @@
+////
+// 🦠 Corona-Warn-App
+//
+
+import XCTest
+
+/// Indicator for CI-triggered tests. If `false` you are running it most likely locally.
+var isCI: Bool {
+	return ProcessInfo.processInfo.environment["CI"] == "YES"
+}
+
+final class CWATestCase: XCTestCase {
+
+	override func tearDownWithError() throws {
+		try super.tearDownWithError()
+
+		if isCI, testRun!.totalFailureCount > 0 {
+			let pid = ProcessInfo.processInfo.processIdentifier
+			try "\(pid)".write(toFile: ".fail", atomically: true, encoding: .utf8)
+		}
+	}
+
+}
