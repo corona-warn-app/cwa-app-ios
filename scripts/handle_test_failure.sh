7149174e9d22c9312b837f39dd64057dbc7b2df0
Carsten Knoblich <carsten@staticline.de>
2021-05-27 10:05:05 +0200

    testing more or less graceful stop of test run

--- scripts/handle_test_failure.sh
+++ scripts/handle_test_failure.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+set -euo pipefail
 
 while [ ! -f "${1}" ]
 do
@@ -10,3 +12,5 @@ echo "Fast Fail! ☠️"
 # End current step and move on in job
 # via: https://support.circleci.com/hc/en-us/articles/360015562253-Conditionally-end-a-running-job-gracefully
 circleci-agent step halt
+
+killall "iOS Simulator"
