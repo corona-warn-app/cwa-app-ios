2cde0d19c38ffeb34620e0be999bea9f383de53a
Carsten Knoblich <carsten@staticline.de>
2021-05-27 10:25:04 +0200

    brought back old code

--- src/xcode/ENA/ENATests/CWATestCase.swift
+++ src/xcode/ENA/ENATests/CWATestCase.swift
@@ -4,6 +4,11 @@
 
 import XCTest
 
+/// Indicator for CI-triggered tests. If `false` you are running it most likely locally.
+var isCI: Bool {
+	return ProcessInfo.processInfo.environment["CI"] == "YES"
+}
+
 class CWATestCase: XCTestCase {
 
 	override func setUpWithError() throws {
@@ -13,6 +18,16 @@ class CWATestCase: XCTestCase {
 
 		// swiftlint:disable:next force_unwrapping
 		try XCTSkipIf(testRun!.totalFailureCount > 0, "Fast fail ☠️")
+	}
+
+	override func tearDownWithError() throws {
+		try super.tearDownWithError()
+
+		// swiftlint:disable:next force_unwrapping
+		if /*isCI,*/ testRun!.totalFailureCount > 0 {
+			let pid = ProcessInfo.processInfo.processIdentifier
+			try "\(pid)".write(toFile: "/Users/distiller/project/.fail", atomically: true, encoding: .utf8)
+		}
 	}
 
 }
