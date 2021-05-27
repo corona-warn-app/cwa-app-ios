3ff6a01eeb82c2012d043fc7797cbfbf4ad67ba3
Carsten Knoblich <carsten@staticline.de>
2021-05-27 11:16:31 +0200

    removing circuit breaker script and handlers

--- src/xcode/ENA/ENATests/CWATestCase.swift
+++ src/xcode/ENA/ENATests/CWATestCase.swift
@@ -4,10 +4,6 @@
 
 import XCTest
 
-/// Indicator for CI-triggered tests. If `false` you are running it most likely locally.
-var isCI: Bool {
-	return ProcessInfo.processInfo.environment["CI"] == "YES"
-}
 
 class CWATestCase: XCTestCase {
 
@@ -17,17 +13,7 @@ class CWATestCase: XCTestCase {
 		continueAfterFailure = false
 
 		// swiftlint:disable:next force_unwrapping
-		try XCTSkipIf(testRun!.totalFailureCount > 0, "Fast fail ☠️")
-	}
-
-	override func tearDownWithError() throws {
-		try super.tearDownWithError()
-
-		// swiftlint:disable:next force_unwrapping
-		if /*isCI,*/ testRun!.totalFailureCount > 0 {
-			let pid = ProcessInfo.processInfo.processIdentifier
-			try "\(pid)".write(toFile: "/Users/distiller/project/.fail", atomically: true, encoding: .utf8)
-		}
+		try XCTSkipIf(testRun!.totalFailureCount > 0, "Fast fail \(self.testRun?.test.name ?? "")")
 	}
 
 }
