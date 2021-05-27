c299a99e2c0ab86aefa6fa3fbd249ea2975002a0
Kai Teuber <Kai.Teuber@app-developer.club>
2021-05-27 16:01:50 +0200

    Update src/xcode/ENA/ENA/Source/Services/CoronaTestService/__tests__/CoronaTestServiceTests.swift
    Co-authored-by: Nick Guendling <hello@nickguendling.com>

--- src/xcode/ENA/ENA/Source/Services/CoronaTestService/__tests__/CoronaTestServiceTests.swift
+++ src/xcode/ENA/ENA/Source/Services/CoronaTestService/__tests__/CoronaTestServiceTests.swift
@@ -1306,7 +1306,7 @@ class CoronaTestServiceTests: XCTestCase {
 		XCTAssertFalse(try XCTUnwrap(testService.pcrTest?.journalEntryCreated))
 	}
 
-	func test_When_UpdateTestResultSuccessWithExpired_Then_ContactJournalHasAnEntry() throws {
+	func test_When_UpdateTestResultSuccessWithExpired_Then_ContactJournalHasNoEntry() throws {
 		let mockNotificationCenter = MockUserNotificationCenter()
 		let client = ClientMock()
 		client.onGetTestResult = { _, _, completion in
