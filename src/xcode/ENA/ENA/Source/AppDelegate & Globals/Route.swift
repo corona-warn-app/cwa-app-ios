e4bb9cc025b324556416c693f17cd14bd0bb5d7b
Nick Guendling <hello@nickguendling.com>
2021-05-31 19:06:16 +0200

    Fix hash implementation

--- src/xcode/ENA/ENA/Source/AppDelegate & Globals/Route.swift
+++ src/xcode/ENA/ENA/Source/AppDelegate & Globals/Route.swift
@@ -65,7 +65,9 @@ enum Route {
 				informationArray.append(String(testInformation.timestamp))
 				informationArray.append(testInformation.testID ?? "")
 				informationArray.append(testInformation.cryptographicSalt ?? "")
-				informationArray.append(testInformation.certificateSupportedByPointOfCare == true ? "1" : "0")
+				if let certificateSupportedByPointOfCare = testInformation.certificateSupportedByPointOfCare {
+					informationArray.append(certificateSupportedByPointOfCare ? "1" : "0")
+				}
 
 				let informationString = informationArray.joined(separator: "#")
 				let recomputedHashString = ENAHasher.sha256(informationString)
