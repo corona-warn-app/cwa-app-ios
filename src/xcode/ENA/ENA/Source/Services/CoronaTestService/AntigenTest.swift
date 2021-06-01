30202818aa493ebe937d1c11fa6ce94b00556e10
Nick Guendling <hello@nickguendling.com>
2021-06-01 16:05:40 +0200

    Implement Codable by hand to define default values

--- src/xcode/ENA/ENA/Source/Services/CoronaTestService/AntigenTest.swift
+++ src/xcode/ENA/ENA/Source/Services/CoronaTestService/AntigenTest.swift
@@ -4,7 +4,7 @@
 
 import Foundation
 
-struct AntigenTest: Equatable, Codable {
+struct AntigenTest: Equatable {
 
 	// The date of when the consent was provided by the tested person at the Point of Care.
 	var pointOfCareConsentDate: Date
@@ -32,6 +32,55 @@ struct AntigenTest: Equatable, Codable {
 
 	var testDate: Date {
 		return sampleCollectionDate ?? pointOfCareConsentDate
+	}
+
+}
+
+extension AntigenTest: Codable {
+
+	// MARK: - Protocol Codable
+
+	enum CodingKeys: String, CodingKey {
+		case pointOfCareConsentDate
+		case sampleCollectionDate
+		case registrationDate
+		case registrationToken
+		case testedPerson
+		case testResult
+		case finalTestResultReceivedDate
+		case positiveTestResultWasShown
+		case isSubmissionConsentGiven
+		case submissionTAN
+		case keysSubmitted
+		case journalEntryCreated
+		case certificateSupportedByPointOfCare
+		case certificateConsentGiven
+		case certificateCreated
+	}
+
+	init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+
+		pointOfCareConsentDate = try container.decode(Date.self, forKey: .pointOfCareConsentDate)
+		sampleCollectionDate = try container.decodeIfPresent(Date.self, forKey: .sampleCollectionDate)
+		registrationDate = try container.decodeIfPresent(Date.self, forKey: .registrationDate)
+		registrationToken = try container.decodeIfPresent(String.self, forKey: .registrationToken)
+
+		testedPerson = try container.decode(TestedPerson.self, forKey: .testedPerson)
+
+		testResult = try container.decode(TestResult.self, forKey: .testResult)
+		finalTestResultReceivedDate = try container.decodeIfPresent(Date.self, forKey: .finalTestResultReceivedDate)
+		positiveTestResultWasShown = try container.decode(Bool.self, forKey: .positiveTestResultWasShown)
+
+		isSubmissionConsentGiven = try container.decode(Bool.self, forKey: .isSubmissionConsentGiven)
+		submissionTAN = try container.decodeIfPresent(String.self, forKey: .submissionTAN)
+		keysSubmitted = try container.decode(Bool.self, forKey: .keysSubmitted)
+
+		journalEntryCreated = try container.decode(Bool.self, forKey: .journalEntryCreated)
+
+		certificateSupportedByPointOfCare = try container.decodeIfPresent(Bool.self, forKey: .certificateSupportedByPointOfCare) ?? false
+		certificateConsentGiven = try container.decodeIfPresent(Bool.self, forKey: .certificateConsentGiven) ?? false
+		certificateCreated = try container.decodeIfPresent(Bool.self, forKey: .certificateCreated) ?? false
 	}
 
 }
