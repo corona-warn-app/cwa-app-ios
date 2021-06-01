565a89327458623cc934bad360993ca845505632
Kai Teuber <Kai.Teuber@app-developer.club>
2021-06-01 22:01:49 +0200

    Update src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/View/Controller/TestCertificate/Cell/BirthdayDatePickerCell.swift
    Co-authored-by: Nick Guendling <hello@nickguendling.com>

--- src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/View/Controller/TestCertificate/Cell/BirthdayDatePickerCell.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/View/Controller/TestCertificate/Cell/BirthdayDatePickerCell.swift
@@ -19,8 +19,6 @@ class BirthdayDatePickerCell: UITableViewCell, ReuseIdentifierProviding, UITextF
 		fatalError("init(coder:) has not been implemented")
 	}
 
-	// MARK: - Overrides
-
 	// MARK: - Protocol UITextFieldDelegate
 
 	func textFieldShouldClear(_ textField: UITextField) -> Bool {
