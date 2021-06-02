00cc1fcb63586db46575789f3607b41598810233
Pascal Brause <pascal@brause.haus>
2021-06-02 16:08:01 +0200

    fix rename

--- src/xcode/ENA/ENA/Source/Client/HTTP Client/HTTPClient+Configuration.swift
+++ src/xcode/ENA/ENA/Source/Client/HTTP Client/HTTPClient+Configuration.swift
@@ -32,7 +32,7 @@ extension HTTPClient {
 					requiresTrailingSlash: false
 				),
 				dcc: .init(
-					baseURL: environmentProvider.currentEnvironment().dgcURL,
+					baseURL: environmentProvider.currentEnvironment().dccURL,
 					requiresTrailingSlash: false
 				)
 			)
