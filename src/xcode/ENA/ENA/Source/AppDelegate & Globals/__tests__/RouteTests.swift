1eebc31116915b1518450b8c7a3a1d47bb3aa12b
Nick Guendling <hello@nickguendling.com>
2021-06-02 14:38:46 +0200

    Extend route tests to check that parsed values are correct

--- src/xcode/ENA/ENA/Source/AppDelegate & Globals/__tests__/RouteTests.swift
+++ src/xcode/ENA/ENA/Source/AppDelegate & Globals/__tests__/RouteTests.swift
@@ -13,18 +13,25 @@ class RouteTests: CWATestCase {
 
 		// WHEN
 		let route = Route(validRATTestURL)
-		guard case let .rapidAntigen(result) = route else {
+
+		// THEN
+		guard
+			case let .rapidAntigen(result) = route,
+			case let .success(coronaTestRegistrationInformation) = result,
+			case let .antigen(qrCodeInformation: antigenTestQRCodeInformation) = coronaTestRegistrationInformation
+		else {
 			XCTFail("unexpected route type")
 			return
 		}
 
-		// THEN
-		switch result {
-		case .success:
-			break
-		case .failure:
-			XCTFail("Error was not intended")
-		}
+		XCTAssertEqual(antigenTestQRCodeInformation.hash, "1ea4c222ff0c0e4ed7373f274caa7f75d10fccbdac56c632771cd9592102a555")
+		XCTAssertEqual(antigenTestQRCodeInformation.timestamp, 1619617269)
+		XCTAssertEqual(antigenTestQRCodeInformation.firstName, "Henry")
+		XCTAssertEqual(antigenTestQRCodeInformation.lastName, "Pinzani")
+		XCTAssertEqual(antigenTestQRCodeInformation.dateOfBirth, Date(timeIntervalSince1970: 620438400))
+		XCTAssertEqual(antigenTestQRCodeInformation.testID, "b4a40c6c-e02c-4448-b8ab-0b5b7c34d60a")
+		XCTAssertEqual(antigenTestQRCodeInformation.cryptographicSalt, "7BFA06B5AD9311B8719BB80661C54EAD")
+		XCTAssertNil(antigenTestQRCodeInformation.certificateSupportedByPointOfCare)
 	}
 
 	func testGIVEN_validRATUrlWithDGCTrue_WHEN_parseRoute_THEN_isValid() {
@@ -33,18 +40,25 @@ class RouteTests: CWATestCase {
 
 		// WHEN
 		let route = Route(validRATTestURL)
-		guard case let .rapidAntigen(result) = route else {
+
+		// THEN
+		guard
+			case let .rapidAntigen(result) = route,
+			case let .success(coronaTestRegistrationInformation) = result,
+			case let .antigen(qrCodeInformation: antigenTestQRCodeInformation) = coronaTestRegistrationInformation
+		else {
 			XCTFail("unexpected route type")
 			return
 		}
 
-		// THEN
-		switch result {
-		case .success:
-			break
-		case .failure:
-			XCTFail("Error was not intended")
-		}
+		XCTAssertEqual(antigenTestQRCodeInformation.hash, "682e33fd77bf5719ef176fb4e7fc7237486d68fc15d01692c0049a24c6fa6563")
+		XCTAssertEqual(antigenTestQRCodeInformation.timestamp, 1622630470)
+		XCTAssertEqual(antigenTestQRCodeInformation.firstName, "Clyde")
+		XCTAssertEqual(antigenTestQRCodeInformation.lastName, "Montigiani")
+		XCTAssertEqual(antigenTestQRCodeInformation.dateOfBirth, Date(timeIntervalSince1970: -323481600))
+		XCTAssertEqual(antigenTestQRCodeInformation.testID, "b1bcdef3-3cda-4245-9e96-6d793ea8b60d")
+		XCTAssertEqual(antigenTestQRCodeInformation.cryptographicSalt, "342FD4F3DE740629F9C97F2BB8511BAC")
+		XCTAssertTrue(try XCTUnwrap(antigenTestQRCodeInformation.certificateSupportedByPointOfCare))
 	}
 
 	func testGIVEN_validRATUrlWithDGCFalse_WHEN_parseRoute_THEN_isValid() {
@@ -53,18 +67,25 @@ class RouteTests: CWATestCase {
 
 		// WHEN
 		let route = Route(validRATTestURL)
-		guard case let .rapidAntigen(result) = route else {
+
+		// THEN
+		guard
+			case let .rapidAntigen(result) = route,
+			case let .success(coronaTestRegistrationInformation) = result,
+			case let .antigen(qrCodeInformation: antigenTestQRCodeInformation) = coronaTestRegistrationInformation
+		else {
 			XCTFail("unexpected route type")
 			return
 		}
 
-		// THEN
-		switch result {
-		case .success:
-			break
-		case .failure:
-			XCTFail("Error was not intended")
-		}
+		XCTAssertEqual(antigenTestQRCodeInformation.hash, "ca9ce0ce195152ad1c3090722a54dbb73fd7e13777ee7bae1ea04c4354b70b50")
+		XCTAssertEqual(antigenTestQRCodeInformation.timestamp, 1622630654)
+		XCTAssertEqual(antigenTestQRCodeInformation.firstName, "Garrett")
+		XCTAssertEqual(antigenTestQRCodeInformation.lastName, "Campbell")
+		XCTAssertEqual(antigenTestQRCodeInformation.dateOfBirth, Date(timeIntervalSince1970: 552182400))
+		XCTAssertEqual(antigenTestQRCodeInformation.testID, "2a92f0fb-f37e-48d3-a319-bca041a8db01")
+		XCTAssertEqual(antigenTestQRCodeInformation.cryptographicSalt, "AC811229B7923F1A8E0530CC689AC0CC")
+		XCTAssertFalse(try XCTUnwrap(antigenTestQRCodeInformation.certificateSupportedByPointOfCare))
 	}
 
 	func testGIVEN_invalidRATUrl_WHEN_parseRoute_THEN_isValid() {
