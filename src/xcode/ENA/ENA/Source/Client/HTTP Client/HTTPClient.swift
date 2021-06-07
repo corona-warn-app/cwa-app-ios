fc853ef04d9056c776c945329d6b7834e0308efc
Nick Guendling <hello@nickguendling.com>
2021-06-07 11:37:02 +0200

    Remove key padding todo

--- src/xcode/ENA/ENA/Source/Client/HTTP Client/HTTPClient.swift
+++ src/xcode/ENA/ENA/Source/Client/HTTP Client/HTTPClient.swift
@@ -1347,7 +1347,6 @@ private extension URLRequest {
 	/// This method recreates the request body of the submit keys request with a padding that fills up to resemble
 	/// a request with 14 +`n` keys. Note that the `n`parameter is currently set to 0, but can change in the future
 	/// when there will be support for 15 keys.
-	// TODO: We already have 15 keys, n = 1?
 	private static func getSubmissionPadding(for keys: [SAP_External_Exposurenotification_TemporaryExposureKey]) -> Data {
 		// This parameter denotes how many keys 14 + n have to be padded.
 		let n = 0
