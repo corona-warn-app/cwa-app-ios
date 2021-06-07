4b703aa5b0f7581022632d65953ac821b8cb0018
Bastian Kohlbauer <bastian@bastian.codes>
2021-06-07 13:56:28 +0200

    wrap label in stack view into views to prevent layout issues

--- src/xcode/ENA/ENA/Source/Scenes/Home/Cells/Statistics/HomeStatisticsCardView/HomeStatisticsCardView.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Home/Cells/Statistics/HomeStatisticsCardView/HomeStatisticsCardView.swift
@@ -12,23 +12,61 @@ class HomeStatisticsCardView: UIView {
 	override func awakeFromNib() {
 		super.awakeFromNib()
 
-		let focusableLabels = [
-			titleLabel as ENALabel,
-			primaryTitleLabel as ENALabel,
-			primaryValueLabel as ENALabel,
-			secondaryTitleLabel as ENALabel,
-			secondaryValueLabel as ENALabel,
-			tertiaryTitleLabel as ENALabel,
-			tertiaryValueLabel as ENALabel,
-			footnoteLabel as ENALabel
-		]
+		titleLabel.adjustsFontSizeToFitWidth = true
+		titleLabel.allowsDefaultTighteningForTruncation = true
+		titleLabel.onAccessibilityFocus = { [weak self] in
+			self?.onAccessibilityFocus?()
+		}
+		
+		primaryTitleLabel.style = .body
+		primaryTitleLabel.textColor = .enaColor(for: .textPrimary2)
+		primaryTitleLabel.adjustsFontSizeToFitWidth = true
+		primaryTitleLabel.allowsDefaultTighteningForTruncation = true
+		primaryTitleLabel.onAccessibilityFocus = { [weak self] in
+			self?.onAccessibilityFocus?()
+		}
+		
+		primaryValueLabel.style = .title1
+		primaryValueLabel.adjustsFontSizeToFitWidth = true
+		primaryValueLabel.allowsDefaultTighteningForTruncation = true
+		primaryValueLabel.onAccessibilityFocus = { [weak self] in
+			self?.onAccessibilityFocus?()
+		}
+		
+		secondaryTitleLabel.style = .body
+		secondaryTitleLabel.textColor = .enaColor(for: .textPrimary2)
+		secondaryTitleLabel.adjustsFontSizeToFitWidth = true
+		secondaryTitleLabel.allowsDefaultTighteningForTruncation = true
+		secondaryTitleLabel.onAccessibilityFocus = { [weak self] in
+			self?.onAccessibilityFocus?()
+		}
+		
+		secondaryValueLabel.style = .headline
+		secondaryValueLabel.adjustsFontSizeToFitWidth = true
+		secondaryValueLabel.allowsDefaultTighteningForTruncation = true
+		secondaryValueLabel.onAccessibilityFocus = { [weak self] in
+			self?.onAccessibilityFocus?()
+		}
+		
+		tertiaryTitleLabel.style = .body
+		tertiaryTitleLabel.textColor = .enaColor(for: .textPrimary2)
+		tertiaryTitleLabel.adjustsFontSizeToFitWidth = true
+		tertiaryTitleLabel.allowsDefaultTighteningForTruncation = true
+		tertiaryTitleLabel.onAccessibilityFocus = { [weak self] in
+			self?.onAccessibilityFocus?()
+		}
+		
+		tertiaryValueLabel.style = .headline
+		tertiaryValueLabel.adjustsFontSizeToFitWidth = true
+		tertiaryValueLabel.allowsDefaultTighteningForTruncation = true
+		tertiaryValueLabel.onAccessibilityFocus = { [weak self] in
+			self?.onAccessibilityFocus?()
+		}
 
-		focusableLabels.forEach {
-			$0.adjustsFontSizeToFitWidth = true
-			$0.allowsDefaultTighteningForTruncation = true
-			$0.onAccessibilityFocus = { [weak self] in
-				self?.onAccessibilityFocus?()
-			}
+		footnoteLabel.adjustsFontSizeToFitWidth = true
+		footnoteLabel.allowsDefaultTighteningForTruncation = true
+		footnoteLabel.onAccessibilityFocus = { [weak self] in
+			self?.onAccessibilityFocus?()
 		}
 
 		primaryTrendImageView.layer.cornerRadius = primaryTrendImageView.bounds.width / 2
@@ -113,16 +151,16 @@ class HomeStatisticsCardView: UIView {
 
 	@IBOutlet weak var illustrationImageView: UIImageView!
 
-	@IBOutlet weak var primaryTitleLabel: ENALabel!
-	@IBOutlet weak var primaryValueLabel: ENALabel!
+	@IBOutlet weak var primaryTitleLabel: StackViewLabel!
+	@IBOutlet weak var primaryValueLabel: StackViewLabel!
 	@IBOutlet weak var primaryTrendImageView: UIImageView!
 
-	@IBOutlet weak var secondaryTitleLabel: ENALabel!
-	@IBOutlet weak var secondaryValueLabel: ENALabel!
+	@IBOutlet weak var secondaryTitleLabel: StackViewLabel!
+	@IBOutlet weak var secondaryValueLabel: StackViewLabel!
 	@IBOutlet weak var secondaryTrendImageView: UIImageView!
 
-	@IBOutlet weak var tertiaryTitleLabel: ENALabel!
-	@IBOutlet weak var tertiaryValueLabel: ENALabel!
+	@IBOutlet weak var tertiaryTitleLabel: StackViewLabel!
+	@IBOutlet weak var tertiaryValueLabel: StackViewLabel!
 
 	@IBOutlet weak var footnoteLabel: ENALabel!
 
---
+++ src/xcode/ENA/ENA/Source/Views/StackViewLabel.swift
@@ -0,0 +1,75 @@
+////
+// 🦠 Corona-Warn-App
+//
+
+import UIKit
+
+class StackViewLabel: UIView {
+	
+	// MARK: - Init
+	
+	convenience init() {
+		self.init(frame: .zero)
+	}
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		setupView()
+	}
+	
+	required init?(coder: NSCoder) {
+		super.init(coder: coder)
+		setupView()
+	}
+	
+	// MARK: - Internal
+	
+	var text: String? {
+		get { label.text }
+		set { label.text = newValue }
+	}
+	
+	var adjustsFontSizeToFitWidth: Bool {
+		get { label.adjustsFontSizeToFitWidth }
+		set { label.adjustsFontSizeToFitWidth = newValue }
+	}
+	
+	var onAccessibilityFocus: (() -> Void)? {
+		get { label.onAccessibilityFocus }
+		set { label.onAccessibilityFocus = newValue }
+	}
+	
+	var allowsDefaultTighteningForTruncation: Bool {
+		get { label.allowsDefaultTighteningForTruncation }
+		set { label.allowsDefaultTighteningForTruncation = newValue }
+	}
+	
+	var textColor: UIColor {
+		get { label.textColor }
+		set { label.textColor = newValue }
+	}
+	
+	var style: ENALabel.Style {
+		get { label.style }
+		set { label.style = newValue }
+	}
+	
+	// MARK: - Private
+	
+	private var label: ENALabel!
+	
+	private func setupView() {
+		
+		label = ENALabel()
+		label.numberOfLines = 0
+		label.translatesAutoresizingMaskIntoConstraints = false
+		addSubview(label)
+		
+		NSLayoutConstraint.activate([
+			label.leadingAnchor.constraint(equalTo: leadingAnchor),
+			label.trailingAnchor.constraint(equalTo: trailingAnchor),
+			label.topAnchor.constraint(equalTo: topAnchor),
+			label.bottomAnchor.constraint(equalTo: bottomAnchor)
+		])
+	}
+}
