ba1807f359a564fe6848c64b87d2aeec49c61289
Kai Teuber <Kai.Teuber@app-developer.club>
2021-06-09 16:38:45 +0200

    removed no longer needed HealthCertificateMock

--- src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/HealthCertificate/__tests__/HealthCertificateViewModelTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/HealthCertificate/__tests__/HealthCertificateViewModelTests.swift
@@ -21,7 +21,7 @@ class HealthCertificateViewModelTests: CWATestCase {
 
 	func testGIVEN_HealthCertificate_WHEN_CreateViewModel_THEN_IsSetup() throws {
 		// GIVEN
-		let healthCertificate = HealthCertificateMock()
+		let healthCertificate = HealthCertificate.mock()
 		let certifiedPerson = HealthCertifiedPerson(healthCertificates: [])
 		let vaccinationValueSetsProvider = VaccinationValueSetsProvider(client: CachingHTTPClientMock(), store: MockTestStore())
 
@@ -44,51 +44,8 @@ class HealthCertificateViewModelTests: CWATestCase {
 		XCTAssertEqual(viewModel.numberOfItems(in: .headline), 1)
 		XCTAssertEqual(viewModel.numberOfItems(in: .qrCode), 1)
 		XCTAssertEqual(viewModel.numberOfItems(in: .topCorner), 1)
-		XCTAssertEqual(viewModel.numberOfItems(in: .details), 9)
+		XCTAssertEqual(viewModel.numberOfItems(in: .details), 8)
 		XCTAssertEqual(viewModel.numberOfItems(in: .bottomCorner), 1)
 	}
-
-}
-
-struct HealthCertificateMock: HealthCertificateData {
-
-	// MARK: - Init
-
-	init() {
-		self.base45 = ""
-		self.version = ""
-		self.name = Name(
-			familyName: "Mustermann",
-			givenName: "Max",
-			standardizedFamilyName: "die mustermanns",
-			standardizedGivenName: "maxi"
-		)
-		self.dateOfBirth = "1981-12-24"
-		self.dateOfBirthDate = Date()
-		self.vaccinationEntry = nil
-		self.testEntry = nil
-		self.type = .test(TestEntry(diseaseOrAgentTargeted: "", typeOfTest: "", testResult: "", naaTestName: nil, ratTestName: nil, dateTimeOfSampleCollection: "", dateTimeOfTestResult: "", testCenter: "", countryOfTest: "", certificateIssuer: "", uniqueCertificateIdentifier: ""))
-		self.isLastDoseInASeries = false
-		self.expirationDate = Date()
-		self.dateOfVaccination = Date()
-		self.doseNumber = 1
-		self.totalSeriesOfDoses = 2
-	}
-
-	// MARK: - Internal
-
-	let base45: Base45
-	let version: String
-	let name: Name
-	let dateOfBirth: String
-	var dateOfBirthDate: Date?
-	let vaccinationEntry: VaccinationEntry?
-	let testEntry: TestEntry?
-	let type: HealthCertificate.CertificateType
-	let isLastDoseInASeries: Bool
-	var expirationDate: Date
-	var dateOfVaccination: Date?
-	var doseNumber: Int
-	var totalSeriesOfDoses: Int
 
 }
