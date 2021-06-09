9a580e6eaaf474ccb9be9adfc5cbe582dbe3e572
Artur Friesen <mail@arturfriesen.de>
2021-06-09 14:20:07 +0200

    Add screenshots for the test certificate request screen.

--- src/xcode/ENA/ENAUITests/ExposureSubmission/ENAUITests_04a_ExposureSubmission.swift
+++ src/xcode/ENA/ENAUITests/ExposureSubmission/ENAUITests_04a_ExposureSubmission.swift
@@ -431,8 +431,12 @@ class ENAUITests_04a_ExposureSubmission: CWATestCase {
 		// QR Code Scanner Screen
 		XCTAssertTrue(app.navigationBars[AccessibilityIdentifiers.General.exposureSubmissionNavigationControllerTitle].waitForExistence(timeout: .medium))
 
-		// Tap on birthday field.
+		// Wait for the birthday field.
 		XCTAssertTrue(app.cells[AccessibilityIdentifiers.ExposureSubmission.TestCertificate.Info.birthdayPlaceholder].waitForExistence(timeout: .extraLong))
+
+		snapshot("submissionflow_screenshot_test_certificate_request")
+
+		// Tap on birthday field.
 		app.cells[AccessibilityIdentifiers.ExposureSubmission.TestCertificate.Info.birthdayPlaceholder].waitAndTap()
 
 		// Pick another year to enable the button
@@ -445,6 +449,8 @@ class ENAUITests_04a_ExposureSubmission: CWATestCase {
 		let buttonEnabled = NSPredicate(format: "enabled == true")
 		expectation(for: buttonEnabled, evaluatedWith: app.buttons[AccessibilityIdentifiers.General.primaryFooterButton], handler: nil)
 		waitForExpectations(timeout: .medium, handler: nil)
+
+		snapshot("submissionflow_screenshot_test_certificate_entered_birthday")
 	}
 	
 	// MARK: - Screenshots
