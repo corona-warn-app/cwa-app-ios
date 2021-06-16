0c9cd38c48e956d4878d97634d148ca97fad6363
Nick Guendling <hello@nickguendling.com>
2021-06-16 18:50:13 +0200

    Determine most relevant health certificate

--- src/xcode/ENA/ENA/Source/Services/HealthCertificate/Models/HealthCertifiedPerson.swift
+++ src/xcode/ENA/ENA/Source/Services/HealthCertificate/Models/HealthCertifiedPerson.swift
@@ -114,6 +114,115 @@ class HealthCertifiedPerson: Codable, Equatable {
 		healthCertificates.filter { $0.testEntry != nil }
 	}
 
+	var mostRelevantHealthCertificate: HealthCertificate? {
+		let sortedHealthCertificates = healthCertificates.sorted()
+		let pcrTestType = "LP6464-4"
+		let antigenTestType = "LP217198-3"
+
+		// PCR Test Certificate < 48 hours
+
+		let currentPCRTest = sortedHealthCertificates
+			.last {
+				guard let typeOfTest = $0.testEntry?.typeOfTest, let ageInHours = $0.ageInHours else {
+					return false
+				}
+
+				return typeOfTest == pcrTestType && ageInHours < 48
+			}
+
+		if let currentPCRTest = currentPCRTest {
+			return currentPCRTest
+		}
+
+		// RAT Test Certificate < 24 hours
+
+		let currentAntigenTest = sortedHealthCertificates
+			.last {
+				guard let typeOfTest = $0.testEntry?.typeOfTest, let ageInHours = $0.ageInHours else {
+					return false
+				}
+
+				return typeOfTest == antigenTestType && ageInHours < 24
+			}
+
+		if let currentAntigenTest = currentAntigenTest {
+			return currentAntigenTest
+		}
+
+		// Series-completing Vaccination Certificate > 14 days
+
+		let protectingVaccinationCertificate = sortedHealthCertificates
+			.last {
+				guard let isLastDoseInASeries = $0.vaccinationEntry?.isLastDoseInASeries, let ageInDays = $0.ageInDays else {
+					return false
+				}
+
+				return isLastDoseInASeries && ageInDays > 14
+			}
+
+		if let protectingVaccinationCertificate = protectingVaccinationCertificate {
+			return protectingVaccinationCertificate
+		}
+
+		// Recovery Certificate <= 180 days
+
+		let validRecoveryCertificate = sortedHealthCertificates
+			.last {
+				guard let ageInDays = $0.ageInDays else {
+					return false
+				}
+
+				return $0.type == .recovery && ageInDays <= 180
+			}
+
+		if let validRecoveryCertificate = validRecoveryCertificate {
+			return validRecoveryCertificate
+		}
+
+		// Series-completing Vaccination Certificate <= 14 days
+
+		let seriesCompletingVaccinationCertificate = sortedHealthCertificates
+			.last {
+				guard let isLastDoseInASeries = $0.vaccinationEntry?.isLastDoseInASeries, let ageInDays = $0.ageInDays else {
+					return false
+				}
+
+				return isLastDoseInASeries && ageInDays <= 14
+			}
+
+		if let seriesCompletingVaccinationCertificate = seriesCompletingVaccinationCertificate {
+			return seriesCompletingVaccinationCertificate
+		}
+
+		// Other Vaccination Certificate
+
+		if let otherVaccinationCertificate = sortedHealthCertificates.last(where: { $0.type == .vaccination }) {
+			return otherVaccinationCertificate
+		}
+
+		// Recovery Certificate > 180 days
+
+		if let otherRecoveryCertificate = sortedHealthCertificates.last(where: { $0.type == .recovery }) {
+			return otherRecoveryCertificate
+		}
+
+		// PCR Test Certificate > 48 hours
+
+		if let otherPCRTestCertificate = sortedHealthCertificates.last(where: { $0.testEntry?.typeOfTest == pcrTestType }) {
+			return otherPCRTestCertificate
+		}
+
+		// RAT Test Certificate > 24 hours
+
+		if let otherAntigenTestCertificate = sortedHealthCertificates.last(where: { $0.testEntry?.typeOfTest == antigenTestType }) {
+			return otherAntigenTestCertificate
+		}
+
+		// Fallback
+
+		return healthCertificates.first
+	}
+
 	// MARK: - Private
 
 	private var subscriptions = Set<AnyCancellable>()
