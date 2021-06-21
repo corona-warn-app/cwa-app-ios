64d8fc6b37293d25a268d37b6d020a0be33a6eb8
Carsten Knoblich <carsten@staticline.de>
2021-06-21 14:03:49 +0200

    using web view for loca data privacy & usage HTMLs

--- src/xcode/ENA/ENA/Source/Scenes/HTMLViewController/HTMLViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/HTMLViewController/HTMLViewController.swift
@@ -3,6 +3,7 @@
 //
 
 import UIKit
+import WebKit
 
 class HTMLViewController: UIViewController, DismissHandling {
 	
@@ -55,6 +56,7 @@ class HTMLViewController: UIViewController, DismissHandling {
 		
 		if let url = Bundle.main.url(forResource: infoModel.urlResourceName, withExtension: "html") {
 			do {
+				htmlView.navigationDelegate = self
 				try htmlView.load(from: url)
 			} catch {
 				Log.error("Could not load url \(url)", log: .ui, error: error)
@@ -70,9 +72,30 @@ class HTMLViewController: UIViewController, DismissHandling {
 		}
 	}
 
-	@IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var containerView: UIView!
+    @IBOutlet private weak var imageView: UIImageView!
 	@IBOutlet private weak var htmlView: HTMLView!
 	@IBOutlet private weak var htmlTitleLabel: ENALabel!
-	
+    @IBOutlet private weak var webViewHeight: NSLayoutConstraint!
+
 	private let infoModel: HtmlInfoModel
+}
+
+extension HTMLViewController: WKNavigationDelegate {
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        htmlView.evaluateJavaScript("document.readyState", completionHandler: { complete, error in
+            if complete != nil {
+                self.htmlView.evaluateJavaScript("document.body.scrollHeight", completionHandler: { [weak self] height, error in
+                    if let height = height as? CGFloat {
+                        Log.debug("Set content height to \(height) @\(UIScreen.main.scale)x")
+
+                        self?.webViewHeight.constant = height
+                    } else {
+                        Log.error("Could not get website height! \(error?.localizedDescription ?? "")", error: error)
+                    }
+                })
+            }
+        })
+    }
 }
--- src/xcode/ENA/ENA/Source/Views/Common/HTMLView.swift
+++ src/xcode/ENA/ENA/Source/Views/Common/HTMLView.swift
@@ -13,6 +13,8 @@ final class HTMLView: WKWebView {
 
 	private var htmlURL: URL!
 
+	// MARK: - Init
+
 	override init(frame: CGRect, configuration: WKWebViewConfiguration) {
 		super.init(frame: frame, configuration: configuration)
 		setup()
@@ -23,28 +25,28 @@ final class HTMLView: WKWebView {
 		setup()
 	}
 
+	init() {
+		// We currently deal with local HTML, so no need to handle Cookies, etc. for now
+		let config = WKWebViewConfiguration()
+		super.init(frame: .zero, configuration: config)
+	}
+
 	private func setup() {
 		backgroundColor = nil
+		scrollView.isScrollEnabled = false
+		scrollView.isUserInteractionEnabled = false
 	}
+
+	// MARK: - Content display
+
+	override var intrinsicContentSize: CGSize {
+		return scrollView.contentSize
+	  }
 
 	func load(from url: URL) throws {
 		let request = URLRequest(url: url)
-		guard let navigation = load(request) else {
+		if load(request) == nil {
 			throw WebViewError.cannotLoad
 		}
-		dump(navigation)
-
-//		if var html = try loadHtml(from: url) {
-//			self.html = html
-//			html = applyColors(to: html)
-//			if let attributedText = try parseHtml(html) {
-//				let mutableAttributedText = NSMutableAttributedString(attributedString: attributedText)
-//				mutableAttributedText.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.enaColor(for: .textPrimary1), range: NSRange(location: 0, length: attributedText.length))
-//				self.attributedText = mutableAttributedText
-//			}
-//		} else {
-//			Log.error("HTML resource could not be loaded: \(url)", log: .api)
-//		}
 	}
-
 }
