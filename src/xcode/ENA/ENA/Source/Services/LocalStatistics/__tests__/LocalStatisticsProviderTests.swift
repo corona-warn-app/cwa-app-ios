7d851f487d370429e2afde6bc8dddb372552f7ca
Naveed Khalid <nkhalid.here@gmail.com>
2021-06-24 17:36:21 +0500

    adding mocks and tests

--- src/xcode/ENA/ENA/Source/Services/LocalStatistics/__tests__/LocalStatisticsProviderTests.swift
+++ src/xcode/ENA/ENA/Source/Services/LocalStatistics/__tests__/LocalStatisticsProviderTests.swift
@@ -1,5 +1,119 @@
-////
+//
 // 🦠 Corona-Warn-App
 //
 
-import Foundation
+import XCTest
+import OpenCombine
+@testable import ENA
+
+class LocalStatisticsProviderTests: CWATestCase {
+
+	private var subscriptions = [AnyCancellable]()
+	
+	func testFetchLocalStatitics() {
+		let fetchedFromClientExpectation = expectation(description: "Local statistics fetched from client")
+		fetchedFromClientExpectation.expectedFulfillmentCount = 1
+
+		let store = MockTestStore()
+		XCTAssertNil(store.localStatistics)
+
+		let client = CachingHTTPClientMock()
+		client.fetchLocalStatistics(administrativeUnit: "1", eTag: "fake") { result in
+			switch result {
+			case .success(let response):
+				XCTAssertNotNil(response.eTag)
+				XCTAssertNotNil(response.timestamp)
+				XCTAssertNotNil(response.localStatistics)
+			case .failure(let error):
+				XCTFail(error.localizedDescription)
+			}
+			fetchedFromClientExpectation.fulfill()
+		}
+
+		waitForExpectations(timeout: .medium)
+	}
+	
+	func testLocalStatiticsProviding() throws {
+		let valueReceived = expectation(description: "Local statistics received")
+		valueReceived.expectedFulfillmentCount = 1
+		
+		let store = MockTestStore()
+		let client = CachingHTTPClientMock()
+		let provider = LocalStatisticsProvider(client: client, store: store)
+		provider.latestLocalStatistics(administrativeUnit: "1", eTag: "fake")
+			.sink(receiveCompletion: { result in
+				switch result {
+				case .finished:
+					break
+				case .failure(let error):
+					XCTFail(error.localizedDescription)
+				}
+			}, receiveValue: { localStatistics in
+				XCTAssertNotNil(localStatistics)
+				valueReceived.fulfill()
+			})
+			.store(in: &subscriptions)
+		
+		waitForExpectations(timeout: .short)
+	}
+	
+	func testLocalStatisticsProvidingHTTPErrors() throws {
+		let store = MockTestStore()
+		let client = CachingHTTPClientMock()
+		let expectedError = URLSessionError.serverError(503)
+		client.onFetchLocalStatistics = { _, completeWith in
+			// fake a broken backend
+			completeWith(.failure(expectedError))
+		}
+		
+		let provider = LocalStatisticsProvider(client: client, store: store)
+		provider.latestLocalStatistics(administrativeUnit: "1", eTag: "fake")
+			.sink(receiveCompletion: { result in
+				switch result {
+				case .finished:
+					break
+				case .failure(let error):
+					XCTAssertEqual(error.localizedDescription, expectedError.errorDescription)
+				}
+			}, receiveValue: { _ in
+				XCTFail("Did not expect a value")
+			})
+			.store(in: &subscriptions)
+	}
+	
+	func testLocalStatisticsProvidingHTTP304() throws {
+		let valueNotChangedExpectation = expectation(description: "Value not changed")
+		valueNotChangedExpectation.expectedFulfillmentCount = 2
+		
+		let store = MockTestStore()
+		store.localStatistics = LocalStatisticsMetadata(
+			lastLocalStatisticsETag: "fake",
+			lastLocalStatisticsFetchDate: try XCTUnwrap(301.secondsAgo),
+			localStatistics: CachingHTTPClientMock.staticLocalStatistics
+		)
+		// Fake, backend returns HTTP 304
+		let client = CachingHTTPClientMock()
+		client.onFetchLocalStatistics = { _, completeWith in
+			let error = URLSessionError.notModified
+			completeWith(.failure(error))
+			valueNotChangedExpectation.fulfill()
+		}
+		
+		let provider = LocalStatisticsProvider(client: client, store: store)
+		provider.latestLocalStatistics(administrativeUnit: "1", eTag: "fake")
+			.sink(receiveCompletion: { result in
+				switch result {
+				case .finished:
+					break
+				case .failure(let error):
+					XCTFail("Did not expect an error, got: \(error)")
+				}
+			}, receiveValue: { value in
+				XCTAssertNotNil(value)
+				valueNotChangedExpectation.fulfill()
+			})
+			.store(in: &subscriptions)
+		
+		waitForExpectations(timeout: .medium)
+	}
+}
