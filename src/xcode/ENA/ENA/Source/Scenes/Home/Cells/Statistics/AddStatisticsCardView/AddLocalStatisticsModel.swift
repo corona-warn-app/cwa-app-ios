d22b6e84b05ddd21a28afac1644fb11fe2d4ea43
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-06-24 15:28:40 +0200

    Add LocalStatisticsModel With helper functions

---
+++ src/xcode/ENA/ENA/Source/Scenes/Home/Cells/Statistics/AddStatisticsCardView/AddLocalStatisticsModel.swift
@@ -0,0 +1,52 @@
+////
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+
+struct AddLocalStatisticsModel {
+
+	// MARK: - Init
+
+	init(
+		store: Store,
+		jsonFileURL: URL
+	) {
+		self.store = store
+
+		do {
+			let jsonData = try Data(contentsOf: jsonFileURL)
+			self.allDistricts = try JSONDecoder().decode([DistrictElement].self, from: jsonData)
+		} catch {
+			Log.debug("Failed to read / parse district json", log: .ppac)
+			self.allDistricts = []
+		}
+	}
+
+	// MARK: - Internal
+
+	var allFederalStateNames: [String] {
+		FederalStateName.allCases.map { $0.rawValue }
+	}
+
+	func allRegions(by federalStateName: String) -> [String] {
+		allDistricts.filter { district -> Bool in
+			district.federalStateName.rawValue == federalStateName
+		}
+		.map { $0.districtName }
+	}
+	func regionId(by region: String) -> DistrictElement? {
+		allDistricts.first(where: { district -> Bool in
+			district.districtName == region
+		})
+	}
+
+//	mutating func save() {
+		// save the returnedLocalStatisticsFrom the API to the store
+//	}
+
+	// MARK: - Private
+
+	private let store: Store
+	private let allDistricts: [DistrictElement]
+}
