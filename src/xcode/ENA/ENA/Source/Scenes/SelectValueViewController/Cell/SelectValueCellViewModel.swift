b8858c9810c84207232a41ea0c8b5f94523d00d2
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-06-24 15:34:00 +0200

    handle right image in selection cell

--- src/xcode/ENA/ENA/Source/Scenes/SelectValueViewController/Cell/SelectValueCellViewModel.swift
+++ src/xcode/ENA/ENA/Source/Scenes/SelectValueViewController/Cell/SelectValueCellViewModel.swift
@@ -9,13 +9,26 @@ final class SelectValueCellViewModel {
 
 	// MARK: - Init
 
-	init(text: String, isSelected: Bool) {
+	init(text: String, isSelected: Bool, cellIconType: SelectionCellIcon) {
 		self.text = text
-		self.checkmarkImage = isSelected ? UIImage(imageLiteralResourceName: "Icons_Checkmark") : nil
+		switch cellIconType {
+		case .checkmark:
+			self.image = isSelected ? UIImage(imageLiteralResourceName: "Icons_Checkmark") : nil
+		case .discloseIndicator:
+			self.image = UIImage(imageLiteralResourceName: "Icons_Chevron_plain")
+		case .none:
+			self.image = nil
+		}
 	}
 
 	// MARK: - Internal
 
 	let text: String
-	let checkmarkImage: UIImage?
+	let image: UIImage?
+}
+
+enum SelectionCellIcon {
+	case checkmark
+	case discloseIndicator
+	case none
 }
