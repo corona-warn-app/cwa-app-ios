a5b7d696e1a3bfbef7b217a93cbd9ce27ff395de
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-06-24 15:30:32 +0200

    Add AddLocalStatistics UI

---
+++ src/xcode/ENA/ENA/Source/Scenes/Home/Cells/Statistics/AddStatisticsCardView/AddStatisticsCardView.swift
@@ -0,0 +1,106 @@
+////
+// 🦠 Corona-Warn-App
+//
+
+import UIKit
+
+class AddStatisticsCardView: CustomDashedView {
+	
+	// MARK: - Overrides
+
+	override func awakeFromNib() {
+		super.awakeFromNib()
+		
+		self.addLocalIncidenceLabel.text = AppStrings.Statistics.AddCard.SevenDayIncidence
+		let borderColor: UIColor = .enaColor(for: .backgroundLightGray)
+		layer.borderColor = borderColor.cgColor
+		addLocalIncidenceLabel.onAccessibilityFocus = { [weak self] in
+			self?.onAccessibilityFocus?()
+		}
+
+	}
+	// if local cards = 0: only show add button
+	// if 0 < local cards < 5: show add and edit
+	// if local cards = 5: show add and edit {disable add}
+	// we can also pass store or something else to check the number or create enum for case
+	
+	// swiftlint:disable:next function_parameter_count
+	func configure(
+		localStatisticsModel: AddLocalStatisticsModel,
+		availableCardsState: CreatedLocalStatisticsState,
+		onAddStateButtonTap: @escaping (SelectValueTableViewController) -> Void,
+		onAddDistrict: @escaping (SelectValueTableViewController) -> Void,
+		onDismissState: @escaping () -> Void,
+		onDismissDistrict: @escaping () -> Void,
+		onEditButtonTap: @escaping () -> Void,
+		onAccessibilityFocus: @escaping () -> Void
+	) {
+		self.model = localStatisticsModel
+
+		self.onAddStateButtonTap = onAddStateButtonTap
+		self.onAddDistrict = onAddDistrict
+		self.onDismissState = onDismissState
+		self.onDismissDistrict = onDismissDistrict
+		self.onEditButtonTap = onEditButtonTap
+		self.onAccessibilityFocus = onAccessibilityFocus
+	}
+
+	@IBAction func addLocalIncidencePressed(_ sender: Any) {
+		guard let model = self.model else {
+			Log.warning("AddStatistics model is nil", log: .localStatistics)
+			return
+		}
+		viewModel = AddStatisticsCardsViewModel(
+			localStatisticsModel: model,
+			presentSelectStateList: { selectedStateValueViewModel in
+				self.presentAddLocalStatistics(selectValueViewModel: selectedStateValueViewModel)
+			},
+			presentSelectDistrictList: { selectedDistrictValueViewModel in
+				self.presentAddLocalStatisticsDistrict(selectValueViewModel: selectedDistrictValueViewModel)
+				
+			}
+		)
+		viewModel?.presentStateSelection()
+	}
+	
+	@IBOutlet weak var addLocalIncidenceLabel: ENALabel!
+	// WE NEED LocalStatisticsModel INJECTION
+	// WE NEED COMPLETION HANDLERS FOR DISMISS AND SHOW
+
+	// MARK: - Private
+
+	private func presentAddLocalStatistics(selectValueViewModel: SelectValueViewModel) {
+		let selectValueViewController = SelectValueTableViewController(
+			selectValueViewModel,
+			closeOnSelection: false,
+			dismiss: { [weak self] in
+				self?.onDismissState?()
+			})
+		onAddStateButtonTap?(selectValueViewController)
+	}
+
+	private func presentAddLocalStatisticsDistrict(selectValueViewModel: SelectValueViewModel) {
+		let selectValueViewController = SelectValueTableViewController(
+			selectValueViewModel,
+			dismiss: { [weak self] in
+				self?.onDismissDistrict?()
+			}
+		)
+		onAddDistrict?(selectValueViewController)
+	}
+
+	private var onAddStateButtonTap: ((SelectValueTableViewController) -> Void)?
+	private var onAddDistrict: ((SelectValueTableViewController) -> Void)?
+	private var onDismissState: (() -> Void)?
+	private var onDismissDistrict: (() -> Void)?
+	private var onEditButtonTap: (() -> Void)?
+	private var onAccessibilityFocus: (() -> Void)?
+	private var viewModel: AddStatisticsCardsViewModel?
+	private var model: AddLocalStatisticsModel?
+}
+
+enum CreatedLocalStatisticsState {
+	case empty // 0
+	case NotYetFull // 0 < number of local statistics cards < 5
+	case full // 5
+}
