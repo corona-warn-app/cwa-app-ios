93869e5d3c44e269545e137a3f8cd797d2fff53a
Nick Guendling <hello@nickguendling.com>
2021-06-25 15:34:24 +0200

    Add localized formatted string

--- src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/Utils/DCCDateStringFormatter.swift
+++ src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/Utils/DCCDateStringFormatter.swift
@@ -9,4 +9,34 @@ enum DCCDateStringFormatter {
 	static func formattedString(from string: String) -> String {
 		return string.components(separatedBy: "T").first ?? string
 	}
+
+	static func localizedFormattedString(from string: String) -> String {
+		let formattedString = Self.formattedString(from: string)
+
+		let inputDateFormatter = DateFormatter()
+
+		let outputDateFormatter = DateFormatter()
+		outputDateFormatter.locale = Locale.current
+
+		inputDateFormatter.dateFormat = "yyyy-MM-dd"
+		if let date = inputDateFormatter.date(from: formattedString) {
+			outputDateFormatter.setLocalizedDateFormatFromTemplate("yyyyMMdd")
+			return outputDateFormatter.string(from: date)
+		}
+
+		inputDateFormatter.dateFormat = "yyyy-MM"
+		if let date = inputDateFormatter.date(from: formattedString) {
+			outputDateFormatter.setLocalizedDateFormatFromTemplate("yyyyMM")
+			return outputDateFormatter.string(from: date)
+		}
+
+		inputDateFormatter.dateFormat = "yyyy"
+		if let date = inputDateFormatter.date(from: formattedString) {
+			outputDateFormatter.setLocalizedDateFormatFromTemplate("yyyy")
+			return outputDateFormatter.string(from: date)
+		}
+
+		return formattedString
+	}
+
 }
