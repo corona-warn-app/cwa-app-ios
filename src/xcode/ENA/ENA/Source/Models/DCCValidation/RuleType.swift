25ec05f453e4595c6ce92ac63b46a3830b2474cd
Pascal Brause <pascal@brause.haus>
2021-06-28 11:18:58 +0200

    add path var for generic solution

--- src/xcode/ENA/ENA/Source/Models/DCCValidation/RuleType.swift
+++ src/xcode/ENA/ENA/Source/Models/DCCValidation/RuleType.swift
@@ -7,4 +7,13 @@ import Foundation
 enum RuleType {
 	case acceptance
 	case invalidation
+	
+	var urlPath: String {
+		switch self {
+		case .acceptance:
+			return "acceptance-rules"
+		case .invalidation:
+			return "invalidation-rules"
+		}
+	}
 }
