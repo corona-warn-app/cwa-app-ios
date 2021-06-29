359ab4f17cd3b2e7d714de72a988be23b8df1f92
Pascal Brause <pascal@brause.haus>
2021-06-29 15:52:28 +0200

    add country list loading to protocol

--- src/xcode/ENA/ENA/Source/Services/DCCValidationService/DCCValidationService.swift
+++ src/xcode/ENA/ENA/Source/Services/DCCValidationService/DCCValidationService.swift
@@ -7,6 +7,10 @@ import OpenCombine
 import HealthCertificateToolkit
 
 protocol DCCValidationProviding {
+	func availableCountries(
+		completion: @escaping (Result<[Country], DCCValidationError>) -> Void
+	)
+	
 	func validateDcc(
 		dcc: DigitalGreenCertificate,
 		issuerCountry: String,
@@ -22,14 +26,22 @@ final class DCCValidationService: DCCValidationProviding {
 	// MARK: - Init
 	
 	init(
-		vaccinationValueSetsProvider: VaccinationValueSetsProvider
+		store: Store,
+		client: Client
 	) {
-		self.vaccinationValueSetsProvider = vaccinationValueSetsProvider
+		self.store = store
+		self.client = client
 	}
 	
 	// MARK: - Overrides
 	
 	// MARK: - Protocol DCCValidationProviding
+	
+	func availableCountries(
+		completion: @escaping (Result<[Country], DCCValidationError>) -> Void
+	) {
+		
+	}
 	
 	func validateDcc(
 		dcc: DigitalGreenCertificate,
@@ -40,109 +52,15 @@ final class DCCValidationService: DCCValidationProviding {
 		completion: @escaping (Result<DCCValidationProgress, DCCValidationError>) -> Void
 	) {
 		
-		// 1. Apply technical validation
-		
-		let expirationDate = Date(timeIntervalSince1970: TimeInterval(cborWebToken.expirationTime))
-		let result = applyTechnicalValidation(validationClock: validationClock, expirationDate: expirationDate)
-		
-		switch result {
-		case let .failure(progress):
-			completion(.failure(progress))
-			
-		case let .success(progress):
-			
-			// 2. update/ download value sets
-			updateValueSets(
-				completion: { result in
-					switch result {
-					case let .failure(error):
-						completion(.failure(error))
-					case let .success(valueSets):
-						
-						// reminder: Caching of the onboarded countries list
-						break
-		
-					}
-				})
-		}
-
-		// 3. update/ download acceptance rules
-		
-		// 4. update/ download invalidation rules
-		
-		// 5. assemble external rule params
-		
-		// 6. assemble external rule params for acceptance rules
-		
-		// 7. apply acceptance rules
-		
-		// 8. assemble external rule params for invalidation rules
-		
-		// 9. apply invalidation rules
-
-		
 	}
-	
+		
 	// MARK: - Public
 	
 	// MARK: - Internal
 	
 	// MARK: - Private
 	
-	private let vaccinationValueSetsProvider: VaccinationValueSetsProvider
-	
-	private var subscriptions = Set<AnyCancellable>()
-	
-	private func applyTechnicalValidation(
-		validationClock: Date,
-		expirationDate: Date
-	) -> Result<DCCValidationProgress, DCCValidationError> {
+	private let store: Store
+	private let client: Client
 		
-		// JsonSchemaCheck is always true because we expect here a DigitalGreenCertificate, which was already json schema validated at its creation.
-		var progress = DCCValidationProgress(
-			expirationCheck: false,
-			jsonSchemaCheck: true,
-			acceptanceRuleValidation: nil,
-			invalidationRuleValidation: nil
-		)
-		
-		// Check expiration date
-		guard expirationDate <= validationClock else {
-			return .failure(DCCValidationError.TECHNICAL_VALIDATION_FAILED(progress))
-		}
-		progress.expirationCheck = true
-		
-		return .success(progress)
-	}
-	
-	private func updateValueSets(
-		completion: @escaping (Result<SAP_Internal_Dgc_ValueSets, DCCValidationError>) -> Void
-	) {
-		vaccinationValueSetsProvider.latestVaccinationCertificateValueSets()
-			.sink(
-				receiveCompletion: { result in
-					switch result {
-					case .finished:
-						break
-					case .failure(let error):
-						if case let URLSession.Response.Failure.httpError(_, response) = error {
-							switch response.statusCode {
-							case 500...509:
-								completion(.failure(.VALUE_SET_SERVER_ERROR))
-							default:
-								Log.error("Unhandled Status Code while fetching certificate value sets", log: .vaccination, error: error)
-							}
-							
-						} else if case URLSession.Response.Failure.noNetworkConnection = error {
-							completion(.failure(.NO_NETWORK))
-						}
-					}
-					
-				}, receiveValue: { valueSets in
-					completion(.success(valueSets))
-				}
-			)
-			.store(in: &subscriptions)
-	}
-	
 }
