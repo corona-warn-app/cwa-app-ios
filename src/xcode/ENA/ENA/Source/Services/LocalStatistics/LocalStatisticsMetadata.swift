29e64170404fbfaaf741fe4cf04e3ca85366381f
Naveed Khalid <nkhalid.here@gmail.com>
2021-06-30 11:12:59 +0500

    changes in metadata

--- src/xcode/ENA/ENA/Source/Services/LocalStatistics/LocalStatisticsMetadata.swift
+++ src/xcode/ENA/ENA/Source/Services/LocalStatistics/LocalStatisticsMetadata.swift
@@ -9,16 +9,19 @@ struct LocalStatisticsMetadata: Codable, Equatable {
 	// MARK: - Init
 	
 	init(with response: LocalStatisticsResponse) {
+		self.federalStateID = response.federalStateID
 		self.lastLocalStatisticsETag = response.eTag
 		self.lastLocalStatisticsFetchDate = response.timestamp
 		self.localStatistics = response.localStatistics
 	}
 
 	init(
+		federalStateID: String,
 		lastLocalStatisticsETag: String,
 		lastLocalStatisticsFetchDate: Date,
 		localStatistics: SAP_Internal_Stats_LocalStatistics
 	) {
+		self.federalStateID = federalStateID
 		self.lastLocalStatisticsETag = lastLocalStatisticsETag
 		self.lastLocalStatisticsFetchDate = lastLocalStatisticsFetchDate
 		self.localStatistics = localStatistics
@@ -27,6 +30,7 @@ struct LocalStatisticsMetadata: Codable, Equatable {
 	// MARK: - Protocol Codable
 	
 	enum CodingKeys: String, CodingKey {
+		case federalStateID
 		case lastLocalStatisticsETag
 		case lastLocalStatisticsFetchDate
 		case localStatistics
@@ -35,6 +39,7 @@ struct LocalStatisticsMetadata: Codable, Equatable {
 	init(from decoder: Decoder) throws {
 		let container = try decoder.container(keyedBy: CodingKeys.self)
 		
+		federalStateID = try container.decode(String.self, forKey: .federalStateID)
 		lastLocalStatisticsETag = try container.decode(String.self, forKey: .lastLocalStatisticsETag)
 		lastLocalStatisticsFetchDate = try container.decode(Date.self, forKey: .lastLocalStatisticsFetchDate)
 
@@ -45,6 +50,7 @@ struct LocalStatisticsMetadata: Codable, Equatable {
 	func encode(to encoder: Encoder) throws {
 		var container = encoder.container(keyedBy: CodingKeys.self)
 		
+		try container.encode(federalStateID, forKey: .federalStateID)
 		try container.encode(lastLocalStatisticsETag, forKey: .lastLocalStatisticsETag)
 		try container.encode(lastLocalStatisticsFetchDate, forKey: .lastLocalStatisticsFetchDate)
 
@@ -54,6 +60,7 @@ struct LocalStatisticsMetadata: Codable, Equatable {
 	
 	// MARK: - Internal
 	
+	var federalStateID: String
 	var lastLocalStatisticsETag: String?
 	var lastLocalStatisticsFetchDate: Date
 	var localStatistics: SAP_Internal_Stats_LocalStatistics
