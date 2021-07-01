24da35467465242bfed59afe5ba5b407ba44474b
Pascal Brause <pascal@brause.haus>
2021-07-01 09:12:33 +0200

    add test file, start over with first unit test

---
+++ src/xcode/ENA/ENA/Source/Services/DCCValidationService/__tests__/DCCValidationServiceTests.swift
@@ -0,0 +1,26 @@
+////
+// 🦠 Corona-Warn-App
+//
+
+@testable import ENA
+import XCTest
+
+class DCCValidationServiceTests: XCTestCase {
+	
+	func testGIVEN_ValidationService_WHEN_GetOnbaordedCountriesHappyCase_THEN_CountriesAreReturned() {
+	// GIVEN
+		let client = ClientMock()
+		let store = MockTestStore()
+		let validationService = DCCValidationService(
+			store: store,
+			client: client
+		)
+	
+	// WHEN
+		
+	
+	// THEN
+
+	
+	}
+}
