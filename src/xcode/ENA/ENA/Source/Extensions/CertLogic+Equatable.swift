0520ca185a643bbe3987df3bbbed6b90b6bc3365
Pascal Brause <pascal@brause.haus>
2021-07-06 16:51:58 +0200

    add another equatable

--- src/xcode/ENA/ENA/Source/Extensions/CertLogic+Equatable.swift
+++ src/xcode/ENA/ENA/Source/Extensions/CertLogic+Equatable.swift
@@ -11,3 +11,25 @@ extension ValidationResult: Equatable {
 			lhs.rule == rhs.rule
 	}
 }
+
+extension Rule: Equatable {
+	// Note: We compare nearly every property, but almost everyone. So it should be enough comparisons to be sure that the object are really equal.
+	public static func == (lhs: Rule, rhs: Rule) -> Bool {
+		return lhs.identifier == rhs.identifier &&
+			lhs.certificateType == rhs.certificateType &&
+			lhs.logic == rhs.logic &&
+			lhs.engine == rhs.engine &&
+			lhs.countryCode == rhs.countryCode &&
+			lhs.engineVersion == rhs.engineVersion &&
+			lhs.hash == rhs.hash &&
+			lhs.ruleType == rhs.ruleType &&
+			lhs.schemaVersion == rhs.schemaVersion &&
+			lhs.validFrom == rhs.validFrom &&
+			lhs.validTo == rhs.validTo &&
+			lhs.validToDate == rhs.validToDate &&
+			lhs.validFromDate == rhs.validFromDate &&
+			lhs.type == rhs.type &&
+			lhs.version == rhs.version &&
+			lhs.versionInt == rhs.versionInt
+	}
+}
