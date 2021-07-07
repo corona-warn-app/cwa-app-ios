3b1657817a408667340af6906acd11d0f6b975ce
Pascal Brause <pascal@brause.haus>
2021-07-07 16:49:06 +0200

    review change

--- src/xcode/ENA/ENA/Source/Services/HealthCertificateValidationService/Model/ValidationRulesCache.swift
+++ src/xcode/ENA/ENA/Source/Services/HealthCertificateValidationService/Model/ValidationRulesCache.swift
@@ -7,16 +7,6 @@ import class CertLogic.Rule
 
 struct ValidationRulesCache: Codable {
 	
-	// MARK: - Init
-	
-	init(
-		validationRules: [Rule],
-		lastValidationRulesETag: String
-	) {
-		self.validationRules = validationRules
-		self.lastValidationRulesETag = lastValidationRulesETag
-	}
-	
 	// MARK: - Internal
 	
 	let lastValidationRulesETag: String
