848d06579c2f4719741a5511b66df43876ef66b8
Nick Guendling <hello@nickguendling.com>
2021-07-12 14:20:37 +0200

    Configure mock for failure state

--- src/xcode/ENA/ENA/Source/Services/HealthCertificateValidationService/MockHealthCertificateValidationService.swift
+++ src/xcode/ENA/ENA/Source/Services/HealthCertificateValidationService/MockHealthCertificateValidationService.swift
@@ -5,6 +5,10 @@
 import Foundation
 import OpenCombine
 import HealthCertificateToolkit
+import SwiftyJSON
+import class CertLogic.ValidationResult
+import class CertLogic.Rule
+import class CertLogic.Description
 
 struct MockHealthCertificateValidationService: HealthCertificateValidationProviding {
 
@@ -16,12 +20,12 @@ struct MockHealthCertificateValidationService: HealthCertificateValidationProvid
 		].compactMap { $0 }
 	)
 
-	var validationResult: Result<HealthCertificateValidationReport, HealthCertificateValidationError> = .success(.validationPassed)
+	var validationResult: Result<HealthCertificateValidationReport, HealthCertificateValidationError> = .success(.validationFailed([validationResult1, validationResult2]))
 
 	func onboardedCountries(
 		completion: @escaping (Result<[Country], ValidationOnboardedCountriesError>) -> Void
 	) {
-		DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+		DispatchQueue.main.asyncAfter(deadline: .now() + 0) {
 			completion(onboardedCountriesResult)
 		}
 	}
@@ -32,9 +36,47 @@ struct MockHealthCertificateValidationService: HealthCertificateValidationProvid
 		validationClock: Date,
 		completion: @escaping (Result<HealthCertificateValidationReport, HealthCertificateValidationError>) -> Void
 	) {
-		DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+		DispatchQueue.main.asyncAfter(deadline: .now() + 0) {
 			completion(validationResult)
 		}
 	}
 
 }
+
+let validationResult1 = ValidationResult(
+	rule: Rule(
+		identifier: "TR-001",
+		type: "Acceptance",
+		version: "",
+		schemaVersion: "",
+		engine: "",
+		engineVersion: "",
+		certificateType: "",
+		description: [Description(lang: "de", desc: "Art des Tests muss Antigen-Test oder Nukleinsäureamplifikations-Test sein.")],
+		validFrom: "",
+		validTo: "",
+		affectedString: ["v.0.tg"],
+		logic: JSON(booleanLiteral: true),
+		countryCode: "IT"),
+	result: .fail,
+	validationErrors: []
+)
+
+let validationResult2 = ValidationResult(
+	rule: Rule(
+		identifier: "TR-002",
+		type: "Invalidation",
+		version: "",
+		schemaVersion: "",
+		engine: "",
+		engineVersion: "",
+		certificateType: "",
+		description: [Description(lang: "de", desc: "Ein Antigentest ist maximal 48h gültig.")],
+		validFrom: "",
+		validTo: "",
+		affectedString: ["t.0.ci"],
+		logic: JSON(booleanLiteral: true),
+		countryCode: "it"),
+	result: .open,
+	validationErrors: []
+)
