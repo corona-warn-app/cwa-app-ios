b8214cdcc0f40d2f6b04f3701279cdce3660c4d1
Carsten Knoblich <carsten@staticline.de>
2021-07-13 22:00:55 +0200

    cleanup cargo cult

--- src/xcode/ENA/ENA/Source/Scenes/Home/Cells/Statistics/Model/LocalStatisticsDistrict.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Home/Cells/Statistics/Model/LocalStatisticsDistrict.swift
@@ -5,47 +5,7 @@
 import Foundation
 
 struct LocalStatisticsDistrict: Codable, Equatable {
-	
-	// MARK: - Init
-
-	init(
-		federalState: LocalStatisticsFederalState,
-		districtName: String,
-		districtId: String
-	) {
-		self.federalState = federalState
-		self.districtName = districtName
-		self.districtId = districtId
-	}
-
-	// MARK: - Protocol Codable
-	
-	enum CodingKeys: String, CodingKey {
-		case federalState
-		case districtName
-		case districtId
-	}
-	
-	init(from decoder: Decoder) throws {
-		let container = try decoder.container(keyedBy: CodingKeys.self)
-		
-		federalState = try container.decode(LocalStatisticsFederalState.self, forKey: .federalState)
-		districtName = try container.decode(String.self, forKey: .districtName)
-		districtId = try container.decode(String.self, forKey: .districtId)
-	}
-		
-	func encode(to encoder: Encoder) throws {
-		var container = encoder.container(keyedBy: CodingKeys.self)
-		
-		try container.encode(federalState, forKey: .federalState)
-		try container.encode(districtName, forKey: .districtName)
-		try container.encode(districtId, forKey: .districtId)
-	}
-	
-	// MARK: - Internal
-	
-	var federalState: LocalStatisticsFederalState
-	var districtName: String
-	var districtId: String
-
+	let federalState: LocalStatisticsFederalState
+	let districtName: String
+	let districtId: String
 }
