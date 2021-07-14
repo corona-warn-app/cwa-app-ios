31a6864a4cefe9b42c5a9ed036511f67d18617c8
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-07-14 14:52:14 +0200

    Apply Marks in a correct way

--- src/xcode/ENA/ENA/Source/Scenes/Home/Cells/Statistics/Manage Statistics/CustomDashedView.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Home/Cells/Statistics/Manage Statistics/CustomDashedView.swift
@@ -5,44 +5,8 @@
 import UIKit
 
 class CustomDashedView: UIView {
-
-	enum Mode {
-		case add, modify
-	}
 	
-	// MARK: - Internal
-
-	@IBOutlet weak var label: ENALabel!
-	@IBOutlet weak var icon: UIImageView!
-	
-	@IBInspectable var cornerRadius: CGFloat = 15 {
-		didSet {
-			layer.cornerRadius = cornerRadius
-			layer.masksToBounds = cornerRadius > 0
-		}
-	}
-	@IBInspectable var dashWidth: CGFloat = 2
-	@IBInspectable var dashColor: UIColor = .enaColor(for: .dashedCardBorder)
-
-	/// Dash pattern - dash length
-	@IBInspectable var dashLength: CGFloat = 5
-	/// Dash pattern - gap length
-	@IBInspectable var betweenDashesSpace: CGFloat = 5
-
-	var tapHandler: (() -> Void)?
-
-	private var dashBorder: CAShapeLayer?
-	private lazy var tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(onTap))
-
-	class func instance(for mode: Mode) -> CustomDashedView {
-		let nibName = String(describing: Self.self)
-
-		guard let view = UINib(nibName: nibName, bundle: nil).instantiate(withOwner: nil, options: nil)[0] as? CustomDashedView else {
-			fatalError("Could not initialize CustomDashedView")
-		}
-		view.configure(for: mode)
-		return view
-	}
+	// MARK: - Overrides
 
 	override func layoutSubviews() {
 		super.layoutSubviews()
@@ -64,6 +28,47 @@ class CustomDashedView: UIView {
 		layer.cornerRadius = cornerRadius
 		self.dashBorder = dashBorder
 	}
+	
+	// MARK: - Internal
+
+	enum Mode {
+		case add, modify
+	}
+	
+	@IBOutlet weak var label: ENALabel!
+
+	var tapHandler: (() -> Void)?
+
+	class func instance(for mode: Mode) -> CustomDashedView {
+		let nibName = String(describing: Self.self)
+
+		guard let view = UINib(nibName: nibName, bundle: nil).instantiate(withOwner: nil, options: nil)[0] as? CustomDashedView else {
+			fatalError("Could not initialize CustomDashedView")
+		}
+		view.configure(for: mode)
+		return view
+	}
+	
+	// MARK: - Private
+	
+	@IBOutlet private weak var icon: UIImageView!
+	
+	@IBInspectable private var cornerRadius: CGFloat = 15 {
+		didSet {
+			layer.cornerRadius = cornerRadius
+			layer.masksToBounds = cornerRadius > 0
+		}
+	}
+	@IBInspectable private var dashWidth: CGFloat = 2
+	@IBInspectable private var dashColor: UIColor = .enaColor(for: .dashedCardBorder)
+
+	/// Dash pattern - dash length
+	@IBInspectable private var dashLength: CGFloat = 5
+	/// Dash pattern - gap length
+	@IBInspectable private var betweenDashesSpace: CGFloat = 5
+
+	private var dashBorder: CAShapeLayer?
+	private lazy var tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(onTap))
 
 	private func configure(for mode: Mode) {
 		switch mode {
