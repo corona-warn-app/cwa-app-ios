b3c153e795a6159afa65828a9e0d9a91dca7886f
Bastian Kohlbauer <bastian@bastian.codes>
2021-07-15 16:40:24 +0200

    add test case for sound cloud

---
+++ src/xcode/ENA/ENA/Source/Scenes/FooterViewController/__tests__/FooterViewControllerTests.swift
@@ -0,0 +1,18 @@
+////
+// 🦠 Corona-Warn-App
+//
+
+import XCTest
+@testable import ENA
+
+class FooterViewControllerTests: CWATestCase {
+
+	func testGIVEN_FooterViewControllerTests() {
+		// GIVEN
+		let model = FooterViewModel(primaryButtonName: "Button")
+		let vc = FooterViewController(model)
+		// THEN
+		XCTAssertNil(vc)
+	}
+}
+
