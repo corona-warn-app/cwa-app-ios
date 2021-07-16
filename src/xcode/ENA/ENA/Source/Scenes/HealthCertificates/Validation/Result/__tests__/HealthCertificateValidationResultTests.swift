0df3bfa5c3909d7e003c67b230ce16f18e6b338c
Pascal Brause <pascal@brause.haus>
2021-07-16 09:46:36 +0200

    ad test

--- src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/Validation/Result/__tests__/HealthCertificateValidationResultTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/Validation/Result/__tests__/HealthCertificateValidationResultTests.swift
@@ -4,8 +4,31 @@
 
 import XCTest
 @testable import ENA
+import class CertLogic.ValidationResult
 
 class HealthCertificateValidationResultTests: XCTestCase {
 
-	
+	func testGIVEN_ValidationResults_WHEN_ModelIsCreated_THEN_OpenIsIncluded() throws {
+		// GIVEN
+		let arrivalCountry = try XCTUnwrap(Country(countryCode: "FR"))
+		let arrivalDate = Date()
+		let validationResults: [ValidationResult] = []
+		
+		let healthCertificate = HealthCertificate.mock()
+		let vaccinationValueSets = VaccinationValueSetsProvider(client: CachingHTTPClientMock(), store: MockTestStore())
+		
+		// WHEN
+		let model = HealthCertificateValidationOpenViewModel(
+			arrivalCountry: arrivalCountry,
+			arrivalDate: arrivalDate,
+			validationResults: validationResults,
+			healthCertificate: healthCertificate,
+			vaccinationValueSetsProvider: vaccinationValueSets
+		)
+		
+		// THEN
+		XCTAssertEqual(model.dynamicTableViewModel.numberOfSection, 1)
+		XCTAssertEqual(model.dynamicTableViewModel.numberOfRows(section: 0), 7)
+		
+	}
 }
