c3e8e37dd974ab036688bc2976519d5e5e3717bb
Pascal Brause <pascal@brause.haus>
2021-07-16 13:40:30 +0200

    finalize test for open view model

--- src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/Validation/Result/__tests__/HealthCertificateValidationResultTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/Validation/Result/__tests__/HealthCertificateValidationResultTests.swift
@@ -5,14 +5,24 @@
 import XCTest
 @testable import ENA
 import class CertLogic.ValidationResult
+import class CertLogic.Rule
 
 class HealthCertificateValidationResultTests: XCTestCase {
-
-	func testGIVEN_ValidationResults_WHEN_ModelIsCreated_THEN_OpenIsIncluded() throws {
+	
+	func testGIVEN_ValidationResults_WHEN_OpenModelIsCreated_THEN_modelIsSetupCorrectly() throws {
 		// GIVEN
 		let arrivalCountry = try XCTUnwrap(Country(countryCode: "FR"))
 		let arrivalDate = Date()
-		let validationResults: [ValidationResult] = []
+		let expectedAcceptanceOpenValidationResult = ValidationResult.fake(rule: Rule.fake(type: "Acceptance"), result: .open)
+		let expectedInvalidationOpenValidationResult = ValidationResult.fake(rule: Rule.fake(type: "Invalidation"), result: .open)
+		let validationResults: [ValidationResult] = [
+			ValidationResult.fake(result: .passed),
+			expectedAcceptanceOpenValidationResult,
+			ValidationResult.fake(rule: Rule.fake(type: "Acceptance"), result: .open),
+			expectedInvalidationOpenValidationResult,
+			ValidationResult.fake(rule: Rule.fake(type: "Invalidation"), result: .open),
+			ValidationResult.fake(rule: Rule.fake(type: "Invalidation"), result: .open)
+		]
 		
 		let healthCertificate = HealthCertificate.mock()
 		let vaccinationValueSets = VaccinationValueSetsProvider(client: CachingHTTPClientMock(), store: MockTestStore())
@@ -28,7 +38,13 @@ class HealthCertificateValidationResultTests: XCTestCase {
 		
 		// THEN
 		XCTAssertEqual(model.dynamicTableViewModel.numberOfSection, 1)
-		XCTAssertEqual(model.dynamicTableViewModel.numberOfRows(section: 0), 7)
+		// 12 rows = 5 rows for 5 open validationResults, 7 is texts, images etc.
+		XCTAssertEqual(model.dynamicTableViewModel.numberOfRows(section: 0), 12)
 		
+		XCTAssertEqual(model.openValidationResults.count, 5)
+		XCTAssertEqual(model.openAcceptanceRuleValidationResults.count, 2)
+		XCTAssertTrue(model.openAcceptanceRuleValidationResults.contains(expectedAcceptanceOpenValidationResult))
+		XCTAssertEqual(model.openInvalidationRuleValidationResults.count, 3)
+		XCTAssertTrue(model.openInvalidationRuleValidationResults.contains(expectedInvalidationOpenValidationResult))
 	}
 }
