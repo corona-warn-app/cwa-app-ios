edb8e6c1a69f2b5679301e5e1011abcf6c6f3946
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-07-19 17:42:01 +0200

    update init to accept only the Region structure

--- src/xcode/ENA/ENA/Source/Scenes/Home/Cells/Statistics/HomeStatisticsCardView/HomeStatisticsCardViewModel.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Home/Cells/Statistics/HomeStatisticsCardView/HomeStatisticsCardViewModel.swift
@@ -51,12 +51,12 @@ class HomeStatisticsCardViewModel {
 		}
 	}
 	
-	init(administrativeUnitData: SAP_Internal_Stats_AdministrativeUnitData, district: String) {
-		let sevenDayTrend = administrativeUnitData.sevenDayIncidence
+	init(regionData: SevenDayData) {
+		let sevenDayTrend = regionData.sevenDayIncidence
 		
 		title = AppStrings.Statistics.AddCard.localCardTitle
 		titleAccessibilityIdentifier = AccessibilityIdentifiers.LocalStatistics.localStatisticsCard
-		subtitle = district
+		subtitle = regionData.regionName
 		illustrationImage = UIImage(named: "LocalIncidence")
 		primaryValue = sevenDayTrend.formattedValue
 		primaryTrendImage = sevenDayTrend.trendImage
@@ -64,7 +64,7 @@ class HomeStatisticsCardViewModel {
 		primaryTrendAccessibilityLabel = sevenDayTrend.trendAccessibilityLabel
 		primaryTrendAccessibilityValue = sevenDayTrend.trendAccessibilityValue
 
-		let updateDate = Date(timeIntervalSince1970: TimeInterval(administrativeUnitData.updatedAt))
+		let updateDate = Date(timeIntervalSince1970: TimeInterval(regionData.updatedAt))
 		primaryTitle = updateDate.formatted(
 			todayString: AppStrings.Statistics.Card.Incidence.today,
 			yesterdayString: AppStrings.Statistics.Card.Incidence.yesterday,
