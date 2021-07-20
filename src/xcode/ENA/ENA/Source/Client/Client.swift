ee6337be12724eee23409183be4ff0644df26470
Kai Teuber <Kai.Teuber@app-developer.club>
2021-07-20 12:22:51 +0200

    extened configuration and protocol

--- src/xcode/ENA/ENA/Source/Client/Client.swift
+++ src/xcode/ENA/ENA/Source/Client/Client.swift
@@ -27,6 +27,7 @@ protocol Client {
 	typealias DCCRegistrationCompletionHandler = (Result<Void, DCCErrors.RegistrationError>) -> Void
 	typealias ValidationOnboardedCountriesCompletionHandler = (Result<PackageDownloadResponse, Failure>) -> Void
 	typealias DCCRulesCompletionHandler = (Result<PackageDownloadResponse, Failure>) -> Void
+	typealias DSCListCompletionHandler = (Result<DSCListResponse, Failure>) -> Void
 	
 	// MARK: Interacting with a Client
 
@@ -225,6 +226,17 @@ protocol Client {
 		ruleType: HealthCertificateValidationRuleType,
 		completion: @escaping DCCRulesCompletionHandler
 	)
+
+	/// GET call to download list of DSCs
+	/// - Parameters:
+	///   - isFake: Flag to indicate a fake request
+	///   - completion: The completion handler of the call, which contains a DSCList or a URLSession.Response.Failure
+	func getDSCList(
+		eTag: String?,
+		isFake: Bool,
+		completion: @escaping DSCListCompletionHandler
+	)
+
 }
 
 enum SubmissionError: Error {
@@ -338,6 +350,11 @@ struct FetchedDaysAndHours {
 	var allKeyPackages: [PackageDownloadResponse] {
 		Array(hours.bucketsByHour.values) + Array(days.bucketsByDay.values)
 	}
+}
+
+struct DSCListResponse {
+	let DSCList: SAP_Internal_Dgc_DscList
+	let eTag: String?
 }
 
 extension Client {
