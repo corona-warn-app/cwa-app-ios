3cb9a7f30bf88d14d8d68e44f3ebc76dd7363fad
Bastian Kohlbauer <bastian@bastian.codes>
2021-07-27 15:42:25 +0200

    improve error comparison

--- src/xcode/ENA/ENA/Source/Services/HealthCertificateValidation/Model/HealthCertificateValidationError.swift
+++ src/xcode/ENA/ENA/Source/Services/HealthCertificateValidation/Model/HealthCertificateValidationError.swift
@@ -52,8 +52,15 @@ enum HealthCertificateValidationError: LocalizedError {
 	}
 }
 
+// swiftlint:disable pattern_matching_keywords
 extension HealthCertificateValidationError: Equatable {
 	static func == (lhs: HealthCertificateValidationError, rhs: HealthCertificateValidationError) -> Bool {
-		lhs.localizedDescription == rhs.localizedDescription
+		
+		switch (lhs, rhs) {
+		case (.TECHNICAL_VALIDATION_FAILED(let lhsExpirationDate, let lhsSignatureInvalid), .TECHNICAL_VALIDATION_FAILED(let rhsExpirationDate, let rhsSignatureInvalid)):
+			return lhsExpirationDate == rhsExpirationDate && lhsSignatureInvalid == rhsSignatureInvalid
+		default:
+			return lhs.localizedDescription == rhs.localizedDescription
+		}
 	}
 }
