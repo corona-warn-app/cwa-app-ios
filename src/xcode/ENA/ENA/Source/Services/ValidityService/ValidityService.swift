69f8c13c76b8bd341ded63ecd9bd278c33712f53
Kai Teuber <Kai.Teuber@app-developer.club>
2021-07-28 15:58:04 +0200

    added subscribeDSCListChanges

--- src/xcode/ENA/ENA/Source/Services/ValidityService/ValidityService.swift
+++ src/xcode/ENA/ENA/Source/Services/ValidityService/ValidityService.swift
@@ -3,6 +3,7 @@
 //
 
 import OpenCombine
+import HealthCertificateToolkit
 
 final class ValidityService {
 
@@ -19,6 +20,7 @@ final class ValidityService {
 
 		// we are done with init - now let's start watching for some updates
 		self.subscribeAppConfigUpdates()
+		self.subscribeDSCListChanges()
 	}
 
 	// MARK: - Overrides
@@ -36,7 +38,9 @@ final class ValidityService {
 	private let appConfigurationProvider: AppConfigurationProviding
 
 	private var subscriptions = Set<AnyCancellable>()
+
 	private var lastKnowAppConfiguration: SAP_Internal_V2_ApplicationConfigurationIOS?
+	private var lastKnownDccCertificates: [DCCSigningCertificate]?
 
 	private func subscribeAppConfigUpdates() {
 		// subscribe app config updates
@@ -45,6 +49,19 @@ final class ValidityService {
 				// only revalidate state if configuration has changed
 				if self?.lastKnowAppConfiguration != configuration {
 					self?.lastKnowAppConfiguration = configuration
+					self?.updateValidityState()
+				}
+			}
+			.store(in: &subscriptions)
+	}
+
+	private func subscribeDSCListChanges() {
+		// subscribe to changes of dcc certificates list
+		dscListProviding.signingCertificates
+			.sink { [weak self] dccCertificates in
+				// only revalidate state if configuration has changed
+				if self?.lastKnownDccCertificates != dccCertificates {
+					self?.lastKnownDccCertificates = dccCertificates
 					self?.updateValidityState()
 				}
 			}
