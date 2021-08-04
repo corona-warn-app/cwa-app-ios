53330418e09ec3ac460b22b2e08fb7a700255584
Nick Guendling <hello@nickguendling.com>
2021-08-04 12:10:13 +0200

    Extend health certified person cell model tests

--- src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/Overview/Cells/__tests__/HealthCertifiedPersonCellModelTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/Overview/Cells/__tests__/HealthCertifiedPersonCellModelTests.swift
@@ -1,4 +1,4 @@
-////
+//
 // 🦠 Corona-Warn-App
 //
 
@@ -8,25 +8,331 @@ import HealthCertificateToolkit
 
 class HealthCertifiedPersonCellModelTests: XCTestCase {
 
-	func testGIVEN_healthCertifiedPerson_THEN_IsSetupCorrectly() throws {
+	func testHealthCertifiedPersonWithValidVaccinationCertificate() throws {
 		// GIVEN
-		let healthCertificate1 = try vaccinationCertificate(daysOffset: -24, doseNumber: 1, identifier: "01DE/84503/1119349007/DXSGWLWL40SU8ZFKIYIBK39A3#S", dateOfBirth: "1988-06-07")
-
-		let healthCertifiedPerson = HealthCertifiedPerson(
-			healthCertificates: [
-				healthCertificate1
-			]
+		let healthCertificate = try HealthCertificate(
+			base45: try base45Fake(
+				from: DigitalCovidCertificate.fake(
+					vaccinationEntries: [.fake()]
+				)
+			),
+			validityState: .valid
 		)
-		let viewModel = try XCTUnwrap(HealthCertifiedPersonCellModel(healthCertifiedPerson: healthCertifiedPerson))
 
-		guard case .partiallyVaccinated = healthCertifiedPerson.vaccinationState else {
-			XCTFail("Not partiallyVaccinated")
-			return
-		}
+		let healthCertifiedPerson = HealthCertifiedPerson(healthCertificates: [healthCertificate])
+
+		let viewModel = try XCTUnwrap(
+			HealthCertifiedPersonCellModel(
+				healthCertifiedPerson: healthCertifiedPerson
+			)
+		)
 
 		// THEN
 		XCTAssertEqual(viewModel.title, AppStrings.HealthCertificate.Overview.covidTitle)
 		XCTAssertEqual(viewModel.name, healthCertifiedPerson.name?.fullName)
+
+		XCTAssertNil(viewModel.validityStateIcon)
+		XCTAssertNil(viewModel.validityStateTitle)
+	}
+
+	func testHealthCertifiedPersonWithSoonExpiringVaccinationCertificate() throws {
+		// GIVEN
+		let healthCertificate = try HealthCertificate(
+			base45: try base45Fake(
+				from: DigitalCovidCertificate.fake(
+					vaccinationEntries: [.fake()]
+				),
+				and: .fake(expirationTime: Date(timeIntervalSince1970: 1627987295))
+			),
+			validityState: .expiringSoon
+		)
+
+		let healthCertifiedPerson = HealthCertifiedPerson(healthCertificates: [healthCertificate])
+
+		let viewModel = try XCTUnwrap(
+			HealthCertifiedPersonCellModel(
+				healthCertifiedPerson: healthCertifiedPerson
+			)
+		)
+
+		// THEN
+		XCTAssertEqual(viewModel.title, AppStrings.HealthCertificate.Overview.covidTitle)
+		XCTAssertEqual(viewModel.name, healthCertifiedPerson.name?.fullName)
+
+		XCTAssertEqual(viewModel.validityStateIcon, UIImage(named: "Icon_ExpiringSoon"))
+		XCTAssertEqual(viewModel.validityStateTitle, "Zertifikat läuft am 03.08.21 um 12:41 ab")
+	}
+
+	func testHealthCertifiedPersonWithExpiredVaccinationCertificate() throws {
+		// GIVEN
+		let healthCertificate = try HealthCertificate(
+			base45: try base45Fake(
+				from: DigitalCovidCertificate.fake(
+					vaccinationEntries: [.fake()]
+				)
+			),
+			validityState: .expired
+		)
+
+		let healthCertifiedPerson = HealthCertifiedPerson(healthCertificates: [healthCertificate])
+
+		let viewModel = try XCTUnwrap(
+			HealthCertifiedPersonCellModel(
+				healthCertifiedPerson: healthCertifiedPerson
+			)
+		)
+
+		// THEN
+		XCTAssertEqual(viewModel.title, AppStrings.HealthCertificate.Overview.covidTitle)
+		XCTAssertEqual(viewModel.name, healthCertifiedPerson.name?.fullName)
+
+		XCTAssertEqual(viewModel.validityStateIcon, UIImage(named: "Icon_ExpiredInvalid"))
+		XCTAssertEqual(viewModel.validityStateTitle, "Zertifikat abgelaufen")
+	}
+
+	func testHealthCertifiedPersonWithInvalidVaccinationCertificate() throws {
+		// GIVEN
+		let healthCertificate = try HealthCertificate(
+			base45: try base45Fake(
+				from: DigitalCovidCertificate.fake(
+					vaccinationEntries: [.fake()]
+				)
+			),
+			validityState: .invalid
+		)
+
+		let healthCertifiedPerson = HealthCertifiedPerson(healthCertificates: [healthCertificate])
+
+		let viewModel = try XCTUnwrap(
+			HealthCertifiedPersonCellModel(
+				healthCertifiedPerson: healthCertifiedPerson
+			)
+		)
+
+		// THEN
+		XCTAssertEqual(viewModel.title, AppStrings.HealthCertificate.Overview.covidTitle)
+		XCTAssertEqual(viewModel.name, healthCertifiedPerson.name?.fullName)
+
+		XCTAssertEqual(viewModel.validityStateIcon, UIImage(named: "Icon_ExpiredInvalid"))
+		XCTAssertEqual(viewModel.validityStateTitle, "Zertifikat (Signatur) ungültig")
+	}
+
+	func testHealthCertifiedPersonWithValidTestCertificate() throws {
+		// GIVEN
+		let healthCertificate = try HealthCertificate(
+			base45: try base45Fake(
+				from: DigitalCovidCertificate.fake(
+					testEntries: [.fake()]
+				)
+			),
+			validityState: .valid
+		)
+
+		let healthCertifiedPerson = HealthCertifiedPerson(healthCertificates: [healthCertificate])
+
+		let viewModel = try XCTUnwrap(
+			HealthCertifiedPersonCellModel(
+				healthCertifiedPerson: healthCertifiedPerson
+			)
+		)
+
+		// THEN
+		XCTAssertEqual(viewModel.title, AppStrings.HealthCertificate.Overview.covidTitle)
+		XCTAssertEqual(viewModel.name, healthCertifiedPerson.name?.fullName)
+
+		XCTAssertNil(viewModel.validityStateIcon)
+		XCTAssertNil(viewModel.validityStateTitle)
+	}
+
+	func testHealthCertifiedPersonWithSoonExpiringTestCertificate() throws {
+		// GIVEN
+		let healthCertificate = try HealthCertificate(
+			base45: try base45Fake(
+				from: DigitalCovidCertificate.fake(
+					testEntries: [.fake()]
+				),
+				and: .fake(expirationTime: Date(timeIntervalSince1970: 1627987295))
+			),
+			validityState: .expiringSoon
+		)
+
+		let healthCertifiedPerson = HealthCertifiedPerson(healthCertificates: [healthCertificate])
+
+		let viewModel = try XCTUnwrap(
+			HealthCertifiedPersonCellModel(
+				healthCertifiedPerson: healthCertifiedPerson
+			)
+		)
+
+		// THEN
+		XCTAssertEqual(viewModel.title, AppStrings.HealthCertificate.Overview.covidTitle)
+		XCTAssertEqual(viewModel.name, healthCertifiedPerson.name?.fullName)
+
+		XCTAssertNil(viewModel.validityStateIcon)
+		XCTAssertNil(viewModel.validityStateTitle)
+	}
+
+	func testHealthCertifiedPersonWithExpiredTestCertificate() throws {
+		// GIVEN
+		let healthCertificate = try HealthCertificate(
+			base45: try base45Fake(
+				from: DigitalCovidCertificate.fake(
+					testEntries: [.fake()]
+				)
+			),
+			validityState: .expired
+		)
+
+		let healthCertifiedPerson = HealthCertifiedPerson(healthCertificates: [healthCertificate])
+
+		let viewModel = try XCTUnwrap(
+			HealthCertifiedPersonCellModel(
+				healthCertifiedPerson: healthCertifiedPerson
+			)
+		)
+
+		// THEN
+		XCTAssertEqual(viewModel.title, AppStrings.HealthCertificate.Overview.covidTitle)
+		XCTAssertEqual(viewModel.name, healthCertifiedPerson.name?.fullName)
+
+		XCTAssertNil(viewModel.validityStateIcon)
+		XCTAssertNil(viewModel.validityStateTitle)
+	}
+
+	func testHealthCertifiedPersonWithInvalidTestCertificate() throws {
+		// GIVEN
+		let healthCertificate = try HealthCertificate(
+			base45: try base45Fake(
+				from: DigitalCovidCertificate.fake(
+					testEntries: [.fake()]
+				)
+			),
+			validityState: .invalid
+		)
+
+		let healthCertifiedPerson = HealthCertifiedPerson(healthCertificates: [healthCertificate])
+
+		let viewModel = try XCTUnwrap(
+			HealthCertifiedPersonCellModel(
+				healthCertifiedPerson: healthCertifiedPerson
+			)
+		)
+
+		// THEN
+		XCTAssertEqual(viewModel.title, AppStrings.HealthCertificate.Overview.covidTitle)
+		XCTAssertEqual(viewModel.name, healthCertifiedPerson.name?.fullName)
+
+		XCTAssertEqual(viewModel.validityStateIcon, UIImage(named: "Icon_ExpiredInvalid"))
+		XCTAssertEqual(viewModel.validityStateTitle, "Zertifikat (Signatur) ungültig")
+	}
+
+	func testHealthCertifiedPersonWithValidRecoveryCertificate() throws {
+		// GIVEN
+		let healthCertificate = try HealthCertificate(
+			base45: try base45Fake(
+				from: DigitalCovidCertificate.fake(
+					recoveryEntries: [.fake()]
+				)
+			),
+			validityState: .valid
+		)
+
+		let healthCertifiedPerson = HealthCertifiedPerson(healthCertificates: [healthCertificate])
+
+		let viewModel = try XCTUnwrap(
+			HealthCertifiedPersonCellModel(
+				healthCertifiedPerson: healthCertifiedPerson
+			)
+		)
+
+		// THEN
+		XCTAssertEqual(viewModel.title, AppStrings.HealthCertificate.Overview.covidTitle)
+		XCTAssertEqual(viewModel.name, healthCertifiedPerson.name?.fullName)
+
+		XCTAssertNil(viewModel.validityStateIcon)
+		XCTAssertNil(viewModel.validityStateTitle)
+	}
+
+	func testHealthCertifiedPersonWithSoonExpiringRecoveryCertificate() throws {
+		// GIVEN
+		let healthCertificate = try HealthCertificate(
+			base45: try base45Fake(
+				from: DigitalCovidCertificate.fake(
+					recoveryEntries: [.fake()]
+				),
+				and: .fake(expirationTime: Date(timeIntervalSince1970: 1627987295))
+			),
+			validityState: .expiringSoon
+		)
+
+		let healthCertifiedPerson = HealthCertifiedPerson(healthCertificates: [healthCertificate])
+
+		let viewModel = try XCTUnwrap(
+			HealthCertifiedPersonCellModel(
+				healthCertifiedPerson: healthCertifiedPerson
+			)
+		)
+
+		// THEN
+		XCTAssertEqual(viewModel.title, AppStrings.HealthCertificate.Overview.covidTitle)
+		XCTAssertEqual(viewModel.name, healthCertifiedPerson.name?.fullName)
+
+		XCTAssertEqual(viewModel.validityStateIcon, UIImage(named: "Icon_ExpiringSoon"))
+		XCTAssertEqual(viewModel.validityStateTitle, "Zertifikat läuft am 03.08.21 um 12:41 ab")
+	}
+
+	func testHealthCertifiedPersonWithExpiredRecoveryCertificate() throws {
+		// GIVEN
+		let healthCertificate = try HealthCertificate(
+			base45: try base45Fake(
+				from: DigitalCovidCertificate.fake(
+					recoveryEntries: [.fake()]
+				)
+			),
+			validityState: .expired
+		)
+
+		let healthCertifiedPerson = HealthCertifiedPerson(healthCertificates: [healthCertificate])
+
+		let viewModel = try XCTUnwrap(
+			HealthCertifiedPersonCellModel(
+				healthCertifiedPerson: healthCertifiedPerson
+			)
+		)
+
+		// THEN
+		XCTAssertEqual(viewModel.title, AppStrings.HealthCertificate.Overview.covidTitle)
+		XCTAssertEqual(viewModel.name, healthCertifiedPerson.name?.fullName)
+
+		XCTAssertEqual(viewModel.validityStateIcon, UIImage(named: "Icon_ExpiredInvalid"))
+		XCTAssertEqual(viewModel.validityStateTitle, "Zertifikat abgelaufen")
+	}
+
+	func testHealthCertifiedPersonWithInvalidRecoveryCertificate() throws {
+		// GIVEN
+		let healthCertificate = try HealthCertificate(
+			base45: try base45Fake(
+				from: DigitalCovidCertificate.fake(
+					recoveryEntries: [.fake()]
+				)
+			),
+			validityState: .invalid
+		)
+
+		let healthCertifiedPerson = HealthCertifiedPerson(healthCertificates: [healthCertificate])
+
+		let viewModel = try XCTUnwrap(
+			HealthCertifiedPersonCellModel(
+				healthCertifiedPerson: healthCertifiedPerson
+			)
+		)
+
+		// THEN
+		XCTAssertEqual(viewModel.title, AppStrings.HealthCertificate.Overview.covidTitle)
+		XCTAssertEqual(viewModel.name, healthCertifiedPerson.name?.fullName)
+
+		XCTAssertEqual(viewModel.validityStateIcon, UIImage(named: "Icon_ExpiredInvalid"))
+		XCTAssertEqual(viewModel.validityStateTitle, "Zertifikat (Signatur) ungültig")
 	}
 
 }
