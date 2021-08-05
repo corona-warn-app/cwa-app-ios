6cdd7109e4324f22301162f167921b596a6de55c
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-08-06 01:25:43 +0200

    Add CBOR trimming extension

---
+++ src/xcode/ENA/HealthCertificateToolkit/Sources/HealthCertificateToolkit/Utils/CBOR+CBORMap.swift
@@ -0,0 +1,41 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+
+extension Dictionary where Key == CBOR, Value == CBOR {
+    var cborMapWithTrimmingWhiteSpaces: [CBOR: CBOR] {
+        var anyMap = [CBOR: CBOR]()
+        for (key, value) in self {
+            anyMap[key] = value.cborValue
+        }
+        return anyMap
+    }
+}
+
+extension CBOR {
+
+    var cborValue: CBOR? {
+        switch self {
+        case .utf8String(let stringValue):
+            let trimmedString = stringValue.trimmingCharacters(in: .whitespacesAndNewlines)
+            return CBOR(stringLiteral: trimmedString)
+            
+        case .map(let cborDictionary):
+            let trimmedDictionary = cborDictionary.cborMapWithTrimmingWhiteSpaces
+            return .map(trimmedDictionary)
+            
+        case .array(let arrayValue):
+            let trimmedArray = arrayValue.compactMap { $0.cborValue }
+            return .array(trimmedArray)
+            
+        case .null:
+            // if the value is null we will remove both the key and value from the dictionary
+            return nil
+            
+        default:
+            return self
+        }
+    }
+}
