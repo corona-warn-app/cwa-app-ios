ad3b803d61e9b64b5b2ad7df791c6d24ca1efce7
Kai Teuber <Kai.Teuber@app-developer.club>
2021-08-05 15:15:06 +0200

    fixed appfeature defaults tests

--- src/xcode/ENA/ENA/Source/Services/Risk/Provider/Helper/__tests__/AppConfiguration+FeaturesTests.swift
+++ src/xcode/ENA/ENA/Source/Services/Risk/Provider/Helper/__tests__/AppConfiguration+FeaturesTests.swift
@@ -8,15 +8,18 @@ import ZIPFoundation
 
 class AppConfiguration_FeaturesTests: XCTestCase {
 
+	// no longer needed?
+
 	func testGIVEN_DefaultAppConfiguration_WHEN_getFeatures_THEN_allMatchDefaultValues() throws {
 		let url = try XCTUnwrap(Bundle.main.url(forResource: "default_app_config_270", withExtension: ""))
 		let data = try Data(contentsOf: url)
 		let zip = try XCTUnwrap(Archive(data: data, accessMode: .read))
 		let config = try zip.extractAppConfiguration()
+		let featureProvider = AppFeatureProvider(appConfigurationProvider: CachedAppConfigurationMock(with: config))
 
 		// WHEN
-		let disableDeviceTimeCheck = config.value(for: .disableDeviceTimeCheck)
-		let unencryptedCheckinsEnabled = config.value(for: .unencryptedCheckinsEnabled)
+		let disableDeviceTimeCheck = featureProvider.value(for: .disableDeviceTimeCheck)
+		let unencryptedCheckinsEnabled = featureProvider.value(for: .unencryptedCheckinsEnabled)
 
 		// THEN
 		XCTAssertFalse(disableDeviceTimeCheck)
