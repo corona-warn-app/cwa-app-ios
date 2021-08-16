36faceae083960e9c4d0bf23c8f67581c5897a80
Artur Friesen <mail@arturfriesen.de>
2021-08-16 17:13:24 +0200

    Add state for onboarding gate keeping.

--- src/xcode/ENA/ENA/Source/Scenes/Home/HomeTableViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Home/HomeTableViewController.swift
@@ -336,6 +336,7 @@ class HomeTableViewController: UITableViewController, NavigationBarOpacityDelega
 	private var onDismissDistrict: (Bool) -> Void
 
 	private var deltaOnboardingCoordinator: DeltaOnboardingCoordinator?
+	private var onboardingIsRunning = false
 	private var riskCell: UITableViewCell?
 	private var pcrTestResultCell: UITableViewCell?
 	private var pcrTestShownPositiveResultCell: UITableViewCell?
@@ -697,11 +698,12 @@ class HomeTableViewController: UITableViewController, NavigationBarOpacityDelega
 	}
 
 	private func showDeltaOnboardingIfNeeded(completion: @escaping () -> Void = {}) {
-		guard deltaOnboardingCoordinator == nil else {
+		guard !onboardingIsRunning else {
 			Log.debug("Skip onboarding call, because onboarding is already running.")
 			completion()
 			return
 		}
+		self.onboardingIsRunning = true
 
 		appConfigurationProvider.appConfiguration().sink { [weak self] configuration in
 			guard let self = self else {
@@ -713,12 +715,12 @@ class HomeTableViewController: UITableViewController, NavigationBarOpacityDelega
 			let supportedCountries = configuration.supportedCountries.compactMap({ Country(countryCode: $0) })
 
 			/// As per feature requirement, the delta onboarding should appear with a slight delay of 0.5
-			var delay = 0.5
+			var delay = 10.5
 
 			#if DEBUG
 			if isUITesting {
 				/// In UI Testing we need to increase the delay slightly again. Otherwise UI Tests fail.
-				delay = 1.5
+				delay = 11.5
 			}
 			#endif
 
@@ -735,14 +737,14 @@ class HomeTableViewController: UITableViewController, NavigationBarOpacityDelega
 			DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
 				guard self.presentedViewController == nil else {
 					Log.debug("Don't show onboarding this time, because another view controller is currently presented.")
-					self.deltaOnboardingCoordinator = nil
+					self.onboardingIsRunning = false
 					completion()
 					return
 				}
 
 				self.deltaOnboardingCoordinator?.finished = { [weak self] in
 					Log.debug("Onboarding finished.")
-					self?.deltaOnboardingCoordinator = nil
+					self?.onboardingIsRunning = false
 					completion()
 				}
 
