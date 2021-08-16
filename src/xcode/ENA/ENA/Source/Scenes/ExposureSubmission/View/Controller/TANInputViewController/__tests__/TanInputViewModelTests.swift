d60d442224b12e21f4d5b823071378159ba96f81
Nick Guendling <hello@nickguendling.com>
2021-08-16 16:19:48 +0200

    Extend tan input view model tests

--- src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/View/Controller/TANInputViewController/__tests__/TanInputViewModelTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/View/Controller/TANInputViewController/__tests__/TanInputViewModelTests.swift
@@ -8,11 +8,29 @@ import HealthCertificateToolkit
 
 class TanInputViewModelTests: CWATestCase {
 
-	func testGIVEN_ValidFormattedTanWithValidChecksum_WHEN_isValidIsChecksumValid_THEN_isInvalidChcksumIsValid() {
+	func testGIVEN_TitleAndDescription_WHEN_GettingTitleAndDescription_THEN_CorrectTitleAndDescriptionAreReturned() {
 		// GIVEN
 		let viewModel = TanInputViewModel(
 			title: "Title",
 			description: "Description",
+			onPrimaryButtonTap: { _, _ in },
+			givenTan: ""
+		)
+
+		// WHEN
+		let title = viewModel.title
+		let description = viewModel.description
+
+		// THEN
+		XCTAssertEqual(title, "Title")
+		XCTAssertEqual(description, "Description")
+	}
+
+	func testGIVEN_ValidFormattedTanWithValidChecksum_WHEN_isValidIsChecksumValid_THEN_isInvalidChecksumIsValid() {
+		// GIVEN
+		let viewModel = TanInputViewModel(
+			title: "",
+			description: "",
 			onPrimaryButtonTap: { _, _ in },
 			givenTan: "234567893D"
 		)
@@ -26,11 +44,11 @@ class TanInputViewModelTests: CWATestCase {
 		XCTAssertTrue(isChecksumValid, "tan checksum is invalid")
 	}
 
-	func testGIVEN_ValidFormattedTanWithInvalidChecksum_WHEN_isValidIsChecksumValid_THEN_isInvalidChcksumIsInvalid() {
+	func testGIVEN_ValidFormattedTanWithInvalidChecksum_WHEN_isValidIsChecksumValid_THEN_isInvalidChecksumIsInvalid() {
 		// GIVEN
 		let viewModel = TanInputViewModel(
-			title: "Title",
-			description: "Description",
+			title: "",
+			description: "",
 			onPrimaryButtonTap: { _, _ in },
 			givenTan: "ZBYKEVDBNU"
 		)
@@ -47,8 +65,8 @@ class TanInputViewModelTests: CWATestCase {
 	func testGIVEN_wrongCharacterTanString_WHEN_isValidCheck_THEN_isInvalidChecksumIsInvalid() {
 		// GIVEN
 		let viewModel = TanInputViewModel(
-			title: "Title",
-			description: "Description",
+			title: "",
+			description: "",
 			onPrimaryButtonTap: { _, _ in },
 			givenTan: "ZBYKEVDBNL"
 		)
