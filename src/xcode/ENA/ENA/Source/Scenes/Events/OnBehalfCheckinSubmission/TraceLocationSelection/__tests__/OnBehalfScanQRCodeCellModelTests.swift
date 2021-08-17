5e2217aefc71516573a5fb62a73749e3c5bb8d2c
Nick Guendling <hello@nickguendling.com>
2021-08-17 11:21:26 +0200

    Add OnBehalfScanQRCodeCellModel tests

--- src/xcode/ENA/ENA/Source/Scenes/Events/OnBehalfCheckinSubmission/TraceLocationSelection/__tests__/OnBehalfScanQRCodeCellModelTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Events/OnBehalfCheckinSubmission/TraceLocationSelection/__tests__/OnBehalfScanQRCodeCellModelTests.swift
@@ -5,27 +5,55 @@
 import XCTest
 @testable import ENA
 
-class AddCheckinCellModelTests: CWATestCase {
+class OnBehalfScanQRCodeCellModelTests: CWATestCase {
 
 	func testGIVEN_CellModel_THEN_InitialStateIsAsExpected() {
 		// GIVEN
-		let cellViewModel = AddCheckinCellModel()
+		let cellViewModel = OnBehalfScanQRCodeCellModel()
 
 		// THEN
-		XCTAssertEqual(cellViewModel.textColorPublisher.value, UIColor.enaColor(for: .textPrimary1))
-		XCTAssertEqual(cellViewModel.iconImagePublisher.value, UIImage(named: "Icons_qrScan"))
+		XCTAssertEqual(
+			cellViewModel.text,
+			AppStrings.OnBehalfCheckinSubmission.TraceLocationSelection.scanButtonTitle
+		)
+		XCTAssertEqual(
+			cellViewModel.textColorPublisher.value,
+			UIColor.enaColor(for: .textPrimary1)
+		)
+		XCTAssertEqual(
+			cellViewModel.iconImagePublisher.value,
+			UIImage(named: "Icons_qrScan")
+		)
+		XCTAssertEqual(
+			cellViewModel.accessibilityTraitsPublisher.value,
+			[.button]
+		)
 	}
 
 	func testGIVEN_CellModel_WHEN_SetEnabled_THEN_TextColorAndButtonStateChange() {
 		// GIVEN
-		let cellViewModel = AddCheckinCellModel()
+		let cellViewModel = OnBehalfScanQRCodeCellModel()
 
 		// WHEN
 		cellViewModel.setEnabled(false)
 
 		// THEN
-		XCTAssertEqual(cellViewModel.textColorPublisher.value, UIColor.enaColor(for: .textPrimary2))
-		XCTAssertEqual(cellViewModel.iconImagePublisher.value, UIImage(named: "Icons_qrScan_Grey"))
+		XCTAssertEqual(
+			cellViewModel.text,
+			AppStrings.OnBehalfCheckinSubmission.TraceLocationSelection.scanButtonTitle
+		)
+		XCTAssertEqual(
+			cellViewModel.textColorPublisher.value,
+			UIColor.enaColor(for: .textPrimary2)
+		)
+		XCTAssertEqual(
+			cellViewModel.iconImagePublisher.value,
+			UIImage(named: "Icons_qrScan_Grey")
+		)
+		XCTAssertEqual(
+			cellViewModel.accessibilityTraitsPublisher.value,
+			[.button, .notEnabled]
+		)
 	}
 
 }
