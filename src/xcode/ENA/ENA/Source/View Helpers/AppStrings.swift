e22030e5e4cdf55627d7c549a94af8c51ec08ee8
Nick Guendling <hello@nickguendling.com>
2021-08-19 15:43:37 +0200

    Use correct key for localizable string

--- src/xcode/ENA/ENA/Source/View Helpers/AppStrings.swift
+++ src/xcode/ENA/ENA/Source/View Helpers/AppStrings.swift
@@ -1944,7 +1944,7 @@ enum AppStrings {
 
 		enum Error {
 			static let failed = NSLocalizedString("OnBehalfCheckinSubmissionError_Failed", comment: "")
-			static let invalidTAN = NSLocalizedString("OnBehalfCheckinSubmissionError_TAN", comment: "")
+			static let invalidTAN = NSLocalizedString("OnBehalfCheckinSubmissionError_InvalidTAN", comment: "")
 			static let tryAgain = NSLocalizedString("OnBehalfCheckinSubmissionError_TryAgain", comment: "")
 			static let noNetwork = NSLocalizedString("OnBehalfCheckinSubmissionError_NoNetwork", comment: "")
 		}
