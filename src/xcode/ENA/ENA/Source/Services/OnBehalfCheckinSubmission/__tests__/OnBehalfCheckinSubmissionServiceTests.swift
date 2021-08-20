91fd1b4ce4a6d918c72e5ccdfc9f58b8721984e6
Nick Guendling <hello@nickguendling.com>
2021-08-20 10:05:31 +0200

    Test on behalf submission request errors

--- src/xcode/ENA/ENA/Source/Services/OnBehalfCheckinSubmission/__tests__/OnBehalfCheckinSubmissionServiceTests.swift
+++ src/xcode/ENA/ENA/Source/Services/OnBehalfCheckinSubmission/__tests__/OnBehalfCheckinSubmissionServiceTests.swift
@@ -301,4 +301,164 @@ class OnBehalfCheckinSubmissionServiceTests: CWATestCase {
 		waitForExpectations(timeout: .short)
 	}
 
+	func testSubmissionWithSubmissionRequestInvalidPayloadOrHeadersError() {
+		let client = ClientMock()
+
+		let submitOnBehalfExpectation = expectation(description: "getRegistrationTokenExpectation called")
+		client.onSubmitOnBehalf = { _, _, completion in
+			completion(.failure(.invalidPayloadOrHeaders))
+			submitOnBehalfExpectation.fulfill()
+		}
+
+		let service = OnBehalfCheckinSubmissionService(
+			client: client,
+			appConfigurationProvider: CachedAppConfigurationMock()
+		)
+
+		let completionExpectation = expectation(description: "completion called")
+		service.submit(checkin: .mock(), teleTAN: "2222222223") { result in
+			switch result {
+			case .success:
+				XCTFail("Expected failure")
+			case .failure(let error):
+				XCTAssertEqual(error, .submissionError(.invalidPayloadOrHeaders))
+				XCTAssertEqual(
+					error.localizedDescription,
+					"Ein Fehler ist aufgetreten. Bitte kontaktieren Sie die technische Hotline über App-Informationen -> Technische Hotline. (SUBMISSION_OB_CLIENT_ERROR)"
+				)
+			}
+			completionExpectation.fulfill()
+		}
+
+		waitForExpectations(timeout: .short)
+	}
+
+	func testSubmissionWithSubmissionRequestInvalidTanError() {
+		let client = ClientMock()
+
+		let submitOnBehalfExpectation = expectation(description: "getRegistrationTokenExpectation called")
+		client.onSubmitOnBehalf = { _, _, completion in
+			completion(.failure(.invalidTan))
+			submitOnBehalfExpectation.fulfill()
+		}
+
+		let service = OnBehalfCheckinSubmissionService(
+			client: client,
+			appConfigurationProvider: CachedAppConfigurationMock()
+		)
+
+		let completionExpectation = expectation(description: "completion called")
+		service.submit(checkin: .mock(), teleTAN: "2222222223") { result in
+			switch result {
+			case .success:
+				XCTFail("Expected failure")
+			case .failure(let error):
+				XCTAssertEqual(error, .submissionError(.invalidTan))
+				XCTAssertEqual(
+					error.localizedDescription,
+					"Ein Fehler ist aufgetreten. Bitte kontaktieren Sie die technische Hotline über App-Informationen -> Technische Hotline. (SUBMISSION_OB_CLIENT_ERROR)"
+				)
+			}
+			completionExpectation.fulfill()
+		}
+
+		waitForExpectations(timeout: .short)
+	}
+
+	func testSubmissionWithSubmissionRequestError40x() {
+		let client = ClientMock()
+
+		let submitOnBehalfExpectation = expectation(description: "getRegistrationTokenExpectation called")
+		client.onSubmitOnBehalf = { _, _, completion in
+			completion(.failure(.other(.serverError(400))))
+			submitOnBehalfExpectation.fulfill()
+		}
+
+		let service = OnBehalfCheckinSubmissionService(
+			client: client,
+			appConfigurationProvider: CachedAppConfigurationMock()
+		)
+
+		let completionExpectation = expectation(description: "completion called")
+		service.submit(checkin: .mock(), teleTAN: "2222222223") { result in
+			switch result {
+			case .success:
+				XCTFail("Expected failure")
+			case .failure(let error):
+				XCTAssertEqual(error, .submissionError(.other(.serverError(400))))
+				XCTAssertEqual(
+					error.localizedDescription,
+					"Ein Fehler ist aufgetreten. Bitte kontaktieren Sie die technische Hotline über App-Informationen -> Technische Hotline. (SUBMISSION_OB_CLIENT_ERROR)"
+				)
+			}
+			completionExpectation.fulfill()
+		}
+
+		waitForExpectations(timeout: .short)
+	}
+
+	func testSubmissionWithSubmissionRequestError50x() {
+		let client = ClientMock()
+
+		let submitOnBehalfExpectation = expectation(description: "getRegistrationTokenExpectation called")
+		client.onSubmitOnBehalf = { _, _, completion in
+			completion(.failure(.other(.serverError(500))))
+			submitOnBehalfExpectation.fulfill()
+		}
+
+		let service = OnBehalfCheckinSubmissionService(
+			client: client,
+			appConfigurationProvider: CachedAppConfigurationMock()
+		)
+
+		let completionExpectation = expectation(description: "completion called")
+		service.submit(checkin: .mock(), teleTAN: "2222222223") { result in
+			switch result {
+			case .success:
+				XCTFail("Expected failure")
+			case .failure(let error):
+				XCTAssertEqual(error, .submissionError(.other(.serverError(500))))
+				XCTAssertEqual(
+					error.localizedDescription,
+					"Ein Fehler ist aufgetreten. Bitte versuchen Sie es später noch einmal oder kontaktieren Sie die technische Hotline über App-Informationen -> Technische Hotline. (SUBMISSION_OB_SERVER_ERROR)"
+				)
+			}
+			completionExpectation.fulfill()
+		}
+
+		waitForExpectations(timeout: .short)
+	}
+
+	func testSubmissionWithSubmissionRequestNoNetworkError() {
+		let client = ClientMock()
+
+		let submitOnBehalfExpectation = expectation(description: "getRegistrationTokenExpectation called")
+		client.onSubmitOnBehalf = { _, _, completion in
+			completion(.failure(.other(.noNetworkConnection)))
+			submitOnBehalfExpectation.fulfill()
+		}
+
+		let service = OnBehalfCheckinSubmissionService(
+			client: client,
+			appConfigurationProvider: CachedAppConfigurationMock()
+		)
+
+		let completionExpectation = expectation(description: "completion called")
+		service.submit(checkin: .mock(), teleTAN: "2222222223") { result in
+			switch result {
+			case .success:
+				XCTFail("Expected failure")
+			case .failure(let error):
+				XCTAssertEqual(error, .submissionError(.other(.noNetworkConnection)))
+				XCTAssertEqual(
+					error.localizedDescription,
+					"Ihre Internetverbindung wurde unterbrochen. Bitte prüfen Sie die Verbindung und versuchen Sie es erneut. (SUBMISSION_OB_NO_NETWORK)"
+				)
+			}
+			completionExpectation.fulfill()
+		}
+
+		waitForExpectations(timeout: .short)
+	}
+
 }
