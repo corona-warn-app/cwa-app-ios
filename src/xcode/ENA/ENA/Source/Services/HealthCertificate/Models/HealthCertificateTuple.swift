65676770f1a6c814388fe216d1b26db64d9b4b40
Naveed Khalid <nkhalid.here@gmail.com>
2021-08-24 18:39:59 +0500

    adding marks

--- src/xcode/ENA/ENA/Source/Services/HealthCertificate/Models/HealthCertificateTuple.swift
+++ src/xcode/ENA/ENA/Source/Services/HealthCertificate/Models/HealthCertificateTuple.swift
@@ -7,10 +7,14 @@ import HealthCertificateToolkit
 
 public struct HealthCertificateTuple {
 	
+	// MARK: - Init
+
 	init(certificate: HealthCertificate, certifiedPerson: HealthCertifiedPerson) {
 		self.certificate = certificate
 		self.certifiedPerson = certifiedPerson
 	}
+	
+	// MARK: - Public
 
 	public let certificate: HealthCertificate
 	public let certifiedPerson: HealthCertifiedPerson
