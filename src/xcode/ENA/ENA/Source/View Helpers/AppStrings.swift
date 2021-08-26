62a30233880929c32dd02e8240721733b0bc76e5
Kai Teuber <Kai.Teuber@app-developer.club>
2021-08-26 11:25:33 +0200

    use generic notification text

--- src/xcode/ENA/ENA/Source/View Helpers/AppStrings.swift
+++ src/xcode/ENA/ENA/Source/View Helpers/AppStrings.swift
@@ -1027,10 +1027,8 @@ enum AppStrings {
 		static let detectExposureBody = NSLocalizedString("local_notifications_detectexposure_body", comment: "")
 		static let testResultsTitle = NSLocalizedString("local_notifications_testresults_title", comment: "")
 		static let testResultsBody = NSLocalizedString("local_notifications_testresults_body", comment: "")
-		static let expiringSoonTitle = NSLocalizedString("local_notifications_expiringSoon_title", comment: "")
-		static let expiringSoonBody = NSLocalizedString("local_notifications_expiringSoon_body", comment: "")
-		static let expiredTitle = NSLocalizedString("local_notifications_expired_title", comment: "")
-		static let expiredBody = NSLocalizedString("local_notifications_expired_body", comment: "")
+		static let certificateGenericTitle = NSLocalizedString("local_notifications_certificate_title", comment: "")
+		static let certificateGenericBody = NSLocalizedString("local_notifications_certificate_body", comment: "")
 	}
 
 	enum RiskLegend {
