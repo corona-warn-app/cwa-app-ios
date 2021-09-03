b9c090d1cb682ddd12f1b6e2804891c9368997ed
Pascal Brause <pascal@brause.haus>
2021-09-03 10:33:11 +0200

    add validationRulesCountries

---
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/Locators/ValidationOnboardedCountries.swift
@@ -0,0 +1,22 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+
+extension Locator {
+
+	static func validationOnboardedCountries(
+		isFake: Bool
+	) -> Locator {
+		let fake = String(isFake ? 1 : 0)
+		return Locator(
+			endpoint: .distribution,
+			paths: ["version", "v1", "ehn-dgc", "onboarded-countries"],
+			method: .get,
+			defaultHeaders: [fake: "cwa-fake", String.getRandomString(of: 14): "cwa-header-padding"],
+			type: .caching
+		)
+	}
+
+}
