be120edbdf940ff4608469d15d6d762b1e8b3c18
Pascal Brause <pascal@brause.haus>
2021-09-03 09:47:05 +0200

    add test result

---
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/Locators/TestResult.swift
@@ -0,0 +1,24 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+
+extension Locator {
+
+	static func testResult(
+		registrationToken: String,
+		isFake: Bool
+	) -> Locator {
+		let fake = String(isFake ? 1 : 0)
+		return Locator(
+			endpoint: .distribution,
+			paths: ["version", "v1", "testresult"],
+			method: .post,
+			defaultHeaders: [fake: "cwa-fake", String.getRandomString(of: 7): "cwa-header-padding"],
+			// TODO: Body is missing here
+			type: .default
+		)
+	}
+
+}
