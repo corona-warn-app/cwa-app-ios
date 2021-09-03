ab872322f6b614b976550f37d0bdfd9f6eae10fd
Pascal Brause <pascal@brause.haus>
2021-09-03 10:35:12 +0200

    add dccrules

---
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/Locators/DCCRules.swift
@@ -0,0 +1,23 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+
+extension Locator {
+
+	static func DCCRules(
+		rulePath: String
+		isFake: Bool
+	) -> Locator {
+		let fake = String(isFake ? 1 : 0)
+		return Locator(
+			endpoint: .distribution,
+			paths: ["version", "v1", "ehn-dgc", rulePath],
+			method: .get,
+			defaultHeaders: [fake: "cwa-fake", String.getRandomString(of: 14): "cwa-header-padding"],
+			type: .caching
+		)
+	}
+
+}
