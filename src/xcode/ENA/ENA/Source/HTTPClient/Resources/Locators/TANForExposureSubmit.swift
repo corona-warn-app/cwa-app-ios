68f99d0b2b5e2d2b7e9205f4242f07aa4dce6b7c
Pascal Brause <pascal@brause.haus>
2021-09-03 09:47:22 +0200

    add tansubmit

---
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/Locators/TANForExposureSubmit.swift
@@ -0,0 +1,24 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+
+extension Locator {
+
+	static func tanForExposureSubmit(
+		registrationToken: String,
+		isFake: Bool
+	) -> Locator {
+		let fake = String(isFake ? 1 : 0)
+		return Locator(
+			endpoint: .distribution,
+			paths: ["version", "v1", "tan"],
+			method: .post,
+			defaultHeaders: [fake: "cwa-fake", String.getRandomString(of: 14): "cwa-header-padding"],
+			// TODO: Body is missing here
+			type: .default
+		)
+	}
+
+}
