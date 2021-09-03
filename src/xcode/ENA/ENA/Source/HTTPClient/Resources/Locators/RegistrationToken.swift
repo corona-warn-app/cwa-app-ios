3eefa24cc81c03e6975b7b79d20b3fa4515b2e8c
Pascal Brause <pascal@brause.haus>
2021-09-03 09:46:49 +0200

    add reg token

---
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/Locators/RegistrationToken.swift
@@ -0,0 +1,23 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+
+extension Locator {
+
+	static func registrationToken(
+		isFake: Bool
+	) -> Locator {
+		let fake = String(isFake ? 1 : 0)
+		return Locator(
+			endpoint: .distribution,
+			paths: ["version", "v1", "diagnosis-keys", "registrationToken"],
+			method: .post,
+			defaultHeaders: [fake: "cwa-fake", "": "cwa-header-padding"],
+			// TODO: Body is missing here
+			type: .default
+		)
+	}
+
+}
