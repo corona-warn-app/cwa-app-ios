5c4ba0820a68167e96999e6332a8d6e95670a48e
Pascal Brause <pascal@brause.haus>
2021-09-03 09:47:45 +0200

    add on behalf

---
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/Locators/SubmitOnBehalf.swift
@@ -0,0 +1,27 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+
+extension Locator {
+
+	static func submitOnBehalf(
+		payload: SubmissionPayload,
+		isFake: Bool
+	) -> Locator {
+		let fake = String(isFake ? 1 : 0)
+		let fakePadding = isFake ? String.getRandomString(of: 36) : ""
+		return Locator(
+			endpoint: .distribution,
+			paths: ["version", "v1", "submission-on-behalf"],
+			method: .post,
+			defaultHeaders: [payload.tan, "cwa-authorization", fake: "cwa-fake", fakePadding: "cwa-header-padding",
+			],
+			// TODO: "application/x-protobuf" as Content Type?
+			// TODO: Body (Protobuf) is missing here
+			type: .default
+		)
+	}
+
+}
