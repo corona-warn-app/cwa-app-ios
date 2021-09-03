fbd695b3757b8554dbcd450e3bb7295ef571120f
Pascal Brause <pascal@brause.haus>
2021-09-03 09:52:37 +0200

    add otpEdus

---
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/Locators/AuthorizeOtpEdus.swift
@@ -0,0 +1,26 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+
+extension Locator {
+
+	static func submitOnBehalf(
+		forceApiTokenHeader: Bool = false,
+		isFake: Bool
+	) -> Locator {
+		let fake = String(isFake ? 1 : 0)
+		let forceApiHeader = String(forceApiTokenHeader ? 1 : 0)
+		return Locator(
+			endpoint: .dataDonation,
+			paths: ["version", "v1", "ios", "otp"],
+			method: .post,
+			defaultHeaders: [fake: "cwa-fake", forceApiHeader: "cwa-ppac-ios-accept-api-token"],
+			// TODO: "application/x-protobuf" as Content Type?
+			// TODO: Body (Protobuf) is missing here
+			type: .default
+		)
+	}
+
+}
