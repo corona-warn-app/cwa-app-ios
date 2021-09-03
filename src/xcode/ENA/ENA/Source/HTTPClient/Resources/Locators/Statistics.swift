a1a7d54dcb3c15b93f5278acc6811082e94b24c7
Pascal Brause <pascal@brause.haus>
2021-09-03 09:44:18 +0200

    add statistics

---
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/Locators/Statistics.swift
@@ -0,0 +1,18 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+
+extension Locator {
+
+	static var statistics: Locator {
+		Locator(
+			endpoint: .distribution,
+			paths: ["version", "v1", "stats"],
+			method: .get,
+			type: .caching
+		)
+	}
+
+}
