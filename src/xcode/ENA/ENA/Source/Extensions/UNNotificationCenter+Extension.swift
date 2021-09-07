981580d406251c52a60e0924bc0626858e18cb1f
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-09-07 09:29:53 +0200

    add key for the booster notification

--- src/xcode/ENA/ENA/Source/Extensions/UNNotificationCenter+Extension.swift
+++ src/xcode/ENA/ENA/Source/Extensions/UNNotificationCenter+Extension.swift
@@ -24,6 +24,7 @@ public enum LocalNotificationIdentifier: String {
 	case checkout = "EventCheckoutNotification"
 	case certificateExpiringSoon = "HealthCertificateNotificationExpireSoon"
 	case certificateExpired = "HealthCertificateNotificationExpired"
+	case boosterVaccination = "BoosterVaccinationNotification"
 }
 
 extension UserNotificationCenter {
