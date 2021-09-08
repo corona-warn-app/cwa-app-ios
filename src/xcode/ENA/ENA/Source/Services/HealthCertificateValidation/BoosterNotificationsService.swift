336b229d1265899db93d6e3525fb76431e584c77
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-09-08 15:52:37 +0200

    Update service tests To Test properly

--- src/xcode/ENA/ENA/Source/Services/HealthCertificateValidation/BoosterNotificationsService.swift
+++ src/xcode/ENA/ENA/Source/Services/HealthCertificateValidation/BoosterNotificationsService.swift
@@ -12,7 +12,7 @@ import class CertLogic.ValidationResult
 protocol BoosterNotificationsServiceProviding {
 	func applyRulesForCertificates(
 		certificates: [DigitalCovidCertificateWithHeader],
-		completion: @escaping (Result<ValidationResult, BoosterNotificationRuleValidationError>) -> Void
+		completion: @escaping (Result<ValidationResult, BoosterNotificationRuleValidationError>?, HealthCertificateValidationError?) -> Void
 	)
 }
 
@@ -33,8 +33,9 @@ class BoosterNotificationsService: BoosterNotificationsServiceProviding {
 		self.validationRulesAccess = validationRulesAccess
 	}
 	
-	func applyRulesForCertificates(certificates: [DigitalCovidCertificateWithHeader],
-		completion: @escaping (Result<ValidationResult, BoosterNotificationRuleValidationError>) -> Void
+	func applyRulesForCertificates(
+		certificates: [DigitalCovidCertificateWithHeader],
+		completion: @escaping (Result<ValidationResult, BoosterNotificationRuleValidationError>?, HealthCertificateValidationError?) -> Void
 	) {
 		downloadBoosterNotificationRules { [weak self] downloadedRules in
 			guard let self = self else { return }
@@ -50,9 +51,10 @@ class BoosterNotificationsService: BoosterNotificationsServiceProviding {
 					certLogicEngine: certLogicEngine) { logs in
 					Log.debug(logs)
 				}
-				completion(resultOfApplyingBoosterRules)
+				completion(resultOfApplyingBoosterRules, nil)
 			case .failure(let error):
 				Log.error("Error downloading the booster notifications", log: .api, error: error)
+				completion(nil, error)
 			}
 		}
 	}
