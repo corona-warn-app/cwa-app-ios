cd061ab68325450421e99628fe0d841c56de124c
Pascal Brause <pascal@brause.haus>
2021-09-10 13:19:25 +0200

    add json resources

--- src/xcode/ENA/ENA/Source/HTTPClient/Resources/ReceiveResources/JSONReceiveResource.swift
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/ReceiveResources/JSONReceiveResource.swift
@@ -3,3 +3,48 @@
 //
 
 import Foundation
+
+struct JSONReceiveResource<R>: ReceiveResource where R: Decodable {
+	
+	// MARK: - Init
+	
+	init() {}
+	
+	// MARK: - Overrides
+	
+	// MARK: - Protocol ReceiveResource
+	
+	typealias ReceiveModel = R
+	
+	func decode(_ data: Data?) -> Result<R, ResourceError> {
+		guard let data = data else {
+			return .failure(.missingData)
+		}
+		do {
+			let model = try decoder.decode(R.self, from: data)
+			return .success(model)
+		} catch let DecodingError.keyNotFound(key, context) {
+			Log.debug("missing key: \(key.stringValue)", log: .client)
+			Log.debug("Debug Description: \(context.debugDescription)", log: .client)
+		} catch let DecodingError.valueNotFound(type, context) {
+			Log.debug("Type not found \(type)", log: .client)
+			Log.debug("Debug Description: \(context.debugDescription)", log: .client)
+		} catch let DecodingError.typeMismatch(type, context) {
+			Log.debug("Type mismatch found \(type)", log: .client)
+			Log.debug("Debug Description: \(context.debugDescription)", log: .client)
+		} catch let DecodingError.dataCorrupted(context) {
+			Log.debug("Debug Description: \(context.debugDescription)", log: .client)
+		} catch {
+			Log.debug("Failed to parse JSON answer - unhandled error", log: .client)
+		}
+		return .failure(.decoding)
+	}
+	
+	// MARK: - Public
+	
+	// MARK: - Internal
+	
+	// MARK: - Private
+	
+	private let decoder = JSONDecoder()
+}
--- src/xcode/ENA/ENA/Source/HTTPClient/Resources/SendResources/JSONSendResource.swift
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/SendResources/JSONSendResource.swift
@@ -3,3 +3,41 @@
 //
 
 import Foundation
+
+struct JSONSendResource<S>: SendResource where S: Encodable {
+	
+	// MARK: - Init
+	
+	init(
+		_ sendModel: S? = nil
+	) {
+		self.sendModel = sendModel
+	}
+	
+	// MARK: - Overrides
+	
+	// MARK: - Protocol ReceiveResource
+	
+	typealias SendModel = S
+	var sendModel: S?
+	
+	func encode() -> Result<Data?, ResourceError> {
+		guard let model = sendModel else {
+			return .success(nil)
+		}
+		do {
+			let data = try encoder.encode(model)
+			return Result.success(data)
+		} catch {
+			return Result.failure(.encoding)
+		}
+	}
+	
+	// MARK: - Public
+	
+	// MARK: - Internal
+	
+	// MARK: - Private
+	
+	private let encoder = JSONEncoder()
+}
