148bdd4d9bca7197099d00caeb83d6115a4e2914
Pascal Brause <pascal@brause.haus>
2021-09-14 16:33:17 +0200

    review change

--- src/xcode/ENA/ENA/Source/Scenes/AntigenTestProfile/AntigenTestProfile/Cells/QRCodeCellViewModel.swift
+++ src/xcode/ENA/ENA/Source/Scenes/AntigenTestProfile/AntigenTestProfile/Cells/QRCodeCellViewModel.swift
@@ -30,7 +30,7 @@ struct QRCodeCellViewModel {
 			with: vCardV4(revDate: revDate),
 			encoding: .utf8,
 			size: CGSize(width: 280.0, height: 280.0),
-			scale: 1,
+			scale: UIScreen.main.scale,
 			qrCodeErrorCorrectionLevel: .medium
 		)
 		else {
