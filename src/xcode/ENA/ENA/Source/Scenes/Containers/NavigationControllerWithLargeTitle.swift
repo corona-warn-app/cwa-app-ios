3220f7b975df42183cb292834abba8e49be76367
ndegendogo <christa.runge@kifaru.de>
2021-09-15 23:30:58 +0200

    Review feedback: comment purpose of NavigationControllerWithLargeTitle

--- src/xcode/ENA/ENA/Source/Scenes/Containers/NavigationControllerWithLargeTitle.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Containers/NavigationControllerWithLargeTitle.swift
@@ -4,7 +4,8 @@
 
 import UIKit
 
-
+/* The navigation bar of this controller is pre-configured with a value for mixed operation of view controllers with large
+   and with normal titles. This property of the shared navigation bar needs not be re-configured at runtime. */
 class NavigationControllerWithLargeTitle: UINavigationController {
 
 	// MARK: - Init
