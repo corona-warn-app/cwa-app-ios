d6ae190218e04a63bbe6c38e6897b8ff1945edb3
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-09-16 02:39:10 +0200

    Add Certificate parsing logic

--- src/xcode/ENA/ENA/Source/Scenes/QRScanner/CertificateQRCodeParser.swift
+++ src/xcode/ENA/ENA/Source/Scenes/QRScanner/CertificateQRCodeParser.swift
@@ -3,3 +3,28 @@
 //
 
 import Foundation
+
+// this should replace HealthCertificateQRCodeScannerViewModel
+class CertificateQRCodeParser: QRCodeParsable {
+	
+	// MARK: - Init
+
+	init(healthCertificateService: HealthCertificateService) {
+		self.healthCertificateService = healthCertificateService
+	}
+	
+	func parse(qrCode: String, completion: @escaping (Result<QRCodeResult, QRCodeParserError>) -> Void) {
+		let result = healthCertificateService.registerHealthCertificate(base45: qrCode)
+		switch result {
+		case let .success((healthCertifiedPerson, healthCertificate)):
+			completion(.success(.certificate(healthCertifiedPerson, healthCertificate)))
+		case .failure(let registrationError):
+			// wrap RegistrationError into an QRScannerError.other error
+			completion(.failure(.scanningError(.other(registrationError))))
+		}
+	}
+	
+	// MARK: - Private
+
+	private let healthCertificateService: HealthCertificateService
+}
