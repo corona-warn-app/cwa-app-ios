f3fd8b676ba65107a16d616ea491f86215d0b832
Kai Teuber <Kai.Teuber@app-developer.club>
2021-09-21 14:00:30 +0200

    added assets

--- src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/CovPassCheckInformation/CovPassCheckInformationViewModel.swift
+++ src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/CovPassCheckInformation/CovPassCheckInformationViewModel.swift
@@ -6,51 +6,26 @@ import UIKit
 
 
 final class CovPassCheckInformationViewModel {
-
+	
 	// MARK: - Init
-
+	
 	// MARK: - Overrides
-
+	
 	// MARK: - Protocol <#Name#>
-
+	
 	// MARK: - Public
-
+	
 	// MARK: - Internal
-
+	
 	var dynamicTableViewModel: DynamicTableViewModel {
 		DynamicTableViewModel([
 			.section(
 				cells: [
-					.space(height: 8),
-					.body(
-						text: AppStrings.HealthCertificate.Validation.body1,
-						accessibilityIdentifier: ""
-					),
-					.space(height: 8),
-					.headline(
-						text: AppStrings.HealthCertificate.Validation.headline1,
-						accessibilityIdentifier: ""
-					)
-			]),
-			// Disclaimer cell
-			.section(
-				separators: .all,
-				cells: [
-					.body(
-						text: AppStrings.HealthCertificate.Validation.body4,
-						style: DynamicCell.TextCellStyle.label,
-						accessibilityIdentifier: AccessibilityIdentifiers.TraceLocation.dataPrivacyTitle,
-						accessibilityTraits: UIAccessibilityTraits.link,
-						configure: { _, cell, _ in
-							cell.accessoryType = .disclosureIndicator
-							cell.selectionStyle = .default
-						}
-					),
-					.space(height: 16)
+					.headlineWithImage(headerText: "Hallo Welt", image: UIImage(imageLiteralResourceName: "Illu_CovPass_Check"))
 				]
 			)
 		])
 	}
-
+	
 	// MARK: - Private
 }
