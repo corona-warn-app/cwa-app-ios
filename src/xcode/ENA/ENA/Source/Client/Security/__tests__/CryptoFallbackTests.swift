7bdc8f4d78e70f13d220f12ddcacd35542065476
Marcus Scherer <marcus@skunkbrothers.de>
2021-09-27 16:01:35 +0200

    Remove performance tests O.o

--- src/xcode/ENA/ENA/Source/Client/Security/__tests__/CryptoFallbackTests.swift
+++ src/xcode/ENA/ENA/Source/Client/Security/__tests__/CryptoFallbackTests.swift
@@ -23,26 +23,6 @@ class CryptoFallbackTests: iOS13TestCase {
 		XCTAssertEqual(customDigest, referenceDigest)
     }
 
-    func testPerformanceCryptoKitSHA256() throws {
-        self.measure {
-			for i in 0..<500_000 {
-				// swiftlint:disable:next force_unwrapping
-				let data = "Alice\(i)".data(using: .utf8)!
-				_ = Data(SHA256.hash(data: data))
-			}
-        }
-    }
-
-	func testPerformanceFallbackSHA256() throws {
-		self.measure {
-			for i in 0..<500_000 {
-				// swiftlint:disable:next force_unwrapping
-				let data = "Alice\(i)".data(using: .utf8)!
-				_ = data.sha256(enforceFallback: true)
-			}
-		}
-	}
-
 	// MARK: - Signature checks
 
 	func testPrivateKeyGeneration() throws {
