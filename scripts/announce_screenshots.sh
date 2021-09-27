5ea5dba7dcf55777f59be922a6f5c88433c73fc7
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-09-27 10:13:50 +0200

    Test on 2.12

--- scripts/announce_screenshots.sh
+++ scripts/announce_screenshots.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 SCREENSHOT_URL=$1
 
-if curl --output /dev/null --silent --head --fail "$SCREENSHOT_URL"; then
+if curl --output /dev/null --silent --head --fail "$Failed_SCREENSHOTS_TEAMS"; then
   curl --output /dev/null --silent $SAP_TEAMS_WEBHOOK \
   -H 'Content-Type: application/json' \
   --data-binary @- << EOF
@@ -43,6 +43,42 @@ if curl --output /dev/null --silent --head --fail "$SCREENSHOT_URL"; then
 }
 EOF
 else
-  echo "Could not locate screenshots at $SCREENSHOT_URL"
+  curl --output /dev/null --silent $Failed_SCREENSHOTS_TEAMS \
+  -H 'Content-Type: application/json' \
+  --data-binary @- << EOF
+{
+  "@type": "MessageCard",
+  "@context": "http://schema.org/extensions",
+  "themeColor": "0076D7",
+  "summary": "screenshots have failed!",
+  "sections": [{
+      "activityTitle": "screenshots have failed!",
+      "activitySubtitle": "On Project ${CIRCLE_PROJECT_REPONAME}",
+      "activityImage": "https://media.staticline.de/pictures/530f4962378e781ea8c5f70aa9fbacca535968f2.png",
+      "facts": [{
+          "name": "Created by",
+          "value": "${CIRCLE_USERNAME}"
+      }, {
+          "name": "Timestamp",
+          "value": "$(date "+%Y-%m-%d %H:%M")"
+      }, {
+          "name": "Branch",
+          "value": "${CIRCLE_BRANCH}"
+      }, {
+          "name": "Build number",
+          "value": "${CIRCLE_BUILD_NUM}"
+      }],
+      "markdown": true
+  }],
+  "potentialAction": [{
+      "@type": "OpenUri",
+      "name": "Download now!",
+      "targets": [{
+          "os": "default",
+          "uri": "${SCREENSHOT_URL}"
+      }]
+  }]
+}
+EOF
   return 1
 fi
