6b94170120d6207588097aa79df55bef21b63d1b
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-09-29 00:54:53 +0200

    refactoring

--- scripts/announce_failing_screenshots.sh
+++ scripts/announce_failing_screenshots.sh
@@ -4,9 +4,7 @@ set -euo pipefail
 
 SCREENSHOT_URL=$1
 
-if curl --output /dev/null --silent --head --fail "$SCREENSHOT_URL"; then
-  echo  "found screenshots at  $SCREENSHOT_URL "
-else
+if ! [[curl --output /dev/null --silent --head --fail "$SCREENSHOT_URL"]]; then
   curl --output /dev/null --silent $Failed_SCREENSHOTS_TEAMS \
   -H 'Content-Type: application/json' \
   --data-binary @- << EOF
