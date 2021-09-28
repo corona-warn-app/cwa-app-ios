883fc4020685647214d25724f1724f3875c224ad
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-09-29 01:34:07 +0200

    reverse commit

--- scripts/announce_failing_screenshots.sh
+++ scripts/announce_failing_screenshots.sh
@@ -4,7 +4,9 @@ set -euo pipefail
 
 SCREENSHOT_URL=$1
 
-if  [[ ! curl --output /dev/null --silent --head --fail "$SCREENSHOT_URL"]]; then
+if curl --output /dev/null --silent --head --fail "$SCREENSHOT_URL"; then
+  echo  "found screenshots at  $SCREENSHOT_URL "
+else
   curl --output /dev/null --silent $Failed_SCREENSHOTS_TEAMS \
   -H 'Content-Type: application/json' \
   --data-binary @- << EOF
