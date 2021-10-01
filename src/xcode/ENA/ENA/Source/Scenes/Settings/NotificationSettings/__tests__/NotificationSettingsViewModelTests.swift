b32a914c3c24637e374310202df113c6fc8f31d9
Pascal Brause <pascal@brause.haus>
2021-10-01 11:41:05 +0200

    add dummy unit test

---
+++ src/xcode/ENA/ENA/Source/Scenes/Settings/NotificationSettings/__tests__/NotificationSettingsViewModelTests.swift
@@ -0,0 +1,34 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import XCTest
+@testable import ENA
+
+class NotificationSettingsViewModelTests: CWATestCase {
+	
+	func testGIVEN_ViewModel_WHEN_NotificationsOn_THEN_CorrectNumberOfCells() {
+		// GIVEN
+		<#given code#>
+		
+		// WHEN
+		<#when code#>
+		
+		// THEN
+		<#then code#>
+		
+	}
+	
+	func testGIVEN_ViewModel_WHEN_NotificationsOff_THEN_CorrectNumberOfCells() {
+		// GIVEN
+		<#given code#>
+		
+		// WHEN
+		<#when code#>
+		
+		// THEN
+		<#then code#>
+		
+	}
+	
+}
