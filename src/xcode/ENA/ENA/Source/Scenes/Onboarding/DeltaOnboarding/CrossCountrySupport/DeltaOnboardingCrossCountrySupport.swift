410cf4ae94dd9f38a62dfa8d7a8315fe5a4346a0
Pascal Brause <pascal@brause.haus>
2021-10-04 17:21:37 +0200

    add some comments and change order of delta on

--- src/xcode/ENA/ENA/Source/Scenes/Onboarding/DeltaOnboarding/CrossCountrySupport/DeltaOnboardingCrossCountrySupport.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Onboarding/DeltaOnboarding/CrossCountrySupport/DeltaOnboardingCrossCountrySupport.swift
@@ -4,11 +4,15 @@
 
 import UIKit
 
+// WARNING: Do not rename class name because it is used to identify already presented onboardings. But if you need to, rename it and override the id property of the DeltaOnboarding Protocol and assign the origin class name
 class DeltaOnboardingCrossCountrySupport: DeltaOnboarding {
 
 	let version = "1.5"
 	let store: Store
 	let supportedCountries: [Country]
+
+	// Needed after renaming class name but to identify saved old onboardings out there.
+	var id = "DeltaOnboardingV15"
 
 	init(store: Store, supportedCountries: [Country]) {
 		self.store = store
