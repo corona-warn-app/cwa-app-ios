cb72ee702864a29cc03de92bf5f0345bf9563094
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-10-04 13:05:24 +0200

    add text for expired and expiring soon certificates

--- src/xcode/ENA/ENA/Source/Services/HealthCertificate/HealthCertificateService.swift
+++ src/xcode/ENA/ENA/Source/Services/HealthCertificate/HealthCertificateService.swift
@@ -947,7 +947,7 @@ class HealthCertificateService {
 
 		let content = UNMutableNotificationContent()
 		content.title = AppStrings.LocalNotifications.certificateGenericTitle
-		content.body = AppStrings.LocalNotifications.certificateGenericBody
+		content.body = AppStrings.LocalNotifications.invalidCertificateBody
 		content.sound = .default
 
 		let expiringSoonDateComponents = Calendar.current.dateComponents(
@@ -975,7 +975,7 @@ class HealthCertificateService {
 
 		let content = UNMutableNotificationContent()
 		content.title = AppStrings.LocalNotifications.certificateGenericTitle
-		content.body = AppStrings.LocalNotifications.certificateGenericBody
+		content.body = AppStrings.LocalNotifications.invalidCertificateBody
 		content.sound = .default
 
 		let expiredDateComponents = Calendar.current.dateComponents(
