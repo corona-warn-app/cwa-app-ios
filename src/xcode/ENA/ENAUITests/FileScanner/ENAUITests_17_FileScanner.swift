cd847b5d228b6d344d0aa4c3e423a54543f45b2a
Artur Friesen <mail@arturfriesen.de>
2021-10-05 13:59:55 +0200

    Add photo button test.

--- src/xcode/ENA/ENAUITests/FileScanner/ENAUITests_17_FileScanner.swift
+++ src/xcode/ENA/ENAUITests/FileScanner/ENAUITests_17_FileScanner.swift
@@ -51,4 +51,21 @@ class ENAUITests_17_FileScanner: CWATestCase {
 		let fileButton = try XCTUnwrap(app.buttons[AccessibilityIdentifiers.FileScanner.file])
 		fileButton.waitAndTap()
 	}
+
+	func test_OpenUniversalScannerAndSelectFile_OpenImage() throws {
+		app.setLaunchArgument(LaunchArguments.infoScreen.checkinInfoScreenShown, to: true)
+		app.launch()
+
+		app.buttons[AccessibilityIdentifiers.TabBar.scanner].waitAndTap()
+
+		/// Simulator only Alert will open where you can choose what the QRScanner should scan
+		let otherButton = try XCTUnwrap(app.buttons[AccessibilityIdentifiers.UniversalQRScanner.other])
+		otherButton.waitAndTap()
+
+		let openFileButton = try XCTUnwrap(app.buttons[AccessibilityIdentifiers.UniversalQRScanner.file])
+		openFileButton.waitAndTap()
+
+		let photoButton = try XCTUnwrap(app.buttons[AccessibilityIdentifiers.FileScanner.photo])
+		photoButton.waitAndTap()
+	}
 }
