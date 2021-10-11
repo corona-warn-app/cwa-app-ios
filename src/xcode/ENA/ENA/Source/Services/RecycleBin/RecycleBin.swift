98e36daea4d92cb18ddb4bbf34a3de1934ee4e03
Artur Friesen <mail@arturfriesen.de>
2021-10-11 12:07:13 +0200

    Add unit tests and logging.

--- src/xcode/ENA/ENA/Source/Services/RecycleBin/RecycleBin.swift
+++ src/xcode/ENA/ENA/Source/Services/RecycleBin/RecycleBin.swift
@@ -14,17 +14,6 @@ enum RecycledItem: Hashable, Equatable, Codable {
 	case certificate(HealthCertificate)
 	case coronaTest(CoronaTest)
 
-	// MARK: Hashable
-
-	func hash(into hasher: inout Hasher) {
-		switch self {
-		case .certificate:
-			hasher.combine(0)
-		case .coronaTest:
-			hasher.combine(1)
-		}
-	}
-
 	var recycleBinIdentifier: String {
 		switch self {
 		case .certificate(let certificate):
@@ -44,16 +33,31 @@ enum RecycledItem: Hashable, Equatable, Codable {
 			return false
 		}
 	}
+
+	// MARK: Hashable
+
+	func hash(into hasher: inout Hasher) {
+		hasher.combine(recycleBinIdentifier)
+	}
 }
 
 struct RecycleBinItem: Equatable, Hashable, Codable {
-	let deletionDate: Date
+	let recycleDate: Date
 	let item: RecycledItem
 
 	// MARK: Equatable
 
+	// We take only the item into account because we don't want to equate against the recycleDate.
+	// It's not possible to have the same item with different recycleDates in the recycle-bin (see hashable and 'set' implementation in the recycle-bin. The hashing is only going against the item, not the date.
 	static func == (lhs: RecycleBinItem, rhs: RecycleBinItem) -> Bool {
 		return lhs.item == rhs.item
+	}
+
+	// MARK: Hashable
+
+	// We take only the item into account because we don't want to have duplicate item entries (with different dates) in the set of recycle-bin items.
+	func hash(into hasher: inout Hasher) {
+		hasher.combine(item)
 	}
 }
 
@@ -76,7 +80,7 @@ enum CertificateRestorationError: Error {
 }
 
 protocol CertificateRestorationHandling {
-	var canRestore: ((HealthCertificate) -> Result<Void, TestRestorationError>) { get set }
+	var canRestore: ((HealthCertificate) -> Result<Void, CertificateRestorationError>) { get set }
 	var restore: ((HealthCertificate) -> Void) { get set }
 }
 
@@ -92,29 +96,45 @@ class RecycleBin {
 
 	// MARK: - Internal
 
-	func recycle(_ item: RecycledItem) {
+	var testRestorationHandler: TestRestorationHandling!
+	var certificateRestorationHandler: CertificateRestorationHandling!
+
+	@discardableResult
+	func recycle(_ item: RecycledItem, recycleDate: Date = Date()) -> RecycleBinItem {
+		Log.info("Recycle item.", log: .recycleBin)
+
 		let binItem = RecycleBinItem(
-			deletionDate: Date(),
+			recycleDate: recycleDate,
 			item: item
 		)
 		store.recycleBinItems.insert(binItem)
+		return binItem
 	}
 
 	func canRestore(_ item: RecycleBinItem) -> Result<Void, RestorationError> {
+		Log.info("Ask for item restoration.", log: .recycleBin)
+
 		switch item.item {
-		case .certificate:
-			return .success(())
+		case .certificate(let certificate):
+			Log.info("Ask for certificate item restoration.", log: .recycleBin)
+			let canRestoreResult = certificateRestorationHandler.canRestore(certificate)
+			return canRestoreResult.mapError { RestorationError.certificateError($0) }
 		case .coronaTest(let coronaTest):
+			Log.info("Ask for test item restoration.", log: .recycleBin)
 			let canRestoreResult = testRestorationHandler.canRestore(coronaTest)
 			return canRestoreResult.mapError { RestorationError.testError($0) }
 		}
 	}
 
 	func restore(_ item: RecycleBinItem) {
+		Log.info("Restore item.", log: .recycleBin)
+
 		switch item.item {
 		case .certificate(let certificate):
+			Log.info("Restore certificate item.", log: .recycleBin)
 			certificateRestorationHandler.restore(certificate)
 		case .coronaTest(let coronaTest):
+			Log.info("Restore test item.", log: .recycleBin)
 			testRestorationHandler.restore(coronaTest)
 		}
 
@@ -122,30 +142,39 @@ class RecycleBin {
 	}
 
 	func remove(_ item: RecycleBinItem) {
+		Log.info("Remove item.", log: .recycleBin)
 		store.recycleBinItems.remove(item)
 	}
 
 	func removeAll() {
+		Log.info("Remove all items.", log: .recycleBin)
 		store.recycleBinItems.removeAll()
 	}
 
 	func item(for identifier: String) -> RecycledItem? {
-		store.recycleBinItems.first { $0.item.recycleBinIdentifier == identifier }?.item
+		Log.info("Read item.", log: .recycleBin)
+		return store.recycleBinItems.first { $0.item.recycleBinIdentifier == identifier }?.item
 	}
 
-	func cleanup() {
-		let cleanedSubsequence = store.recycleBinItems.drop { item in
-			guard let treshholdDate = Calendar.current.date(byAdding: .day, value: -30, to: Date()) else {
-				fatalError("Could not create date.")
-			}
-			return item.deletionDate < treshholdDate
+	func cleanup(_ now: Date = Date()) {
+		Log.info("Cleanup items...", log: .recycleBin)
+		Log.info("Number of items before cleanup: \(store.recycleBinItems.count)", log: .recycleBin)
+
+		guard let treshholdDate = Calendar.current.date(byAdding: .day, value: -30, to: now) else {
+			fatalError("Could not create date.")
+		}
+
+		let cleanedSubsequence = store.recycleBinItems.filter {
+			return $0.recycleDate >= treshholdDate
 		}
 		store.recycleBinItems = Set(cleanedSubsequence)
+
+		Log.info("Number of items after cleanup: \(store.recycleBinItems.count)", log: .recycleBin)
+		Log.info("Finished Cleanup.", log: .recycleBin)
+
 	}
 
 	// MARK: - Private
 
-	private var testRestorationHandler: TestRestorationHandling!
-	private var certificateRestorationHandler: CertificateRestorationHandling!
 	private let store: RecycleBinStoring
 }
