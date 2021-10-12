c86e2ec0a2a21c0ece09b9dba81523f47100e802
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-10-13 01:59:11 +0200

    Add configuration API

--- src/xcode/ENA/ENA/Source/Scenes/Home/Cells/More/MoreActionItem/MoreActionItemView.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Home/Cells/More/MoreActionItem/MoreActionItemView.swift
@@ -11,4 +11,27 @@ class MoreActionItemView: UIView {
 	override func awakeFromNib() {
 		super.awakeFromNib()
 	}
+	
+	// MARK: - Internal
+	
+	func configure(
+		actionItem: MoreActionItem,
+		completion: @escaping ((MoreActionItem) -> Void)
+	) {
+		self.imageView.image = actionItem.image
+		self.titleLabel.text = actionItem.title
+		self.titleLabel.accessibilityIdentifier = actionItem.accessibilityIdentifier
+		self.separatorView.isHidden = actionItem == .share
+		self.actionItem = actionItem
+		self.completion = completion
+	}
+	
+	// MARK: - Private
+	
+	@IBOutlet private weak var titleLabel: ENALabel!
+	@IBOutlet private weak var imageView: UIImageView!
+	@IBOutlet weak var separatorView: UIView!
+	
+	private var actionItem: MoreActionItem?
+	private var completion: ((MoreActionItem) -> Void)?
 }
