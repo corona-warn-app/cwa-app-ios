5e094ac633afdf8dccea7f817af4ee1b8a245a3c
Naveed Khalid <nkhalid.here@gmail.com>
2021-10-12 16:15:27 +0200

    fixing test

--- src/xcode/ENA/ENA/Source/Scenes/Home/Cells/Statistics/__tests__/HomeStatisticsCardViewModelTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Home/Cells/Statistics/__tests__/HomeStatisticsCardViewModelTests.swift
@@ -680,7 +680,7 @@ class HomeStatisticsCardViewModelTests: CWATestCase {
 		XCTAssertEqual(viewModel.primaryTrendImageTintColor, .enaColor(for: .riskHigh))
 		XCTAssertEqual(viewModel.primaryTrendAccessibilityLabel, AppStrings.Statistics.Card.trendIncreasing)
 		XCTAssertEqual(viewModel.primaryTrendAccessibilityValue, AppStrings.Statistics.Card.trendSemanticNegative)
-		XCTAssertEqual(viewModel.primarySubtitle, AppStrings.Statistics.AddCard.localCardPrimarySubtitle)
+		XCTAssertEqual(viewModel.primarySubtitle, AppStrings.Statistics.Card.Region.primaryLabelSubtitle)
 	}
 	
 	private func regionStatisticsDataLocalTrend(trend: SAP_Internal_Stats_KeyFigure.Trend, value: Double) -> RegionStatisticsData {
