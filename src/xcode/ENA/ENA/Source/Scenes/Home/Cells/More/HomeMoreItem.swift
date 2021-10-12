8133ca6403919f0fee2d9c92991242f91e7aa61b
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-10-13 01:45:48 +0200

    add cases titles

--- src/xcode/ENA/ENA/Source/Scenes/Home/Cells/More/HomeMoreItem.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Home/Cells/More/HomeMoreItem.swift
@@ -10,5 +10,21 @@ enum MoreActionItem: Int, CaseIterable {
 	case appInformation
 	case faq
 	case share
+	
+	var title: String {
+		switch self {
+		case .settings:
+			return AppStrings.Home.MoreCard.settingsTitle
+		case .recycleBin:
+			return AppStrings.Home.MoreCard.recycleBinTitle
+		case .appInformation:
+			return AppStrings.Home.MoreCard.appInformationTitle
+		case .faq:
+			return AppStrings.Home.MoreCard.faqTitle
+		case .share:
+			return AppStrings.Home.MoreCard.shareTitle
+		}
+	}
+	
 
 }
