47cabce1bfef5a7af6edd525a28c47c57856dce4
Artur Friesen <mail@arturfriesen.de>
2021-10-13 14:29:37 +0200

    Add reference to app delegate and call cleanup.

--- src/xcode/ENA/ENA/Source/Services/RecycleBin/Model/RecycleBinRestorationHandling.swift
+++ src/xcode/ENA/ENA/Source/Services/RecycleBin/Model/RecycleBinRestorationHandling.swift
@@ -24,3 +24,13 @@ protocol CertificateRestorationHandling {
 	var canRestore: ((HealthCertificate) -> Result<Void, CertificateRestorationError>) { get set }
 	var restore: ((HealthCertificate) -> Void) { get set }
 }
+
+struct TestRestorationHandlerFake: TestRestorationHandling {
+	var canRestore: ((CoronaTest) -> Result<Void, TestRestorationError>) = { _ in return .success(()) }
+	var restore: ((CoronaTest) -> Void) = { _ in }
+}
+
+struct CertificateRestorationHandlerFake: CertificateRestorationHandling {
+	var canRestore: ((HealthCertificate) -> Result<Void, CertificateRestorationError>) = { _ in return .success(()) }
+	var restore: ((HealthCertificate) -> Void) = { _ in }
+}
