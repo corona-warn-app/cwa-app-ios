cffb9b02a50591a0fd42cc627fb2320ff4d5544b
Pascal Brause <pascal@brause.haus>
2021-10-15 14:09:42 +0200

    change type

--- src/xcode/ENA/ENA/Source/HTTPClient/Resources/LocationResource/ValidationOnboardedCountriesLocationResource.swift
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/LocationResource/ValidationOnboardedCountriesLocationResource.swift
@@ -12,21 +12,12 @@ struct ValidationOnboardedCountriesLocationResource: LocationResource {
 		isFake: Bool
 	) {
 		self.locator = Locator.validationOnboardedCountries(isFake: isFake)
-		self.type = .caching
+		self.type = .default
 	}
-
-	// MARK: - Overrides
 
 	// MARK: - Protocol LocationResource
 
 	var locator: Locator
 
 	var type: ServiceType
-
-	// MARK: - Public
-
-	// MARK: - Internal
-
-	// MARK: - Private
-
 }
