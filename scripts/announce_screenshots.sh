bbe40ef341b400fa49c4f167a314ef9c638bab47
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-10-18 03:35:00 +0200

    call script after constructing the url

--- scripts/announce_screenshots.sh
+++ scripts/announce_screenshots.sh
@@ -43,6 +43,42 @@ if curl --output /dev/null --silent --head --fail "$SCREENSHOT_URL"; then
 }
 EOF
 else
-  echo "Could not locate screenshots at $SCREENSHOT_URL"
+  curl --output /dev/null --silent $Failed_SCREENSHOTS_TEAMS \
+  -H 'Content-Type: application/json' \
+  --data-binary @- << EOF
+{
+  "@type": "MessageCard",
+  "@context": "http://schema.org/extensions",
+  "themeColor": "0076D7",
+  "summary": "screenshots have failed!",
+  "sections": [{
+      "activityTitle": "Screenshots have failed!",
+      "activitySubtitle": "On Project ${CIRCLE_PROJECT_REPONAME}",
+      "activityImage": "https://media.staticline.de/pictures/530f4962378e781ea8c5f70aa9fbacca535968f2.png",
+      "facts": [{
+          "name": "Created by",
+          "value": "${CIRCLE_USERNAME}"
+      }, {
+          "name": "Timestamp",
+          "value": "$(date "+%Y-%m-%d %H:%M")"
+      }, {
+          "name": "Branch",
+          "value": "${CIRCLE_BRANCH}"
+      }, {
+          "name": "Build number",
+          "value": "${CIRCLE_BUILD_NUM}"
+      }],
+      "markdown": true
+  }],
+  "potentialAction": [{
+      "@type": "OpenUri",
+      "name": "Open failed Job",
+      "targets": [{
+          "os": "default",
+          "uri": "${JOB_URL}"
+      }]
+  }]
+}
+EOF
   return 1
 fi
