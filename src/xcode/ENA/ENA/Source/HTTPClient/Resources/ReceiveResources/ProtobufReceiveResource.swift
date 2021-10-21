87690dd6d02fbc9bf4a58f3489c53a03064989c7
Kai Teuber <Kai.Teuber@app-developer.club>
2021-10-21 15:52:31 +0200

    Bring back some Resources
    This reverts commit 55c76290202dfece6037129db5cd48ac32d21aa4.

---
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/ReceiveResources/ProtobufReceiveResource.swift
@@ -0,0 +1,51 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+import SwiftProtobuf
+
+/**
+Concrete implementation of ReceiveResource for Protobuf objects.
+Because Protofbuf objects are always packed into a signed package, we need the SignatureVerifier to ensure the correctness of the package.
+When a service receives a http response with body, containing some data, we just decode the data to make some protobuf file of it.
+Returns different RessourceErrors when decoding fails.
+*/
+struct ProtobufReceiveResource<R>: ReceiveResource where R: SwiftProtobuf.Message {
+
+	// MARK: - Init
+	
+	init(
+		signatureVerifier: SignatureVerifier = SignatureVerifier()
+	) {
+		self.signatureVerifier = signatureVerifier
+	}
+
+	// MARK: - Protocol ReceiveResource
+
+	typealias ReceiveModel = R
+
+	func decode(_ data: Data?) -> Result<R?, ResourceError> {
+		guard let data = data else {
+			return .failure(.missingData)
+		}
+		guard let package = SAPDownloadedPackage(compressedData: data) else {
+			return .failure(.packageCreation)
+		}
+		guard signatureVerifier.verify(package) else {
+			return .failure(.signatureVerification)
+		}
+
+		do {
+			let model = try R(serializedData: package.bin)
+			return Result.success(model)
+		} catch {
+			return Result.failure(.decoding)
+		}
+	}
+
+	// MARK: - Private
+
+	private let signatureVerifier: SignatureVerifier
+
+}
---
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/SendResources/EmptySendResource.swift
@@ -0,0 +1,21 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+
+/**
+Dummy empty implementation of SendResource when we expect a http request without any body data. In this case, we can always return a success with nil Data because there is nothing to encode. The sendModel is ignored in this case.
+Will always return .success.
+*/
+struct EmptySendResource: SendResource {
+
+	// MARK: - Protocol ReceiveResource
+	
+	typealias SendModel = Any
+	var sendModel: Any?
+	
+	func encode() -> Result<Data?, ResourceError> {
+		return .success(nil)
+	}
+}
