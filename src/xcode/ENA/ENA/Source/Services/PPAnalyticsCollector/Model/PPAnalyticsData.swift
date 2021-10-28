82fe545817a233d8d0bd6599d44a060f6f43d8ff
Naveed Khalid <nkhalid.here@gmail.com>
2021-10-28 15:16:38 +0200

    adding reason

--- src/xcode/ENA/ENA/Source/Services/PPAnalyticsCollector/Model/PPAnalyticsData.swift
+++ src/xcode/ENA/ENA/Source/Services/PPAnalyticsCollector/Model/PPAnalyticsData.swift
@@ -37,7 +37,7 @@ protocol PPAnalyticsData: AnyObject {
 	var antigenTestResultMetadata: TestResultMetadata? { get set }
 	/// Analytics data.
 	var exposureWindowsMetadata: ExposureWindowsMetadata? { get set }
-	/// Stores the current exposure windows, should not be assigned to nil in deleteAnalyticsData!
+	/// Stores the current exposure windows, should not be assigned to nil in deleteAnalyticsData! because we need the expsure windows until new risk calculation.
 	var currentExposureWindows: [SubmissionExposureWindow]? { get set }
 	/// Date when the ENF risk was changed to high
 	var dateOfConversionToENFHighRisk: Date? { get set }
