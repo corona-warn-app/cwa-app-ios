95162cc157e640019743de19b7dbc570a23e00d9
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-11-03 12:22:49 +0100

    add RegistrationTokenResource

---
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/RegistrationTokenResource.swift
@@ -0,0 +1,57 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+
+struct RegistrationTokenResource: Resource {
+
+	// MARK: - Init
+
+	init(
+		isFake: Bool = false,
+		sendModel: SendRegistrationTokenModel
+	) {
+		self.locator = .tanForExposureSubmit(registrationToken: sendModel.tokenString, isFake: isFake)
+		self.type = .default
+		self.sendResource = JSONSendResource<SendRegistrationTokenModel>(sendModel)
+		self.receiveResource = JSONReceiveResource<SubmissionTANModel>()
+		self.registrationTokenModel = sendModel
+	}
+
+	// MARK: - Protocol Resource
+
+	typealias Send = JSONSendResource<SendRegistrationTokenModel>
+	typealias Receive = JSONReceiveResource<SubmissionTANModel>
+	typealias CustomError = RegistrationTokenError
+
+	var locator: Locator
+	var type: ServiceType
+	var sendResource: JSONSendResource<SendRegistrationTokenModel>
+	var receiveResource: JSONReceiveResource<SubmissionTANModel>
+
+	func customStatusCodeError(statusCode: Int) -> RegistrationTokenError? {
+		switch statusCode {
+		case (400):
+			return .regTokenNotExist
+		default:
+			return nil
+		}
+	}
+
+	// MARK: - Private
+
+	private let registrationTokenModel: SendRegistrationTokenModel
+
+}
+
+enum RegistrationTokenError: Error {
+	case regTokenNotExist
+
+	var errorDescription: String? {
+		switch self {
+		case .regTokenNotExist:
+			return AppStrings.ExposureSubmissionError.regTokenNotExist
+		}
+	}
+}
