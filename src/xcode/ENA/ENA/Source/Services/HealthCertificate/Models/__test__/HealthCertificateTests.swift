47cc426c67950f25589e6cbff3dab8638a764a37
Nick Guendling <hello@nickguendling.com>
2021-11-05 16:51:16 +0100

    Add unit tests for identifier chunks and blocklist check

--- src/xcode/ENA/ENA/Source/Services/HealthCertificate/Models/__test__/HealthCertificateTests.swift
+++ src/xcode/ENA/ENA/Source/Services/HealthCertificate/Models/__test__/HealthCertificateTests.swift
@@ -608,4 +608,89 @@ class HealthCertificateTests: XCTestCase {
 		XCTAssertNil(fifthWrongCertificate)
 	}
 
+	func testUniqueCertificateIdentifierChunks() throws {
+		let certificate1Base45 = try base45Fake(
+			from: .fake(vaccinationEntries: [.fake(uniqueCertificateIdentifier: "foo/bar::baz#999lizards")])
+		)
+		let certificate2Base45 = try base45Fake(
+			from: .fake(recoveryEntries: [.fake(uniqueCertificateIdentifier: "URN:UVCI:foo/bar::baz#999lizards")])
+		)
+		let certificate3Base45 = try base45Fake(
+			from: .fake(testEntries: [.fake(uniqueCertificateIdentifier: "a::c/#/f")])
+		)
+
+		let certificate1 = try HealthCertificate(base45: certificate1Base45)
+		let certificate2 = try HealthCertificate(base45: certificate2Base45)
+		let certificate3 = try HealthCertificate(base45: certificate3Base45)
+
+		XCTAssertEqual(
+			certificate1.uniqueCertificateIdentifierChunks,
+			["foo", "bar", "", "baz", "999lizards"]
+		)
+		XCTAssertEqual(
+			certificate2.uniqueCertificateIdentifierChunks,
+			["foo", "bar", "", "baz", "999lizards"]
+		)
+		XCTAssertEqual(
+			certificate3.uniqueCertificateIdentifierChunks,
+			["a", "", "c", "", "", "f"]
+		)
+	}
+
+	func testIsBlocked1() throws {
+		let certificateBase45 = try base45Fake(
+			from: .fake(vaccinationEntries: [.fake(uniqueCertificateIdentifier: "foo/bar::baz#999lizards")])
+		)
+
+		let certificate = try HealthCertificate(base45: certificateBase45)
+
+		var blockedChunk = SAP_Internal_V2_DGCBlockedUVCIChunk()
+		blockedChunk.indices = [1]
+		blockedChunk.hash = try XCTUnwrap(Data(base64Encoded: "fcde2b2edba56bf408601fb721fe9b5c338d10ee429ea04fae5511b68fbf8fb9"))
+
+		XCTAssertTrue(certificate.isBlocked(by: [blockedChunk]))
+	}
+
+	func testIsBlocked2() throws {
+		let certificateBase45 = try base45Fake(
+			from: .fake(vaccinationEntries: [.fake(uniqueCertificateIdentifier: "foo/baz::baz#999lizards")])
+		)
+
+		let certificate = try HealthCertificate(base45: certificateBase45)
+
+		var blockedChunk = SAP_Internal_V2_DGCBlockedUVCIChunk()
+		blockedChunk.indices = [1]
+		blockedChunk.hash = try XCTUnwrap(Data(base64Encoded: "fcde2b2edba56bf408601fb721fe9b5c338d10ee429ea04fae5511b68fbf8fb9"))
+
+		XCTAssertFalse(certificate.isBlocked(by: [blockedChunk]))
+	}
+
+	func testIsBlocked3() throws {
+		let certificateBase45 = try base45Fake(
+			from: .fake(vaccinationEntries: [.fake(uniqueCertificateIdentifier: "foo/bar::baz#999lizards")])
+		)
+
+		let certificate = try HealthCertificate(base45: certificateBase45)
+
+		var blockedChunk = SAP_Internal_V2_DGCBlockedUVCIChunk()
+		blockedChunk.indices = [0, 1]
+		blockedChunk.hash = try XCTUnwrap(Data(base64Encoded: "cc5d46bdb4991c6eae3eb739c9c8a7a46fe9654fab79c47b4fe48383b5b25e1c"))
+
+		XCTAssertTrue(certificate.isBlocked(by: [blockedChunk]))
+	}
+
+	func testIsBlocked4() throws {
+		let certificateBase45 = try base45Fake(
+			from: .fake(vaccinationEntries: [.fake(uniqueCertificateIdentifier: "foo/baz::baz#999lizards")])
+		)
+
+		let certificate = try HealthCertificate(base45: certificateBase45)
+
+		var blockedChunk = SAP_Internal_V2_DGCBlockedUVCIChunk()
+		blockedChunk.indices = [0, 1]
+		blockedChunk.hash = try XCTUnwrap(Data(base64Encoded: "cc5d46bdb4991c6eae3eb739c9c8a7a46fe9654fab79c47b4fe48383b5b25e1c"))
+
+		XCTAssertFalse(certificate.isBlocked(by: [blockedChunk]))
+	}
+
 }
