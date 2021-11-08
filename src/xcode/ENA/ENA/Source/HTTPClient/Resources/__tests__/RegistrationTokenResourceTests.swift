8b7d4bfd4207cec034b84d08537268cb2e3862bd
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-11-08 11:17:04 +0100

    add testGetTANForExposureSubmission_RegistrationTokenSuccess

--- src/xcode/ENA/ENA/Source/HTTPClient/Resources/__tests__/RegistrationTokenResourceTests.swift
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/__tests__/RegistrationTokenResourceTests.swift
@@ -2,28 +2,39 @@
 // 🦠 Corona-Warn-App
 //
 
+@testable import ENA
+import Foundation
 import XCTest
 
-class RegistrationTokenResourceTests: XCTestCase {
+final class RegistrationTokenResourceTests: CWATestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+	func testGetTANForExposureSubmission_RegistrationTokenSuccess() throws {
+		let fakeTan = "fakeTan"
+		let expectation = expectation(description: "Expect that we got a completion")
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+		let stack = MockNetworkStack(
+			httpStatus: 200,
+			responseData: try JSONEncoder().encode(
+				SubmissionTANModel(submissionTAN: fakeTan)
+			)
+		)
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+		let serviceProvider = RestServiceProvider(session: stack.urlSession)
+		let registrationTokenResource = RegistrationTokenResource(
+			isFake: false,
+			sendModel: SendRegistrationTokenModel(token: "Fake")
+		)
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+		serviceProvider.load(registrationTokenResource) { result in
+			switch result {
+			case .success(let model):
+				XCTAssertEqual(model.submissionTAN, fakeTan)
+			case .failure:
+				XCTFail("Encountered Error when receiving TAN!")
+			}
+			expectation.fulfill()
+		}
+		waitForExpectations(timeout: .short)
+	}
 
 }
