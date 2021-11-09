1b45ceba167f781b030fbccfd410ab68c62890f8
Artur Friesen <mail@arturfriesen.de>
2021-11-09 15:46:58 +0100

    Removed not needed comments.

--- src/xcode/ENA/ENASecurity/Sources/ENASecurity/Utils/Data+SHA256.swift
+++ src/xcode/ENA/ENASecurity/Sources/ENASecurity/Utils/Data+SHA256.swift
@@ -10,17 +10,12 @@ extension Data {
     func sha256() -> Data {
         // via https://www.agnosticdev.com/content/how-use-commoncrypto-apis-swift-5
 
-        // #define CC_SHA256_DIGEST_LENGTH     32
         // Creates an array of unsigned 8 bit integers that contains 32 zeros
         var digest = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
 
         // CC_SHA256 performs digest calculation and places the result in the caller-supplied buffer for digest (md)
         // Takes the strData referenced value (const unsigned char *d) and hashes it into a reference to the digest parameter.
         _ = self.withUnsafeBytes {
-            // CommonCrypto
-            // extern unsigned char *CC_SHA256(const void *data, CC_LONG len, unsigned char *md)  -|
-            // OpenSSL                                                                             |
-            // unsigned char *SHA256(const unsigned char *d, size_t n, unsigned char *md)        <-|
             CC_SHA256($0.baseAddress, UInt32(self.count), &digest)
         }
 
