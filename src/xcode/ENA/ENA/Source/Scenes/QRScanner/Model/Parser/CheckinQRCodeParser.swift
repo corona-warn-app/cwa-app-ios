b74beaab9238246450b8b5ae792bdce3df5947a5
Kai Teuber <Kai.Teuber@app-developer.club>
2021-11-09 11:24:39 +0100

    refactoring

--- src/xcode/ENA/ENA/Source/Scenes/QRScanner/Model/Parser/CheckinQRCodeParser.swift
+++ src/xcode/ENA/ENA/Source/Scenes/QRScanner/Model/Parser/CheckinQRCodeParser.swift
@@ -57,44 +57,42 @@ class CheckinQRCodeParser: QRCodeParsable {
 		onSuccess: @escaping((TraceLocation) -> Void),
 		onError: @escaping((CheckinQRScannerError) -> Void)
 	) {
-		appConfigurationProvider.currentAppConfig.sink { [weak self] appConfig in
-			// 1-Validate URL
-			var match: NSTextCheckingResult?
-			let descriptor = appConfig.presenceTracingParameters.qrCodeDescriptors.first {
-				do {
-					let regex = try NSRegularExpression(pattern: $0.regexPattern, options: [.caseInsensitive])
-					match = regex.firstMatch(in: url, range: .init(location: 0, length: url.count))
-					return match != nil
-				} catch {
-					Log.error(error.localizedDescription, log: .checkin)
-					return false
-				}
+		let appConfig = appConfigurationProvider.currentAppConfig.value
+		// 1-Validate URL
+		var match: NSTextCheckingResult?
+		let descriptor = appConfig.presenceTracingParameters.qrCodeDescriptors.first {
+			do {
+				let regex = try NSRegularExpression(pattern: $0.regexPattern, options: [.caseInsensitive])
+				match = regex.firstMatch(in: url, range: .init(location: 0, length: url.count))
+				return match != nil
+			} catch {
+				Log.error(error.localizedDescription, log: .checkin)
+				return false
 			}
+		}
 
-			// Extract ENCODED_PAYLOAD
-			// for some reason we get an extra match at index 0 which is the entire URL so we need to add an offset of 1 to each index after that to get the correct corresponding parts
-			guard let unWrappedMatch = match, let qrDescriptor = descriptor else {
-				Log.error("the QRCode matched none of the regular expressions", log: .checkin)
-				onError(CheckinQRScannerError.codeNotFound)
-				return
-			}
-			let payLoadIndex = qrDescriptor.encodedPayloadGroupIndex
-			guard payLoadIndex < unWrappedMatch.numberOfRanges,
-				  let payLoadRange = Range(unWrappedMatch.range(at: Int(payLoadIndex) + 1), in: url) else {
-				Log.error("payLoadIndex is out of bounds, invalid payload", log: .checkin)
-				onError(CheckinQRScannerError.invalidPayload)
-				return
-			}
+		// Extract ENCODED_PAYLOAD
+		// for some reason we get an extra match at index 0 which is the entire URL so we need to add an offset of 1 to each index after that to get the correct corresponding parts
+		guard let unWrappedMatch = match, let qrDescriptor = descriptor else {
+			Log.error("the QRCode matched none of the regular expressions", log: .checkin)
+			onError(CheckinQRScannerError.codeNotFound)
+			return
+		}
+		let payLoadIndex = qrDescriptor.encodedPayloadGroupIndex
+		guard payLoadIndex < unWrappedMatch.numberOfRanges,
+			  let payLoadRange = Range(unWrappedMatch.range(at: Int(payLoadIndex) + 1), in: url) else {
+				  Log.error("payLoadIndex is out of bounds, invalid payload", log: .checkin)
+				  onError(CheckinQRScannerError.invalidPayload)
+				  return
+			  }
 
-			let payLoad = url[payLoadRange]
-			guard let traceLocation = TraceLocation(qrCodeString: String(payLoad)) else {
-				Log.error("error decoding the Payload, invalid Vendor data", log: .checkin)
-				onError(CheckinQRScannerError.invalidVendorData)
-				return
-			}
-			self?.validateTraceLocationInformation(traceLocation: traceLocation, onSuccess: onSuccess, onError: onError)
-
-		}.store(in: &subscriptions)
+		let payLoad = url[payLoadRange]
+		guard let traceLocation = TraceLocation(qrCodeString: String(payLoad)) else {
+			Log.error("error decoding the Payload, invalid Vendor data", log: .checkin)
+			onError(CheckinQRScannerError.invalidVendorData)
+			return
+		}
+		validateTraceLocationInformation(traceLocation: traceLocation, onSuccess: onSuccess, onError: onError)
 	}
 
 	func validateTraceLocationInformation(
@@ -142,6 +140,4 @@ class CheckinQRCodeParser: QRCodeParsable {
 	// MARK: - Private
 
 	private let appConfigurationProvider: AppConfigurationProviding
-	private var subscriptions: Set<AnyCancellable> = []
-
 }
