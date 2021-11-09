ab83e8c16c3de5f8311dafe0fce6da4ad9cf8141
Artur Friesen <mail@arturfriesen.de>
2021-11-09 14:35:15 +0100

    Add missing dependency.

--- src/xcode/ENA/ENASecurity/Package.swift
+++ src/xcode/ENA/ENASecurity/Package.swift
@@ -17,13 +17,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/filom/ASN1Decoder", .upToNextMajor(from: "1.8.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ENASecurity",
-            dependencies: []),
+            dependencies: ["ASN1Decoder"]),
         .testTarget(
             name: "ENASecurityTests",
             dependencies: ["ENASecurity"]),
