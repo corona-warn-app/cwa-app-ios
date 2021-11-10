dd4a475d6d11976b588fa040cf5f0bce08319309
Kai Teuber <Kai.Teuber@app-developer.club>
2021-11-10 12:48:13 +0100

    encoding should work

--- src/xcode/ENA/ENASecurity/Sources/ENASecurity/RSAEncryption.swift
+++ src/xcode/ENA/ENASecurity/Sources/ENASecurity/RSAEncryption.swift
@@ -5,8 +5,10 @@
 import Foundation
 import CryptoSwift
 
-public enum RSAEncryptionError {
-    case encryptionFailed
+public enum RSAEncryptionError: Error {
+    case publicKeyIrretrievable
+    case algorithmNotSupported
+    case encryptionFailed(String?)
 }
 
 public struct RSAEncryption {
@@ -14,7 +16,7 @@ public struct RSAEncryption {
     // MARK: - Init
 
     init(
-        _ publicKey: Data
+        _ publicKey: SecKey
     ) {
         self.publicKey = publicKey
     }
@@ -26,13 +28,32 @@ public struct RSAEncryption {
     // MARK: - Public
 
     public func encrypt(_ plainText: Data) -> Result<Data, RSAEncryptionError> {
+        // try to get the public key from pair or private key
+        guard let publicKey = SecKeyCopyPublicKey(publicKey) else {
+            return .failure(.publicKeyIrretrievable)
+        }
+        //
+        guard SecKeyIsAlgorithmSupported(publicKey, .encrypt, SecKeyAlgorithm.rsaEncryptionOAEPSHA256) else {
+            return .failure(.algorithmNotSupported)
+        }
 
+        var error: Unmanaged<CFError>?
+        guard let cipherData = SecKeyCreateEncryptedData(
+            publicKey,
+            SecKeyAlgorithm.rsaEncryptionOAEPSHA256,
+            plainText as CFData,
+            &error
+        ) as Data?,
+              error == nil else {
+                  return .failure(.encryptionFailed(error?.takeRetainedValue().localizedDescription))
+              }
+        return .success(cipherData)
     }
 
     // MARK: - Internal
 
     // MARK: - Private
 
-    private let publicKey: Data
+    private let publicKey: SecKey
 
 }
