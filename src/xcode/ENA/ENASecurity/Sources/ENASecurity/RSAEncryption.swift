3ef55dbadc4e10cf36502d4c78d1c2ba35711be6
Kai Teuber <Kai.Teuber@app-developer.club>
2021-11-10 14:43:36 +0100

    encrypt and decryp seems to work

--- src/xcode/ENA/ENASecurity/Sources/ENASecurity/RSAEncryption.swift
+++ src/xcode/ENA/ENASecurity/Sources/ENASecurity/RSAEncryption.swift
@@ -11,6 +11,12 @@ public enum RSAEncryptionError: Error {
     case encryptionFailed(String?)
 }
 
+public enum RSADecryptionError: Error {
+    case publicKeyIrretrievable
+    case algorithmNotSupported
+    case decryptionFailed(String?)
+}
+
 public struct RSAEncryption {
 
     // MARK: - Init
@@ -32,7 +38,7 @@ public struct RSAEncryption {
         guard let publicKey = SecKeyCopyPublicKey(publicKey) else {
             return .failure(.publicKeyIrretrievable)
         }
-        //
+        // check algorithm is sipporrted
         guard SecKeyIsAlgorithmSupported(publicKey, .encrypt, SecKeyAlgorithm.rsaEncryptionOAEPSHA256) else {
             return .failure(.algorithmNotSupported)
         }
@@ -50,6 +56,75 @@ public struct RSAEncryption {
         return .success(cipherData)
     }
 
+    public func decrypt(privateKey: SecKey, cipherData: Data) -> Result<Data, RSADecryptionError> {
+
+        // check algorithm is sipporrted
+//        guard SecKeyIsAlgorithmSupported(privateKey, .encrypt, SecKeyAlgorithm.rsaEncryptionOAEPSHA256) else {
+//            return .failure(.algorithmNotSupported)
+//        }
+        guard SecKeyIsAlgorithmSupported(publicKey, .encrypt, SecKeyAlgorithm.rsaEncryptionOAEPSHA256) else {
+            return .failure(.algorithmNotSupported)
+        }
+
+
+        var error: Unmanaged<CFError>?
+
+        guard let decodedData = SecKeyCreateDecryptedData(
+            privateKey,
+            SecKeyAlgorithm.rsaEncryptionOAEPSHA256,
+            cipherData as CFData,
+            &error
+        ) as Data?,
+              error == nil else {
+                  return .failure(.decryptionFailed(error?.takeRetainedValue().localizedDescription))
+              }
+        return .success(decodedData)
+    }
+
+    public func decode(privateKey: SecKey, _ encrypted: [UInt8]) -> Result<String, RSADecryptionError> {
+        var plaintextBufferSize = Int(SecKeyGetBlockSize(privateKey))
+        var plaintextBuffer = [UInt8](repeating:0, count:Int(plaintextBufferSize))
+
+        let status = SecKeyDecrypt(privateKey, SecPadding.PKCS1, encrypted, plaintextBufferSize, &plaintextBuffer, &plaintextBufferSize)
+
+        if (status != errSecSuccess) {
+            return .failure(.decryptionFailed(nil))
+        }
+        if let resultString = String(bytesNoCopy: &plaintextBuffer, length: plaintextBufferSize, encoding: .utf8, freeWhenDone: true) {
+            return .success(resultString)
+        }
+        else {
+            return .failure(.decryptionFailed("failed to convert to string"))
+        }
+    }
+
+    /*
+    func encrypt(text: String) -> [UInt8] {
+        let plainBuffer = [UInt8](text.utf8)
+        var cipherBufferSize : Int = Int(SecKeyGetBlockSize((self.publicKey)!))
+        var cipherBuffer = [UInt8](repeating:0, count:Int(cipherBufferSize))
+
+        // Encrypto  should less than key length
+        let status = SecKeyEncrypt((self.publicKey)!, SecPadding.PKCS1, plainBuffer, plainBuffer.count, &cipherBuffer, &cipherBufferSize)
+        if (status != errSecSuccess) {
+            print("Failed Encryption")
+        }
+        return cipherBuffer
+    }
+
+    func decprypt(encrpted: [UInt8]) -> String? {
+        var plaintextBufferSize = Int(SecKeyGetBlockSize((self.privateKey)!))
+        var plaintextBuffer = [UInt8](repeating:0, count:Int(plaintextBufferSize))
+
+        let status = SecKeyDecrypt((self.privateKey)!, SecPadding.PKCS1, encrpted, plaintextBufferSize, &plaintextBuffer, &plaintextBufferSize)
+
+        if (status != errSecSuccess) {
+            print("Failed Decrypt")
+            return nil
+        }
+        return NSString(bytes: &plaintextBuffer, length: plaintextBufferSize, encoding: String.Encoding.utf8.rawValue)! as String
+    }
+*/
     // MARK: - Internal
 
     // MARK: - Private
