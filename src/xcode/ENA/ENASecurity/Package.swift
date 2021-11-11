5af9bd107ee188c50b0d642bbb7e01d930f765c6
Artur Friesen <mail@arturfriesen.de>
2021-11-11 16:16:19 +0100

    Use fork with new OID.

--- src/xcode/ENA/ENASecurity/Package.swift
+++ src/xcode/ENA/ENASecurity/Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.4.2")),
-        .package(url: "https://github.com/filom/ASN1Decoder", .upToNextMajor(from: "1.8.0"))
+        .package(url: "https://github.com/corona-warn-app/ASN1Decoder", .upToNextMajor(from: "1.8.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
