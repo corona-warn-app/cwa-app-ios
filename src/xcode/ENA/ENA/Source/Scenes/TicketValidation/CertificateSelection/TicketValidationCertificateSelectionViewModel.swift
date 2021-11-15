da2e2f4eff1c5b170ffeb1e800676974b6a520cc
Naveed Khalid <nkhalid.here@gmail.com>
2021-11-15 18:09:18 +0100

    initial changes to view model

--- src/xcode/ENA/ENA/Source/Scenes/TicketValidation/CertificateSelection/TicketValidationCertificateSelectionViewModel.swift
+++ src/xcode/ENA/ENA/Source/Scenes/TicketValidation/CertificateSelection/TicketValidationCertificateSelectionViewModel.swift
@@ -3,3 +3,22 @@
 //
 
 import Foundation
+import UIKit
+
+struct TicketValidationCertificateSelectionViewModel {
+
+	// MARK: - Internal
+
+	var dynamicTableViewModel: DynamicTableViewModel {
+		DynamicTableViewModel([
+			.section(
+				separators: .none,
+				cells: [
+					.headline(text: AppStrings.TicketValidation.CertificateSelection.serviceProviderRequirementsHeadline),
+					.headline(text: AppStrings.TicketValidation.CertificateSelection.serviceProviderRelevantCertificatesHeadline)
+				]
+			)
+		])
+	}
+
+}
