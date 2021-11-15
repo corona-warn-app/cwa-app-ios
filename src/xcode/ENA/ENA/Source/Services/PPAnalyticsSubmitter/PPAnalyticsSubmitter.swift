c33a5798c57396cb720db993015f1dc8e2097511
Naveed Khalid <nkhalid.here@gmail.com>
2021-11-15 14:29:08 +0100

    changes to ppa submitter

--- src/xcode/ENA/ENA/Source/Services/PPAnalyticsSubmitter/PPAnalyticsSubmitter.swift
+++ src/xcode/ENA/ENA/Source/Services/PPAnalyticsSubmitter/PPAnalyticsSubmitter.swift
@@ -701,6 +701,33 @@ final class PPAnalyticsSubmitter: PPAnalyticsSubmitting {
 				 }
 				}
 			}
+            if let exposureWindowsUntilTestResult = metadata?.exposureWindowsUntilTestResult {
+                $0.exposure = exposureWindowsAtTestRegistration.map { exposureWindow in
+                    SAP_Internal_Ppdd_PPANewExposureWindow.with {
+                  
+                      $0.normalizedTime = exposureWindow.normalizedTime
+                      $0.transmissionRiskLevel = Int32(exposureWindow.transmissionRiskLevel)
+                      $0.exposureWindow = SAP_Internal_Ppdd_PPAExposureWindow.with({ protobufWindow in
+                          if let infectiousness = exposureWindow.exposureWindow.infectiousness.protobuf {
+                              protobufWindow.infectiousness = infectiousness
+                          }
+                          if let reportType = exposureWindow.exposureWindow.reportType.protobuf {
+                              protobufWindow.reportType = reportType
+                          }
+                          protobufWindow.calibrationConfidence = Int32(exposureWindow.exposureWindow.calibrationConfidence.rawValue)
+                          protobufWindow.date = Int64(exposureWindow.date.timeIntervalSince1970)
+                          
+                          protobufWindow.scanInstances = exposureWindow.exposureWindow.scanInstances.map({ scanInstance in
+                              SAP_Internal_Ppdd_PPAExposureWindowScanInstance.with { protobufScanInstance in
+                                  protobufScanInstance.secondsSinceLastScan = Int32(scanInstance.secondsSinceLastScan)
+                                  protobufScanInstance.typicalAttenuation = Int32(scanInstance.typicalAttenuation)
+                                  protobufScanInstance.minAttenuation = Int32(scanInstance.minAttenuation)
+                              }
+                          })
+                      })
+                 }
+                }
+            }
 		}
 		return resultProtobuf
 	}
