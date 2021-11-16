79d670dc47d03182e8ce5e9dcc0782056806d8c8
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-11-16 09:58:27 +0100

    give descriptive error messages

--- src/xcode/ENA/ENASecurity/Tests/ENASecurityTests/ECKeyPairGenerationTests.swift
+++ src/xcode/ENA/ENASecurity/Tests/ENASecurityTests/ECKeyPairGenerationTests.swift
@@ -12,14 +12,18 @@ final class ECKeyPairGenerationTests: XCTestCase {
     func test_validPublicKeyBase64_isGenerated() {
         let generationClass = ECKeyPairGeneration()
         let generationOutput = generationClass.generatePrivateKey(with: "test")
-        guard let privateKey = generationOutput.0,
-              let publicKey = generationClass.generatePublicKey(from: privateKey) else {
-                  XCTFail()
-                  return
-              }
         
-        guard let publicKeyData = generationClass.generateData(from: publicKey).0 else {
-            XCTFail()
+        guard let privateKey = generationOutput.0 else {
+            XCTFail("Error while generating private EC key: \(generationOutput.1)")
+            return
+        }
+        guard let publicKey = generationClass.generatePublicKey(from: privateKey) else {
+            XCTFail("Error while generating Public EC key")
+            return
+        }
+        let generatedDataFromKey = generationClass.generateData(from: publicKey)
+        guard let publicKeyData = generatedDataFromKey.0 else {
+            XCTFail("Error Converting the SecKey to CFData \(generatedDataFromKey.1)")
             return
         }
         let bas64String = publicKeyData.base64EncodedString()
