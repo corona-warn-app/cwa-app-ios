083c98309fc450d9fde97d9ca1d5c3d87d46d0b5
Pascal Brause <pascal@brause.haus>
2021-11-17 13:27:15 +0100

    add new test to screenshot testplan

--- src/xcode/ENA/ENAUITests/TicketValidation/ENAUITests_19_TicketValidation.swift
+++ src/xcode/ENA/ENAUITests/TicketValidation/ENAUITests_19_TicketValidation.swift
@@ -11,6 +11,7 @@ class ENAUITests_19_TicketValidation: CWATestCase {
 		try super.setUpWithError()
 		continueAfterFailure = false
 		app = XCUIApplication()
+		setupSnapshot(app)
 		app.setDefaults()
 		app.setLaunchArgument(LaunchArguments.onboarding.isOnboarded, to: true)
 		app.setLaunchArgument(LaunchArguments.onboarding.setCurrentOnboardingVersion, to: true)
@@ -31,5 +32,25 @@ class ENAUITests_19_TicketValidation: CWATestCase {
 	
 		XCTAssertTrue(app.buttons[AccessibilityIdentifiers.General.primaryFooterButton].waitForExistence(timeout: .short))
 		XCTAssertTrue(app.buttons[AccessibilityIdentifiers.General.secondaryFooterButton].waitForExistence(timeout: .short))
+	}
+	
+	func test_screenshot_ticket_validation_firstConsent() throws {
+		app.launch()
+		
+		app.buttons[AccessibilityIdentifiers.TabBar.scanner].waitAndTap()
+		
+		/// Simulator only Alert will open where you can choose what the QRScanner should scan
+		let ticketValidationButton = try XCTUnwrap(app.buttons[AccessibilityIdentifiers.UniversalQRScanner.fakeTicketValidation])
+		ticketValidationButton.waitAndTap()
+		
+		XCTAssertTrue(app.cells[AccessibilityIdentifiers.TicketValidation.FirstConsent.legalBox].waitForExistence(timeout: .short))
+		
+		snapshot("screenshot_ticket_validation_first_consent_1")
+		app.swipeDown()
+		snapshot("screenshot_ticket_validation_first_consent_2")
+		app.swipeDown()
+		snapshot("screenshot_ticket_validation_first_consent_3")
+		app.swipeDown()
+		snapshot("screenshot_ticket_validation_first_consent_4")
 	}
 }
