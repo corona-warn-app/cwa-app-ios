ae555b8164220bf020d76545844884f7e2866255
Naveed Khalid <nkhalid.here@gmail.com>
2021-11-18 11:32:55 +0100

    changes to view model

--- src/xcode/ENA/ENA/Source/Scenes/TicketValidation/CertificateSelection/TicketValidationCertificateSelectionViewModel.swift
+++ src/xcode/ENA/ENA/Source/Scenes/TicketValidation/CertificateSelection/TicketValidationCertificateSelectionViewModel.swift
@@ -17,9 +17,9 @@ class TicketValidationCertificateSelectionViewModel {
 		self.validationConditions = validationConditions
 		self.healthCertificateService = healthCertificateService
 		
-		let serviceProviderRequirementsDescription = "Impfzertifikat, Genesenenzertifikat, Schnelltest-Testzertifikat, PCR-Testzertifikat Geburtsdatum: 1989-12-12 SCHNEIDER<<ANDREA"
+//		let serviceProviderRequirementsDescription = "Impfzertifikat, Genesenenzertifikat, Schnelltest-Testzertifikat, PCR-Testzertifikat Geburtsdatum: 1989-12-12 SCHNEIDER<<ANDREA"
 
-		self.setup(for: .suitableCertificates, with: serviceProviderRequirementsDescription)
+		self.filterCertificatesBasedOnValidationConditions()
 	}
 	
 	// MARK: - Internal
@@ -31,6 +31,68 @@ class TicketValidationCertificateSelectionViewModel {
 	private var healthCertificateService: HealthCertificateService
 	private var validationConditions: ValidationConditions
 	
+	private func filterCertificatesBasedOnValidationConditions() {
+		print(validationConditions)
+		guard let certificateTypes = validationConditions.type else {
+			return
+		}
+
+		var supportedCertificateTypes: [String] = []
+		var suitableCertificates: [HealthCertificate] = []
+		var serviceProviderRequirementsDescription: String = ""
+
+		if let healthCertifiedPerson = self.healthCertificateService.healthCertifiedPersons.first(where: {
+			$0.name?.standardizedGivenName == validationConditions.gnt &&
+			$0.name?.standardizedFamilyName == validationConditions.fnt &&
+			$0.dateOfBirth == validationConditions.dob
+		}) {
+			// if type contains v, all Vaccination Certificates shall pass the filter
+			if certificateTypes.contains("v") {
+				suitableCertificates.append(contentsOf: healthCertifiedPerson.vaccinationCertificates)
+				supportedCertificateTypes.append(AppStrings.TicketValidation.SupportedCertificateType.vaccinationCertificate)
+			}
+		    // if type contains r, all Recovery Certificates shall pass the filter
+			if certificateTypes.contains("r") {
+				suitableCertificates.append(contentsOf: healthCertifiedPerson.recoveryCertificates)
+				supportedCertificateTypes.append(AppStrings.TicketValidation.SupportedCertificateType.recoveryCertificate)
+			}
+			// if type contains t, all Test Certificates shall pass the filter
+			if certificateTypes.contains("t") {
+				suitableCertificates.append(contentsOf: healthCertifiedPerson.testCertificates)
+				supportedCertificateTypes.append(AppStrings.TicketValidation.SupportedCertificateType.testCertificate)
+			}
+			// if type contains tp, all PCR tests shall pass the filter
+			if certificateTypes.contains("tp") {
+				suitableCertificates.append(contentsOf: healthCertifiedPerson.pcrTestCertificates)
+				supportedCertificateTypes.append(AppStrings.TicketValidation.SupportedCertificateType.pcrTestCertificate)
+			}
+			// if type contains tr, all RAT tests shall pass the filter
+			if certificateTypes.contains("tr") {
+				suitableCertificates.append(contentsOf: healthCertifiedPerson.ratTestCertificates)
+				supportedCertificateTypes.append(AppStrings.TicketValidation.SupportedCertificateType.ratTestCertificate)
+			}
+			
+			// sorting on the basis of certificate type
+			suitableCertificates = suitableCertificates.sorted(by: >)
+			
+			// creating service provider requirements descriptions
+			serviceProviderRequirementsDescription += supportedCertificateTypes.joined(separator: ",")
+			if let dateOfBirth = validationConditions.dob {
+				serviceProviderRequirementsDescription += String(format: AppStrings.TicketValidation.CertificateSelection.dateOfBirth, dateOfBirth)
+			}
+			if let firstName = validationConditions.fnt, let givenName = validationConditions.gnt {
+				serviceProviderRequirementsDescription += "\n\(firstName)<<\(givenName)"
+			}
+			
+			// setting up view model
+			if suitableCertificates.isEmpty {
+				setup(for: .noSuitableCertificate, with: serviceProviderRequirementsDescription)
+			} else {
+				setup(for: .suitableCertificates, with: serviceProviderRequirementsDescription)
+			}
+		}
+	}
+
 	private func setup(for ticketValidationCertificateSelectionState: TicketValidationCertificateSelectionState, with serviceProviderRequirementsDescription: String) {
 		switch ticketValidationCertificateSelectionState {
 		case .suitableCertificates:
