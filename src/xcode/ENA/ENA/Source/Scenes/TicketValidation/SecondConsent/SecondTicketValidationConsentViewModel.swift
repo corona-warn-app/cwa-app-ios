d591a0e7d43287b44c9f6591501fb8638fce41ff
Pascal Brause <pascal@brause.haus>
2021-11-18 13:42:53 +0100

    create second consent screen, VC and VM

---
+++ src/xcode/ENA/ENA/Source/Scenes/TicketValidation/SecondConsent/SecondTicketValidationConsentViewModel.swift
@@ -0,0 +1,152 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+import UIKit
+
+struct SecondTicketValidationConsentViewModel {
+	
+	init(
+		serviceIdentity: String,
+		serviceProvider: String,
+		healthCertificate: HealthCertificate,
+		onDataPrivacyTap: @escaping () -> Void
+	) {
+		self.serviceIdentity = serviceIdentity
+		self.serviceProvider = serviceProvider
+		self.healthCertificate = healthCertificate
+		self.onDataPrivacyTap = onDataPrivacyTap
+	}
+	
+	// MARK: - Internal
+	
+	var dynamicTableViewModel: DynamicTableViewModel {
+		/*
+		DynamicTableViewModel([
+			// Image with title
+			.section(
+				header: .image(
+					UIImage(named: "Illu_TicketValidation"),
+					title: AppStrings.TicketValidation.FirstConsent.title,
+					accessibilityLabel: AppStrings.TicketValidation.FirstConsent.imageDescription,
+					accessibilityIdentifier: AccessibilityIdentifiers.TicketValidation.FirstConsent.image
+				),
+				cells: []
+			),
+			// Subtitle with service provider and subject
+			.section(
+				cells: [
+					.title2(
+						text: AppStrings.TicketValidation.FirstConsent.subtitle
+					),
+					.footnote(
+						text: AppStrings.TicketValidation.FirstConsent.serviceProvider,
+						color: .enaColor(for: .textPrimary2)
+					),
+					.bodyWithoutTopInset(
+						text: String(
+							format: AppStrings.TicketValidation.FirstConsent.serviceProviderValue,
+							serviceProvider
+						)
+					),
+					.footnote(
+						text: AppStrings.TicketValidation.FirstConsent.subject,
+						color: .enaColor(for: .textPrimary2)
+					),
+					.bodyWithoutTopInset(
+						text: String(
+							format: AppStrings.TicketValidation.FirstConsent.subjectValue,
+							subject
+						)
+					),
+					.body(
+						text: AppStrings.TicketValidation.FirstConsent.explination
+					)
+				]
+			),
+			// Bulletpoints in consent box
+			.section(
+				cells: [
+					.legalExtendedDataDonation(
+						title: NSAttributedString(
+							string: AppStrings.TicketValidation.FirstConsent.legalTitle
+						),
+						description: NSAttributedString(
+							string: AppStrings.TicketValidation.FirstConsent.legalSubtitle,
+							attributes: [.font: UIFont.preferredFont(forTextStyle: .body)]
+						),
+						bulletPoints: [
+							NSAttributedString(string: AppStrings.TicketValidation.FirstConsent.legalBulletPoint1),
+							NSAttributedString(string: AppStrings.TicketValidation.FirstConsent.legalBulletPoint2)
+						],
+						accessibilityIdentifier: AccessibilityIdentifiers.TicketValidation.FirstConsent.legalBox,
+						configure: { _, cell, _ in
+							cell.backgroundColor = .enaColor(for: .background)
+						}
+					)
+				]
+			),
+			// Bulletpoints without box
+			.section(
+				cells: [
+					.space(
+						height: 20
+					),
+					.bulletPoint(
+						text: AppStrings.TicketValidation.FirstConsent.bulletPoint1
+					),
+					.space(
+						height: 20
+					),
+					.bulletPoint(
+						text: AppStrings.TicketValidation.FirstConsent.bulletPoint2
+					),
+					.space(
+						height: 20
+					),
+					.bulletPoint(
+						text: AppStrings.TicketValidation.FirstConsent.bulletPoint3
+					),
+					.space(
+						height: 20
+					),
+					.bulletPoint(
+						text: AppStrings.TicketValidation.FirstConsent.bulletPoint4
+					),
+					.space(
+						height: 20
+					)
+				]
+			),
+			
+			// Data privacy cell
+			.section(
+				separators: .all,
+				cells: [
+					.body(
+						text: AppStrings.TicketValidation.FirstConsent.dataPrivacyTitle,
+						style: DynamicCell.TextCellStyle.label,
+						accessibilityIdentifier: AccessibilityIdentifiers.TicketValidation.FirstConsent.dataPrivacy,
+						accessibilityTraits: UIAccessibilityTraits.link,
+						action: .execute { _, _ in
+							onDataPrivacyTap()
+						},
+						configure: { _, cell, _ in
+							cell.accessoryType = .disclosureIndicator
+							cell.selectionStyle = .default
+						}
+					)
+				]
+			)
+		])
+		 */
+	}
+	
+	// MARK: - Private
+	
+	private let serviceIdentity: String
+	private let serviceProvider: String
+	private let healthCertificate: HealthCertificate
+	private let onDataPrivacyTap: () -> Void	
+}
