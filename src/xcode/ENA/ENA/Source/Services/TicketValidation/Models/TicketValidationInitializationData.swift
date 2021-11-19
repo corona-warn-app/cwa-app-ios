071869d9c18a2ac927d22ba3b5d6c857948a7551
Artur Friesen <mail@arturfriesen.de>
2021-11-19 14:47:31 +0100

    Add encrypt and sign of DCC.

--- src/xcode/ENA/ENA/Source/Services/TicketValidation/Models/TicketValidationInitializationData.swift
+++ src/xcode/ENA/ENA/Source/Services/TicketValidation/Models/TicketValidationInitializationData.swift
@@ -13,4 +13,26 @@ struct TicketValidationInitializationData: Codable {
 	let consent: String
 	let subject: String
 	let serviceProvider: String
+	
+	static func fake (
+		`protocol`: String = "protocol",
+		protocolVersion: String = "protocolVersion",
+		serviceIdentity: String = "serviceIdentity",
+		privacyUrl: String = "privacyUrl",
+		token: String = "token",
+		consent: String = "consent",
+		subject: String = "subject",
+		serviceProvider: String = "serviceProvider"
+	) -> TicketValidationInitializationData {
+		return TicketValidationInitializationData(
+			protocol: `protocol`,
+			protocolVersion: protocolVersion,
+			serviceIdentity: serviceIdentity,
+			privacyUrl: privacyUrl,
+			token: token,
+			consent: consent,
+			subject: subject,
+			serviceProvider: serviceProvider
+		)
+	}
 }
--- src/xcode/ENA/ENA/Source/Services/TicketValidation/TicketValidation.swift
+++ src/xcode/ENA/ENA/Source/Services/TicketValidation/TicketValidation.swift
@@ -3,6 +3,7 @@
 //
 
 import Foundation
+import ENASecurity
 
 final class TicketValidation: TicketValidating {
 	
@@ -43,5 +44,92 @@ final class TicketValidation: TicketValidating {
 	func cancel() {
 
 	}
-
+	
+	func encryptAndSignDCC(
+		dccBarcodeData: String,
+		nonceBase64: String,
+		encryptionScheme: EncryptionScheme,
+		publicKeyForEncryption: SecKey,
+		privateKeyForSigning: SecKey
+	) -> Result<EncryptAndSignResult, EncryptAndSignError> {
+		
+		// Determine iv (initializationVector): the iv shall be derived as the byte sequence (Data / ByteArray) of nonceBase64 using base64 encoding.
+		guard let initializationVector = Data(base64Encoded: nonceBase64) else {
+			return .failure(.UNKNOWN)
+		}
+		
+		// Determine plaintext: the plaintext shall be derived as the byte sequence (Data / ByteArray) of dccBarcodeData using UTF-8 encoding.
+		guard let plaintext = dccBarcodeData.data(using: .utf8) else {
+			return .failure(.UNKNOWN)
+		}
+		
+		// Generate key: the key shall be generated as a secure random byte sequence (Data / ByteArray) of 32 bytes
+		guard let key = Data.randomBytes(length: 32) else {
+			return .failure(.UNKNOWN)
+		}
+		
+		// Encrypt DCC as encryptedDCC
+		let encryptedDCC: Data
+		
+		switch encryptionScheme {
+		case .RSAOAEPWithSHA256AESCBC:
+			let cbcEncryption = CBCEncryption(
+				encryptionKey: key,
+				initializationVector: initializationVector,
+				ivLengthConstraint: 16
+			)
+			switch cbcEncryption.encrypt(data: plaintext) {
+			case .success(let encryptedData):
+				encryptedDCC = encryptedData
+			case .failure(let error):
+				return .failure(.AES_CBC_ERROR(error))
+			}
+		case .RSAOAEPWithSHA256AESGCM:
+			let gcmEncryption = GCMEncryption(
+				encryptionKey: key,
+				initializationVector: initializationVector,
+				ivLengthConstraint: 16
+			)
+			switch gcmEncryption.encrypt(data: plaintext) {
+			case .success(let encryptedData):
+				encryptedDCC = encryptedData
+			case .failure(let error):
+				return .failure(.AES_GCM_ERROR(error))
+			}
+		}
+		
+		// Encrypt key as encryptionKey
+		let encryptionKey: Data
+		switch RSAEncryption().encrypt(key, publicKey: publicKeyForEncryption) {
+		case .success(let encryptedData):
+			encryptionKey = encryptedData
+		case .failure(let error):
+			return .failure(.RSA_ENC_ERROR(error))
+		}
+		
+		// Sign ciphertext
+		let signer = ECSHA256Signer(
+			privateKey: privateKeyForSigning,
+			data: encryptedDCC
+		)
+		
+		let signature: Data
+		switch signer.sign() {
+		case .success(let signedData):
+			signature = signedData
+		case .failure(let error):
+			return .failure(.EC_SIGN_ERROR(error))
+		}
+		
+		// Determine encryptedDCCBase64: the encryptedDCCBase64 shall be derived as the base64-encoded string of encryptedDCC
+		// Determine encryptionKeyBase64: the encryptionKeyBase64 shall be derived as the base64-encoded string of encryptionKey
+		// Determine signatureBase64: the signatureBase64 shall be derived as the base64-encoded string of signature
+		// Determine signatureAlgorithm: the signatureAlgorithm shall be set to the string SHA256withECDSA
+		return .success(EncryptAndSignResult(
+			encryptedDCCBase64: encryptedDCC.base64EncodedString(),
+			encryptionKeyBase64: encryptionKey.base64EncodedString(),
+			signatureBase64: signature.base64EncodedString(),
+			signatureAlgorithm: "SHA256withECDSA"
+		))
+	}
 }
