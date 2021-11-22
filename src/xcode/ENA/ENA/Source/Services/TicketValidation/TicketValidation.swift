6647a3b9d7a8610ffa77dbdb38b80ae45040fa24
Artur Friesen <mail@arturfriesen.de>
2021-11-22 11:17:55 +0100

    Add data models.

--- src/xcode/ENA/ENA/Source/Services/TicketValidation/TicketValidation.swift
+++ src/xcode/ENA/ENA/Source/Services/TicketValidation/TicketValidation.swift
@@ -3,6 +3,7 @@
 //
 
 import Foundation
+import ENASecurity
 
 final class TicketValidation: TicketValidating {
 	
@@ -44,4 +45,30 @@ final class TicketValidation: TicketValidating {
 
 	}
 
+	struct ServiceIdentityRequestResult {
+		let validationServiceEncKeyJwkSetForRSAOAEPWithSHA256AESCBC: [JSONWebKey]
+		let validationServiceEncKeyJwkSetForRSAOAEPWithSHA256AESGCM: [JSONWebKey]
+		let validationServiceSignKeyJwkSet: [JSONWebKey]
+	}
+	
+	enum ServiceIdentityRequestError: Error {
+		case VS_ID_CLIENT_ERR
+		case VS_ID_NO_NETWORK
+		case VS_ID_SERVER_ERR
+		case VS_ID_NO_ENC_KEY
+		case VS_ID_NO_SIGN_KEY
+		case VS_ID_CERT_PIN_NO_JWK_FOR_KID
+		case VS_ID_CERT_PIN_MISMATCH
+		case VS_ID_PARSE_ERR
+		case VS_ID_EMPTY_X5C
+	}
+	
+	func requestServiceIdentityDocument(
+		validationServiceData: ValidationServiceData,
+		validationServiceJwkSet: [JSONWebKey]
+	) -> Result<ServiceIdentityRequestResult, ServiceIdentityRequestError> {
+			
+			
+		return .failure(.VS_ID_CERT_PIN_MISMATCH)
+	}
 }
