d4252042e2a090c21165b58384ad5f849e870e41
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-11-24 10:28:56 +0100

    add custom error handeling

--- src/xcode/ENA/ENA/Source/HTTPClient/Resources/DecoratorIdentityDocumentResource.swift
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/DecoratorIdentityDocumentResource.swift
@@ -26,4 +26,21 @@ struct DecoratorIdentityDocumentResource: Resource {
 	var sendResource: EmptySendResource
 	var receiveResource: JSONReceiveResource<ServiceIdentityDocument>
 	
+	func customError(for error: ServiceError<DecoratorServiceIdentityDocumentError>) -> DecoratorServiceIdentityDocumentError? {
+		switch error {
+		case .unexpectedServerError(let statusCode):
+			switch statusCode {
+			case 400...409:
+				return .VD_ID_CLIENT_ERR
+			case 500...509:
+				return .VD_ID_SERVER_ERR
+			default:
+				return nil
+			}
+		case .resourceError(.decoding):
+			return .VD_ID_PARSE_ERR
+		default:
+			return nil
+		}
+	}
 }
