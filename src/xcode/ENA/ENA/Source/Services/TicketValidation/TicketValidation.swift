43fa6722f18dc75a919eea9d1cc19069150ea95d
Artur Friesen <mail@arturfriesen.de>
2021-11-24 16:58:30 +0100

    Finish service identity document checking.

--- src/xcode/ENA/ENA/Source/Services/TicketValidation/TicketValidation.swift
+++ src/xcode/ENA/ENA/Source/Services/TicketValidation/TicketValidation.swift
@@ -87,12 +87,61 @@ final class TicketValidation: TicketValidating {
 				}
                 
                 // 3. Find verificationMethodsForRSAOAEPWithSHA256AESCBC
-                let regEx = "ValidationServiceEncScheme-RSAOAEPWithSHA256AESCBC$"
-                
-                var verificationMethodsForRSAOAEPWithSHA256AESCBC = serviceIdentityDocument.verificationMethod.first { verificationMethod in
-                    let regExExists = verificationMethod.id.check(regex: regEx)
+                let cbcRegEx = "ValidationServiceEncScheme-RSAOAEPWithSHA256AESCBC$"
+                let verificationMethodsForRSAOAEPWithSHA256AESCBC = serviceIdentityDocument.verificationMethod.first { verificationMethod in
+                    let regExExists = verificationMethod.id.check(regex: cbcRegEx)
                     return regExExists && verificationMethod.verificationMethods != nil
                 }?.verificationMethods ?? []
+				
+				// 4. Find validationServiceEncKeyJwkSetForRSAOAEPWithSHA256AESCBC
+				let validationServiceEncKeyJwkSetForRSAOAEPWithSHA256AESCBC = serviceIdentityDocument.verificationMethod.filter { verificationMethod in
+					verificationMethodsForRSAOAEPWithSHA256AESCBC.contains(verificationMethod.id)
+				}.compactMap { verificationMethod in
+					verificationMethod.publicKeyJwk
+				}
+				Log.debug("Found validationServiceEncKeyJwkSetForRSAOAEPWithSHA256AESCBC: \(private: validationServiceEncKeyJwkSetForRSAOAEPWithSHA256AESCBC)", log: .ticketValidation)
+				
+				// 5. Find verificationMethodsForRSAOAEPWithSHA256AESGCM
+				let gcmRegEx = "ValidationServiceEncScheme-RSAOAEPWithSHA256AESGCM$"
+				let verificationMethodsForRSAOAEPWithSHA256AESGCM = serviceIdentityDocument.verificationMethod.first { verificationMethod in
+					let regExExists = verificationMethod.id.check(regex: gcmRegEx)
+					return regExExists && verificationMethod.verificationMethods != nil
+				}?.verificationMethods ?? []
+				
+				// 6. Find validationServiceEncKeyJwkSetForRSAOAEPWithSHA256AESGCM
+				let validationServiceEncKeyJwkSetForRSAOAEPWithSHA256AESGCM = serviceIdentityDocument.verificationMethod.filter { verificationMethod in
+					verificationMethodsForRSAOAEPWithSHA256AESGCM.contains(verificationMethod.id)
+				}.compactMap { verificationMethod in
+					verificationMethod.publicKeyJwk
+				}
+				Log.debug("Found validationServiceEncKeyJwkSetForRSAOAEPWithSHA256AESGCM: \(private: validationServiceEncKeyJwkSetForRSAOAEPWithSHA256AESGCM)")
+				
+				// 7. Check encryption keys
+				if validationServiceEncKeyJwkSetForRSAOAEPWithSHA256AESCBC.isEmpty && validationServiceEncKeyJwkSetForRSAOAEPWithSHA256AESGCM.isEmpty {
+					completion(.failure(.VS_ID_NO_ENC_KEY))
+					return
+				}
+				
+				// 8. Find validationServiceSignKeyJwkSet
+				let regex = "ValidationServiceSignKey-\\d+$"
+				let validationServiceSignKeyJwkSet = serviceIdentityDocument.verificationMethod.filter { verificationMethod in
+					verificationMethod.id.check(regex: regex)
+				}.compactMap { verificationMethod in
+					verificationMethod.publicKeyJwk
+				}
+				
+				if validationServiceSignKeyJwkSet.isEmpty {
+					completion(.failure(.VS_ID_NO_SIGN_KEY))
+					return
+				}
+				Log.debug("Found validationServiceSignKeyJwkSet: \(private: validationServiceSignKeyJwkSet)")
+
+				let result = ServiceIdentityRequestResult(
+					validationServiceEncKeyJwkSetForRSAOAEPWithSHA256AESCBC: validationServiceEncKeyJwkSetForRSAOAEPWithSHA256AESCBC,
+					validationServiceEncKeyJwkSetForRSAOAEPWithSHA256AESGCM: validationServiceEncKeyJwkSetForRSAOAEPWithSHA256AESGCM,
+					validationServiceSignKeyJwkSet: validationServiceSignKeyJwkSet
+				)
+				completion(.success(result))
 				
 			case .failure(let error):
 				completion(.failure(.REST_SERVICE_ERROR(error)))
