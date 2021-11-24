936dc0ffd54b4253aa3f0a0367f723bbcad696be
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-11-24 11:41:43 +0100

    add DecoratorService

---
+++ src/xcode/ENA/ENA/Source/Services/TicketValidation/Models/ValidationDecorator/DecoratorService.swift
@@ -0,0 +1,12 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+
+public struct DecoratorService: Codable {
+	let id: String
+	let type: String
+	let serviceEndpoint: String
+	let name: String
+}
