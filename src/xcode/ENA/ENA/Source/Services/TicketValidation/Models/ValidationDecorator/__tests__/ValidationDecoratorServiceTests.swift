2b52c33b405795d3ff2623f903b9a73ad6e02cff
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-11-25 08:40:10 +0100

    add happy scenario test

--- src/xcode/ENA/ENA/Source/Services/TicketValidation/Models/ValidationDecorator/__tests__/ValidationDecoratorServiceTests.swift
+++ src/xcode/ENA/ENA/Source/Services/TicketValidation/Models/ValidationDecorator/__tests__/ValidationDecoratorServiceTests.swift
@@ -8,4 +8,21 @@ import ENASecurity
 
 class ValidationDecoratorServiceTests: XCTestCase {
 
+	// Happy scenario success Case
+	
+	func test_happyScenario_Return_Identity_Document() {
+		let restServiceProvider = RestServiceProviderStub(results: [
+			.success(ServiceIdentityDocument.fake())
+		])
+		let decoratorService = ValidationDecoratorService(restServiceProvider: restServiceProvider)
+		
+		decoratorService.requestIdentityDocumentOfTheValidationDecorator(urlString: "test") { result in
+			switch result {
+			case .success(let identityDocument):
+				XCTAssertEqual(identityDocument.accessTokenService.id, "test")
+			case .failure(let error):
+				XCTFail("expected test to succeeds" + error.localizedDescription)
+			}
+		}
+	}
 }
