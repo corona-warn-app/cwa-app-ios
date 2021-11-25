a62c6afdac91187330dfa0146b868a8947fc952f
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-11-25 08:38:38 +0100

    add fake ServiceIdentityDocument

--- src/xcode/ENA/ENA/Source/Services/TicketValidation/Models/ValidationDecorator/ServiceIdentityDocument.swift
+++ src/xcode/ENA/ENA/Source/Services/TicketValidation/Models/ValidationDecorator/ServiceIdentityDocument.swift
@@ -3,9 +3,80 @@
 //
 
 import Foundation
+import ENASecurity
 
 struct ServiceIdentityDocument: Codable {
 	let id: String
 	let verificationMethod: [VerificationMethod]
 	let service: [DecoratorServiceModel]?
+	
+	public static func fake(
+		id: String = "fake",
+		verificationMethod: [VerificationMethod] = [
+			VerificationMethod(
+				id: "https://test.com/AccessTokenSignKey-1",
+				type: "JsonWebKey2020",
+				controller: "https://test.com/api/identity",
+				publicKeyJwk: JSONWebKey.fake(),
+				verificationMethods: nil
+			),
+			VerificationMethod(
+				id: "https://test.com/AccessTokenServiceKey-1",
+				type: "JsonWebKey2020",
+				controller: "https://test.com/api/identity",
+				publicKeyJwk: JSONWebKey.fake(),
+				verificationMethods: nil
+			),
+			VerificationMethod(
+				id: "https://test.com/ServiceProviderKey-1",
+				type: "JsonWebKey2020",
+				controller: "https://test.com/api/identity",
+				publicKeyJwk: JSONWebKey.fake(),
+				verificationMethods: nil
+			),
+			VerificationMethod(
+				id: "https://test.com/CancellationServiceKey-1",
+				type: "JsonWebKey2020",
+				controller: "https://test.com/api/identity",
+				publicKeyJwk: JSONWebKey.fake(),
+				verificationMethods: nil
+			),
+			VerificationMethod(
+				id: "https://test.com/StatusServiceKey-1",
+				type: "JsonWebKey2020",
+				controller: "https://test.com/api/identity",
+				publicKeyJwk: JSONWebKey.fake(),
+				verificationMethods: nil
+			),
+			VerificationMethod(
+				id: "https://test.com/ValidationServiceKey-1",
+				type: "JsonWebKey2020",
+				controller: "https://test.com/api/identity",
+				publicKeyJwk: JSONWebKey.fake(),
+				verificationMethods: nil
+			)
+		],
+		service: [DecoratorServiceModel]? = [
+			DecoratorServiceModel(
+				id: "test",
+				type: "CancellationService",
+				serviceEndpoint: "test",
+				name: "test"
+			),
+			DecoratorServiceModel(
+				id: "test",
+				type: "ValidationService",
+				serviceEndpoint: "test",
+				name: "test"
+			),
+			DecoratorServiceModel(
+				id: "test",
+				type: "AccessTokenService",
+				serviceEndpoint: "test",
+				name: "test"
+			)
+		]
+	) -> ServiceIdentityDocument {
+		ServiceIdentityDocument(id: id, verificationMethod: verificationMethod, service: service)
+	}
 }
