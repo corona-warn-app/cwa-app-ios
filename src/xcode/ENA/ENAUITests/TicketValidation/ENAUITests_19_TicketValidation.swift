a346130c7596acb805e698369c50296b90681d40
Naveed Khalid <nkhalid.here@gmail.com>
2021-11-25 20:56:39 +0100

    adding tests

--- src/xcode/ENA/ENAUITests/TicketValidation/ENAUITests_19_TicketValidation.swift
+++ src/xcode/ENA/ENAUITests/TicketValidation/ENAUITests_19_TicketValidation.swift
@@ -17,8 +17,9 @@ class ENAUITests_19_TicketValidation: CWATestCase {
 		app.setLaunchArgument(LaunchArguments.onboarding.setCurrentOnboardingVersion, to: true)
 	}
 	
-	func test_ticketValidation_flow() throws {
+	func test_ticketValidation_flow_open() throws {
 		app.setLaunchArgument(LaunchArguments.healthCertificate.secondHealthCertificate, to: true)
+		app.setLaunchArgument(LaunchArguments.ticketValidation.result.isOpen, to: true)
 		app.launch()
 		
 		app.buttons[AccessibilityIdentifiers.TabBar.scanner].waitAndTap()
@@ -54,9 +55,16 @@ class ENAUITests_19_TicketValidation: CWATestCase {
 		/// check for the action buttons on second consent screen
 		XCTAssertTrue(app.buttons[AccessibilityIdentifiers.General.primaryFooterButton].waitForExistence(timeout: .short))
 		XCTAssertTrue(app.buttons[AccessibilityIdentifiers.General.secondaryFooterButton].waitForExistence(timeout: .short))
+		
+		/// navigate to the validation result screen
+		app.buttons[AccessibilityIdentifiers.General.primaryFooterButton].waitAndTap()
+		
+		/// check for the content on validation result screen
+		XCTAssertTrue(app.cells[AccessibilityIdentifiers.TicketValidation.ValidationResult.Open.headerImageWithTitle].waitForExistence(timeout: .short))
+		XCTAssertTrue(app.cells[AccessibilityIdentifiers.TicketValidation.ValidationResult.Open.subtitle].waitForExistence(timeout: .short))
 	}
 	
-	func test_screenshot_ticketValidation_flow() throws {
+	func test_screenshot_ticketValidation_passed() throws {
 		app.setLaunchArgument(LaunchArguments.healthCertificate.secondHealthCertificate, to: true)
 		app.launch()
 		
@@ -99,5 +107,52 @@ class ENAUITests_19_TicketValidation: CWATestCase {
 		snapshot("screenshot_ticket_validation_second_consent_3")
 		app.swipeUp()
 		snapshot("screenshot_ticket_validation_second_consent_4")
+		
+		/// navigate to the validation result screen
+		app.buttons[AccessibilityIdentifiers.General.primaryFooterButton].waitAndTap()
+		
+		/// check for the content on validation result screen
+		XCTAssertTrue(app.cells[AccessibilityIdentifiers.TicketValidation.ValidationResult.Passed.headerImageWithTitle].waitForExistence(timeout: .short))
+		XCTAssertTrue(app.cells[AccessibilityIdentifiers.TicketValidation.ValidationResult.Passed.subtitle].waitForExistence(timeout: .short))
+		
+		/// take screenshot on validation result screen
+		snapshot("screenshot_ticket_validation_result_passed")
+	}
+	
+	func test_screenshot_ticketValidation_failed() throws {
+		app.setLaunchArgument(LaunchArguments.healthCertificate.secondHealthCertificate, to: true)
+		app.setLaunchArgument(LaunchArguments.ticketValidation.result.isFailed, to: true)
+		app.launch()
+		
+		app.buttons[AccessibilityIdentifiers.TabBar.scanner].waitAndTap()
+		
+		/// Simulator only Alert will open where you can choose what the QRScanner should scan
+		let ticketValidationButton = try XCTUnwrap(app.buttons[AccessibilityIdentifiers.UniversalQRScanner.fakeTicketValidation])
+		ticketValidationButton.waitAndTap()
+		
+		/// check for the content on first consent screen
+		XCTAssertTrue(app.cells[AccessibilityIdentifiers.TicketValidation.FirstConsent.legalBox].waitForExistence(timeout: .short))
+		
+		/// navigate to certificate selection screen
+		app.buttons[AccessibilityIdentifiers.General.primaryFooterButton].waitAndTap()
+		
+		/// check for health certificate cell on certificate selection screen
+		XCTAssertTrue(app.cells[AccessibilityIdentifiers.HealthCertificate.Person.certificateCell].waitForExistence(timeout: .short))
+		
+		/// navigate to second consent screen
+		app.cells[AccessibilityIdentifiers.HealthCertificate.Person.certificateCell].firstMatch.waitAndTap()
+		
+		/// check the conent on second consent screen
+		XCTAssertTrue(app.cells[AccessibilityIdentifiers.TicketValidation.SecondConsent.legalBox].waitForExistence(timeout: .short))
+		
+		/// navigate to the validation result screen
+		app.buttons[AccessibilityIdentifiers.General.primaryFooterButton].waitAndTap()
+		
+		/// check for the content on validation result screen
+		XCTAssertTrue(app.cells[AccessibilityIdentifiers.TicketValidation.ValidationResult.Failed.headerImageWithTitle].waitForExistence(timeout: .short))
+		XCTAssertTrue(app.cells[AccessibilityIdentifiers.TicketValidation.ValidationResult.Failed.subtitle].waitForExistence(timeout: .short))
+		
+		/// take screenshot on validation result screen
+		snapshot("screenshot_ticket_validation_result_failed")
 	}
 }
