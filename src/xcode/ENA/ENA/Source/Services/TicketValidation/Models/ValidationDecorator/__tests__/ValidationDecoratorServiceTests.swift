659ead1178722780241543d45326bbbe4daeb9da
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-11-25 08:43:17 +0100

    test not finding matches for AccessToeknSignJWKSet

--- src/xcode/ENA/ENA/Source/Services/TicketValidation/Models/ValidationDecorator/__tests__/ValidationDecoratorServiceTests.swift
+++ src/xcode/ENA/ENA/Source/Services/TicketValidation/Models/ValidationDecorator/__tests__/ValidationDecoratorServiceTests.swift
@@ -116,4 +116,42 @@ class ValidationDecoratorServiceTests: XCTestCase {
 			}
 		}
 	}
+	
+	func test_If_AccessTokenSignJwkSet_Not_Found_Then_Abort() {
+		let restServiceProvider = RestServiceProviderStub(results: [
+			.success(ServiceIdentityDocument.fake(verificationMethod: [
+				VerificationMethod(
+					id: "https://test.com/AccessTokenServiceKey-1",
+					type: "JsonWebKey2020",
+					controller: "https://test.com/api/identity",
+					publicKeyJwk: JSONWebKey.fake(),
+					verificationMethods: nil
+				),
+				VerificationMethod(
+					id: "https://test.com/AccessTokenServiceKey-1",
+					type: "JsonWebKey2020",
+					controller: "https://test.com/api/identity",
+					publicKeyJwk: JSONWebKey.fake(),
+					verificationMethods: nil
+				),
+				VerificationMethod(
+					id: "https://test.com/ValidationServiceKey-1",
+					type: "JsonWebKey2020",
+					controller: "https://test.com/api/identity",
+					publicKeyJwk: JSONWebKey.fake(),
+					verificationMethods: nil
+				)
+			]))
+		])
+		let decoratorService = ValidationDecoratorService(restServiceProvider: restServiceProvider)
+		
+		decoratorService.requestIdentityDocumentOfTheValidationDecorator(urlString: "test") { result in
+			switch result {
+			case .success:
+				XCTFail("expected test to fail")
+			case .failure(let error):
+				XCTAssertEqual(error, .VD_ID_NO_ATS_SIGN_KEY, "An error should occur due to not finding the accessTokenSignJwkSet")
+			}
+		}
+	}
 }
