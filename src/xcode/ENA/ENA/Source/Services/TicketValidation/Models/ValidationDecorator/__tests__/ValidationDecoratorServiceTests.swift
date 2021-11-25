77db7b67d444f8a054de6edf4ba871955768faba
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-11-25 08:40:49 +0100

    test verification of JWKs

--- src/xcode/ENA/ENA/Source/Services/TicketValidation/Models/ValidationDecorator/__tests__/ValidationDecoratorServiceTests.swift
+++ src/xcode/ENA/ENA/Source/Services/TicketValidation/Models/ValidationDecorator/__tests__/ValidationDecoratorServiceTests.swift
@@ -25,4 +25,37 @@ class ValidationDecoratorServiceTests: XCTestCase {
 			}
 		}
 	}
+	
+	// Validation Failure Cases
+
+	func test_If_Verification_Of_JWKs_Fails_Then_Abort() {
+		let restServiceProvider = RestServiceProviderStub(results: [
+			.success(ServiceIdentityDocument.fake(verificationMethod: [
+				VerificationMethod(
+					id: "https://test.com/AccessTokenSignKey-1",
+					type: "JsonWebKey2020",
+					controller: "https://test.com/api/identity",
+					publicKeyJwk: JSONWebKey.fake(x5c: [], kid: "test", alg: "test", use: "test"),
+					verificationMethods: nil
+				),
+				VerificationMethod(
+					id: "https://test.com/AccessTokenServiceKey-1",
+					type: "JsonWebKey2020",
+					controller: "https://test.com/api/identity",
+					publicKeyJwk: JSONWebKey.fake(),
+					verificationMethods: nil
+				)
+			]))
+		])
+		let decoratorService = ValidationDecoratorService(restServiceProvider: restServiceProvider)
+		
+		decoratorService.requestIdentityDocumentOfTheValidationDecorator(urlString: "test") { result in
+			switch result {
+			case .success:
+				XCTFail("expected test to fail")
+			case .failure(let error):
+				XCTAssertEqual(error, .VD_ID_EMPTY_X5C, "An error should occur due to empty x5c array")
+			}
+		}
+	}
 }
