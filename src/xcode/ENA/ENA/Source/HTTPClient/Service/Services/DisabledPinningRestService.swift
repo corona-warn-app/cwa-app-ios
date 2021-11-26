6904f205fc7c4592c55bb6f9130209acedcb8b60
Pascal Brause <pascal@brause.haus>
2021-11-26 15:40:54 +0100

    WIP: create restService

---
+++ src/xcode/ENA/ENA/Source/HTTPClient/Service/Services/DisabledPinningRestService.swift
@@ -0,0 +1,39 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+
+/**
+Specific implementation of a service who is doing nothing special, but disables the certificate pinning.
+It uses the coronaWarnSessionConfiguration.
+*/
+class DisabledPinningRestService: Service {
+
+	// MARK: - Init
+
+	required init(
+		environment: EnvironmentProviding = Environments(),
+		session: URLSession? = nil
+	) {
+		self.environment = environment
+		self.optionalSession = session
+	}
+
+	// MARK: - Protocol Service
+
+	let environment: EnvironmentProviding
+
+	lazy var session: URLSession = {
+		optionalSession ??
+		.coronaWarnSession(
+			configuration: .coronaWarnSessionConfiguration(),
+			delegateQueue: .main
+		)
+	}()
+
+	// MARK: - Private
+
+	private let optionalSession: URLSession?
+
+}
