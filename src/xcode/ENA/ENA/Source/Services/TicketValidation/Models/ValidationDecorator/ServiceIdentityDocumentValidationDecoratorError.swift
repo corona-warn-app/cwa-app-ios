5ec6a796f92b5e11d7e6b1d1be4c9d69e907b6cf
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-11-26 05:00:57 +0100

    confirm to equatable

--- src/xcode/ENA/ENA/Source/Services/TicketValidation/Models/ValidationDecorator/ServiceIdentityDocumentValidationDecoratorError.swift
+++ src/xcode/ENA/ENA/Source/Services/TicketValidation/Models/ValidationDecorator/ServiceIdentityDocumentValidationDecoratorError.swift
@@ -19,3 +19,27 @@ enum ServiceIdentityResourceDecoratorError: Error {
 	case VD_ID_SERVER_ERR
 	case VD_ID_PARSE_ERR
 }
+
+// swiftlint:disable pattern_matching_keywords
+extension ServiceIdentityValidationDecoratorError: Equatable {
+	static func == (lhs: ServiceIdentityValidationDecoratorError, rhs: ServiceIdentityValidationDecoratorError) -> Bool {
+		switch (lhs, rhs) {
+		case (.VD_ID_NO_ATS, .VD_ID_NO_ATS):
+			return true
+		case (.VD_ID_NO_ATS_SIGN_KEY, .VD_ID_NO_ATS_SIGN_KEY):
+			return true
+		case (.VD_ID_NO_ATS_SVC_KEY, .VD_ID_NO_ATS_SVC_KEY):
+			return true
+		case (.VD_ID_NO_VS, .VD_ID_NO_VS):
+			return true
+		case (.VD_ID_NO_VS_SVC_KEY, .VD_ID_NO_VS_SVC_KEY):
+			return true
+		case (.VD_ID_EMPTY_X5C, .VD_ID_EMPTY_X5C):
+			return true
+		case (.REST_SERVICE_ERROR(let lhsResult), .REST_SERVICE_ERROR(let rhsResult)):
+			return lhsResult == rhsResult
+		default:
+			return false
+		}
+	}
+}
