02b1e3ecabc865f6b7eb91aab8aafdc2314ec9c1
Naveed Khalid <nkhalid.here@gmail.com>
2021-11-29 16:09:25 +0100

    refactor qr scanner coordinator

--- src/xcode/ENA/ENA/Source/Scenes/QRScanner/QRScannerCoordinator.swift
+++ src/xcode/ENA/ENA/Source/Scenes/QRScanner/QRScannerCoordinator.swift
@@ -195,7 +195,15 @@ class QRScannerCoordinator {
 		case let .traceLocation(traceLocation):
 			showScannedCheckin(traceLocation)
 		case let .ticketValidation(ticketValidationInitializationData):
+			#if DEBUG
+			if isUITesting {
+				showMockScannedTicketValidation(ticketValidationInitializationData)
+			} else {
+				evaluateScannedTicketValidation(ticketValidationInitializationData)
+			}
+			#else
 			evaluateScannedTicketValidation(ticketValidationInitializationData)
+			#endif
 		}
 	}
 
@@ -364,26 +372,7 @@ class QRScannerCoordinator {
 		_ initializationData: TicketValidationInitializationData
 	) {
 		showActivityIndicator()
-		
-		#if !DEBUG
 		let ticketValidation = TicketValidation(with: initializationData)
-		#else
-		if isUITesting {
-			let ticketValidation = MockTicketValidation(with: initializationData)
-			
-			ticketValidation.firstConsentResult = .success(.fake(fnt: "SCHNEIDER", gnt: "ANDREA", dob: "1989-12-12", type: ["v", "r", "tp", "tr"]))
-			
-			if LaunchArguments.ticketValidation.result.isFailed.boolValue {
-				ticketValidation.validationResult = .success(.fake(result: .failed, results: [.fake(identifier: "TR-002", result: .failed, type: "", details: "Ein Testzertifikat muss von einem zertifizierten Testzentrum ausgestellt werden.")]))
-			} else if LaunchArguments.ticketValidation.result.isOpen.boolValue {
-				ticketValidation.validationResult = .success(.fake(result: .open, results: [.fake(identifier: "TR-002", result: .open, type: "", details: "Ein Antigentest ist maximal 48h gültig.")]))
-			} else {
-				// set the default to passed
-				ticketValidation.validationResult = .success(.fake())
-			}
-		}
-		let ticketValidation = TicketValidation(with: initializationData)
-		#endif
 
 		ticketValidation.initialize { [weak self] result in
 			DispatchQueue.main.async {
@@ -624,6 +613,35 @@ class QRScannerCoordinator {
 		}
 		animator.startAnimation()
 	}
+
+	#if DEBUG
+	private func showMockScannedTicketValidation(_ initializationData: TicketValidationInitializationData) {
+		let ticketValidation = MockTicketValidation(with: initializationData)
+		ticketValidation.firstConsentResult = .success(.fake(fnt: "SCHNEIDER", gnt: "ANDREA", dob: "1989-12-12", type: ["v", "r", "tp", "tr"]))
+		
+		if LaunchArguments.ticketValidation.result.isFailed.boolValue {
+			ticketValidation.validationResult = .success(.fake(result: .failed, results: [.fake(identifier: "TR-002", result: .failed, type: "", details: "Ein Testzertifikat muss von einem zertifizierten Testzentrum ausgestellt werden.")]))
+		} else if LaunchArguments.ticketValidation.result.isOpen.boolValue {
+			ticketValidation.validationResult = .success(.fake(result: .open, results: [.fake(identifier: "TR-002", result: .open, type: "", details: "Ein Antigentest ist maximal 48h gültig.")]))
+		} else {
+			// set the default to passed
+			ticketValidation.validationResult = .success(.fake())
+		}
+
+		ticketValidation.initialize { [weak self] result in
+			DispatchQueue.main.async {
+				self?.hideActivityIndicator()
+
+				switch result {
+				case .success:
+					self?.showScannedTicketValidation(ticketValidation)
+				case .failure(let error):
+					self?.showErrorAlert(error: error)
+				}
+			}
+		}
+	}
+	#endif
 
 	// MARK: Helpers
 
