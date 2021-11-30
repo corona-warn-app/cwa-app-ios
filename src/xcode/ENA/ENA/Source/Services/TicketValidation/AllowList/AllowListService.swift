982e8690c7269bab26d3e17a88d69162cddcbc4a
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-11-30 14:27:49 +0100

    Fetch allowList

--- src/xcode/ENA/ENA/Source/Services/TicketValidation/AllowList/AllowListService.swift
+++ src/xcode/ENA/ENA/Source/Services/TicketValidation/AllowList/AllowListService.swift
@@ -3,6 +3,7 @@
 //
 
 import Foundation
+import ENASecurity
 
 final class AllowListService {
 	
@@ -17,4 +18,32 @@ final class AllowListService {
 
 	private let restServiceProvider: RestServiceProviding
 	private let store: Store
+	
+	// MARK: - Internal
+
+	func fetchAllowList() {
+		let resource = AllowListResource()
+		restServiceProvider.load(resource) { result in
+			switch result {
+			case .success(let allowList):
+				let serviceProviderAllowlist = allowList.serviceProviders.map({
+					$0.serviceIdentityHash
+				})
+				let validationServiceAllowlist = allowList.certificates.map({
+					ValidationServiceAllowlistEntry(
+						serviceProvider: $0.serviceProvider,
+						hostname: $0.hostname,
+						fingerprint256: $0.fingerprint256.base64EncodedString()
+					)
+				})
+				self.store.ticketValidationAllowList = TicketValidationAllowList(
+					validationServiceAllowList: validationServiceAllowlist,
+					serviceProviderAllowList: serviceProviderAllowlist
+				)
+				
+			case .failure(let error):
+				Log.debug(error.localizedDescription)
+			}
+		}
+	}
 }
