b67f66f978e8b5c43f94815fd09eee8f83a858f0
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-12-02 16:42:33 +0100

    add rest service error to the allow list

--- src/xcode/ENA/ENA/Source/Services/TicketValidation/AllowList/AllowListError.swift
+++ src/xcode/ENA/ENA/Source/Services/TicketValidation/AllowList/AllowListError.swift
@@ -6,4 +6,5 @@ enum AllowListError: Error {
 	case CERT_PIN_HOST_MISMATCH
 	case CERT_PIN_MISMATCH
 	case SP_ALLOWLIST_NO_MATCH
+	case REST_SERVICE_ERROR(ServiceError<AllowListResource.CustomError>)
 }
