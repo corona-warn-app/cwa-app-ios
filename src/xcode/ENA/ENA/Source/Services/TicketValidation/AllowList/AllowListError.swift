c9cccc85eff2fa57afe5de2dd76f571bca0d7171
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-12-03 13:55:30 +0100

    add localized descriptions

--- src/xcode/ENA/ENA/Source/Services/TicketValidation/AllowList/AllowListError.swift
+++ src/xcode/ENA/ENA/Source/Services/TicketValidation/AllowList/AllowListError.swift
@@ -2,9 +2,19 @@
 // 🦠 Corona-Warn-App
 //
 
-enum AllowListError: Error {
+enum AllowListError: LocalizedError {
 	case CERT_PIN_HOST_MISMATCH
-	case CERT_PIN_MISMATCH
 	case SP_ALLOWLIST_NO_MATCH
 	case REST_SERVICE_ERROR(ServiceError<AllowListResource.CustomError>)
+	
+	var localizedDescription: String {
+		switch self {
+		case .CERT_PIN_HOST_MISMATCH:
+			return "CERT_PIN_HOST_MISMATCH"
+		case .SP_ALLOWLIST_NO_MATCH:
+			return "SP_ALLOWLIST_NO_MATCH"
+		case .REST_SERVICE_ERROR(let serviceError):
+			return "REST_SERVICE_ERROR \(serviceError.localizedDescription)"
+		}
+	}
 }
