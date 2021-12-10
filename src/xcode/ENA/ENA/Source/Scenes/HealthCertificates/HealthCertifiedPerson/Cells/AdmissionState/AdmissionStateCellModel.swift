7698bb7706ab58934b44488d095db9c4af925655
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-12-10 14:34:45 +0100

    Revert "Revert "add AdmissionStateCellModel""
    This reverts commit 4daa9319dd00fc6329c4a7f7dade988de73f5f47.

---
+++ src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/HealthCertifiedPerson/Cells/AdmissionState/AdmissionStateCellModel.swift
@@ -0,0 +1,62 @@
+////
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+import OpenCombine
+import UIKit.UIFont
+
+final class AdmissionStateCellModel {
+
+	// MARK: - Init
+
+	init(
+		healthCertifiedPerson: HealthCertifiedPerson
+	) {
+		self.healthCertifiedPerson = healthCertifiedPerson
+	}
+
+	// MARK: - Internal
+
+	let title = AppStrings.HealthCertificate.Person.AdmissionState.title
+	var subtitle: String? {
+		return healthCertifiedPerson.admissionState.subtitle
+	}
+
+	var description: String? {
+		return healthCertifiedPerson.admissionState.description
+	}
+
+	var faqLink: NSAttributedString? {
+		
+		let text = String(
+			format: AppStrings.HealthCertificate.Person.AdmissionState.faq,
+			AppStrings.HealthCertificate.Person.AdmissionState.faqPlaceHolder
+		)
+
+		let textAttributes: [NSAttributedString.Key: Any] = [
+			.font: UIFont.preferredFont(forTextStyle: ENAFont.body.textStyle)
+				.scaledFont(
+					size: ENAFont.body.fontSize,
+					weight: ENAFont.body.fontWeight
+				),
+			.foregroundColor: UIColor.enaColor(for: .textPrimary1)
+		]
+		let attributedString = NSMutableAttributedString(
+			string: text,
+			attributes: textAttributes
+		)
+
+		attributedString.mark(
+			AppStrings.HealthCertificate.Person.AdmissionState.faqPlaceHolder,
+			with: AppStrings.Links.healthCertificateBoosterFAQ
+		)
+
+		return attributedString
+	}
+
+	// MARK: - Private
+
+	let healthCertifiedPerson: HealthCertifiedPerson
+
+}
