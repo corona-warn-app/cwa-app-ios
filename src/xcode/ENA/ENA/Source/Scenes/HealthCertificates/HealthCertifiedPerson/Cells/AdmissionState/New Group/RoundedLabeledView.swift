61b0813f34e88f5e5e24c92679212998d0afe1d5
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-12-14 15:28:36 +0100

    Configure title and add gradient

--- src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/HealthCertifiedPerson/Cells/AdmissionState/New Group/RoundedLabeledView.swift
+++ src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/HealthCertifiedPerson/Cells/AdmissionState/New Group/RoundedLabeledView.swift
@@ -5,6 +5,38 @@
 import UIKit
 
 class RoundedLabeledView: UIView {
+		
+	// MARK: - Overrides
+
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		
+		roundedView.layer.cornerRadius = roundedView.bounds.height / 2
+	}
+
+	// MARK: - Internal
+
+	func configure(title: String?) {
+		titleLabel.text = title
+
+		setupGradient()
+	}
+	
+	// MARK: - Private
+	
+	private func setupGradient() {
+		let gradientView = GradientView(type: .darkBlue(withStars: false))
+		gradientView.translatesAutoresizingMaskIntoConstraints = false
+		roundedView.insertSubview(gradientView, belowSubview: titleLabel)
+		NSLayoutConstraint.activate(
+			[
+				gradientView.leadingAnchor.constraint(equalTo: roundedView.leadingAnchor),
+				gradientView.topAnchor.constraint(equalTo: roundedView.topAnchor),
+				gradientView.trailingAnchor.constraint(equalTo: roundedView.trailingAnchor),
+				gradientView.bottomAnchor.constraint(equalTo: roundedView.bottomAnchor)
+			]
+		)
+	}
 	
 	@IBOutlet private weak var roundedView: UIView!
 	@IBOutlet private weak var titleLabel: ENALabel!
