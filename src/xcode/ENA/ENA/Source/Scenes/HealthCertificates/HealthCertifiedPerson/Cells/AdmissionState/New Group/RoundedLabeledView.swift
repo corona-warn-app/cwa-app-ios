55ee8b8d813b355b9e30f95834a7ca88d8fc1e60
Omar Ahmed <omarabdelaziz125@gmail.com>
2021-12-14 15:27:22 +0100

    Create the RoundedLabeledView

---
+++ src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/HealthCertifiedPerson/Cells/AdmissionState/New Group/RoundedLabeledView.swift
@@ -0,0 +1,11 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import UIKit
+
+class RoundedLabeledView: UIView {
+	
+	@IBOutlet private weak var roundedView: UIView!
+	@IBOutlet private weak var titleLabel: ENALabel!
+}
