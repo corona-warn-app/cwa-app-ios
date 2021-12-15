74bf5c04cb76ad27cf6b4e0266f899f23a183b77
Pascal Brause <pascal@brause.haus>
2021-12-15 17:00:03 +0100

    add more tests

--- src/xcode/ENA/ENA/Source/HTTPClient/Service/__tests__/CachedRestServiceTests.swift
+++ src/xcode/ENA/ENA/Source/HTTPClient/Service/__tests__/CachedRestServiceTests.swift
@@ -46,7 +46,7 @@ class CachedRestServiceTests: XCTestCase {
 		waitForExpectations(timeout: .short)
 	}
 
-	func test_ResonseIsLoadedFromCache() throws {
+	func test_CachedValueIsLoaded() throws {
 		let eTag = "DummyDataETag"
 		let dummyModel = DummyResourceModel(
 			dummyValue: "SomeValue"
@@ -119,6 +119,154 @@ class CachedRestServiceTests: XCTestCase {
 			}
 
 			XCTAssertEqual(responseModel, dummyModel)
+			loadExpectation.fulfill()
+		}
+
+		waitForExpectations(timeout: .short)
+	}
+	
+	func test_CachedValueIsLoadedButDefaultValueNot() throws {
+		let eTag = "DummyDataETag"
+		let cachedDummyModel = DummyResourceModel(
+			dummyValue: "Goofy"
+		)
+		let defaultDummyModel = DummyResourceModel(
+			dummyValue: "Pluto"
+		)
+		
+		let cachedDummyData = try JSONEncoder().encode( cachedDummyModel )
+		let locator: Locator = .fake()
+
+		// Store the dummy data in the cache.
+		let cache = KeyValueCacheFake()
+		cache[locator.hashValue] = CacheData(data: cachedDummyData, eTag: eTag, date: Date())
+
+		// Return nil and http code 304. In this case the service should load the reault from the cache.
+		let stack = MockNetworkStack(
+			httpStatus: 304,
+			headerFields: [
+				"ETag": eTag
+			],
+			responseData: nil
+		)
+
+		let cachedService = CachedRestService(
+			session: stack.urlSession,
+			cache: cache
+		)
+
+		let resource = ResourceFake(locator: locator, defaultModel: defaultDummyModel)
+		let loadExpectation = expectation(description: "Load completion should be called.")
+
+		cachedService.load(resource) { result in
+			// Check if the value returned is the same like the one stored in the cache before.
+
+			guard case let .success(responseModel) = result else {
+				XCTFail("Success expected")
+				return
+			}
+
+			XCTAssertEqual(responseModel, cachedDummyModel)
+			XCTAssertNotEqual(responseModel, defaultDummyModel)
+
+			loadExpectation.fulfill()
+		}
+
+		waitForExpectations(timeout: .short)
+	}
+	
+	func test_DefaultValueIsLoadedButCachedValueNot() throws {
+		let eTag = "DummyDataETag"
+		let cachedDummyModel = DummyResourceModel(
+			dummyValue: "Donald"
+		)
+		let defaultDummyModel = DummyResourceModel(
+			dummyValue: "Minnie"
+		)
+		
+		let cachedDummyData = try JSONEncoder().encode( cachedDummyModel )
+		let locator: Locator = .fake()
+
+		// Store the dummy data in the cache.
+		let cache = KeyValueCacheFake()
+		cache[locator.hashValue] = CacheData(data: cachedDummyData, eTag: eTag, date: Date())
+
+		// Return nil and http code 304. In this case the service should load the reault from the cache.
+		let stack = MockNetworkStack(
+			httpStatus: 500,
+			headerFields: [
+				"ETag": eTag
+			],
+			responseData: nil
+		)
+
+		let cachedService = CachedRestService(
+			session: stack.urlSession,
+			cache: cache
+		)
+
+		let resource = ResourceFake(locator: locator, defaultModel: defaultDummyModel)
+		let loadExpectation = expectation(description: "Load completion should be called.")
+
+		cachedService.load(resource) { result in
+			// Check if the value returned is the same like the one stored in the cache before.
+
+			guard case let .success(responseModel) = result else {
+				XCTFail("Success expected")
+				return
+			}
+
+			XCTAssertNotEqual(responseModel, cachedDummyModel)
+			XCTAssertEqual(responseModel, defaultDummyModel)
+			loadExpectation.fulfill()
+		}
+
+		waitForExpectations(timeout: .short)
+	}
+	
+	func test_CachedValueIsLoadedWithDefinedCacheUseCases() throws {
+		let eTag = "DummyDataETag"
+		let cachedDummyModel = DummyResourceModel(
+			dummyValue: "Donald"
+		)
+		let defaultDummyModel = DummyResourceModel(
+			dummyValue: "Minnie"
+		)
+		
+		let cachedDummyData = try JSONEncoder().encode( cachedDummyModel )
+		let locator: Locator = .fake()
+
+		// Store the dummy data in the cache.
+		let cache = KeyValueCacheFake()
+		cache[locator.hashValue] = CacheData(data: cachedDummyData, eTag: eTag, date: Date())
+
+		// Return nil and http code 304. In this case the service should load the reault from the cache.
+		let stack = MockNetworkStack(
+			httpStatus: 500,
+			headerFields: [
+				"ETag": eTag
+			],
+			responseData: nil
+		)
+
+		let cachedService = CachedRestService(
+			session: stack.urlSession,
+			cache: cache
+		)
+
+		let resource = ResourceFake(locator: locator, defaultModel: defaultDummyModel)
+		let loadExpectation = expectation(description: "Load completion should be called.")
+
+		cachedService.load(resource) { result in
+			// Check if the value returned is the same like the one stored in the cache before.
+
+			guard case let .success(responseModel) = result else {
+				XCTFail("Success expected")
+				return
+			}
+
+			XCTAssertNotEqual(responseModel, cachedDummyModel)
+			XCTAssertEqual(responseModel, defaultDummyModel)
 			loadExpectation.fulfill()
 		}
 
