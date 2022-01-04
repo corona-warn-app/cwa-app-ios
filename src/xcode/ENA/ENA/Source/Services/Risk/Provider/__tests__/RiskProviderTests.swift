005fb6719af1bb6b0fc93798e205ca3651860b5d
Naveed Khalid <nkhalid.here@gmail.com>
2022-01-04 15:50:42 +0100

    adding test for manual risk

--- src/xcode/ENA/ENA/Source/Services/Risk/Provider/__tests__/RiskProviderTests.swift
+++ src/xcode/ENA/ENA/Source/Services/Risk/Provider/__tests__/RiskProviderTests.swift
@@ -654,6 +654,88 @@ final class RiskProviderTests: CWATestCase {
 		
 		let didFailCalculateRiskExpectation = expectation(description: "expect didFailCalculateRisk not to be called")
 		
+		let expectedActivityStates: [RiskProviderActivityState] = [.riskRequested, .downloading, .detecting, .idle]
+		let didChangeActivityStateExpectation = expectation(description: "expect didChangeActivityState to be called")
+		didChangeActivityStateExpectation.expectedFulfillmentCount = expectedActivityStates.count
+		
+		consumer.didCalculateRisk = { _ in
+			didCalculateRiskExpectation.fulfill()
+		}
+		
+		consumer.didFailCalculateRisk = { _ in
+			didFailCalculateRiskExpectation.fulfill()
+		}
+		
+		var receivedActivityStates = [RiskProviderActivityState]()
+		consumer.didChangeActivityState = {
+			receivedActivityStates.append($0)
+			didChangeActivityStateExpectation.fulfill()
+		}
+		
+		sut.observeRisk(consumer)
+		sut.requestRisk(userInitiated: false)
+		
+		waitForExpectations(timeout: .medium)
+		
+		XCTAssertEqual(receivedActivityStates, expectedActivityStates)
+	}
+	
+	func testThatDetectionFails_RiskManuallyRequested() throws {
+		let duration = DateComponents(day: 1)
+		
+		let client = ClientMock()
+		let store = MockTestStore()
+		store.enfRiskCalculationResult = nil
+		store.positiveTestResultWasShown = false
+		store.lastSuccessfulSubmitDiagnosisKeyTimestamp = nil
+		
+		let config = RiskProvidingConfiguration(
+			exposureDetectionValidityDuration: duration,
+			exposureDetectionInterval: duration
+		)
+		
+		let exposureDetectionDelegateStub = ExposureDetectionDelegateStub(result: .failure(DummyError()))
+		let appConfig = CachedAppConfigurationMock(with: SAP_Internal_V2_ApplicationConfigurationIOS())
+		
+		let sut = RiskProvider(
+			configuration: config,
+			store: store,
+			appConfigurationProvider: appConfig,
+			exposureManagerState: .init(authorized: true, enabled: true, status: .active),
+			enfRiskCalculation: ENFRiskCalculationFake(),
+			checkinRiskCalculation: CheckinRiskCalculationFake(),
+			keyPackageDownload: makeKeyPackageDownloadMock(with: store),
+			traceWarningPackageDownload: makeTraceWarningPackageDownloadMock(with: store, appConfig: appConfig),
+			exposureDetectionExecutor: exposureDetectionDelegateStub,
+			coronaTestService: CoronaTestService(
+				client: client,
+				store: store,
+				eventStore: MockEventStore(),
+				diaryStore: MockDiaryStore(),
+				appConfiguration: appConfig,
+				healthCertificateService: HealthCertificateService(
+					store: store,
+					dccSignatureVerifier: DCCSignatureVerifyingStub(),
+					dscListProvider: MockDSCListProvider(),
+					client: client,
+					appConfiguration: appConfig,
+					boosterNotificationsService: BoosterNotificationsService(
+						rulesDownloadService: RulesDownloadService(store: store, client: client)
+					),
+					recycleBin: .fake()
+				),
+				recycleBin: .fake(),
+				badgeWrapper: .fake()
+			)
+		)
+		
+		let consumer = RiskConsumer()
+		
+		let didCalculateRiskExpectation = expectation(description: "expect didCalculateRisk to be called")
+		didCalculateRiskExpectation.isInverted = true
+		
+		let didFailCalculateRiskExpectation = expectation(description: "expect didFailCalculateRisk not to be called")
+		
 		let expectedActivityStates: [RiskProviderActivityState] = [.riskManuallyRequested, .downloading, .detecting, .idle]
 		let didChangeActivityStateExpectation = expectation(description: "expect didChangeActivityState to be called")
 		didChangeActivityStateExpectation.expectedFulfillmentCount = expectedActivityStates.count
@@ -679,7 +761,7 @@ final class RiskProviderTests: CWATestCase {
 		
 		XCTAssertEqual(receivedActivityStates, expectedActivityStates)
 	}
-	
+
 	func testThatDetectionIsNotRequestedIfPositiveTestResultWasShown() throws {
 		let duration = DateComponents(day: 1)
 		
