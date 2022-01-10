f2ff2eb72a22b9a04e1b11363b0601c1d90bb751
Marcus Scherer <marcus@skunkbrothers.de>
2022-01-10 13:30:47 +0100

    Move fake into extension

--- src/xcode/ENA/ENA/Source/HTTPClient/Service/Models/Receive/TestResultModel.swift
+++ src/xcode/ENA/ENA/Source/HTTPClient/Service/Models/Receive/TestResultModel.swift
@@ -5,16 +5,4 @@ struct TestResultModel: Codable {
 	let testResult: Int
 	let sc: Int?
 	let labId: String?
-
-	static func fake(
-		testResult: Int = 0,
-		sc: Int? = nil,
-		labId: String? = nil
-	) -> TestResultModel {
-		TestResultModel(
-			testResult: testResult,
-			sc: sc,
-			labId: labId
-		)
-	}
 }
