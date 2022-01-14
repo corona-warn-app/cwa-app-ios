ef8f6ec8ca026406e7f65bb48eef427bbe955136
Pascal Brause <pascal@brause.haus>
2022-01-14 12:06:03 +0100

    add server error

--- src/xcode/ENA/ENA/Source/Services/HealthCertificateValidationOnboardedCountries/__tests__/HealthCertificateValidationOnboardedCountriesProviderTests.swift
+++ src/xcode/ENA/ENA/Source/Services/HealthCertificateValidationOnboardedCountries/__tests__/HealthCertificateValidationOnboardedCountriesProviderTests.swift
@@ -42,60 +42,32 @@ class HealthCertificateValidationOnboardedCountriesProviderTests: XCTestCase {
 	
 	
 	// MARK: - Failures
-	/*
-
-	func testGIVEN_ValidationService_GetOnboardedCountries_WHEN_NoResponse_THEN_ONBOARDED_COUNTRIES_NO_NETWORKIsReturned() {
+	
+	func testGIVEN_ValidationService_GetOnboardedCountries_WHEN_SomeError_THEN_Error() throws {
 		// GIVEN
-		let client = ClientMock()
-		client.onValidationOnboardedCountries = { _, completion in
-			completion(.failure(.noResponse))
-		}
-		let store = MockTestStore()
-		let provider = HealthCertificateValidationOnboardedCountriesProvider(
-			store: store,
-			client: client,
-			signatureVerifier: MockVerifier()
+		
+		let expectation = self.expectation(description: "Test should success with new countries")
+		let expectedCountries = try ValidationOnboardedCountriesModel(decodeCBOR: HealthCertificateToolkit.onboardedCountriesCBORDataFake)
+		let expectedResource = LoadResource(
+			result: .success(expectedCountries),
+			willLoadResource: { _ in }
 		)
-		let expectation = self.expectation(description: "Test should fail ONBOARDED_COUNTRIES_NO_NETWORK")
-		var receivedError: ValidationOnboardedCountriesError?
-
-		// WHEN
-		provider.onboardedCountries(completion: { result in
+		
+		let restServiceProvider = RestServiceProviderStub(loadResources: [expectedResource])
+		
+		restServiceProvider.load(ValidationOnboardedCountriesResource()) { result in
 			switch result {
-			case .success:
-				XCTFail("Test should not succeed.")
-			case let .failure(error):
-				receivedError = error
-				expectation.fulfill()
+				
+			case .success(let model):
+				let countries = model.countries
+				XCTAssertEqual(countries, expectedCountries.countries)
+			case .failure(let error):
+				XCTFail("Test should not fail with error: \(error)")
 			}
-		})
-
+			expectation.fulfill()
+		}
+		
 		// THEN
 		waitForExpectations(timeout: .short)
-		XCTAssertEqual(receivedError, .ONBOARDED_COUNTRIES_NO_NETWORK)
 	}
-
-	private lazy var dummyOnboardedCountriesResponse: PackageDownloadResponse = {
-		let fakeData = onboardedCountriesCBORDataFake
-				
-		let package = SAPDownloadedPackage(
-			keysBin: fakeData,
-			signature: Data()
-		)
-		let response = PackageDownloadResponse(
-			package: package,
-			etag: "FakeEtag"
-		)
-		return response
-	}()
-	
-	private var onboardedCountriesFake: [Country] {
-		guard let countryDE = Country(countryCode: "DE"),
-			  let countryFR = Country(countryCode: "FR") else {
-			XCTFail("Could not create countries")
-			return []
-		}
-		return [countryDE, countryFR]
-	}
-	 */
 }
