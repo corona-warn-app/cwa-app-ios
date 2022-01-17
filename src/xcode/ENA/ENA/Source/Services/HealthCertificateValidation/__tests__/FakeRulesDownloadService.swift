d0f5a524eba642d95271194a218796e450673708
Kai Teuber <Kai.Teuber@app-developer.club>
2022-01-17 16:09:23 +0100

    added FakeRulesDownloadService.swift

--- src/xcode/ENA/ENA/Source/Services/HealthCertificateValidation/__tests__/FakeRulesDownloadService.swift
+++ src/xcode/ENA/ENA/Source/Services/HealthCertificateValidation/__tests__/FakeRulesDownloadService.swift
@@ -2,4 +2,30 @@
 // 🦠 Corona-Warn-App
 //
 
-import Foundation
+@testable import ENA
+import class CertLogic.Rule
+
+struct FakeRulesDownloadService: RulesDownloadServiceProviding {
+
+	// MARK: - Init
+
+	init(
+		_ result: Result<[Rule], HealthCertificateValidationError>?
+	) {
+		self.result = result
+	}
+
+	// MARK: - Protocol RulesDownloadServiceProviding
+
+	func downloadRules(
+		ruleType: HealthCertificateValidationRuleType,
+		completion: @escaping (Result<[Rule], HealthCertificateValidationError>) -> Void
+	) {
+		completion(result ?? .failure(.RULES_VALIDATION_ERROR(.JSON_VALIDATION_RULE_SCHEMA_NOTFOUND)))
+	}
+
+	// MARK: - Internal
+
+	var result: Result<[Rule], HealthCertificateValidationError>?
+
+}
