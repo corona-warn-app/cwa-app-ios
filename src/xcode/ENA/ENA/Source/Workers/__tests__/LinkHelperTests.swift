fcf57073ee1bfc8e5058461280d5904777e19cbc
Naveed Khalid <nkhalid.here@gmail.com>
2022-01-20 15:25:33 +0100

    adding some tests

--- src/xcode/ENA/ENA/Source/Workers/__tests__/LinkHelperTests.swift
+++ src/xcode/ENA/ENA/Source/Workers/__tests__/LinkHelperTests.swift
@@ -2,4 +2,32 @@
 // 🦠 Corona-Warn-App
 //
 
-import Foundation
+import XCTest
+@testable import ENA
+
+final class LinkHelperTests: CWATestCase {
+	
+	func testFAQAnchorDE() throws {
+		let url = LinkHelper.urlString(suffix: "test", type: .faq)
+
+		XCTAssertEqual(NSLocale.current.languageCode, "de")
+		XCTAssertEqual(url, "https://www.coronawarn.app/de/faq/#test")
+	}
+	
+	func testFAQAnchorEN() throws {
+		if NSLocale.current.languageCode == "en" {
+			let url = LinkHelper.urlString(suffix: "test", type: .faq)
+
+			XCTAssertEqual(url, "https://www.coronawarn.app/en/faq/#test")
+		}
+	}
+	
+	func testFAQAnchorOtherLanguages() throws {
+		let supportedLocale = ["tr", "ro", "pl"]
+		if supportedLocale.contains(NSLocale.current.languageCode ?? "") {
+			let url = LinkHelper.urlString(suffix: "test", type: .faq)
+
+			XCTAssertEqual(url, "https://www.coronawarn.app/en/faq/#test")
+		}
+	}
+}
