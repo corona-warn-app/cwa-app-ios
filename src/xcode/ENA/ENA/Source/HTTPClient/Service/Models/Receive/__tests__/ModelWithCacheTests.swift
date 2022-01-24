6840e74c552ef3d69b12fc35958d22995db5d5d3
Pascal Brause <pascal@brause.haus>
2022-01-24 15:01:19 +0100

    renaming

--- src/xcode/ENA/ENA/Source/HTTPClient/Service/Models/Receive/__tests__/ModelWithCacheTests.swift
+++ src/xcode/ENA/ENA/Source/HTTPClient/Service/Models/Receive/__tests__/ModelWithCacheTests.swift
@@ -23,7 +23,7 @@ class ModelWithCacheTests: CWATestCase {
 			responseData: archiveData
 		)
 		
-		let resource = ResourceCachingCBORFake()
+		let resource = CBORReceiveTestResource()
 		resource.receiveResource = CBORReceiveResource(
 			signatureVerifier: MockVerifier()
 		)
@@ -64,7 +64,51 @@ class ModelWithCacheTests: CWATestCase {
 			responseData: archiveData
 		)
 		
-		let resource = ResourceCachingCBORFake()
+		let resource = CBORReceiveTestResource()
+		resource.receiveResource = CBORReceiveResource(
+			signatureVerifier: MockVerifier()
+		)
+		
+		let cache = KeyValueCacheFake()
+		cache[resource.locator.hashValue] = CacheData(data: archiveData, eTag: "Some eTag", date: Date())
+		
+		let serviceProvider = RestServiceProvider(
+			session: stack.urlSession,
+			cache: cache
+		)
+		
+		// WHEN
+		serviceProvider.load(resource) { result in
+			switch result {
+				
+			case let .success(cachingModel):
+				// THEN
+				XCTAssertTrue(cachingModel.isCached)
+			case let .failure(error):
+				XCTFail("Test should success but failed with error: \(error)")
+			}
+			expectation.fulfill()
+		}
+		waitForExpectations(timeout: .short)
+	}
+	
+	func test_GIVEN_ModelWithCache_WHEN_NonCachingRestServiceIsUsed_THEN_IsCachedIsFalse() throws {
+		// GIVEN
+		let expectation = expectation(description: "Expect that we got a completion")
+		
+		let archiveData = try createSomeCBORArchive()
+		
+		let stack = MockNetworkStack(
+			httpStatus: 200,
+			headerFields: [
+				"ETag": "Some eTag"
+			],
+			responseData: archiveData
+		)
+		
+		let resource = CBORReceiveTestResource(
+			type: .default
+		)
 		resource.receiveResource = CBORReceiveResource(
 			signatureVerifier: MockVerifier()
 		)
@@ -92,17 +136,6 @@ class ModelWithCacheTests: CWATestCase {
 		waitForExpectations(timeout: .short)
 	}
 	
-	func test_GIVEN_ModelWithCache_WHEN_NonCachingRestServiceIsUsed_THEN_IsCachedIsFalse() {
-	
-		// GIVEN
-		
-		// WHEN
-		
-		// THEN
-		
-		
-	}
-	
 	private func createSomeCBORArchive() throws -> Data {
 		return try XCTUnwrap(Archive.createArchiveData(
 			accessMode: .create,
@@ -112,37 +145,44 @@ class ModelWithCacheTests: CWATestCase {
 	
 }
 
-private struct TestCachingModel: CBORDecoding {
-		
-	private init(property: String ) {
-		self.property = property
+private struct CBORDecodingTestModel: CBORDecoding {
+	
+	//MARK: - Protocol CBORDecoding
+	
+	static func decode(_ data: Data) -> Result<CBORDecodingTestModel, ModelDecodingError> {
+		return Result.success(CBORDecodingTestModel(property: "Decoded Value"))
 	}
+	
+	// MARK: - Internal
 
 	let property: String
 	
-	static func decode(_ data: Data) -> Result<TestCachingModel, ModelDecodingError> {
-		return Result.success(TestCachingModel(property: "Decoded Value"))
+	// MARK: - Private
+	
+	private init(property: String ) {
+		self.property = property
 	}
 }
 
-private class ResourceCachingCBORFake: Resource {
+private class CBORReceiveTestResource: Resource {
 	
 	init(
 		locator: Locator = .fake(),
-		receiveResource: CBORReceiveResource<ModelWithCache<TestCachingModel>> = CBORReceiveResource<ModelWithCache<TestCachingModel>>()
+		type: ServiceType = .caching(),
+		receiveResource: CBORReceiveResource<ModelWithCache<CBORDecodingTestModel>> = CBORReceiveResource<ModelWithCache<CBORDecodingTestModel>>()
 	) {
 		self.locator = locator
-		self.type = .caching()
+		self.type = type
 		self.sendResource = EmptySendResource()
 		self.receiveResource = receiveResource
 	}
 	
 	typealias Send = EmptySendResource
-	typealias Receive = CBORReceiveResource<ModelWithCache<TestCachingModel>>
+	typealias Receive = CBORReceiveResource<ModelWithCache<CBORDecodingTestModel>>
 	typealias CustomError = Error
 	
 	let locator: Locator
 	let type: ServiceType
 	let sendResource: EmptySendResource
-	var receiveResource: CBORReceiveResource<ModelWithCache<TestCachingModel>>
+	var receiveResource: CBORReceiveResource<ModelWithCache<CBORDecodingTestModel>>
 }
