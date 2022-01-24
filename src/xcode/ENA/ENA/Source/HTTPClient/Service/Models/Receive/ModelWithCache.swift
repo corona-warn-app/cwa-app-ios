04ccbe9f557425fd5331378580db54978110c4e2
Pascal Brause <pascal@brause.haus>
2022-01-24 14:15:13 +0100

    extend new model

--- src/xcode/ENA/ENA/Source/HTTPClient/Service/Models/Receive/ModelWithCache.swift
+++ src/xcode/ENA/ENA/Source/HTTPClient/Service/Models/Receive/ModelWithCache.swift
@@ -6,14 +6,36 @@
  Generel model to encapsulate the real model, which has to fullfill CBORDecoding. The main purpose of this model is to store a property if the real model returned from the service is from the cache or freshly fetched. Normally you cannot differentiate here.
  */
 struct ModelWithCache<M>: CBORDecoding where M: CBORDecoding {
+		
+	static func decode(_ data: Data) -> Result<ModelWithCache, ModelDecodingError> {
+		let result = M.decode(data)
+		switch result {
+			
+		case let .success(someModel):
+			// We need that cast for the compiler.
+			if let wrappedModel = someModel as? M {
+				return Result.success(ModelWithCache(model: wrappedModel, isCached: false))
+			} else {
+				return .failure(.CBOR_DECODING)
+			}
+
+		case let .failure(error):
+			return .failure(error)
+		}
+	}
 	
-	typealias Model = M
+	// MARK: - Internal
 	
 	let model: M
-	let isCached: Bool
+	var isCached: Bool
 	
-	static func decode(_ data: Data) -> Result<M, ModelDecodingError> {
-//		return M.decode(data)
-		return .failure(.CBOR_DECODING)
+	// MARK: - Private
+	
+	private init(
+		model: M,
+		isCached: Bool
+	) {
+		self.model = model
+		self.isCached = isCached
 	}
 }
