0d4cdfa481df1353b8a87d9d5a95399c7693187d
Pascal Brause <pascal@brause.haus>
2022-01-26 14:16:04 +0100

    simplifying

--- src/xcode/ENA/ENA/Source/HTTPClient/Resources/DCCRules/DCCRulesReceiveModel.swift
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/DCCRules/DCCRulesReceiveModel.swift
@@ -14,9 +14,9 @@ struct DCCRulesReceiveModel: CBORDecoding {
 	static func decode(_ data: Data) -> Result<DCCRulesReceiveModel, ModelDecodingError> {
 		switch ValidationRulesAccess().extractValidationRules(from: data) {
 		case .success(let rules):
-			return Result.success(DCCRulesReceiveModel(rules: rules))
+			return .success(DCCRulesReceiveModel(rules: rules))
 		case .failure(let error):
-			return Result.failure(.CBOR_DECODING_VALIDATION_RULES(error))
+			return .failure(.CBOR_DECODING_VALIDATION_RULES(error))
 		}
 	}
 
