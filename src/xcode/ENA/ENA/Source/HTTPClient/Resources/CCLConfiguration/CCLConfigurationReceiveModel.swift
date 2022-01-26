d7c43548b2315b62df28e84c2ba63f0db04398ec
Pascal Brause <pascal@brause.haus>
2022-01-26 14:17:59 +0100

    add some future things

--- src/xcode/ENA/ENA/Source/HTTPClient/Resources/CCLConfiguration/CCLConfigurationReceiveModel.swift
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/CCLConfiguration/CCLConfigurationReceiveModel.swift
@@ -9,19 +9,26 @@ struct CCLConfigurationReceiveModel: CBORDecoding {
 	
 	// MARK: - Protocol CBORDecoding
 	
-	static func decode(_ data: Data) -> Result<CCLConfiguration, ModelDecodingError> {
+	static func decode(_ data: Data) -> Result<Data, ModelDecodingError> {
+		/*
+		 switch CCLConfigurationAccess().extractConfiguration(from: data) {
+		 case .success(let configurationData):
+			return .success(CCLConfigurationReceiveModel(with: configurationData)
+		 case .failure(let error):
+			return .failure(.CBOR_DECODING_CLLCONFIGURATION(error))
+		 */
+		
 		// TODO call decode from HealthCertifiedToolkit
 		return .failure(.CBOR_DECODING)
 	}
 
 	// MARK: - Internal
+	
+	// Call only for init with fallback data or when decoding was successfull. Otherwise never call this explicit.
+	init(someVar: Data ) {
+		self.someVar = someVar
+	}
 
 	// TODO take real model
 	let someVar: Data
-	
-	// MARK: - Private
-	
-	private init(someVar: Data ) {
-		self.someVar = someVar
-	}
 }
