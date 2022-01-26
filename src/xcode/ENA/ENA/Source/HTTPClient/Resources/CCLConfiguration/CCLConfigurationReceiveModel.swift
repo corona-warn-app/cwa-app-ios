7f78f73d63e50f278d8902815847752575253cec
Pascal Brause <pascal@brause.haus>
2022-01-26 13:47:30 +0100

    add some TODOs

--- src/xcode/ENA/ENA/Source/HTTPClient/Resources/CCLConfiguration/CCLConfigurationReceiveModel.swift
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/CCLConfiguration/CCLConfigurationReceiveModel.swift
@@ -3,17 +3,20 @@
 //
 
 import Foundation
+import HealthCertificateToolkit
 
 struct CCLConfigurationReceiveModel: CBORDecoding {
 	
 	// MARK: - Protocol CBORDecoding
 	
 	static func decode(_ data: Data) -> Result<CCLConfiguration, ModelDecodingError> {
+		// TODO call decode from HealthCertifiedToolkit
 		return .failure(.CBOR_DECODING)
 	}
 
 	// MARK: - Internal
 
+	// TODO take real model
 	let someVar: Data
 	
 	// MARK: - Private
