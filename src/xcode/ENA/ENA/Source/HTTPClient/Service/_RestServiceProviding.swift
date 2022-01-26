5ed2c2e6e96bcc4403b054c5e2f5e550cedab1bf
Pascal Brause <pascal@brause.haus>
2022-01-26 16:47:58 +0100

    WIP: new API

--- src/xcode/ENA/ENA/Source/HTTPClient/Service/_RestServiceProviding.swift
+++ src/xcode/ENA/ENA/Source/HTTPClient/Service/_RestServiceProviding.swift
@@ -13,6 +13,11 @@ protocol RestServiceProviding {
 		_ resource: R,
 		_ completion: @escaping (Result<R.Receive.ReceiveModel, ServiceError<R.CustomError>>) -> Void
 	) where R: Resource
+	
+	func load<R>(
+		_ resource: R,
+		_ completion: @escaping (Result<(R.Receive.ReceiveModel, MetaData), ServiceError<R.CustomError>>) -> Void
+	) where R: Resource
 
 	// get ReceiveModel if it's available inside a cache
 	func cached<R>(
@@ -20,4 +25,9 @@ protocol RestServiceProviding {
 	) -> Result<R.Receive.ReceiveModel, ServiceError<R.CustomError>> where R: Resource
 
 	func update(_ evaluateTrust: EvaluateTrust)
+}
+
+struct MetaData {
+	var loadedFromCache: Bool
+	var headers: [AnyHashable: Any]
 }
