a3e85230fdccf0f6cb193f4ebec6181194fdf9ee
Pascal Brause <pascal@brause.haus>
2022-01-27 16:36:13 +0100

    fix small things

--- src/xcode/ENA/ENA/Source/HTTPClient/Resources/CCLConfiguration/CCLConfigurationReceiveModel.swift
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/CCLConfiguration/CCLConfigurationReceiveModel.swift
@@ -5,24 +5,24 @@
 import Foundation
 import HealthCertificateToolkit
 
-struct CCLConfigurationReceiveModel: CBORDecodable & MetaDataProviding {
+struct CCLConfigurationReceiveModel: CBORDecodable, MetaDataProviding {
 	
 	// MARK: - Protocol CBORDecoding
 	
 	static func make(with data: Data) -> Result<Data, ModelDecodingError> {
+		
 	
-//		switch HealthCertificateToolkit.DCCWalletCertificate ().extractConfiguration(from: data) {
+//		switch CCLConfigurationAccess().extractCCLConfiguration(from: data) {
 //		 case .success(let configurationData):
 //			return .success(CCLConfigurationReceiveModel(with: configurationData)
 //		 case .failure(let error):
 //			return .failure(.CBOR_DECODING_CLLCONFIGURATION(error))
-//		 
-		
+//
 		// TODO call decode from HealthCertifiedToolkit
 		return .failure(.CBOR_DECODING)
 	}
 	
-	// MARK: - Protocol CBORDecoding
+	// MARK: - Protocol MetaDataProviding
 	
 	var metaData: MetaData = MetaData()
 
