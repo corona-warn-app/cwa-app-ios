899746f808bb78513671f608a578a90073cc262e
Pascal Brause <pascal@brause.haus>
2022-01-28 13:02:12 +0100

    add succes 2

--- src/xcode/ENA/ENA/Source/HTTPClient/Resources/CCLConfiguration/__tests__/CCLConfigurationResourceTests.swift
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/CCLConfiguration/__tests__/CCLConfigurationResourceTests.swift
@@ -49,5 +49,53 @@ final class CCLConfigurationResourceTests: CWATestCase {
 		waitForExpectations(timeout: .medium)
 	}
 	
+	func testGIVEN_Resource_WHEN_Response_304_THEN_CCLConfigIsReturned() throws {
+		// GIVEN
+		let expectation = self.expectation(description: "completion handler succeeds")
+		let archiveDataCache = try XCTUnwrap(Archive.createArchiveData(
+			accessMode: .create,
+			cborData: HealthCertificateToolkit.CCLConfigurationCBORDataFake()
+		))
+		
+		let eTag = "DummyDataETag"
+		let stack = MockNetworkStack(
+			httpStatus: 304,
+			headerFields: ["eTag": eTag]
+		)
+
+		var cclConfigurationResource = CCLConfigurationResource()
+		cclConfigurationResource.receiveResource = CBORReceiveResource(signatureVerifier: MockVerifier())
+
+		let cache = KeyValueCacheFake()
+		cache[cclConfigurationResource.locator.hashValue] = CacheData(
+			data: archiveDataCache,
+			eTag: eTag,
+			serverDate: nil,
+			clientDate: Date()
+		)
+		
+		let restServiceProvider = RestServiceProvider(
+			session: stack.urlSession,
+			cache: cache
+		)
+		
+		// We should load cached configuration
+		// WHEN
+		restServiceProvider.load(cclConfigurationResource) { result in
+			switch result {
+			case let .success(model):
+				// CBOR rulesCBORDataFake contains 4 rules
+				XCTAssertEqual(model.cclConfigurations.count, 4)
+				XCTAssertTrue(model.metaData.loadedFromCache)
+			case let .failure(error):
+				XCTFail("Test should not fail with error: \(error)")
+			}
+			expectation.fulfill()
+		}
+
+		// THEN
+		waitForExpectations(timeout: .medium)
+	}
+	
 	
 }
