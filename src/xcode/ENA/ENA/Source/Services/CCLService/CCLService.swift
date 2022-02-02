706a93def923c58d0902fc6dcf93092ecf52e7ab
Nick Guendling <hello@nickguendling.com>
2022-02-02 15:30:58 +0100

    Revert "Pass in dictionary as parameters so they can be converted to JSON"
    This reverts commit 454ad91f9e2f78c689c496730a0f3b301587ecab.

--- src/xcode/ENA/ENA/Source/Services/CCLService/CCLService.swift
+++ src/xcode/ENA/ENA/Source/Services/CCLService/CCLService.swift
@@ -187,13 +187,3 @@ class CCLService: CCLServable {
 	}
 
 }
-
-extension Encodable {
-
-	var dict: [String: Any]? {
-		guard let data = try? JSONEncoder().encode(self) else { return nil }
-		guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else { return nil }
-		return json
-	}
-
-}
