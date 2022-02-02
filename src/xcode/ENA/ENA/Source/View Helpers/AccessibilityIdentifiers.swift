617722cbdc472748bf9bd2a70437c7b1ae6aad48
Kai Teuber <Kai.Teuber@app-developer.club>
2022-02-02 15:37:57 +0100

    removed title incl. accessibility identifiers

--- src/xcode/ENA/ENA/Source/View Helpers/AccessibilityIdentifiers.swift
+++ src/xcode/ENA/ENA/Source/View Helpers/AccessibilityIdentifiers.swift
@@ -312,9 +312,8 @@ enum AccessibilityIdentifiers {
 		static let furtherInfos_Title = "AppStrings.ExposureSubmissionResult.furtherInfos_Title"
 		static let warnOthersConsentGivenCell = "AppStrings.ExposureSubmissionResult.warnOthersConsentGiven"
 		static let warnOthersConsentNotGivenCell = "AppStrings.ExposureSubmissionResult.warnOthersConsentNotGiven"
-		
+
 		enum Antigen {
-			static let proofTitle = "AppStrings.ExposureSubmissionResult.Antigen.proofTitle"
 			static let proofDesc = "AppStrings.ExposureSubmissionResult.Antigen.proofDesc"
 		}
 	}
