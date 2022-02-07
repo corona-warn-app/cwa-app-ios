856cd01142aca65c827ce013160f424e6267a79e
Artur Friesen <mail@arturfriesen.de>
2022-02-07 16:52:50 +0100

    Load test cases from jsons. Fix same person logic.

--- src/xcode/ENA/ENA/Source/Services/HealthCertificate/Models/__tests__/HealthCertificate+BelongsToSamePersonTests.swift
+++ src/xcode/ENA/ENA/Source/Services/HealthCertificate/Models/__tests__/HealthCertificate+BelongsToSamePersonTests.swift
@@ -9,13 +9,22 @@ import HealthCertificateToolkit
 class HealthCertificate_BelongsToSamePersonTests: XCTestCase {
 
 	func test_BelongsToSamePerson() throws {
+		let bundle = Bundle(for: Name_ExtensionTests.self)
+		guard let url = bundle.url(forResource: "dcc-holder-comparison", withExtension: "json"),
+			  let data = FileManager.default.contents(atPath: url.path)
+			  else {
+				  XCTFail("Could not load json with testcases.")
+				  return
+		}
+		let testCases = try JSONDecoder().decode(TestCases.self, from: data).data
+		
 		for testCase in testCases {
 			let lhsCertificate = try HealthCertificate(
 				base45: try base45Fake(
 					from: DigitalCovidCertificate.fake(
 						name: Name.fake(
-							standardizedFamilyName: testCase.actHolderA.fnt,
-							standardizedGivenName: testCase.actHolderA.gnt
+							standardizedFamilyName: testCase.actHolderA.nam.fnt,
+							standardizedGivenName: testCase.actHolderA.nam.gnt
 						),
 						dateOfBirth: testCase.actHolderA.dob
 					)
@@ -27,8 +36,8 @@ class HealthCertificate_BelongsToSamePersonTests: XCTestCase {
 				base45: try base45Fake(
 					from: DigitalCovidCertificate.fake(
 						name: Name.fake(
-							standardizedFamilyName: testCase.actHolderB.fnt,
-							standardizedGivenName: testCase.actHolderB.gnt
+							standardizedFamilyName: testCase.actHolderB.nam.fnt,
+							standardizedGivenName: testCase.actHolderB.nam.gnt
 						),
 						dateOfBirth: testCase.actHolderB.dob
 					)
@@ -40,285 +49,24 @@ class HealthCertificate_BelongsToSamePersonTests: XCTestCase {
 		}
 	}
 	
-	struct TestCase {
-		struct Holder {
-			let gnt: String
-			let fnt: String
-			let dob: String
-		}
-		
+	private struct TestCases: Decodable {
+		let data: [TestCase]
+	}
+
+	private struct TestCase: Decodable {
 		let description: String
 		let actHolderA: Holder
 		let actHolderB: Holder
 		let expIsSameHolder: Bool
 	}
 	
-	let testCases = [
-		TestCase(
-			description: "happy path - match",
-			actHolderA: TestCase.Holder(
-				gnt: "ERIKA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			actHolderB: TestCase.Holder(
-				gnt: "ERIKA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			expIsSameHolder: true
-		),
-		TestCase(
-			description: "happy path - no match - different dob",
-			actHolderA: TestCase.Holder(
-				gnt: "ERIKA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			actHolderB: TestCase.Holder(
-				gnt: "ERIKA",
-				fnt: "MUSTERMANN",
-				dob: "1970-01-01"
-			),
-			expIsSameHolder: false
-		),
-		TestCase(
-			description: "happy path - no match - no matches in gnt",
-			actHolderA: TestCase.Holder(
-				gnt: "ERIKA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			actHolderB: TestCase.Holder(
-				gnt: "ANGELIKA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			expIsSameHolder: false
-		),
-		TestCase(
-			description: "happy path - no match - no matches in fnt",
-			actHolderA: TestCase.Holder(
-				gnt: "ERIKA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			actHolderB: TestCase.Holder(
-				gnt: "ANGELIKA",
-				fnt: "BEISPIELFRAU",
-				dob: "1980-02-03"
-			),
-			expIsSameHolder: false
-		),
-		TestCase(
-			description: "match despite optional middle name",
-			actHolderA: TestCase.Holder(
-				gnt: "ERIKA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			actHolderB: TestCase.Holder(
-				gnt: "ERIKA<MARIA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			expIsSameHolder: true
-		),
-		TestCase(
-			description: "match despite last name addendum",
-			actHolderA: TestCase.Holder(
-				gnt: "ERIKA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			actHolderB: TestCase.Holder(
-				gnt: "ERIKA",
-				fnt: "MUSTERMANN<GABLER",
-				dob: "1980-02-03"
-			),
-			expIsSameHolder: true
-		),
-		TestCase(
-			description: "no match for twins",
-			actHolderA: TestCase.Holder(
-				gnt: "ERIKA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			actHolderB: TestCase.Holder(
-				gnt: "ANGELIKA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			expIsSameHolder: false
-		),
-		TestCase(
-			description: "match for twins with same middle name (false positive)",
-			actHolderA: TestCase.Holder(
-				gnt: "ERIKA<MARIA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			actHolderB: TestCase.Holder(
-				gnt: "ANGELIKA<MARIA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			expIsSameHolder: true
-		),
-		TestCase(
-			description: "no match for siblings with same middle name (different dob)",
-			actHolderA: TestCase.Holder(
-				gnt: "ERIKA<MARIA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			actHolderB: TestCase.Holder(
-				gnt: "ANGELIKA<MARIA",
-				fnt: "MUSTERMANN",
-				dob: "1970-01-01"
-			),
-			expIsSameHolder: false
-		),
-		TestCase(
-			description: "match despite leading and trailing chevron (<)",
-			actHolderA: TestCase.Holder(
-				gnt: "ERIKA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			actHolderB: TestCase.Holder(
-				gnt: "<ERIKA<",
-				fnt: "<MUSTERMANN<",
-				dob: "1980-02-03"
-			),
-			expIsSameHolder: true
-		),
-		TestCase(
-			description: "no match because of matching chevrons (<)",
-			actHolderA: TestCase.Holder(
-				gnt: "<ERIKA<",
-				fnt: "<MUSTERMANN<",
-				dob: "1980-02-03"
-			),
-			actHolderB: TestCase.Holder(
-				gnt: "<ANGELIKA<",
-				fnt: "<MUSTERMANN<",
-				dob: "1980-02-03"
-			),
-			expIsSameHolder: false
-		),
-		TestCase(
-			description: "match despite leading and trailing whitespace",
-			actHolderA: TestCase.Holder(
-				gnt: "ERIKA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			actHolderB: TestCase.Holder(
-				gnt: " ERIKA ",
-				fnt: " MUSTERMANN ",
-				dob: "  1980-02-03  "
-			),
-			expIsSameHolder: true
-		),
-		TestCase(
-			description: "no match because of matching whitespace",
-			actHolderA: TestCase.Holder(
-				gnt: " ERIKA ",
-				fnt: " MUSTERMANN ",
-				dob: "1980-02-03"
-			),
-			actHolderB: TestCase.Holder(
-				gnt: " ANGELIKA ",
-				fnt: " MUSTERMANN ",
-				dob: "1980-02-03"
-			),
-			expIsSameHolder: false
-		),
-		TestCase(
-			description: "match despite doctor's degree in fnt (no space)",
-			actHolderA: TestCase.Holder(
-				gnt: "ERIKA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			actHolderB: TestCase.Holder(
-				gnt: "ERIKA",
-				fnt: "DR<MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			expIsSameHolder: true
-		),
-		TestCase(
-			description: "match despite doctor's degree in fnt (with space)",
-			actHolderA: TestCase.Holder(
-				gnt: "ERIKA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			actHolderB: TestCase.Holder(
-				gnt: "ERIKA",
-				fnt: "DR<<MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			expIsSameHolder: true
-		),
-		TestCase(
-			description: "match despite doctor's degree in gnt (no space)",
-			actHolderA: TestCase.Holder(
-				gnt: "ERIKA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			actHolderB: TestCase.Holder(
-				gnt: "DR<ERIKA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			expIsSameHolder: true
-		),
-		TestCase(
-			description: "match despite doctor's degree in gnt (with space)",
-			actHolderA: TestCase.Holder(
-				gnt: "ERIKA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			actHolderB: TestCase.Holder(
-				gnt: "DR<<ERIKA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			expIsSameHolder: true
-		),
-		TestCase(
-			description: "no match because of matching doctor's degree in gnt",
-			actHolderA: TestCase.Holder(
-				gnt: "DR<<ERIKA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			actHolderB: TestCase.Holder(
-				gnt: "DR<<ANGELIKA",
-				fnt: "MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			expIsSameHolder: false
-		),
-		TestCase(
-			description: "no match because of matching doctor's degree in fnt",
-			actHolderA: TestCase.Holder(
-				gnt: "ERIKA",
-				fnt: "DR<<MUSTERMANN",
-				dob: "1980-02-03"
-			),
-			actHolderB: TestCase.Holder(
-				gnt: "ERIKA",
-				fnt: "DR<<BEISPIELFRAU",
-				dob: "1980-02-03"
-			),
-			expIsSameHolder: false
-		)
-	]
+	private struct Holder: Decodable {
+		let nam: TesCaseName
+		let dob: String
+	}
+	
+	private struct TesCaseName: Decodable {
+		let gnt: String
+		let fnt: String
+	}
 }
