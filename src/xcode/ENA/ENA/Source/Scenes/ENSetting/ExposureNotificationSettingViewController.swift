3ac790dfd4835c164dfea5a31350e9794efa1077
Kai Teuber <Kai.Teuber@app-developer.club>
2022-02-14 14:33:31 +0100

    unify days of installation calculation

--- src/xcode/ENA/ENA/Source/Scenes/ENSetting/ExposureNotificationSettingViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ENSetting/ExposureNotificationSettingViewController.swift
@@ -318,12 +318,12 @@ final class ExposureNotificationSettingViewController: UITableViewController, Ac
 	}
 
 	private func daysSinceInstallationCell(for indexPath: IndexPath, in tableView: UITableView) -> UITableViewCell {
-		guard let daysSinceInstallationCell = tableView.dequeueReusableCell(withIdentifier: ReusableCellIdentifier.daysSinceInstallationCell.rawValue, for: indexPath) as? DaysSinceInstallTableViewCell else {
+		guard let daysSinceInstallationCell = tableView.dequeueReusableCell(withIdentifier: ReusableCellIdentifier.daysSinceInstallationCell.rawValue, for: indexPath) as? DaysSinceInstallTableViewCell,
+			  let appInstallationDate = store.appInstallationDate else {
 			fatalError("Cell is not registered")
 		}
 
-		daysSinceInstallationCell.configure(daysSinceInstall: store.appInstallationDate?.ageInDays ?? 0)
-
+		daysSinceInstallationCell.configure(daysSinceInstall: max(Calendar.autoupdatingCurrent.startOfDay(for: appInstallationDate).ageInDays ?? 0, 0))
 		return daysSinceInstallationCell
 	}
 
