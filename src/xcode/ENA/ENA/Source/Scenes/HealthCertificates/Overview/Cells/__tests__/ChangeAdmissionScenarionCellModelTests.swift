a4b40ace6e6fea24fb1c4dfb594a16283cb7ca3c
Naveed Khalid <nkhalid.here@gmail.com>
2022-02-14 16:05:11 +0100

    fixing tests

--- src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/Overview/Cells/__tests__/ChangeAdmissionScenarionCellModelTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/Overview/Cells/__tests__/ChangeAdmissionScenarionCellModelTests.swift
@@ -9,11 +9,12 @@ class ChangeAdmissionScenarionCellModelTests: XCTestCase {
 
 	func testGIVEN_CellModel_THEN_InitialStateIsAsExpected() {
 		// GIVEN
-		let cellViewModel = changeAdmissionScenarionCellModel()
+		let cellViewModel = ChangeAdmissionScenarionCellModel()
 
 		// THEN
 		XCTAssertEqual(cellViewModel.textColorPublisher.value, UIColor.enaColor(for: .textPrimary1))
 		XCTAssertEqual(cellViewModel.iconImagePublisher.value, UIImage(named: "Icons_Admission_State"))
+		XCTAssertFalse(cellViewModel.isCustomAccessoryViewHiddenPublisher.value)
 	}
 
 }
