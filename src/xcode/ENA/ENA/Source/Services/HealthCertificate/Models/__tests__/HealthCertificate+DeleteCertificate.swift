eee4e96dd926750f36ea081e3b6a288fc0f4892d
Pascal Brause <pascal@brause.haus>
2022-02-15 13:32:34 +0100

    improve tests

--- src/xcode/ENA/ENA/Source/Services/HealthCertificate/Models/__tests__/HealthCertificate+DeleteCertificate.swift
+++ src/xcode/ENA/ENA/Source/Services/HealthCertificate/Models/__tests__/HealthCertificate+DeleteCertificate.swift
@@ -23,44 +23,10 @@ class HealthCertificate_DeleteCertificateTests: XCTestCase {
 			cclService: FakeCCLService(),
 			recycleBin: .fake()
 		)
-		
-		let dob = "1986-01-01"
-		let certificate1 = try base45Fake(
-			from: DigitalCovidCertificate.fake(
-				name: Name.fake(
-					standardizedFamilyName: "DUCK",
-					standardizedGivenName: "DONALD"
-				),
-				dateOfBirth: dob,
-				testEntries: [TestEntry.fake()]
-			)
-		)
-		
-		let certificate2 = try base45Fake(
-			from: DigitalCovidCertificate.fake(
-				name: Name.fake(
-					standardizedFamilyName: "DUCK",
-					standardizedGivenName: "DONALD<DONNI"
-				),
-				dateOfBirth: dob,
-				testEntries: [TestEntry.fake()]
-			)
-		)
-		
-		let certificate3 = try base45Fake(
-			from: DigitalCovidCertificate.fake(
-				name: Name.fake(
-					standardizedFamilyName: "DUCK",
-					standardizedGivenName: "DONALD<DUBBY"
-				),
-				dateOfBirth: dob,
-				testEntries: [TestEntry.fake()]
-			)
-		)
-		
-		service.registerHealthCertificate(base45: certificate1)
-		service.registerHealthCertificate(base45: certificate2)
-		service.registerHealthCertificate(base45: certificate3)
+		let single1 = try certificateSingle1()
+		service.registerHealthCertificate(base45: single1)
+		service.registerHealthCertificate(base45: try certificateSingle2())
+		service.registerHealthCertificate(base45: try certificateCombiner())
 		
 		// We should have now 1 person with three different certificate attributes.
 		XCTAssertEqual(service.healthCertifiedPersons.count, 1)
@@ -68,7 +34,7 @@ class HealthCertificate_DeleteCertificateTests: XCTestCase {
 		// WHEN
 		
 		guard let person = service.healthCertifiedPersons.first,
-			  let certificateToRemove = person.healthCertificates.first(where: { $0.base45 == certificate2 }) else {
+			  let certificateToRemove = person.healthCertificates.first(where: { $0.base45 == single1 }) else {
 				  XCTFail("Person should not be empty")
 				  return
 			  }
@@ -96,61 +62,73 @@ class HealthCertificate_DeleteCertificateTests: XCTestCase {
 			cclService: FakeCCLService(),
 			recycleBin: .fake()
 		)
-		
-		let dob = "1986-01-01"
-		let certificate1 = try base45Fake(
-			from: DigitalCovidCertificate.fake(
-				name: Name.fake(
-					standardizedFamilyName: "DUCK",
-					standardizedGivenName: "DONALD"
-				),
-				dateOfBirth: dob,
-				testEntries: [TestEntry.fake()]
-			)
-		)
-		
-		let certificate2 = try base45Fake(
-			from: DigitalCovidCertificate.fake(
-				name: Name.fake(
-					standardizedFamilyName: "DUCK",
-					standardizedGivenName: "DONALD<DONNI"
-				),
-				dateOfBirth: dob,
-				testEntries: [TestEntry.fake()]
-			)
-		)
-		
-		let certificate3 = try base45Fake(
-			from: DigitalCovidCertificate.fake(
-				name: Name.fake(
-					standardizedFamilyName: "DONALD",
-					standardizedGivenName: "DUCK"
-				),
-				dateOfBirth: dob,
-				testEntries: [TestEntry.fake()]
-			)
-		)
-		
-		service.registerHealthCertificate(base45: certificate1)
-		service.registerHealthCertificate(base45: certificate2)
-		service.registerHealthCertificate(base45: certificate3)
+		let combiner = try certificateCombiner()
+		service.registerHealthCertificate(base45: try certificateSingle1())
+		service.registerHealthCertificate(base45: try certificateSingle2())
+		service.registerHealthCertificate(base45: combiner)
 		
 		// We should have now 1 person with three different certificate attributes.
 		XCTAssertEqual(service.healthCertifiedPersons.count, 1)
 		
 		// WHEN
 		
-		guard let person = service.healthCertifiedPersons.first else {
-			XCTFail("Person should not be empty")
-			return
-		}
+		guard let person = service.healthCertifiedPersons.first,
+			  let certificateToRemove = person.healthCertificates.first(where: { $0.base45 == combiner }) else {
+				  XCTFail("Person should not be empty")
+				  return
+			  }
 		
-		// We remove now the second certificate because it acts like glue between 1 and 3.
-		person.healthCertificates.removeAll { $0 == certificate2 }
+		// We remove now the second certificate because it acts like glue between 2 and 3.
+		service.moveHealthCertificateToBin(certificateToRemove)
 		
 		// THEN
 		
 		// After delering certificate2, 1 and 3 should not match as one person so the result is 2 different persons.
 		XCTAssertEqual(service.healthCertifiedPersons.count, 2)
+	}
+	
+	private let dob = "1986-01-01"
+	
+	private func certificateCombiner() throws -> Base45 {
+		try base45Fake(
+			from: DigitalCovidCertificate.fake(
+				name: Name.fake(
+					standardizedFamilyName: "DUCK<QUACK",
+					standardizedGivenName: "DONALD<DONNI"
+				),
+				dateOfBirth: dob,
+				testEntries: [TestEntry.fake(
+					uniqueCertificateIdentifier: "1"
+				)]
+			)
+		)
+	}
+	private func certificateSingle1() throws -> Base45 {
+		try base45Fake(
+			from: DigitalCovidCertificate.fake(
+				name: Name.fake(
+					standardizedFamilyName: "DUCK",
+					standardizedGivenName: "DONALD"
+				),
+				dateOfBirth: dob,
+				testEntries: [TestEntry.fake(
+					uniqueCertificateIdentifier: "2"
+				)]
+			)
+		)
+	}
+	private func certificateSingle2() throws -> Base45 {
+		try base45Fake(
+			from: DigitalCovidCertificate.fake(
+				name: Name.fake(
+					standardizedFamilyName: "QUACK",
+					standardizedGivenName: "DONNI"
+				),
+				dateOfBirth: dob,
+				testEntries: [TestEntry.fake(
+					uniqueCertificateIdentifier: "3"
+				)]
+			)
+		)
 	}
 }
