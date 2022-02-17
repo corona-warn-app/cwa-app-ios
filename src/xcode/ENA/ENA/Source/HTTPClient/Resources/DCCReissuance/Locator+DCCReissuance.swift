965dd927e34e3c3df5183cefd786573a547367ad
Pascal Brause <pascal@brause.haus>
2022-02-17 16:29:39 +0100

    Add locator

---
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/DCCReissuance/Locator+DCCReissuance.swift
@@ -0,0 +1,21 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+
+extension Locator {
+
+	// send:	JSON
+	// receive:	JSON
+	// type:	default
+	// comment:
+
+	static var dccReissuance: Locator {
+		return Locator(
+			endpoint: .dccRecertify,
+			paths: ["api", "certify", "v2", "reissue"],
+			method: .post
+		)
+	}
+}
