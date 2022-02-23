17a79fe74e6bc6a72e41974ac44c0e0ee1665cd1
Naveed Khalid <nkhalid.here@gmail.com>
2022-02-23 14:46:23 +0100

    adding accessibility label

--- src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/Overview/HealthCertificateOverviewViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/Overview/HealthCertificateOverviewViewController.swift
@@ -213,6 +213,7 @@ class HealthCertificateOverviewViewController: UITableViewController {
 
 		cell.configure(cellModel: ChangeAdmissionScenarionCellModel(changeAdmissionScenarioButtonText: viewModel.changeAdmissionScenarioButtonText?.localized(cclService: cclService) ?? AppStrings.HealthCertificate.Overview.admissionScenarioButtonLabel))
 		cell.accessibilityIdentifier = AccessibilityIdentifiers.HealthCertificate.Overview.changeAdmissionScenarioCell
+		cell.accessibilityLabel = viewModel.changeAdmissionScenarioButtonText?.localized(cclService: cclService) ?? AppStrings.HealthCertificate.Overview.admissionScenarioButtonLabel
 		return cell
 	}
 	
