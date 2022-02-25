5f180f2594dceb2d064cee65a49348c8e2bcb1c8
Artur Friesen <mail@arturfriesen.de>
2022-02-25 16:38:16 +0100

    Add logic to replace certificate.

--- src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/HealthCertifiedPerson/ReissuanceConsent/HealthCertifiedPersonReissuanceConsentViewModel.swift
+++ src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/HealthCertifiedPerson/ReissuanceConsent/HealthCertifiedPersonReissuanceConsentViewModel.swift
@@ -11,6 +11,8 @@ import OpenCombine
 enum HealthCertifiedPersonUpdateError: Error {
 	case updateFailedError
 	case restServiceError(ServiceError<DCCReissuanceResourceError>)
+	case certificateDecodingErrro(Error)
+	case DCC_RI_NO_RELATION
 }
 
 
@@ -21,11 +23,13 @@ class HealthCertifiedPersonReissuanceConsentViewModel {
 	init(
 		person: HealthCertifiedPerson,
 		appConfigProvider: AppConfigurationProviding,
-		restServiceProvider: RestServiceProviding
+		restServiceProvider: RestServiceProviding,
+		healthCertificateService: HealthCertificateService
 	) {
 		self.healthCertifiedPerson = person
 		self.appConfigProvider = appConfigProvider
 		self.restServiceProvider = restServiceProvider
+		self.healthCertificateService = healthCertificateService
 	}
 
 	// MARK: - Internal
@@ -39,15 +43,17 @@ class HealthCertifiedPersonReissuanceConsentViewModel {
 					}
 					
 					let publicKeyHash = appConfig.dgcParameters.reissueServicePublicKeyDigest.sha256String()
-					
 					let trustEvaluation = DefaultTrustEvaluation(publicKeyHash: publicKeyHash)
 					
-					guard let certificateToReissue = self.healthCertifiedPerson.dccWalletInfo?.certificateReissuance?.certificateToReissue.certificateRef.barcodeData else {
+					guard let certificateToReissue = self.healthCertifiedPerson.dccWalletInfo?.certificateReissuance?.certificateToReissue.certificateRef.barcodeData,
+						let certificateToReissueRef = self.healthCertifiedPerson.dccWalletInfo?.certificateReissuance?.certificateToReissue.certificateRef else {
 						completion(.failure(.updateFailedError))
 						return
 					}
 					
-					let accompanyingCertificates = self.healthCertifiedPerson.dccWalletInfo?.certificateReissuance?.accompanyingCertificates.compactMap { $0.certificateRef.barcodeData } ?? []
+					let accompanyingCertificates = self.healthCertifiedPerson.dccWalletInfo?.certificateReissuance?.accompanyingCertificates.compactMap {
+						$0.certificateRef.barcodeData
+					} ?? []
 					
 					let certificates = [certificateToReissue] + accompanyingCertificates
 					let sendModel = DCCReissuanceSendModel(certificates: certificates)
@@ -57,16 +63,39 @@ class HealthCertifiedPersonReissuanceConsentViewModel {
 					)
 					
 					self.restServiceProvider.load(resource) { [weak self] result in
+						guard let self = self else {
+							completion(.failure(.updateFailedError))
+							return
+						}
+						
 						switch result {
-						case .success(let receiveModel):
+						case .success(let certificates):
+
+							let certificate = certificates.first { certificate in
+								return certificate.relations.contains { relation in
+									relation.index == 0 && relation.action == "replace"
+								}
+							}
 							
-							break
+							guard let certificate = certificate else {
+								completion(.failure(.DCC_RI_NO_RELATION))
+								return
+							}
+							
+							do {
+								try self.healthCertificateService.replaceHealthCertificate(
+									oldCertificateRef: certificateToReissueRef,
+									with: certificate.certificate,
+									for: self.healthCertifiedPerson
+								)
+							} catch {
+								completion(.failure(.certificateDecodingErrro(error)))
+							}
+
 						case .failure(let error):
 							completion(.failure(.restServiceError(error)))
 						}
 					}
-					
-					
 			 }
 			 .store(in: &subscriptions)
 	}
@@ -76,6 +105,7 @@ class HealthCertifiedPersonReissuanceConsentViewModel {
 	private let healthCertifiedPerson: HealthCertifiedPerson
 	private let appConfigProvider: AppConfigurationProviding
 	private let restServiceProvider: RestServiceProviding
+	private let healthCertificateService: HealthCertificateService
 	private var subscriptions = Set<AnyCancellable>()
 	
 }
