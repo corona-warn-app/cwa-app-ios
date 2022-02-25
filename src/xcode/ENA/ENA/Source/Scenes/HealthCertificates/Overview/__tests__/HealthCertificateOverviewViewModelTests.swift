b91cfcea893ed6973bc94c44b785d5df7869d075
Naveed Khalid <nkhalid.here@gmail.com>
2022-02-25 16:53:18 +0100

    adding some tests

--- src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/Overview/__tests__/HealthCertificateOverviewViewModelTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/Overview/__tests__/HealthCertificateOverviewViewModelTests.swift
@@ -18,13 +18,85 @@ class HealthCertificateOverviewViewModelTests: XCTestCase {
 		)
 
 		// THEN
-		XCTAssertEqual(viewModel.numberOfSections, 6)
+		XCTAssertEqual(viewModel.numberOfSections, 7)
+		XCTAssertEqual(viewModel.numberOfRows(in: 0), 0)
+		XCTAssertEqual(viewModel.numberOfRows(in: 1), 0)
+		XCTAssertEqual(viewModel.numberOfRows(in: 2), 1)
+		XCTAssertEqual(viewModel.numberOfRows(in: 3), 0)
+		XCTAssertEqual(viewModel.numberOfRows(in: 4), 0)
+		XCTAssertEqual(viewModel.numberOfRows(in: 5), 0)
+		XCTAssertEqual(viewModel.numberOfRows(in: 6), 0)
+	}
+
+	func testGIVEN_HealthCertificateOverviewViewModel_WHEN_dccAdmissionCheckScenariosEnabled_THEN_SetupIsCorrect() throws {
+		// GIVEN
+		var cclService = FakeCCLService()
+		cclService.dccAdmissionCheckScenariosEnabled = true
+		
+		let viewModel = HealthCertificateOverviewViewModel(
+			store: MockTestStore(),
+			healthCertificateService: service,
+			healthCertificateRequestService: requestService,
+			cclService: cclService
+		)
+
+		// THEN
+		XCTAssertEqual(viewModel.numberOfSections, 7)
 		XCTAssertEqual(viewModel.numberOfRows(in: 0), 0)
 		XCTAssertEqual(viewModel.numberOfRows(in: 1), 0)
 		XCTAssertEqual(viewModel.numberOfRows(in: 2), 0)
 		XCTAssertEqual(viewModel.numberOfRows(in: 3), 0)
+		XCTAssertEqual(viewModel.numberOfRows(in: 4), 0)
+		XCTAssertEqual(viewModel.numberOfRows(in: 5), 0)
+		XCTAssertEqual(viewModel.numberOfRows(in: 6), 0)
 	}
+	
+	func testGIVEN_HealthCertificateOverviewViewModel_WHEN_dccAdmissionCheckScenariosEnabled_healthCertificates_THEN_SetupIsCorrect() throws {
+		// GIVEN
+		var cclService = FakeCCLService()
+		cclService.dccAdmissionCheckScenariosEnabled = true
+		
+		let vaccinationCertificate1Base45 = try base45Fake(
+			from: DigitalCovidCertificate.fake(
+				name: .fake(),
+				vaccinationEntries: [VaccinationEntry.fake(
+					dateOfVaccination: "2021-09-03",
+					uniqueCertificateIdentifier: "1"
+				)]
+			)
+		)
+		
+		let vaccinationCertificate2Base45 = try base45Fake(
+			from: DigitalCovidCertificate.fake(
+				name: .fake(),
+				vaccinationEntries: [VaccinationEntry.fake(
+					dateOfVaccination: "2021-09-06",
+					uniqueCertificateIdentifier: "2"
+				)]
+			)
+		)
+	
+		service.registerHealthCertificate(base45: vaccinationCertificate1Base45)
+		service.registerHealthCertificate(base45: vaccinationCertificate2Base45)
+		
+		let viewModel = HealthCertificateOverviewViewModel(
+			store: MockTestStore(),
+			healthCertificateService: service,
+			healthCertificateRequestService: requestService,
+			cclService: cclService
+		)
 
+		// THEN
+		XCTAssertEqual(viewModel.numberOfSections, 7)
+		XCTAssertEqual(viewModel.numberOfRows(in: 0), 1)
+		XCTAssertEqual(viewModel.numberOfRows(in: 1), 1)
+		XCTAssertEqual(viewModel.numberOfRows(in: 2), 0)
+		XCTAssertEqual(viewModel.numberOfRows(in: 3), 0)
+		XCTAssertEqual(viewModel.numberOfRows(in: 4), 1)
+		XCTAssertEqual(viewModel.numberOfRows(in: 5), 1)
+		XCTAssertEqual(viewModel.numberOfRows(in: 6), 0)
+	}
+	
 	func testGIVEN_requestTestCertificate_THEN_noErrorIsSet() {
 		// GIVEN
 		let viewModel = HealthCertificateOverviewViewModel(
