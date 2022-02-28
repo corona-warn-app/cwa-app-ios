eb4a56fa1746f260a256fa3c55e41b0ab6e02e12
Omar Ahmed <omarabdelaziz125@gmail.com>
2022-02-28 09:34:59 +0100

    add string and accessibility identifier

--- src/xcode/ENA/ENA/Source/View Helpers/AppStrings.swift
+++ src/xcode/ENA/ENA/Source/View Helpers/AppStrings.swift
@@ -1116,6 +1116,8 @@ enum AppStrings {
 		static let contactHotlineForeignNumber = NSLocalizedString("App_Information_Contact_Hotline_Number_Foreign", comment: "")
 		static let contactHotlineForeignDetails = NSLocalizedString("App_Information_Contact_Hotline_Details_Foreign", comment: "")
 		static let contactHotlineTerms = NSLocalizedString("App_Information_Contact_Hotline_Terms", comment: "")
+		
+		static let accessibilityNavigation = NSLocalizedString("App_Information_Accessibility_Navigation", comment: "")
 
 		static let imprintNavigation = NSLocalizedString("App_Information_Imprint_Navigation", comment: "")
 		static let imprintImageDescription = NSLocalizedString("App_Information_Imprint_ImageDescription", comment: "")
