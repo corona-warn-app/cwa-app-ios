037dad58394a9adb42b3780c4f8118e683529cd1
Artur Friesen <mail@arturfriesen.de>
2022-03-04 12:32:32 +0100

    Ass accidentally removed test case.

---
+++ src/xcode/ENA/ENA/Source/Services/CCLService/__tests__/GetWalletInfoInputTests.swift
@@ -0,0 +1,21 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+import XCTest
+import AnyCodable
+@testable import ENA
+
+class GetWalletInfoInputTests: XCTestCase {
+
+	func test_InputData() throws {
+		let input = GetWalletInfoInput.make(with: Date(), language: "de", certificates: [], boosterNotificationRules: [], identifier: "")
+		
+		XCTAssertEqual(input["os"], AnyDecodable("ios"))
+		XCTAssertEqual(input["language"], AnyDecodable("de"))
+		XCTAssertNotNil(input["now"])
+		XCTAssertNotNil(input["certificates"])
+		XCTAssertNotNil(input["boosterNotificationRules"])
+	}
+}
