1032188c3e9f6aafb7a33d70a7d803c4effe49f2
Nick Guendling <hello@nickguendling.com>
2022-03-04 18:14:01 +0100

    Add loading screen

---
+++ src/xcode/ENA/ENA/Source/Scenes/LoadingScreen/LoadingScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import UIKit
+
+class LoadingScreenViewController: UIViewController {
+
+	// MARK: - Init
+
+	init() {
+		super.init(nibName: nil, bundle: nil)
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+}
