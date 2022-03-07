634c806e44db6687aba1530e1a80d01ddcb9e1bd
Nick Guendling <hello@nickguendling.com>
2022-03-07 19:04:44 +0100

    Show loading screen contents delayed to skip them on fast devices

--- src/xcode/ENA/ENA/Source/Scenes/LoadingScreen/LoadingScreenViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/LoadingScreen/LoadingScreenViewController.swift
@@ -16,4 +16,23 @@ class LoadingScreenViewController: UIViewController {
 		fatalError("init(coder:) has not been implemented")
 	}
 
+	// MARK: - Overrides
+
+	override func viewDidAppear(_ animated: Bool) {
+		Log.info("Loading screen did appear", log: .appLifecycle)
+		// Don't show loading screen content on fast devices
+		DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+			Log.info("Loading screen did show activity indicator", log: .appLifecycle)
+
+			self.logoImageView.isHidden = false
+			self.activityIndicator.isHidden = false
+		}
+	}
+
+	// MARK: - Private
+
+	@IBOutlet private weak var logoImageView: UIImageView!
+	@IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
+
+
 }
