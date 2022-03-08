7750717b06ff73b65659f42ab68991163419f6f3
Nick Guendling <hello@nickguendling.com>
2022-03-08 17:31:17 +0100

    Don't use .large indicator style on iOS 12 devices as it leads to a warning

--- src/xcode/ENA/ENA/Source/Scenes/LoadingScreen/LoadingScreenViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/LoadingScreen/LoadingScreenViewController.swift
@@ -18,7 +18,20 @@ class LoadingScreenViewController: UIViewController {
 
 	// MARK: - Overrides
 
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		if #available(iOS 13.0, *) {
+			activityIndicator.style = .large
+		} else {
+			activityIndicator.style = .whiteLarge
+			activityIndicator.color = .enaColor(for: .textPrimary2)
+		}
+	}
+
 	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+
 		Log.info("Loading screen did appear", log: .appLifecycle)
 		// Don't show loading screen content on fast devices
 		DispatchQueue.main.asyncAfter(deadline: .now() + 3) { [weak self] in
@@ -38,6 +51,5 @@ class LoadingScreenViewController: UIViewController {
 
 	@IBOutlet private weak var logoImageView: UIImageView!
 	@IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
-
 
 }
