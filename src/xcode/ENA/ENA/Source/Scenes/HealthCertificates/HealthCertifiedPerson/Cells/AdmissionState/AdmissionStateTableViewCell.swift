c3ccfc7b7f2e37e55afc0ece0c528285747c9338
Omar Ahmed <omarabdelaziz125@gmail.com>
2022-03-14 10:18:53 +0100

    fix compression

--- src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/HealthCertifiedPerson/Cells/AdmissionState/AdmissionStateTableViewCell.swift
+++ src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/HealthCertifiedPerson/Cells/AdmissionState/AdmissionStateTableViewCell.swift
@@ -109,6 +109,7 @@ class AdmissionStateTableViewCell: UITableViewCell, UITextViewDelegate, ReuseIde
 		titleLabel.numberOfLines = 0
 		titleLabel.textColor = .enaColor(for: .textPrimary1)
 		titleLabel.accessibilityIdentifier = AccessibilityIdentifiers.HealthCertificate.AdmissionState.title
+		titleLabel.contentCompressionResistancePriority(.required, for: .vertical)
 
 		return titleLabel
 	}()
