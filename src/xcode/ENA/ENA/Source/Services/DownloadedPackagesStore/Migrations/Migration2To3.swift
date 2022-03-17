1cb9974d225cd09bd2adf29e524145cc0e430a77
Artur Friesen <mail@arturfriesen.de>
2022-03-17 13:55:57 +0100

    Add new store and migration files for v3.

---
+++ src/xcode/ENA/ENA/Source/Services/DownloadedPackagesStore/Migrations/Migration2To3.swift
@@ -0,0 +1,36 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import FMDB
+
+final class Migration2To3: Migration {
+
+	private let database: FMDatabase
+
+	// MARK: - Init
+
+	init(database: FMDatabase) {
+		self.database = database
+	}
+
+	// MARK: - Protocol Migration
+
+	let version = 2
+
+	func execute() throws {
+		let sql = """
+			BEGIN TRANSACTION;
+
+			-- add column
+			ALTER TABLE Z_DOWNLOADED_PACKAGE
+			ADD Z_CHECKED_FOR_EXPOSURES INTEGER DEFAULT 0;
+
+			COMMIT;
+		"""
+
+		guard database.executeStatements(sql) else {
+			throw MigrationError.general(description: "(\(database.lastErrorCode())) \(database.lastErrorMessage())")
+		}
+	}
+}
