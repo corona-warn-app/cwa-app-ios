aefe0d20696c25ebab49ed775cee306e498cfa90
Pascal Brause <pascal@brause.haus>
2022-03-18 13:36:53 +0100

    remove

--- src/xcode/ENA/ENA/Source/Extensions/UIStackView+Background.swift
+++ src/xcode/ENA/ENA/Source/Extensions/UIStackView+Background.swift
@@ -9,14 +9,5 @@ extension UIStackView {
 		let viewWithBackgroundColor = UIView(frame: bounds)
 		viewWithBackgroundColor.backgroundColor = backgroundColor
 		insertSubview(viewWithBackgroundColor, at: 0)
-
-		translatesAutoresizingMaskIntoConstraints = false
-
-		NSLayoutConstraint.activate([
-			viewWithBackgroundColor.topAnchor.constraint(equalTo: topAnchor),
-			viewWithBackgroundColor.leadingAnchor.constraint(equalTo: leadingAnchor),
-			viewWithBackgroundColor.trailingAnchor.constraint(equalTo: trailingAnchor),
-			viewWithBackgroundColor.bottomAnchor.constraint(equalTo: bottomAnchor)
-		])
 	}
 }
