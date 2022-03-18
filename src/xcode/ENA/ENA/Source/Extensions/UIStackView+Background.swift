5dca5d61e6d3a8cd466b1a96eb18bc014b0f1008
Pascal Brause <pascal@brause.haus>
2022-03-18 13:27:32 +0100

    rename

--- src/xcode/ENA/ENA/Source/Extensions/UIStackView+Background.swift
+++ src/xcode/ENA/ENA/Source/Extensions/UIStackView+Background.swift
@@ -6,9 +6,9 @@ import UIKit
 
 extension UIStackView {
 	func add(backgroundColor: UIColor) {
-		let additionalBackground = UIView(frame: bounds)
-		additionalBackground.backgroundColor = backgroundColor
-		additionalBackground.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-		insertSubview(additionalBackground, at: 0)
+		let viewWithBackgroundColor = UIView(frame: bounds)
+		viewWithBackgroundColor.backgroundColor = backgroundColor
+		viewWithBackgroundColor.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+		insertSubview(viewWithBackgroundColor, at: 0)
 	}
 }
