35ce9f7c5f91b1bdc1dfd8711149ca6d9efe681d
Kai Teuber <Kai.Teuber@app-developer.club>
2022-03-21 15:19:37 +0100

    added basic FamilyMemberConsentViewController.swift

---
+++ src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/FamilyMemberConsent/FamilyMemberConsentViewController.swift
@@ -0,0 +1,72 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import UIKit
+
+class FamilyMemberConsentViewController: DynamicTableViewController, DismissHandling {
+
+	// MARK: - Init
+
+	init(
+		dismiss: @escaping () -> Void,
+		didTapSubmit: @escaping (String) -> Void
+	) {
+		self.dismiss = dismiss
+		self.didTapSubmit = didTapSubmit
+		self.viewModel = FamilyMemberConsentViewModel()
+		super.init(nibName: nil, bundle: nil)
+	}
+
+	@available(*, unavailable)
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	// MARK: - Overrides
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		setupNavigationBar()
+		setupTableView()
+	}
+
+	// MARK: - Protocol DismissHandling
+
+	func wasAttemptedToBeDismissed() {
+		dismiss()
+	}
+
+	// MARK: - Protocol TableViewDataSource
+
+	override func numberOfSections(in tableView: UITableView) -> Int {
+		// #warning Incomplete implementation, return the number of sections
+		return 0
+	}
+
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		// #warning Incomplete implementation, return the number of rows
+		return 0
+	}
+
+	// MARK: - Public
+
+	// MARK: - Internal
+
+	// MARK: - Private
+
+	private let dismiss: () -> Void
+	private let didTapSubmit: (String) -> Void
+	private let viewModel: FamilyMemberConsentViewModel
+
+	private func setupNavigationBar() {
+		navigationItem.rightBarButtonItem = dismissHandlingCloseBarButton
+	}
+
+	private func setupTableView() {
+		view.backgroundColor = .enaColor(for: .background)
+
+	}
+
+}
---
+++ src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/FamilyMemberConsent/FamilyMemberConsentViewModel.swift
@@ -0,0 +1,29 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import Foundation
+
+final class FamilyMemberConsentViewModel {
+
+	// MARK: - Init
+
+	init(
+		_ name: String? = nil
+	) {
+		self.name = name
+	}
+
+	// MARK: - Overrides
+
+	// MARK: - Protocol <#Name#>
+
+	// MARK: - Public
+
+	// MARK: - Internal
+
+	// MARK: - Private
+
+	private let name: String?
+
+}
