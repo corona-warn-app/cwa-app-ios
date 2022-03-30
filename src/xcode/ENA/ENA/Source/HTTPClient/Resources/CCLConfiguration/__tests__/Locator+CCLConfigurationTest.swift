c2b19393dcb6ef8eed2f183f3f8ec71f3f26066a
Marcus Scherer <marcus@skunkbrothers.de>
2022-03-30 18:19:21 +0200

    Fix unit-test

--- src/xcode/ENA/ENA/Source/HTTPClient/Resources/CCLConfiguration/__tests__/Locator+CCLConfigurationTest.swift
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/CCLConfiguration/__tests__/Locator+CCLConfigurationTest.swift
@@ -9,7 +9,7 @@ class Locator_CCLConfigurationTest: XCTestCase {
 
 	func testGIVEN_Locator_WHEN_getUniqueIdentifier_THEN_IsSameAsKnowUniqueIdentifier() {
 		// GIVEN
-		let knownUniqueIdentifier = "6945f9d05c93e022e25c7efcb1f0f0428bca355841b2d1a85622515dfce67db6"
+		let knownUniqueIdentifier = "b4f065feda7b87f0de440516d6edae6d1d3348d75eb8bba6c473d8ee6111fcfe"
 		let locator = Locator.CCLConfiguration(isFake: false)
 
 		// WHEN
