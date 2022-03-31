c12e77de8fb83e9a0c98956d940555b5a3659356
Kai Teuber <Kai.Teuber@app-developer.club>
2022-03-31 16:21:12 +0200

    add familiymember test

--- src/xcode/ENA/ENAUITests/FamilyMember/ENAUITests_FamilyMember.swift
+++ src/xcode/ENA/ENAUITests/FamilyMember/ENAUITests_FamilyMember.swift
@@ -21,6 +21,7 @@ class ENAUITests_FamilyMember: CWATestCase {
 	func test_RegisterCoronaTestFromUniversalQRCodeScanner() throws {
 		app.launch()
 
+
 		app.buttons[AccessibilityIdentifiers.TabBar.scanner].waitAndTap()
 
 		/// Simulator only Alert will open where you can choose what the QRScanner should scan
@@ -33,13 +34,31 @@ class ENAUITests_FamilyMember: CWATestCase {
 
 		/// Exposure submission family member consent screen
 		XCTAssertTrue(app.images[AccessibilityIdentifiers.HealthCertificate.FamilyMemberConsent.imageDescription].waitForExistence(timeout: .short))
+		XCTAssertTrue(app.cells[AccessibilityIdentifiers.HealthCertificate.FamilyMemberConsent.Legal.acknowledgementTitle].waitForExistence(timeout: .short))
+
+		/// data privacy screen
+		app.cells[AccessibilityIdentifiers.HealthCertificate.FamilyMemberConsent.dataPrivacyTitle].waitAndTap()
+		XCTAssertTrue(app.staticTexts["AppStrings.AppInformation.privacyTitle"].waitForExistence(timeout: .short))
+
+		/// back navigation
+		app.navigationBars.firstMatch.buttons.element(boundBy: 0).waitAndTap()
+
+		/// primary button
+		let primaryButton = try XCTUnwrap(app.buttons[AccessibilityIdentifiers.HealthCertificate.FamilyMemberConsent.primaryButton])
+		XCTAssertFalse(primaryButton.isEnabled)
 
 		/// Exposure submission family member consent screen
 		let textField = try XCTUnwrap(app.textFields[AccessibilityIdentifiers.HealthCertificate.FamilyMemberConsent.textInput])
 		textField.waitAndTap(.short)
 		textField.typeText("Lara")
 
-		
+		/// primary button enabled after name was given
+		XCTAssertTrue(primaryButton.isEnabled)
+		primaryButton.waitAndTap(.short)
+
+		/// test certificate consent screen
+		XCTAssertTrue(app.images[AccessibilityIdentifiers.ExposureSubmission.TestCertificate.Info.imageDescription].waitForExistence(timeout: .short))
+		app.buttons[AccessibilityIdentifiers.General.secondaryFooterButton].waitAndTap()
 	}
 
 }
