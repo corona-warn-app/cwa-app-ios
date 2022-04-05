69d4babb221de10ff32272f40bfaf75b7814812d
Pascal Brause <pascal@brause.haus>
2022-04-05 17:00:58 +0200

    add new test for resource class and change fake into struct

--- src/xcode/ENA/ENA/Source/HTTPClientCore/Service/__tests__/RetryingTests.swift
+++ src/xcode/ENA/ENA/Source/HTTPClientCore/Service/__tests__/RetryingTests.swift
@@ -179,17 +179,105 @@ class RetryingTests: XCTestCase {
 		)
 
 		standardService.load(resource, { result in
-
 			switch result {
-
 			case .success(let model):
 				XCTAssertEqual(model, defaultModel)
 			case .failure(let error):
 				XCTFail("Test should not fail but received error: \(error)")
 			}
-			loadExpectation.fulfill()
 		})
 
 		waitForExpectations(timeout: .short)
 	}
+
+	func testGIVEN_RetryingResourceAsClass_WHEN_RetryIsCanceled_THEN_Error_WrongResourceType_IsReturned() throws {
+
+		let loadExpectation = expectation(description: "Retrying is done.")
+		loadExpectation.expectedFulfillmentCount = 1
+
+		var retryCount = 3
+
+		// Call fullfill on every request and decrease the retryCount
+		let mockSession = MockUrlSession(
+			data: nil,
+			nextResponse: nil,
+			error: nil
+		) { _ in
+			loadExpectation.fulfill()
+			retryCount -= 1
+		}
+
+		let url = try XCTUnwrap(URL(string: "https://example.com"))
+
+		// Prepare responses
+		let failingResponse = HTTPURLResponse(
+			url: url,
+			statusCode: 500,
+			httpVersion: nil,
+			headerFields: nil
+		)
+
+		// Return success with our model as soon we arrive the last retry. otherwise, return always a failure
+		mockSession.onPrepareResponse = {
+			mockSession.data = nil
+			mockSession.nextResponse = failingResponse
+		}
+
+		// Return nil and http code 500. In this case the service would ignore caching behavior and look up for default values.
+		let stack = MockNetworkStack(
+			mockSession: mockSession
+		)
+
+		let standardService = StandardRestService(session: stack.urlSession)
+
+		let defaultModel = DummyResourceModel(dummyValue: "DefaultModel")
+		let resource = ResourceClassFake(
+			defaultModel: defaultModel,
+			retryingCount: 3
+		)
+
+		standardService.load(resource, { result in
+			switch result {
+			case .success:
+				XCTFail("Test should not succeed")
+			case .failure(let error):
+				XCTAssertEqual(error, .wrongResourceType)
+			}
+		})
+
+		waitForExpectations(timeout: .short)
+	}
+}
+
+// Should only be used for a failing test!
+private class ResourceClassFake: Resource {
+	init(
+		locator: Locator = .fake(),
+		type: ServiceType = .caching(),
+		sendResource: PaddingJSONSendResource<DummyResourceModel> = PaddingJSONSendResource<DummyResourceModel>(DummyResourceModel(dummyValue: "SomeValue", requestPadding: "")),
+		receiveResource: JSONReceiveResource<DummyResourceModel> = JSONReceiveResource<DummyResourceModel>(),
+		defaultModel: ResourceFake.Receive.ReceiveModel? = nil,
+		trustEvaluation: TrustEvaluating = DisabledTrustEvaluation(),
+		retryingCount: Int = 0
+	) {
+		self.locator = locator
+		self.type = type
+		self.sendResource = sendResource
+		self.receiveResource = receiveResource
+		self.defaultModel = defaultModel
+		self.trustEvaluation = trustEvaluation
+		self.retryingCount = retryingCount
+	}
+
+	let locator: Locator
+	let type: ServiceType
+	let sendResource: PaddingJSONSendResource<DummyResourceModel>
+	let receiveResource: JSONReceiveResource<DummyResourceModel>
+	let defaultModel: ResourceFake.Receive.ReceiveModel?
+	let trustEvaluation: TrustEvaluating
+	var retryingCount: Int
+
+	typealias Send = PaddingJSONSendResource<DummyResourceModel>
+	typealias Receive = JSONReceiveResource<DummyResourceModel>
+	typealias CustomError = Error
 }
