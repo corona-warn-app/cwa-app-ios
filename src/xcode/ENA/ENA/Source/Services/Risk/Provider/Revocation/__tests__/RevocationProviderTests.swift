3868dd776f3e43c75ecd1fc5e31ae983e0d480ac
Nick Guendling <hello@nickguendling.com>
2022-04-19 15:58:27 +0200

    Remove unrelated hex string tests

--- src/xcode/ENA/ENA/Source/Services/Risk/Provider/Revocation/__tests__/RevocationProviderTests.swift
+++ src/xcode/ENA/ENA/Source/Services/Risk/Provider/Revocation/__tests__/RevocationProviderTests.swift
@@ -7,28 +7,6 @@ import XCTest
 
 class RevocationProviderTests: CWATestCase {
 
-	func testGIVEN_base64_WHEN_ToHexString_THEN_ResultIsValid() {
-		// GIVEN
-		let base64 = "yLHLNvSl428="
-
-		// WHEN
-		let hexString = Data(base64Encoded: base64)?.toHexString() ?? ""
-
-		// THEN
-		XCTAssertEqual(hexString, "c8b1cb36f4a5e36f")
-	}
-
-	func testGIVEN_base64_WHEN_HexEncodedString_THEN_ResultIsValid() {
-		// GIVEN
-		let base64 = "yLHLNvSl428="
-
-		// WHEN
-		let hexString = Data(base64Encoded: base64)?.hexEncodedString() ?? ""
-
-		// THEN
-		XCTAssertEqual(hexString, "c8b1cb36f4a5e36f".uppercased())
-	}
-
 
 	func testGIVEN_given_WHEN_when_THEN_then() throws {
 		// GIVEN
