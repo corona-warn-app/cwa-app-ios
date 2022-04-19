8654992c9ac968df479d84baa0dcd677025c071d
Nick Guendling <hello@nickguendling.com>
2022-04-19 15:59:17 +0200

    Add more sophisticated revocation scenario

--- src/xcode/ENA/ENA/Source/Services/Risk/Provider/Revocation/__tests__/RevocationProviderTests.swift
+++ src/xcode/ENA/ENA/Source/Services/Risk/Provider/Revocation/__tests__/RevocationProviderTests.swift
@@ -7,16 +7,18 @@ import XCTest
 
 class RevocationProviderTests: CWATestCase {
 
-
-	func testGIVEN_given_WHEN_when_THEN_then() throws {
+	func testUpdatingCacheSuccessfully() throws {
 		// GIVEN
 
 		let restService = RestServiceProviderStub(
 			loadResources: [
+				// KID list update
 				LoadResource(
 					result: .success(revocationKidList),
 					willLoadResource: nil
 				),
+				// Update KID-Type index for recovery certificate key identifier, hash type 0a
+				// As the requests are done sorted by type, 0a should be requested first
 				LoadResource(
 					result: .success(try kidTypeIndex()),
 					willLoadResource: { resource in
@@ -24,10 +26,11 @@ class RevocationProviderTests: CWATestCase {
 							XCTFail("wrong resource type")
 							return
 						}
-						// As the requests are done sorted by type, 0a should be requested first
+
 						XCTAssertEqual(resource.locator.paths, ["version", "v1", "dcc-rl", "\(self.recoveryCertificateKeyIdentifier)0a", "index"])
 					}
 				),
+				// Update KID-Type chunk for recovery certificate, recovery certificate is revoked
 				LoadResource(
 					result: .success(try revocationChunk()),
 					willLoadResource: { resource in
@@ -37,6 +40,55 @@ class RevocationProviderTests: CWATestCase {
 						}
 
 						XCTAssertEqual(resource.locator.paths, ["version", "v1", "dcc-rl", "\(self.recoveryCertificateKeyIdentifier)0a", "e3", "d6", "chunk"])
+					}
+				),
+				// As recovery certificate is revoked after checking with hash type 0a already, 0b KID-Type index update is skipped for recovery certificate
+				// Continue with update of KID-Type index for booster certificate key identifier, hash type 0b
+				LoadResource(
+					result: .success(try kidTypeIndex()),
+					willLoadResource: { resource in
+						guard let resource = resource as? KIDTypeIndexResource else {
+							XCTFail("wrong resource type")
+							return
+						}
+
+						XCTAssertEqual(resource.locator.paths, ["version", "v1", "dcc-rl", "\(self.boosterCertificateKeyIdentifier)0b", "index"])
+					}
+				),
+				// Update KID-Type chunk for booster certificate, booster certificate is not revoked for hash type 0b
+				LoadResource(
+					result: .success(try revocationChunk()),
+					willLoadResource: { resource in
+						guard let resource = resource as? KIDTypeChunkResource else {
+							XCTFail("wrong resource type, KIDTypeChunkResource expected")
+							return
+						}
+
+						XCTAssertEqual(resource.locator.paths, ["version", "v1", "dcc-rl", "\(self.boosterCertificateKeyIdentifier)0b", "70", "12", "chunk"])
+					}
+				),
+				// Update of KID-Type index for booster certificate key identifier, hash type 0c
+				LoadResource(
+					result: .success(try kidTypeIndex()),
+					willLoadResource: { resource in
+						guard let resource = resource as? KIDTypeIndexResource else {
+							XCTFail("wrong resource type")
+							return
+						}
+
+						XCTAssertEqual(resource.locator.paths, ["version", "v1", "dcc-rl", "\(self.boosterCertificateKeyIdentifier)0c", "index"])
+					}
+				),
+				// Update KID-Type chunk for booster certificate, booster certificate is revoked for hash type 0c
+				LoadResource(
+					result: .success(try revocationChunk()),
+					willLoadResource: { resource in
+						guard let resource = resource as? KIDTypeChunkResource else {
+							XCTFail("wrong resource type, KIDTypeChunkResource expected")
+							return
+						}
+
+						XCTAssertEqual(resource.locator.paths, ["version", "v1", "dcc-rl", "\(self.boosterCertificateKeyIdentifier)0c", "25", "89", "chunk"])
 					}
 				)
 			]
@@ -61,8 +113,11 @@ class RevocationProviderTests: CWATestCase {
 		waitForExpectations(timeout: .greatestFiniteMagnitude)
 	}
 
+	// MARK: - Helpers
+
 	private let testCertificateKeyIdentifier = "0123456789abcdef"
 	private let recoveryCertificateKeyIdentifier = "f5c5970c3039d854"
+	private let boosterCertificateKeyIdentifier = "abcdef0123456789"
 
 	private lazy var certificates: [HealthCertificate] = {
 		[
@@ -85,9 +140,41 @@ class RevocationProviderTests: CWATestCase {
 			try? recoveryCertificate(
 				keyIdentifier: recoveryCertificateKeyIdentifier.dataWithHexString(),
 				signature: recoveryCertificateKeyIdentifier.dataWithHexString()
+			),
+			try? vaccinationCertificate(
+				doseNumber: 3,
+				totalSeriesOfDoses: 3,
+				keyIdentifier: boosterCertificateKeyIdentifier.dataWithHexString(),
+				signature: boosterCertificateKeyIdentifier.dataWithHexString()
 			)
 		].compactMap { $0 }
 	}()
+
+	private var revocationKidList: SAP_Internal_Dgc_RevocationKidList {
+		// recoveryCertificate
+		var item1 = SAP_Internal_Dgc_RevocationKidListItem()
+		item1.kid = recoveryCertificateKeyIdentifier.dataWithHexString()
+		item1.hashTypes = ["0b".dataWithHexString(), "0a".dataWithHexString()]
+
+		// dummy
+		var item2 = SAP_Internal_Dgc_RevocationKidListItem()
+		item2.kid = "fedcba9876543210".dataWithHexString()
+		item2.hashTypes = ["0c".dataWithHexString()]
+
+		// testCertificate
+		var item3 = SAP_Internal_Dgc_RevocationKidListItem()
+		item3.kid = testCertificateKeyIdentifier.dataWithHexString()
+		item3.hashTypes = ["0a".dataWithHexString(), "0c".dataWithHexString()]
+
+		// boosterCertificate
+		var item4 = SAP_Internal_Dgc_RevocationKidListItem()
+		item4.kid = boosterCertificateKeyIdentifier.dataWithHexString()
+		item4.hashTypes = ["0b".dataWithHexString(), "0c".dataWithHexString()]
+
+		var kidList = SAP_Internal_Dgc_RevocationKidList()
+		kidList.items = [item1, item2, item3, item4].shuffled()
+		return kidList
+	}
 
 	private func kidTypeIndex() throws -> SAP_Internal_Dgc_RevocationKidTypeIndex {
 		let recoveryCoordinate0b = try coordinate(for: certificates[3], hashType: "0b")
@@ -114,13 +201,25 @@ class RevocationProviderTests: CWATestCase {
 		item4.x = "11".dataWithHexString()
 		item4.y = ["12".dataWithHexString()]
 
+		let boosterCoordinate0b = try coordinate(for: certificates[4], hashType: "0b")
+		var item5 = SAP_Internal_Dgc_RevocationKidTypeIndexItem()
+		item5.x = boosterCoordinate0b.x.dataWithHexString()
+		item5.y = [boosterCoordinate0b.y.dataWithHexString()]
+
+		let boosterCoordinate0c = try coordinate(for: certificates[4], hashType: "0c")
+		var item6 = SAP_Internal_Dgc_RevocationKidTypeIndexItem()
+		item6.x = boosterCoordinate0c.x.dataWithHexString()
+		item6.y = [boosterCoordinate0c.y.dataWithHexString()]
+
 		var kidTypeIndex = SAP_Internal_Dgc_RevocationKidTypeIndex()
 		kidTypeIndex.items = [
 			item0,
 			item1,
 			item2,
 			item3,
-			item4
+			item4,
+			item5,
+			item6
 		].shuffled()
 		return kidTypeIndex
 	}
@@ -130,37 +229,18 @@ class RevocationProviderTests: CWATestCase {
 		return RevocationCoordinate(hash: recoveryHash)
 	}
 
-	var revocationKidList: SAP_Internal_Dgc_RevocationKidList {
-		// recoveryCertificate
-		var item1 = SAP_Internal_Dgc_RevocationKidListItem()
-		item1.kid = recoveryCertificateKeyIdentifier.dataWithHexString()
-		item1.hashTypes = ["0b".dataWithHexString(), "0a".dataWithHexString()]
-
-		// dummy
-		var item2 = SAP_Internal_Dgc_RevocationKidListItem()
-		item2.kid = "fedcba9876543210".dataWithHexString()
-		item2.hashTypes = ["0c".dataWithHexString()]
-
-		// testCertificate
-		var item3 = SAP_Internal_Dgc_RevocationKidListItem()
-		item3.kid = testCertificateKeyIdentifier.dataWithHexString()
-		item3.hashTypes = ["0a".dataWithHexString(), "0c".dataWithHexString()]
-
-		var kidList = SAP_Internal_Dgc_RevocationKidList()
-		kidList.items = [item1, item2, item3].shuffled()
-		return kidList
-	}
-
-	func revocationChunk() throws -> SAP_Internal_Dgc_RevocationChunk {
+	private func revocationChunk() throws -> SAP_Internal_Dgc_RevocationChunk {
 		let recoveryHash0a = try XCTUnwrap(certificates[3].hash(by: "0a"))
 		let recoveryHash0b = try XCTUnwrap(certificates[3].hash(by: "0b"))
+		let boosterHash0c = try XCTUnwrap(certificates[3].hash(by: "0c"))
 		let vaccinationHash = try XCTUnwrap(certificates[1].hash(by: "0a"))
 
 		var revocationChunk = SAP_Internal_Dgc_RevocationChunk()
 		revocationChunk.hashes = [
 			recoveryHash0a.dataWithHexString(),
 			recoveryHash0b.dataWithHexString(),
-			vaccinationHash.dataWithHexString()
+			vaccinationHash.dataWithHexString(),
+			boosterHash0c.dataWithHexString()
 		]
 
 		return revocationChunk
