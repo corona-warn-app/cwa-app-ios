e59bc8317d90bf8e95b663a108fc016e1b71b91a
Kai Teuber <Kai.Teuber@app-developer.club>
2022-04-28 14:08:03 +0200

    slit tests

--- src/xcode/ENA/ENA/Source/HTTPClient/Resources/AvailableDays/__tests__/AvailableDaysResourceTests.swift
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/AvailableDays/__tests__/AvailableDaysResourceTests.swift
@@ -8,16 +8,14 @@ import XCTest
 
 class AvailableDaysResourceTests: CWATestCase {
 
-	func testGIVEN_Locator_WHEN_getUniqueIdentifier_THEN_IsSameAsKnownidentifier() {
+	func testGIVEN_Locator_WHEN_getPath_THEN_isCorrect() {
 		// GIVEN
-		let knownUniqueIdentifier = "ec4d142271653f1cae9bfa8012bc9b2ce1f5e4502ae3d266cdc58702909eb241"
 		let locator = Locator.availableDays(country: "IT")
-		
+
 		// WHEN
-		let uniqueIdentifier = locator.uniqueIdentifier
+		let paths = locator.paths
 
 		// THEN
-		XCTAssertEqual(uniqueIdentifier, knownUniqueIdentifier)
 		XCTAssertEqual(
 			[
 				"version",
@@ -26,8 +24,19 @@ class AvailableDaysResourceTests: CWATestCase {
 				"country",
 				"IT",
 				"date"
-			], locator.paths)
+			], paths)
 	}
 
+	func testGIVEN_Locator_WHEN_getUniqueIdentifier_THEN_IsSameAsKnownidentifier() {
+		// GIVEN
+		let knownUniqueIdentifier = "ec4d142271653f1cae9bfa8012bc9b2ce1f5e4502ae3d266cdc58702909eb241"
+		let locator = Locator.availableDays(country: "IT")
+
+		// WHEN
+		let uniqueIdentifier = locator.uniqueIdentifier
+
+		// THEN
+		XCTAssertEqual(uniqueIdentifier, knownUniqueIdentifier)
+	}
 
 }
