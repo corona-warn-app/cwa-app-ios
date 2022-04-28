8024d7c14ae7dec9f9f446ce2241a4936129cb85
Kai Teuber <Kai.Teuber@app-developer.club>
2022-04-28 17:43:47 +0200

    remove old code

--- src/xcode/ENA/ENA/Source/Client/ClientWifiOnly.swift
+++ src/xcode/ENA/ENA/Source/Client/ClientWifiOnly.swift
@@ -9,16 +9,6 @@ protocol ClientWifiOnly {
 
 	typealias HourCompletionHandler = (Result<PackageDownloadResponse, Client.Failure>) -> Void
 
-	@available(*, deprecated)
-	typealias AvailableHoursCompletionHandler = (Result<[Int], Client.Failure>) -> Void
-
-	/// Determines hours that can be downloaded for a given day.
-	func availableHours(
-		day: String,
-		country: String,
-		completion: @escaping AvailableHoursCompletionHandler
-	)
-
 	/// Fetches the keys for a given `hour` of a specific `day`.
 	func fetchHour(
 		_ hour: Int,
