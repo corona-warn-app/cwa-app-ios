bc86fd3b6a95d5c1cbc51ab0082f0c10a75a42b6
Nick Guendling <hello@nickguendling.com>
2022-04-29 19:14:24 +0200

    Fix locator

--- src/xcode/ENA/ENA/Source/HTTPClient/Resources/DigitalCovid19Certificate/__tests__/Locator+DigitalCovid19CertificateTest.swift
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/DigitalCovid19Certificate/__tests__/Locator+DigitalCovid19CertificateTest.swift
@@ -11,7 +11,7 @@ class Locator_DigitalCovid19CertificateTest: XCTestCase {
 	func testGIVEN_Locator_WHEN_getUniqueIdentifier_THEN_IsSameAsKnowUniqueIdentifier() {
 		// GIVEN
 		let knownUniqueIdentifier = "ff62963de7b74bab38a2e6a94c7e56d822561498b0a9a4bf2b2efaf95fffb1eb"
-		let locator = Locator.digitalCovid19Certificate(registrationToken: "", isFake: false)
+		let locator = Locator.digitalCovid19Certificate(isFake: false)
 
 		// WHEN
 		let uniqueIdentifier = locator.uniqueIdentifier
