a9d903a511f1ea1e6cfa7c6c8dbb43fbcfabb5f5
Kai Teuber <Kai.Teuber@app-developer.club>
2022-04-29 14:27:12 +0200

    swiftlint changes

--- src/xcode/ENA/ENA/Source/Services/Risk/__tests__/DayKeyPackageDownloadTests.swift
+++ src/xcode/ENA/ENA/Source/Services/Risk/__tests__/DayKeyPackageDownloadTests.swift
@@ -6,7 +6,6 @@
 import Foundation
 import XCTest
 
-// swiftlint:disable:next type_body_length
 final class DayKeyPackageDownloadTest: CWATestCase {
 
 	private lazy var dummyResponse: [String: PackageDownloadResponse] = {
