9e4b101686899da9b536bf3be7bd84becc04814f
Pascal Brause <pascal@brause.haus>
2022-05-04 09:36:44 +0200

    make defaultModel static because CCLResource is now a struct

--- src/xcode/ENA/ENA/Source/HTTPClient/Resources/CCLConfiguration/CCLConfigurationResource.swift
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/CCLConfiguration/CCLConfigurationResource.swift
@@ -44,7 +44,7 @@ struct CCLConfigurationResource: Resource {
 	var type: ServiceType
 	var sendResource: EmptySendResource
 	var receiveResource: CBORReceiveResource<CCLConfigurationReceiveModel>
-	lazy var defaultModel: CCLConfigurationReceiveModel? = bundledDefaultModel
+	var defaultModel: CCLConfigurationReceiveModel? = bundledDefaultModel
 
 	// MARK: - Internal
 	
@@ -55,7 +55,7 @@ struct CCLConfigurationResource: Resource {
 
 	// MARK: - Private
 
-	private var bundledDefaultModel: CCLConfigurationReceiveModel? {
+	private static var bundledDefaultModel: CCLConfigurationReceiveModel? {
 		guard let url = Bundle.main.url(forResource: "ccl-configuration", withExtension: "bin"),
 			  let fallbackBin = try? Data(contentsOf: url) else {
 			Log.error("Creating the default model failed due to loading default bin from disc", log: .client)
