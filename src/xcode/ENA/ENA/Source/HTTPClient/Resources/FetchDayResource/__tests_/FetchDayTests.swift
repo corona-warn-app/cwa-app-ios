17fe354d27a8dcdc59e76b9f0167b9c8ab5e34b8
Kai Teuber <Kai.Teuber@app-developer.club>
2022-05-05 15:52:11 +0200

    moved and fixed tests

--- src/xcode/ENA/ENA/Source/HTTPClient/Resources/FetchDayResource/__tests_/FetchDayTests.swift
+++ src/xcode/ENA/ENA/Source/HTTPClient/Resources/FetchDayResource/__tests_/FetchDayTests.swift
@@ -42,4 +42,67 @@ class FetchDayTests: CWATestCase {
 		XCTAssertEqual(uniqueIdentifier, knownUniqueIdentifier)
 	}
 
+	// MARK: - Logic
+
+	func testFetchDay_Success() throws {
+		let url = try XCTUnwrap(Bundle(for: type(of: self)).url(forResource: "api-response-day-2020-05-16", withExtension: nil))
+		let stack = MockNetworkStack(
+			httpStatus: 200,
+			headerFields: ["etAg": "\"SomeEtag\""],
+			responseData: try Data(contentsOf: url)
+		)
+
+		let successExpectation = expectation(
+			description: "expect error result"
+		)
+
+		let resource = FetchDayResource(day: "2020-05-01", country: "IT", signatureVerifier: MockVerifier())
+		let restService = RestServiceProvider(session: stack.urlSession, cache: KeyValueCacheFake())
+		restService.load(resource) { result in
+			defer {
+				successExpectation.fulfill()
+			}
+			switch result {
+			case let .success(sapPackage):
+				XCTAssertFalse(sapPackage.isEmpty)
+				XCTAssertNotNil(sapPackage.etag)
+				XCTAssertEqual(sapPackage.package?.bin.count, 501)
+				XCTAssertEqual(sapPackage.package?.signature.count, 144)
+			case let .failure(error):
+				XCTFail("a valid response should never yield and error like: \(error)")
+			}
+		}
+		waitForExpectations(timeout: .medium)
+	}
+
+	func testFetchDay_InvalidPackage() throws {
+		let stack = MockNetworkStack(
+			httpStatus: 200,
+			responseData: Data(bytes: [0xA, 0xB] as [UInt8], count: 2)
+		)
+
+		let successExpectation = expectation(
+			description: "expect error result"
+		)
+
+		let resource = FetchDayResource(day: "2020-05-01", country: "IT", signatureVerifier: MockVerifier())
+		let restService = RestServiceProvider(session: stack.urlSession, cache: KeyValueCacheFake())
+		restService.load(resource) { result in
+			defer {
+				successExpectation.fulfill()
+			}
+			switch result {
+			case .success:
+				XCTFail("An invalid server response should not result in success!")
+			case let .failure(error):
+				if case let .resourceError(resourceError) = error,
+				   case .packageCreation = resourceError {
+				} else {
+					XCTFail("Incorrect error type \(error) received, expected .invalidResponse")
+				}
+			}
+		}
+		waitForExpectations(timeout: .medium)
+	}
+
 }
