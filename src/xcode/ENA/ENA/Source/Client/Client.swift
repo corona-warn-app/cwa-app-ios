35fb918b347d492d73b325898ef6a2144cd1b818
Kai Teuber <Kai.Teuber@app-developer.club>
2022-05-10 14:36:30 +0200

    moved code to file PackageDownloadResponse.swift

--- src/xcode/ENA/ENA/Source/Client/Client.swift
+++ src/xcode/ENA/ENA/Source/Client/Client.swift
@@ -114,29 +114,6 @@ struct FetchTestResultResponse: Codable {
 	}
 }
 
-/// A container for a downloaded `SAPDownloadedPackage` and its corresponding `ETag`, if given.
-struct PackageDownloadResponse: MetaDataProviding {
-
-	let package: SAPDownloadedPackage?
-
-	/// The response ETag
-	///
-	/// This is used to identify and revoke packages.
-	var etag: String? {
-		return metaData.headers.value(caseInsensitiveKey: "ETag")
-	}
-	
-	var isEmpty: Bool {
-		return package == nil
-	}
-
-
-	// MARK: - MetaDataProviding
-
-	var metaData: MetaData = MetaData()
-
-}
-
 /// Combined model for a submit keys request
 struct SubmissionPayload {
 
