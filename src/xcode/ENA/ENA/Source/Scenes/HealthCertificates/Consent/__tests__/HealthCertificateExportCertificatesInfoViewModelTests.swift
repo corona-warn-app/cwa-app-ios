a4f22634f491a058808fc9f2080b31d6caef1bec
Felix Schmidt <io@felixschmidt.software>
2022-06-10 11:37:17 +0200

    13285: Fix Build for Testing

--- src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/Consent/__tests__/HealthCertificateExportCertificatesInfoViewModelTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/Consent/__tests__/HealthCertificateExportCertificatesInfoViewModelTests.swift
@@ -26,14 +26,6 @@ class HealthCertificateExportCertificatesInfoViewModelTests: XCTestCase {
 		XCTAssertEqual(dynamicTableViewModel.numberOfRows(section: 0), 4)
 	}
 	
-	func testGIVEN_Title_THEN_AreCorrect() {
-		// GIVEN
-		let title = sut.title
-		
-		// THEN
-		XCTAssertEqual(title, AppStrings.HealthCertificate.ExportCertificatesInfo.title)
-	}
-	
 	func testGIVEN_HidesCloseButton_THEN_ShouldBeFalse() {
 		// GIVEN
 		let hidesCloseButton = sut.hidesCloseButton
