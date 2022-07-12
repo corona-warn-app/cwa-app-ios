bbbcf0e7da1cc4fca1c13653cc8c137c05643b93
Naveed Khalid <nkhalid.here@gmail.com>
2022-07-12 17:28:51 +0200

    updating unit test

--- src/xcode/ENA/ENA/Source/Extensions/__tests__/FileManager+TemporaryDirectoryTests.swift
+++ src/xcode/ENA/ENA/Source/Extensions/__tests__/FileManager+TemporaryDirectoryTests.swift
@@ -9,27 +9,57 @@ import PDFKit
 class FileManager_TemporaryDirectoryTests: CWATestCase {
 	
 	func testPDFRemoval() throws {
-		let pdfView = PDFView()
-		guard let data = pdfView.document?.dataRepresentation() else {
-			XCTFail("Could not create data representation of pdf to print")
-			return
+		// removing all previous files
+		let temporaryDirectoryURL = FileManager.default.temporaryDirectory
+		let temporaryDirectory = try FileManager.default.contentsOfDirectory(atPath: temporaryDirectoryURL.path)
+		try temporaryDirectory.forEach { file in
+			let fileUrl = temporaryDirectoryURL.appendingPathComponent(file)
+			try FileManager.default.removeItem(atPath: fileUrl.path)
 		}
+		
+		// creating a dummy pdf document
+		let format = UIGraphicsPDFRendererFormat()
+		let metaData = [
+			kCGPDFContextTitle: "Hello, World!",
+			kCGPDFContextAuthor: "Naveed Khalid"
+		  ]
+		format.documentInfo = metaData as [String: Any]
+		
+		let pageRect = CGRect(x: 0, y: 0, width: 595, height: 842)
+		let renderer = UIGraphicsPDFRenderer(bounds: pageRect, format: format)
+		let data = renderer.pdfData { context in
+		  context.beginPage()
+		  
+		  let paragraphStyle = NSMutableParagraphStyle()
+		  paragraphStyle.alignment = .center
+		  let attributes = [
+			NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 14),
+			NSAttributedString.Key.paragraphStyle: paragraphStyle
+		  ]
+		  let text = "Hello, World!"
+		  let textRect = CGRect(x: 100, y: 100, width: 200, height: 20)
 
-		let temporaryFolder = FileManager.default.temporaryDirectory
+		  text.draw(in: textRect, withAttributes: attributes)
+		}
+		
+		// naming the pdf file and writing the content
 		let pdfFileName = "healthCertificates.pdf"
-		let pdfFileURL = temporaryFolder.appendingPathComponent(pdfFileName)
+		let pdfFileURL = temporaryDirectoryURL.appendingPathComponent(pdfFileName)
 		
 		do {
 			try data.write(to: pdfFileURL)
 		} catch {
-			XCTFail("Could not write the template data to the pdf file")
+			XCTFail("Could not write content to pdf file")
 		}
 		
-		XCTAssertEqual(temporaryFolder.pathComponents.count, 1)
+		var updatedTemporaryDirectory = try FileManager.default.contentsOfDirectory(atPath: temporaryDirectoryURL.path)
+		XCTAssertEqual(updatedTemporaryDirectory.count, 1)
 		
 		FileManager.default.removePDFsFromTemporaryDirectory()
 		
-		XCTAssertEqual(temporaryFolder.pathComponents.count, 0)
+		updatedTemporaryDirectory = try FileManager.default.contentsOfDirectory(atPath: temporaryDirectoryURL.path)
+		
+		XCTAssertEqual(updatedTemporaryDirectory.count, 0)
 	}
 	
 }
