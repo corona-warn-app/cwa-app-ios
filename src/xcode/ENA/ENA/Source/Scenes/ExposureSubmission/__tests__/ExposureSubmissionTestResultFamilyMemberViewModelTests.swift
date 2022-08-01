7c380f9108a7604b8e647e29786c20bcb8756a1d
puneetmahali1 <puneet.mahali@sap.com>
2022-08-01 12:26:03 +0200

    Fix ExposureSubmissionTestResult test case

--- src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/__tests__/ExposureSubmissionTestResultFamilyMemberViewModelTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/__tests__/ExposureSubmissionTestResultFamilyMemberViewModelTests.swift
@@ -327,7 +327,7 @@ class ExposureSubmissionTestResultFamilyMemberViewModelTests: CWATestCase {
 		
 		let section = model.dynamicTableViewModelPublisher.value.section(1)
 		let cells = section.cells
-		XCTAssertEqual(cells.count, 10)
+		XCTAssertEqual(cells.count, 9)
 		
 		let firstItem = cells[0]
 		var id = firstItem.cellReuseIdentifier
@@ -339,15 +339,15 @@ class ExposureSubmissionTestResultFamilyMemberViewModelTests: CWATestCase {
 		
 		let thirdItem = cells[2]
 		id = thirdItem.cellReuseIdentifier
-		XCTAssertEqual(id.rawValue, "labelCell")
+		XCTAssertEqual(id.rawValue, "stepCell")
 		
 		let fourthItem = cells[3]
 		id = fourthItem.cellReuseIdentifier
 		XCTAssertEqual(id.rawValue, "stepCell")
 		
-		let fifthItem = cells[4]
+		let  fifthItem = cells[4]
 		id = fifthItem.cellReuseIdentifier
-		XCTAssertEqual(id.rawValue, "stepCell")
+		XCTAssertEqual(id.rawValue, "labelCell")
 		
 		let sixthItem = cells[5]
 		id = sixthItem.cellReuseIdentifier
@@ -355,18 +355,14 @@ class ExposureSubmissionTestResultFamilyMemberViewModelTests: CWATestCase {
 		
 		let seventhItem = cells[6]
 		id = seventhItem.cellReuseIdentifier
-		XCTAssertEqual(id.rawValue, "labelCell")
+		XCTAssertEqual(id.rawValue, "bulletPointCell")
 		
 		let eigthItem = cells[7]
 		id = eigthItem.cellReuseIdentifier
 		XCTAssertEqual(id.rawValue, "bulletPointCell")
-		
+
 		let ninthItem = cells[8]
 		id = ninthItem.cellReuseIdentifier
-		XCTAssertEqual(id.rawValue, "bulletPointCell")
-
-		let tenthItem = cells[9]
-		id = tenthItem.cellReuseIdentifier
 		XCTAssertEqual(id.rawValue, "bulletPointCell")
 	}
 	
