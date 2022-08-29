86a90c75be11aeb1465823994e225c39dc9d9fc1
Felix Schmidt <io@felixschmidt.software>
2022-08-29 17:09:43 +0200

    13820: Configure the content elements layout.

--- src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/HealthCertifiedPerson/Cells/MaskState/MaskStateTableViewCell.swift
+++ src/xcode/ENA/ENA/Source/Scenes/HealthCertificates/HealthCertifiedPerson/Cells/MaskState/MaskStateTableViewCell.swift
@@ -27,17 +27,99 @@ class MaskStateTableViewCell: UITableViewCell, UITextViewDelegate, ReuseIdentifi
 	
 	// MARK: - Private
 	
+	/// The main container for any elements
 	private let backgroundContainerView: UIView = {
-		let view = UIView()
-		view.backgroundColor = .enaColor(for: .cellBackground2)
-		view.layer.borderColor = .enaColor(for: .hairline)
+		let backgroundContainerView = UIView()
+		backgroundContainerView.backgroundColor = .enaColor(for: .cellBackground2)
+		backgroundContainerView.layer.borderColor = .enaColor(for: .hairline)
 		if #available(iOS 13.0, *) {
-			view.layer.cornerCurve = .continuous
+			backgroundContainerView.layer.cornerCurve = .continuous
 		}
-		view.layer.cornerRadius = 15
-		view.layer.masksToBounds = true
-		view.translatesAutoresizingMaskIntoConstraints = false
-		return view
+		backgroundContainerView.layer.cornerRadius = 15
+		backgroundContainerView.layer.masksToBounds = true
+
+		return backgroundContainerView
+	}()
+	
+	/// The container for all content elements
+	private let contentStackView: UIStackView = {
+		let contentStackView = UIStackView()
+		contentStackView.axis = .vertical
+		contentStackView.alignment = .fill
+		contentStackView.spacing = 6
+
+		return contentStackView
+	}()
+	
+	/// The container for upper elements like titles or badge
+	private let topElementsStackView: UIStackView = {
+		let topElementsStackView = UIStackView()
+		topElementsStackView.axis = .horizontal
+		topElementsStackView.alignment = .top
+		topElementsStackView.distribution = .equalSpacing
+		return topElementsStackView
+	}()
+	
+	/// The container to all title elements
+	private let titlesStackView: UIStackView = {
+		let titleStackView = UIStackView()
+		titleStackView.axis = .vertical
+		titleStackView.alignment = .leading
+		
+		return titleStackView
+	}()
+	
+	/// The title element
+	private let titleLabel: ENALabel = {
+		let titleLabel = ENALabel(style: .headline)
+		titleLabel.numberOfLines = 0
+		titleLabel.textColor = .enaColor(for: .textPrimary1)
+		titleLabel.setContentCompressionResistancePriority(.required, for: .horizontal)
+
+		return titleLabel
+	}()
+	
+	/// The subtitle element
+	private let subtitleLabel: ENALabel = {
+		let subtitleLabel = ENALabel(style: .body)
+		subtitleLabel.numberOfLines = 0
+		subtitleLabel.textColor = .enaColor(for: .textPrimary2)
+
+		return subtitleLabel
+	}()
+	
+	/// The badge icon element (mask status)
+	private var badgeImageView: UIImageView = {
+		let badgeImageView = UIImageView()
+		badgeImageView.contentMode = .scaleAspectFit
+		
+		return badgeImageView
+	}()
+	
+	/// The description text element
+	private let descriptionLabel: ENALabel = {
+		let descriptionLabel = ENALabel(style: .body)
+		descriptionLabel.numberOfLines = 0
+
+		return descriptionLabel
+	}()
+	
+	/// The FAQ link element
+	private lazy var faqLinkTextView: UITextView = {
+		let faqLinkTextView = UITextView()
+		faqLinkTextView.isScrollEnabled = false
+		faqLinkTextView.isEditable = false
+		faqLinkTextView.textContainerInset = .zero
+		faqLinkTextView.textContainer.lineFragmentPadding = .zero
+		faqLinkTextView.textColor = .enaColor(for: .textPrimary1)
+		faqLinkTextView.tintColor = .enaColor(for: .textTint)
+		faqLinkTextView.accessibilityTraits = .button
+		faqLinkTextView.linkTextAttributes = [
+			.foregroundColor: UIColor.enaColor(for: .textTint),
+			.underlineColor: UIColor.clear
+		]
+		
+		return faqLinkTextView
 	}()
 	
 	private func setupView() {
@@ -45,14 +127,38 @@ class MaskStateTableViewCell: UITableViewCell, UITextViewDelegate, ReuseIdentifi
 		contentView.backgroundColor = .clear
 		selectionStyle = .none
 		
+		[
+			backgroundContainerView,
+			contentStackView,
+			topElementsStackView,
+			titleLabel
+		].forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
+		
 		contentView.addSubview(backgroundContainerView)
+		backgroundContainerView.addSubview(contentStackView)
+		contentStackView.addArrangedSubview(topElementsStackView)
+		topElementsStackView.addArrangedSubview(titlesStackView)
+		topElementsStackView.addArrangedSubview(badgeImageView)
+		titlesStackView.addArrangedSubview(titleLabel)
+		titlesStackView.addArrangedSubview(subtitleLabel)
+		contentStackView.addArrangedSubview(descriptionLabel)
+		contentStackView.addArrangedSubview(faqLinkTextView)
+		
+		contentStackView.setCustomSpacing(16, after: descriptionLabel)
 		
 		NSLayoutConstraint.activate([
 			backgroundContainerView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 4),
 			backgroundContainerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -4),
 			backgroundContainerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
 			backgroundContainerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-			backgroundContainerView.heightAnchor.constraint(equalToConstant: 200) // TODO: Remove, just for test
+			
+			contentStackView.topAnchor.constraint(equalTo: backgroundContainerView.topAnchor, constant: 16),
+			contentStackView.bottomAnchor.constraint(equalTo: backgroundContainerView.bottomAnchor, constant: -16),
+			contentStackView.leadingAnchor.constraint(equalTo: backgroundContainerView.leadingAnchor, constant: 16),
+			contentStackView.trailingAnchor.constraint(equalTo: backgroundContainerView.trailingAnchor, constant: -16),
+			
+			badgeImageView.widthAnchor.constraint(equalToConstant: 51),
+			badgeImageView.heightAnchor.constraint(equalToConstant: 31)
 		])
 	}
 }
