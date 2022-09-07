61b83398387fc2466d299d117b91ba073e7cf031
Omar Ahmed <omarabdelaziz125@gmail.com>
2022-09-07 02:07:24 +0200

    fix warning

--- src/xcode/ENA/ENA/Source/Services/OTP/__tests__/OTPServiceTests.swift
+++ src/xcode/ENA/ENA/Source/Services/OTP/__tests__/OTPServiceTests.swift
@@ -5,8 +5,8 @@
 import XCTest
 @testable import ENA
 
-// swiftlint:disable:next type_body_length
 // swiftlint:disable file_length
+// swiftlint:disable:next type_body_length
 class OTPServiceTests: CWATestCase {
 
 	// MARK: - getValidOTPEdus
