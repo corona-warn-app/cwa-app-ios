5dc75f2b63b3126c6fa315403745f81121a9a296
Naveed Khalid <nkhalid.here@gmail.com>
2022-09-08 16:33:42 +0200

    fixing test plans and disabling flaky ones

--- src/xcode/ENA/ENAUITests/HealthCertificate/ENAUITests_13_CreateHealthCertificate.swift
+++ src/xcode/ENA/ENAUITests/HealthCertificate/ENAUITests_13_CreateHealthCertificate.swift
@@ -364,6 +364,28 @@ class ENAUITests_13_CreateHealthCertificate: CWATestCase {
 		app.swipeUp(velocity: .slow)
 	}
 	
+	func test_HealthCertificate_FederalState_Flow() throws {
+		app.setLaunchArgument(LaunchArguments.healthCertificate.firstAndSecondHealthCertificate, to: true)
+		app.setLaunchArgument(LaunchArguments.healthCertificate.isDCCAdmissionCheckScenariosEnabled, to: true)
+		app.setLaunchArgument(LaunchArguments.infoScreen.healthCertificateInfoScreenShown, to: true)
+		app.launch()
+
+		app.buttons[AccessibilityIdentifiers.TabBar.certificates].waitAndTap()
+
+		// Check if the selection button exists
+		let selectionButtonCell = try XCTUnwrap(app.cells[AccessibilityIdentifiers.HealthCertificate.Overview.changeAdmissionScenarioCell])
+		
+		// Navigate to the scenario selection screen
+		selectionButtonCell.waitAndTap()
+
+		// Select federal state Berlin
+		app.cells.element(boundBy: 2).waitAndTap()
+		
+		// Check if the button with label Berlin exists
+		let selectedStateButton = try XCTUnwrap(app.buttons[app.localized("FederalState_Berlin")])
+		XCTAssertTrue(selectedStateButton.waitForExistence(timeout: .medium))
+	}
+	
 	// MARK: - Screenshots
 	
 	func test_screenshot_shownConsentScreenAndDisclaimer() throws {
@@ -438,28 +460,6 @@ class ENAUITests_13_CreateHealthCertificate: CWATestCase {
 		let certificateCells = try XCTUnwrap(app.cells[AccessibilityIdentifiers.HealthCertificate.Person.certificateCell])
 		XCTAssertTrue(certificateCells.waitForExistence(timeout: .short))
 		XCTAssertEqual(app.cells.matching(identifier: AccessibilityIdentifiers.HealthCertificate.Person.certificateCell).count, 2)
-	}
-
-	func test_HealthCertificate_FederalState_Flow() throws {
-		app.setLaunchArgument(LaunchArguments.healthCertificate.firstAndSecondHealthCertificate, to: true)
-		app.setLaunchArgument(LaunchArguments.healthCertificate.isDCCAdmissionCheckScenariosEnabled, to: true)
-		app.setLaunchArgument(LaunchArguments.infoScreen.healthCertificateInfoScreenShown, to: true)
-		app.launch()
-
-		app.buttons[AccessibilityIdentifiers.TabBar.certificates].waitAndTap()
-
-		// Check if the selection button exists
-		let selectionButtonCell = try XCTUnwrap(app.cells[AccessibilityIdentifiers.HealthCertificate.Overview.changeAdmissionScenarioCell])
-		
-		// Navigate to the scenario selection screen
-		selectionButtonCell.waitAndTap()
-
-		// Select federal state Berlin
-		app.cells.element(boundBy: 2).waitAndTap()
-		
-		// Check if the button with label Berlin exists
-		let selectedStateButton = try XCTUnwrap(app.buttons[app.localized("FederalState_Berlin")])
-		XCTAssertTrue(selectedStateButton.waitForExistence(timeout: .medium))
 	}
 	
 	func test_screenshot_ExportCertificatesTooltip() throws {
