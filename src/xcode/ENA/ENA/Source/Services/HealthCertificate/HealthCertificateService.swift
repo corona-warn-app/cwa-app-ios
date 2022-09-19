64761c152d54092e8b353a5ebc097e6c773c5fc2
Naveed Khalid <nkhalid.here@gmail.com>
2022-09-19 14:01:44 +0200

    checking for change in version

--- src/xcode/ENA/ENA/Source/Services/HealthCertificate/HealthCertificateService.swift
+++ src/xcode/ENA/ENA/Source/Services/HealthCertificate/HealthCertificateService.swift
@@ -123,6 +123,10 @@ class HealthCertificateService: HealthCertificateServiceServable {
 		return allDatesToExam.min()
 	}
 
+	var isVersionChanged: Bool {
+		return store.appVersion != Bundle.main.appVersion
+	}
+	
 	func setup(
 		updatingWalletInfos: Bool,
 		completion: @escaping () -> Void
@@ -407,14 +411,15 @@ class HealthCertificateService: HealthCertificateServiceServable {
 				return
 			}
 			let dispatchGroup = DispatchGroup()
-			for person in self.healthCertifiedPersons where (configurationDidChange || person.needsDCCWalletInfoUpdate || isForced) {
+			for person in self.healthCertifiedPersons where (configurationDidChange || person.needsDCCWalletInfoUpdate || self.isVersionChanged || isForced) {
 				dispatchGroup.enter()
 				self.updateDCCWalletInfo(for: person) {
 					dispatchGroup.leave()
 				}
 			}
 
-			dispatchGroup.notify(queue: .global()) {
+			dispatchGroup.notify(queue: .global()) { [weak self] in
+				self?.store.appVersion = Bundle.main.appVersion
 				completion?()
 			}
 		}
