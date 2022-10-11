787222c682a2e064121d9f946f685b5fcb10daf0
Omar Ahmed <omarabdelaziz125@gmail.com>
2022-10-11 14:41:09 +0200

    update scrolling behaviour

--- src/xcode/ENA/ENA/Source/Scenes/Home/Cells/Statistics/HomeStatisticsCellModel.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Home/Cells/Statistics/HomeStatisticsCellModel.swift
@@ -31,9 +31,11 @@ class HomeStatisticsCellModel {
 			.store(in: &subscriptions)
 		
 		localStatisticsProvider.regionStatisticsData
-			.sink { [weak self] regionStatisticsData in
-				  self?.regionStatisticsData = regionStatisticsData
-				  Log.debug("Updating local statistics cell model. \(private: "\(self?.regionStatisticsData.count ?? -1)")", log: .localStatistics)
+			.sink { [weak self] newRegionStatisticsData in
+				guard let self = self else { return }
+				self.hasFreshRegion = newRegionStatisticsData.count > self.regionStatisticsData.count
+				self.regionStatisticsData = newRegionStatisticsData
+				Log.debug("Updating local statistics cell model. \(private: "\(self.regionStatisticsData.count ?? -1)")", log: .localStatistics)
 			}
 			.store(in: &subscriptions)
 		
@@ -48,10 +50,12 @@ class HomeStatisticsCellModel {
 	// MARK: - Internal
 	
 	/// The default set of 'global' statistics for every user
+	private(set) var hasFreshRegion = false
+	
 	@DidSetPublished private(set) var linkCards = [SAP_Internal_Stats_LinkCard]()
 	@DidSetPublished private(set) var keyFigureCards = [SAP_Internal_Stats_KeyFigureCard]()
 	@DidSetPublished private(set) var regionStatisticsData = [RegionStatisticsData]()
-
+	
 	func add(_ region: LocalStatisticsRegion) {
 		localStatisticsProvider.add(region)
 	}
