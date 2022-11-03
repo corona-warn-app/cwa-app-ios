fb9cc4829d9c0fd6c5bcf74e47bea723e074b028
Felix Schmidt <io@felixschmidt.software>
2022-11-03 18:01:00 +0100

    14274: Move extension to own Swift file.

--- src/xcode/ENA/ENA/Source/Scenes/Home/Cells/TestRegistration/HomeTestRegistrationTableViewCell.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Home/Cells/TestRegistration/HomeTestRegistrationTableViewCell.swift
@@ -95,16 +95,3 @@ final class HomeTestRegistrationTableViewCell: UITableViewCell {
 	}
 
 }
-
-extension UIView {
-   func roundCorners(corners: UIRectCorner, radius: CGFloat) {
-		let path = UIBezierPath(
-			roundedRect: bounds,
-			byRoundingCorners: corners,
-			cornerRadii: CGSize(width: radius, height: radius)
-		)
-		let mask = CAShapeLayer()
-		mask.path = path.cgPath
-		layer.mask = mask
-	}
-}
