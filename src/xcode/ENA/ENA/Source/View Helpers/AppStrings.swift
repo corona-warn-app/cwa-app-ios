b165d3330f639f12dfe48f6d9511ba68e9deb48f
Omar Ahmed <omarabdelaziz125@gmail.com>
2022-11-08 13:42:51 +0100

    refactoring

--- src/xcode/ENA/ENA/Source/View Helpers/AppStrings.swift
+++ src/xcode/ENA/ENA/Source/View Helpers/AppStrings.swift
@@ -335,7 +335,7 @@ enum AppStrings {
 		
 		static let legalHeadline = NSLocalizedString("SRS_Consent_Legal_Headline_1", tableName: "Localizable.legal", comment: "")
 
-		static let legalDescription = NSLocalizedString("SRS_Description_Legal_Text", tableName: "Localizable.legal", comment: "")
+		static let legalDescription = NSLocalizedString("SRS_Consent_Legal_Description_Text", tableName: "Localizable.legal", comment: "")
 		static let legalBulletPoint01 = NSLocalizedString("SRS_Consent_Legal_Text_1", tableName: "Localizable.legal", comment: "")
 		static let legalBulletPoint02 = NSLocalizedString("SRS_Consent_Legal_Text_2", tableName: "Localizable.legal", comment: "")
 		static let legalBulletPoint03 = NSLocalizedString("SRS_Consent_Legal_Text_3", tableName: "Localizable.legal", comment: "")
