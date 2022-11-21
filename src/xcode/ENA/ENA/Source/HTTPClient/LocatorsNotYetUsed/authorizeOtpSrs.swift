1f150ed2ff68b983ffcafea41b2122f1a44fa75c
Omar Ahmed <omarabdelaziz125@gmail.com>
2022-11-21 08:33:29 +0100

    add locator

--- src/xcode/ENA/ENA/Source/HTTPClient/LocatorsNotYetUsed/authorizeOtpSrs.swift
+++ src/xcode/ENA/ENA/Source/HTTPClient/LocatorsNotYetUsed/authorizeOtpSrs.swift
@@ -3,3 +3,25 @@
 //
 
 import Foundation
+
+extension Locator {
+    
+    // send:	ProtoBuf SAP_Internal_Ppdd_SRSOneTimePasswordRequestIOS
+    // receive:	JSON
+    // type:	default
+    // comment:
+    static func authorizeOtpSrs(
+        isFake: Bool
+    ) -> Locator {
+        let fake = String(isFake ? 1 : 0)
+        return Locator(
+            endpoint: .dataDonation,
+            paths: ["version", "v1", "ios", "srs"],
+            method: .post,
+            defaultHeaders: [
+                "Content-Type": "application/x-protobuf",
+                "cwa-fake": fake
+            ]
+        )
+    }
+}
