ea19227e16e0d6bc55138be42a79f6a9923f3e36
Omar Ahmed <omarabdelaziz125@gmail.com>
2022-11-22 10:17:18 +0100

    update srsError

--- src/xcode/ENA/ENA/Source/Services/PPAccessControl/Model/SRSError.swift
+++ src/xcode/ENA/ENA/Source/Services/PPAccessControl/Model/SRSError.swift
@@ -14,61 +14,41 @@ enum SRSError: Error {
 	case srsOTP400
 	case srsOTP401
 	case srsOTP403
-    
-    case otherServerError
-    case apiTokenAlreadyIssued
-    case apiTokenExpired
-    case apiTokenQuotaExceeded
-    case deviceTokenInvalid
-    case deviceTokenRedeemed
-    case deviceTokenSyntaxError
-    
+
 	case srsSUBClientError
 	case srsSUBNoNetwork
 	case srsSUBServerError
 	case srsSUB400
 	case srsSUB403
 	
-	var description: String {
-		switch self {
-		case .ppacError(let ppacError):
-			return "ppacError: \(ppacError.description)"
-		case .srsOTPClientError:
-			return "SRS_OTP_CLIENT_ERROR"
-		case .srsOTPNetworkError:
-			return "SRS_OTP_NO_NETWORK"
-		case .srsOTPServerError:
-			return "SRS_OTP_SERVER_ERROR"
-		case .srsOTP400:
-			return "SRS_OTP_400"
-		case .srsOTP401:
-			return "SRS_OTP_401"
-		case .srsOTP403:
-			return "SRS_OTP_403"
-        case .otherServerError:
-            return "otherServerError"
-        case .apiTokenAlreadyIssued:
-            return "apiTokenAlreadyIssued"
-        case .apiTokenExpired:
-            return "apiTokenExpired"
-        case .apiTokenQuotaExceeded:
-            return "apiTokenQuotaExceeded"
-        case .deviceTokenInvalid:
-            return "deviceTokenInvalid"
-        case .deviceTokenRedeemed:
-            return "deviceTokenRedeemed"
-        case .deviceTokenSyntaxError:
-            return "deviceTokenSyntaxError"
-		case .srsSUBClientError:
-			return "SRS_SUB_CLIENT_ERROR"
-		case .srsSUBNoNetwork:
-			return "SRS_SUB_NO_NETWORK"
-		case .srsSUBServerError:
-			return "SRS_SUB_SERVER_ERROR"
-		case .srsSUB400:
-			return "SRS_SUB_400"
-		case .srsSUB403:
-			return "SRS_SUB_403"
-		}
-	}
+    var description: String {
+        switch self {
+        case .ppacError(let ppacError):
+            return "ppacError: \(ppacError.description)"
+        case .otpError(let otpError):
+            return "otpError: \(otpError.description)"
+        case .srsOTPClientError:
+            return "SRS_OTP_CLIENT_ERROR"
+        case .srsOTPNetworkError:
+            return "SRS_OTP_NO_NETWORK"
+        case .srsOTPServerError:
+            return "SRS_OTP_SERVER_ERROR"
+        case .srsOTP400:
+            return "SRS_OTP_400"
+        case .srsOTP401:
+            return "SRS_OTP_401"
+        case .srsOTP403:
+            return "SRS_OTP_403"
+        case .srsSUBClientError:
+            return "SRS_SUB_CLIENT_ERROR"
+        case .srsSUBNoNetwork:
+            return "SRS_SUB_NO_NETWORK"
+        case .srsSUBServerError:
+            return "SRS_SUB_SERVER_ERROR"
+        case .srsSUB400:
+            return "SRS_SUB_400"
+        case .srsSUB403:
+            return "SRS_SUB_403"
+        }
+    }
 }
