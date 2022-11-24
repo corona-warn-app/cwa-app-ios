d4fb9eb3c9123eb7a8e0b2b8e8522e31d8d202df
Felix Schmidt <io@felixschmidt.software>
2022-11-24 13:52:25 +0100

    14315: Remove a developer mock

--- src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/ExposureSubmissionCoordinatorModel.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/ExposureSubmissionCoordinatorModel.swift
@@ -423,8 +423,6 @@ class ExposureSubmissionCoordinatorModel {
 		isLoading: @escaping CompletionBool,
 		completion: @escaping (Result<Void, SRSPreconditionError>) -> Void
 	) {
-		completion(.success(()))
-		return
 		// Check if app was installed less than 48 hours
 		guard
 			let appInstallationDate = store.appInstallationDate,
