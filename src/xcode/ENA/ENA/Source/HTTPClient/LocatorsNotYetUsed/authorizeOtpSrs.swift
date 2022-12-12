574f65c74ccb8b236d992187c42b1bd21650bdd9
Naveed Khalid <nkhalid.here@gmail.com>
2022-12-12 15:13:33 +0100

    adding another locator

--- src/xcode/ENA/ENA/Source/HTTPClient/LocatorsNotYetUsed/authorizeOtpSrs.swift
+++ src/xcode/ENA/ENA/Source/HTTPClient/LocatorsNotYetUsed/authorizeOtpSrs.swift
@@ -12,19 +12,33 @@ extension Locator {
     // comment: the endpoint for otp authorization for SRS
     static func authorizeOtpSrs(
 		forceApiTokenHeader: Bool = false,
-        isFake: Bool
+		isFake: Bool
     ) -> Locator {
         let fake = String(isFake ? 1 : 0)
 		let forceApiHeader = String(forceApiTokenHeader ? 1 : 0)
-        return Locator(
-            endpoint: .dataDonation,
-            paths: ["version", "v1", "ios", "srs"],
-            method: .post,
-            defaultHeaders: [
-                "Content-Type": "application/x-protobuf",
-                "cwa-fake": fake,
+		
+		#if !RELEASE
+		return Locator(
+			endpoint: .dataDonation,
+			paths: ["version", "v1", "ios", "srs"],
+			method: .post,
+			defaultHeaders: [
+			   "Content-Type": "application/x-protobuf",
+			   "cwa-fake": fake,
+			   "cwa-ppac-ios-accept-api-token": forceApiHeader
+			]
+		)
+		#else
+		return Locator(
+			endpoint: .dataDonation,
+			paths: ["version", "v1", "ios", "srs"],
+			method: .post,
+			defaultHeaders: [
+				"Content-Type": "application/x-protobuf",
+				"cwa-fake": fake,
 				"cwa-ppac-ios-accept-api-token": forceApiHeader
-            ]
-        )
+			]
+		)
+		#endif
     }
 }
