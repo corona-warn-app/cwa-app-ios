0c8ed3c09d499ce75a6f87788dd761bb94787618
Naveed Khalid <nkhalid.here@gmail.com>
2023-01-25 14:19:20 +0100

    changes to AppClosureNoticeDetailsViewController and AppClosureNoticeDetailsViewModel

--- src/xcode/ENA/ENA/Source/Scenes/AppClosureNoticeDetails/AppClosureNoticeDetailsViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/AppClosureNoticeDetails/AppClosureNoticeDetailsViewController.swift
@@ -3,3 +3,63 @@
 //
 
 import Foundation
+import UIKit
+
+class AppClosureNoticeDetailsViewController: DynamicTableViewController, DismissHandling {
+
+	// MARK: - Init
+
+	init(
+		viewModel: AppClosureNoticeDetailsViewModel,
+		dismiss: @escaping () -> Void
+	) {
+		self.viewModel = viewModel
+		self.dismiss = dismiss
+
+		super.init(nibName: nil, bundle: nil)
+	}
+
+	@available(*, unavailable)
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	// MARK: - Overrides
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		navigationItem.rightBarButtonItem = dismissHandlingCloseBarButton
+
+		setupTableView()
+	}
+	
+	// MARK: - Protocol DismissHandling
+
+	func wasAttemptedToBeDismissed() {
+		dismiss()
+	}
+	
+	// MARK: - Private
+
+	private let dismiss: () -> Void
+	private let viewModel: AppClosureNoticeDetailsViewModel
+
+	private func setupTableView() {
+		view.backgroundColor = .enaColor(for: .background)
+		tableView.separatorStyle = .none
+		
+		if traitCollection.userInterfaceStyle == .dark {
+			navigationController?.navigationBar.tintColor = .enaColor(for: .textContrast)
+		} else {
+			navigationController?.navigationBar.tintColor = .enaColor(for: .tint)
+		}
+		
+		tableView.register(
+			UINib(nibName: "ExposureDetectionLinkCell", bundle: nil),
+			forCellReuseIdentifier: ExposureDetectionViewController.ReusableCellIdentifier.link.rawValue
+		)
+		
+		dynamicTableViewModel = viewModel.dynamicTableViewModel
+	}
+}
--- src/xcode/ENA/ENA/Source/Scenes/AppClosureNoticeDetails/AppClosureNoticeDetailsViewModel.swift
+++ src/xcode/ENA/ENA/Source/Scenes/AppClosureNoticeDetails/AppClosureNoticeDetailsViewModel.swift
@@ -3,3 +3,69 @@
 //
 
 import Foundation
+import UIKit
+
+class AppClosureNoticeDetailsViewModel {
+	
+	init (
+		cclService: CCLServable,
+		statusTabNotice: StatusTabNotice
+	) {
+		self.cclService = cclService
+		self.statusTabNotice = statusTabNotice
+	}
+
+	// MARK: - Internal
+
+	var dynamicTableViewModel: DynamicTableViewModel {
+		var cells: [DynamicCell] = []
+		
+		if let titleText = statusTabNotice.titleText?.localized(cclService: cclService), !titleText.isEmpty {
+			cells.append(
+				.title1(text: titleText)
+			)
+		}
+		
+		if let subtitleText = statusTabNotice.subtitleText?.localized(cclService: cclService), !subtitleText.isEmpty {
+			cells.append(
+				.subheadline(text: subtitleText, color: .enaColor(for: .textPrimary2)) { _, cell, _ in
+					cell.contentView.preservesSuperviewLayoutMargins = false
+					
+					cell.contentView.layoutMargins.top = 0
+				}
+			)
+		}
+
+		if let longText = statusTabNotice.longText?.localized(cclService: cclService), !longText.isEmpty {
+			cells.append(
+				.body(text: longText)
+			)
+		}
+		
+		if let faqAnchor = statusTabNotice.faqAnchor, !faqAnchor.isEmpty {
+			cells.append(
+				.link(
+					text: AppStrings.HealthCertificate.Person.faq,
+					url: URL(string: LinkHelper.urlString(suffix: faqAnchor, type: .faq))
+				)
+			)
+		}
+		
+		return DynamicTableViewModel.with {
+			$0.add(
+				.section(
+					header: .image(
+						UIImage(named: "Illu_AppClosure_Notice"),
+						accessibilityIdentifier: AccessibilityIdentifiers.BoosterNotification.Details.image
+					),
+					cells: cells
+				)
+			)
+		}
+	}
+	
+	// MARK: - Private
+	
+	private let cclService: CCLServable
+	private let statusTabNotice: StatusTabNotice
+}
