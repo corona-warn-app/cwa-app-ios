a9cb060ed2debc70ab5ed42bd320015a647eeb29
Naveed Khalid <nkhalid.here@gmail.com>
2023-01-27 16:07:01 +0100

    setup for UI test

--- src/xcode/ENA/ENA/Source/Services/CCLService/CCLService.swift
+++ src/xcode/ENA/ENA/Source/Services/CCLService/CCLService.swift
@@ -213,6 +213,12 @@ class CCLService: CCLServable {
 	}
 	
 	func statusTabNotice() -> Swift.Result<StatusTabNotice, StatusTabNoticeAccessError> {
+		#if DEBUG
+		if isUITesting {
+			return .success(mockStatusTabNotice)
+		}
+		#endif
+		
 		let getStatusTabNoticeInput = GetStatusTabNoticeInput.make()
 		
 		do {
@@ -309,6 +315,39 @@ class CCLService: CCLServable {
 	private var isSetUp = false
 	
 	#if DEBUG
+	private var mockStatusTabNotice: StatusTabNotice {
+		let titleText = DCCUIText(
+			type: "string",
+			quantity: nil,
+			quantityParameterIndex: nil,
+			functionName: nil,
+			localizedText: ["de": "Betriebsende"],
+			parameters: []
+		)
+
+		let subtitleText = DCCUIText(
+			type: "string",
+			quantity: nil,
+			quantityParameterIndex: nil,
+			functionName: nil,
+			localizedText: ["de": "Der Betrieb der Corona-Warn-App wird am xx.xx.xxxx eingestellt."],
+			parameters: []
+		)
+		
+		let longText = DCCUIText(
+			type: "string",
+			quantity: nil,
+			quantityParameterIndex: nil,
+			functionName: nil,
+			localizedText: ["de": "Sie erhalten dann keine Warnungen mehr über Risikobegegnungen und können selbst andere nicht mehr warnen. Sie können keine Tests mehr registrieren und erhalten keine Testergebnisse mehr über die App. Auf Ihre Zertifikate und das Kontakt-Tagebuch haben Sie weiterhin Zugriff. Allerdings können Sie keine neuen Zertifikate mehr hinzufügen."],
+			parameters: []
+		)
+		
+		let faqText = "Mehr Informationen finden Sie in den FAQ."
+
+		return StatusTabNotice(visible: true, titleText: titleText, subtitleText: subtitleText, longText: longText, faqAnchor: faqText)
+	}
+	
 	private var mockDCCAdmissionCheckScenarios: DCCAdmissionCheckScenarios {
 		let statusTitle = DCCUIText(
 			type: "string",
