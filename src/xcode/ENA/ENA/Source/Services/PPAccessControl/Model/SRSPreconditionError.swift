15497fde197135e8a52ca9c88a2c1bb48e199b10
Felix Schmidt <io@felixschmidt.software>
2023-02-10 14:03:17 +0100

    14480: Show new warn alert text for SRS precondition error MIN_TIME_SINCE_ONBOARDING.
    # Conflicts:
    #	src/xcode/ENA/ENA/Resources/Localization/de.lproj/Localizable.strings

--- src/xcode/ENA/ENA/Source/Services/PPAccessControl/Model/SRSPreconditionError.swift
+++ src/xcode/ENA/ENA/Source/Services/PPAccessControl/Model/SRSPreconditionError.swift
@@ -8,7 +8,7 @@ enum SRSPreconditionError: Error {
 	case deviceTimeError(PPACError)
 
 	/// Precondition: the app was installed less than 48h
-	case insufficientAppUsageTime
+	case insufficientAppUsageTime(timeSinceOnboardingInHours: Int, timeStillToWaitInHours: Int)
 	
 	/// Precondition: there was already a key submission without a registered test, depending from configuration (for e.g. in the last 3 months)
 	case positiveTestResultWasAlreadySubmittedWithinThreshold(timeBetweenSubmissionsInDays: Int)
@@ -22,9 +22,11 @@ enum SRSPreconditionError: Error {
 				format: AppStrings.ExposureSubmissionDispatch.SRSWarnOthersPreconditionAlert.deviceCheckError,
 				errorCode
 			)
-		case .insufficientAppUsageTime:
+		case .insufficientAppUsageTime(let timeSinceOnboardingInHours, let timeStillToWaitInHours):
 			return String(
 				format: AppStrings.ExposureSubmissionDispatch.SRSWarnOthersPreconditionAlert.insufficientAppUsageTimeMessage,
+				String(timeSinceOnboardingInHours),
+				String(timeStillToWaitInHours),
 				errorCode
 			)
 		case let .positiveTestResultWasAlreadySubmittedWithinThreshold(timeBetweenSubmissionsInDays):
