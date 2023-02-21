0ee754c14cb2c81684a29aeadf554e50b13bb780
Omar Ahmed <omarabdelaziz125@gmail.com>
2023-02-21 10:10:24 +0100

    add EndOfLifeThankYouCellViewModel

---
+++ src/xcode/ENA/ENA/Source/Scenes/Home/Cells/EndOfLifeThankYou/EndOfLifeThankYouCellViewModel.swift
@@ -0,0 +1,22 @@
+//
+// 🦠 Corona-Warn-App
+//
+
+import UIKit
+
+class EndOfLifeThankYouCellViewModel {
+
+	// MARK: - Internal
+
+	var title = AppStrings.Home.EndOfLifeThankYouTile.title
+	// TODO get the xx value in figma
+	var description = String(
+		format: AppStrings.Home.EndOfLifeThankYouTile.description,
+		   4
+	   )
+	var image = UIImage(named: "EndOfLifeThankYouIllustration")
+	var tintColor: UIColor = .enaColor(for: .textPrimary1)
+
+	var titleAccessibilityIdentifier = AccessibilityIdentifiers.Home.EndOfLifeThankYouCell.titleLabel
+	var descriptionAccessibilityIdentifier = AccessibilityIdentifiers.Home.EndOfLifeThankYouCell.descriptionLabel
+}
