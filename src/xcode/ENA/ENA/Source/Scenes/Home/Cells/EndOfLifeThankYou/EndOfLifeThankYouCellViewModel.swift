0f35a41c65397409d83511e94f0a096b972fdfd8
Omar Ahmed <omarabdelaziz125@gmail.com>
2023-02-21 15:21:34 +0100

    Update src/xcode/ENA/ENA/Source/Scenes/Home/Cells/EndOfLifeThankYou/EndOfLifeThankYouCellViewModel.swift
    Co-authored-by: Felix Schmidt <io@felixschmidt.software>

--- src/xcode/ENA/ENA/Source/Scenes/Home/Cells/EndOfLifeThankYou/EndOfLifeThankYouCellViewModel.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Home/Cells/EndOfLifeThankYou/EndOfLifeThankYouCellViewModel.swift
@@ -9,7 +9,7 @@ class EndOfLifeThankYouCellViewModel {
 	// MARK: - Internal
 
 	var title = AppStrings.Home.EndOfLifeThankYouTile.title
-	// TODO get the xx value in figma
+	// to.do get the xx value in figma
 	var description = String(
 		format: AppStrings.Home.EndOfLifeThankYouTile.description,
 		   4
