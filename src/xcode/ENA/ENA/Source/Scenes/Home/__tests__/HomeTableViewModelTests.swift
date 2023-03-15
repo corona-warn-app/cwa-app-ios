f10538713a38b6fbd48766de76da2431fd9edc42
Omar Ahmed <omarabdelaziz125@gmail.com>
2023-03-15 16:03:00 +0100

    fix tests

--- src/xcode/ENA/ENA/Source/Scenes/Home/__tests__/HomeTableViewModelTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Home/__tests__/HomeTableViewModelTests.swift
@@ -10,7 +10,7 @@ import HealthCertificateToolkit
 // swiftlint:disable type_body_length
 class HomeTableViewModelTests: CWATestCase {
 
-	func testNumberOfSections_isHibernation_true_showAppClosureNotice_false() throws {
+	func testNumberOfSections_isHibernation_true() throws {
 		// GIVEN
 		let store = MockTestStore()
 
@@ -42,7 +42,6 @@ class HomeTableViewModelTests: CWATestCase {
 
 		// Is hibernation and should not show app closure notice
 		sut.isHibernationState = true
-		sut.shouldShowAppClosureNotice = false
 		
 		// THEN
 		
@@ -60,7 +59,7 @@ class HomeTableViewModelTests: CWATestCase {
 		XCTAssertEqual(sut.numberOfRows(in: 7), 1) // moreInfo visible
 	}
 	
-	func testNumberOfSections_isHibernation_true_showAppClosureNotice_true() throws {
+	func testNumberOfSections_isHibernation_false() throws {
 		// GIVEN
 		let store = MockTestStore()
 
@@ -91,8 +90,7 @@ class HomeTableViewModelTests: CWATestCase {
 		// WHEN
 
 		// Is hibernation and should show app closure notice
-		sut.isHibernationState = true
-		sut.shouldShowAppClosureNotice = true
+		sut.isHibernationState = false
 		
 		// THEN
 		
