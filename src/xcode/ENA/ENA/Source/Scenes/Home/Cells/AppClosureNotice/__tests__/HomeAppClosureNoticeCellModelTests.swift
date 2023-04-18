bbc515e19f45ffe526336a49d855ce92b32bfb2f
Naveed Khalid <nkhalid.here@gmail.com>
2023-04-18 16:40:07 +0500

    fixing swiftlint

--- src/xcode/ENA/ENA/Source/Scenes/Home/Cells/AppClosureNotice/__tests__/HomeAppClosureNoticeCellModelTests.swift
+++ src/xcode/ENA/ENA/Source/Scenes/Home/Cells/AppClosureNotice/__tests__/HomeAppClosureNoticeCellModelTests.swift
@@ -29,12 +29,13 @@ class HomeAppClosureNoticeCellModelTests: XCTestCase {
 			parameters: []
 		)
 
+		// swiftlint:disable line_length
 		let longText = DCCUIText(
 			type: "string",
 			quantity: nil,
 			quantityParameterIndex: nil,
 			functionName: nil,
-			localizedText: ["de": "Ab dem 1. Mai 2023 können Sie andere Personen hinsichtlich eines erhöhten Infektionsrisikos nicht mehr warnen und Sie erhalten keine Warnungen mehr über Risikobegegnungen. Ab dem 1. Juni 2023 wird die Corona-Warn-App nicht mehr weiterentwickelt. Auf Ihre in der App gespeicherten Zertifikate und das Kontakt-Tagebuch haben Sie jedoch weiterhin Zugriff. Allerdings können Sie keine neuen Zertifikate mehr hinzufügen."],
+			localizedText: ["de": "Ab dem 1. Mai 2023 können Sie andere Personen hinsichtlich eines erhöhten Infektionsrisikos nicht mehr warnen und Sie erhalten keine Warnungen mehr über Risikobegegnungen. Ab dem 1. Juni 2023 wird die Corona-Warn-App nicht mehr weiterentwickelt. Auf Ihre in der App gespeicherten Zertifikate und das Kontakt-Tagebuch haben Sie jedoch weiterhin Zugriff. Allerdings können Sie keine neuen Zertifikate mehr hinzufügen.", "en": "As of May 1, 2023, you will no longer be able to warn others of an increased risk of infection, nor will you receive warnings from your own risk exposures. Development of the Corona-Warn-App will be discontinued as of June 1, 2023. You will still be able to access the certificates you have saved in the app, as well as your contact diary, but you will not be able to add new certificates."],
 			parameters: []
 		)
 		
--- src/xcode/ENA/ENA/Source/Services/CCLService/CCLService.swift
+++ src/xcode/ENA/ENA/Source/Services/CCLService/CCLService.swift
@@ -315,8 +315,7 @@ class CCLService: CCLServable {
 			quantity: nil,
 			quantityParameterIndex: nil,
 			functionName: nil,
-			localizedText: ["de": "Achtung!",
-							"en": "Important!"],
+			localizedText: ["de": "Achtung!", "en": "Important!"],
 			parameters: []
 		)
 
@@ -325,17 +324,17 @@ class CCLService: CCLServable {
 			quantity: nil,
 			quantityParameterIndex: nil,
 			functionName: nil,
-			localizedText: ["de": "Es wird nur noch bis zum 30. April 2023 möglich sein, andere Personen über die Corona-Warn-App zu warnen!"],
+			localizedText: ["de": "Es wird nur noch bis zum 30. April 2023 möglich sein, andere Personen über die Corona-Warn-App zu warnen!", "en": "You will only be able to warn others through the Corona-Warn-App until April 30, 2023."],
 			parameters: []
 		)
 		
+		// swiftlint:disable line_length
 		let longText = DCCUIText(
 			type: "string",
 			quantity: nil,
 			quantityParameterIndex: nil,
 			functionName: nil,
-			localizedText: ["de": "Ab dem 1. Mai 2023 können Sie andere Personen hinsichtlich eines erhöhten Infektionsrisikos nicht mehr warnen und Sie erhalten keine Warnungen mehr über Risikobegegnungen. Ab dem 1. Juni 2023 wird die Corona-Warn-App nicht mehr weiterentwickelt. Auf Ihre in der App gespeicherten Zertifikate und das Kontakt-Tagebuch haben Sie jedoch weiterhin Zugriff. Allerdings können Sie keine neuen Zertifikate mehr hinzufügen.",
-							"en": "As of May 1, 2023, you will no longer be able to warn others of an increased risk of infection, nor will you receive warnings from your own risk exposures. Development of the Corona-Warn-App will be discontinued as of June 1, 2023. You will still be able to access the certificates you have saved in the app, as well as your contact diary, but you will not be able to add new certificates."],
+			localizedText: ["de": "Ab dem 1. Mai 2023 können Sie andere Personen hinsichtlich eines erhöhten Infektionsrisikos nicht mehr warnen und Sie erhalten keine Warnungen mehr über Risikobegegnungen. Ab dem 1. Juni 2023 wird die Corona-Warn-App nicht mehr weiterentwickelt. Auf Ihre in der App gespeicherten Zertifikate und das Kontakt-Tagebuch haben Sie jedoch weiterhin Zugriff. Allerdings können Sie keine neuen Zertifikate mehr hinzufügen.", "en": "As of May 1, 2023, you will no longer be able to warn others of an increased risk of infection, nor will you receive warnings from your own risk exposures. Development of the Corona-Warn-App will be discontinued as of June 1, 2023. You will still be able to access the certificates you have saved in the app, as well as your contact diary, but you will not be able to add new certificates."],
 			parameters: []
 		)
 		
