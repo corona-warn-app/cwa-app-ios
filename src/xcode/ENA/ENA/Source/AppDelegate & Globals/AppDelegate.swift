cc74c36bb00d088a5c3fe40bb4e01ee134d94b6d
Naveed Khalid <nkhalid.here@gmail.com>
2023-05-03 16:37:49 +0500

    changing location of disabling enf

--- src/xcode/ENA/ENA/Source/AppDelegate & Globals/AppDelegate.swift
+++ src/xcode/ENA/ENA/Source/AppDelegate & Globals/AppDelegate.swift
@@ -122,6 +122,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CoronaWarnAppDelegate, Re
 	) -> Bool {
 		Log.info("Application did finish launching.", log: .appLifecycle)
 
+		if CWAHibernationProvider.shared.isHibernationState {
+			applyEndOfLifeChanges()
+		}
+		
 		// Save and possibly log current app version number and the timestamp.
 		logCurrentAppVersion()
 		logCurrentCensoringState()
@@ -237,10 +241,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CoronaWarnAppDelegate, Re
 
 	func applicationDidBecomeActive(_ application: UIApplication) {
 		Log.info("Application did become active.", log: .appLifecycle)
-		
-		if CWAHibernationProvider.shared.isHibernationState {
-			applyEndOfLifeChanges()
-		}
+
 		
 		// If the UI was not setup before, and the app was NOT started from an user activity,
 		// 'applicationDidBecomeActive' is the last delegate callback and needs to build up the UI.
